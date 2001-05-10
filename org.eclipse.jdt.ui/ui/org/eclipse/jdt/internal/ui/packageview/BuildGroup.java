4bd8728653deadc9348a1fe378a401fb11383d76
cvs <>
2001-05-10 14:49:45 +0000

    Build action on all levels of package viewer

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/packageview/BuildGroup.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/packageview/BuildGroup.java
@@ -6,26 +6,7 @@ package org.eclipse.jdt.internal.ui.packageview;
  * (c) Copyright IBM Corp 1999, 2000
  */
 
-import java.util.Iterator;
-
-import org.eclipse.jface.action.IMenuManager;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
-
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.IncrementalProjectBuilder;
-
-import org.eclipse.ui.actions.BuildAction;
-
-import org.eclipse.jdt.core.IJavaElement;
-import org.eclipse.jdt.core.JavaModelException;
-
-import org.eclipse.jdt.ui.IContextMenuConstants;
-
-import org.eclipse.jdt.internal.ui.JavaPlugin;
-import org.eclipse.jdt.internal.ui.actions.ContextMenuGroup;
-import org.eclipse.jdt.internal.ui.actions.GroupContext;
+import java.util.HashSet;import java.util.Iterator;import org.eclipse.core.resources.IProject;import org.eclipse.core.resources.IResource;import org.eclipse.core.resources.IStorage;import org.eclipse.core.resources.IncrementalProjectBuilder;import org.eclipse.core.runtime.IPath;import org.eclipse.jdt.core.IJavaElement;import org.eclipse.jdt.core.JavaModelException;import org.eclipse.jdt.internal.ui.JavaPlugin;import org.eclipse.jdt.internal.ui.actions.ContextMenuGroup;import org.eclipse.jdt.internal.ui.actions.GroupContext;import org.eclipse.jdt.ui.IContextMenuConstants;import org.eclipse.jface.action.IMenuManager;import org.eclipse.jface.viewers.ISelection;import org.eclipse.jface.viewers.IStructuredSelection;import org.eclipse.jface.viewers.StructuredSelection;import org.eclipse.ui.actions.BuildAction;
 
 
 public class BuildGroup extends ContextMenuGroup {
@@ -39,19 +20,23 @@ public class BuildGroup extends ContextMenuGroup {
 		if (! shouldContribute(context))
 			return;
 			
-		if (fBuildAction == null)
+		if (fBuildAction == null) {
 			fBuildAction= new BuildAction(JavaPlugin.getActiveWorkbenchShell(),
 				IncrementalProjectBuilder.INCREMENTAL_BUILD);
+			fBuildAction.setText("Build Project");
+		}
 		
-		fBuildAction.selectionChanged((IStructuredSelection)context.getSelection());
+		
+		fBuildAction.selectionChanged(convertSelectionToProjects((IStructuredSelection)context.getSelection()));
 		manager.appendToGroup(GROUP_NAME, fBuildAction);
 		
 		if (fFullBuildAction == null) {
 			fFullBuildAction= new BuildAction(JavaPlugin.getActiveWorkbenchShell(),
 				IncrementalProjectBuilder.FULL_BUILD);
+			fFullBuildAction.setText("Rebuild Project");
 		}
 		
-		fFullBuildAction.selectionChanged((IStructuredSelection)context.getSelection());
+		fFullBuildAction.selectionChanged(convertSelectionToProjects((IStructuredSelection)context.getSelection()));
 		manager.appendToGroup(GROUP_NAME, fFullBuildAction);
 	}
 	
@@ -60,16 +45,16 @@ public class BuildGroup extends ContextMenuGroup {
 		if (! (s instanceof IStructuredSelection))
 			return false;
 			
-		IStructuredSelection selection= (IStructuredSelection)s;
+		IStructuredSelection selection= convertSelectionToProjects((IStructuredSelection)s);
 		if (selection.isEmpty())
 			return false;
 			
-		Iterator iter= selection.iterator();
+		/*Iterator iter= selection.iterator();
 		while (iter.hasNext()) {
 			IResource res= getResourceFor(iter.next());
 			if (!(res instanceof IProject))
 				return false;
-		}
+		}*/
 		return true;
 	}
 	
@@ -86,4 +71,26 @@ public class BuildGroup extends ContextMenuGroup {
 		}
 		return (IResource)element;
 	}		
+	
+	private IStructuredSelection convertSelectionToProjects(IStructuredSelection selection) {
+		HashSet result= new HashSet();
+		Iterator elements= selection.iterator();
+		while (elements.hasNext()) {
+			IProject p= getProjectFor(elements.next());
+			if (p != null)
+				result.add(p);
+		}
+		Object[] projects= new Object[result.size()];
+		projects= result.toArray(projects);
+		return new StructuredSelection(projects);
+	}
+	
+	private IProject getProjectFor(Object element) {
+		if (element instanceof IJavaElement) {
+			return ((IJavaElement)element).getJavaProject().getProject();
+		} else if (element instanceof IResource) {
+			return ((IResource)element).getProject();
+		}
+		return null;
+	}
 }
