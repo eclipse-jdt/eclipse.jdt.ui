af1d37e5b59fb963554a1c0a67fa3728e1e4f367
cvs <>
2001-05-14 16:01:24 +0000

    ma: classpath vars cut2

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/IUIConstants.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/IUIConstants.java
@@ -3,12 +3,18 @@
  * WebSphere Studio Workbench
  * (c) Copyright IBM Corp 1999, 2000
  */
-package org.eclipse.jdt.internal.ui;
+package org.eclipse.jdt.internal.ui;import org.eclipse.jdt.ui.JavaUI;
 
 
 public interface IUIConstants {
-	public static final String KEY_OK= "org.eclipse.jdt.ui.ok.label";
-	public static final String KEY_CANCEL= "org.eclipse.jdt.ui.cancel.label";
 	
-	public static final String P_ICON_NAME= "org.eclipse.jdt.ui.icon_name";
+	public static final String KEY_OK= JavaUI.ID_PLUGIN + ".ok.label";
+	public static final String KEY_CANCEL= JavaUI.ID_PLUGIN + ".cancel.label";
+	
+	public static final String P_ICON_NAME= JavaUI.ID_PLUGIN + ".icon_name";
+	
+	public static final String DIALOGSTORE_LASTEXTJAR= JavaUI.ID_PLUGIN + ".lastextjar";
+	public static final String DIALOGSTORE_LASTJARATTACH= JavaUI.ID_PLUGIN + ".lastjarattach";
+	
+	
 }
