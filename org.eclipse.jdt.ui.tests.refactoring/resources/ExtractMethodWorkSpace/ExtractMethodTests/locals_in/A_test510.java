c9cffbe25705fd9e4d54547278092a5c48099e84
cvs <>
2001-05-21 08:22:15 +0000

    New test cases

---
+++ org.eclipse.jdt.ui.tests.refactoring/resources/ExtractMethodWorkSpace/ExtractMethodTests/locals_in/A_test510.java
@@ -0,0 +1,10 @@
+package locals_in;
+public class A_test510 {
+	public void foo() {
+		/*]*/foo();
+		int i= 10;
+		i= 2;/*[*/
+		
+		i++;
+	}
+}
---
+++ org.eclipse.jdt.ui.tests.refactoring/resources/ExtractMethodWorkSpace/ExtractMethodTests/locals_out/A_test510.java
@@ -0,0 +1,14 @@
+package locals_out;
+public class A_test510 {
+	public void foo() {
+		/*]*/int i= extracted();/*[*/
+		
+		i++;
+	}
+	protected int extracted() {
+		foo();
+		int i= 10;
+		i= 2;
+		return i;
+	}
+}
