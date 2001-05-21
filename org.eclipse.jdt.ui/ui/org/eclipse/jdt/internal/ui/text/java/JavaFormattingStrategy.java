3a32e344df67496c66bd3dc8fb7ba2a9e5172600
cvs <>
2001-05-21 12:40:03 +0000

    CD: Bugfixes, and new Code for positions updating

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/java/JavaFormattingStrategy.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/java/JavaFormattingStrategy.java
@@ -7,7 +7,7 @@ package org.eclipse.jdt.internal.ui.text.java;
  */
  
 
-import org.eclipse.jdt.internal.compiler.ConfigurableOption;import org.eclipse.jdt.internal.formatter.CodeFormatter;import org.eclipse.jdt.internal.ui.JavaPlugin;import org.eclipse.jface.text.formatter.IFormattingStrategy;import org.eclipse.jface.text.source.ISourceViewer; 
+import org.eclipse.jdt.internal.compiler.ConfigurableOption;import org.eclipse.jdt.internal.formatter.CodeFormatter;import org.eclipse.jdt.internal.ui.JavaPlugin;import org.eclipse.jface.text.BadLocationException;import org.eclipse.jface.text.IDocument;import org.eclipse.jface.text.formatter.IFormattingStrategy;import org.eclipse.jface.text.source.ISourceViewer; 
 
 public class JavaFormattingStrategy implements IFormattingStrategy {
 	
@@ -16,6 +16,11 @@ public class JavaFormattingStrategy implements IFormattingStrategy {
 	private final static int CLEAR_ALL_NEWLINES_OPTION = 3;
 	
 
+	public JavaFormattingStrategy(ISourceViewer viewer)
+	{
+		fViewer = viewer;
+	}
+	
 	/**
 	 * @see IFormattingStrategy#formatterStarts(String)
 	 */
@@ -36,6 +41,7 @@ public class JavaFormattingStrategy implements IFormattingStrategy {
 		
 		ConfigurableOption[] options = JavaPlugin.getDefault().getCodeFormatterOptions();
 
+		System.out.println(content);
 		boolean clearAllNewlines = false;
 
 		for (int i=0; i < options.length; i++)
@@ -52,8 +58,10 @@ public class JavaFormattingStrategy implements IFormattingStrategy {
 
 		String lineSeparator= null;
 		try {
-			lineSeparator = fViewer.getDocument().getLineDelimiter(1);
-		} catch (Exception e) {}
+			IDocument document = fViewer.getDocument();
+			if (document != null)	
+				lineSeparator = document.getLineDelimiter(1);
+		} catch (Exception e) {e.printStackTrace();}
 
 		if (lineSeparator == null)
 			lineSeparator = System.getProperty("line.separator");
@@ -67,25 +75,40 @@ public class JavaFormattingStrategy implements IFormattingStrategy {
 		{
 			reverseIndex--;
 		}
-		if (positions != null)
-			formatter.setPositionsToMap(positions);
+		
+	
 
-		formatter.setInitialIndentationLevel(fInitialIndentation == null ? 0 : fInitialIndentation.length());
-
-		String result= null;
-
+		String result= null;	
+			
 		if (reverseIndex >= index)
 		{
-			result = formatter.formatSourceString(content.substring(index, reverseIndex+1));
-
-			if (positions != null) {
-				int[] newPositions= formatter.getMappedPositions();
-				for (int i= 0; i < positions.length; i++)
-				positions[i]= newPositions[i];
+			int counter=0;
+			int firstPosition=0;
+			if (positions != null && positions.length != 0)
+			{
+				for (int i=0; i < positions.length; i++)
+				{
+					if (positions[i] >= index || positions[i] <= reverseIndex)
+					{
+						if (counter ==0)
+							firstPosition=i;
+						counter++;
+					}
+				}
+				int[] activePositions = new int[counter];
+				for (int i=0; i < counter; i++)
+					activePositions[i] = positions[i+firstPosition] - index;
+				formatter.setPositionsToMap(activePositions);
 			}
-		}		
-		if (reverseIndex >= index){
-				return content.substring(0, index) + result + content.substring(reverseIndex+1);
+			formatter.setInitialIndentationLevel(fInitialIndentation == null ? 0 : fInitialIndentation.length());
+			result = formatter.formatSourceString(content.substring(index, reverseIndex+1));
+			if (positions != null && positions.length !=0) {
+					int[] newPositions= formatter.getMappedPositions();
+				for (int i= 0; i < counter; i++)
+					positions[i+firstPosition] = newPositions[i] + index;
+				System.out.println("");
+			}
+			return content.substring(0, index) + result + content.substring(reverseIndex+1);
 		}
 		else
 			return content;
