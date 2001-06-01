0bca5b4b9d532a7999df9d9ef27b15714ad57116
cvs <>
2001-06-01 17:33:09 +0000

    (ak) open type speedup

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/dialogs/TypeSelectionDialog.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/dialogs/TypeSelectionDialog.java
@@ -4,27 +4,7 @@
  */
 package org.eclipse.jdt.internal.ui.dialogs;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.widgets.Shell;
-
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.operation.IRunnableContext;
-import org.eclipse.jface.util.Assert;
-import org.eclipse.jface.viewers.LabelProvider;
-
-import org.eclipse.jdt.core.IType;
-import org.eclipse.jdt.core.JavaModelException;
-import org.eclipse.jdt.core.search.IJavaSearchScope;
-
-import org.eclipse.jdt.internal.ui.JavaPlugin;
-import org.eclipse.jdt.internal.ui.JavaPluginImages;
-import org.eclipse.jdt.internal.ui.util.AllTypesSearchEngine;
-import org.eclipse.jdt.internal.ui.util.JdtHackFinder;
-import org.eclipse.jdt.internal.ui.util.TypeRef;
-import org.eclipse.jdt.internal.ui.util.TypeRefLabelProvider;
+import java.util.ArrayList;import java.util.List;import org.eclipse.swt.widgets.Shell;import org.eclipse.jface.dialogs.MessageDialog;import org.eclipse.jface.operation.IRunnableContext;import org.eclipse.jface.util.Assert;import org.eclipse.jdt.core.ElementChangedEvent;import org.eclipse.jdt.core.ICompilationUnit;import org.eclipse.jdt.core.IElementChangedListener;import org.eclipse.jdt.core.IField;import org.eclipse.jdt.core.IImportContainer;import org.eclipse.jdt.core.IImportDeclaration;import org.eclipse.jdt.core.IInitializer;import org.eclipse.jdt.core.IJavaElement;import org.eclipse.jdt.core.IJavaElementDelta;import org.eclipse.jdt.core.IMethod;import org.eclipse.jdt.core.IPackageDeclaration;import org.eclipse.jdt.core.IType;import org.eclipse.jdt.core.IWorkingCopy;import org.eclipse.jdt.core.JavaCore;import org.eclipse.jdt.core.JavaModelException;import org.eclipse.jdt.internal.ui.JavaPlugin;import org.eclipse.jdt.internal.ui.util.AllTypesSearchEngine;import org.eclipse.jdt.internal.ui.util.JdtHackFinder;import org.eclipse.jdt.internal.ui.util.TypeRef;import org.eclipse.jdt.internal.ui.util.TypeRefLabelProvider;import org.eclipse.jdt.core.search.IJavaSearchScope;
 
 
 /**
@@ -38,6 +18,9 @@ public class TypeSelectionDialog extends TwoPaneElementSelector {
 	
 	private final static String PREFIX= "type_selector.";
 	private final static String NO_MAPPING_PREFIX= PREFIX+"no_mapping.";
+	
+	private static List fgAllTypesList;
+	private static IElementChangedListener fgListener;
 			
 	public TypeSelectionDialog(Shell parent, IRunnableContext context, IJavaSearchScope scope, int style, boolean ignoreCase, boolean matchEmtpyString) {
 		super(parent, "", null, new TypeRefLabelProvider(0), new TypeRefLabelProvider(TypeRefLabelProvider.SHOW_PACKAGE_ONLY + TypeRefLabelProvider.SHOW_ROOT_POSTFIX), ignoreCase, matchEmtpyString);		
@@ -55,12 +38,18 @@ public class TypeSelectionDialog extends TwoPaneElementSelector {
 	 */
 	public int open() {
 		AllTypesSearchEngine engine= new AllTypesSearchEngine(JavaPlugin.getWorkspace());
-		List typesFound= engine.searchTypes(fRunnableContext, fScope, fStyle);
+		if (fgAllTypesList == null){
+			fgAllTypesList= engine.searchTypes(fRunnableContext, fScope, fStyle);
+			if (fgListener == null){
+				fgListener= new DeltaListener();
+				JavaCore.addElementChangedListener(fgListener);
+			}	
+		}	
 
-		if (typesFound.size() == 0)
+		if (fgAllTypesList.isEmpty())
 			return CANCEL;
 			
-		TypeRef[] typeRefs= (TypeRef[])typesFound.toArray(new TypeRef[typesFound.size()]);
+		TypeRef[] typeRefs= (TypeRef[])fgAllTypesList.toArray(new TypeRef[fgAllTypesList.size()]);
 		setElements(typeRefs);
 		setInitialSelection("A");
 		return super.open();
@@ -91,6 +80,83 @@ public class TypeSelectionDialog extends TwoPaneElementSelector {
 				MessageDialog.openError(getShell(), title, message);
 				setResult(null);
 			}
+		}
+	}
+	
+	private static class DeltaListener implements IElementChangedListener{
+		public void elementChanged(ElementChangedEvent event){
+			if (fgAllTypesList == null)
+				return;
+				
+			IJavaElementDelta delta= event.getDelta();
+			IJavaElement element= delta.getElement();
+			int type= element.getElementType();
+			
+			if (type == IJavaElement.CLASS_FILE)
+				return;				
+			
+			processDelta(delta);
+		}
+		
+		private boolean mustFlush(IJavaElementDelta delta){
+			if (delta.getKind() != IJavaElementDelta.CHANGED)
+				return true;
+			
+			//if it's a cu we wait
+			if (delta.getElement().getElementType() == IJavaElement.COMPILATION_UNIT) 
+				return false;
+			
+			//must be only children
+			if (delta.getFlags() != IJavaElementDelta.F_CHILDREN)
+				return true;
+			
+			//special case: if it's a type then _it_ must be added or removed
+			if (delta.getElement().getElementType() == IJavaElement.TYPE) 
+				return false;
+				
+			if ((delta.getAddedChildren() != null) && (delta.getAddedChildren().length != 0))
+				return true;
+				
+			return false;	
+		}
+		
+		/*
+		 * returns false iff list is flushed and we can stop processing
+		 */
+		private boolean processDelta(IJavaElementDelta delta){
+			if (shouldStopProcessing(delta))
+				return true;
+				
+			if (mustFlush(delta)){
+				fgAllTypesList= null;
+				return false;
+			}	
+			IJavaElementDelta[] affectedChildren= delta.getAffectedChildren();
+			if (affectedChildren == null)
+				return true;
+				
+			for (int i= 0; i < affectedChildren.length; i++){
+				if (! processDelta(affectedChildren[i]))
+					return false;
+			}	
+			return true;
+		}
+		
+		private static boolean shouldStopProcessing(IJavaElementDelta delta){
+			int type= delta.getElement().getElementType();
+			if (type == IJavaElement.FIELD)
+				return true;
+			if (type == IJavaElement.METHOD)
+				return true;
+			if (type == IJavaElement.INITIALIZER)
+				return true;
+			if (type == IJavaElement.PACKAGE_DECLARATION)
+				return true;
+			if (type == IJavaElement.IMPORT_CONTAINER)
+				return true;	
+			if (type == IJavaElement.IMPORT_DECLARATION)
+				return true;		
+			return false;	
 		}
 	}
 }
