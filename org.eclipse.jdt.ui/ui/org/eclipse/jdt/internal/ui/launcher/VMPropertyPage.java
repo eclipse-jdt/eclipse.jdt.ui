8d39a0a9fdc5fc303ad32c655705bcab70c607f1
cvs <>
2001-06-02 15:09:56 +0000

    1GEP3HM: ITPJUI:ALL - Properties dialog gets stuck on "Launcher" contents

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/launcher/VMPropertyPage.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/launcher/VMPropertyPage.java
@@ -5,13 +5,17 @@
 
 package org.eclipse.jdt.internal.ui.launcher;
 
-import org.eclipse.swt.widgets.Composite;import org.eclipse.swt.widgets.Control;import org.eclipse.core.runtime.CoreException;import org.eclipse.core.runtime.IAdaptable;import org.eclipse.jface.viewers.ISelectionChangedListener;import org.eclipse.jface.viewers.SelectionChangedEvent;import org.eclipse.ui.help.DialogPageContextComputer;import org.eclipse.ui.help.WorkbenchHelp;import org.eclipse.jdt.core.IJavaProject;import org.eclipse.jdt.internal.ui.IJavaHelpContextIds;import org.eclipse.jdt.launching.JavaRuntime;
+import org.eclipse.core.runtime.CoreException;import org.eclipse.core.runtime.IAdaptable;import org.eclipse.jdt.core.IJavaProject;import org.eclipse.jdt.internal.ui.IJavaHelpContextIds;import org.eclipse.jdt.launching.IVMInstall;import org.eclipse.jdt.launching.JavaRuntime;import org.eclipse.jface.viewers.ISelectionChangedListener;import org.eclipse.jface.viewers.SelectionChangedEvent;import org.eclipse.swt.SWT;import org.eclipse.swt.layout.GridData;import org.eclipse.swt.layout.GridLayout;import org.eclipse.swt.widgets.Button;import org.eclipse.swt.widgets.Composite;import org.eclipse.swt.widgets.Control;import org.eclipse.swt.widgets.Event;import org.eclipse.swt.widgets.Listener;import org.eclipse.ui.help.DialogPageContextComputer;import org.eclipse.ui.help.WorkbenchHelp;
 
 /*
  * The page for setting java runtime
  */
 public class VMPropertyPage extends JavaProjectPropertyPage {
 	private VMSelector fVMSelector;
+	private Control fVMSelectorWidget;
+	private Button fUseDefault;
+	private Button fUseCustom;
+	private boolean fFirstTime= true;
 	
 	public VMPropertyPage() {
 		fVMSelector= new VMSelector();
@@ -19,23 +23,64 @@ public class VMPropertyPage extends JavaProjectPropertyPage {
 	
 	protected Control createJavaContents(Composite ancestor) {
 		noDefaultAndApplyButton();
-		Control vmSelector= fVMSelector.createContents(ancestor);
-		fVMSelector.initFromProject(getJavaProject());
+		Composite parent= new Composite(ancestor, SWT.NULL);
+		parent.setLayout(new GridLayout());
+		
+		fUseDefault= new Button(parent, SWT.RADIO);
+		fUseDefault.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		fUseDefault.setText("Use default JRE for launching");
+		fUseDefault.addListener(SWT.Selection, new Listener() {
+			public void handleEvent(Event evt) {
+				enableCustom(false);
+			}
+		});
+		
+		fUseCustom= new Button(parent, SWT.RADIO);
+		fUseCustom.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		fUseCustom.setText("Use custom JRE for launching");
+		fUseCustom.addListener(SWT.Selection, new Listener() {
+			public void handleEvent(Event evt) {
+				enableCustom(true);
+			}
+		});
+
+		fVMSelectorWidget= fVMSelector.createContents(parent);
 		fVMSelector.addSelectionChangedListener(new ISelectionChangedListener() {
 			public void selectionChanged(SelectionChangedEvent event) {
 				setValid(fVMSelector.validateSelection(event.getSelection()));
 			}
 		});
+		fVMSelectorWidget.setLayoutData(new GridData(GridData.FILL_BOTH));
 		WorkbenchHelp.setHelp(ancestor, new DialogPageContextComputer(this, IJavaHelpContextIds.LAUNCH_JRE_PROPERYY_PAGE));			
-		return vmSelector;
+		return parent;
 	}
-
+	
+	/**
+	 * Must be called after createContents
+	 */ 
+	public void initFromProject(IJavaProject project) {
+		IVMInstall vm= null;
+		try {
+			vm= JavaRuntime.getVMInstall(project);
+		} catch (CoreException e) {
+		}
+		enableCustom(vm != null);
+		fUseCustom.setSelection(vm != null);
+		fUseDefault.setSelection(vm == null);
+		if (vm == null) {
+			vm= JavaRuntime.getDefaultVMInstall();
+		}
+		fVMSelector.selectVM(vm);
+	}
 	
 	protected boolean performJavaOk() {
 		IJavaProject project= getJavaProject();
 		if (project != null) {
 			try {
-				JavaRuntime.setVM(project, fVMSelector.getSelectedVM());
+				IVMInstall vm= null;
+				if (fUseCustom.getSelection()) 
+					 vm= fVMSelector.getSelectedVM();
+				JavaRuntime.setVM(project, vm);
 			} catch (CoreException e) {
 			}
 		}
@@ -45,11 +90,19 @@ public class VMPropertyPage extends JavaProjectPropertyPage {
 	public void setElement(IAdaptable element) {
 		super.setElement(element);
 		if (getJavaProject() != null && isOpenProject())
-			setDescription("Select the JRE for running Java programs");
+			setDescription("Select the JRE for launching Java programs");
 	} 
 	
 	public void setVisible(boolean visible) {
-		setTitle("Java Runtime Environment");		
+		if (visible && fFirstTime) {
+			fFirstTime= false;
+			setTitle("Java Runtime Environment Launch Settings");		
+		}
+		initFromProject(getJavaProject());
+		super.setVisible(visible);
 	}
-
+	
+	private void enableCustom(boolean useCustom) {
+		fVMSelectorWidget.setEnabled(useCustom);
+	}
 }
--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/launcher/VMSelector.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/launcher/VMSelector.java
@@ -1,46 +1,32 @@
-/* * (c) Copyright IBM Corp. 2000, 2001. * All Rights Reserved. */package org.eclipse.jdt.internal.ui.launcher;import java.util.ArrayList;import java.util.List;import org.eclipse.core.runtime.CoreException;import org.eclipse.jdt.core.IJavaProject;import org.eclipse.jdt.launching.IVMInstall;import org.eclipse.jdt.launching.JavaRuntime;import org.eclipse.jface.viewers.CheckStateChangedEvent;import org.eclipse.jface.viewers.CheckboxTableViewer;import org.eclipse.jface.viewers.ColumnWeightData;import org.eclipse.jface.viewers.ICheckStateListener;import org.eclipse.jface.viewers.ISelection;import org.eclipse.jface.viewers.ISelectionChangedListener;import org.eclipse.jface.viewers.ISelectionProvider;import org.eclipse.jface.viewers.IStructuredSelection;import org.eclipse.jface.viewers.SelectionChangedEvent;import org.eclipse.jface.viewers.StructuredSelection;import org.eclipse.jface.viewers.TableLayout;import org.eclipse.swt.SWT;import org.eclipse.swt.widgets.Composite;import org.eclipse.swt.widgets.Control;import org.eclipse.swt.widgets.Table;import org.eclipse.swt.widgets.TableColumn;
+/* * (c) Copyright IBM Corp. 2000, 2001. * All Rights Reserved. */package org.eclipse.jdt.internal.ui.launcher;import org.eclipse.jdt.launching.IVMInstall;import org.eclipse.jdt.launching.JavaRuntime;import org.eclipse.jface.viewers.ColumnWeightData;import org.eclipse.jface.viewers.ISelection;import org.eclipse.jface.viewers.ISelectionChangedListener;import org.eclipse.jface.viewers.IStructuredSelection;import org.eclipse.jface.viewers.StructuredSelection;import org.eclipse.jface.viewers.TableLayout;import org.eclipse.jface.viewers.TableViewer;import org.eclipse.jface.viewers.Viewer;import org.eclipse.jface.viewers.ViewerSorter;import org.eclipse.swt.SWT;import org.eclipse.swt.widgets.Composite;import org.eclipse.swt.widgets.Control;import org.eclipse.swt.widgets.Table;import org.eclipse.swt.widgets.TableColumn;
 
-public class VMSelector implements ISelectionProvider {
-	private CheckboxTableViewer fVMList;	private List fListeners;		public VMSelector() {		fListeners= new ArrayList();	}
+public class VMSelector {
+	private TableViewer fVMList;		public VMSelector() {	}
 	
 	protected Control createContents(Composite ancestor) {		
 
-		fVMList= new CheckboxTableViewer(ancestor, SWT.BORDER | SWT.SINGLE | SWT.HIDE_SELECTION);		fVMList.setLabelProvider(new VMLabelProvider());		fVMList.setContentProvider(new VMContentProvider());			Table table= fVMList.getTable();				fVMList.addCheckStateListener(new ICheckStateListener() {			public void checkStateChanged(CheckStateChangedEvent event) {				IVMInstall vm=  (IVMInstall)event.getElement();				if (event.getChecked())					fVMList.setCheckedElements(new Object[] { vm });				fireSelectedVMChanged(vm);			}		});		table.setHeaderVisible(true);		table.setLinesVisible(true);				TableLayout tableLayout= new TableLayout();		table.setLayout(tableLayout);				TableColumn column1= table.getColumn(0);		column1.setText("JRE Type");		tableLayout.addColumnData(new ColumnWeightData(30));			TableColumn column2= new TableColumn(table, SWT.NULL);		column2.setText("Name");		tableLayout.addColumnData(new ColumnWeightData(30));				TableColumn column3= new TableColumn(table, SWT.NULL);		column3.setText("Location");		tableLayout.addColumnData(new ColumnWeightData(50));				fVMList.setInput(JavaRuntime.getVMInstallTypes());		
+		fVMList= new TableViewer(ancestor, SWT.BORDER | SWT.SINGLE | SWT.FULL_SELECTION);		fVMList.setLabelProvider(new VMLabelProvider());		fVMList.setContentProvider(new VMContentProvider());			Table table= fVMList.getTable();				fVMList.setSorter(new ViewerSorter() {			public int compare(Viewer viewer, Object e1, Object e2) {				if ((e1 instanceof IVMInstall) && (e2 instanceof IVMInstall)) {					IVMInstall left= (IVMInstall)e1;					IVMInstall right= (IVMInstall)e2;					String leftType= left.getVMInstallType().getName();					String rightType= right.getVMInstallType().getName();					int res= leftType.compareToIgnoreCase(rightType);					if (res != 0)						return res;					return left.getName().compareToIgnoreCase(right.getName());				}				return super.compare(viewer, e1, e2);			}						public boolean isSorterProperty(Object element, String property) {				return true;			}		});		table.setHeaderVisible(true);		table.setLinesVisible(true);				TableLayout tableLayout= new TableLayout();		table.setLayout(tableLayout);				TableColumn column1= new TableColumn(table, SWT.NULL);		column1.setText("JRE Type");		tableLayout.addColumnData(new ColumnWeightData(30));			TableColumn column2= new TableColumn(table, SWT.NULL);		column2.setText("Name");		tableLayout.addColumnData(new ColumnWeightData(30));				TableColumn column3= new TableColumn(table, SWT.NULL);		column3.setText("Location");		tableLayout.addColumnData(new ColumnWeightData(50));				fVMList.setInput(JavaRuntime.getVMInstallTypes());		
 		return table;
-	}
-
-		
-	/**
-	 * Must be called after createContents
-	 */ 
-	public void initFromProject(IJavaProject project) {
-		IVMInstall vm= null;
-		try {
-			selectVM(JavaRuntime.getVMInstall(project));
-		} catch (CoreException e) {
-		}
-	}
-	
+	}		
 	public void selectVM(IVMInstall vm) {
 		if (vm != null)
-			fVMList.setCheckedElements(new Object[] { vm });
+			fVMList.setSelection(new StructuredSelection(vm));
 	}
 	
 	public IVMInstall getSelectedVM() {
-		Object[] selection= fVMList.getCheckedElements();
-		if (selection == null || selection.length != 1)
+		IStructuredSelection selection= (IStructuredSelection)fVMList.getSelection();
+		if (selection == null || selection.size() != 1)
 			return null;
-		return (IVMInstall)selection[0];
+		return (IVMInstall)selection.getFirstElement();
 	}
 	
 	public void addSelectionChangedListener(ISelectionChangedListener l) {
-		fListeners.add(l);
+		fVMList.addSelectionChangedListener(l);
 	}
 	
 	public void removeSelectionChangedListener(ISelectionChangedListener l) {
-		fListeners.remove(l);
-	}		private void fireSelectedVMChanged(IVMInstall vmInstall) {		ISelection selection= null;		if (vmInstall != null)			selection= new StructuredSelection(vmInstall);		else 			selection= new StructuredSelection();		for (int i= 0; i < fListeners.size(); i++) {			ISelectionChangedListener l= (ISelectionChangedListener)fListeners.get(i);			l.selectionChanged(new SelectionChangedEvent(this, selection));		}	}
-	
+		fVMList.removeSelectionChangedListener(l);
+	}		
 	public boolean validateSelection(ISelection sel) {
 		if (sel instanceof IStructuredSelection) {
 			IStructuredSelection selection= (IStructuredSelection)sel;
@@ -50,5 +36,4 @@ public class VMSelector implements ISelectionProvider {
 		} else {
 			return false;
 		}
-	}		public ISelection getSelection() {		return new StructuredSelection(getSelectedVM());	}	/**	 * does nothing	 */	public void setSelection(ISelection selection) {	}
-}
+	}	}
