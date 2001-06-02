7645e90b12569613f5eb85ba09a6bf4f3710b65d
cvs <>
2001-06-02 14:39:50 +0000

    Secon part of Type Hierarchy work [db]

---
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/typehierarchy/FocusOnSelectionAction.java
@@ -0,0 +1,50 @@
+/*
+ * (c) Copyright IBM Corp. 2000, 2001.
+ * All Rights Reserved.
+ */
+package org.eclipse.jdt.internal.ui.typehierarchy;
+
+import org.eclipse.jface.viewers.ISelectionProvider;import org.eclipse.ui.help.WorkbenchHelp;import org.eclipse.ui.texteditor.IUpdate;import org.eclipse.jdt.core.IType;import org.eclipse.jdt.internal.ui.IJavaHelpContextIds;import org.eclipse.jdt.internal.ui.JavaPlugin;import org.eclipse.jdt.internal.ui.actions.JavaUIAction;import org.eclipse.jdt.internal.ui.util.SelectionUtil;
+
+/**
+ * Refocuses the type hierarchy on the currently selection type.
+ */
+public class FocusOnSelectionAction extends JavaUIAction implements IUpdate {
+	
+	private static final String PREFIX= "FocusOnSelectionAction.";
+		
+	private TypeHierarchyViewPart fViewPart;
+	private ISelectionProvider fSelectionProvider;
+	private String fText;
+	
+	public FocusOnSelectionAction(TypeHierarchyViewPart part, ISelectionProvider selProvider) {
+		super(JavaPlugin.getResourceBundle(), PREFIX);
+		fViewPart= part;
+		fSelectionProvider= selProvider;
+		fText= JavaPlugin.getResourceString(PREFIX + "label");
+		
+		WorkbenchHelp.setHelp(this,	new Object[] { IJavaHelpContextIds.FOCUS_ON_SELECTION_ACTION });
+	}
+	/**
+	 *orm the action
+	 */
+	public void run() {
+		Object element= SelectionUtil.getSingleElement(fSelectionProvider.getSelection());
+		if (element instanceof IType) {
+			fViewPart.setInput((IType)element);
+		}
+	}
+	
+	public void update() {
+		setEnabled(canActionBeAdded());
+	}
+	
+	public boolean canActionBeAdded() {
+		Object element= SelectionUtil.getSingleElement(fSelectionProvider.getSelection());
+		if (element instanceof IType) {
+			setText(fText + " \"" + ((IType)element).getElementName() + "\"");
+			return true;
+		}
+		return false;
+	}
+}
---
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/typehierarchy/FocusOnTypeAction.java
@@ -0,0 +1,46 @@
+/*
+ * (c) Copyright IBM Corp. 2000, 2001.
+ * All Rights Reserved.
+ */
+package org.eclipse.jdt.internal.ui.typehierarchy;
+
+import org.eclipse.swt.widgets.Shell;import org.eclipse.jface.dialogs.IDialogConstants;import org.eclipse.jface.dialogs.ProgressMonitorDialog;import org.eclipse.jface.viewers.ISelectionProvider;import org.eclipse.ui.help.WorkbenchHelp;import org.eclipse.jdt.core.IType;import org.eclipse.jdt.core.search.SearchEngine;import org.eclipse.jdt.internal.ui.IJavaHelpContextIds;import org.eclipse.jdt.internal.ui.JavaPlugin;import org.eclipse.jdt.internal.ui.actions.JavaUIAction;import org.eclipse.jdt.internal.ui.dialogs.TypeSelectionDialog;import org.eclipse.jdt.ui.IJavaElementSearchConstants;
+
+/**
+ * Refocuses the type hierarchy on a type selection from a all types dialog.
+ */
+public class FocusOnTypeAction extends JavaUIAction {
+	
+	private static final String PREFIX= "FocusOnTypeAction.";
+		
+	private TypeHierarchyViewPart fViewPart;
+	private ISelectionProvider fSelectionProvider;
+	
+	public FocusOnTypeAction(TypeHierarchyViewPart part) {
+		super(JavaPlugin.getResourceBundle(), PREFIX);
+		fViewPart= part;
+		
+		WorkbenchHelp.setHelp(this,	new Object[] { IJavaHelpContextIds.FOCUS_ON_TYPE_ACTION });
+	}
+	/**
+	 *orm the action
+	 */
+	public void run() {
+		Shell parent= fViewPart.getSite().getWorkbenchWindow().getShell();
+		TypeSelectionDialog dialog= new TypeSelectionDialog(parent, new ProgressMonitorDialog(parent), 
+			SearchEngine.createWorkspaceScope(), IJavaElementSearchConstants.CONSIDER_TYPES, 
+			true, true);				
+	
+		dialog.setTitle(JavaPlugin.getResourceString(PREFIX + "dialog.title"));
+		dialog.setMessage(JavaPlugin.getResourceString(PREFIX + "dialog.message"));
+		if (dialog.open() == IDialogConstants.CANCEL_ID) {
+			return;
+		}
+		
+		Object[] types= dialog.getResult();
+		if (types != null && types.length > 0) {
+			IType type= (IType)types[0];
+			fViewPart.setInput(type);
+		}
+	}	
+}
