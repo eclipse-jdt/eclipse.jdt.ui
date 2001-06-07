4072f49ce94a79bd3b35388cc1bb10e5bd0ae027
cvs <>
2001-06-07 10:32:46 +0000

    eg: adapted to new wizard API

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/wizards/NewProjectCreationWizard.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/wizards/NewProjectCreationWizard.java
@@ -4,7 +4,7 @@
  */
 package org.eclipse.jdt.internal.ui.wizards;
 
-import java.lang.reflect.InvocationTargetException;import org.eclipse.core.resources.IWorkspaceRoot;import org.eclipse.core.runtime.IConfigurationElement;import org.eclipse.core.runtime.IExecutableExtension;import org.eclipse.core.runtime.Platform;import org.eclipse.jdt.internal.ui.JavaPlugin;import org.eclipse.jdt.internal.ui.JavaPluginImages;import org.eclipse.jdt.internal.ui.util.ExceptionHandler;import org.eclipse.jdt.ui.wizards.NewJavaProjectWizardPage;import org.eclipse.jface.dialogs.MessageDialog;import org.eclipse.jface.operation.IRunnableWithProgress;import org.eclipse.ui.IPerspectiveDescriptor;import org.eclipse.ui.IPerspectiveRegistry;import org.eclipse.ui.IWorkbenchPage;import org.eclipse.ui.IWorkbenchWindow;import org.eclipse.ui.PlatformUI;import org.eclipse.ui.WorkbenchException;import org.eclipse.ui.actions.WorkspaceModifyDelegatingOperation;import org.eclipse.ui.dialogs.WizardNewProjectCreationPage;import org.eclipse.ui.IWorkbenchPreferenceConstants;import org.eclipse.ui.plugin.AbstractUIPlugin;
+import java.lang.reflect.InvocationTargetException;import org.eclipse.core.resources.IWorkspaceRoot;import org.eclipse.core.runtime.IConfigurationElement;import org.eclipse.core.runtime.IExecutableExtension;import org.eclipse.core.runtime.Platform;import org.eclipse.jdt.internal.ui.JavaPlugin;import org.eclipse.jdt.internal.ui.JavaPluginImages;import org.eclipse.jdt.internal.ui.util.ExceptionHandler;import org.eclipse.jdt.ui.wizards.NewJavaProjectWizardPage;import org.eclipse.jface.dialogs.MessageDialog;import org.eclipse.jface.operation.IRunnableWithProgress;import org.eclipse.ui.IPerspectiveDescriptor;import org.eclipse.ui.IPerspectiveRegistry;import org.eclipse.ui.IWorkbenchPage;import org.eclipse.ui.IWorkbenchWindow;import org.eclipse.ui.PlatformUI;import org.eclipse.ui.WorkbenchException;import org.eclipse.ui.actions.WorkspaceModifyDelegatingOperation;import org.eclipse.ui.dialogs.WizardNewProjectCreationPage;import org.eclipse.ui.IWorkbenchPreferenceConstants;import org.eclipse.ui.plugin.AbstractUIPlugin;import org.eclipse.ui.wizards.newresource.BasicNewProjectResourceWizard;
 
 public class NewProjectCreationWizard extends NewElementWizard implements IExecutableExtension {
 
@@ -58,7 +58,7 @@ public class NewProjectCreationWizard extends NewElementWizard implements IExecu
 		} catch  (InterruptedException e) {
 			return false;
 		}
-		updatePerspective();
+		BasicNewProjectResourceWizard.updatePerspective(fConfigElement);
 		selectAndReveal(fJavaPage.getNewJavaProject().getProject());
 		return true;
 	}
@@ -70,111 +70,4 @@ public class NewProjectCreationWizard extends NewElementWizard implements IExecu
 	public void setInitializationData(IConfigurationElement cfig, String propertyName, Object data) {
 		fConfigElement= cfig;
 	}
-	/**
-	 * Updates the perspective for the active page within the window.
-	 */
-	protected void updatePerspective() {
-		// When there is no config info the wizard didn't get created
-		// from the New wizard. Don't switch the perspective in this
-		// case since we are already in the Java context.
-		if (fConfigElement == null)
-			return;
-			
-		AbstractUIPlugin plugin =
-			(AbstractUIPlugin) Platform.getPlugin(PlatformUI.PLUGIN_ID);
-		String perspectiveSetting =
-			plugin.getPreferenceStore().getString(
-				IWorkbenchPreferenceConstants.PROJECT_OPEN_NEW_PERSPECTIVE);
-
-		//Do not switch perspectives if that is the setting
-		if (perspectiveSetting
-			.equals(IWorkbenchPreferenceConstants.NO_NEW_PERSPECTIVE))
-			return;
-
-		// Read final persp from config.
-		String perspID = fConfigElement.getAttribute("finalPerspective");
-		if (perspID == null)
-			return;
-		// Map persp id to descriptor.
-		IPerspectiveRegistry reg = PlatformUI.getWorkbench().getPerspectiveRegistry();
-		IPerspectiveDescriptor persp = reg.findPerspectiveWithId(perspID);
-		if (persp == null)
-			return;
-
-		if (perspectiveSetting
-			.equals(IWorkbenchPreferenceConstants.OPEN_PERSPECTIVE_WINDOW))
-			openInNewWindow(persp);
-		if (perspectiveSetting
-			.equals(IWorkbenchPreferenceConstants.OPEN_PERSPECTIVE_PAGE))
-			openInNewPage(persp);
-		if (perspectiveSetting
-			.equals(IWorkbenchPreferenceConstants.OPEN_PERSPECTIVE_REPLACE))
-			replaceCurrentPerspective(persp);
-	}
-
-	/* (non-Javadoc)
-	 * Opens a new page with a particular perspective and input.
-	 */
-	private void openInNewPage(IPerspectiveDescriptor desc) {
-
-		IWorkbenchWindow window = getWorkbench().getActiveWorkbenchWindow();
-		if (window == null)
-			return;
-
-		IWorkbenchPage page = window.getActivePage();
-		if (page != null) {
-			//Do not open the perspective if it is already there
-			if (page.getPerspective().getId().equals(desc.getId()))
-				return;
-		}
-
-		// Open the page.
-		try {
-			window.openPage(desc.getId(), getWorkspaceRoot());
-		} catch (WorkbenchException e) {
-			MessageDialog.openError(window.getShell(), "Problems Opening Workbench Page", e.getMessage());
-		}
-	}
-
-	/* (non-Javadoc)
-	 * Opens a new window with a particular perspective and input.
-	 */
-	private void openInNewWindow(IPerspectiveDescriptor desc) {
-		IWorkbenchWindow window = getWorkbench().getActiveWorkbenchWindow();
-
-		// Open the page.
-		try {
-			window.getWorkbench().openWorkbenchWindow(
-				desc.getId(),
-				getWorkspaceRoot());
-		} catch (WorkbenchException e) {
-			MessageDialog.openError(
-				window.getShell(),
-				"Problems Opening Workbench Window",
-				e.getMessage());
-		}
-	}
-
-
-	/**
-	 * Replace the current perspective with the new one.
-	 */
-	private void replaceCurrentPerspective(IPerspectiveDescriptor persp) {
-
-		//Get the active page.
-		IWorkbenchWindow dw = getWorkbench().getActiveWorkbenchWindow();
-		if (dw == null)
-			return;
-		IWorkbenchPage page = dw.getActivePage();
-		if (page == null)
-			return;
-
-		// Set the perspective.
-		page.setPerspective(persp);
-	}
-
-	IWorkspaceRoot getWorkspaceRoot() {
-		return JavaPlugin.getDefault().getWorkspace().getRoot();
-	}
-
 }
