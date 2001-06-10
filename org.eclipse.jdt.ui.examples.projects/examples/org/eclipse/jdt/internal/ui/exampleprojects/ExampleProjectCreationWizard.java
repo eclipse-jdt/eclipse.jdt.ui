a7ed0173dd9b6177cd1ff00189a2087c3cb289e9
cvs <>
2001-06-10 13:59:00 +0000

    eg: renamed project, adjusted build.properties

---
+++ org.eclipse.jdt.ui.examples.projects/examples/org/eclipse/jdt/internal/ui/exampleprojects/ExampleProjectCreationWizard.java
@@ -0,0 +1,104 @@
+/*
+ * (c) Copyright IBM Corp. 2000, 2001.
+ * All Rights Reserved.
+ */
+package org.eclipse.jdt.internal.ui.exampleprojects;
+
+import java.lang.reflect.InvocationTargetException;import org.eclipse.swt.widgets.Shell;import org.eclipse.core.resources.IProject;import org.eclipse.core.resources.IWorkspaceRoot;import org.eclipse.core.runtime.CoreException;import org.eclipse.core.runtime.IConfigurationElement;import org.eclipse.core.runtime.IExecutableExtension;import org.eclipse.core.runtime.IStatus;import org.eclipse.jface.dialogs.ErrorDialog;import org.eclipse.jface.dialogs.MessageDialog;import org.eclipse.jface.operation.IRunnableWithProgress;import org.eclipse.jface.resource.ImageDescriptor;import org.eclipse.ui.INewWizard;import org.eclipse.ui.actions.WorkspaceModifyDelegatingOperation;import org.eclipse.ui.dialogs.WizardNewProjectCreationPage;import org.eclipse.ui.wizards.newresource.BasicNewProjectResourceWizard;import org.eclipse.ui.wizards.newresource.BasicNewResourceWizard;import org.eclipse.jdt.ui.wizards.NewJavaProjectWizardPage;
+
+public class ExampleProjectCreationWizard extends BasicNewResourceWizard implements INewWizard, IExecutableExtension {
+
+	private static final String WIZARD_NAME= "ExampleProjectCreationWizard";
+	private static final String WIZARD_TITLE= WIZARD_NAME + ".title";
+	private static final String ERROR_TITLE= WIZARD_NAME + ".op_error.title";
+	private static final String ERROR_MESSAGE= WIZARD_NAME + ".op_error.message";
+
+	private WizardNewProjectCreationPage fMainPage;
+	private IConfigurationElement fConfigElement;
+	
+	private NewJavaProjectWizardPage fJavaProjectPage;
+
+	public ExampleProjectCreationWizard() {
+		super();
+		setDialogSettings(ExampleProjectsPlugin.getDefault().getDialogSettings());
+		setWindowTitle(ExampleProjectsPlugin.getResourceString(WIZARD_TITLE));		
+		setNeedsProgressMonitor(true);
+	}
+	
+	/*
+	 * @see BasicNewResourceWizard#initializeDefaultPageImageDescriptor
+	 */
+	protected void initializeDefaultPageImageDescriptor() {
+		ImageDescriptor desc= ExampleProjectsPlugin.getDefault().getImageDescriptor("wizban/newjprjex_wiz.gif");
+		setDefaultPageImageDescriptor(desc);
+	}
+
+	/**
+	 * @see Wizard#addPages
+	 */	
+	public void addPages() {
+		super.addPages();
+		fMainPage= new WizardNewProjectCreationPage("id");
+		fMainPage.setTitle(getConfigurationText("pagetitle"));
+		fMainPage.setDescription(getConfigurationText("pagedescription"));
+
+		addPage(fMainPage);
+		
+		IWorkspaceRoot root= ExampleProjectsPlugin.getWorkspace().getRoot();
+		fJavaProjectPage= new NewJavaProjectWizardPage(root, fMainPage);
+		
+	}
+	
+	protected String getConfigurationText(String tag) {
+		if (fConfigElement != null) {
+			IConfigurationElement[] children = fConfigElement.getChildren(tag);
+			if (children.length >= 1) {
+				return children[0].getValue();
+			}
+		}
+		return "!" + tag + "!";
+	}
+	
+	/**
+	 * @see Wizard#performFinish
+	 */		
+	public boolean performFinish() {
+		IProject projectHandle= fMainPage.getProjectHandle();
+		Shell shell= fMainPage.getControl().getShell();
+		ExampleProjectCreationOperation runnable= new ExampleProjectCreationOperation(shell, projectHandle, fConfigElement, fJavaProjectPage);
+		
+		IRunnableWithProgress op= new WorkspaceModifyDelegatingOperation(runnable);
+		try {
+			getContainer().run(false, true, op);
+		} catch (InvocationTargetException e) {
+			handleException(e.getTargetException());
+			return false;
+		} catch  (InterruptedException e) {
+			return false;
+		}
+		BasicNewProjectResourceWizard.updatePerspective(fConfigElement);
+		selectAndReveal(runnable.getResourceToReveal());
+		return true;
+	}
+	
+	private void handleException(Throwable target) {
+		String title= ExampleProjectsPlugin.getResourceString(ERROR_TITLE);
+		String message= ExampleProjectsPlugin.getResourceString(ERROR_MESSAGE);
+		if (target instanceof CoreException) {
+			IStatus status= ((CoreException)target).getStatus();
+			ErrorDialog.openError(getShell(), title, message, status);
+			ExampleProjectsPlugin.log(status);
+		} else {
+			MessageDialog.openError(getShell(), title, target.getMessage());
+			ExampleProjectsPlugin.log(target);
+		}
+	}
+		
+	/**
+	 * Stores the configuration element for the wizard.  The config element will be used
+	 * in <code>performFinish</code> to set the result perspective.
+	 */
+	public void setInitializationData(IConfigurationElement cfig, String propertyName, Object data) {
+		fConfigElement= cfig;
+	}
+}
---
+++ org.eclipse.jdt.ui.examples.projects/examples/org/eclipse/jdt/internal/ui/exampleprojects/ExampleProjectsPlugin.java
@@ -0,0 +1,103 @@
+/*
+ * (c) Copyright IBM Corp. 2000, 2001.
+ * All Rights Reserved.
+ */
+package org.eclipse.jdt.internal.ui.exampleprojects;
+
+import java.net.MalformedURLException;import java.net.URL;import java.text.MessageFormat;import java.util.MissingResourceException;import java.util.ResourceBundle;import org.eclipse.swt.widgets.Display;import org.eclipse.core.resources.IWorkspace;import org.eclipse.core.resources.ResourcesPlugin;import org.eclipse.core.runtime.IPluginDescriptor;import org.eclipse.core.runtime.IStatus;import org.eclipse.core.runtime.Status;import org.eclipse.jface.resource.ImageDescriptor;import org.eclipse.ui.plugin.AbstractUIPlugin;
+
+/**
+ * The main plugin class to be used in the desktop.
+ */
+public class ExampleProjectsPlugin extends AbstractUIPlugin {
+
+	// The shared instance.
+	private static ExampleProjectsPlugin fgPlugin;	// Resource bundle.
+	private static ResourceBundle fgResourceBundle;
+
+	/**
+	 * The constructor.
+	 */
+	public ExampleProjectsPlugin(IPluginDescriptor descriptor) {
+		super(descriptor);
+		fgPlugin= this;
+		try {
+			fgResourceBundle= getDescriptor().getResourceBundle();
+		} catch (MissingResourceException x) {
+			log("ProjectTemplatesPlugin resourcebundle not found");
+			fgResourceBundle= null;
+		}
+	}
+
+	/**
+	 * Returns the shared instance.
+	 */
+	public static ExampleProjectsPlugin getDefault() {
+		return fgPlugin;
+	}
+
+	/**
+	 * Returns the workspace instance.
+	 */
+	public static IWorkspace getWorkspace() {
+		return ResourcesPlugin.getWorkspace();
+	}
+
+	/**
+	 * Returns the string from the plugin's resource bundle,
+	 * or 'key' if not found.
+	 */
+	public static String getResourceString(String key) {
+		ResourceBundle bundle= ExampleProjectsPlugin.getResourceBundle();
+		try {
+			return bundle.getString(key);
+		} catch (MissingResourceException e) {
+			return key;
+		}
+	}
+
+	/**
+	 * Gets a string from the resource bundle and formats it with arguments
+	 */	
+	public static String getFormattedString(String key, String args) {
+		return MessageFormat.format(getResourceString(key), new String[] { args });
+	}
+	/**
+	 * Returns the plugin's resource bundle,
+	 */
+	public static ResourceBundle getResourceBundle() {
+		return fgResourceBundle;
+	}
+	
+	public ImageDescriptor getImageDescriptor(String name) {
+		try {
+			URL url;
+			Display d= Display.getCurrent();
+			if (d != null && d.getIconDepth() <= 4) {
+				url= new URL(getDescriptor().getInstallURL(), "icons/basic/" + name);
+			} else {
+				url= new URL(getDescriptor().getInstallURL(), "icons/full/" + name);
+			}
+			return ImageDescriptor.createFromURL(url);
+		} catch (MalformedURLException e) {
+			return ImageDescriptor.getMissingImageDescriptor();
+		}
+	}	
+	
+	public static String getPluginId() {
+		return getDefault().getDescriptor().getUniqueIdentifier();
+	}	
+
+	public static void log(IStatus status) {
+		getDefault().getLog().log(status);
+	}
+
+	public static void log(String message) {
+		log(new Status(IStatus.ERROR, getPluginId(), IStatus.ERROR, message, null));
+	}
+
+	public static void log(Throwable e) {
+		log(new Status(IStatus.ERROR, getPluginId(), IStatus.ERROR, "Internal Error", e));
+	}
+
+}
