dec218d7f6d20da5c49ecc86ef1015f1a9414998
Martin Aeschlimann <maeschlimann>
2001-09-13 15:17:20 +0000

    code review

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/preferences/SourceAttachmentPropertyPage.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/preferences/SourceAttachmentPropertyPage.java
@@ -4,21 +4,56 @@
  */
 package org.eclipse.jdt.internal.ui.preferences;
 
-import java.lang.reflect.InvocationTargetException;import org.eclipse.swt.SWT;import org.eclipse.swt.widgets.Composite;import org.eclipse.swt.widgets.Control;import org.eclipse.swt.widgets.Label;import org.eclipse.core.resources.IFile;import org.eclipse.core.resources.IProject;import org.eclipse.core.resources.IResource;import org.eclipse.core.resources.IWorkspaceRoot;
-import org.eclipse.core.runtime.CoreException;import org.eclipse.core.runtime.IAdaptable;import org.eclipse.core.runtime.IPath;import org.eclipse.core.runtime.IProgressMonitor;import org.eclipse.core.runtime.IStatus;import org.eclipse.jface.dialogs.ErrorDialog;import org.eclipse.jface.dialogs.IDialogConstants;import org.eclipse.jface.dialogs.MessageDialog;import org.eclipse.jface.dialogs.ProgressMonitorDialog;import org.eclipse.jface.operation.IRunnableWithProgress;import org.eclipse.ui.dialogs.PropertyPage;import org.eclipse.ui.help.DialogPageContextComputer;import org.eclipse.ui.help.WorkbenchHelp;import org.eclipse.jdt.core.IClasspathEntry;import org.eclipse.jdt.core.IJavaElement;import org.eclipse.jdt.core.IJavaProject;import org.eclipse.jdt.core.IPackageFragmentRoot;import org.eclipse.jdt.core.JavaCore;import org.eclipse.jdt.core.JavaModelException;import org.eclipse.jdt.internal.ui.IJavaHelpContextIds;import org.eclipse.jdt.internal.ui.JavaPlugin;import org.eclipse.jdt.internal.ui.dialogs.IStatusChangeListener;import org.eclipse.jdt.internal.ui.dialogs.StatusTool;import org.eclipse.jdt.internal.ui.util.ExceptionHandler;import org.eclipse.jdt.internal.ui.util.JavaModelUtility;import org.eclipse.jdt.internal.ui.wizards.buildpaths.SourceAttachmentBlock;
-import org.eclipse.jdt.internal.ui.JavaUIMessages;
+import java.lang.reflect.InvocationTargetException;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IStatus;
+
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.dialogs.ProgressMonitorDialog;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+
+import org.eclipse.ui.dialogs.PropertyPage;
+import org.eclipse.ui.help.DialogPageContextComputer;
+import org.eclipse.ui.help.WorkbenchHelp;
+
+import org.eclipse.jdt.core.IClasspathEntry;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IPackageFragmentRoot;
+import org.eclipse.jdt.core.JavaCore;
+
+import org.eclipse.jdt.internal.ui.IJavaHelpContextIds;
+import org.eclipse.jdt.internal.ui.JavaPlugin;
+import org.eclipse.jdt.internal.ui.JavaUIMessages;
+import org.eclipse.jdt.internal.ui.dialogs.IStatusChangeListener;
+import org.eclipse.jdt.internal.ui.dialogs.StatusTool;
+import org.eclipse.jdt.internal.ui.util.ExceptionHandler;
+import org.eclipse.jdt.internal.ui.util.JavaModelUtility;
+import org.eclipse.jdt.internal.ui.wizards.buildpaths.SourceAttachmentBlock;
+
+/**
+ * Property page to configure a archive's JARs source attachment
+ */
 public class SourceAttachmentPropertyPage extends PropertyPage implements IStatusChangeListener {
 
 	private SourceAttachmentBlock fSourceAttachmentBlock;
 	private IPackageFragmentRoot fJarRoot;
 
 	public SourceAttachmentPropertyPage() {
-		fSourceAttachmentBlock= null;
-		fJarRoot= null;
 	}
 	
-	/**
+	/*
 	 * @see PreferencePage#createContents
 	 */
 	protected Control createContents(Composite composite) {
@@ -38,14 +73,14 @@ public class SourceAttachmentPropertyPage extends PropertyPage implements IStatu
 			}		
 		}
 		Label label= new Label(composite, SWT.LEFT + SWT.WRAP);
-		label.setText(JavaUIMessages.getString("SourceAttachmentPropertyPage.labelText")); //$NON-NLS-1$
+		label.setText(JavaUIMessages.getString("SourceAttachmentPropertyPage.noarchive.message")); //$NON-NLS-1$
 		label.setFont(composite.getFont());
 		
 		WorkbenchHelp.setHelp(composite, new DialogPageContextComputer(this, IJavaHelpContextIds.SOURCE_ATTACHMENT_PROPERTY_PAGE));		
 		return label;
 	}
 
-	/**
+	/*
 	 * @see IPreferencePage#performOk
 	 */
 	public boolean performOk() {
@@ -54,9 +89,10 @@ public class SourceAttachmentPropertyPage extends PropertyPage implements IStatu
 				IRunnableWithProgress runnable= fSourceAttachmentBlock.getRunnable(fJarRoot.getJavaProject(), getShell());		
 				new ProgressMonitorDialog(getShell()).run(true, true, runnable);						
 			} catch (InvocationTargetException e) {
-				String title= JavaUIMessages.getString("SourceAttachmentPropertyPage.errorTitle"); //$NON-NLS-1$
-				String message= JavaUIMessages.getString("SourceAttachmentPropertyPage.errorMessage"); //$NON-NLS-1$
+				String title= JavaUIMessages.getString("SourceAttachmentPropertyPage.error.title"); //$NON-NLS-1$
+				String message= JavaUIMessages.getString("SourceAttachmentPropertyPage.error.message"); //$NON-NLS-1$
 				if (!ExceptionHandler.handle(e, getShell(), title, message)) {
+					JavaPlugin.log(e);
 					MessageDialog.openError(getShell(), title, message);
 				}
 				return false;
@@ -96,8 +132,10 @@ public class SourceAttachmentPropertyPage extends PropertyPage implements IStatu
 		return null;		
 	}
 		
-	// ------- IStatusChangeListener --------
-	
+
+	/*
+	 * @see IStatusChangeListener#statusChanged
+	 */
 	public void statusChanged(IStatus status) {
 		setValid(!status.matches(IStatus.ERROR));
 		StatusTool.applyToStatusLine(this, status);
