aac165d0ba813ef33634f8657c955457ec9ac622
Adam Kiezun <akiezun>
2001-09-17 13:55:07 +0000

    comments+

--- org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/core/refactoring/changes/MovePackageChange.java
+++ org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/core/refactoring/changes/MovePackageChange.java
@@ -1,3 +1,7 @@
+/*
+ * (c) Copyright IBM Corp. 2000, 2001.
+ * All Rights Reserved.
+ */
 package org.eclipse.jdt.internal.core.refactoring.changes;
 
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -7,29 +11,38 @@ import org.eclipse.jdt.core.IPackageFragmentRoot;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.internal.core.refactoring.NullChange;
 import org.eclipse.jdt.internal.core.refactoring.base.IChange;
+import org.eclipse.jdt.internal.core.refactoring.changes.*;
+import org.eclipse.jdt.internal.core.refactoring.*;
 
-class MovePackageChange extends PackageReorgChange {
+public class MovePackageChange extends PackageReorgChange {
 	
-	MovePackageChange(IPackageFragment pack, IPackageFragmentRoot dest, String newName){
+	public MovePackageChange(IPackageFragment pack, IPackageFragmentRoot dest, String newName){
 		super(pack, dest, newName);
+	}
+	
+	public MovePackageChange(IPackageFragment pack, IPackageFragmentRoot dest){
+		this(pack, dest, null);
 	}
 	
 	protected void doPerform(IProgressMonitor pm) throws JavaModelException{
 		getPackage().move(getDestination(), null, getNewName(), true, pm);
 	}
 	
-	/**
+	/* non java-doc
 	 * @see IChange#getUndoChange()
 	 */
 	public IChange getUndoChange() {
 		return new NullChange();
 	}
-	
+
+	/* non java-doc
+	 * @see IChange#isUndoable()
+	 */	
 	public boolean isUndoable(){
 		return false;
 	}
 
-	/**
+	/* non java-doc
 	 * @see IChange#getName()
 	 */
 	public String getName() {
