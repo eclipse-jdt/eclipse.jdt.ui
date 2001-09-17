e4e173065ff2edc6bf36109d8f0998119a2fab5d
Adam Kiezun <akiezun>
2001-09-17 14:18:14 +0000

    new reorg story

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/packageview/SelectionTransferDropAdapter.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/packageview/SelectionTransferDropAdapter.java
@@ -4,36 +4,40 @@
  */
 package org.eclipse.jdt.internal.ui.packageview;
 
-import java.util.Iterator;
 import java.util.List;
 
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.internal.core.refactoring.DebugUtils;
+import org.eclipse.jdt.internal.core.refactoring.reorg.CopyRefactoring;
+import org.eclipse.jdt.internal.core.refactoring.reorg.MoveRefactoring;
+import org.eclipse.jdt.internal.core.refactoring.reorg.ReorgRefactoring;
+import org.eclipse.jdt.internal.core.refactoring.reorg.ReorgUtils;
+import org.eclipse.jdt.internal.core.refactoring.text.ITextBufferChangeCreator;
+import org.eclipse.jdt.internal.ui.JavaPlugin;
+import org.eclipse.jdt.internal.ui.dnd.JdtTreeViewerDropAdapter;
+import org.eclipse.jdt.internal.ui.dnd.LocalSelectionTransfer;
+import org.eclipse.jdt.internal.ui.dnd.TransferDropTargetListener;
+import org.eclipse.jdt.internal.ui.refactoring.changes.DocumentTextBufferChangeCreator;
+import org.eclipse.jdt.internal.ui.reorg.CopyAction;
+import org.eclipse.jdt.internal.ui.reorg.MoveAction;
+import org.eclipse.jdt.internal.ui.util.ExceptionHandler;
+import org.eclipse.jface.viewers.AbstractTreeViewer;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.dnd.DND;
 import org.eclipse.swt.dnd.DropTargetEvent;
 import org.eclipse.swt.dnd.Transfer;
-import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.texteditor.IDocumentProvider;
 
-import org.eclipse.jface.viewers.AbstractTreeViewer;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
-
-import org.eclipse.jdt.core.IJavaElement;
-
-import org.eclipse.jdt.internal.ui.dnd.JdtTreeViewerDropAdapter;
-import org.eclipse.jdt.internal.ui.dnd.LocalSelectionTransfer;
-import org.eclipse.jdt.internal.ui.dnd.TransferDropTargetListener;
-import org.eclipse.jdt.internal.ui.reorg.CopyAction;
-import org.eclipse.jdt.internal.ui.reorg.ICopySupport;
-import org.eclipse.jdt.internal.ui.reorg.IMoveSupport;
-import org.eclipse.jdt.internal.ui.reorg.MoveAction;
-import org.eclipse.jdt.internal.ui.reorg.ReorgSupportFactory;
 
 public class SelectionTransferDropAdapter extends JdtTreeViewerDropAdapter implements TransferDropTargetListener {
 
 	private List fElements;
-	private IMoveSupport fMoveSupport;
+	private MoveRefactoring fMoveRefactoring;
 	private int fCanMoveElements;
-	private ICopySupport fCopySupport;
+	private CopyRefactoring fCopyRefactoring;
 	private int fCanCopyElements;
 
 	public SelectionTransferDropAdapter(AbstractTreeViewer viewer) {
@@ -60,9 +64,9 @@ public class SelectionTransferDropAdapter extends JdtTreeViewerDropAdapter imple
 	
 	private void clear() {
 		fElements= null;
-		fMoveSupport= null;
+		fMoveRefactoring= null;
 		fCanMoveElements= 0;
-		fCopySupport= null;
+		fCopyRefactoring= null;
 		fCanCopyElements= 0;
 	}
 	
@@ -74,63 +78,77 @@ public class SelectionTransferDropAdapter extends JdtTreeViewerDropAdapter imple
 				return;
 			fElements= ((IStructuredSelection)s).toList();
 		}	
-		
+
 		boolean success= false;
-		if (operation == DND.DROP_COPY) {
-			success= handleValidateCopy(target, event);
-		} else if (operation == DND.DROP_MOVE) {
-			success= handleValidateMove(target, event);
-		}
+
+		try {
+			if (operation == DND.DROP_COPY) {
+				success= handleValidateCopy(target, event);
+			} else if (operation == DND.DROP_MOVE) {
+				success= handleValidateMove(target, event);
+			}
+		} catch (JavaModelException e){
+			ExceptionHandler.handle(e, "Drag'n'drop", "Unexpected exception. See log for details.");
+			success= false;
+		}	
+		
 		if (success)
 			event.detail= operation;
-			
-		return;	
 	}	
 
 	public void drop(Object target, DropTargetEvent event) {
-		if (event.detail == DND.DROP_MOVE) {
-			handleDropMove(target, event);
-		} else if (event.detail == DND.DROP_COPY) {
-			handleDropCopy(target, event);
-		}
-		
-		// The drag source listener must not perform any operation
-		// since this drop adapter did the remove of the source even
-		// if we moved something.
-		event.detail= DND.DROP_NONE;
-		return;
+		try{
+			if (event.detail == DND.DROP_MOVE) {
+				handleDropMove(target, event);
+			} else if (event.detail == DND.DROP_COPY) {
+				handleDropCopy(target, event);
+			}
+			
+			// The drag source listener must not perform any operation
+			// since this drop adapter did the remove of the source even
+			// if we moved something.
+			event.detail= DND.DROP_NONE;
+		} catch (JavaModelException e){
+			ExceptionHandler.handle(e, "Drag'n'drop", "Unexpected exception. See log for details.");
+		}	
 	}
 	
-	private boolean handleValidateMove(Object target, DropTargetEvent event) {
-		if (fMoveSupport == null)
-			fMoveSupport= ReorgSupportFactory.createMoveSupport(fElements);
+	private boolean handleValidateMove(Object target, DropTargetEvent event) throws JavaModelException{
+		if (fMoveRefactoring == null){
+			IDocumentProvider documentProvider= JavaPlugin.getDefault().getCompilationUnitDocumentProvider();
+			ITextBufferChangeCreator changeCreator= new DocumentTextBufferChangeCreator(documentProvider);
+			fMoveRefactoring= new MoveRefactoring(fElements, changeCreator);
+		}	
 		
 		if (!canMoveElements())
 			return false;	
-
-		return fMoveSupport.canMove(fElements, target);
+		
+		return fMoveRefactoring.isValidDestination(target);
 	}
 	
 	private boolean canMoveElements() {
 		if (fCanMoveElements == 0) {
 			fCanMoveElements= 2;
-			Iterator iter= fElements.iterator();
-			while (iter.hasNext()) {
-				if (!fMoveSupport.isMovable(iter.next())) {
-					fCanMoveElements= 1;
-					break;
-				}
-			}
+			if (! canActivate(fMoveRefactoring))
+				fCanMoveElements= 1;
 		}
 		return fCanMoveElements == 2;
 	}
 	
-	private void handleDropMove(final Object target, DropTargetEvent event) {
-		MoveAction action= new MoveAction(getViewer(), "#MOVE") { //$NON-NLS-1$
-			protected Object selectDestination(IJavaElement root, List elements, Shell shell) {
+	private boolean canActivate(ReorgRefactoring ref){
+		try{
+			return ref.checkActivation(new NullProgressMonitor()).isOK();
+		} catch(JavaModelException e){
+			ExceptionHandler.handle(e, "Drag'n'drop", "Unexpected exception. See log for details.");
+			return false;
+		}
+	}
+	
+	private void handleDropMove(final Object target, DropTargetEvent event) throws JavaModelException{
+		MoveAction action= new MoveAction(getViewer(), "#MOVE"){
+			protected Object selectDestination(ReorgRefactoring ref) {
 				return target;
 			}
-			
 		};
 		action.run();
 		// Remove the action from the selection provider.
@@ -138,36 +156,30 @@ public class SelectionTransferDropAdapter extends JdtTreeViewerDropAdapter imple
 		action.getSelectionProvider().removeSelectionChangedListener(action);
 	}
 	
-	private boolean handleValidateCopy(Object target, DropTargetEvent event) {
-		if (fCopySupport == null)
-			fCopySupport= ReorgSupportFactory.createCopySupport(fElements);
+	private boolean handleValidateCopy(Object target, DropTargetEvent event) throws JavaModelException{
+		if (fCopyRefactoring == null)
+			fCopyRefactoring= new CopyRefactoring(fElements);
 		
 		if (!canCopyElements())
 			return false;	
 
-		return fCopySupport.canCopy(fElements, target);
+		return fCopyRefactoring.isValidDestination(target);
 	}
 			
 	private boolean canCopyElements() {
 		if (fCanCopyElements == 0) {
 			fCanCopyElements= 2;
-			Iterator iter= fElements.iterator();
-			while (iter.hasNext()) {
-				if (!fCopySupport.isCopyable(iter.next())) {
-					fCanCopyElements= 1;
-					break;
-				}
-			}
+			if (!canActivate(fCopyRefactoring))
+				fCanCopyElements= 1;
 		}
 		return fCanCopyElements == 2;
 	}		
 	
-	private void handleDropCopy(final Object target, DropTargetEvent event) {
-		CopyAction action= new CopyAction(getViewer(), "#COPY") { //$NON-NLS-1$
-			protected Object selectDestination(IJavaElement root, List elements, Shell shell) {
+	private void handleDropCopy(final Object target, DropTargetEvent event) throws JavaModelException{
+		CopyAction action= new CopyAction(getViewer(), "#COPY"){
+			protected Object selectDestination(ReorgRefactoring ref) {
 				return target;
 			}
-			
 		};
 		action.run();
 		// Remove the action from the selection provider.
