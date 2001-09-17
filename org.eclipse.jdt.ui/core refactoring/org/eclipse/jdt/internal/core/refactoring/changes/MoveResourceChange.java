46ebecba9c84ff0aa540bfaefd17fc76172d0cdb
Adam Kiezun <akiezun>
2001-09-17 13:55:22 +0000

    not  force

--- org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/core/refactoring/changes/MoveResourceChange.java
+++ org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/core/refactoring/changes/MoveResourceChange.java
@@ -1,3 +1,7 @@
+/*
+ * (c) Copyright IBM Corp. 2000, 2001.
+ * All Rights Reserved.
+ */
 package org.eclipse.jdt.internal.core.refactoring.changes;
 
 import org.eclipse.core.resources.IContainer;
@@ -5,42 +9,51 @@ import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.SubProgressMonitor;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.internal.core.refactoring.base.IChange;
 
 public class MoveResourceChange extends ResourceReorgChange {
 	
-	MoveResourceChange(IResource res, IContainer dest, String newName){
+	public MoveResourceChange(IResource res, IContainer dest, String newName){
 		super(res, dest, newName);
 	}
 	
-	/**
+	public MoveResourceChange(IResource res, IContainer dest){
+		this(res, dest, null);
+	}
+	
+	/* non java-doc
 	 * @see ResourceReorgChange#doPerform(IPath, IProgressMonitor)
 	 */
 	protected void doPerform(IPath path, IProgressMonitor pm) throws JavaModelException{
 		try{
-			getResource().move(path, true, pm);
+			pm.beginTask("moving", 1);
+			getResource().move(path, false, new SubProgressMonitor(pm, 1));
 		}catch(CoreException e){
 			throw new JavaModelException(e);
 		}	
 	}
 	
-	/**
+	/* non java-doc
 	 * @see IChange#getUndoChange()
 	 */
 	public IChange getUndoChange() {
 		return null;
 	}
 
+	/* non java-doc
+	 * @see IChange#isUndoable()
+	 */
 	public boolean isUndoable(){
 		return false;
 	}
 
-	/**
+	/* non java-doc
 	 * @see IChange#getName()
 	 */
 	public String getName() {
-		return "Move resource";
+		return "Move resource:" + getResource().getFullPath() + " to: " + getDestination().getName();
 	}
 }
 
