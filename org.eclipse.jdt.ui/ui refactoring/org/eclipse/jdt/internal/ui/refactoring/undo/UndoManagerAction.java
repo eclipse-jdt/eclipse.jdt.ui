77359e7772ea124dc7b78839c40f3fdf04b0cbd0
Dirk Baeumer <dbaeumer>
2001-09-26 15:15:47 +0000

    Deleted classes

--- org.eclipse.jdt.ui/ui refactoring/org/eclipse/jdt/internal/ui/refactoring/undo/UndoManagerAction.java
+++
@@ -1,92 +0,0 @@
-/*
- * (c) Copyright IBM Corp. 2000, 2001.
- * All Rights Reserved.
- */
-
-package org.eclipse.jdt.internal.ui.refactoring.undo;
-
-import java.lang.reflect.InvocationTargetException;import java.util.ArrayList;import java.util.Iterator;import java.util.List;import org.eclipse.swt.widgets.Shell;import org.eclipse.jface.action.Action;
-import org.eclipse.jface.dialogs.ErrorDialog;import org.eclipse.jface.dialogs.IDialogConstants;import org.eclipse.jface.dialogs.ProgressMonitorDialog;import org.eclipse.jface.operation.IRunnableWithProgress;import org.eclipse.core.resources.IFile;import org.eclipse.core.runtime.IStatus;import org.eclipse.core.runtime.MultiStatus;import org.eclipse.core.runtime.Status;import org.eclipse.ui.IEditorInput;import org.eclipse.ui.IEditorPart;import org.eclipse.ui.IFileEditorInput;import org.eclipse.jdt.internal.core.refactoring.base.ChangeContext;import org.eclipse.jdt.internal.core.refactoring.base.RefactoringStatus;import org.eclipse.jdt.internal.core.refactoring.base.RefactoringStatusEntry;import org.eclipse.jdt.internal.ui.JavaPlugin;import org.eclipse.jdt.internal.ui.actions.JavaUIAction;import org.eclipse.jdt.internal.ui.refactoring.changes.AbortChangeExceptionHandler;import org.eclipse.jdt.internal.ui.util.ExceptionHandler;
-import org.eclipse.jdt.internal.ui.refactoring.RefactoringMessages;
-
-public abstract class UndoManagerAction extends Action {
-
-	private RefactoringStatus fPreflightStatus;
-
-	public UndoManagerAction(String label) {
-		super(label);
-	}
-	
-	public abstract IRunnableWithProgress createOperation(ChangeContext context);
-	
-	protected abstract String getName();
-	
-	public void run() {
-		// PR: 1GEWDUH: ITPJCORE:WINNT - Refactoring - Unable to undo refactor change
-		Shell parent= JavaPlugin.getActiveWorkbenchShell();
-		ChangeContext context= new ChangeContext(new AbortChangeExceptionHandler(), getUnsavedFiles());
-		IRunnableWithProgress op= createOperation(context);
-		try {
-			ProgressMonitorDialog dialog= new ProgressMonitorDialog(parent);
-			// Don't execute in separate thread since it updates the UI.
-			dialog.run(false, false, op);
-		} catch (InvocationTargetException e) {
-			ExceptionHandler.handle(e, RefactoringMessages.getString("UndoManagerAction.error"), RefactoringMessages.getString("UndoManagerAction.internal_error")); //$NON-NLS-2$ //$NON-NLS-1$
-		} catch (InterruptedException e) {
-			// Opertation isn't cancelable.
-		} finally {
-			context.clearPerformedChanges();
-		}
-		
-		if (fPreflightStatus.hasError()) {
-			String name= getName();
-			MultiStatus status = createMultiStatus(name);
-			String message= RefactoringMessages.getFormattedString("UndoManagerAction.cannot_be_executed", name); //$NON-NLS-1$
-			ErrorDialog error= new ErrorDialog(parent, name, message, status, IStatus.ERROR) {
-				public void create() {
-					super.create();
-					buttonPressed(IDialogConstants.DETAILS_ID);
-				}
-			};
-			error.open();
-		}
-	}
-
-	protected MultiStatus createMultiStatus(String name) {
-		// PR: 1GEWDUH: ITPJCORE:WINNT - Refactoring - Unable to undo refactor change
-		MultiStatus status= new MultiStatus(
-			JavaPlugin.getPluginId(), 
-			IStatus.ERROR,
-			RefactoringMessages.getString("UndoManagerAction.unsaved_filed"), //$NON-NLS-1$
-			null);
-		String id= JavaPlugin.getPluginId();
-		for (Iterator iter= fPreflightStatus.getEntries().iterator(); iter.hasNext(); ) {
-			RefactoringStatusEntry entry= (RefactoringStatusEntry)iter.next();
-			status.merge(new Status(
-				IStatus.ERROR,
-				id,
-				IStatus.ERROR,
-				entry.getMessage(),
-				null));
-		}
-		return status;
-	}
-	
-	protected void setPreflightStatus(RefactoringStatus status) {
-		// PR: 1GEWDUH: ITPJCORE:WINNT - Refactoring - Unable to undo refactor change
-		fPreflightStatus= status;
-	}
-	
-	protected IFile[] getUnsavedFiles() {
-		// PR: 1GEWDUH: ITPJCORE:WINNT - Refactoring - Unable to undo refactor change
-		IEditorPart[] parts= JavaPlugin.getDirtyEditors();
-		List result= new ArrayList(parts.length);
-		for (int i= 0; i < parts.length; i++) {
-			IEditorInput input= parts[i].getEditorInput();
-			if (input instanceof IFileEditorInput) {
-				result.add(((IFileEditorInput)input).getFile());
-			}
-		}
-		return (IFile[])result.toArray(new IFile[result.size()]);
-	}	
-}
--- org.eclipse.jdt.ui/ui refactoring/org/eclipse/jdt/internal/ui/refactoring/undo/UndoRefactoringAction.java
+++
@@ -1,56 +0,0 @@
-/*
- * (c) Copyright IBM Corp. 2000, 2001.
- * All Rights Reserved.
- */
-
-package org.eclipse.jdt.internal.ui.refactoring.undo;
-
-import java.lang.reflect.InvocationTargetException;import org.eclipse.jface.operation.IRunnableWithProgress;import org.eclipse.core.runtime.IProgressMonitor;import org.eclipse.jdt.core.JavaModelException;import org.eclipse.jdt.internal.core.refactoring.base.ChangeAbortException;import org.eclipse.jdt.internal.core.refactoring.base.ChangeContext;import org.eclipse.jdt.internal.core.refactoring.base.Refactoring;
-import org.eclipse.jdt.internal.ui.refactoring.RefactoringMessages;
-
-public class UndoRefactoringAction extends UndoManagerAction {
-
-	private String fText;
-
-	public UndoRefactoringAction() {
-		super(RefactoringMessages.getString("UndoRefactoringAction.label")); //$NON-NLS-1$
-		fText= getText();
-		setDescription(RefactoringMessages.getString("UndoRefactoringAction.description")); //$NON-NLS-1$
-		setToolTipText(RefactoringMessages.getString("UndoRefactoringAction.tooltip")); //$NON-NLS-1$
-	}
-
-	public boolean isEnabled(){
-		return Refactoring.getUndoManager().anythingToUndo();
-	}
-	
-	public void update() {
-		String text= Refactoring.getUndoManager().peekUndoName();
-		if (text != null) {
-			text= fText + " - " + text; //$NON-NLS-1$
-		} else {
-			text= fText;
-		}
-		setText(text);
-	}
-
-	protected String getName() {
-		// PR: 1GEWDUH: ITPJCORE:WINNT - Refactoring - Unable to undo refactor change
-		return RefactoringMessages.getString("UndoRefactoringAction.name"); //$NON-NLS-1$
-	}
-	
-	public IRunnableWithProgress createOperation(final ChangeContext context) {
-		// PR: 1GEWDUH: ITPJCORE:WINNT - Refactoring - Unable to undo refactor change
-		return new IRunnableWithProgress(){
-			public void run(IProgressMonitor pm) throws InvocationTargetException {
-				try {
-					setPreflightStatus(Refactoring.getUndoManager().performUndo(context, pm));
-				} catch (JavaModelException e) {
-					throw new InvocationTargetException(e);			
-				} catch (ChangeAbortException e) {
-					throw new InvocationTargetException(e);
-				}
-			}
-
-		};
-	}
-}
