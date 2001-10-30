470e888e4a49d3eb2b5675788d70ee0a29971aa5
Darin Swanson <dswanson>
2001-10-30 22:23:24 +0000

    Removed unused local

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/launcher/AddExceptionAction.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/launcher/AddExceptionAction.java
@@ -1,13 +1,12 @@
+
+package org.eclipse.jdt.internal.ui.launcher;
+
 /*
  * (c) Copyright IBM Corp. 2000, 2001.
  * All Rights Reserved.
  */
-package org.eclipse.jdt.internal.ui.launcher;
 
 import org.eclipse.debug.core.DebugException;
-import org.eclipse.debug.core.DebugPlugin;
-import org.eclipse.debug.core.IBreakpointManager;
-import org.eclipse.debug.core.model.IBreakpoint;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.debug.core.JDIDebugModel;
 import org.eclipse.jdt.internal.ui.JavaPlugin;
@@ -32,9 +31,8 @@ public class AddExceptionAction implements IViewActionDelegate {
 				
 			boolean caught= dialog.isCaughtSelected();
 			boolean uncaught= dialog.isUncaughtSelected();
-			IBreakpointManager mgr= DebugPlugin.getDefault().getBreakpointManager();
 			try {
-				IBreakpoint breakpoint= JDIDebugModel.createExceptionBreakpoint(result, caught, uncaught, exceptionKind == AddExceptionDialog.CHECKED_EXCEPTION);
+				JDIDebugModel.createExceptionBreakpoint(result, caught, uncaught, exceptionKind == AddExceptionDialog.CHECKED_EXCEPTION);
 			} catch (DebugException exc) {
 				ExceptionHandler.handle(exc, LauncherMessages.getString("addExceptionAction.error.title"), LauncherMessages.getString("addExceptionAction.error.message")); //$NON-NLS-2$ //$NON-NLS-1$
 			}
