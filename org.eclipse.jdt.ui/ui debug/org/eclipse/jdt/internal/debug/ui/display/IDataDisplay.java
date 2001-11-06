ee1c6fe0ad45347e37656b93916fa7ddd572c88c
Darin Swanson <dswanson>
2001-11-06 15:32:25 +0000

    Removed unused code

--- org.eclipse.jdt.ui/ui debug/org/eclipse/jdt/internal/debug/ui/display/IDataDisplay.java
+++ org.eclipse.jdt.ui/ui debug/org/eclipse/jdt/internal/debug/ui/display/IDataDisplay.java
@@ -1,18 +1,16 @@
+
+package org.eclipse.jdt.internal.debug.ui.display;
+
 /*
  * (c) Copyright IBM Corp. 2000, 2001.
  * All Rights Reserved.
  */
-package org.eclipse.jdt.internal.debug.ui.display;
-
-import org.eclipse.jface.text.ITextSelection;
 
 public interface IDataDisplay {
 	
-	void clear();
+	public void clear();
 	
-	void displayExpression(String expression);
+	public void displayExpression(String expression);
 	
-	void displayExpressionValue(String value);
-	
-	void selectLineForEvaluation(ITextSelection selection);
+	public void displayExpressionValue(String value);
 }
