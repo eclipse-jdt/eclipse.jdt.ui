9481d71f1ce0fa4368a4f413218853d3bd742e39
Adam Kiezun <akiezun>
2001-11-14 18:19:40 +0000

    5722 Java delete should update build path

--- org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/core/refactoring/reorg/DeleteRefactoring.java
+++ org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/core/refactoring/reorg/DeleteRefactoring.java
@@ -266,8 +266,11 @@ public class DeleteRefactoring extends Refactoring {
 		CompositeChange composite= new CompositeChange("Delete package fragment root", 2);
 		if (ReorgUtils.isClasspathDelete(root))
 			composite.addChange(new DeleteFromClasspathChange(root));
-		else 
+		else {
+			if (root.isArchive())
+				composite.addChange(new DeleteFromClasspathChange(root));
 			composite.addChange(createDeleteChange(getResourceToDelete(root)));
+		}	
 		return composite;
 	}
 	
