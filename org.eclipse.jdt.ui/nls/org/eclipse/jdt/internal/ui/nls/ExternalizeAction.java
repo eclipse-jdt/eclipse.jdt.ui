73f4d6f334a8fd5684b2aae82c9d5316210be496
Adam Kiezun <akiezun>
2001-11-15 17:46:35 +0000

    adapted to constructor signature change

--- org.eclipse.jdt.ui/nls/org/eclipse/jdt/internal/ui/nls/ExternalizeAction.java
+++ org.eclipse.jdt.ui/nls/org/eclipse/jdt/internal/ui/nls/ExternalizeAction.java
@@ -1,17 +1,8 @@
 /*
  * (c) Copyright IBM Corp. 2000, 2001.
  * All Rights Reserved.
- */package org.eclipse.jdt.internal.ui.nls;import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
-
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.IWorkbenchWindowActionDelegate;
-import org.eclipse.ui.texteditor.IDocumentProvider;
-
-import org.eclipse.jdt.core.ICompilationUnit;
+ */package org.eclipse.jdt.internal.ui.nls;import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.JavaModelException;
-
 import org.eclipse.jdt.internal.core.refactoring.base.Refactoring;
 import org.eclipse.jdt.internal.core.refactoring.text.ITextBufferChangeCreator;
 import org.eclipse.jdt.internal.ui.JavaPlugin;
@@ -19,10 +10,15 @@ import org.eclipse.jdt.internal.ui.nls.model.NLSRefactoring;
 import org.eclipse.jdt.internal.ui.nls.search.ExceptionHandler;
 import org.eclipse.jdt.internal.ui.refactoring.actions.RefactoringAction;
 import org.eclipse.jdt.internal.ui.refactoring.changes.DocumentTextBufferChangeCreator;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+import org.eclipse.ui.texteditor.IDocumentProvider;
 
 public class ExternalizeAction implements IWorkbenchWindowActionDelegate{
 
-	protected static Refactoring createNewRefactoringInstance(ICompilationUnit cu) {		IDocumentProvider documentProvider= JavaPlugin.getDefault().getCompilationUnitDocumentProvider();		ITextBufferChangeCreator changeCreator= new DocumentTextBufferChangeCreator(documentProvider);
-		return new NLSRefactoring(changeCreator, cu);
+	protected static Refactoring createNewRefactoringInstance(ICompilationUnit cu) {		return new NLSRefactoring(cu);
 	}		/*	 * @see IWorkbenchWindowActionDelegate#dispose()	 */	public void dispose() {	}	/*	 * @see IWorkbenchWindowActionDelegate#init(IWorkbenchWindow)	 */	public void init(IWorkbenchWindow window) {	}		protected IStructuredSelection getSelection() {		IWorkbenchWindow window= JavaPlugin.getDefault().getWorkbench().getActiveWorkbenchWindow();		if (window == null)			return null;					ISelection selection= window.getSelectionService().getSelection();		if (selection instanceof IStructuredSelection) 			return (IStructuredSelection) selection;					return null;	}		protected ICompilationUnit getCompilationUnit(IStructuredSelection selection) {		if (selection == null)			return null;					Object first= selection.getFirstElement();		if (first instanceof ICompilationUnit) 			return (ICompilationUnit) first;				return null;	}		/*	 * @see IActionDelegate#run(IAction)	 */	public void run(IAction action) {		try{			ICompilationUnit unit= getCompilationUnit(getSelection());			if (unit == null)				return;				Refactoring refactoring= createNewRefactoringInstance(unit);			ExternalizeWizard wizard= new ExternalizeWizard(refactoring);			String title= Messages.getString("action.name"); //$NON-NLS-1$			RefactoringAction.activateRefactoringWizard(refactoring, wizard, title);		} catch (JavaModelException e){			ExceptionHandler.handle(e, "Externalize Strings", "Unexpected exception. See log for details.");		}	}	/*	 * @see IActionDelegate#selectionChanged(IAction, ISelection)	 */	public void selectionChanged(IAction action, ISelection selection) {	}
 }
