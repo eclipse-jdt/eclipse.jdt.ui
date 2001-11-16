eacfc4159c4a0580485483f38de2ad39e5dfb8db
Adam Kiezun <akiezun>
2001-11-16 16:00:23 +0000

    little rework

--- org.eclipse.jdt.ui/nls/org/eclipse/jdt/internal/ui/nls/ListDialog.java
+++ org.eclipse.jdt.ui/nls/org/eclipse/jdt/internal/ui/nls/ListDialog.java
@@ -3,9 +3,6 @@
  * All Rights Reserved.
  */
 package org.eclipse.jdt.internal.ui.nls;
-import org.eclipse.jface.viewers.ILabelProvider;
-import org.eclipse.jface.viewers.IStructuredContentProvider;
-import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ControlAdapter;
 import org.eclipse.swt.events.ControlEvent;
@@ -15,40 +12,64 @@ import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
+
+import org.eclipse.jface.viewers.ILabelProvider;
+import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.ViewerFilter;
+
 import org.eclipse.ui.dialogs.SelectionDialog;
 class ListDialog extends SelectionDialog {
+
 	private IStructuredContentProvider fContentProvider;
 	private ILabelProvider fLabelProvider;
 	private Object fInput;
+	private TableViewer fTableViewer;
 	
-	public ListDialog(Shell parent, Object input, String title, String message, IStructuredContentProvider sp, ILabelProvider lp) {
+	public ListDialog(Shell parent) {
 		super(parent);
-		setTitle(title);
-		setMessage(message);
+	}
+	protected void setInput(Object input) {
 		fInput= input;
+	}
+	
+	protected void setContentProvider(IStructuredContentProvider sp){
 		fContentProvider= sp;
+	}
+	
+	protected void setLabelProvider(ILabelProvider lp){
 		fLabelProvider= lp;
 	}
-
+	
+	protected TableViewer getTableViewer(){
+		return fTableViewer;
+	}
+		
+	protected boolean hasFilters(){
+		return fTableViewer.getFilters() != null && fTableViewer.getFilters().length != 0;
+	}
+	
 	protected Control createDialogArea(Composite container) {
 		Composite parent= (Composite) super.createDialogArea(container);
 		createMessageArea(parent);
-		TableViewer v= new TableViewer(parent);
-		v.setContentProvider(fContentProvider);
-		final Table table= v.getTable();
+		fTableViewer= new TableViewer(parent);
+		fTableViewer.setContentProvider(fContentProvider);
+		final Table table= fTableViewer.getTable();
 		final TableColumn c= new TableColumn(table, SWT.NULL);
-		v.getTable().addControlListener(new ControlAdapter() {
+		fTableViewer.getTable().addControlListener(new ControlAdapter() {
 			public void controlResized(ControlEvent e) {
 				c.setWidth(table.getSize().x-2*table.getBorderWidth());
 			}
 		});
-
-		v.setLabelProvider(fLabelProvider);
-		v.setInput(fInput);
+
+		fTableViewer.setLabelProvider(fLabelProvider);
+		fTableViewer.setInput(fInput);
 		GridData gd= new GridData(GridData.FILL_BOTH);
 		gd.heightHint= convertHeightInCharsToPixels(15);
 		gd.widthHint= convertWidthInCharsToPixels(50);
 		table.setLayoutData(gd);
-		return table;
+		return parent;
 	}
+
+
 }
