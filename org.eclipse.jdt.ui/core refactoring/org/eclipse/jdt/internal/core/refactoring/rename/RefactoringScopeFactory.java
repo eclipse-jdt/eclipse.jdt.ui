687fb41b8fac64314468fd8b7c335d5e2cbea181
Adam Kiezun <akiezun>
2001-11-19 16:53:44 +0000

    using new SearchEngine APi

--- org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/core/refactoring/rename/RefactoringScopeFactory.java
+++ org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/core/refactoring/rename/RefactoringScopeFactory.java
@@ -4,37 +4,52 @@
  */
 package org.eclipse.jdt.internal.core.refactoring.rename;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IResource;
+
 import org.eclipse.jdt.core.Flags;
 import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.IMember;
 import org.eclipse.jdt.core.IPackageFragment;
+import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.core.search.IJavaSearchScope;
 import org.eclipse.jdt.core.search.SearchEngine;
-import org.eclipse.jdt.internal.core.refactoring.base.Refactoring;
 
 class RefactoringScopeFactory {
-	private static IJavaSearchScope create(IJavaProject javaProject){
-		IProject project = javaProject.getProject();
-		Set projects= new  HashSet();
-		projects.add(project);
-		projects.addAll(Arrays.asList(project.getReferencingProjects()));
-		return SearchEngine.createJavaSearchScope((IResource[])projects.toArray(new IResource[projects.size()]));
+	private static IJavaSearchScope create(IJavaProject javaProject) {
+		List projects= new  ArrayList();
+		projects.add(javaProject);
+		projects.addAll(getReferencingJavaProjects(javaProject));
+		IJavaProject[] javaProjects= (IJavaProject[]) projects.toArray(new IJavaProject[projects.size()]);
+		return SearchEngine.createJavaSearchScope(javaProjects, false);
 	}
 	
-	static IJavaSearchScope create(IJavaElement javaElement) throws JavaModelException{
-		if (javaElement instanceof IMember){
+	private static List getReferencingJavaProjects(IJavaProject javaProject){
+		IProject[] refProjects= javaProject.getProject().getReferencingProjects();
+		List result= new ArrayList(refProjects.length);
+		for (int i= 0; i < refProjects.length; i++) {
+			IProject refProject= refProjects[i];
+			IJavaProject refJavaProject= JavaCore.create(refProject);
+			if (refJavaProject != null)
+				result.add(refJavaProject);
+		}
+		return result;
+	}
+	
+	static IJavaSearchScope create(IJavaElement javaElement) throws JavaModelException {
+		if (javaElement instanceof IMember) {
 			IMember member= (IMember)javaElement;
-			if (Flags.isPrivate(member.getFlags()))
-				return SearchEngine.createJavaSearchScope(new IJavaElement[]{member.getCompilationUnit()});
-			if (! Flags.isPublic(member.getFlags()) && !Flags.isProtected(member.getFlags())){
+			if (Flags.isPrivate(member.getFlags())) {
+				if (member.getCompilationUnit() != null)
+					return SearchEngine.createJavaSearchScope(new IJavaElement[]{member.getCompilationUnit()});
+				else 	
+					return SearchEngine.createJavaSearchScope(new IJavaElement[]{member});
+			}	
+			if (! Flags.isPublic(member.getFlags()) && !Flags.isProtected(member.getFlags()) && member.getCompilationUnit() != null) {
 				IPackageFragment pack= (IPackageFragment)member.getCompilationUnit().getParent();
 				return SearchEngine.createJavaSearchScope(new IJavaElement[]{pack});
 			}	
