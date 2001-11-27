ee6a61b28e0c5f0c57614b3306af576e21fa4526
Adam Kiezun <akiezun>
2001-11-27 16:57:35 +0000

    all tests

---
+++ org.eclipse.jdt.ui.tests.refactoring/test cases/org/eclipse/jdt/ui/tests/actions/AllTests.java
@@ -0,0 +1,19 @@
+package org.eclipse.jdt.ui.tests.actions;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import junit.textui.TestRunner;
+
+public class AllTests {
+	
+	public static void main (String[] args) {
+		junit.textui.TestRunner.run (suite());
+	}
+	public static Test suite ( ) {
+		TestSuite suite= new TestSuite("All Actions Tests");
+		suite.addTest(PasteSourceReferenceActionTests.suite());
+	    return suite;
+	}
+}
+
+
