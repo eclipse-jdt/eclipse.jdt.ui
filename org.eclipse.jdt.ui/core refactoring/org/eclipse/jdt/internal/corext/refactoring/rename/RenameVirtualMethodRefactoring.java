e63e873761d95b67db95e266ad9c31c7013e7a43
Adam Kiezun <akiezun>
2001-12-17 15:17:51 +0000

    moved util methods out

--- org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/rename/RenameVirtualMethodRefactoring.java
+++ org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/rename/RenameVirtualMethodRefactoring.java
@@ -55,12 +55,12 @@ class RenameVirtualMethodRefactoring extends RenameMethodRefactoring {
 
 			pm.subTask(RefactoringCoreMessages.getString("RenameVirtualMethodRefactoring.checking")); //$NON-NLS-1$
 
-			if (overridesAnotherMethod(new SubProgressMonitor(pm, 2)))
+			if (MethodChecks.overridesAnotherMethod(getMethod(), new SubProgressMonitor(pm, 2)))
 				result.addError(RefactoringCoreMessages.getString("RenameVirtualMethodRefactoring.overrides_another")); //$NON-NLS-1$
 
 			pm.subTask(RefactoringCoreMessages.getString("RenameVirtualMethodRefactoring.analyzing_hierarchy")); //$NON-NLS-1$
 
-			if (isDeclaredInInterface(new SubProgressMonitor(pm, 2)))
+			if (MethodChecks.isDeclaredInInterface(getMethod(), new SubProgressMonitor(pm, 2)))
 				result.addError(RefactoringCoreMessages.getFormattedString("RenameVirtualMethodRefactoring.from_interface", getMethod().getElementName( ))); //$NON-NLS-1$
 
 			pm.subTask(RefactoringCoreMessages.getString("RenameVirtualMethodRefactoring.analyzing_hierarchy")); //$NON-NLS-1$
@@ -80,77 +80,11 @@ class RenameVirtualMethodRefactoring extends RenameMethodRefactoring {
 		}
 	}
 	
-	private boolean isDeclaredInInterface(IProgressMonitor pm) throws JavaModelException {
-		try{	
-			pm.beginTask("", 4);
-			ITypeHierarchy hier= getMethod().getDeclaringType().newTypeHierarchy(new SubProgressMonitor(pm, 1));
-			IType[] classes= hier.getAllClasses();
-			IProgressMonitor subPm= new SubProgressMonitor(pm, 3);
-			subPm.beginTask("", classes.length);
-			for (int i= 0; i < classes.length; i++) {
-				ITypeHierarchy superTypes= classes[i].newSupertypeHierarchy(new SubProgressMonitor(subPm, 1));
-				IType[] superinterfaces= superTypes.getAllSuperInterfaces(classes[i]);
-				for (int j= 0; j < superinterfaces.length; j++) {
-					if (Checks.findMethod(getMethod(), superinterfaces[j]) != null)
-						return true;
-				}
-				subPm.worked(1);
-			}
-			return false;
-		} finally{
-			pm.done();
-		}
-	}
-	
-	
 	private boolean hierarchyDeclaresSimilarNativeMethod(IProgressMonitor pm) throws JavaModelException{
 		IType[] classes= getMethod().getDeclaringType().newTypeHierarchy(pm).getAllSubtypes(getMethod().getDeclaringType());
 		return classesDeclareOverridingNativeMethod(classes);
 	}
 		
-	boolean overridesAnotherMethod(IProgressMonitor pm) throws JavaModelException {
-		pm.beginTask("", 1); //$NON-NLS-1$
-		//XXX: use the commented code once this is fixed: 1GCZZS1: ITPJCORE:WINNT - inconsistent search for method declarations
-		//XXX: and delete findMethod
-		
-//		IType declaringType= getMethod().getDeclaringType();	
-//		ITypeHierarchy superTypes= declaringType.newSupertypeHierarchy(new SubProgressMonitor(pm, 1));
-//		pm.worked(1);
-//		IJavaSearchScope scope= SearchEngine.createHierarchyScope(declaringType);
-//		ISearchPattern pattern= SearchEngine.createSearchPattern(getMethod(), IJavaSearchConstants.DECLARATIONS);
-//		SearchResultCollector collector= new SearchResultCollector(new SubProgressMonitor(pm, 1));
-//		new SearchEngine().search(ResourcesPlugin.getWorkspace(), pattern, scope, collector);
-//		pm.done();
-//		for (Iterator iter= collector.getResults().iterator(); iter.hasNext(); ){
-//			SearchResult result= (SearchResult)iter.next();
-//			IType t= ((IMethod)result.getEnclosingElement()).getDeclaringType();
-//			if ((!t.equals(declaringType)) && superTypes.contains(t))
-//				return true;
-//		}
-//		return false;
-		IMethod found= findInHierarchy(getMethod().getDeclaringType().newSupertypeHierarchy(new SubProgressMonitor(pm, 1)), getMethod());
-		return (found != null && (! Flags.isStatic(found.getFlags())) && (! Flags.isPrivate(found.getFlags())));	
-	}
-	
-	/**
-	 * Finds a method in a type's hierarchy
-	 * This searches for a method with the same name and signature. Parameter types are only
-	 * compared by the simple name, no resolving for the fully qualified type name is done
-	 * The input type of the hierarchy is not searched for the method
-	 * @return The first found method or null, if nothing found
-	 */	
-	private static final IMethod findInHierarchy(ITypeHierarchy hierarchy, IMethod method) throws JavaModelException {
-		IType curr= hierarchy.getSuperclass(hierarchy.getType());
-		while (curr != null) {
-			IMethod found= Checks.findMethod(method, curr);
-			if (found != null) {
-				return found;
-			}
-			curr= hierarchy.getSuperclass(curr);
-		}
-		return null;
-	}
-	
 	private boolean classesDeclareOverridingNativeMethod(IType[] classes) throws JavaModelException{
 		for (int i= 0; i < classes.length; i++){
 			IMethod[] methods= classes[i].getMethods();
