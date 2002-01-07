e79a50045bffdef0916b67d16519b9eb640a26ea
Adam Kiezun <akiezun>
2002-01-07 16:01:27 +0000

    add a method

--- org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/util/JavaElementUtil.java
+++ org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/util/JavaElementUtil.java
@@ -1,5 +1,9 @@
 package org.eclipse.jdt.internal.corext.refactoring.util;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IMethod;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.core.Signature;
@@ -13,4 +17,15 @@ public class JavaElementUtil {
 	public static String createMethodSignature(IMethod method) throws JavaModelException {
 		return Signature.toString(method.getSignature(), method.getElementName(), method.getParameterNames(), false, true);
 	}
+	
+	public static IJavaElement[] getElementsOfType(IJavaElement[] elements, int type){
+		Set result= new HashSet(elements.length);
+		for (int i= 0; i < elements.length; i++) {
+			IJavaElement element= elements[i];
+			if (element.getElementType() == type)
+				result.add(element);
+		}
+		return (IJavaElement[]) result.toArray(new IJavaElement[result.size()]);
+	}
+	
 }
