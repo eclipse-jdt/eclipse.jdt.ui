d272437dc268244991799f8b0abce7bbaa46f25a
Adam Kiezun <akiezun>
2002-01-25 16:04:38 +0000

    removed a reduntant validity check

--- org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/rename/RenameTempRefactoring.java
+++ org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/rename/RenameTempRefactoring.java
@@ -116,9 +116,6 @@ public class RenameTempRefactoring extends Refactoring implements IRenameRefacto
 	 */
 	public RefactoringStatus checkActivation(IProgressMonitor pm)	throws JavaModelException {
 		
-		if (fSelectionStart < 0)
-			return RefactoringStatus.createFatalErrorStatus("A local variable declaration or reference must be selected to activate this refactoring");
-		
 		if (!(fCu instanceof CompilationUnit))
 			return RefactoringStatus.createFatalErrorStatus("Internal Error");
 
