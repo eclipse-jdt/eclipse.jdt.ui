8ee8b8bfd1f58076580461483ef468ffc0c73594
Adam Kiezun <akiezun>
2002-02-01 18:44:13 +0000

    added 'Move Members'

--- org.eclipse.jdt.ui/ui refactoring/org/eclipse/jdt/internal/ui/refactoring/actions/RefactoringGroup.java
+++ org.eclipse.jdt.ui/ui refactoring/org/eclipse/jdt/internal/ui/refactoring/actions/RefactoringGroup.java
@@ -17,6 +17,7 @@ import org.eclipse.jdt.core.IMethod;
 import org.eclipse.jdt.core.JavaModelException;
 
 import org.eclipse.jdt.internal.corext.refactoring.base.Refactoring;
+import org.eclipse.jdt.internal.corext.refactoring.structure.MoveMembersRefactoring;
 import org.eclipse.jdt.internal.corext.refactoring.structure.PullUpRefactoring;
 import org.eclipse.jdt.internal.corext.refactoring.structure.ReorderRenameParameterWrapperRefactoring;
 import org.eclipse.jdt.internal.ui.IJavaHelpContextIds;
@@ -24,6 +25,7 @@ import org.eclipse.jdt.internal.ui.actions.ContextMenuGroup;
 import org.eclipse.jdt.internal.ui.actions.GroupContext;
 import org.eclipse.jdt.internal.ui.actions.StructuredSelectionProvider;
 import org.eclipse.jdt.internal.ui.preferences.JavaPreferencesSettings;
+import org.eclipse.jdt.internal.ui.refactoring.MoveMembersWizard;
 import org.eclipse.jdt.internal.ui.refactoring.PullUpWizard;
 import org.eclipse.jdt.internal.ui.refactoring.RefactoringWizard;
 import org.eclipse.jdt.internal.ui.refactoring.ReorderParametersWizard;
@@ -56,6 +58,7 @@ public class RefactoringGroup extends ContextMenuGroup {
 		fRefactoringActions= new RefactoringAction[]{
 			createReorderParametersAction(provider),
 			createPullUpAction(provider),
+			createMoveMembersAction(provider),
 			new SelfEncapsulateFieldAction(provider)
 		};
 		
@@ -105,4 +108,28 @@ public class RefactoringGroup extends ContextMenuGroup {
 			}
 		};
 	}
+	
+	static OpenRefactoringWizardAction createMoveMembersAction(StructuredSelectionProvider selectionProvider) {
+		String label= "&Move...";
+		return new OpenRefactoringWizardAction(label, selectionProvider, IMember.class) {
+			protected Refactoring createNewRefactoringInstance(Object obj){
+				Set memberSet= new HashSet();
+				memberSet.addAll(Arrays.asList((Object[])obj));
+				IMember[] methods= (IMember[]) memberSet.toArray(new IMember[memberSet.size()]);
+				return new MoveMembersRefactoring(methods, JavaPreferencesSettings.getCodeGenerationSettings());
+			}
+			boolean canActivateRefactoring(Refactoring refactoring)  throws JavaModelException{
+				return ((MoveMembersRefactoring)refactoring).checkActivation(new NullProgressMonitor()).isOK();
+			}
+			protected boolean canOperateOnMultiSelection(){
+				return true;
+			}	
+			protected RefactoringWizard createWizard(Refactoring ref){
+				String title= "Move Members";
+				//FIX ME: wrong
+				String helpId= "HELPID";
+				return new MoveMembersWizard((MoveMembersRefactoring)ref, title, helpId);
+			}
+		};
+	}	
 }
