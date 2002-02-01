7a2c1af4fb731dbc97921eb76217a60f4d7c5a23
Adam Kiezun <akiezun>
2002-02-01 16:45:33 +0000

    add sorting method

--- org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/SourceRange.java
+++ org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/SourceRange.java
@@ -4,6 +4,9 @@
  */
 package org.eclipse.jdt.internal.corext.refactoring;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 import org.eclipse.jdt.core.ISourceRange;
 
 public class SourceRange implements ISourceRange{
@@ -37,6 +40,20 @@ public class SourceRange implements ISourceRange{
 	 */
 	public String toString(){
 		return "<offset: " + fOffset +" length: " + fLength + "/>";
+	}
+	
+	/**
+	 * Sorts the given ranges by offset (backwards).
+	 * Note: modifies the parameter.
+	 */
+	public static ISourceRange[] reverseSortByOffset(ISourceRange[] ranges){
+		Comparator comparator= new Comparator(){
+			public int compare(Object o1, Object o2){
+				return ((ISourceRange)o2).getOffset() - ((ISourceRange)o1).getOffset();
+			}
+		};
+		Arrays.sort(ranges, comparator);
+		return ranges;
 	}
 }
 
