c0c7504bc580f72d99ee5d24d034842bfb39a15d
Adam Kiezun <akiezun>
2002-02-05 11:12:15 +0000

    initial

---
+++ org.eclipse.jdt.ui/ui refactoring/org/eclipse/jdt/internal/ui/refactoring/actions/IRefactoringAction.java
@@ -0,0 +1,8 @@
+package org.eclipse.jdt.internal.ui.refactoring.actions;
+
+import org.eclipse.jface.action.IAction;
+
+public interface IRefactoringAction extends IAction {
+
+	public void update();
+}
