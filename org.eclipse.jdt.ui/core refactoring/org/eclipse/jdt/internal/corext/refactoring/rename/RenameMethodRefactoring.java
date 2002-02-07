f131a7307c6860880a54ca67a28110998e9b1b20
Adam Kiezun <akiezun>
2002-02-07 15:37:55 +0000

    converted UpdateMethodReferencesEdit to a top level class

--- org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/rename/RenameMethodRefactoring.java
+++ org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/rename/RenameMethodRefactoring.java
@@ -45,10 +45,7 @@ import org.eclipse.jdt.internal.corext.refactoring.tagging.IReferenceUpdatingRef
 import org.eclipse.jdt.internal.corext.refactoring.tagging.IRenameRefactoring;
 import org.eclipse.jdt.internal.corext.refactoring.util.WorkingCopyUtil;
 import org.eclipse.jdt.internal.corext.textmanipulation.SimpleTextEdit;
-import org.eclipse.jdt.internal.corext.textmanipulation.TextBuffer;
-import org.eclipse.jdt.internal.corext.textmanipulation.TextBufferEditor;
 import org.eclipse.jdt.internal.corext.textmanipulation.TextEdit;
-import org.eclipse.jdt.internal.corext.textmanipulation.TextRange;
 import org.eclipse.jdt.internal.corext.util.JavaModelUtil;
 
 public abstract class RenameMethodRefactoring extends Refactoring implements IRenameRefactoring, IReferenceUpdatingRefactoring{
@@ -420,58 +417,5 @@ public abstract class RenameMethodRefactoring extends Refactoring implements IRe
 	
 	TextEdit createTextChange(SearchResult searchResult) {
 		return new UpdateMethodReferenceEdit(searchResult.getStart(), searchResult.getEnd() - searchResult.getStart(), fNewName, fMethod.getElementName());		
-	}
-	
-	//----
-	private static class UpdateMethodReferenceEdit extends SimpleTextEdit{
-
-		private String fOldName;
-		
-		UpdateMethodReferenceEdit(int offset, int length, String newName, String oldName) {
-			super(offset, length, newName);
-			Assert.isNotNull(oldName);
-			fOldName= oldName;			
-		}
-		
-		private UpdateMethodReferenceEdit(TextRange range, String newName, String oldName) {
-			super(range, newName);
-			Assert.isNotNull(oldName);
-			fOldName= oldName;			
-		}
-
-		/* non Java-doc
-		 * @see TextEdit#copy
-		 */
-		public TextEdit copy() {
-			return new UpdateMethodReferenceEdit(getTextRange().copy(), getText(), fOldName);
-		}
-
-		/* non Java-doc
-		 * @see TextEdit#connect(TextBufferEditor)
-		 */
-		public void connect(TextBufferEditor editor) throws CoreException {
-			TextBuffer buffer= editor.getTextBuffer();
-			TextRange range= getTextRange();
-			String oldText= buffer.getContent(range.getOffset(), range.getLength());
-			String oldMethodName= fOldName;
-			int leftBracketIndex= oldText.indexOf("("); //$NON-NLS-1$
-			if (leftBracketIndex == -1)
-				return; 
-			int offset= range.getOffset();
-			int length= leftBracketIndex;
-			oldText= oldText.substring(0, leftBracketIndex);
-			int theDotIndex= oldText.lastIndexOf("."); //$NON-NLS-1$
-			if (theDotIndex == -1) {
-				setText(getText() + oldText.substring(oldMethodName.length()));
-			} else {
-				String subText= oldText.substring(theDotIndex);
-				int oldNameIndex= subText.indexOf(oldMethodName) + theDotIndex;
-				String ending= oldText.substring(theDotIndex, oldNameIndex) + getText();
-				oldText= oldText.substring(0, oldNameIndex + oldMethodName.length());
-				length= oldNameIndex + oldMethodName.length();
-				setText(oldText.substring(0, theDotIndex) + ending);
-			}			
-			setTextRange(new TextRange(offset, length));
-		}
 	}
 }	
