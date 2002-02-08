ba9daf6d4fc2275bcbee7937ac3d2af16c3be466
Martin Aeschlimann <maeschlimann>
2002-02-08 11:22:30 +0000

    added again until debugger does the change

---
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/util/AllTypesSearchEngine.java
@@ -0,0 +1,123 @@
+/*
+ * (c) Copyright IBM Corp. 2000, 2001.
+ * All Rights Reserved.
+ */
+
+package org.eclipse.jdt.internal.ui.util;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.search.IJavaSearchConstants;
+import org.eclipse.jdt.core.search.IJavaSearchScope;
+import org.eclipse.jdt.core.search.ITypeNameRequestor;
+import org.eclipse.jdt.core.search.SearchEngine;
+import org.eclipse.jdt.ui.IJavaElementSearchConstants;
+import org.eclipse.jface.operation.IRunnableContext;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.util.Assert;
+
+/**
+ * Search for all types in the workspace. Instead of returning objects of type <code>IType</code>
+ * the methods of this class returns a list of the lightweight objects <code>TypeInfo</code>.
+ * @deprecated use AllTypesCache
+ */
+public class AllTypesSearchEngine {
+
+	private IWorkspace fWorkspace;
+
+	public AllTypesSearchEngine(IWorkspace workspace) {
+		Assert.isNotNull(workspace);
+		fWorkspace= workspace;
+	}
+
+	/**
+	 * Search for type in the given search scope.
+	 * @param style a combination of <code>IJavaElementSearchConstants</code> flags
+	 */
+	public List searchTypes(IRunnableContext context, final IJavaSearchScope scope, final int style) {
+		final List typesFound= new ArrayList(2000);
+		
+		if (TypeCache.canReuse(style, scope)){
+			typesFound.addAll(TypeCache.getCachedTypes());
+			return typesFound;
+		}
+		
+		IRunnableWithProgress runnable= new IRunnableWithProgress() {
+			public void run(IProgressMonitor pm) {
+				searchTypes(typesFound, scope, style, pm);
+			}
+		};
+		
+		try {
+			context.run(true, true, runnable);
+		} catch (InvocationTargetException e) {
+			Throwable t= e.getTargetException();
+			if (t instanceof Error)
+				throw (Error)t;
+			if (t instanceof RuntimeException)
+				throw (RuntimeException)t;	
+			ExceptionHandler.handle(e, "Exception", "Unexpected exception. See log for details.");
+		} catch (InterruptedException e) {
+		}
+		
+		return typesFound;	
+	}
+	
+	/**
+	 * Search for type in the given search scope.
+	 * @param style a combination of <code>IJavaElementSearchConstants</code> flags
+	 */		
+	public void searchTypes(List typesFound, IJavaSearchScope scope, int style, IProgressMonitor pm) {
+		if (TypeCache.canReuse(style, scope)){
+			typesFound.addAll(TypeCache.getCachedTypes());
+			pm.done();
+			return;
+		}
+		TypeCache.flush();
+		TypeCache.setConfiguration(style);
+		doSearchTypes(typesFound, scope, style, pm);
+
+		TypeCache.setCachedTypes(createCopy(typesFound));
+		TypeCache.registerIfNecessary();
+	}
+
+	private static List createCopy(List list) {
+		List newList= new ArrayList(list.size());
+		newList.addAll(list);
+		return newList;
+	}
+	
+	private void doSearchTypes(List typesFound, IJavaSearchScope scope, int style, IProgressMonitor pm) {
+		Assert.isTrue((style & IJavaElementSearchConstants.CONSIDER_TYPES) != 0);
+				
+		try {
+			new SearchEngine().searchAllTypeNames(
+				fWorkspace,
+				null,
+				null,
+				IJavaSearchConstants.PATTERN_MATCH,
+				IJavaSearchConstants.CASE_INSENSITIVE,
+				computeKind(style),
+				scope,
+				new TypeInfoRequestor(typesFound),
+				IJavaSearchConstants.WAIT_UNTIL_READY_TO_SEARCH,
+				pm);
+		} catch(JavaModelException e){
+			ExceptionHandler.handle(e,  "Exception", "Unexpected exception. See log for details.");
+		}
+	}
+	
+	private static int computeKind(int style) {
+		int kind= IJavaSearchConstants.INTERFACE;
+		if ((style & IJavaElementSearchConstants.CONSIDER_INTERFACES) != 0 && (style & IJavaElementSearchConstants.CONSIDER_CLASSES) != 0) 
+			kind= IJavaSearchConstants.TYPE;
+		else if ((style & IJavaElementSearchConstants.CONSIDER_CLASSES) != 0)
+			kind= IJavaSearchConstants.CLASS;
+		return kind;
+	}
+}
---
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/util/TypeCache.java
@@ -0,0 +1,83 @@
+/* * (c) Copyright IBM Corp. 2000, 2001. * All Rights Reserved. */package org.eclipse.jdt.internal.ui.util;import java.util.ArrayList;import java.util.List;import org.eclipse.jdt.core.ElementChangedEvent;import org.eclipse.jdt.core.IElementChangedListener;import org.eclipse.jdt.core.IJavaElement;import org.eclipse.jdt.core.IJavaElementDelta;import org.eclipse.jdt.core.JavaCore;import org.eclipse.jdt.core.search.IJavaSearchScope;import org.eclipse.jdt.internal.core.search.JavaWorkspaceScope;/** * Cache used by AllTypesSeachEngine * @deprecated use AllTypesCache */
+class TypeCache{
+
+	private static int fgLastStyle= -1;
+	private static List fgTypeList;
+	private static boolean fgIsRegistered= false;		//no instances	private TypeCache(){	}
+	static List getCachedTypes() {		//must not return null		if (fgTypeList == null)			return new ArrayList(0); 		else				return fgTypeList;	}		static boolean canReuse(int style, IJavaSearchScope scope){		if (style != fgLastStyle)			return false;		if (! (scope instanceof JavaWorkspaceScope))			return false;		if (fgTypeList == null)			return false;		if (fgTypeList.isEmpty())			return false;			return true;			}		static void flush(){		fgTypeList= null;	}		static void setConfiguration(int style){		fgLastStyle= style;	}		static void setCachedTypes(List types){ //copy is passed here - no reason to copy again		fgTypeList= types;	}		static void registerIfNecessary(){		if (fgIsRegistered)			return;		JavaCore.addElementChangedListener(new DeltaListener());		fgIsRegistered= true;	}	
+	private static class DeltaListener implements IElementChangedListener {
+		public void elementChanged(ElementChangedEvent event) {
+			if (fgTypeList == null)
+				return;
+	
+			IJavaElementDelta delta= event.getDelta();
+			IJavaElement element= delta.getElement();			int type= element.getElementType();
+	
+			if (type == IJavaElement.CLASS_FILE)
+				return;
+	
+			processDelta(delta);
+		}
+	
+		private boolean mustFlush(IJavaElementDelta delta) {
+			if (delta.getKind() != IJavaElementDelta.CHANGED)
+				return true;
+	
+			//if it's a cu we wait
+			if (delta.getElement().getElementType() == IJavaElement.COMPILATION_UNIT)
+				return false;					
+			//must be only children
+			if (delta.getFlags() != IJavaElementDelta.F_CHILDREN)
+				return true;
+	
+			//special case: if it's a type then the type itself must be added or removed
+			if (delta.getElement().getElementType() == IJavaElement.TYPE)
+				return false;
+	
+			if ((delta.getAddedChildren() != null)
+				&& (delta.getAddedChildren().length != 0))
+				return true;
+	
+			return false;
+		}
+	
+		/*
+		 * returns false iff list is flushed and we can stop processing
+		 */
+		private boolean processDelta(IJavaElementDelta delta) {
+			if (mustStopProcessing(delta))
+				return true;
+	
+			if (mustFlush(delta)) {
+				flush();
+				return false;
+			}
+			IJavaElementDelta[] affectedChildren= delta.getAffectedChildren();
+			if (affectedChildren == null)
+				return true;
+	
+			for (int i= 0; i < affectedChildren.length; i++) {
+				if (!processDelta(affectedChildren[i]))
+					return false;
+			}
+			return true;
+		}
+	
+		private static boolean mustStopProcessing(IJavaElementDelta delta) {
+			int type= delta.getElement().getElementType();			if (type == IJavaElement.CLASS_FILE)				return true;
+			if (type == IJavaElement.FIELD)
+				return true;
+			if (type == IJavaElement.METHOD)
+				return true;
+			if (type == IJavaElement.INITIALIZER)
+				return true;
+			if (type == IJavaElement.PACKAGE_DECLARATION)
+				return true;
+			if (type == IJavaElement.IMPORT_CONTAINER)
+				return true;
+			if (type == IJavaElement.IMPORT_DECLARATION)
+				return true;
+			return false;
+		}
+	}
+}
