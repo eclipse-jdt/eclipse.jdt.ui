913cdf0206f80ba92c5def30424048eb94a21827
Adam Kiezun <akiezun>
2002-02-15 11:06:24 +0000

    2 more methods

--- org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/util/JavaElementUtil.java
+++ org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/util/JavaElementUtil.java
@@ -3,11 +3,17 @@ package org.eclipse.jdt.internal.corext.refactoring.util;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.eclipse.jdt.core.Flags;
 import org.eclipse.jdt.core.IField;
+import org.eclipse.jdt.core.IInitializer;
 import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IMember;
 import org.eclipse.jdt.core.IMethod;
+import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.core.Signature;
+
+import org.eclipse.jdt.internal.corext.refactoring.Assert;
 
 public class JavaElementUtil {
 	
@@ -25,6 +31,34 @@ public class JavaElementUtil {
 	
 	public static String createFieldSignature(IField field){
 		return field.getDeclaringType().getFullyQualifiedName() + "." + field.getElementName();
+	}
+	
+	public static String createInitializerSignature(IInitializer initializer){
+		String label= "initializer in " + initializer.getDeclaringType().getFullyQualifiedName();
+		try {
+			if (Flags.isStatic(initializer.getFlags()))
+				return "static " + label;
+			else 
+				return label;
+		} catch(JavaModelException e) {
+			return label; //fallback
+		}
+	}
+	
+	public static String createSignature(IMember member){
+		switch (member.getElementType()){
+			case IJavaElement.FIELD:
+				return createFieldSignature((IField)member);
+			case IJavaElement.TYPE:
+				return ((IType)member).getFullyQualifiedName();
+			case IJavaElement.INITIALIZER:
+				return "initializer";
+			case IJavaElement.METHOD:
+				return createMethodSignature((IMethod)member);				
+			default:
+				Assert.isTrue(false);
+				return null;	
+		}
 	}
 	
 	public static IJavaElement[] getElementsOfType(IJavaElement[] elements, int type){
