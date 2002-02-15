02c6a8ae44507bbcc296fd9a3868033fa6d0f275
Adam Kiezun <akiezun>
2002-02-15 14:14:08 +0000

    moved from refactoring

---
+++ org.eclipse.jdt.ui/core extension/org/eclipse/jdt/internal/corext/codemanipulation/GetterSetterUtil.java
@@ -0,0 +1,25 @@
+package org.eclipse.jdt.internal.corext.codemanipulation;
+
+import org.eclipse.jdt.core.IField;
+import org.eclipse.jdt.core.IMethod;
+import org.eclipse.jdt.core.JavaModelException;
+
+import org.eclipse.jdt.internal.corext.util.JavaModelUtil;
+
+public class GetterSetterUtil {
+	
+	//no instances
+	private GetterSetterUtil(){
+	}
+	
+	public static IMethod getGetter(IField field, String[] namePrefixes, String[] nameSuffixes) throws JavaModelException{
+		String getterName= new NameProposer(namePrefixes, nameSuffixes).proposeGetterName(field.getElementName());
+		return JavaModelUtil.findMethod(getterName, new String[0], false, field.getDeclaringType());
+	}
+	
+	public static IMethod getSetter(IField field, String[] namePrefixes, String[] nameSuffixes) throws JavaModelException{
+		String[] args= new String[] { field.getTypeSignature() };	
+		String setterName= new NameProposer(namePrefixes, nameSuffixes).proposeSetterName(field.getElementName());
+		return JavaModelUtil.findMethod(setterName, args, false, field.getDeclaringType());
+	}
+}
