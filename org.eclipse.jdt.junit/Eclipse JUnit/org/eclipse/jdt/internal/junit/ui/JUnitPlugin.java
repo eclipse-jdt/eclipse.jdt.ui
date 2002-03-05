9388bee2806b69fdcdcfda6ecc43c1676c4b962a
Adam Kiezun <akiezun>
2002-03-05 16:39:08 +0000

    backed out JUnit launch configuration support

--- org.eclipse.jdt.junit/Eclipse JUnit/org/eclipse/jdt/internal/junit/ui/JUnitPlugin.java
+++ org.eclipse.jdt.junit/Eclipse JUnit/org/eclipse/jdt/internal/junit/ui/JUnitPlugin.java
@@ -7,7 +7,6 @@ package org.eclipse.jdt.internal.junit.ui;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import org.eclipse.jdt.internal.junit.launcher.JUnitBaseLaunchConfiguration;
 import org.eclipse.jdt.internal.junit.oldlauncher.*;
 
 import org.eclipse.core.runtime.CoreException;
@@ -138,18 +137,19 @@ public class JUnitPlugin extends AbstractUIPlugin implements ILaunchListener {
 			port= launcherDelegate.getPort();
 		} else {
 			// new launch configs
-			ILaunchConfiguration config= launch.getLaunchConfiguration();
-			if (config != null) {
-				// test whether the launch defines the JUnit attributes
-				String portStr= launch.getAttribute(JUnitBaseLaunchConfiguration.PORT_ATTR);
-				String typeStr= launch.getAttribute(JUnitBaseLaunchConfiguration.TESTTYPE_ATTR);
-				if (portStr != null && typeStr != null) {
-					port= Integer.parseInt(portStr);
-					IJavaElement element= JavaCore.create(typeStr);
-					if (element instanceof IType) 
-						launchedType= (IType)element;
-				}
-			}	
+			// disable new launch config support
+//			ILaunchConfiguration config= launch.getLaunchConfiguration();
+//			if (config != null) {
+//				// test whether the launch defines the JUnit attributes
+//				String portStr= launch.getAttribute(JUnitBaseLaunchConfiguration.PORT_ATTR);
+//				String typeStr= launch.getAttribute(JUnitBaseLaunchConfiguration.TESTTYPE_ATTR);
+//				if (portStr != null && typeStr != null) {
+//					port= Integer.parseInt(portStr);
+//					IJavaElement element= JavaCore.create(typeStr);
+//					if (element instanceof IType) 
+//						launchedType= (IType)element;
+//				}
+//			}	
 		}
 		if (launchedType != null) 
 			connectTestRunner(launch, launchedType, port);
