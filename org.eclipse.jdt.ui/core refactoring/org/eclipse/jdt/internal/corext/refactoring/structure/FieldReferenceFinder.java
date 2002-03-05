4751cd0abc431cf086120d2df2cb6515fcb6ad18
Adam Kiezun <akiezun>
2002-03-05 12:10:55 +0000

    converted to the new ast

--- org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/structure/FieldReferenceFinder.java
+++ org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/structure/FieldReferenceFinder.java
@@ -1,36 +1,27 @@
 package org.eclipse.jdt.internal.corext.refactoring.structure;
+
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
 
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.ISourceRange;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.FieldAccess;
+import org.eclipse.jdt.core.dom.QualifiedName;
+import org.eclipse.jdt.core.dom.SimpleName;
 
-import org.eclipse.jdt.internal.compiler.ast.AstNode;
-import org.eclipse.jdt.internal.compiler.ast.FieldReference;
-import org.eclipse.jdt.internal.compiler.ast.QualifiedNameReference;
-import org.eclipse.jdt.internal.compiler.ast.SingleNameReference;
-import org.eclipse.jdt.internal.compiler.lookup.BlockScope;
 import org.eclipse.jdt.internal.corext.refactoring.Assert;
 import org.eclipse.jdt.internal.corext.refactoring.SearchResult;
 import org.eclipse.jdt.internal.corext.refactoring.SearchResultGroup;
 import org.eclipse.jdt.internal.corext.refactoring.SourceRange;
-import org.eclipse.jdt.internal.corext.refactoring.util.AST;
-import org.eclipse.jdt.internal.corext.refactoring.util.ASTUtil;
-import org.eclipse.jdt.internal.corext.refactoring.util.GenericVisitor;
 
-class FieldReferenceFinder extends GenericVisitor{
+class FieldReferenceFinder {
 
-	private List fFieldReferenceRanges;
-	private SearchResult[] fSearchResults;
-	
-	private FieldReferenceFinder(SearchResult[] searchResults){
-		fSearchResults= searchResults;
-		fFieldReferenceRanges= new ArrayList();
-	}
-	
 	public static ISourceRange[] findFieldReferenceRanges(SearchResultGroup searchResultGroup) throws JavaModelException {
 		IJavaElement je= JavaCore.create(searchResultGroup.getResource());
 		if (je == null || je.getElementType() != IJavaElement.COMPILATION_UNIT)
@@ -43,137 +34,108 @@ class FieldReferenceFinder extends GenericVisitor{
 		Assert.isNotNull(searchResults);
 		if (searchResults.length == 0)
 			return new ISourceRange[0];
-		AST ast= new AST(cu);
-		if (ast.isMalformed())
-			return new ISourceRange[0];
-		FieldReferenceFinder instance= new FieldReferenceFinder(searchResults);
-		ast.accept(instance);
-		return (ISourceRange[]) instance.fFieldReferenceRanges.toArray(new ISourceRange[instance.fFieldReferenceRanges.size()]);
+		FieldReferenceFinderVisitor visitor= new FieldReferenceFinderVisitor(searchResults);
+		AST.parseCompilationUnit(cu, false).accept(visitor);
+		return visitor.getFoundRanges();
 	}
 	
-	//-----
-	private static boolean areReportedForSameNode(SingleNameReference node, SearchResult searchResult){
-		int nodeEnd= ASTUtil.getSourceEnd(node) + 1;//???
+	///---- ast visitor ----------
+	
+	private static class FieldReferenceFinderVisitor extends ASTVisitor{
+		private Collection fFoundRanges;
+		private SearchResult[] fSearchResults;
 		
-		if (ASTUtil.getSourceStart(node) != searchResult.getStart())
-			return false;
-		if (nodeEnd < searchResult.getEnd())	
-			return false;
-			
-		return true;	
-	}
-
-	private static boolean areReportedForSameNode(FieldReference node, SearchResult searchResult){
-		int nodeStart= ASTUtil.getSourceStart(node);
-		int nodeEnd= ASTUtil.getSourceEnd(node) + 1;//???
+		FieldReferenceFinderVisitor(SearchResult[] searchResults){
+			fFoundRanges= new ArrayList();
+			fSearchResults= searchResults;
+		}
 		
-		if (nodeStart > searchResult.getStart())
-			return false;
-		if (nodeEnd < searchResult.getEnd())	
-			return false;
-		if (ASTUtil.getStart(node.nameSourcePosition) != searchResult.getStart())
-			return false;
-			
-		return true;	
-	}	
-	
-	private static boolean areReportedForSameNode(QualifiedNameReference node, SearchResult searchResult){
-		int nodeStart= ASTUtil.getSourceStart(node);
-		int nodeEnd= ASTUtil.getSourceEnd(node) + 1;//???
+		ISourceRange[] getFoundRanges(){
+			return (ISourceRange[]) fFoundRanges.toArray(new ISourceRange[fFoundRanges.size()]);
+		}
 		
-		if (nodeStart > searchResult.getStart())
-			return false;
-		if (nodeEnd < searchResult.getEnd())	
-			return false;
-			
-		return true;	
-	}	
-	
-	private boolean isReported(FieldReference node){
-		for (int i= 0; i < fSearchResults.length; i++) {
-			if (areReportedForSameNode(node, fSearchResults[i]))
-				return true;
+		private static int getNodeEnd(ASTNode node){
+			return node.getStartPosition() + node.getLength();
 		}
-		return false;
-	}
-	
-	private boolean isReported(QualifiedNameReference node){
-		for (int i= 0; i < fSearchResults.length; i++) {
-			if (areReportedForSameNode(node, fSearchResults[i]))
-				return true;
+		
+		private static boolean areReportedForSameNode(SimpleName node, SearchResult searchResult){
+			if (node.getStartPosition() != searchResult.getStart())
+				return false;
+			if (getNodeEnd(node) < searchResult.getEnd())	
+				return false;
+				
+			return true;	
 		}
-		return false;
-	}
 	
-	private boolean isReported(SingleNameReference node){
-		for (int i= 0; i < fSearchResults.length; i++) {
-			if (areReportedForSameNode(node, fSearchResults[i]))
-				return true;
-		}
-		return false;
-	}
-	
-	private boolean isReported(AstNode node){
-		if (node instanceof FieldReference)
-			return isReported((FieldReference)node);
-	
-		if (node instanceof QualifiedNameReference)
-			return isReported((QualifiedNameReference)node);
-			
-		if (node instanceof SingleNameReference)
-			return isReported((SingleNameReference)node);
-
-		return false;	
-	}
-	
-	///----  visit methods 
-
-	private void addIfReported(AstNode node) {
-		if (! isReported(node))
-			return;
-			
-		int start= getSourceStart(node);
-		int end= getSourceEnd(node);
-		int length= end - start + 1;
-		fFieldReferenceRanges.add(new SourceRange(start, length));
-	}
-
-	private int getSourceEnd(AstNode node) {
-		return ASTUtil.getSourceEnd(node);
-	}
-
-	private int getSourceStart(AstNode node) {
-		if (node instanceof FieldReference){
-			FieldReference fr= (FieldReference)node;
-			if (fr.receiver != null)
-				return ASTUtil.getSourceStart(fr.receiver);
-			else	
-				return ASTUtil.getSourceStart(node);
+		private static boolean areReportedForSameNode(FieldAccess node, SearchResult searchResult){
+			if (node.getStartPosition() > searchResult.getStart())
+				return false;
+			if (getNodeEnd(node) < searchResult.getEnd())	
+				return false;
+			if (node.getName().getStartPosition() != searchResult.getStart())
+				return false;
+				
+			return true;	
 		}	
-		return ASTUtil.getSourceStart(node);
-	}
-
-	/*
-	 * @see GenericVisitor#visit(FieldReference, BlockScope)
-	 */	
-	public boolean visit(FieldReference node, BlockScope scope) {
-		addIfReported(node);
-		return visitNode(node, scope);
-	}
-
-	/*
-	 * @see GenericVisitor#visit(QualifiedNameReference, BlockScope)
-	 */		
-	public boolean visit(QualifiedNameReference node, BlockScope scope) {
-		addIfReported(node);
-		return visitNode(node, scope);
-	}
-
-	/*
-	 * @see GenericVisitor#visit(SingleNameReference, BlockScope)
-	 */		
-	public boolean visit(SingleNameReference node, BlockScope scope) {
-		addIfReported(node);
-		return visitNode(node, scope);
+		
+		private static boolean areReportedForSameNode(QualifiedName node, SearchResult searchResult){
+			if (node.getStartPosition() > searchResult.getStart())
+				return false;
+			if (getNodeEnd(node) < searchResult.getEnd())	
+				return false;
+				
+			return true;	
+		}
+		
+		private boolean isReported(FieldAccess node){
+			for (int i= 0; i < fSearchResults.length; i++) {
+				if (areReportedForSameNode(node, fSearchResults[i]))
+					return true;
+			}
+			return false;
+		}
+		
+		private boolean isReported(QualifiedName node){
+			for (int i= 0; i < fSearchResults.length; i++) {
+				if (areReportedForSameNode(node, fSearchResults[i]))
+					return true;
+			}
+			return false;
+		}
+		
+		private boolean isReported(SimpleName node){
+			for (int i= 0; i < fSearchResults.length; i++) {
+				if (areReportedForSameNode(node, fSearchResults[i]))
+					return true;
+			}
+			return false;
+		}
+		
+		//-- visit methods ---
+		
+		public boolean visit(FieldAccess node) {
+			if (! isReported(node))
+				return true;
+			
+			fFoundRanges.add(new SourceRange(node));
+			return false;
+		}
+	
+		public boolean visit(QualifiedName node) {
+			if (! isReported(node))
+				return true;
+			
+			fFoundRanges.add(new SourceRange(node));
+			return false;
+		}
+	
+		public boolean visit(SimpleName node) {
+			if (! isReported(node))
+				return true;
+			
+			fFoundRanges.add(new SourceRange(node));
+			return false;
+		}
+		
 	}
 }
