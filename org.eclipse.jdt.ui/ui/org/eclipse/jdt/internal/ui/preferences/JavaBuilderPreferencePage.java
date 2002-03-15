c959ea63ac321c0a2bfafad77bdeae1719a2ee98
Martin Aeschlimann <maeschlimann>
2002-03-15 16:12:32 +0000

    added builderpreferencepage

---
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/preferences/JavaBuilderPreferencePage.java
@@ -0,0 +1,237 @@
+/*
+ * (c) Copyright IBM Corp. 2000, 2001.
+ * All Rights Reserved.
+ */
+package org.eclipse.jdt.internal.ui.preferences;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Hashtable;
+import java.util.StringTokenizer;
+
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.IncrementalProjectBuilder;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.dialogs.ProgressMonitorDialog;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.preference.PreferencePage;
+
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+import org.eclipse.jdt.core.JavaCore;
+
+import org.eclipse.jdt.internal.ui.JavaPlugin;
+import org.eclipse.jdt.internal.ui.JavaUIMessages;
+import org.eclipse.jdt.internal.ui.dialogs.StatusInfo;
+import org.eclipse.jdt.internal.ui.dialogs.StatusUtil;
+import org.eclipse.jdt.internal.ui.util.ExceptionHandler;
+import org.eclipse.jdt.internal.ui.wizards.dialogfields.DialogField;
+import org.eclipse.jdt.internal.ui.wizards.dialogfields.IDialogFieldListener;
+import org.eclipse.jdt.internal.ui.wizards.dialogfields.LayoutUtil;
+import org.eclipse.jdt.internal.ui.wizards.dialogfields.StringDialogField;
+
+public class JavaBuilderPreferencePage extends PreferencePage implements IWorkbenchPreferencePage {
+
+	private StringDialogField fResourceFilterField;
+	private StatusInfo fResourceFilterStatus;
+	
+	private Hashtable fWorkingValues;
+
+	private static final String PREF_RESOURCE_FILTER= "org.eclipse.jdt.core.builder.resourceCopyExclusionFilter";
+
+	private static String[] getAllKeys() {
+		return new String[] {
+			PREF_RESOURCE_FILTER
+		};	
+	}
+	
+	public JavaBuilderPreferencePage() {
+		setPreferenceStore(JavaPlugin.getDefault().getPreferenceStore());
+		setDescription(JavaUIMessages.getString("JavaBuilderPreferencePage.description")); //$NON-NLS-1$
+	
+		fWorkingValues= JavaCore.getOptions();
+
+
+		IDialogFieldListener listener= new IDialogFieldListener() {
+			public void dialogFieldChanged(DialogField field) {
+				doValidation();
+			}
+		};
+		
+		fResourceFilterField= new StringDialogField();
+		fResourceFilterField.setDialogFieldListener(listener);
+		fResourceFilterField.setLabelText(JavaUIMessages.getString("JavaBuilderPreferencePage.filter.label"));
+				
+		updateControls();				
+	}
+	
+
+	/*
+	 * @see PreferencePage#createContents(Composite)
+	 */
+	protected Control createContents(Composite parent) {
+		initializeDialogUnits(parent);
+		
+		GridLayout layout= new GridLayout();
+		layout.marginHeight= 0;
+		layout.marginWidth= 0;
+		layout.numColumns= 2;
+
+		Composite composite= new Composite(parent, SWT.NONE);
+		composite.setLayout(layout);
+
+		DialogField resourceFilterLabel= new DialogField();
+		resourceFilterLabel.setLabelText(JavaUIMessages.getString("JavaBuilderPreferencePage.filter.description"));
+
+		resourceFilterLabel.doFillIntoGrid(composite, 2);
+		LayoutUtil.setWidthHint(resourceFilterLabel.getLabelControl(null), convertWidthInCharsToPixels(80));
+
+		fResourceFilterField.doFillIntoGrid(composite, 2);
+		LayoutUtil.setHorizontalGrabbing(fResourceFilterField.getTextControl(null));
+		LayoutUtil.setWidthHint(fResourceFilterField.getTextControl(null), convertWidthInCharsToPixels(50));
+
+		return composite;
+	}
+	
+	/**
+	 * Initializes the current options (read from preference store)
+	 */
+	public static void initDefaults(IPreferenceStore store) {
+		Hashtable hashtable= JavaCore.getDefaultOptions();
+		Hashtable currOptions= JavaCore.getOptions();
+		String[] allKeys= getAllKeys();
+		for (int i= 0; i < allKeys.length; i++) {
+			String key= allKeys[i];
+			String defValue= (String) hashtable.get(key);
+			if (defValue != null) {
+				store.setDefault(key, defValue);
+			} else {
+				JavaPlugin.logErrorMessage("JavaBuilderPreferencePage: value is null: " + key);
+			}
+			// update the JavaCore options from the pref store
+			String val= store.getString(key);
+			if (val != null) {
+				currOptions.put(key, val);
+			}			
+		}
+		JavaCore.setOptions(currOptions);
+	}	
+	
+
+	/*
+	 * @see IWorkbenchPreferencePage#init(IWorkbench)
+	 */
+	public void init(IWorkbench workbench) {
+	}
+
+
+	/*
+	 * @see IPreferencePage#performOk()
+	 */
+	public boolean performOk() {
+		String[] allKeys= getAllKeys();
+		// preserve other options
+		// store in JCore and the preferences
+		Hashtable actualOptions= JavaCore.getOptions();
+		IPreferenceStore store= getPreferenceStore();
+		boolean hasChanges= false;
+		for (int i= 0; i < allKeys.length; i++) {
+			String key= allKeys[i];
+			String val=  (String) fWorkingValues.get(key);
+			String oldVal= (String) actualOptions.get(key);
+			hasChanges= hasChanges | !val.equals(oldVal);
+			
+			actualOptions.put(key, val);
+			store.setValue(key, val);
+		}
+		JavaCore.setOptions(actualOptions);
+		
+		if (hasChanges) {
+			String title= JavaUIMessages.getString("JavaBuilderPreferencePage.needsbuild.title");
+			String message= JavaUIMessages.getString("JavaBuilderPreferencePage.needsbuild.message");
+			if (MessageDialog.openQuestion(getShell(), title, message)) {
+				doFullBuild();
+			}
+		}
+		return super.performOk();
+	}
+
+
+	private void doValidation() {
+		fWorkingValues.put(PREF_RESOURCE_FILTER,  fResourceFilterField.getText());
+		IStatus status= validateResourceFilters();
+		updateStatus(status);
+	}
+	
+	private IStatus validateResourceFilters() {
+		IWorkspace workspace= ResourcesPlugin.getWorkspace();
+		
+		String text= fResourceFilterField.getText();
+		StringTokenizer tok= new StringTokenizer(text, ",");
+		while (tok.hasMoreTokens()) {
+			String curr= tok.nextToken().trim();
+			String fileName= curr.replace('*', 'x');
+			IStatus status= workspace.validateName(fileName, IResource.FILE);
+			if (status.matches(IStatus.ERROR)) {
+				String message= JavaUIMessages.getFormattedString("JavaBuilderPreferencePage.filter.invalidsegment.error", status.getMessage());
+				return new StatusInfo(IStatus.ERROR, message);
+			}
+		}
+		return new StatusInfo();
+	}
+	
+
+	private void updateStatus(IStatus status) {
+		setValid(!status.matches(IStatus.ERROR));
+		StatusUtil.applyToStatusLine(this, status);
+	}
+
+	private void doFullBuild() {
+		ProgressMonitorDialog dialog= new ProgressMonitorDialog(getShell());
+		try {
+			dialog.run(true, true, new IRunnableWithProgress() {
+				public void run(IProgressMonitor monitor) throws InvocationTargetException {
+					try {
+						JavaPlugin.getWorkspace().build(IncrementalProjectBuilder.FULL_BUILD, monitor);
+					} catch (CoreException e) {
+						throw new InvocationTargetException(e);
+					}
+				}
+			});
+		} catch (InterruptedException e) {
+			// cancelled by user
+		} catch (InvocationTargetException e) {
+			String title= JavaUIMessages.getString("JavaBuilderPreferencePage.builderror.title");
+			String message= JavaUIMessages.getString("JavaBuilderPreferencePage.builderror.message");
+			ExceptionHandler.handle(e, getShell(), title, message);
+		}
+	}		
+	
+	/*
+	 * @see PreferencePage#performDefaults()
+	 */
+	protected void performDefaults() {
+		fWorkingValues= JavaCore.getDefaultOptions();
+		updateControls();
+		doValidation();
+		super.performDefaults();
+	}
+	
+	private void updateControls() {
+		// update the UI
+		fResourceFilterField.setText((String) fWorkingValues.get(PREF_RESOURCE_FILTER));
+	}
+
+}
--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/preferences/JavadocPreferencePage.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/preferences/JavadocPreferencePage.java
@@ -34,10 +34,6 @@ import org.eclipse.jdt.internal.ui.wizards.dialogfields.IStringButtonAdapter;
 import org.eclipse.jdt.internal.ui.wizards.dialogfields.LayoutUtil;
 import org.eclipse.jdt.internal.ui.wizards.dialogfields.StringButtonDialogField;
 
-/**
- * @version 	1.0
- * @author
- */
 public class JavadocPreferencePage extends PreferencePage implements IWorkbenchPreferencePage {
 
 	private StringButtonDialogField fJavadocSelection;
@@ -45,7 +41,7 @@ public class JavadocPreferencePage extends PreferencePage implements IWorkbenchP
 
 	private StatusInfo fJavadocCommandStatus;
 
-	private static final String SETTINGS_JAVADOCPREFERENCES_COMMAND= "command";
+	private static final String PREF_JAVADOC_COMMAND= "command";
 
 	private class JDocDialogFieldAdapter implements IDialogFieldListener, IStringButtonAdapter {
 		/*
@@ -67,13 +63,19 @@ public class JavadocPreferencePage extends PreferencePage implements IWorkbenchP
 
 	public static String getJavaDocCommand() {
 		IPreferenceStore store= JavaPlugin.getDefault().getPreferenceStore();
-		return store.getString(SETTINGS_JAVADOCPREFERENCES_COMMAND);
+		return store.getString(PREF_JAVADOC_COMMAND);
 	}
 
+	public JavadocPreferencePage() {
+		setPreferenceStore(JavaPlugin.getDefault().getPreferenceStore());
+		//setDescription("Javadoc command"); //$NON-NLS-1$
+	}
 	/*
 	 * @see PreferencePage#createContents(Composite)
 	 */
 	protected Control createContents(Composite parent) {
+		initializeDialogUnits(parent);
+		
 		GridLayout layout= new GridLayout();
 		layout.marginHeight= 0;
 		layout.marginWidth= 0;
@@ -83,14 +85,19 @@ public class JavadocPreferencePage extends PreferencePage implements IWorkbenchP
 		fComposite.setLayout(layout);
 
 		DialogField javaDocCommentLabel= new DialogField();
-		javaDocCommentLabel.setLabelText("Location of the Javadoc command must be an absolute path.");
+		javaDocCommentLabel.setLabelText("Specify the location of the Javadoc command to be used by the Javadoc export wizard. Location must be an absolute path.");
 		javaDocCommentLabel.doFillIntoGrid(fComposite, 3);
+		LayoutUtil.setWidthHint(javaDocCommentLabel.getLabelControl(null), convertWidthInCharsToPixels(80));
 
-		fJavadocSelection= new StringButtonDialogField(new JDocDialogFieldAdapter());
+		JDocDialogFieldAdapter adapter= new JDocDialogFieldAdapter();
+
+		fJavadocSelection= new StringButtonDialogField(adapter);
+		fJavadocSelection.setDialogFieldListener(adapter);
 		fJavadocSelection.setLabelText("J&avadoc command:");
 		fJavadocSelection.setButtonLabel("Bro&wse...");
 		fJavadocSelection.doFillIntoGrid(fComposite, 3);
 		LayoutUtil.setHorizontalGrabbing(fJavadocSelection.getTextControl(null));
+		LayoutUtil.setWidthHint(fJavadocSelection.getTextControl(null), convertWidthInCharsToPixels(50));
 
 		initFields();
 
@@ -106,9 +113,9 @@ public class JavadocPreferencePage extends PreferencePage implements IWorkbenchP
 	public static void initDefaults(IPreferenceStore store) {
 		File file= findJavaDocCommand();
 		if (file != null)
-			store.setDefault(SETTINGS_JAVADOCPREFERENCES_COMMAND, file.getPath());
+			store.setDefault(PREF_JAVADOC_COMMAND, file.getPath());
 		else
-			store.setDefault(SETTINGS_JAVADOCPREFERENCES_COMMAND, "");
+			store.setDefault(PREF_JAVADOC_COMMAND, "");
 	}
 
 	private static File findJavaDocCommand() {
@@ -134,26 +141,26 @@ public class JavadocPreferencePage extends PreferencePage implements IWorkbenchP
 	}
 
 	private void initFields() {
-		IPreferenceStore prefs= JavaPlugin.getDefault().getPreferenceStore();
+		IPreferenceStore prefs= getPreferenceStore();
 
-		String command= prefs.getString(SETTINGS_JAVADOCPREFERENCES_COMMAND);
+		String command= prefs.getString(PREF_JAVADOC_COMMAND);
 		fJavadocSelection.setText(command);
 	}
 
 	public boolean performOk() {
-		IPreferenceStore prefs= JavaPlugin.getDefault().getPreferenceStore();
-		prefs.setValue(SETTINGS_JAVADOCPREFERENCES_COMMAND, fJavadocSelection.getText());
+		IPreferenceStore prefs= getPreferenceStore();
+		prefs.setValue(PREF_JAVADOC_COMMAND, fJavadocSelection.getText());
 		return super.performOk();
 	}
 
 	protected void performDefaults() {
-		IPreferenceStore prefs= JavaPlugin.getDefault().getPreferenceStore();
+		IPreferenceStore prefs= getPreferenceStore();
 
-		String str= prefs.getDefaultString(SETTINGS_JAVADOCPREFERENCES_COMMAND);
+		String str= prefs.getDefaultString(PREF_JAVADOC_COMMAND);
 		File jdocCommand= findJavaDocCommand();
 		if (jdocCommand != null) {
 			str= jdocCommand.getPath();
-			prefs.setDefault(SETTINGS_JAVADOCPREFERENCES_COMMAND, str);
+			prefs.setDefault(PREF_JAVADOC_COMMAND, str);
 		}
 		fJavadocSelection.setText(str);
 
