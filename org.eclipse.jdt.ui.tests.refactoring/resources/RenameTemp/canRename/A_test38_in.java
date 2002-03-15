d2c7c7e52fd7884aba706ef2b25e515d70ad7cfe
Adam Kiezun <akiezun>
2002-03-15 16:46:48 +0000

    test for 11453

---
+++ org.eclipse.jdt.ui.tests.refactoring/resources/RenameTemp/canRename/A_test38_in.java
@@ -0,0 +1,10 @@
+//rename to: i
+package p;
+class A{
+void m(){
+   for(int i= 0; i < 1; i++){
+   }
+   for(int j= 0; j < 1; j++){
+   }
+}
+}
---
+++ org.eclipse.jdt.ui.tests.refactoring/resources/RenameTemp/canRename/A_test38_out.java
@@ -0,0 +1,10 @@
+//rename to: i
+package p;
+class A{
+void m(){
+   for(int i= 0; i < 1; i++){
+   }
+   for(int i= 0; i < 1; i++){
+   }
+}
+}
