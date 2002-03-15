025a13cdf417522dd7574e6f9a8e6750f29068c4
Adam Kiezun <akiezun>
2002-03-15 16:34:19 +0000

    test for 11424

--- org.eclipse.jdt.ui.tests.refactoring/test cases/org/eclipse/jdt/ui/tests/actions/StructureSelectionActionTests.java
+++ org.eclipse.jdt.ui.tests.refactoring/test cases/org/eclipse/jdt/ui/tests/actions/StructureSelectionActionTests.java
@@ -232,6 +232,11 @@ import org.eclipse.jdt.ui.tests.refactoring.infra.TextRangeUtil;public class S
 		helperSelectPrevious(5, 30, 7, 10);
 	}
 	
+	public void test24() throws Exception{
+		//regression test for bug#11424
+		helperSelectPrevious(3, 13, 5, 6);
+	}
+	
 	public void testZeroLength0() throws Exception{
 		//printTestDisabledMessage("");
 		helperZeroLength(4, 20);
