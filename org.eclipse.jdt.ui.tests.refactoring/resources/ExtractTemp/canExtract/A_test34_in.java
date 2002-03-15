0f2611300d5802103586c25aea16abf9397fef67
Adam Kiezun <akiezun>
2002-03-15 17:44:43 +0000

    new test

---
+++ org.eclipse.jdt.ui.tests.refactoring/resources/ExtractTemp/canExtract/A_test34_in.java
@@ -0,0 +1,6 @@
+package p;
+class A {
+	void m(){
+		Object a= new java.util.ArrayList(){};
+	}
+} 
---
+++ org.eclipse.jdt.ui.tests.refactoring/resources/ExtractTemp/canExtract/A_test34_out.java
@@ -0,0 +1,7 @@
+package p;
+class A {
+	void m(){
+		java.util.ArrayList temp= new java.util.ArrayList(){};
+		Object a= temp;
+	}
+} 
