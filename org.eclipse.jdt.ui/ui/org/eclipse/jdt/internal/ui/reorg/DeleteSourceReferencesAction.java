36626399e19fb23455e76ff3264e2a4661e57f5c
Adam Kiezun <akiezun>
2002-04-02 18:29:32 +0000

    workaround for 8405 Delete field action broken for multiple declarations

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/reorg/DeleteSourceReferencesAction.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/reorg/DeleteSourceReferencesAction.java
@@ -27,6 +27,7 @@ import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.JavaModelException;
 
 import org.eclipse.jdt.internal.corext.codemanipulation.GetterSetterUtil;
+import org.eclipse.jdt.internal.corext.refactoring.Assert;
 import org.eclipse.jdt.internal.corext.refactoring.reorg.DeleteSourceReferenceEdit;
 import org.eclipse.jdt.internal.corext.refactoring.reorg.SourceReferenceUtil;
 import org.eclipse.jdt.internal.corext.refactoring.util.WorkingCopyUtil;
@@ -68,15 +69,18 @@ public class DeleteSourceReferencesAction extends SourceReferenceAction{
 	private static void deleteAll(Map mapping, IFile file) throws CoreException {
 		List l= (List)mapping.get(file);
 		ISourceReference[] refs= (ISourceReference[]) l.toArray(new ISourceReference[l.size()]);
-		delete(file, refs);
+
+		delete(file, getNonFields(refs));
+		delete(getFields(refs));
 	}
 
-	private static void delete(IFile file, ISourceReference[] elems) throws CoreException{
+	private static void delete(IFile file, ISourceReference[] nonFields) throws CoreException{
 		TextBuffer tb= TextBuffer.acquire(file);
 		try{
 			TextBufferEditor tbe= new TextBufferEditor(tb);
-			for (int i= 0; i < elems.length; i++) {
-				tbe.add(createDeleteEdit(elems[i]));
+			for (int i= 0; i < nonFields.length; i++) {
+				Assert.isTrue(! (nonFields[i] instanceof IField));
+				tbe.add(createDeleteEdit(nonFields[i]));
 			}
 			if (! tbe.canPerformEdits())
 				return; ///XXX can i assert here?
@@ -86,6 +90,12 @@ public class DeleteSourceReferencesAction extends SourceReferenceAction{
 			if (tb != null)
 				TextBuffer.release(tb);
 		}	
+	}
+	
+	private static void delete(IField[] fields) throws JavaModelException{
+		for (int i= 0; i < fields.length; i++) {
+			fields[i].delete(false, new NullProgressMonitor());
+		}
 	}
 			
 	private static TextEdit createDeleteEdit(ISourceReference ref) throws JavaModelException{
@@ -100,7 +110,7 @@ public class DeleteSourceReferencesAction extends SourceReferenceAction{
 		if (cusToDelete.length == 0)
 			return cusToDelete;
 			
-		if (! isOkToDeleteCus(cusToDelete))
+		if (! confirmCusDelete(cusToDelete))
 			return cusToDelete;
 		
 		List notDeletedCus= new ArrayList();
@@ -123,15 +133,6 @@ public class DeleteSourceReferencesAction extends SourceReferenceAction{
 		return false;	
 	}
 
-	//made protected for ui-less testing
-	protected boolean isOkToDeleteCus(ICompilationUnit[] cusToDelete) {
-		String message;
-		if (cusToDelete.length == 1) 
-			message= "After the delete operation the compilation unit \'" + cusToDelete[0].getElementName() + "\' contains no types. \nOK to delete it as well?";
-		else
-			message= "After the delete operation " + cusToDelete.length + " compilation units contain no types. \nOK to delete them as well?";	
-		return MessageDialog.openQuestion(JavaPlugin.getActiveWorkbenchShell(), "Delete", message);
-	}
 
 	private static boolean isOkToDeleteReadOnly(ICompilationUnit cu){
 		String message= "Compilation unit \'" + cu.getElementName() + "\' is read-only. Do you still want to delete it?"; 
@@ -165,14 +166,6 @@ public class DeleteSourceReferencesAction extends SourceReferenceAction{
 		return true;
 	}	
 	
-	//made protected for ui-less testing
-	protected boolean confirmDelete() {
-		String title= ReorgMessages.getString("deleteAction.confirm.title"); //$NON-NLS-1$
-		String label= ReorgMessages.getString("deleteAction.confirm.message"); //$NON-NLS-1$
-		Shell parent= JavaPlugin.getActiveWorkbenchShell();
-		return MessageDialog.openQuestion(parent, title, label);
-	}
-	
 	//overridden to add getters/setters
 	protected ISourceReference[] getElementsToProcess() {
 		ISourceReference[] elements= super.getElementsToProcess();
@@ -194,6 +187,15 @@ public class DeleteSourceReferencesAction extends SourceReferenceAction{
 		return (ISourceReference[]) newElementSet.toArray(new ISourceReference[newElementSet.size()]);
 	}
 
+	private static ISourceReference[] getNonFields(ISourceReference[] elements){
+		List nonFields= new ArrayList();
+		for (int i= 0; i < elements.length; i++) {
+			if (! (elements[i] instanceof IField))
+				nonFields.add(elements[i]);
+		}
+		return (ISourceReference[]) nonFields.toArray(new ISourceReference[nonFields.size()]);
+	}
+	
 	private static IField[] getFields(ISourceReference[] elements){
 		List fields= new ArrayList();
 		for (int i= 0; i < elements.length; i++) {
@@ -222,6 +224,24 @@ public class DeleteSourceReferencesAction extends SourceReferenceAction{
 			ExceptionHandler.handle(e, JavaPlugin.getActiveWorkbenchShell(), "Delete elements", "Unexpected exception. Please see log for details.");
 			return new IMethod[0];
 		}
+	}
+
+	//made protected for ui-less testing
+	protected boolean confirmDelete() {
+		String title= ReorgMessages.getString("deleteAction.confirm.title"); //$NON-NLS-1$
+		String label= ReorgMessages.getString("deleteAction.confirm.message"); //$NON-NLS-1$
+		Shell parent= JavaPlugin.getActiveWorkbenchShell();
+		return MessageDialog.openQuestion(parent, title, label);
+	}
+
+	//made protected for ui-less testing
+	protected  boolean confirmCusDelete(ICompilationUnit[] cusToDelete) {
+		String message;
+		if (cusToDelete.length == 1) 
+			message= "After the delete operation the compilation unit \'" + cusToDelete[0].getElementName() + "\' contains no types. \nOK to delete it as well?";
+		else
+			message= "After the delete operation " + cusToDelete.length + " compilation units contain no types. \nOK to delete them as well?";	
+		return MessageDialog.openQuestion(JavaPlugin.getActiveWorkbenchShell(), "Delete", message);
 	}
 	
 	//made protected for ui-less testing
