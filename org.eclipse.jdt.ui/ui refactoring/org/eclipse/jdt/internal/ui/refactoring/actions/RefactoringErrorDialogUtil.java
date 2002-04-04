4764c808435d6f9be64bda0ae13bc78df0d7b9c4
Adam Kiezun <akiezun>
2002-04-04 11:18:03 +0000

    renamed from dialog

---
+++ org.eclipse.jdt.ui/ui refactoring/org/eclipse/jdt/internal/ui/refactoring/actions/RefactoringErrorDialogUtil.java
@@ -0,0 +1,38 @@
+/*
+ * (c) Copyright IBM Corp. 2000, 2001.
+ * All Rights Reserved.
+ */
+package org.eclipse.jdt.internal.ui.refactoring.actions;
+
+import org.eclipse.jface.dialogs.MessageDialog;
+
+import org.eclipse.jdt.internal.corext.refactoring.base.RefactoringStatus;
+import org.eclipse.jdt.internal.ui.JavaPlugin;
+import org.eclipse.jdt.internal.ui.refactoring.RefactoringStatusContentProvider;
+import org.eclipse.jdt.internal.ui.refactoring.RefactoringStatusEntryLabelProvider;
+
+public class RefactoringErrorDialogUtil {
+	
+	private RefactoringErrorDialogUtil() {
+		// no instance.
+	}
+	
+	public static void open(String dialogTitle, RefactoringStatus status) {
+		if (status.getEntries().size() == 1) {
+			String message= "This operation cannot be performed:\n\n" + status.getFirstMessage(RefactoringStatus.FATAL);
+			MessageDialog.openInformation(JavaPlugin.getActiveWorkbenchShell(), dialogTitle, message);
+		} else {
+			openListDialog(dialogTitle, status);	
+		}
+	}
+	
+	private static void openListDialog(String dialogTitle, RefactoringStatus status) {
+		ListDialog dialog= new ListDialog(JavaPlugin.getActiveWorkbenchShell());
+		dialog.setInput(status);
+		dialog.setTitle(dialogTitle);
+		dialog.setMessage("This operation cannot be performed:");
+		dialog.setContentProvider(new RefactoringStatusContentProvider());
+		dialog.setLabelProvider(new RefactoringStatusEntryLabelProvider());
+		dialog.open();	
+	}
+}
