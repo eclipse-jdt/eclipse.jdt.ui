5c4a6f80f6eb07a05348dcdc6b0b688eace5738f
Kai Maetzel <kmaetzel>
2002-04-09 15:34:32 +0000

    fun

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/ui/actions/ShowInPackageViewAction.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/ui/actions/ShowInPackageViewAction.java
@@ -27,7 +27,7 @@ import org.eclipse.jdt.internal.ui.actions.ActionUtil;
 import org.eclipse.jdt.internal.ui.actions.OpenActionUtil;
 import org.eclipse.jdt.internal.ui.actions.SelectionConverter;
 import org.eclipse.jdt.internal.ui.javaeditor.JavaEditor;
-import org.eclipse.jdt.internal.ui.packageview.PackageExplorerPart;
+import org.eclipse.jdt.internal.ui.packageview.PackageExplorerPart;
 /**
  * This action reveals the currently selected Java element in the packages
  * view. The Java element can be represeented by either
