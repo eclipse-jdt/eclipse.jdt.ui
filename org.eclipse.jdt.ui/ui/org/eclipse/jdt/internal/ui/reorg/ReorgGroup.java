dcdc2c8f4f42e91406660e995a094aaffd1f718b
Adam Kiezun <akiezun>
2002-04-15 16:45:27 +0000

    12611

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/reorg/ReorgGroup.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/reorg/ReorgGroup.java
@@ -4,11 +4,17 @@
  */
 package org.eclipse.jdt.internal.ui.reorg;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.viewers.ISelectionProvider;
 
 import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.IWorkbenchActionConstants;
+
+import org.eclipse.jdt.core.ISourceReference;
+import org.eclipse.jdt.core.JavaModelException;
 
 import org.eclipse.jdt.ui.IContextMenuConstants;
 
@@ -19,31 +25,41 @@ import org.eclipse.jdt.internal.ui.refactoring.actions.IRefactoringAction;
 
 public class ReorgGroup extends ContextMenuGroup {
 	private static final String GROUP_NAME= IContextMenuConstants.GROUP_REORGANIZE;
-	private IRefactoringAction[] fActions;
+	
+	private IRefactoringAction[] fBasicActions; //always added - just grayed out if disabled
+	private IRefactoringAction[] fDynamicActions; //added only if enabled
 		
 	public void fill(IMenuManager manager, GroupContext context) {
 		createActions(context.getSelectionProvider());
 		
-		for (int i= 0; i < fActions.length; i++) {
-			fActions[i].update();
-			manager.appendToGroup(GROUP_NAME, fActions[i]);
+		for (int i= 0; i < fBasicActions.length; i++) {
+			fBasicActions[i].update();
+			manager.appendToGroup(GROUP_NAME, fBasicActions[i]);
+		}
+		
+		for (int i= 0; i < fDynamicActions.length; i++) {
+			fDynamicActions[i].update();
+			if (fDynamicActions[i].isEnabled())
+				manager.appendToGroup(GROUP_NAME, fDynamicActions[i]);
 		}
 	}
 	
 	private void createActions(ISelectionProvider p) {
 		StructuredSelectionProvider provider= StructuredSelectionProvider.createFrom(p);
-		if (fActions != null)
+		if (fBasicActions != null)
 			return;
 			
-		fActions= new IRefactoringAction[] {	
+		fBasicActions= new IRefactoringAction[] {	
 			createCutAction(p),
 			createCopyAction(p),
 			createPasteAction(p),
 			createDeleteAction(p),
 			//new DuplicateSourceReferenceAction(provider, p),
+		};
+		fDynamicActions= new IRefactoringAction[]{			
 			new JdtMoveAction(provider),
 			new RenameAction(p)
-		};		
+		};
 	}	
 	
 	public static void addGlobalReorgActions(IActionBars actionBars, ISelectionProvider provider) {
@@ -74,4 +90,19 @@ public class ReorgGroup extends ContextMenuGroup {
 		return new DualReorgAction(p, ReorgMessages.getString("ReorgGroup.delete"), ReorgMessages.getString("deleteAction.description"), deleteResources, deleteSourceReferences); //$NON-NLS-1$ //$NON-NLS-2$
 	}
 	
+	public static IRefactoringAction createPasteAction(final ISourceReference[] elements, Object target){
+		return new PasteSourceReferencesFromClipboardAction(new SimpleSelectionProvider(new Object[]{target})){
+			protected TypedSource[] getContentsToPaste(){
+				List result= new ArrayList(elements.length);
+				for(int i= 0; i < elements.length; i++){
+					try {
+						result.add(new TypedSource(elements[i]));
+					} catch(JavaModelException e) {
+						//ignore
+					}
+				}
+				return (TypedSource[])result.toArray(new TypedSource[result.size()]);
+			}
+		};
+	}
 }
