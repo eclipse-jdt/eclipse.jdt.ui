5cfdae282e13829661b072cd1bd08069a157c62a
Dani Megert <dmegert>
2002-04-16 14:15:43 +0000

    Fixed bug 13877: List in Java Browsing perspective don't update CVS decorators

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/viewsupport/ProblemTableViewer.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/viewsupport/ProblemTableViewer.java
@@ -2,14 +2,21 @@ package org.eclipse.jdt.internal.ui.viewsupport;
 
 import java.util.Set;
 
+import org.eclipse.core.resources.IResource;
+
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Item;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.Widget;
 
+import org.eclipse.jface.viewers.IBaseLabelProvider;
 import org.eclipse.jface.viewers.ILabelProvider;
+import org.eclipse.jface.viewers.LabelProviderChangedEvent;
 import org.eclipse.jface.viewers.TableViewer;
+
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.JavaCore;
 
 /**
  * Extends a  TableViewer to allow more performance when showing error ticks.
@@ -85,7 +92,36 @@ public class ProblemTableViewer extends TableViewer implements IProblemChangedLi
 		}		
 		super.unmapElement(element, item);
 	}
-
+	
+	/*
+	 * @see ContentViewer#handleLabelProviderChanged(LabelProviderChangedEvent)
+	 */
+	protected void handleLabelProviderChanged(LabelProviderChangedEvent event) {
+		Object[] source= event.getElements();
+		if (source == null) {
+			super.handleLabelProviderChanged(event);
+			return;
+		}
+		
+		// map the event to the Java elements if possible
+		// this does not handle the ambiguity of default packages
+		Object[] mapped= new Object[source.length];
+		for (int i= 0; i < source.length; i++) {
+			Object o= source[i];
+			// needs to handle the case of:
+			// default package
+			// package fragment root on project
+			if (o instanceof IResource) {
+				IResource r= (IResource)o;
+				IJavaElement element= JavaCore.create(r);
+				if (element != null) 
+					mapped[i]= element;
+				else
+					mapped[i]= o;
+			} else {
+				mapped[i]= o;
+			}
+		}
+		super.handleLabelProviderChanged(new LabelProviderChangedEvent((IBaseLabelProvider)event.getSource(), mapped));	
+	}
 }
-
-
