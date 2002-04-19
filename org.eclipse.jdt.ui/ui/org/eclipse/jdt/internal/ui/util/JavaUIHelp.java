07769ae73b5afa00c9ed595d15265cfd40aa6ea9
Martin Aeschlimann <maeschli>
2002-04-19 09:32:42 +0000

    help support for editor

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/util/JavaUIHelp.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/util/JavaUIHelp.java
@@ -13,11 +13,13 @@ import java.util.List;
 
 import org.eclipse.core.runtime.CoreException;
 
+import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.events.HelpEvent;
 import org.eclipse.swt.events.HelpListener;
 
 import org.eclipse.jface.util.Assert;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.StructuredViewer;
 
 import org.eclipse.ui.help.WorkbenchHelp;
@@ -36,6 +38,8 @@ import org.eclipse.jdt.internal.corext.javadoc.SingleCharReader;
 import org.eclipse.jdt.internal.corext.util.JavaModelUtil;
 import org.eclipse.jdt.internal.ui.JavaPlugin;
 import org.eclipse.jdt.internal.ui.JavaUIMessages;
+import org.eclipse.jdt.internal.ui.actions.SelectionConverter;
+import org.eclipse.jdt.internal.ui.javaeditor.JavaEditor;
 import org.eclipse.jdt.internal.ui.text.HTML2TextReader;
 import org.eclipse.jdt.internal.ui.viewsupport.JavaElementLabels;
 
@@ -46,14 +50,25 @@ public class JavaUIHelp {
 		viewer.getControl().addHelpListener(listener);
 	}
 
+	public static void setHelp(JavaEditor editor, StyledText text, String contextId) {
+		JavaUIHelpListener listener= new JavaUIHelpListener(editor, contextId);
+		text.addHelpListener(listener);
+	}
+
 	private static class JavaUIHelpListener implements HelpListener {
 
 		private StructuredViewer fViewer;
 		private String fContextId;
+		private JavaEditor fEditor;
 
 		public JavaUIHelpListener(StructuredViewer viewer, String contextId) {
 			fViewer= viewer;
 			fContextId= contextId;
+		}
+
+		public JavaUIHelpListener(JavaEditor editor, String contextId) {
+			fContextId= contextId;
+			fEditor= editor;
 		}
 
 		/*
@@ -61,78 +76,91 @@ public class JavaUIHelp {
 		* 
 		*/
 		public void helpRequested(HelpEvent e) {
-
-			ISelection selection= fViewer.getSelection();
-			Object selected= SelectionUtil.getSingleElement(selection);
-
 			IHelp help= WorkbenchHelp.getHelpSupport();
-
-			IContext context= help.getContext(fContextId);
-			if (context == null) {
-				return;
-			}
-
-			if (selected instanceof IJavaElement) {
-				context= new JavaUIContext(context, (IJavaElement) selected);
-			}
-
-			WorkbenchHelp.displayHelp(context);
-		}
-
-	}
-
-	private static class JavaUIContext implements IContext {
-
-		private IJavaElement fElement;
-		private IHelpResource[] fHelpResources;
-		private IContext fContext;
-		private String fText;
-
-		public JavaUIContext(IContext context, IJavaElement element) {
-			Assert.isNotNull(element);
-			Assert.isNotNull(context);
-
-			fElement= element;
-			List helpResources= new ArrayList();
-			fContext= context;
-
-			IHelpResource[] resources= context.getRelatedTopics();
-			if (resources != null) {
-				for (int j= 0; j < resources.length; j++) {
-					helpResources.add(resources[j]);
-				}
+			if (help == null) {
+				return; // no help support installed
 			}
 
 			try {
-				URL url= JavaDocLocations.getJavaDocLocation(fElement, true);
+				Object[] selected= null;
+				if (fViewer != null) {
+					ISelection selection= fViewer.getSelection();
+					if (selection instanceof IStructuredSelection) {
+						selected= ((IStructuredSelection) selection).toArray();
+					}
+				} else if (fEditor != null) {
+					selected= SelectionConverter.codeResolve(fEditor);
+				}
 
-				if (url == null || doesNotExist(url)) {
+				IContext context= help.getContext(fContextId);
+				if (selected != null && selected.length > 0) {
+					context= new JavaUIContext(context, selected);
+				}
 
-					if (fElement.getElementType() != IJavaElement.JAVA_PROJECT) {
-						//must insure that what is sent here is lower in the hiarchy then IJavaProject
-						IPackageFragmentRoot root= JavaModelUtil.getPackageFragmentRoot(fElement);
-						if (root != null)
-							if (root.getKind() == IPackageFragmentRoot.K_BINARY) {
-								url= JavaDocLocations.getJavaDocLocation(root, true);
-								element= root;
-							} else if (root.getKind() == IPackageFragmentRoot.K_SOURCE) {
-								url= JavaDocLocations.getJavaDocLocation(root.getJavaProject(), true);
-								element= root.getJavaProject();
-							}
+				WorkbenchHelp.displayHelp(context);
+			} catch (CoreException ex) {
+				JavaPlugin.log(ex);
+			}
+		}
+	}
+	
+	private static class JavaUIContext implements IContext {
+
+		private IHelpResource[] fHelpResources;
+		private String fText;
+
+		public JavaUIContext(IContext context, Object[] elements) throws CoreException {
+			Assert.isNotNull(elements);
+
+			List helpResources= new ArrayList();
+
+			if (context != null) {
+				IHelpResource[] resources= context.getRelatedTopics();
+				if (resources != null) {
+					for (int j= 0; j < resources.length; j++) {
+						helpResources.add(resources[j]);
 					}
 				}
-
-				if (url != null) {
-					String extForm= url.toExternalForm();
-
-					IHelpResource javaResource= new JavaUIHelpResource(element, extForm);
-					helpResources.add(javaResource);
-				}
-			} catch (CoreException e) {
-				JavaPlugin.log(e);
 			}
 
+			for (int i= 0; i < elements.length; i++) {
+				if (elements[i] instanceof IJavaElement) {
+					IJavaElement element= (IJavaElement) elements[i];
+					if (fText == null) {
+						fText= retrieveText(element);
+					} else {
+						fText= ""; // no doc on multiple selection
+					}					
+					
+					URL url= JavaDocLocations.getJavaDocLocation(element, true);
+					if (url == null || doesNotExist(url)) {
+						IPackageFragmentRoot root= JavaModelUtil.getPackageFragmentRoot(element);
+						if (root != null) {
+							url= JavaDocLocations.getJavadocBaseLocation(element);
+							if (root.getKind() == IPackageFragmentRoot.K_SOURCE) {
+								element= element.getJavaProject();
+							} else {
+								element= root;
+							}
+							url= JavaDocLocations.getJavaDocLocation(element, false);
+						}
+					}
+					if (url != null) {
+						IHelpResource javaResource= new JavaUIHelpResource(element, url.toExternalForm());
+						helpResources.add(javaResource);
+					}
+				}
+			}
 			fHelpResources= (IHelpResource[]) helpResources.toArray(new IHelpResource[helpResources.size()]);
+			if (fText == null && fText.length() == 0) {
+				if (context != null) {
+					fText= context.getText();
+				}
+			}
+			if (fText != null && fText.length() == 0) {
+				fText= null; // see 14207 
+			}
+			
 		}
 
 		private boolean doesNotExist(URL url) {
@@ -143,37 +171,31 @@ public class JavaUIHelp {
 			return false;
 		}
 
-		public IHelpResource[] getRelatedTopics() {
-			return fHelpResources;
-		}
-
-		public String getText() {
-
-			if (fElement instanceof IMember) {
-
+		private String retrieveText(IJavaElement elem) throws JavaModelException {
+			if (elem instanceof IMember) {
 				try {
-					SingleCharReader reader= JavaDocAccess.getJavaDoc((IMember) fElement, true);
+					SingleCharReader reader= JavaDocAccess.getJavaDoc((IMember) elem, true);
 					if (reader != null) {
-
 						HTML2TextReader htmlReader= new HTML2TextReader(reader, null);
 						String str= htmlReader.getString();
 
 						BreakIterator breakIterator= BreakIterator.getSentenceInstance();
 						breakIterator.setText(str);
 						return str.substring(0, breakIterator.next());
-
-					} else
-						return fContext.getText();
-				} catch (JavaModelException e) {
-					JavaPlugin.log(e);
+					}
 				} catch (IOException e) {
-					JavaPlugin.log(e);
+					JavaPlugin.log(e); // ignore
 				}
-
 			}
+			return "";
+		}
 
-			return fContext.getText();
+		public IHelpResource[] getRelatedTopics() {
+			return fHelpResources;
+		}
 
+		public String getText() {
+			return fText;
 		}
 
 	}
