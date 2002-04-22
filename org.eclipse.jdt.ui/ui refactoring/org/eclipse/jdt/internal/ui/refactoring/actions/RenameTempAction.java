e6ab3c34034fea0b3781e12d7d5044e828c6aa23
Adam Kiezun <akiezun>
2002-04-22 13:00:19 +0000

    rework to the new action story

--- org.eclipse.jdt.ui/ui refactoring/org/eclipse/jdt/internal/ui/refactoring/actions/RenameTempAction.java
+++ org.eclipse.jdt.ui/ui refactoring/org/eclipse/jdt/internal/ui/refactoring/actions/RenameTempAction.java
@@ -1,7 +1,3 @@
-/*
- * (c) Copyright IBM Corp. 2000, 2001.
- * All Rights Reserved.
- */
 package org.eclipse.jdt.internal.ui.refactoring.actions;
 
 import org.eclipse.core.runtime.NullProgressMonitor;
@@ -12,29 +8,29 @@ import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.JavaModelException;
 
+import org.eclipse.jdt.ui.actions.SelectionDispatchAction;
+import org.eclipse.jdt.ui.actions.UnifiedSite;
+
 import org.eclipse.jdt.internal.corext.refactoring.base.Refactoring;
 import org.eclipse.jdt.internal.corext.refactoring.rename.RenameTempRefactoring;
 import org.eclipse.jdt.internal.ui.IJavaHelpContextIds;
+import org.eclipse.jdt.internal.ui.actions.SelectionConverter;
+import org.eclipse.jdt.internal.ui.javaeditor.CompilationUnitEditor;
 import org.eclipse.jdt.internal.ui.refactoring.RefactoringMessages;
 import org.eclipse.jdt.internal.ui.refactoring.RefactoringWizard;
 import org.eclipse.jdt.internal.ui.refactoring.RenameRefactoringWizard;
 
-class RenameTempAction extends TextSelectionBasedRefactoringAction{
+class RenameTempAction extends NewTextRefactoringAction {
 
-	public RenameTempAction() {
-		super(RefactoringMessages.getString("RenameTempAction.rename_Local_Variable")); //$NON-NLS-1$
+	public RenameTempAction(CompilationUnitEditor editor) {
+		super(editor);
+		setText(RefactoringMessages.getString("RenameTempAction.rename_Local_Variable"));//$NON-NLS-1$
 	}
 	
-	/* (non-Javadoc)
-	 * Method declated in TextSelectionBasedRefactoringAction
-	 */	
 	protected Refactoring createRefactoring(ICompilationUnit cunit, ITextSelection selection) {
 		return new RenameTempRefactoring(cunit, selection.getOffset(), selection.getLength());
 	}
 	
-	/* (non-Javadoc)
-	 * Method declated in TextSelectionBasedRefactoringAction
-	 */
 	protected RefactoringWizard createWizard(Refactoring refactoring) {
 		String message= RefactoringMessages.getString("RenameTempAction.choose_new_name"); //$NON-NLS-1$
 		String wizardPageHelp= IJavaHelpContextIds.RENAME_TEMP_WIZARD_PAGE; 
@@ -42,25 +38,18 @@ class RenameTempAction extends TextSelectionBasedRefactoringAction{
 		return new RenameRefactoringWizard((RenameTempRefactoring)refactoring, getText(), message, wizardPageHelp, errorPageHelp);
 	}
 	
-	/* (non-Javadoc)
-	 * Method declated in TextSelectionBasedRefactoringAction
-	 */	
 	protected String getMessageDialogTitle() {
 		return RefactoringMessages.getString("RenameTempAction.rename_Local_Variable"); //$NON-NLS-1$
 	}
 	
-	/* (non-Javadoc)
-	 * @see TextSelectionAction#canOperateOnCurrentSelection(ISelection)
-	 */
-	protected boolean canOperateOnCurrentSelection(ISelection selection) {
-		if (!(selection instanceof ITextSelection))
+	protected boolean canEnableOn(ITextSelection selection) {
+		return getCompilationUnit() != null;
+	}	
+	
+	protected boolean canRun(ITextSelection selection) {
+		if (! super.canRun(selection))
 			return false;
-		
-		if (getCompilationUnit() == null)
-			return false;
-			
-		//must check it here - see bug 12590
-		//if it's not a local variable - we will try to resolve the symbol as a IJavaElement in rename action
+
 		Refactoring renameTempRefactoring= createRefactoring(getCompilationUnit(), (ITextSelection)selection);
 		try {
 			return (renameTempRefactoring.checkActivation(new NullProgressMonitor()).isOK());
@@ -69,6 +58,5 @@ class RenameTempAction extends TextSelectionBasedRefactoringAction{
 			return false;
 		}
 	}
-	
-}
 
+}
