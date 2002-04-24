01550b4a550d72ac47b052830e7871fba426b7ba
Adam Kiezun <akiezun>
2002-04-24 08:35:29 +0000

    fix for 14665

--- org.eclipse.jdt.ui/ui refactoring/org/eclipse/jdt/internal/ui/refactoring/actions/RenameJavaElementAction.java
+++ org.eclipse.jdt.ui/ui refactoring/org/eclipse/jdt/internal/ui/refactoring/actions/RenameJavaElementAction.java
@@ -34,10 +34,10 @@ class RenameJavaElementAction extends SelectionDispatchAction {
 	public void run(IStructuredSelection selection) {
 		if (!canOperateOn(selection))
 			return;
-		run((IJavaElement)selection.getFirstElement());	
+		run(selection.getFirstElement());	
 	}
 
-	private void run(IJavaElement element){
+	private void run(Object element){
 		IRefactoringRenameSupport refactoringSupport= RefactoringSupportFactory.createRenameSupport(element);
 		if (! canRename(refactoringSupport, element))
 			return;
