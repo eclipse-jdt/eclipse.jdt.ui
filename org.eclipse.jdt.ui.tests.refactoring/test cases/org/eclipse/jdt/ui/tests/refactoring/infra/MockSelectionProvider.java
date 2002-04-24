3fcbef0634c2b23971bd26dc7e321b13f1d527d0
Adam Kiezun <akiezun>
2002-04-24 10:29:45 +0000

    *** empty log message ***

--- org.eclipse.jdt.ui.tests.refactoring/test cases/org/eclipse/jdt/ui/tests/refactoring/infra/MockSelectionProvider.java
+++ org.eclipse.jdt.ui.tests.refactoring/test cases/org/eclipse/jdt/ui/tests/refactoring/infra/MockSelectionProvider.java
@@ -1,29 +1,39 @@
 package org.eclipse.jdt.ui.tests.refactoring.infra;
 
+import java.util.Collection;
+
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.ISelectionProvider;
 import org.eclipse.jface.viewers.StructuredSelection;
 
+import org.eclipse.jdt.internal.corext.refactoring.Assert;
+
 public class MockSelectionProvider implements ISelectionProvider {
-		private ISelection fSelection;
-		public MockSelectionProvider(Object[] elements){
-			fSelection= new StructuredSelection(elements);
-		}
-		
-		public void addSelectionChangedListener(ISelectionChangedListener listener) {
-		}
+	private ISelection fSelection;
 
-		public ISelection getSelection() {
-			return fSelection;
-		}
-
-		public void removeSelectionChangedListener(ISelectionChangedListener listener) {
-		}
-
-		public void setSelection(ISelection selection) {
-		}
-
+	public MockSelectionProvider(Collection collection) {
+		this(collection.toArray());
 	}
+	
+	public MockSelectionProvider(Object[] elements) {
+		Assert.isNotNull(elements);
+		fSelection= new StructuredSelection(elements);
+	}
+
+	public void addSelectionChangedListener(ISelectionChangedListener listener) {
+	}
+
+	public ISelection getSelection() {
+		return fSelection;
+	}
+
+	public void removeSelectionChangedListener(ISelectionChangedListener listener) {
+	}
+
+	public void setSelection(ISelection selection) {
+	}
+
+}
 
 
