6ad9cec6cdc4784b09d334c154d16c0ecfe928cf
Adam Kiezun <akiezun>
2002-05-03 10:19:02 +0000

    inheriting from package drop adapter, linking on empty target == focus on adding subs moved to superclass

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/typehierarchy/TypeHierarchyTransferDropAdapter.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/typehierarchy/TypeHierarchyTransferDropAdapter.java
@@ -1,78 +1,60 @@
-/*
- * (c) Copyright IBM Corp. 2000, 2001.
- * All Rights Reserved.
- */
+/*******************************************************************************
+ * Copyright (c) 2002 International Business Machines Corp. and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v0.5 
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v05.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ ******************************************************************************/
 package org.eclipse.jdt.internal.ui.typehierarchy;
-import org.eclipse.swt.dnd.DND;
+
+import org.eclipse.swt.dnd.DND;
 import org.eclipse.swt.dnd.DropTargetEvent;
-import org.eclipse.swt.dnd.Transfer;
 
 import org.eclipse.jface.viewers.AbstractTreeViewer;
 import org.eclipse.jface.viewers.ISelection;
 
 import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IMember;
-import org.eclipse.jdt.core.IType;
 
-import org.eclipse.jdt.internal.ui.actions.AddMethodStubAction;
-import org.eclipse.jdt.internal.ui.dnd.JdtViewerDropAdapter;
-import org.eclipse.jdt.internal.ui.dnd.LocalSelectionTransfer;
-import org.eclipse.jdt.internal.ui.dnd.TransferDropTargetListener;
+import org.eclipse.jdt.internal.ui.packageview.SelectionTransferDropAdapter;
 import org.eclipse.jdt.internal.ui.util.OpenTypeHierarchyUtil;
 import org.eclipse.jdt.internal.ui.util.SelectionUtil;
-
-public class TypeHierarchyTransferDropAdapter extends JdtViewerDropAdapter implements TransferDropTargetListener {
-	private AddMethodStubAction fAddMethodStubAction;
+
+public class TypeHierarchyTransferDropAdapter extends SelectionTransferDropAdapter {
+
 	private TypeHierarchyViewPart fTypeHierarchyViewPart;
+
 	public TypeHierarchyTransferDropAdapter(TypeHierarchyViewPart viewPart, AbstractTreeViewer viewer) {
-		super(viewer, DND.FEEDBACK_EXPAND | DND.FEEDBACK_SCROLL);
-		
-		fAddMethodStubAction= new AddMethodStubAction();
+		super(viewer);
 		fTypeHierarchyViewPart= viewPart;
 	}
-	//---- TransferDropTargetListener interface ---------------------------------------
-	
-	public Transfer getTransfer() {
-		return LocalSelectionTransfer.getInstance();
-	}
-	//---- Actual DND -----------------------------------------------------------------
-		
+
 	public void validateDrop(Object target, DropTargetEvent event, int operation) {
-		event.detail= DND.DROP_NONE;
-		
-		ISelection selection= LocalSelectionTransfer.getInstance().getSelection();
-		if (target == null) {
-			if (getInputElement(selection) != null) {
-				event.detail= DND.DROP_COPY;
-			}
-		} else if (target instanceof IType) {
-			if (AddMethodStubAction.canActionBeAdded((IType)target, selection)) {
-				if (operation == DND.DROP_NONE) {
-					operation= DND.DROP_COPY; // use copy as default operation
-				}
-				event.detail= operation;
-			}
-		}
-		return;	
-	}	
+		if (target != null){
+			super.validateDrop(target, event, operation);
+			return;
+		}	
+		if (operation == DND.DROP_LINK && getInputElement(getSelection()) != null) 
+			event.detail= DND.DROP_LINK;
+		else
+			event.detail= DND.DROP_NONE;
+	}
+
 	public void drop(Object target, DropTargetEvent event) {
-		ISelection selection= LocalSelectionTransfer.getInstance().getSelection();
-		
-		if (target == null) {
-			IJavaElement input= getInputElement(selection);
-			fTypeHierarchyViewPart.setInputElement(input);
-			if (input instanceof IMember) {
-				fTypeHierarchyViewPart.selectMember((IMember) input);
-			}
-		} else if (target instanceof IType) {
-			if (fAddMethodStubAction.init((IType)target, selection)) {
-				fAddMethodStubAction.run();
-			}
-		}
-		return;
+		if (target != null || event.detail != DND.DROP_LINK){
+			super.drop(target, event);
+			return;
+		}	
+		IJavaElement input= getInputElement(getSelection());
+		fTypeHierarchyViewPart.setInputElement(input);
+		if (input instanceof IMember) 
+			fTypeHierarchyViewPart.selectMember((IMember) input);
 	}
 	
-	private IJavaElement getInputElement(ISelection selection) {
+	private static IJavaElement getInputElement(ISelection selection) {
 		Object single= SelectionUtil.getSingleElement(selection);
 		if (single != null) {
 			IJavaElement[] candidates= OpenTypeHierarchyUtil.getCandidates(single);
@@ -82,6 +64,4 @@ public class TypeHierarchyTransferDropAdapter extends JdtViewerDropAdapter imple
 		}
 		return null;
 	}
-	
-	
 }
