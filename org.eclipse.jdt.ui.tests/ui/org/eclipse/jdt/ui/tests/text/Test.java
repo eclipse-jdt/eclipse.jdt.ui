dd332269056012cdd15902dee2834ad465d8092d
cknaus <cknaus>
2002-05-06 16:28:13 +0000

    test

---
+++ org.eclipse.jdt.ui.tests/ui/org/eclipse/jdt/ui/tests/text/Test.java
@@ -0,0 +1,18 @@
+package org.eclipse.jdt.ui.tests.text;
+
+/**
+ * @author cknaus
+ *
+ * To change this generated comment edit the template variable "typecomment":
+ * Window>Preferences>Java>Templates.
+ */
+public class Test {
+
+	/**
+	 * Constructor for Test.
+	 */
+	public Test() {
+		super();
+	}
+
+}
