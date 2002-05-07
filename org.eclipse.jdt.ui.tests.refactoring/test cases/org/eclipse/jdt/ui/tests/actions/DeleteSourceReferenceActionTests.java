f2413759ed61c84c1ea01ad5f0e645e20731d671
Adam Kiezun <akiezun>
2002-05-07 14:43:31 +0000

    enabled test for15412

--- org.eclipse.jdt.ui.tests.refactoring/test cases/org/eclipse/jdt/ui/tests/actions/DeleteSourceReferenceActionTests.java
+++ org.eclipse.jdt.ui.tests.refactoring/test cases/org/eclipse/jdt/ui/tests/actions/DeleteSourceReferenceActionTests.java
@@ -194,9 +194,9 @@ public class DeleteSourceReferenceActionTests extends RefactoringTest{
 	}
 
 	public void test16() throws Exception{
-		printTestDisabledMessage("test for bug#15412 deleting type removes too much from editor");		
-		if (true)
-			return;
+//		printTestDisabledMessage("test for bug#15412 deleting type removes too much from editor");		
+//		if (true)
+//			return;
 		
 		Object elem0= fCuA.getType("Test");
 		Object[] elems= new Object[]{elem0};
