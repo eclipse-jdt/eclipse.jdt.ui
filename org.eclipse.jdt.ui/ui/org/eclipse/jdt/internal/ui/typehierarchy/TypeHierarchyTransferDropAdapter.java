974533d95355095643cc2588c8d0eacef1e9855b
Adam Kiezun <akiezun>
2002-05-08 11:02:20 +0000

    simply sets input to TH now

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/typehierarchy/TypeHierarchyTransferDropAdapter.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/typehierarchy/TypeHierarchyTransferDropAdapter.java
@@ -12,46 +12,82 @@ package org.eclipse.jdt.internal.ui.typehierarchy;
 
 import org.eclipse.swt.dnd.DND;
 import org.eclipse.swt.dnd.DropTargetEvent;
+import org.eclipse.swt.dnd.Transfer;
 
-import org.eclipse.jface.viewers.AbstractTreeViewer;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.StructuredViewer;
 
 import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IMember;
 
-import org.eclipse.jdt.internal.ui.packageview.SelectionTransferDropAdapter;
+import org.eclipse.jdt.internal.ui.dnd.JdtViewerDropAdapter;
+import org.eclipse.jdt.internal.ui.dnd.LocalSelectionTransfer;
+import org.eclipse.jdt.internal.ui.dnd.TransferDropTargetListener;
 import org.eclipse.jdt.internal.ui.util.OpenTypeHierarchyUtil;
 import org.eclipse.jdt.internal.ui.util.SelectionUtil;
 
-public class TypeHierarchyTransferDropAdapter extends SelectionTransferDropAdapter {
+class TypeHierarchyTransferDropAdapter extends JdtViewerDropAdapter implements TransferDropTargetListener {
 
+	private ISelection fSelection;
 	private TypeHierarchyViewPart fTypeHierarchyViewPart;
-
-	public TypeHierarchyTransferDropAdapter(TypeHierarchyViewPart viewPart, AbstractTreeViewer viewer) {
-		super(viewer);
+	
+	public TypeHierarchyTransferDropAdapter(TypeHierarchyViewPart viewPart, StructuredViewer viewer) {
+		super(viewer,  DND.FEEDBACK_SCROLL | DND.FEEDBACK_EXPAND);
 		fTypeHierarchyViewPart= viewPart;
 	}
 
+	//---- TransferDropTargetListener interface ---------------------------------------
+	
+	public Transfer getTransfer() {
+		return LocalSelectionTransfer.getInstance();
+	}
+
+	//---- Actual DND -----------------------------------------------------------------
+	
+	public void dragEnter(DropTargetEvent event) {
+		clear();
+		super.dragEnter(event);
+	}
+	
+	public void dragLeave(DropTargetEvent event) {
+		clear();
+		super.dragLeave(event);
+	}
+	
+	private void clear() {
+		fSelection= null;
+	}
+	
 	public void validateDrop(Object target, DropTargetEvent event, int operation) {
-		if (target != null){
-			super.validateDrop(target, event, operation);
-			return;
+		event.detail= DND.DROP_NONE;	
+		
+		if (fSelection == null) {
+			ISelection s= LocalSelectionTransfer.getInstance().getSelection();
+			if (!(s instanceof IStructuredSelection))
+				return;
+			fSelection= s;	
 		}	
-		if (operation == DND.DROP_LINK && getInputElement(getSelection()) != null) 
-			event.detail= DND.DROP_LINK;
-		else
-			event.detail= DND.DROP_NONE;
+		
+		if (getInputElement(fSelection) != null) 
+			event.detail= DND.DROP_COPY;	
 	}
 
 	public void drop(Object target, DropTargetEvent event) {
-		if (target != null || event.detail != DND.DROP_LINK){
-			super.drop(target, event);
-			return;
-		}	
-		IJavaElement input= getInputElement(getSelection());
-		fTypeHierarchyViewPart.setInputElement(input);
-		if (input instanceof IMember) 
-			fTypeHierarchyViewPart.selectMember((IMember) input);
+		try{
+			if (event.detail == DND.DROP_NONE)
+				return;
+	
+			IJavaElement input= getInputElement(fSelection);
+			fTypeHierarchyViewPart.setInputElement(input);
+			if (input instanceof IMember) 
+				fTypeHierarchyViewPart.selectMember((IMember) input);
+		} finally{
+			// The drag source listener must not perform any operation
+			// since this drop adapter did the remove of the source even
+			// if we moved something.
+			event.detail= DND.DROP_NONE;
+		}
 	}
 	
 	private static IJavaElement getInputElement(ISelection selection) {
@@ -64,4 +100,5 @@ public class TypeHierarchyTransferDropAdapter extends SelectionTransferDropAdapt
 		}
 		return null;
 	}
+	
 }
