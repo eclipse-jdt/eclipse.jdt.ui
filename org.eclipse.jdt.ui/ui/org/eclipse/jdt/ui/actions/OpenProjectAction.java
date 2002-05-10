a397d8e30356789410329ff2a1299cf91e2dc4cc
Dirk Baeumer <dbaeumer>
2002-05-10 17:33:27 +0000

    [15586]

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/ui/actions/OpenProjectAction.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/ui/actions/OpenProjectAction.java
@@ -19,6 +19,8 @@ import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.ProgressMonitorDialog;
 import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.util.Assert;
+import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.Viewer;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResourceChangeEvent;
@@ -34,8 +36,11 @@ import org.eclipse.core.runtime.SubProgressMonitor;
 import org.eclipse.ui.IWorkbenchSite;
 import org.eclipse.ui.dialogs.ElementListSelectionDialog;
 
+import org.eclipse.jdt.core.JavaCore;
+
 import org.eclipse.jdt.internal.ui.JavaPlugin;
 import org.eclipse.jdt.internal.ui.actions.ActionMessages;
+import org.eclipse.jdt.internal.ui.dialogs.ListDialog;
 import org.eclipse.jdt.internal.ui.util.ExceptionHandler;
 
 import org.eclipse.jdt.ui.JavaElementLabelProvider;
@@ -62,49 +67,6 @@ public class OpenProjectAction extends Action implements IResourceChangeListener
 	}
 	
 	/*
-	 * @see IAction#run()
-	 */
-	public void run() {
-		ElementListSelectionDialog dialog= new ElementListSelectionDialog(fSite.getShell(), new JavaElementLabelProvider());
-		dialog.setTitle(ActionMessages.getString("OpenProjectAction.dialog.title")); //$NON-NLS-1$
-		dialog.setMessage(ActionMessages.getString("OpenProjectAction.dialog.message")); //$NON-NLS-1$
-		dialog.setElements(getClosedProjects());
-		dialog.setMultipleSelection(true);
-		int result= dialog.open();
-		if (result != Dialog.OK)
-			return;
-		final Object[] projects= dialog.getResult();
-		IRunnableWithProgress runnable= new IRunnableWithProgress() {
-			public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
-				monitor.beginTask("", projects.length); //$NON-NLS-1$
-				MultiStatus errorStatus= null;
-				for (int i = 0; i < projects.length; i++) {
-					IProject project= (IProject)projects[i];
-					try {
-						project.open(new SubProgressMonitor(monitor, 1));
-					} catch (CoreException e) {
-						if (errorStatus == null)
-							errorStatus = new MultiStatus(JavaPlugin.getPluginId(), IStatus.ERROR, ActionMessages.getString("OpenProjectAction.error.message"), e); //$NON-NLS-1$
-						errorStatus.merge(e.getStatus());
-					}
-				}
-				monitor.done();
-				if (errorStatus != null)
-					throw new InvocationTargetException(new CoreException(errorStatus));
-			}
-		};
-		ProgressMonitorDialog pd= new ProgressMonitorDialog(fSite.getShell());
-		try {
-			pd.run(true, true, runnable);	
-		} catch (InvocationTargetException e) {
-			ExceptionHandler.handle(e, fSite.getShell(), 
-				ActionMessages.getString("OpenProjectAction.dialog.title"), //$NON-NLS-1$
-				ActionMessages.getString("OpenProjectAction.error.message")); //$NON-NLS-1$
-		} catch (InterruptedException e) {
-		}
-	}
-	
-	/*
 	 * @see IResourceChangeListener#resourceChanged(IResourceChangeEvent)
 	 */
 	public void resourceChanged(IResourceChangeEvent event) {
@@ -121,6 +83,85 @@ public class OpenProjectAction extends Action implements IResourceChangeListener
 		}
 	}
 		
+	/*
+	 * @see IAction#run()
+	 */
+	public void run() {
+		ElementListSelectionDialog dialog= new ElementListSelectionDialog(fSite.getShell(), new JavaElementLabelProvider());
+		dialog.setTitle(ActionMessages.getString("OpenProjectAction.dialog.title")); //$NON-NLS-1$
+		dialog.setMessage(ActionMessages.getString("OpenProjectAction.dialog.message")); //$NON-NLS-1$
+		dialog.setElements(getClosedProjects());
+		dialog.setMultipleSelection(true);
+		int result= dialog.open();
+		if (result != Dialog.OK)
+			return;
+		final Object[] projects= dialog.getResult();
+		final List nonJavaProjects= new ArrayList(3);
+		IRunnableWithProgress runnable= createRunnable(projects, nonJavaProjects);
+		ProgressMonitorDialog pd= new ProgressMonitorDialog(fSite.getShell());
+		try {
+			pd.run(true, true, runnable);	
+		} catch (InvocationTargetException e) {
+			ExceptionHandler.handle(e, fSite.getShell(), 
+				ActionMessages.getString("OpenProjectAction.dialog.title"), //$NON-NLS-1$
+				ActionMessages.getString("OpenProjectAction.error.message")); //$NON-NLS-1$
+		} catch (InterruptedException e) {
+		}
+		showWarningDialog(nonJavaProjects);
+	}
+	
+	private IRunnableWithProgress createRunnable(
+		final Object[] projects,
+		final List nonJavaProjects) {
+		return new IRunnableWithProgress() {
+			public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+				monitor.beginTask("", projects.length); //$NON-NLS-1$
+				MultiStatus errorStatus= null;
+				for (int i = 0; i < projects.length; i++) {
+					IProject project= (IProject)projects[i];
+					try {
+						project.open(new SubProgressMonitor(monitor, 1));
+						if (project.getNature(JavaCore.NATURE_ID) == null) {
+							nonJavaProjects.add(project);
+						}		
+					} catch (CoreException e) {
+						if (errorStatus == null)
+							errorStatus = new MultiStatus(JavaPlugin.getPluginId(), IStatus.ERROR, ActionMessages.getString("OpenProjectAction.error.message"), e); //$NON-NLS-1$
+						errorStatus.merge(e.getStatus());
+					}
+				}
+				monitor.done();
+				if (errorStatus != null)
+					throw new InvocationTargetException(new CoreException(errorStatus));
+			}
+		};
+	}
+	
+	private void showWarningDialog(final List nonJavaProjects) {
+		int size= nonJavaProjects.size();
+		if (size > 0) {
+			ListDialog warningDialog= new ListDialog(fSite.getShell());
+			warningDialog.setAddCancelButton(false);
+			warningDialog.setTitle(ActionMessages.getString("OpenProjectAction.dialog.title")); //$NON-NLS-1$
+			if (size == 1)
+				warningDialog.setMessage(ActionMessages.getString("OpenProjectAction.no_java_nature.one")); //$NON-NLS-1$
+			else
+				warningDialog.setMessage(ActionMessages.getString("OpenProjectAction.no_java_nature.multiple")); //$NON-NLS-1$
+			warningDialog.setContentProvider(new IStructuredContentProvider() {
+				public Object[] getElements(Object inputElement) {
+					return ((List)inputElement).toArray();
+				}
+				public void dispose() {
+				}
+				public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+				}
+			});
+			warningDialog.setLabelProvider(new JavaElementLabelProvider());
+			warningDialog.setInput(nonJavaProjects);
+			warningDialog.open();
+		}
+	}
+	
 	private Object[] getClosedProjects() {
 		IProject[] projects= ResourcesPlugin.getWorkspace().getRoot().getProjects();
 		List result= new ArrayList(5);
