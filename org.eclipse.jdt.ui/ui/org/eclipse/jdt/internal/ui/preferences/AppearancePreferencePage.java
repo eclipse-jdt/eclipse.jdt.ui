ea63bbf19eb3928e8c8ac4576cc7a3d5de652500
Adam Kiezun <akiezun>
2002-05-15 11:43:10 +0000

    checkbox for package name compression

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/preferences/AppearancePreferencePage.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/preferences/AppearancePreferencePage.java
@@ -19,46 +19,44 @@ import org.eclipse.jdt.ui.JavaUI;
 import org.eclipse.jdt.internal.ui.IJavaHelpContextIds;
 import org.eclipse.jdt.internal.ui.JavaPlugin;
 import org.eclipse.jdt.internal.ui.JavaUIMessages;
-
 import org.eclipse.jdt.internal.ui.dialogs.StatusInfo;
 import org.eclipse.jdt.internal.ui.dialogs.StatusUtil;
-
 import org.eclipse.jdt.internal.ui.wizards.dialogfields.DialogField;
 import org.eclipse.jdt.internal.ui.wizards.dialogfields.IDialogFieldListener;
 import org.eclipse.jdt.internal.ui.wizards.dialogfields.LayoutUtil;
 import org.eclipse.jdt.internal.ui.wizards.dialogfields.SelectionButtonDialogField;
+import org.eclipse.jdt.internal.ui.wizards.dialogfields.Separator;
 import org.eclipse.jdt.internal.ui.wizards.dialogfields.StringDialogField;
 
 public class AppearancePreferencePage extends PreferencePage implements IWorkbenchPreferencePage {
 
-	// need to be 
 	public static final String PREF_METHOD_RETURNTYPE= JavaUI.ID_PLUGIN + ".methodreturntype"; //$NON-NLS-1$
 	public static final String PREF_OVERRIDE_INDICATOR= JavaUI.ID_PLUGIN + ".overrideindicator"; //$NON-NLS-1$
+	public static final String PREF_COMPRESS_PACKAGE_NAMES= JavaUI.ID_PLUGIN + ".compresspackagenames"; //$NON-NLS-1$
 	public static final String PREF_PKG_NAME_PATTERN_FOR_PKG_VIEW= "PackagesView.pkgNamePatternForPackagesView"; //$NON-NLS-1$
 
 	public static boolean showMethodReturnType() {
-		IPreferenceStore prefs= JavaPlugin.getDefault().getPreferenceStore();
-		return prefs.getBoolean(PREF_METHOD_RETURNTYPE);
+		return JavaPlugin.getDefault().getPreferenceStore().getBoolean(PREF_METHOD_RETURNTYPE);
 	}
 	
 	public static boolean showOverrideIndicators() {
-		IPreferenceStore prefs= JavaPlugin.getDefault().getPreferenceStore();
-		return prefs.getBoolean(PREF_OVERRIDE_INDICATOR);
+		return JavaPlugin.getDefault().getPreferenceStore().getBoolean(PREF_OVERRIDE_INDICATOR);
 	}	
 
-	static public String getPkgNamePatternForPackagesView() {
-		IPreferenceStore store= JavaPlugin.getDefault().getPreferenceStore();
-		return store.getString(PREF_PKG_NAME_PATTERN_FOR_PKG_VIEW);
+	public static String getPkgNamePatternForPackagesView() {
+		return JavaPlugin.getDefault().getPreferenceStore().getString(PREF_PKG_NAME_PATTERN_FOR_PKG_VIEW);
 	}
 
-	static public boolean isCompressingPkgNameInPackagesView() {
-		return getPkgNamePatternForPackagesView().length() > 0;
+	public static boolean isCompressingPkgNameInPackagesView() {
+		boolean enabled= JavaPlugin.getDefault().getPreferenceStore().getBoolean(PREF_COMPRESS_PACKAGE_NAMES);
+		return enabled && getPkgNamePatternForPackagesView().length() > 0;
 	}
 	
 	/**
 	 * Initializes the current options (read from preference store)
 	 */
 	public static void initDefaults(IPreferenceStore prefs) {
+		prefs.setDefault(PREF_COMPRESS_PACKAGE_NAMES, false);
 		prefs.setDefault(PREF_METHOD_RETURNTYPE, false);
 		prefs.setDefault(PREF_OVERRIDE_INDICATOR, true);
 		prefs.setDefault(PREF_PKG_NAME_PATTERN_FOR_PKG_VIEW, ""); //$NON-NLS-1$
@@ -66,9 +64,9 @@ public class AppearancePreferencePage extends PreferencePage implements IWorkben
 	
 	private SelectionButtonDialogField fShowMethodReturnType;
 	private SelectionButtonDialogField fShowOverrideIndicator;
+	private SelectionButtonDialogField fCompressPackageNames;
 	private StringDialogField fPackageNamePattern;
-	
-		
+			
 	public AppearancePreferencePage() {
 		setPreferenceStore(JavaPlugin.getDefault().getPreferenceStore());
 		setDescription(JavaUIMessages.getString("AppearancePreferencePage.description")); //$NON-NLS-1$
@@ -87,15 +85,22 @@ public class AppearancePreferencePage extends PreferencePage implements IWorkben
 		fShowOverrideIndicator.setDialogFieldListener(listener);
 		fShowOverrideIndicator.setLabelText(JavaUIMessages.getString("AppearancePreferencePage.overrideindicator.label")); //$NON-NLS-1$
 
+		fCompressPackageNames= new SelectionButtonDialogField(SWT.CHECK);
+		fCompressPackageNames.setDialogFieldListener(listener);
+		fCompressPackageNames.setLabelText("Co&mpress package name segments (except for the last one)"); 
+
 		fPackageNamePattern= new StringDialogField();
 		fPackageNamePattern.setDialogFieldListener(listener);
 		fPackageNamePattern.setLabelText(JavaUIMessages.getString("AppearancePreferencePage.pkgNamePattern.label")); //$NON-NLS-1$
 	}	
+
 	private void initFields() {
 		IPreferenceStore prefs= JavaPlugin.getDefault().getPreferenceStore();
 		fShowMethodReturnType.setSelection(prefs.getBoolean(PREF_METHOD_RETURNTYPE));
 		fShowOverrideIndicator.setSelection(prefs.getBoolean(PREF_OVERRIDE_INDICATOR));
 		fPackageNamePattern.setText(prefs.getString(PREF_PKG_NAME_PATTERN_FOR_PKG_VIEW));
+		fCompressPackageNames.setSelection(prefs.getBoolean(PREF_COMPRESS_PACKAGE_NAMES));
+		fPackageNamePattern.setEnabled(fCompressPackageNames.isSelected());
 	}
 	
 	/*
@@ -123,6 +128,8 @@ public class AppearancePreferencePage extends PreferencePage implements IWorkben
 				
 		fShowMethodReturnType.doFillIntoGrid(composite, nColumns);
 		fShowOverrideIndicator.doFillIntoGrid(composite, nColumns);
+		new Separator().doFillIntoGrid(composite, nColumns);
+		fCompressPackageNames.doFillIntoGrid(composite, nColumns);
 		fPackageNamePattern.doFillIntoGrid(composite, 2);
 		LayoutUtil.setWidthHint(fPackageNamePattern.getLabelControl(null), convertWidthInCharsToPixels(80));
 		
@@ -132,8 +139,17 @@ public class AppearancePreferencePage extends PreferencePage implements IWorkben
 	}
 	
 	private void doDialogFieldChanged(DialogField field) {
-		// no validation needed
-		updateStatus(new StatusInfo());
+		if (field == fCompressPackageNames)
+			fPackageNamePattern.setEnabled(fCompressPackageNames.isSelected());
+	
+		updateStatus(getValidationStatus());
+	}
+	
+	private IStatus getValidationStatus(){
+		if (fCompressPackageNames.isSelected() && fPackageNamePattern.getText().equals(""))
+			return new StatusInfo(IStatus.ERROR, "Enter a package name compression pattern");
+		else	
+			return new StatusInfo();
 	}
 	
 	private void updateStatus(IStatus status) {
@@ -155,6 +171,7 @@ public class AppearancePreferencePage extends PreferencePage implements IWorkben
 		prefs.setValue(PREF_METHOD_RETURNTYPE, fShowMethodReturnType.isSelected());
 		prefs.setValue(PREF_OVERRIDE_INDICATOR, fShowOverrideIndicator.isSelected());
 		prefs.setValue(PREF_PKG_NAME_PATTERN_FOR_PKG_VIEW, fPackageNamePattern.getText());
+		prefs.setValue(PREF_COMPRESS_PACKAGE_NAMES, fCompressPackageNames.isSelected());
 		return super.performOk();
 	}	
 	
@@ -166,6 +183,7 @@ public class AppearancePreferencePage extends PreferencePage implements IWorkben
 		fShowMethodReturnType.setSelection(prefs.getDefaultBoolean(PREF_METHOD_RETURNTYPE));
 		fShowOverrideIndicator.setSelection(prefs.getDefaultBoolean(PREF_OVERRIDE_INDICATOR));
 		fPackageNamePattern.setText(prefs.getDefaultString(PREF_PKG_NAME_PATTERN_FOR_PKG_VIEW));
+		fCompressPackageNames.setSelection(prefs.getDefaultBoolean(PREF_COMPRESS_PACKAGE_NAMES));
 		super.performDefaults();
 	}
 }
