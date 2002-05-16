f15094cb937a8da190514d461d72298cd83444e0
Andrew Weinand <aweinand>
2002-05-16 11:06:55 +0000

    #14680 Compare unreadable in high contrast black

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/compare/JavaMergeViewer.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/compare/JavaMergeViewer.java
@@ -5,6 +5,7 @@
 package org.eclipse.jdt.internal.ui.compare;
 
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.events.DisposeEvent;
 
 import org.eclipse.jface.text.IDocumentPartitioner;
 import org.eclipse.jface.text.TextViewer;
@@ -17,11 +18,41 @@ import org.eclipse.compare.contentmergeviewer.TextMergeViewer;
 import org.eclipse.jdt.ui.text.JavaSourceViewerConfiguration;
 import org.eclipse.jdt.ui.text.JavaTextTools;
 
+import org.eclipse.jdt.internal.ui.JavaPlugin;
 
-public class JavaMergeViewer extends TextMergeViewer {			
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.util.IPropertyChangeListener;
+import org.eclipse.jface.util.PropertyChangeEvent;
+
+public class JavaMergeViewer extends TextMergeViewer {
+	
+	private IPropertyChangeListener fPreferenceChangeListener;
+	private IPreferenceStore fPreferenceStore;
+		
 		
 	public JavaMergeViewer(Composite parent, int styles, CompareConfiguration mp) {
 		super(parent, styles, mp);
+		
+		fPreferenceStore= JavaPlugin.getDefault().getPreferenceStore();
+		if (fPreferenceStore != null) {
+			 fPreferenceChangeListener= new IPropertyChangeListener() {
+				public void propertyChange(PropertyChangeEvent event) {
+					JavaTextTools tools= JavaCompareUtilities.getJavaTextTools();
+					if (tools.affectsBehavior(event))
+						invalidateTextPresentation();
+				}
+			};
+			fPreferenceStore.addPropertyChangeListener(fPreferenceChangeListener);
+		}
+	}
+	
+	protected void handleDispose(DisposeEvent event) {
+		if (fPreferenceChangeListener != null) {
+			if (fPreferenceStore != null)
+				fPreferenceStore.removePropertyChangeListener(fPreferenceChangeListener);
+			fPreferenceChangeListener= null;
+		}
+		super.handleDispose(event);
 	}
 	
 	public String getTitle() {
