48313e7860e2b58877261466f2d2f3fbfaa65199
Adam Kiezun <akiezun>
2002-05-16 17:16:24 +0000

    status codes

---
+++ org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/base/RefactoringStatusCodes.java
@@ -0,0 +1,11 @@
+package org.eclipse.jdt.internal.corext.refactoring.base;
+
+public class RefactoringStatusCodes {
+
+	private RefactoringStatusCodes() {
+	}
+
+	public static final int NONE= 0;
+	public static final int OVERRIDES_ANOTHER_METHOD= 1;
+	public static final int METHOD_DECLARED_IN_INTERFACE= 2;
+}
