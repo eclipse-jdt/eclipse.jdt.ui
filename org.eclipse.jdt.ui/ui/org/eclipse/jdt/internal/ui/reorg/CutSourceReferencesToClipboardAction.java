5032af8057625904a6339e3a763c69d6afe2b880
Adam Kiezun <akiezun>
2002-05-16 11:54:09 +0000

    disabled cut of top level types - see 16075

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/reorg/CutSourceReferencesToClipboardAction.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/reorg/CutSourceReferencesToClipboardAction.java
@@ -1,16 +1,16 @@
 package org.eclipse.jdt.internal.ui.reorg;
 
+import java.util.Iterator;
+
 import org.eclipse.core.runtime.CoreException;
 
 import org.eclipse.swt.dnd.Clipboard;
 
-import org.eclipse.ui.IWorkbenchSite;
-
-import org.eclipse.jdt.internal.corext.refactoring.Assert;
-
 import org.eclipse.jface.viewers.IStructuredSelection;
 
-import org.eclipse.jdt.ui.actions.SelectionDispatchAction;
+import org.eclipse.ui.IWorkbenchSite;
+
+import org.eclipse.jdt.core.IType;
 
 public class CutSourceReferencesToClipboardAction extends SourceReferenceAction {
 
@@ -31,8 +31,24 @@ public class CutSourceReferencesToClipboardAction extends SourceReferenceAction 
 	}
 
 	protected void selectionChanged(IStructuredSelection selection) {
+		/*
+		 * cannot cut top-level types. this deltes the cu and then you cannot paste because the cu is gone. 
+		 */
+		if (containsTopLevelTypes(selection)){
+			setEnabled(false);
+			return;
+		}	
 		fCopy.selectionChanged(selection);
 		fDelete.selectionChanged(selection);
 		setEnabled(fCopy.isEnabled() && fDelete.isEnabled());
+	}
+
+	private static boolean containsTopLevelTypes(IStructuredSelection selection) {
+		for (Iterator iter = selection.iterator(); iter.hasNext();) {
+			Object each= iter.next();
+			if ((each instanceof IType) && ((IType)each).getDeclaringType() == null)
+				return true;
+		}
+		return false;
 	}
 }
