c2fec2171f450060a5df44e052375b0e58611b8b
Erich Gamma <egamma>
2002-05-20 19:33:16 +0000

    add icon for junit launch actions

--- org.eclipse.jdt.junit/src/org/eclipse/jdt/internal/junit/ui/ProgressImages.java
+++ org.eclipse.jdt.junit/src/org/eclipse/jdt/internal/junit/ui/ProgressImages.java
@@ -11,7 +11,7 @@ import org.eclipse.swt.graphics.Image;
 public class ProgressImages {
 	static final int PROGRESS_STEPS= 9;
 	
-	static final String BASE= JavaBrowsingMessages.getString("prgss/_1"); //$NON-NLS-1$
+	static final String BASE= "prgss/"; //$NON-NLS-1$
 	static final String FAILURE= "ff"; //$NON-NLS-1$
 	static final String OK= "ss"; //$NON-NLS-1$
 	
