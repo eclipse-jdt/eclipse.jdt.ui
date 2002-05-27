745b260c889ff26cd6d89bd75d2c20a5fb841769
Adam Kiezun <akiezun>
2002-05-27 16:08:38 +0000

    tests for 17473

---
+++ org.eclipse.jdt.ui.tests.refactoring/resources/ExtractTemp/canExtract/A_test38_in.java
@@ -0,0 +1,7 @@
+package p;
+class A{
+	private static void foo() {
+		int temp= 4;
+		System.out.println(temp);
+	}
+}
---
+++ org.eclipse.jdt.ui.tests.refactoring/resources/ExtractTemp/canExtract/A_test38_out.java
@@ -0,0 +1,8 @@
+package p;
+class A{
+	private static void foo() {
+		int temp= 4;
+		int temp1= temp;
+		System.out.println(temp1);
+	}
+}
