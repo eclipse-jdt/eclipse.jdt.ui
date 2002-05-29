7b82915a847408e63621b99f589f4a98b21c016b
Dirk Baeumer <dbaeumer>
2002-05-29 15:14:31 +0000

    [17045]

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/ui/actions/ImportActionGroup.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/ui/actions/ImportActionGroup.java
@@ -42,11 +42,6 @@ public class ImportActionGroup extends ActionGroup {
 
 	private static final String GROUP_IMPORT= "group.import"; //$NON-NLS-1$
 	
-	private static final int FAILED= 1 << 0;
-	private static final int IMPORT= 1 << 1;
-	private static final int EXPORT= 1 << 2;
-	private static final int IMPORT_EXPORT= IMPORT | EXPORT;
-
 	private ImportResourcesAction fImportAction;
 	private ExportResourcesAction fExportAction;
 
@@ -65,59 +60,9 @@ public class ImportActionGroup extends ActionGroup {
 	 * Method declared in ActionGroup
 	 */
 	public void fillContextMenu(IMenuManager menu) {
-		IStructuredSelection selection= getStructuredSelection();
-		if (selection == null)
-			return;
-			
-		int mode= checkSelection(selection);
-		if ((mode & FAILED) == 0) {
-			menu.appendToGroup(IContextMenuConstants.GROUP_REORGANIZE, new Separator(GROUP_IMPORT));
-			if ((mode  & IMPORT) != 0)
-				menu.appendToGroup(GROUP_IMPORT, fImportAction);
-			if ((mode & EXPORT) != 0)
-				menu.appendToGroup(GROUP_IMPORT, fExportAction);
-		}
+		menu.appendToGroup(IContextMenuConstants.GROUP_REORGANIZE, new Separator(GROUP_IMPORT));
+		menu.appendToGroup(GROUP_IMPORT, fImportAction);
+		menu.appendToGroup(GROUP_IMPORT, fExportAction);
 		super.fillContextMenu(menu);
-	}
-	
-	private void appendToGroup(IMenuManager menu, IAction action) {
-		if (action.isEnabled())
-			menu.appendToGroup(GROUP_IMPORT, action);
-	}
-	
-	private IStructuredSelection getStructuredSelection() {
-		ISelection selection= getContext().getSelection();
-		if (selection instanceof IStructuredSelection)
-			return (IStructuredSelection)selection;
-		return null;
-	}
-	
-	private int checkSelection(IStructuredSelection selection) {
-		int result= 0;
-		for (Iterator iter= selection.iterator(); iter.hasNext();) {
-			Object element= (Object) iter.next();
-			if (element instanceof IJavaElement) {
-				int type= ((IJavaElement)element).getElementType();
-				switch (type) {
-					case IJavaElement.JAVA_PROJECT:
-					case IJavaElement.PACKAGE_FRAGMENT_ROOT:
-					case IJavaElement.PACKAGE_FRAGMENT:
-						result= result | IMPORT_EXPORT;
-						break;
-					case IJavaElement.COMPILATION_UNIT:
-						result= result | EXPORT;
-						break;
-					default:
-						return FAILED;
-				}
-			} else if (element instanceof IContainer) {
-				result= result | IMPORT_EXPORT;
-			} else if (element instanceof IFile) {
-				result= result | EXPORT;
-			} else {
-				return FAILED;
-			}
-		}
-		return result;
-	}
+	}	
 }
