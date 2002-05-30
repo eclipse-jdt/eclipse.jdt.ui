155b985b0953ec937f49674511d89d8bf8cec34d
Adam Kiezun <akiezun>
2002-05-30 11:56:24 +0000

    generic error message instead of detail messages

--- org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/code/CodeRefactoringUtil.java
+++ org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/code/CodeRefactoringUtil.java
@@ -1,17 +1,16 @@
 package org.eclipse.jdt.internal.corext.refactoring.code;
 
+import java.text.MessageFormat;
+
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.Block;
 import org.eclipse.jdt.core.dom.CompilationUnit;
-import org.eclipse.jdt.core.dom.Message;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
 
-import org.eclipse.jdt.internal.corext.SourceRange;
 import org.eclipse.jdt.internal.corext.dom.ASTNodes;
 import org.eclipse.jdt.internal.corext.dom.Selection;
 import org.eclipse.jdt.internal.corext.dom.SelectionAnalyzer;
-import org.eclipse.jdt.internal.corext.refactoring.base.Context;
-import org.eclipse.jdt.internal.corext.refactoring.base.JavaSourceContext;
 import org.eclipse.jdt.internal.corext.refactoring.base.RefactoringStatus;
 
 class CodeRefactoringUtil {
@@ -21,18 +20,15 @@ class CodeRefactoringUtil {
     public static RefactoringStatus checkMethodSyntaxErrors(int selectionStart, int selectionLength, CompilationUnit cuNode, ICompilationUnit cu, String invalidSelectionMessage){
 			SelectionAnalyzer analyzer= new SelectionAnalyzer(Selection.createFromStartLength(selectionStart, selectionLength), true);
 			cuNode.accept(analyzer);
-			ASTNode lastCovering= analyzer.getLastCoveringNode();
-			if (! (lastCovering instanceof Block))
+			ASTNode coveringNode= analyzer.getLastCoveringNode();
+			if (! (coveringNode instanceof Block) || ! (coveringNode.getParent() instanceof MethodDeclaration))
 				return RefactoringStatus.createFatalErrorStatus(invalidSelectionMessage); 
-			Message[] messages= ASTNodes.getMessages(lastCovering, ASTNodes.INCLUDE_ALL_PARENTS);
-			RefactoringStatus result= new RefactoringStatus();
-			for (int i= 0; i < messages.length; i++) {
-                Context context= JavaSourceContext.create(cu, new SourceRange(messages[i].getStartPosition(), messages[i].getLength()));
-                result.addFatalError(messages[i].getMessage(), context);
-            }
-            if (result.hasFatalError())
-				return result;
-			else	
+			if (ASTNodes.getMessages(coveringNode, ASTNodes.NODE_ONLY).length == 0)
 				return RefactoringStatus.createFatalErrorStatus(invalidSelectionMessage); 
+
+			MethodDeclaration methodDecl= (MethodDeclaration)coveringNode.getParent();
+			String key= "Compilation errors in method ''{0}'' prevent analyzing the body of the method. Please fix the errors first.";
+			String message= MessageFormat.format(key, new String[]{methodDecl.getName().getIdentifier()});
+			return RefactoringStatus.createFatalErrorStatus(message);	
 	}
 }
