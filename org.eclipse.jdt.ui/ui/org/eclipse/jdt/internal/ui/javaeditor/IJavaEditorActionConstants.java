2a1d1c52ce4333f232dbfe8d23c3c5a7876f1930
Kai Maetzel <kmaetzel>
2002-06-10 08:55:27 +0000

    #18968

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/javaeditor/IJavaEditorActionConstants.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/javaeditor/IJavaEditorActionConstants.java
@@ -5,18 +5,30 @@
 package org.eclipse.jdt.internal.ui.javaeditor;
 
 /**
- * Defines action IDs for JavaEditor actions.
+ * Defines action IDs for private JavaEditor actions.
  */
 public interface IJavaEditorActionConstants {
 
 	/**
-	 * Edit menu: Name of the action to toggle presentation.
+	 * ID of the action to toggle the style of the presentation.
 	 */
 	public static final String TOGGLE_PRESENTATION= "togglePresentation"; //$NON-NLS-1$
 
 	/**
-	 * Edit menu: Name of the action to toggle text hover.
+	 * ID of the action to toggle the visibility of the text hover.
 	 */
 	public static final String TOGGLE_TEXT_HOVER= "toggleTextHover"; //$NON-NLS-1$
-
+	
+	
+	// http://dev.eclipse.org/bugs/show_bug.cgi?id=18968
+	/**
+	 * ID of the toolbar action to go to the previous error.
+	 */
+	public static final String PREVIOUS_ERROR= "gotoPreviousError"; //$NON-NLS-1$
+	
+	// http://dev.eclipse.org/bugs/show_bug.cgi?id=18968
+	/**
+	 * ID of the toolbar action to go to the next error.
+	 */
+	public static final String NEXT_ERROR= "gotoNextError"; //$NON-NLS-1$
 }
