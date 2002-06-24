273a16f298b866951ebf8297f1f0adc02edc8ddc
Dirk Baeumer <dbaeumer>
2002-06-24 16:40:29 +0000

    Javadoc

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/ui/text/java/hover/IJavaEditorTextHover.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/ui/text/java/hover/IJavaEditorTextHover.java
@@ -1,9 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2002 International Business Machines Corp. and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v0.5 
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ ******************************************************************************/
 package org.eclipse.jdt.ui.text.java.hover;
-
-/*
- * (c) Copyright IBM Corp. 2000, 2001.
- * All Rights Reserved.
- */
 
 import org.eclipse.jface.text.ITextHover;
 
@@ -17,6 +22,8 @@ import org.eclipse.ui.IEditorPart;
  *
  * @see IEditorPart
  * @see ITextHover
+ * 
+ * @since 2.0
  */
 public interface IJavaEditorTextHover extends ITextHover {
 
