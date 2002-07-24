7b19e44382899b3346aa05a94f868b719723fc07
Adam Kiezun <akiezun>
2002-07-24 14:31:28 +0000

    removed incorrect comment

--- org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/rename/RenameFieldRefactoring.java
+++ org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/rename/RenameFieldRefactoring.java
@@ -248,9 +248,6 @@ public class RenameFieldRefactoring extends Refactoring implements IRenameRefact
 		
 	// -------------- Preconditions -----------------------
 	
-	/* non java-doc
-	 * IPreactivatedRefactoring#checkPreactivation
-	 */
 	public RefactoringStatus checkPreactivation() throws JavaModelException {
 		return Checks.checkAvailability(fField);	
 	}
