76092dbeafe77ea3e72d07352af2281ee31b91f7
Adam Kiezun <akiezun>
2002-07-25 15:03:52 +0000

    +1 test

--- org.eclipse.jdt.ui.tests.refactoring/test cases/org/eclipse/jdt/ui/tests/refactoring/ExtractInterfaceTests.java
+++ org.eclipse.jdt.ui.tests.refactoring/test cases/org/eclipse/jdt/ui/tests/refactoring/ExtractInterfaceTests.java
@@ -247,6 +247,12 @@ public class ExtractInterfaceTests extends RefactoringTest {
 		validatePassingTest("A", "I", names, signatures, true);
 	}
 
+	public void test31() throws Exception{
+		String[] names= new String[]{"m"};
+		String[][] signatures= new String[][]{new String[0]};
+		validatePassingTest("A", "I", names, signatures, true);
+	}
+
 	public void testFail0() throws Exception{
 		validateFailingTest("A", "I", true, RefactoringStatus.FATAL);
 	}
