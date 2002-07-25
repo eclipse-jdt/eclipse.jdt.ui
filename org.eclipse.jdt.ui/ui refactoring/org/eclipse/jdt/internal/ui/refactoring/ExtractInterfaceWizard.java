2da48312c2cc543b6796568112779b1b6ba62086
Adam Kiezun <akiezun>
2002-07-25 09:18:14 +0000

    don't expoand the first node

--- org.eclipse.jdt.ui/ui refactoring/org/eclipse/jdt/internal/ui/refactoring/ExtractInterfaceWizard.java
+++ org.eclipse.jdt.ui/ui refactoring/org/eclipse/jdt/internal/ui/refactoring/ExtractInterfaceWizard.java
@@ -3,11 +3,10 @@ package org.eclipse.jdt.internal.ui.refactoring;
 import org.eclipse.jdt.internal.corext.refactoring.structure.ExtractInterfaceRefactoring;
 
 public class ExtractInterfaceWizard extends RefactoringWizard {
-
+	
+	private static final String EXTRACT_INTERFACE_ERROR_PAGE_HELP_ID= "errorPageContextHelpId"; //XXX
 	public ExtractInterfaceWizard(ExtractInterfaceRefactoring ref) {
-		//XXX
-		super(ref, "Extract Interface", "errorPageContextHelpId");
-		setExpandFirstNode(true);
+		super(ref, "Extract Interface", EXTRACT_INTERFACE_ERROR_PAGE_HELP_ID);
 	}
 
 	/* non java-doc
