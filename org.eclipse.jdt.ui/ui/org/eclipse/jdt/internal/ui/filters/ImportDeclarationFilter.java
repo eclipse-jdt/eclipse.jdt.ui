e9cfe203c80efdc394b2287bd4e4d52e501d0be2
Dani Megert <dmegert>
2002-08-19 10:47:09 +0000

    Added import declaration filter

---
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/filters/ImportDeclarationFilter.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2000, 2002 IBM Corp. and others..
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Common Public License v0.5
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v05.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ */
+package org.eclipse.jdt.internal.ui.filters;
+
+
+import org.eclipse.jface.viewers.Viewer;
+
+import org.eclipse.jdt.core.IImportContainer;
+import org.eclipse.jdt.core.dom.ImportDeclaration;
+
+
+/**
+ * Filters empty non-leaf package fragments
+ */
+public class ImportDeclarationFilter extends AbstractFilter {
+
+	/*
+	 * @see ViewerFilter
+	 */
+	public boolean select(Viewer viewer, Object parent, Object element) {
+		return !((element instanceof IImportContainer) || (element instanceof ImportDeclaration));
+	}
+}
