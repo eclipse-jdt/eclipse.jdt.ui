1f8255e536ce80ababfc28cc4b9f348935fc4851
Adam Kiezun <akiezun>
2002-08-28 13:59:46 +0000

    *** empty log message ***

---
+++ org.eclipse.jdt.ui.tests.refactoring/resources/ExtractInterface/test61/in/A.java
@@ -0,0 +1,10 @@
+package p;
+
+class A {
+	public void m() {}
+	public void m1() {}
+	public static int s= 0;
+	void t(){
+		A.s= 1;
+	}
+}
--- org.eclipse.jdt.ui.tests.refactoring/test cases/org/eclipse/jdt/ui/tests/refactoring/ExtractInterfaceTests.java
+++ org.eclipse.jdt.ui.tests.refactoring/test cases/org/eclipse/jdt/ui/tests/refactoring/ExtractInterfaceTests.java
@@ -364,6 +364,14 @@ public class ExtractInterfaceTests extends RefactoringTest {
 //		standardPassingTest();
 	}
 
+	public void test60() throws Exception{
+		standardPassingTest();
+	}
+
+	public void test61() throws Exception{
+		standardPassingTest();
+	}
+
 	public void testFail0() throws Exception{
 		validateFailingTest("A", "I", true, RefactoringStatus.FATAL);
 	}
