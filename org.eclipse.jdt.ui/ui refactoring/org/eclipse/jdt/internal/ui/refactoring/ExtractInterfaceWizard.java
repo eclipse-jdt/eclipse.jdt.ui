a9bc64f2648dd22e2869e47f5cbff2c729124257
Adam Kiezun <akiezun>
2002-08-30 12:07:11 +0000

    using correct help id

--- org.eclipse.jdt.ui/ui refactoring/org/eclipse/jdt/internal/ui/refactoring/ExtractInterfaceWizard.java
+++ org.eclipse.jdt.ui/ui refactoring/org/eclipse/jdt/internal/ui/refactoring/ExtractInterfaceWizard.java
@@ -1,12 +1,12 @@
 package org.eclipse.jdt.internal.ui.refactoring;
 
 import org.eclipse.jdt.internal.corext.refactoring.structure.ExtractInterfaceRefactoring;
+import org.eclipse.jdt.internal.ui.IJavaHelpContextIds;
 
 public class ExtractInterfaceWizard extends RefactoringWizard {
 	
-	private static final String EXTRACT_INTERFACE_ERROR_PAGE_HELP_ID= "errorPageContextHelpId"; //XXX
 	public ExtractInterfaceWizard(ExtractInterfaceRefactoring ref) {
-		super(ref, "Extract Interface", EXTRACT_INTERFACE_ERROR_PAGE_HELP_ID);
+		super(ref, "Extract Interface", IJavaHelpContextIds.EXTRACT_INTERFACE_ERROR_WIZARD_PAGE);
 	}
 
 	/* non java-doc
