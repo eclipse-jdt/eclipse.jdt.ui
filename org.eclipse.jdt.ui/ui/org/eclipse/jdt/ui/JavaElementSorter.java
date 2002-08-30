5a00178640e1d41b0fdd74eec3efded073c266c2
Dani Megert <dmegert>
2002-08-30 12:13:03 +0000

    Changed import order

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/ui/JavaElementSorter.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/ui/JavaElementSorter.java
@@ -32,8 +32,8 @@ import org.eclipse.jdt.core.IPackageFragmentRoot;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.core.Signature;
 
-import org.eclipse.jdt.internal.ui.JavaPlugin;
 import org.eclipse.jdt.internal.corext.util.JavaModelUtil;
+import org.eclipse.jdt.internal.ui.JavaPlugin;
 
 /**
  * Sorter for Java elements. Ordered by element category, then by element name. 
