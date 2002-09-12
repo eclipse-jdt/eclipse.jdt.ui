1d6b6e279f0cc41dc2c0b60dca6a42a8b9870235
Adam Kiezun <akiezun>
2002-09-12 14:42:37 +0000

    1 util method

--- org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/code/CodeRefactoringUtil.java
+++ org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/code/CodeRefactoringUtil.java
@@ -2,6 +2,9 @@ package org.eclipse.jdt.internal.corext.refactoring.code;
 
 import java.text.MessageFormat;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.CoreException;
+
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.Block;
@@ -13,23 +16,39 @@ import org.eclipse.jdt.internal.corext.dom.Selection;
 import org.eclipse.jdt.internal.corext.dom.SelectionAnalyzer;
 import org.eclipse.jdt.internal.corext.refactoring.RefactoringCoreMessages;
 import org.eclipse.jdt.internal.corext.refactoring.base.RefactoringStatus;
+import org.eclipse.jdt.internal.corext.textmanipulation.TextBuffer;
+import org.eclipse.jdt.internal.corext.util.CodeFormatterUtil;
 
-class CodeRefactoringUtil {
+public class CodeRefactoringUtil {
 
     private CodeRefactoringUtil() {}
 
     public static RefactoringStatus checkMethodSyntaxErrors(int selectionStart, int selectionLength, CompilationUnit cuNode, ICompilationUnit cu, String invalidSelectionMessage){
-			SelectionAnalyzer analyzer= new SelectionAnalyzer(Selection.createFromStartLength(selectionStart, selectionLength), true);
-			cuNode.accept(analyzer);
-			ASTNode coveringNode= analyzer.getLastCoveringNode();
-			if (! (coveringNode instanceof Block) || ! (coveringNode.getParent() instanceof MethodDeclaration))
-				return RefactoringStatus.createFatalErrorStatus(invalidSelectionMessage); 
-			if (ASTNodes.getMessages(coveringNode, ASTNodes.NODE_ONLY).length == 0)
-				return RefactoringStatus.createFatalErrorStatus(invalidSelectionMessage); 
+		SelectionAnalyzer analyzer= new SelectionAnalyzer(Selection.createFromStartLength(selectionStart, selectionLength), true);
+		cuNode.accept(analyzer);
+		ASTNode coveringNode= analyzer.getLastCoveringNode();
+		if (! (coveringNode instanceof Block) || ! (coveringNode.getParent() instanceof MethodDeclaration))
+			return RefactoringStatus.createFatalErrorStatus(invalidSelectionMessage); 
+		if (ASTNodes.getMessages(coveringNode, ASTNodes.NODE_ONLY).length == 0)
+			return RefactoringStatus.createFatalErrorStatus(invalidSelectionMessage); 
 
-			MethodDeclaration methodDecl= (MethodDeclaration)coveringNode.getParent();
-			String key= RefactoringCoreMessages.getString("CodeRefactoringUtil.error.message"); //$NON-NLS-1$
-			String message= MessageFormat.format(key, new String[]{methodDecl.getName().getIdentifier()});
-			return RefactoringStatus.createFatalErrorStatus(message);	
+		MethodDeclaration methodDecl= (MethodDeclaration)coveringNode.getParent();
+		String key= RefactoringCoreMessages.getString("CodeRefactoringUtil.error.message"); //$NON-NLS-1$
+		String message= MessageFormat.format(key, new String[]{methodDecl.getName().getIdentifier()});
+		return RefactoringStatus.createFatalErrorStatus(message);	
 	}
+	
+	public static int getIndentationLevel(ASTNode node, IFile file) throws CoreException {
+		TextBuffer buffer= null;
+		try{
+			buffer= TextBuffer.acquire(file);
+			int startLine= buffer.getLineOfOffset(node.getStartPosition());
+			return buffer.getLineIndent(startLine, CodeFormatterUtil.getTabWidth());
+		} finally {
+			if (buffer != null)
+				TextBuffer.release(buffer);
+		}
+	}
+	
+	
 }
