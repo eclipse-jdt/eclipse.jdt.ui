9bf1b901f3d6bddc42025981c19d80770d06a389
Dani Megert <dmegert>
2002-09-12 14:47:44 +0000

    Replace single open actions with Navigate action grup

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/search/SearchViewActionGroup.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/search/SearchViewActionGroup.java
@@ -10,93 +10,23 @@
  ******************************************************************************/
 package org.eclipse.jdt.internal.ui.search;
 
-import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.util.Assert;
-import org.eclipse.jface.viewers.ISelectionProvider;
-import org.eclipse.jface.viewers.IStructuredSelection;
 
-import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.IViewPart;
-import org.eclipse.ui.IWorkbenchSite;
 import org.eclipse.ui.actions.ActionGroup;
 
-import org.eclipse.jdt.ui.IContextMenuConstants;
-
 import org.eclipse.jdt.ui.actions.JavaSearchActionGroup;
-import org.eclipse.jdt.ui.actions.JdtActionConstants;
-import org.eclipse.jdt.ui.actions.OpenAction;
-import org.eclipse.jdt.ui.actions.OpenTypeHierarchyAction;
+import org.eclipse.jdt.ui.actions.NavigateActionGroup;
 
 import org.eclipse.jdt.internal.ui.actions.CompositeActionGroup;
 
 class SearchViewActionGroup extends CompositeActionGroup {
 
-	private ISelectionProvider fSelectionProvider;
-	private IActionBars fActionBars;
-
-	private OpenTypeHierarchyAction fOpenTypeHierarchyAction;
-	private OpenAction fOpenAction;
-  	
 	public SearchViewActionGroup(IViewPart part) {
 		Assert.isNotNull(part);
-		fSelectionProvider= part.getSite().getSelectionProvider();
-		
-		setGroups(new ActionGroup[] { new JavaSearchActionGroup(part)});
-		initializeActions(new SearchViewSiteAdapter(part.getSite()));
-	}
-
-	private void initializeActions(IWorkbenchSite site) {
-		ISelectionProvider provider= site.getSelectionProvider();
-		IStructuredSelection selection= (IStructuredSelection)provider.getSelection();
-
-		// Open Type Hierarchy (F3)
-		fOpenAction= new OpenAction(site);
-		fOpenAction.update(selection);
-		provider.addSelectionChangedListener(fOpenAction);
-		
-		// Open Type Hierarchy (F4)
-		fOpenTypeHierarchyAction= new OpenTypeHierarchyAction(site);
-		fOpenTypeHierarchyAction.update(selection);
-		provider.addSelectionChangedListener(fOpenTypeHierarchyAction);
-	}
-
-	public void dispose() {
-		if (fSelectionProvider != null) {
-			fSelectionProvider.removeSelectionChangedListener(fOpenTypeHierarchyAction);
-			fSelectionProvider.removeSelectionChangedListener(fOpenAction);
-		}
-		fOpenAction= null;
-		fOpenTypeHierarchyAction= null;
-		updateGlobalActionHandlers();
-		super.dispose();
-	}
-	
-	public void fillActionBars(IActionBars actionBars) {
-		Assert.isNotNull(actionBars);
-		super.fillActionBars(actionBars);
-		fActionBars= actionBars;
-		updateGlobalActionHandlers();
-	}
-
-	private void updateGlobalActionHandlers() {
-		if (fActionBars != null) {
-			fActionBars.setGlobalActionHandler(JdtActionConstants.OPEN_TYPE_HIERARCHY, fOpenTypeHierarchyAction);
-			fActionBars.setGlobalActionHandler(JdtActionConstants.OPEN, fOpenAction);
-		}
-	}
-
-	//---- Context menu -------------------------------------------------------------------------
-
-	public void fillContextMenu(IMenuManager menu) {		
-		super.fillContextMenu(menu);
-		appendToGroup(menu, fOpenAction, IContextMenuConstants.GROUP_OPEN);
-		appendToGroup(menu, fOpenTypeHierarchyAction, IContextMenuConstants.GROUP_OPEN);		
-	}
-	
-	private void appendToGroup(IMenuManager menu, IAction action, String groupName) {
-		if (action.isEnabled())
-			menu.appendToGroup(groupName, action);
+		setGroups(new ActionGroup[] {
+			new JavaSearchActionGroup(part),
+			new NavigateActionGroup(new SearchViewAdapter(new SearchViewSiteAdapter(part.getSite())))});
 	}
 }
 
--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/search/SearchViewSiteAdapter.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/search/SearchViewSiteAdapter.java
@@ -15,13 +15,17 @@ import org.eclipse.core.runtime.CoreException;
 
 import org.eclipse.swt.widgets.Shell;
 
+import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.util.Assert;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.ISelectionProvider;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.StructuredSelection;
 
+import org.eclipse.ui.IKeyBindingService;
 import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchPartSite;
 import org.eclipse.ui.IWorkbenchSite;
 import org.eclipse.ui.IWorkbenchWindow;
 
@@ -39,11 +43,15 @@ import org.eclipse.jdt.internal.ui.util.SelectionUtil;
  * This class adapts a Search view site.
  * It converts selection of Search view entries to
  * be a selection of Java elements.
+ * 
+ * @since 2.0
  */
-class SearchViewSiteAdapter implements IWorkbenchSite {
+class SearchViewSiteAdapter implements IWorkbenchPartSite {
 	
 	private ISelectionProvider fProvider;
 	private IWorkbenchSite fSite;
+	
+	private ISelectionChangedListener fListener;
 
 	public SearchViewSiteAdapter(IWorkbenchSite site){
 		fSite= site;
@@ -70,12 +78,20 @@ class SearchViewSiteAdapter implements IWorkbenchSite {
 		Assert.isNotNull(provider);
 		fProvider= 
 			new ISelectionProvider() {
-				public void addSelectionChangedListener(ISelectionChangedListener listener) {
+				public void addSelectionChangedListener(final ISelectionChangedListener listener) {
+					fListener=
+						new ISelectionChangedListener() {
+							public void selectionChanged(SelectionChangedEvent event) {
+								listener.selectionChanged(new SelectionChangedEvent(fProvider, convertSelection(event.getSelection())));
+							}
+						};
+					provider.addSelectionChangedListener(fListener);
 				}
 				public ISelection getSelection() {
 					return convertSelection(provider.getSelection());
 				}
 				public void removeSelectionChangedListener(ISelectionChangedListener listener) {
+					provider.removeSelectionChangedListener(fListener);
 				}
 				public void setSelection(ISelection selection) {
 				}
@@ -96,5 +112,47 @@ class SearchViewSiteAdapter implements IWorkbenchSite {
 			}
 		}
 		return StructuredSelection.EMPTY;		
+	}
+	
+	// --------- only empty stubs below ---------
+		
+	/*
+	 * @see org.eclipse.ui.IWorkbenchPartSite#getId()
+	 */
+	public String getId() {
+		return null;
+	}
+
+	/*
+	 * @see org.eclipse.ui.IWorkbenchPartSite#getKeyBindingService()
+	 */
+	public IKeyBindingService getKeyBindingService() {
+		return null;
+	}
+
+	/*
+	 * @see org.eclipse.ui.IWorkbenchPartSite#getPluginId()
+	 */
+	public String getPluginId() {
+		return null;
+	}
+
+	/*
+	 * @see org.eclipse.ui.IWorkbenchPartSite#getRegisteredName()
+	 */
+	public String getRegisteredName() {
+		return null;
+	}
+
+	/*
+	 * @see org.eclipse.ui.IWorkbenchPartSite#registerContextMenu(MenuManager, ISelectionProvider)
+	 */
+	public void registerContextMenu(MenuManager menuManager, ISelectionProvider selectionProvider) {
+	}
+
+	/*
+	 * @see org.eclipse.ui.IWorkbenchPartSite#registerContextMenu(String, MenuManager, ISelectionProvider)
+	 */
+	public void registerContextMenu(String menuId, MenuManager menuManager, ISelectionProvider selectionProvider) {
 	}
 }
