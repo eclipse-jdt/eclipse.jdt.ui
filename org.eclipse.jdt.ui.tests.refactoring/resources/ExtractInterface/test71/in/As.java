98b356f3aca8e729b742a22390e36dba3619a568
Adam Kiezun <akiezun>
2002-09-17 10:48:26 +0000

    *** empty log message ***

---
+++ org.eclipse.jdt.ui.tests.refactoring/resources/ExtractInterface/test71/in/As.java
@@ -0,0 +1,10 @@
+package p;
+/** typecomment template*/
+public class As {
+	void f(){
+		A a= new A();
+		if (a instanceof A){
+		} else {
+		}
+	}
+}
---
+++ org.eclipse.jdt.ui.tests.refactoring/resources/ExtractInterface/test71/out/As.java
@@ -0,0 +1,10 @@
+package p;
+/** typecomment template*/
+public class As {
+	void f(){
+		I a= new A();
+		if (a instanceof I){
+		} else {
+		}
+	}
+}
