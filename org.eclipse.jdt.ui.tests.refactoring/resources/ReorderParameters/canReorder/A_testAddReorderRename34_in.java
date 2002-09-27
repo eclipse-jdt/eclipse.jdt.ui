cfc7c1f2e95d8d0378457b2bd4f8474158935748
Adam Kiezun <akiezun>
2002-09-27 12:08:21 +0000

    *** empty log message ***

---
+++ org.eclipse.jdt.ui.tests.refactoring/resources/ReorderParameters/canReorder/A_testAddReorderRename34_in.java
@@ -0,0 +1,6 @@
+package p;
+class A{
+	private int m(int iii, boolean j){
+		return m(m(iii, j), false);
+	}
+}
---
+++ org.eclipse.jdt.ui.tests.refactoring/resources/ReorderParameters/canReorder/A_testAddReorderRename34_out.java
@@ -0,0 +1,6 @@
+package p;
+class A{
+	private int m(boolean jj, Object x, int i){
+		return m(false, null, m(jj, null, i));
+	}
+}
