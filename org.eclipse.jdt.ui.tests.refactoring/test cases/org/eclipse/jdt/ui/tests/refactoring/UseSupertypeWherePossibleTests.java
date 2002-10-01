ed374fe0f15fd963e31367438d78aee982f27f2e
Adam Kiezun <akiezun>
2002-10-01 17:25:13 +0000

    *** empty log message ***

--- org.eclipse.jdt.ui.tests.refactoring/test cases/org/eclipse/jdt/ui/tests/refactoring/UseSupertypeWherePossibleTests.java
+++ org.eclipse.jdt.ui.tests.refactoring/test cases/org/eclipse/jdt/ui/tests/refactoring/UseSupertypeWherePossibleTests.java
@@ -192,6 +192,16 @@ public class UseSupertypeWherePossibleTests extends RefactoringTest {
 		validatePassingTest("A", new String[]{"A", "B"}, "p.B");
 	}
 
+	public void testNew29() throws Exception{
+		printTestDisabledMessage("bug 24278");
+//		validatePassingTest("A", new String[]{"A"}, "p.B");
+	}
+
+	public void testNew30() throws Exception{
+		printTestDisabledMessage("bug 24278");
+//		validatePassingTest("A", new String[]{"A"}, "p.B");
+	}
+
 	/* i had to rename tests 0-15 because of cvs problems*/
 	
 	
