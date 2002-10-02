79338e515c4a368668f346e4c7e89707a4834ce3
Adam Kiezun <akiezun>
2002-10-02 08:42:54 +0000

    new codes  - for extract constant

--- org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/base/RefactoringStatusCodes.java
+++ org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/base/RefactoringStatusCodes.java
@@ -8,4 +8,8 @@ public class RefactoringStatusCodes {
 	public static final int NONE= 0;
 	public static final int OVERRIDES_ANOTHER_METHOD= 1;
 	public static final int METHOD_DECLARED_IN_INTERFACE= 2;
+	
+	public static final int EXPRESSION_NOT_RVALUE= 64;
+	public static final int EXPRESSION_NOT_RVALUE_VOID= 65;
+	public static final int EXTRANEOUS_TEXT= 66;
 }
