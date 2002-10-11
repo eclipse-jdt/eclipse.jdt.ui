7597f3f1e987b598889099625f58df90ce1e514e
Adam Kiezun <akiezun>
2002-10-11 08:30:42 +0000

    parameterAdded needs special treatment

--- org.eclipse.jdt.ui/ui refactoring/org/eclipse/jdt/internal/ui/refactoring/ParameterListChangeListener.java
+++ org.eclipse.jdt.ui/ui refactoring/org/eclipse/jdt/internal/ui/refactoring/ParameterListChangeListener.java
@@ -15,10 +15,18 @@ import org.eclipse.jdt.internal.corext.refactoring.ParameterInfo;
 public interface ParameterListChangeListener {
 
 	/**
-	 * Gets fired when the given parameter has changed	 * @param parameter the paramter that has changed.	 */
+	 * Gets fired when the given parameter has changed	 * @param parameter the parameter that has changed.	 */
 	public void parameterChanged(ParameterInfo parameter);
 
 	/**
-	 * Gets fired if the parameter list got modified by reordering, adding or removing parameters)	 */
+	 * Gets fired when the given parameter has been added
+	 * @param parameter the parameter that has been added.
+	 */
+	public void parameterAdded(ParameterInfo parameter);
+	
+	
+	/**
+	 * Gets fired if the parameter list got modified by reordering or removing 
+	 * parameters (note that adding is handled by <code>parameterAdded</code>))	 */
 	public void parameterListChanged();
 }
