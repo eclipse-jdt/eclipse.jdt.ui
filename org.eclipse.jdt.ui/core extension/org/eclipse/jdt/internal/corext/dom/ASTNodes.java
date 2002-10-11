fad2aff3d07c593a144981b4728ccda4e2748b13
Adam Kiezun <akiezun>
2002-10-11 13:20:50 +0000

    getProblems getNameIdentifier

--- org.eclipse.jdt.ui/core extension/org/eclipse/jdt/internal/corext/dom/ASTNodes.java
+++ org.eclipse.jdt.ui/core extension/org/eclipse/jdt/internal/corext/dom/ASTNodes.java
@@ -7,25 +7,22 @@ package org.eclipse.jdt.internal.corext.dom;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.jdt.core.compiler.IProblem;
 import org.eclipse.jdt.core.compiler.ITerminalSymbols;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.ArrayType;
 import org.eclipse.jdt.core.dom.CompilationUnit;
-import org.eclipse.jdt.core.dom.ConditionalExpression;
 import org.eclipse.jdt.core.dom.Expression;
 import org.eclipse.jdt.core.dom.ForStatement;
 import org.eclipse.jdt.core.dom.IBinding;
 import org.eclipse.jdt.core.dom.IMethodBinding;
 import org.eclipse.jdt.core.dom.ITypeBinding;
 import org.eclipse.jdt.core.dom.IVariableBinding;
-import org.eclipse.jdt.core.dom.InfixExpression;
 import org.eclipse.jdt.core.dom.Message;
 import org.eclipse.jdt.core.dom.Modifier;
 import org.eclipse.jdt.core.dom.Name;
-import org.eclipse.jdt.core.dom.PostfixExpression;
-import org.eclipse.jdt.core.dom.PrefixExpression;
 import org.eclipse.jdt.core.dom.PrimitiveType;
 import org.eclipse.jdt.core.dom.QualifiedName;
 import org.eclipse.jdt.core.dom.SimpleName;
@@ -45,6 +42,7 @@ public class ASTNodes {
 	public static final int INCLUDE_ALL_PARENTS= 	2;
 
 	private static final Message[] EMPTY_MESSAGES= new Message[0];
+	private static final IProblem[] EMPTY_PROBLEMS= new IProblem[0];
 	
 	private static final int[] MODIFIERS= {Modifier.PUBLIC, Modifier.PROTECTED, Modifier.PRIVATE, Modifier.ABSTRACT, Modifier.STATIC,
 		Modifier.FINAL, Modifier.TRANSIENT, Modifier.VOLATILE, Modifier.NATIVE, Modifier.SYNCHRONIZED, Modifier.STRICTFP };
@@ -304,6 +302,33 @@ public class ASTNodes {
 		return -1;
 	}
 	
+	public static IProblem[] getProblems(ASTNode node, int flags) {
+		ASTNode root= node.getRoot();
+		if (!(root instanceof CompilationUnit))
+			return EMPTY_PROBLEMS;
+		IProblem[] problems= ((CompilationUnit)root).getProblems();
+		if (root == node)
+			return problems;
+		final int iterations= computeIterations(flags);
+		List result= new ArrayList(5);
+		for (int i= 0; i < problems.length; i++) {
+			IProblem problem= problems[i];
+			ASTNode temp= node;
+			int count= iterations;
+			do {
+				int nodeOffset= temp.getStartPosition();
+				int problemOffset= problem.getSourceStart();
+				if (nodeOffset <= problemOffset && problemOffset < nodeOffset + temp.getLength()) {
+					result.add(problem);
+					count= 0;
+				} else {
+					count--;
+				}
+			} while ((temp= temp.getParent()) != null && count > 0);
+		}
+		return (IProblem[]) result.toArray(new IProblem[result.size()]);
+	}
+	
 	public static Message[] getMessages(ASTNode node, int flags) {
 		ASTNode root= node.getRoot();
 		if (!(root instanceof CompilationUnit))
@@ -343,4 +368,17 @@ public class ASTNodes {
 				return 1;
 		}
 	}
+	
+	//recursive
+	public static String getNameIdentifier(Name name) {
+		if (name.isSimpleName())
+			return ((SimpleName) name).getIdentifier();
+		if (name.isQualifiedName()) {
+			QualifiedName qn= (QualifiedName) name;
+			return getNameIdentifier(qn.getQualifier()) + "." + qn.getName().getIdentifier(); //$NON-NLS-1$
+		}
+		Assert.isTrue(false);
+		return ""; //$NON-NLS-1$
+	}
+	
 }
