0d06aa95606b688088001dc49cc1efd1341a489b
Martin Aeschlimann <maeschli>
2002-10-23 17:13:32 +0000

    catch clause to throws

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/correction/QuickAssistProcessor.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/correction/QuickAssistProcessor.java
@@ -8,10 +8,17 @@ import org.eclipse.swt.graphics.Image;
 
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.BodyDeclaration;
+import org.eclipse.jdt.core.dom.CatchClause;
 import org.eclipse.jdt.core.dom.Expression;
 import org.eclipse.jdt.core.dom.ExpressionStatement;
 import org.eclipse.jdt.core.dom.ITypeBinding;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.Name;
+import org.eclipse.jdt.core.dom.SimpleType;
 import org.eclipse.jdt.core.dom.Statement;
+import org.eclipse.jdt.core.dom.TryStatement;
+import org.eclipse.jdt.core.dom.Type;
 import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
 import org.eclipse.jdt.core.dom.VariableDeclarationStatement;
 
@@ -36,6 +43,7 @@ public class QuickAssistProcessor implements ICorrectionProcessor {
 			return;
 		}
 		getAssignToVariableProposals(context, resultingCollections);
+		getCatchClauseToThrowsProposals(context, resultingCollections);
 	}
 	
 	private void getAssignToVariableProposals(ICorrectionContext context, List resultingCollections) throws CoreException {
@@ -68,12 +76,59 @@ public class QuickAssistProcessor implements ICorrectionProcessor {
 		rewrite.markAsReplaced(expressionStatement, newDecl);
 		
 		String label= CorrectionMessages.getString("QuickAssistProcessor.assigntolocal.description");
-		Image image= JavaPluginImages.get(JavaPluginImages.IMG_CORRECTION_CHANGE);
+		Image image= JavaPluginImages.get(JavaPluginImages.IMG_CORRECTION_LOCAL);
 		ASTRewriteCorrectionProposal proposal= new ASTRewriteCorrectionProposal(label, context.getCompilationUnit(), rewrite, 1, image);
 		proposal.addImport(typeBinding);
 		proposal.ensureNoModifications();
 		resultingCollections.add(proposal);
 	}
+	
+	private void getCatchClauseToThrowsProposals(ICorrectionContext context, List resultingCollections) throws CoreException {
+		ASTNode node= context.getCoveringNode();
+		CatchClause catchClause= (CatchClause) ASTResolving.findAncestor(node, ASTNode.CATCH_CLAUSE);
+		if (catchClause == null) {
+			return;
+		}
+		Type type= catchClause.getException().getType();
+		if (!type.isSimpleType()) {
+			return;
+		}
+		
+		BodyDeclaration bodyDeclaration= ASTResolving.findParentBodyDeclaration(catchClause);
+		if (!(bodyDeclaration instanceof MethodDeclaration)) {
+			return;
+		}
+		
+		MethodDeclaration methodDeclaration= (MethodDeclaration) bodyDeclaration;
+		
+		ASTRewrite rewrite= new ASTRewrite(methodDeclaration);
+		AST ast= methodDeclaration.getAST();
+		
+		TryStatement tryStatement= (TryStatement) catchClause.getParent();
+		if (tryStatement.catchClauses().size() > 1 || tryStatement.getFinally() != null) {
+			rewrite.markAsRemoved(catchClause);
+		} else {
+			List statements= tryStatement.getBody().statements();
+			if (statements.size() > 0) {
+				ASTNode placeholder= rewrite.createCopy((ASTNode) statements.get(0), (ASTNode) statements.get(statements.size() - 1));
+				rewrite.markAsReplaced(tryStatement, placeholder);
+			} else {
+				rewrite.markAsRemoved(tryStatement);
+			}
+		}
+		Name name= ((SimpleType) type).getName();
+		Name newName= (Name) ASTNode.copySubtree(ast, name);
+		rewrite.markAsInserted(newName);
+		methodDeclaration.thrownExceptions().add(newName);
+	
+		String label= CorrectionMessages.getString("QuickAssistProcessor.catchclausetothrows.description");
+		Image image= JavaPluginImages.get(JavaPluginImages.IMG_OBJS_EXCEPTION);
+		ASTRewriteCorrectionProposal proposal= new ASTRewriteCorrectionProposal(label, context.getCompilationUnit(), rewrite, 1, image);
+		proposal.ensureNoModifications();
+		resultingCollections.add(proposal);
+
+	}
+	
 	
 	
 
