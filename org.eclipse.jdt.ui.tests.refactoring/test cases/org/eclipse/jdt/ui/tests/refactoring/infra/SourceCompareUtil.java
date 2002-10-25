3f1a6868fc5de3a4b078a3971c0d0ee803ecc8c9
Adam Kiezun <akiezun>
2002-10-25 17:12:32 +0000

    new util

---
+++ org.eclipse.jdt.ui.tests.refactoring/test cases/org/eclipse/jdt/ui/tests/refactoring/infra/SourceCompareUtil.java
@@ -0,0 +1,22 @@
+package org.eclipse.jdt.ui.tests.refactoring.infra;
+
+import junit.framework.Assert;
+
+import org.eclipse.jdt.internal.corext.util.Strings;
+
+public class SourceCompareUtil extends Assert{
+
+    private SourceCompareUtil() {}
+
+	public static void compare(String actual, String expected) {
+		String[] actualCode= Strings.convertIntoLines(actual);
+		String[] expectedCode= Strings.convertIntoLines(expected);
+		if(expectedCode.length != actualCode.length){
+			assertEquals("Different number of lines", expected, actual);
+			return;
+		}
+		for (int i= 0; i < expectedCode.length; i++) {
+			assertEquals("Difference in line " + (i+1) , expectedCode[i], actualCode[i]);
+		}
+	}
+}
