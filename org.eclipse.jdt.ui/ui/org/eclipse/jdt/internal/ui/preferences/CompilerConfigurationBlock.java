fdf03a5407ea9aec9c1e574d6192a335b2d61ee9
Martin Aeschlimann <maeschli>
2002-10-30 14:14:37 +0000

    NPE fix

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/preferences/CompilerConfigurationBlock.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/preferences/CompilerConfigurationBlock.java
@@ -243,7 +243,7 @@ public class CompilerConfigurationBlock {
 		fContext= context;
 		fProject= project;
 		
-		fWorkingValues= getOptions();
+		fWorkingValues= getOptions(true);
 		fWorkingValues.put(INTR_DEFAULT_COMPLIANCE, getCurrentCompliance());
 		
 		fCheckBoxes= new ArrayList();
@@ -304,9 +304,9 @@ public class CompilerConfigurationBlock {
 		fTaskTagsStatus= new StatusInfo();		
 	}
 	
-	private Map getOptions() {
+	private Map getOptions(boolean inheritJavaCoreOptions) {
 		if (fProject != null) {
-			return fProject.getOptions(true);
+			return fProject.getOptions(inheritJavaCoreOptions);
 		} else {
 			return JavaCore.getOptions();
 		}	
@@ -894,7 +894,7 @@ public class CompilerConfigurationBlock {
 		packTodoTasks();
 
 		String[] allKeys= getAllKeys();
-		Map actualOptions= getOptions();
+		Map actualOptions= getOptions(false);
 		
 		// preserve other options
 		boolean hasChanges= false;
