4a265aa3d0692dfa0b03d33c394c81900ef9529c
Adam Kiezun <akiezun>
2002-11-21 11:46:58 +0000

    *** empty log message ***

---
+++ org.eclipse.jdt.ui.tests.refactoring/resources/ExtractInterface/testConstant86/out/I.java
@@ -0,0 +1,7 @@
+package p;
+/** typecomment template*/
+interface I {
+	public static final int X= 0; /*constant comment*/
+
+	public static final int Y= 0; //constant line comment
+}
--- org.eclipse.jdt.ui.tests.refactoring/resources/ExtractInterface/testInterface2/out/I.java
+++ org.eclipse.jdt.ui.tests.refactoring/resources/ExtractInterface/testInterface2/out/I.java
@@ -2,6 +2,8 @@ package p;
 /** typecomment template*/
 interface I {
 	public static final int i= 0;
+
 	int j= 1;
+
 	void m();
 }
