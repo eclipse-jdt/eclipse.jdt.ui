451f4acc322f4117d4410f6447295e5d236d255d
Adam Kiezun <akiezun>
2002-11-22 14:26:52 +0000

    removed unused methods and constants

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/preferences/RefactoringPreferencePage.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/preferences/RefactoringPreferencePage.java
@@ -10,7 +10,6 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.jface.preference.BooleanFieldEditor;
 import org.eclipse.jface.preference.FieldEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
-import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.preference.RadioGroupFieldEditor;
 
 import org.eclipse.ui.IWorkbench;
@@ -19,10 +18,10 @@ import org.eclipse.ui.help.WorkbenchHelp;
 
 import org.eclipse.jdt.ui.PreferenceConstants;
 
-import org.eclipse.jdt.internal.corext.refactoring.base.RefactoringStatus;
 import org.eclipse.jdt.internal.ui.IJavaHelpContextIds;
 import org.eclipse.jdt.internal.ui.JavaPlugin;
 import org.eclipse.jdt.internal.ui.refactoring.RefactoringMessages;
+import org.eclipse.jdt.internal.ui.refactoring.RefactoringPreferences;
 
 public class RefactoringPreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
 
@@ -32,11 +31,6 @@ public class RefactoringPreferencePage extends FieldEditorPreferencePage impleme
 	private static final String INFO_SEVERITY= PreferenceConstants.REFACTOR_INFO_SEVERITY;
 	private static final String OK_SEVERITY= PreferenceConstants.REFACTOR_OK_SEVERITY;
 		
-	private static final String PREF_ERROR_PAGE_SEVERITY_THRESHOLD= PreferenceConstants.REFACTOR_ERROR_PAGE_SEVERITY_THRESHOLD;
-	private static final String PREF_SAVE_ALL_EDITORS= PreferenceConstants.REFACTOR_SAVE_ALL_EDITORS;
-
-	//public static final String PREF_JAVA_STYLE_GUIDE_CONFORM= "Refactoring.javaStyleGuideConform"; //$NON-NLS-1$
-
 	public RefactoringPreferencePage() {
 		super(GRID);
 		setPreferenceStore(JavaPlugin.getDefault().getPreferenceStore());
@@ -55,7 +49,7 @@ public class RefactoringPreferencePage extends FieldEditorPreferencePage impleme
 	
 	private FieldEditor createSeverityLevelField(Composite parent){
 			RadioGroupFieldEditor editor= new RadioGroupFieldEditor(
-			PREF_ERROR_PAGE_SEVERITY_THRESHOLD,
+			RefactoringPreferences.PREF_ERROR_PAGE_SEVERITY_THRESHOLD,
 			RefactoringMessages.getString("RefactoringPreferencePage.show_error_page"), //$NON-NLS-1$
 			1,
 			new String[] [] {
@@ -71,7 +65,7 @@ public class RefactoringPreferencePage extends FieldEditorPreferencePage impleme
 	
 	private FieldEditor createSaveAllField(Composite parent){
 		BooleanFieldEditor editor= new BooleanFieldEditor(
-		PREF_SAVE_ALL_EDITORS,
+		RefactoringPreferences.PREF_SAVE_ALL_EDITORS,
 			RefactoringMessages.getString("RefactoringPreferencePage.auto_save"), //$NON-NLS-1$
 			BooleanFieldEditor.DEFAULT,
 			parent);
@@ -80,42 +74,6 @@ public class RefactoringPreferencePage extends FieldEditorPreferencePage impleme
 	
 	public void init(IWorkbench workbench) {
 	}
-	
-	/**
-	 * @deprecated Inline to avoid reference to preference page
-	 */
-	static public int getCheckPassedSeverity() {	
-		String value= JavaPlugin.getDefault().getPreferenceStore().getString(PREF_ERROR_PAGE_SEVERITY_THRESHOLD);
-		int threshold= RefactoringStatus.ERROR;
-		try {
-			threshold= Integer.valueOf(value).intValue() - 1;
-		} catch (NumberFormatException e) {
-		}
-		return threshold;
-	}
-	
-	/*
-	static public boolean getCodeIsJavaStyleGuideConform() {
-		IPreferenceStore store= JavaPlugin.getDefault().getPreferenceStore();
-		return store.getBoolean(PREF_JAVA_STYLE_GUIDE_CONFORM);
-	}
-	*/
-	
-	/**
-	 * @deprecated Inline to avoid reference to preference page
-	 */	
-	static public boolean getSaveAllEditors() {
-		IPreferenceStore store= JavaPlugin.getDefault().getPreferenceStore();
-		return store.getBoolean(PREF_SAVE_ALL_EDITORS);
-	}
-
-	/**
-	 * @deprecated Inline to avoid reference to preference page
-	 */	
-	static public void setSaveAllEditors(boolean value) {
-		IPreferenceStore store= JavaPlugin.getDefault().getPreferenceStore();
-		store.setValue(PREF_SAVE_ALL_EDITORS, value);
-	}	
 	
 	public boolean performOk() {
 		JavaPlugin.getDefault().savePluginPreferences();
