c640fd2ae96dbb5ecd62cd7dfe171d8e38d0b939
Martin Aeschlimann <maeschli>
2002-11-25 16:52:34 +0000

    removed reference to OrganizeImportsPreferencePage

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/java/JavaTypeCompletionProposal.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/java/JavaTypeCompletionProposal.java
@@ -10,19 +10,22 @@
  ******************************************************************************/
 package org.eclipse.jdt.internal.ui.text.java;
 
-import org.eclipse.swt.graphics.Image;
-
 import org.eclipse.core.runtime.CoreException;
 
+import org.eclipse.swt.graphics.Image;
+
+import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
 
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IType;
 
+import org.eclipse.jdt.ui.PreferenceConstants;
+
 import org.eclipse.jdt.internal.corext.codemanipulation.ImportsStructure;
 import org.eclipse.jdt.internal.ui.JavaPlugin;
-import org.eclipse.jdt.internal.ui.preferences.ImportOrganizePreferencePage;
+import org.eclipse.jdt.internal.ui.preferences.JavaPreferencesSettings;
 
 /**
  * If passed compilation unit is not null, the replacement string will be seen as a qualified type name.
@@ -98,8 +101,9 @@ public class JavaTypeCompletionProposal extends JavaCompletionProposal {
 			ImportsStructure impStructure= null;
 			
 			if (fCompilationUnit != null) {
-				String[] prefOrder= ImportOrganizePreferencePage.getImportOrderPreference();
-				int threshold= ImportOrganizePreferencePage.getImportNumberThreshold();					
+				IPreferenceStore store= PreferenceConstants.getPreferenceStore();
+				String[] prefOrder= JavaPreferencesSettings.getImportOrderPreference(store);
+				int threshold= JavaPreferencesSettings.getImportNumberThreshold(store);					
 				impStructure= new ImportsStructure(fCompilationUnit, prefOrder, threshold, true);
 			}
 			
