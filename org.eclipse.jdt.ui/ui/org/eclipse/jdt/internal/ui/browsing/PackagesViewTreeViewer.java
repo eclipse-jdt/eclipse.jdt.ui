6f2c224deeb4c0238604871fbedb83a4567f6bbc
Dani Megert <dmegert>
2002-11-27 16:04:18 +0000

    First cut of logical packages

---
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/browsing/PackagesViewTreeViewer.java
@@ -0,0 +1,96 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2002 International Business Machines Corp. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Common Public License v0.5
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v05.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial implementation
+ ******************************************************************************/
+package org.eclipse.jdt.internal.ui.browsing;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Widget;
+
+import org.eclipse.jdt.core.IPackageFragment;
+import org.eclipse.jdt.core.JavaModelException;
+
+import org.eclipse.jdt.internal.ui.JavaPlugin;
+import org.eclipse.jdt.internal.ui.viewsupport.ProblemTreeViewer;
+	
+/**
+ * Special problem tree viewer to handle logical packages.
+ */
+public class PackagesViewTreeViewer extends ProblemTreeViewer {
+
+	public PackagesViewTreeViewer(Composite parent, int style) {
+		super(parent, style);
+	}
+
+	/*
+	 * @see org.eclipse.jface.viewers.StructuredViewer#mapElement(java.lang.Object, org.eclipse.swt.widgets.Widget)
+	 */
+	public void mapElement(Object element, Widget item) {
+		if (element instanceof LogicalPackage) {
+			LogicalPackage cp= (LogicalPackage) element;
+			IPackageFragment[] fragments= cp.getFragments();
+			for (int i= 0; i < fragments.length; i++) {
+				IPackageFragment fragment= fragments[i];
+				super.mapElement(fragment, item);
+			}
+		}
+		super.mapElement(element, item);		
+	}
+
+	/*
+	 * @see org.eclipse.jface.viewers.StructuredViewer#unmapElement(java.lang.Object, org.eclipse.swt.widgets.Widget)
+	 */
+	public void unmapElement(Object element, Widget item) {
+		if (element instanceof LogicalPackage) {
+			LogicalPackage cp= (LogicalPackage) element;
+			IPackageFragment[] fragments= cp.getFragments();
+			for (int i= 0; i < fragments.length; i++) {
+				IPackageFragment fragment= fragments[i];
+				super.unmapElement(fragment, item);
+			}	
+		}
+		super.unmapElement(element, item);
+	}
+	
+	/*
+	 * @see org.eclipse.jface.viewers.StructuredViewer#getFilteredChildren(java.lang.Object)
+	 */
+	protected Object[] getFilteredChildren(Object parent) {
+		List list= new ArrayList();
+		Object[] result= getRawChildren(parent);
+		if (result != null)	{
+			Object[] toBeFiltered= new Object[1];
+			for (int i= 0; i < result.length; i++) {
+				Object object= result[i];
+				toBeFiltered[0]= object;
+				if (isEssential(object) || filter(toBeFiltered).length == 1)
+					list.add(object);
+			}
+		}
+		return list.toArray();
+	}
+	
+	private boolean isEssential(Object object) {
+		if(object instanceof LogicalPackage)
+			return true;
+		try {
+			if (object instanceof IPackageFragment) {
+				IPackageFragment fragment= (IPackageFragment) object;
+				return !fragment.isDefaultPackage() && fragment.hasSubpackages();
+			}
+		} catch (JavaModelException e) {
+			JavaPlugin.log(e);
+		}
+		
+		return false;
+	}
+}
