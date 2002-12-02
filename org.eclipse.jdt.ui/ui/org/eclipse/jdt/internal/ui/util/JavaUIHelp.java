9d57af1ec5fc4ae67860a63ac4a0c4adfd5a99d8
Martin Aeschlimann <maeschli>
2002-12-02 11:34:11 +0000

    avoid loading of help plugin

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/util/JavaUIHelp.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/util/JavaUIHelp.java
@@ -4,44 +4,19 @@
  */
 package org.eclipse.jdt.internal.ui.util;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
-import java.text.BreakIterator;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.eclipse.core.runtime.CoreException;
 
 import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.events.HelpEvent;
 import org.eclipse.swt.events.HelpListener;
 
-import org.eclipse.jface.util.Assert;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.StructuredViewer;
 
-import org.eclipse.ui.help.WorkbenchHelp;
-
-import org.eclipse.help.IContext;
-import org.eclipse.help.IHelp;
-import org.eclipse.help.IHelpResource;
-import org.eclipse.jdt.core.IJavaElement;
-import org.eclipse.jdt.core.IMember;
-import org.eclipse.jdt.core.IPackageFragmentRoot;
-import org.eclipse.jdt.core.JavaModelException;
-
-import org.eclipse.jdt.internal.corext.javadoc.JavaDocAccess;
-import org.eclipse.jdt.internal.corext.javadoc.JavaDocLocations;
-import org.eclipse.jdt.internal.corext.javadoc.SingleCharReader;
-import org.eclipse.jdt.internal.corext.util.JavaModelUtil;
 import org.eclipse.jdt.internal.ui.JavaPlugin;
-import org.eclipse.jdt.internal.ui.JavaUIMessages;
 import org.eclipse.jdt.internal.ui.actions.SelectionConverter;
 import org.eclipse.jdt.internal.ui.javaeditor.JavaEditor;
-import org.eclipse.jdt.internal.ui.text.HTML2TextReader;
-import org.eclipse.jdt.internal.ui.viewsupport.JavaElementLabels;
 
 public class JavaUIHelp {
 
@@ -76,11 +51,6 @@ public class JavaUIHelp {
 		* 
 		*/
 		public void helpRequested(HelpEvent e) {
-			IHelp help= WorkbenchHelp.getHelpSupport();
-			if (help == null) {
-				return; // no help support installed
-			}
-
 			try {
 				Object[] selected= null;
 				if (fViewer != null) {
@@ -91,132 +61,10 @@ public class JavaUIHelp {
 				} else if (fEditor != null) {
 					selected= SelectionConverter.codeResolve(fEditor);
 				}
-
-				IContext context= help.getContext(fContextId);
-				if (selected != null && selected.length > 0) {
-					context= new JavaUIContext(context, selected);
-				}
-
-				WorkbenchHelp.displayHelp(context);
-			} catch (CoreException ex) {
-				JavaPlugin.log(ex);
+				JavadocHelpContext.displayHelp(fContextId, selected);
+			} catch (CoreException x) {
+				JavaPlugin.log(x);
 			}
-		}
-	}
-	
-	private static class JavaUIContext implements IContext {
-
-		private IHelpResource[] fHelpResources;
-		private String fText;
-
-		public JavaUIContext(IContext context, Object[] elements) throws CoreException {
-			Assert.isNotNull(elements);
-
-			List helpResources= new ArrayList();
-
-			if (context != null) {
-				IHelpResource[] resources= context.getRelatedTopics();
-				if (resources != null) {
-					for (int j= 0; j < resources.length; j++) {
-						helpResources.add(resources[j]);
-					}
-				}
-			}
-
-			for (int i= 0; i < elements.length; i++) {
-				if (elements[i] instanceof IJavaElement) {
-					IJavaElement element= (IJavaElement) elements[i];
-					if (fText == null) {
-						fText= retrieveText(element);
-					} else {
-						fText= ""; // no doc on multiple selection //$NON-NLS-1$
-					}					
-					
-					URL url= JavaDocLocations.getJavadocLocation(element, true);
-					if (url == null || doesNotExist(url)) {
-						IPackageFragmentRoot root= JavaModelUtil.getPackageFragmentRoot(element);
-						if (root != null) {
-							url= JavaDocLocations.getJavadocBaseLocation(element);
-							if (root.getKind() == IPackageFragmentRoot.K_SOURCE) {
-								element= element.getJavaProject();
-							} else {
-								element= root;
-							}
-							url= JavaDocLocations.getJavadocLocation(element, false);
-						}
-					}
-					if (url != null) {
-						IHelpResource javaResource= new JavaUIHelpResource(element, url.toExternalForm());
-						helpResources.add(javaResource);
-					}
-				}
-			}
-			fHelpResources= (IHelpResource[]) helpResources.toArray(new IHelpResource[helpResources.size()]);
-			if (fText == null || fText.length() == 0) {
-				if (context != null) {
-					fText= context.getText();
-				}
-			}
-			if (fText != null && fText.length() == 0) {
-				fText= null; // see 14207 
-			}
-			
-		}
-
-		private boolean doesNotExist(URL url) {
-			if (url.getProtocol().equals("file")) { //$NON-NLS-1$
-				File file= new File(url.getFile());
-				return !file.exists();
-			}
-			return false;
-		}
-
-		private String retrieveText(IJavaElement elem) throws JavaModelException {
-			if (elem instanceof IMember) {
-				try {
-					SingleCharReader reader= JavaDocAccess.getJavaDoc((IMember) elem, true);
-					if (reader != null) {
-						HTML2TextReader htmlReader= new HTML2TextReader(reader, null);
-						String str= htmlReader.getString();
-
-						BreakIterator breakIterator= BreakIterator.getSentenceInstance();
-						breakIterator.setText(str);
-						return str.substring(0, breakIterator.next());
-					}
-				} catch (IOException e) {
-					JavaPlugin.log(e); // ignore
-				}
-			}
-			return ""; //$NON-NLS-1$
-		}
-
-		public IHelpResource[] getRelatedTopics() {
-			return fHelpResources;
-		}
-
-		public String getText() {
-			return fText;
-		}
-
-	}
-
-	private static class JavaUIHelpResource implements IHelpResource {
-
-		private IJavaElement fElement;
-		private String fUrl;
-
-		public JavaUIHelpResource(IJavaElement element, String url) {
-			fElement= element;
-			fUrl= url;
-		}
-
-		public String getHref() {
-			return fUrl;
-		}
-
-		public String getLabel() {
-			String label= JavaElementLabels.getTextLabel(fElement, JavaElementLabels.ALL_DEFAULT);
-			return JavaUIMessages.getFormattedString("JavaUIHelp.link.label", label); //$NON-NLS-1$
 		}
 	}
 
