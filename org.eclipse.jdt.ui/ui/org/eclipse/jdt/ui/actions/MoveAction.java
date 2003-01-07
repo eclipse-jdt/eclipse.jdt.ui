32fcc631804f6bdab7dfdc13b4e512973b67ceca
Adam Kiezun <akiezun>
2003-01-07 14:28:14 +0000

    add support to move instance method

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/ui/actions/MoveAction.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/ui/actions/MoveAction.java
@@ -10,18 +10,35 @@
  ******************************************************************************/
 package org.eclipse.jdt.ui.actions;
 
+import org.eclipse.core.runtime.NullProgressMonitor;
+
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
 
 import org.eclipse.ui.IWorkbenchSite;
 import org.eclipse.ui.help.WorkbenchHelp;
 
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IMember;
+import org.eclipse.jdt.core.JavaModelException;
+
 import org.eclipse.jdt.internal.ui.IJavaHelpContextIds;
 
+import org.eclipse.jdt.internal.ui.actions.SelectionConverter;
 import org.eclipse.jdt.internal.ui.javaeditor.CompilationUnitEditor;
+import org.eclipse.jdt.internal.ui.preferences.JavaPreferencesSettings;
 import org.eclipse.jdt.internal.ui.refactoring.RefactoringMessages;
-import org.eclipse.jdt.internal.ui.refactoring.actions.MoveMembersAction;
+import org.eclipse.jdt.internal.ui.refactoring.actions.MoveInstanceMethodAction;
+import org.eclipse.jdt.internal.ui.refactoring.actions.MoveStaticMembersAction;
 import org.eclipse.jdt.internal.ui.reorg.JdtMoveAction;
+
+import org.eclipse.jdt.internal.corext.Assert;
+import org.eclipse.jdt.internal.corext.refactoring.structure.MoveInstanceMethodRefactoring;
+import org.eclipse.jdt.internal.corext.refactoring.structure.MoveStaticMembersRefactoring;
 
 /**
  * This action moves Java elements to a new location. The action prompts
@@ -39,7 +56,9 @@ import org.eclipse.jdt.internal.ui.reorg.JdtMoveAction;
  */
 public class MoveAction extends SelectionDispatchAction{
 	
-	private SelectionDispatchAction fMoveMembersAction;
+	private CompilationUnitEditor fEditor;
+	private SelectionDispatchAction fMoveInstanceMethodAction;
+	private SelectionDispatchAction fMoveStaticMembersAction;
 	private SelectionDispatchAction fJdtMoveAction;
 	
 	/**
@@ -52,7 +71,8 @@ public class MoveAction extends SelectionDispatchAction{
 	public MoveAction(IWorkbenchSite site) {
 		super(site);
 		setText(RefactoringMessages.getString("MoveAction.text")); //$NON-NLS-1$
-		fMoveMembersAction= new MoveMembersAction(site);
+		fMoveStaticMembersAction= new MoveStaticMembersAction(site);
+		fMoveInstanceMethodAction= new MoveInstanceMethodAction(site);
 		fJdtMoveAction= new JdtMoveAction(site);
 		WorkbenchHelp.setHelp(this, IJavaHelpContextIds.MOVE_ACTION);
 	}
@@ -62,8 +82,10 @@ public class MoveAction extends SelectionDispatchAction{
 	 */
 	public MoveAction(CompilationUnitEditor editor) {
 		super(editor.getEditorSite());
+		fEditor= editor;
 		setText(RefactoringMessages.getString("MoveAction.text")); //$NON-NLS-1$
-		fMoveMembersAction= new MoveMembersAction(editor);
+		fMoveStaticMembersAction= new MoveStaticMembersAction(editor);
+		fMoveInstanceMethodAction= new MoveInstanceMethodAction(editor);
 		WorkbenchHelp.setHelp(this, IJavaHelpContextIds.MOVE_ACTION);
 	}	
 
@@ -71,36 +93,111 @@ public class MoveAction extends SelectionDispatchAction{
 	 * @see ISelectionChangedListener#selectionChanged(SelectionChangedEvent)
 	 */
 	public void selectionChanged(SelectionChangedEvent event) {
-		fMoveMembersAction.selectionChanged(event);
+		fMoveStaticMembersAction.selectionChanged(event);
+		fMoveInstanceMethodAction.selectionChanged(event);
 		if (fJdtMoveAction != null)
 			fJdtMoveAction.selectionChanged(event);
 		setEnabled(computeEnableState());	
 	}
 
 	/*
-	 * @see IAction#run()
+	 * @see org.eclipse.jdt.ui.actions.SelectionDispatchAction#run(org.eclipse.jface.viewers.IStructuredSelection)
 	 */
-	public void run() {
+	protected void run(IStructuredSelection selection) {
+		assertThatExactlyOneIsEnabled();
+		
 		if (fJdtMoveAction != null && fJdtMoveAction.isEnabled())
 			fJdtMoveAction.run();
-		else if (fMoveMembersAction.isEnabled())	
-			fMoveMembersAction.run();
+		else if (fMoveInstanceMethodAction.isEnabled()) 
+			fMoveInstanceMethodAction.run();	
+		else if (fMoveStaticMembersAction.isEnabled())	
+			fMoveStaticMembersAction.run();
+	}
+
+	private void assertThatExactlyOneIsEnabled() {
+		Assert.isTrue(! fMoveInstanceMethodAction.isEnabled() || ! fMoveStaticMembersAction.isEnabled());
+		if (fJdtMoveAction != null){
+			Assert.isTrue(! fJdtMoveAction.isEnabled() || ! fMoveInstanceMethodAction.isEnabled());
+			Assert.isTrue(! fJdtMoveAction.isEnabled() || ! fMoveStaticMembersAction.isEnabled());
+			Assert.isTrue(fJdtMoveAction.isEnabled() || fMoveInstanceMethodAction.isEnabled() || fMoveStaticMembersAction.isEnabled());
+		} else
+			Assert.isTrue(fMoveInstanceMethodAction.isEnabled() || fMoveStaticMembersAction.isEnabled());
+	}
+
+	/*
+	 * @see org.eclipse.jdt.ui.actions.SelectionDispatchAction#run(org.eclipse.jface.text.ITextSelection)
+	 */
+	protected void run(ITextSelection selection) {
+		if (fJdtMoveAction != null && fJdtMoveAction.isEnabled()){
+			fJdtMoveAction.run();
+			return;
+		}
+		if (fMoveInstanceMethodAction.isEnabled() && tryMoveInstanceMethod(selection))
+			return;
+			
+		if (fMoveStaticMembersAction.isEnabled() && tryMoveStaticMembers(selection))
+			return;
+	
+		MessageDialog.openInformation(getShell(), "Move", "Select a static method, a static field or an instance method that can be moved to a component (parameter or field).");
+	}
+	
+	private boolean tryMoveStaticMembers(ITextSelection selection) {
+		try {
+			IJavaElement[] elements= SelectionConverter.codeResolveHandled(fEditor, getShell(),  RefactoringMessages.getString("OpenRefactoringWizardAction.refactoring"));  //$NON-NLS-1$
+			if (elements.length != 1)
+				return false;
+			
+			if (! (elements[0] instanceof IMember))
+				return false;
+			
+			MoveStaticMembersRefactoring refactoring= new MoveStaticMembersRefactoring(new IMember[]{(IMember)elements[0]}, JavaPreferencesSettings.getCodeGenerationSettings());
+			if (! refactoring.checkPreactivation().isOK())
+				return false;
+			fMoveStaticMembersAction.run(selection);
+			return true;
+		} catch (JavaModelException e) {
+			return false;
+		}
+	}
+	
+	private boolean tryMoveInstanceMethod(ITextSelection selection) {
+		try {
+			MoveInstanceMethodRefactoring refactoring= MoveInstanceMethodRefactoring.create(
+				getCompilationUnitForTextSelection(), selection.getOffset(), selection.getLength(),
+				JavaPreferencesSettings.getCodeGenerationSettings());
+			if (refactoring == null)
+				return false;
+			if (refactoring.checkActivation(new NullProgressMonitor()).hasFatalError())
+				return false;
+			fMoveInstanceMethodAction.run(selection);	
+			return true;
+		} catch (JavaModelException e) {
+			return false;
+		}
+	}
+
+	private ICompilationUnit getCompilationUnitForTextSelection() {
+		Assert.isNotNull(fEditor);
+		return SelectionConverter.getInputAsCompilationUnit(fEditor);
 	}
 	
 	/*
 	 * @see SelectionDispatchAction#update(ISelection)
 	 */
 	public void update(ISelection selection) {
-		fMoveMembersAction.update(selection);
+		fMoveStaticMembersAction.update(selection);
+		fMoveInstanceMethodAction.update(selection);
 		if (fJdtMoveAction != null)
 			fJdtMoveAction.update(selection);
 		setEnabled(computeEnableState());
 	}
 	
 	private boolean computeEnableState(){
-		if (fJdtMoveAction == null)	
-			return fMoveMembersAction.isEnabled();
-		else
-			return fMoveMembersAction.isEnabled() || fJdtMoveAction.isEnabled();
+		if (fMoveStaticMembersAction.isEnabled() || fMoveInstanceMethodAction.isEnabled())
+			return true;
+		else if (fJdtMoveAction == null)
+			return false;
+		else  
+			return fJdtMoveAction.isEnabled();
 	}
 }
