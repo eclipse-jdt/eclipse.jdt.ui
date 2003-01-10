a3049ac10cd99e0ead63d2bfd7c099837c95fb70
Adam Kiezun <akiezun>
2003-01-10 11:29:39 +0000

    test taken from junit

---
+++ org.eclipse.jdt.ui.tests.refactoring/resources/MoveInstanceMethod/canMove/test13/out/TR.java
@@ -0,0 +1,17 @@
+package p1;
+
+public class TR {
+	protected void run(final TC test) {
+		test.run(this);
+	}
+
+	private void runProtected(TC test, P p) {
+	}
+
+	private void endTest(TC test) {
+	}
+
+	private void startTest(TC test) {
+	}
+
+}
