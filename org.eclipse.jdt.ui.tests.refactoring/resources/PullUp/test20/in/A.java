6c48b567eb8c8f0ca1fbea884470f9a4cb29a968
Adam Kiezun <akiezun>
2003-01-13 15:50:59 +0000

    *** empty log message ***

---
+++ org.eclipse.jdt.ui.tests.refactoring/resources/PullUp/test20/in/A.java
@@ -0,0 +1,12 @@
+package p;
+
+class A {
+}
+
+class C extends A {
+}
+
+class B extends C {
+	void m() {
+	}
+}
---
+++ org.eclipse.jdt.ui.tests.refactoring/resources/PullUp/test20/out/A.java
@@ -0,0 +1,12 @@
+package p;
+
+class A {
+	protected void m() {
+	}
+}
+
+class C extends A {
+}
+
+class B extends C {
+}
