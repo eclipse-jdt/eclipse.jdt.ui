e6a7dc32023533d6914f370dd62e5dad854688ce
Adam Kiezun <akiezun>
2003-01-21 17:17:41 +0000

    *** empty log message ***

---
+++ org.eclipse.jdt.ui.tests.refactoring/resources/PushDown/test1/in/A.java
@@ -0,0 +1,6 @@
+package p;
+class A{
+	public void m() {}
+}
+class B extends A{
+}
---
+++ org.eclipse.jdt.ui.tests.refactoring/resources/PushDown/test1/out/A.java
@@ -0,0 +1,8 @@
+package p;
+abstract class A{
+	public abstract void m();
+}
+class B extends A{
+
+	public void m() {}
+}
