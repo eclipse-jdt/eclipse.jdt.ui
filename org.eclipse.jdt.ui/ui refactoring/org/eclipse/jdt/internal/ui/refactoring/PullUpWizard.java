c970a6e768e36bc915e923bae822094df19d1e82
Adam Kiezun <akiezun>
2003-01-23 15:20:51 +0000

    added comment, removed 1 useless method

--- org.eclipse.jdt.ui/ui refactoring/org/eclipse/jdt/internal/ui/refactoring/PullUpWizard.java
+++ org.eclipse.jdt.ui/ui refactoring/org/eclipse/jdt/internal/ui/refactoring/PullUpWizard.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2002 International Business Machines Corp. and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0 
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ ******************************************************************************/
 package org.eclipse.jdt.internal.ui.refactoring;
 
 import org.eclipse.jdt.internal.ui.JavaPluginImages;
@@ -19,15 +29,10 @@ public class PullUpWizard extends RefactoringWizard {
 		addPage(new PullUpInputPage2());
 	}
 
-	private PullUpRefactoring getPullUpRefactoring(){
-		return (PullUpRefactoring)getRefactoring();
-	}
-	
 	/*
 	 * @see org.eclipse.jdt.internal.ui.refactoring.RefactoringWizard#hasMultiPageUserInput()
 	 */
 	public boolean hasMultiPageUserInput() {
 		return true;
 	}
-
 }
