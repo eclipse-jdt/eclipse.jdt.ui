e95fe51a3825d51eb8754f9edcf3f2f5a0301c8b
Dani Megert <dmegert>
2003-01-29 16:15:09 +0000

    Implemented feature 21896: Package view filter - would like ability to filter out non-shared projects [package explorer] [filters]

---
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/filters/NonSharedFilter.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2000, 2003 IBM Corp. and others..
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Common Public License v0.5
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v05.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ */
+package org.eclipse.jdt.internal.ui.filters;
+
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+
+import org.eclipse.team.core.RepositoryProvider;
+
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerFilter;
+
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IJavaProject;
+
+/**
+ * Filters non-shared resources and Java elements
+ * i.e. those controlled by a team provider.
+ * 
+ * @since 2.1
+ */
+public class NonSharedFilter extends ViewerFilter {
+
+	/*
+	 * @see ViewerFilter
+	 */
+	public boolean select(Viewer viewer, Object parent, Object element) {
+		if (element instanceof IResource)
+			return isSharedProject(((IResource)element).getProject());
+		else if (element instanceof IJavaElement) {
+			IJavaProject jp= ((IJavaElement)element).getJavaProject();
+			if (jp == null)
+				return false;
+			return isSharedProject(jp.getProject());
+		} else
+			return false;
+	}
+	
+	private boolean isSharedProject(IProject project) {
+		return RepositoryProvider.getProvider(project) != null;
+	}
+}
