29c90b64f25253c60a1a07869cf8b2bd60c8bc8d
Adam Kiezun <akiezun>
2003-02-05 14:13:49 +0000

    removed unused field

--- org.eclipse.jdt.ui/ui refactoring/org/eclipse/jdt/internal/ui/refactoring/actions/RenameResourceAction.java
+++ org.eclipse.jdt.ui/ui refactoring/org/eclipse/jdt/internal/ui/refactoring/actions/RenameResourceAction.java
@@ -10,30 +10,27 @@
  ******************************************************************************/
 package org.eclipse.jdt.internal.ui.refactoring.actions;
 
-import org.eclipse.jface.viewers.IStructuredSelection;
-
 import org.eclipse.core.resources.IResource;
+
+import org.eclipse.jface.viewers.IStructuredSelection;
 
 import org.eclipse.ui.IWorkbenchSite;
 
 import org.eclipse.jdt.core.JavaModelException;
 
-import org.eclipse.jdt.internal.corext.util.JavaModelUtil;
+import org.eclipse.jdt.ui.actions.SelectionDispatchAction;
 
 import org.eclipse.jdt.internal.ui.JavaPlugin;
 import org.eclipse.jdt.internal.ui.actions.ActionUtil;
-import org.eclipse.jdt.internal.ui.javaeditor.CompilationUnitEditor;
 import org.eclipse.jdt.internal.ui.refactoring.RefactoringMessages;
 import org.eclipse.jdt.internal.ui.refactoring.RefactoringSupport;
 import org.eclipse.jdt.internal.ui.reorg.IRefactoringRenameSupport;
 import org.eclipse.jdt.internal.ui.util.ExceptionHandler;
 
-import org.eclipse.jdt.ui.actions.SelectionDispatchAction;
+import org.eclipse.jdt.internal.corext.util.JavaModelUtil;
 
 public class RenameResourceAction extends SelectionDispatchAction {
 
-	private CompilationUnitEditor fEditor;
-	
 	public RenameResourceAction(IWorkbenchSite site) {
 		super(site);
 	}
