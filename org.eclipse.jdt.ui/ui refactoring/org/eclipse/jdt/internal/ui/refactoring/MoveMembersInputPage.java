4866e3033e68a23fc4b8bce32ca92d50f81a602c
Adam Kiezun <akiezun>
2003-02-10 16:53:33 +0000

    fix for 31265 Move Static Members: Dialog has focus not set

--- org.eclipse.jdt.ui/ui refactoring/org/eclipse/jdt/internal/ui/refactoring/MoveMembersInputPage.java
+++ org.eclipse.jdt.ui/ui refactoring/org/eclipse/jdt/internal/ui/refactoring/MoveMembersInputPage.java
@@ -1,6 +1,7 @@
 package org.eclipse.jdt.internal.ui.refactoring;
 
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
@@ -17,6 +18,7 @@ import org.eclipse.swt.widgets.Text;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.wizard.IWizardPage;
 
+import org.eclipse.ui.dialogs.ISelectionStatusValidator;
 import org.eclipse.ui.help.WorkbenchHelp;
 
 import org.eclipse.jdt.core.IJavaElement;
@@ -33,8 +35,10 @@ import org.eclipse.jdt.internal.ui.dialogs.TypeSelectionDialog;
 import org.eclipse.jdt.internal.ui.util.ExceptionHandler;
 import org.eclipse.jdt.internal.ui.util.SWTUtil;
 
+import org.eclipse.jdt.internal.corext.Assert;
 import org.eclipse.jdt.internal.corext.refactoring.structure.MoveStaticMembersRefactoring;
 import org.eclipse.jdt.internal.corext.util.JavaModelUtil;
+import org.eclipse.jdt.internal.corext.util.TypeInfo;
 
 class MoveMembersInputPage extends UserInputWizardPage {
 
@@ -67,34 +71,7 @@ class MoveMembersInputPage extends UserInputWizardPage {
 		label.setText(RefactoringMessages.getString("MoveMembersInputPage.destination")); //$NON-NLS-1$
 		label.setLayoutData(new GridData());
 		
-		fTextField= new Text(composite, SWT.SINGLE | SWT.BORDER);
-		fTextField.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-		fTextField.addModifyListener(new ModifyListener(){
-			public void modifyText(ModifyEvent e) {
-				IStatus status= JavaConventions.validateJavaTypeName(fTextField.getText());
-				if (status.getSeverity() == IStatus.ERROR){
-					error(status.getMessage());
-				} else {
-					try {
-						IType resolvedType= getMoveRefactoring().getDeclaringType().getJavaProject().findType(fTextField.getText());
-						if (resolvedType == null){
-							String message= RefactoringMessages.getFormattedString("MoveMembersInputPage.not_found", fTextField.getText());//$NON-NLS-1$
-							error(message);
-						}else {
-							setErrorMessage(null);
-							setPageComplete(true);
-						}	
-					} catch(JavaModelException ex) {
-						JavaPlugin.log(ex); //no ui here
-						error(RefactoringMessages.getString("MoveMembersInputPage.invalid_name")); //$NON-NLS-1$
-					}
-				}	
-			}
-			private void error(String message){
-				setErrorMessage(message);
-				setPageComplete(false);
-			}
-		});
+		addTextField(composite);
 		
 		Button button= new Button(composite, SWT.PUSH);
 		button.setText(RefactoringMessages.getString("MoveMembersInputPage.browse")); //$NON-NLS-1$
@@ -106,8 +83,41 @@ class MoveMembersInputPage extends UserInputWizardPage {
 			}
 		});
 		
+		setPageComplete(false);
 		setControl(composite);
 		WorkbenchHelp.setHelp(composite, IJavaHelpContextIds.MOVE_MEMBERS_WIZARD_PAGE);
+	}
+
+	public void addTextField(Composite composite) {
+		fTextField= new Text(composite, SWT.SINGLE | SWT.BORDER);
+		fTextField.setFocus();
+		fTextField.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		fTextField.addModifyListener(new ModifyListener(){
+			public void modifyText(ModifyEvent e) {
+				IStatus status= JavaConventions.validateJavaTypeName(fTextField.getText());
+				if (status.getSeverity() == IStatus.ERROR){
+					error(status.getMessage());
+				} else {
+					try {
+						IType resolvedType= getMoveRefactoring().getDeclaringType().getJavaProject().findType(fTextField.getText());
+						IStatus validationStatus= validateDestinationType(resolvedType, fTextField.getText());
+						if (validationStatus.isOK()){
+							setErrorMessage(null);
+							setPageComplete(true);
+						} else {
+							error(validationStatus.getMessage());
+						}
+					} catch(JavaModelException ex) {
+						JavaPlugin.log(ex); //no ui here
+						error(RefactoringMessages.getString("MoveMembersInputPage.invalid_name")); //$NON-NLS-1$
+					}
+				}	
+			}
+			private void error(String message){
+				setErrorMessage(message);
+				setPageComplete(false);
+			}
+		});
 	}
 	
 	protected boolean performFinish() {
@@ -134,12 +144,29 @@ class MoveMembersInputPage extends UserInputWizardPage {
 	
 	private void openTypeSelectionDialog(){
 		int elementKinds= IJavaSearchConstants.CLASS | IJavaSearchConstants.INTERFACE;
-		IJavaSearchScope scope= createWorkspaceSourceScope();
+		final IJavaSearchScope scope= createWorkspaceSourceScope();
 		TypeSelectionDialog dialog= new TypeSelectionDialog(getShell(), getWizard().getContainer(), elementKinds, scope);
 		dialog.setTitle(RefactoringMessages.getString("MoveMembersInputPage.choose_Type")); //$NON-NLS-1$
 		dialog.setMessage(RefactoringMessages.getString("MoveMembersInputPage.dialogMessage")); //$NON-NLS-1$
 		dialog.setUpperListLabel(RefactoringMessages.getString("MoveMembersInputPage.upperListLabel")); //$NON-NLS-1$
 		dialog.setLowerListLabel(RefactoringMessages.getString("MoveMembersInputPage.lowerListLabel")); //$NON-NLS-1$
+		dialog.setValidator(new ISelectionStatusValidator(){
+			public IStatus validate(Object[] selection) {
+				Assert.isTrue(selection.length <= 1);
+				if (selection.length == 0)
+					return new Status(IStatus.ERROR, JavaPlugin.getPluginId(), IStatus.OK, RefactoringMessages.getString("MoveMembersInputPage.Invalid_selection"), null); //$NON-NLS-1$
+				Object element= selection[0];
+				if (! (element instanceof TypeInfo))
+					return new Status(IStatus.ERROR, JavaPlugin.getPluginId(), IStatus.OK, RefactoringMessages.getString("MoveMembersInputPage.Invalid_selection"), null); //$NON-NLS-1$
+				try {
+					TypeInfo info= (TypeInfo)element;
+					return validateDestinationType(info.resolveType(scope), info.getTypeName());
+				} catch (JavaModelException e) {
+					JavaPlugin.log(e);
+					return new Status(IStatus.ERROR, JavaPlugin.getPluginId(), IStatus.OK, RefactoringMessages.getString("MoveMembersInputPage.internal_error"), null); //$NON-NLS-1$
+				}
+			}
+		});
 		dialog.setMatchEmptyString(false);
 		dialog.setFilter(createInitialFilter());
 		if (dialog.open() == Dialog.CANCEL)
@@ -153,6 +180,14 @@ class MoveMembersInputPage extends UserInputWizardPage {
 			return fTextField.getText();
 		else
 			return RefactoringMessages.getString("MoveMembersInputPage.initialFiler");	 //$NON-NLS-1$
+	}
+	
+	private static IStatus validateDestinationType(IType type, String typeName){
+		if (type == null || ! type.exists())
+			return new Status(IStatus.ERROR, JavaPlugin.getPluginId(), IStatus.OK, RefactoringMessages.getFormattedString("MoveMembersInputPage.not_found", typeName), null); //$NON-NLS-1$
+		if (type.isBinary())
+			return new Status(IStatus.ERROR, JavaPlugin.getPluginId(), IStatus.OK, RefactoringMessages.getString("MoveMembersInputPage.no_binary"), null); //$NON-NLS-1$
+		return new Status(IStatus.OK, JavaPlugin.getPluginId(), IStatus.OK, "", null); //$NON-NLS-1$
 	}
 	
 	private MoveStaticMembersRefactoring getMoveRefactoring(){
