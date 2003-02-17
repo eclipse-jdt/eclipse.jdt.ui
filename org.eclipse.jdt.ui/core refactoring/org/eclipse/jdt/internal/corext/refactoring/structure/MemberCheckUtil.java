d01cca7a39584276a3c312872e9fd5534e909ad1
Adam Kiezun <akiezun>
2003-02-17 14:43:11 +0000

    added checks for moving a type to inside of another type

--- org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/structure/MemberCheckUtil.java
+++ org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/structure/MemberCheckUtil.java
@@ -1,5 +1,10 @@
 package org.eclipse.jdt.internal.corext.refactoring.structure;
 
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.eclipse.jdt.core.IField;
 import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IMember;
@@ -24,9 +29,10 @@ class MemberCheckUtil {
 		for (int i= 0; i < members.length; i++) {
 			if (members[i].getElementType() == IJavaElement.METHOD)
 				checkMethodInType(destinationType, result, (IMethod)members[i]);
-			else 
-			if (members[i].getElementType() == IJavaElement.FIELD)
+			else if (members[i].getElementType() == IJavaElement.FIELD)
 				checkFieldInType(destinationType, result, (IField)members[i]);
+			else if (members[i].getElementType() == IJavaElement.TYPE)
+				checkTypeInType(destinationType, result, (IType)members[i]);
 		}
 		return result;	
 	}
@@ -36,13 +42,13 @@ class MemberCheckUtil {
 		IMethod found= findMethod(method, destinationTypeMethods);
 		if (found != null){
 			Context context= JavaSourceContext.create(destinationType.getCompilationUnit(), found.getSourceRange());
-			String message= RefactoringCoreMessages.getFormattedString("MemberChecksUtil.signature_exists", //$NON-NLS-1$
+			String message= RefactoringCoreMessages.getFormattedString("MemberCheckUtil.signature_exists", //$NON-NLS-1$
 					new String[]{method.getElementName(), JavaModelUtil.getFullyQualifiedName(destinationType)});
 			result.addError(message, context);
 		} else {
 			IMethod similar= Checks.findMethod(method, destinationType);
 			if (similar != null){
-				String message= RefactoringCoreMessages.getFormattedString("MemberChecksUtil.same_param_count",//$NON-NLS-1$
+				String message= RefactoringCoreMessages.getFormattedString("MemberCheckUtil.same_param_count",//$NON-NLS-1$
 						 new String[]{method.getElementName(), JavaModelUtil.getFullyQualifiedName(destinationType)});
 				Context context= JavaSourceContext.create(destinationType.getCompilationUnit(), similar.getSourceRange());
 				result.addWarning(message, context);
@@ -54,10 +60,76 @@ class MemberCheckUtil {
 		IField destinationTypeField= destinationType.getField(field.getElementName());	
 		if (! destinationTypeField.exists())
 			return;
-		String message= RefactoringCoreMessages.getFormattedString("MemberChecksUtil.field_exists", //$NON-NLS-1$
+		String message= RefactoringCoreMessages.getFormattedString("MemberCheckUtil.field_exists", //$NON-NLS-1$
 				new String[]{field.getElementName(), JavaModelUtil.getFullyQualifiedName(destinationType)});
 		Context context= JavaSourceContext.create(destinationType.getCompilationUnit(), destinationTypeField.getSourceRange());
 		result.addError(message, context);
+	}
+	
+	private static void checkTypeInType(IType destinationType, RefactoringStatus result, IType type) throws JavaModelException {
+		String typeName= type.getElementName();
+		IType destinationTypeType= destinationType.getType(typeName);
+		if (destinationTypeType.exists()){
+			String message= MessageFormat.format(RefactoringCoreMessages.getString("MemberCheckUtil.type_name_conflict0"),  //$NON-NLS-1$
+					new String[]{typeName, JavaModelUtil.getFullyQualifiedName(destinationType)});
+			Context context= JavaSourceContext.create(destinationType.getCompilationUnit(), destinationTypeType.getNameRange());
+			result.addError(message, context);
+		} else {
+			//need to check the hierarchy of enclosing and enclosed types
+			if (destinationType.getElementName().equals(typeName)){
+				String message= MessageFormat.format(RefactoringCoreMessages.getString("MemberCheckUtil.type_name_conflict1"),  //$NON-NLS-1$
+						new String[]{JavaModelUtil.getFullyQualifiedName(type)});
+				Context context= JavaSourceContext.create(destinationType.getCompilationUnit(), destinationType.getNameRange());
+				result.addError(message, context);
+			}
+			if (typeNameExistsInEnclosingTypeChain(destinationType, typeName)){
+				String message= MessageFormat.format(RefactoringCoreMessages.getString("MemberCheckUtil.type_name_conflict2"),  //$NON-NLS-1$
+						new String[]{JavaModelUtil.getFullyQualifiedName(type)});
+				Context context= JavaSourceContext.create(destinationType.getCompilationUnit(), destinationType.getNameRange());
+				result.addError(message, context);
+			}
+			checkHierarchyOfEnclosedTypes(destinationType, result, type);
+		}
+	}
+
+	private static void checkHierarchyOfEnclosedTypes(IType destinationType, RefactoringStatus result, IType type) throws JavaModelException {
+		IType[] enclosedTypes= getAllEnclosedTypes(type);
+		for (int i= 0; i < enclosedTypes.length; i++) {
+			IType enclosedType= enclosedTypes[i];
+			if (destinationType.getElementName().equals(enclosedType.getElementName())){
+				String message= MessageFormat.format(RefactoringCoreMessages.getString("MemberCheckUtil.type_name_conflict3"),  //$NON-NLS-1$
+						new String[]{JavaModelUtil.getFullyQualifiedName(enclosedType), JavaModelUtil.getFullyQualifiedName(type)});
+				Context context= JavaSourceContext.create(destinationType.getCompilationUnit(), destinationType.getNameRange());
+				result.addError(message, context);
+			}
+			if (typeNameExistsInEnclosingTypeChain(destinationType, enclosedType.getElementName())){
+				String message= MessageFormat.format(RefactoringCoreMessages.getString("MemberCheckUtil.type_name_conflict4"),  //$NON-NLS-1$
+						new String[]{JavaModelUtil.getFullyQualifiedName(enclosedType), JavaModelUtil.getFullyQualifiedName(type)});
+				Context context= JavaSourceContext.create(destinationType.getCompilationUnit(), destinationType.getNameRange());
+				result.addError(message, context);
+			}
+		}
+	}
+	
+	private static IType[] getAllEnclosedTypes(IType type) throws JavaModelException {
+		List result= new ArrayList(2);
+		IType[] directlyEnclosed= type.getTypes();
+		result.addAll(Arrays.asList(directlyEnclosed));
+		for (int i= 0; i < directlyEnclosed.length; i++) {
+			IType enclosedType= directlyEnclosed[i];
+			result.addAll(Arrays.asList(getAllEnclosedTypes(enclosedType)));
+		}
+		return (IType[]) result.toArray(new IType[result.size()]);
+	}
+
+	private static boolean typeNameExistsInEnclosingTypeChain(IType type, String typeName){
+		IType enclosing= type.getDeclaringType();
+		while (enclosing != null){
+			if (enclosing.getElementName().equals(typeName))
+				return true;
+			enclosing= enclosing.getDeclaringType();
+		}
+		return false;
 	}
 	
 	/**
