13707ad0020a23d75f64f7e7b655dcfdd801722b
Adam Kiezun <akiezun>
2003-03-05 14:26:12 +0000

    adding an optional message

--- org.eclipse.jdt.ui.tests.refactoring/test cases/org/eclipse/jdt/ui/tests/refactoring/infra/SourceCompareUtil.java
+++ org.eclipse.jdt.ui.tests.refactoring/test cases/org/eclipse/jdt/ui/tests/refactoring/infra/SourceCompareUtil.java
@@ -9,14 +9,18 @@ public class SourceCompareUtil extends Assert{
     private SourceCompareUtil() {}
 
 	public static void compare(String actual, String expected) {
+		compare("", actual, expected);
+	}
+
+	public static void compare(String message, String actual, String expected) {
 		String[] actualCode= Strings.convertIntoLines(actual);
 		String[] expectedCode= Strings.convertIntoLines(expected);
 		if(expectedCode.length != actualCode.length){
-			assertEquals("Different number of lines (" + actualCode.length + " not " + expectedCode.length+")" , expected, actual);
+			assertEquals(message + " Different number of lines (" + actualCode.length + " not " + expectedCode.length+")" , expected, actual);
 			return;
 		}
 		for (int i= 0; i < expectedCode.length; i++) {
-			assertEquals("Difference in line " + (i+1) , expectedCode[i], actualCode[i]);
+			assertEquals(message + " Difference in line " + (i+1) , expectedCode[i], actualCode[i]);
 		}
 	}
 }
