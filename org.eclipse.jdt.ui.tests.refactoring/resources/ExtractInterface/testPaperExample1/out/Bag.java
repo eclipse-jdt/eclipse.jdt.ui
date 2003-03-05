d74d81bde3e58ba4d6cfa33152b6dbb42e5e69b3
Adam Kiezun <akiezun>
2003-03-05 16:30:37 +0000

    *** empty log message ***

---
+++ org.eclipse.jdt.ui.tests.refactoring/resources/ExtractInterface/testPaperExample1/out/Bag.java
@@ -0,0 +1,10 @@
+package p;
+
+import java.util.Iterator;
+
+/** typecomment template*/
+interface Bag {
+	public abstract java.util.Iterator iterator();
+	public abstract Bag add(Comparable e);
+	public abstract Bag addAll(Bag v1);
+}
