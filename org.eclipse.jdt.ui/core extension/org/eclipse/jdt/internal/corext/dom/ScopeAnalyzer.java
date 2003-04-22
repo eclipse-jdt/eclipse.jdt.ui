02ace15eec37ecc081ac2c8c9666121fdcbab886
Martin Aeschlimann <maeschli>
2003-04-22 20:46:50 +0000

    comment added

--- org.eclipse.jdt.ui/core extension/org/eclipse/jdt/internal/corext/dom/ScopeAnalyzer.java
+++ org.eclipse.jdt.ui/core extension/org/eclipse/jdt/internal/corext/dom/ScopeAnalyzer.java
@@ -1,8 +1,12 @@
-/*
- * Created on 10.04.2003
- *
- * To change this generated comment go to 
- * Window>Preferences>Java>Code Generation>Code Template
+/*******************************************************************************
+ * Copyright (c) 2000, 2003 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
  */
 package org.eclipse.jdt.internal.corext.dom;
 
@@ -15,10 +19,7 @@ import org.eclipse.jdt.core.dom.*;
 import org.eclipse.jdt.internal.ui.text.correction.ASTResolving;
 
 /**
- * @author aes
- *
- * To change this generated comment go to 
- * Window>Preferences>Java>Code Generation>Code Template
+ * Evaluates all fields, methods and types avaiable at a given offset in a compilation unit.
  */
 public class ScopeAnalyzer {
 	
