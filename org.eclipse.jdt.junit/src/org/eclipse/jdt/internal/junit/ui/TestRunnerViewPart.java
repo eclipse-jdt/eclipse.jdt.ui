8f4e3cb5b4980a13840df9a105ef55cf68ff5c97
cvs2svn <platform-releng-dev@eclipse.org>
2003-04-28 10:48:21 +0000

    This commit was manufactured by cvs2svn to create tag 'v20030430_R2_1_m'.
    Sprout from master 2003-03-27 23:21:43 UTC droberts2 <droberts2> 'Javadoc correction'
    Cherrypick from R2_1_maintenance 2003-04-28 10:48:20 UTC Dirk Baeumer <dbaeumer> '[35851] Content of folders with illegal package name are no shown in package explorer':
        org.eclipse.jdt.junit.core/plugin.xml
        org.eclipse.jdt.junit/plugin.xml
        org.eclipse.jdt.junit/scripts/exportplugin.xml
        org.eclipse.jdt.junit/src/org/eclipse/jdt/internal/junit/ui/TestRunnerViewPart.java
        org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/code/flow/FlowAnalyzer.java
        org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/code/flow/LocalFlowInfo.java
        org.eclipse.jdt.ui/plugin.xml
        org.eclipse.jdt.ui/scripts/exportplugin.xml
        org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/JavaPlugin.java
        org.eclipse.jdt.ui/ui/org/eclipse/jdt/ui/StandardJavaElementContentProvider.java
    Delete:
        org.eclipse.jdt.ui.vcm/.classpath
        org.eclipse.jdt.ui.vcm/.cvsignore
        org.eclipse.jdt.ui.vcm/.project
        org.eclipse.jdt.ui.vcm/.vcm_meta
        org.eclipse.jdt.ui.vcm/about.html
        org.eclipse.jdt.ui.vcm/build.properties
        org.eclipse.jdt.ui.vcm/plugin.properties
        org.eclipse.jdt.ui.vcm/plugin.xml.off
        org.eclipse.jdt.ui.vcm/scripts/exportplugin.xml
        org.junit/.classpath
        org.junit/.project
        org.junit/about.html
        org.junit/build.properties
        org.junit/junit.jar
        org.junit/junitsrc.zip
        org.junit/plugin.properties
        org.junit/plugin.xml
        org.junit/scripts/exportplugin.xml

--- org.eclipse.jdt.junit/src/org/eclipse/jdt/internal/junit/ui/TestRunnerViewPart.java
+++ org.eclipse.jdt.junit/src/org/eclipse/jdt/internal/junit/ui/TestRunnerViewPart.java
@@ -538,7 +538,8 @@ public class TestRunnerViewPart extends ViewPart implements ITestRunListener2, I
 		fStackViewIcon.dispose();
 		fTestRunOKDirtyIcon.dispose();
 		fTestRunFailDirtyIcon.dispose();
-		fClipboard.dispose();
+		if (fClipboard != null)
+			fClipboard.dispose();
 		getSite().getKeyBindingService().unregisterAction(fRerunLastTestAction);
 	}
 
