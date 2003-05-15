db67b5128c27e189e08e693080a1658dd68f6b70
Adam Kiezun <akiezun>
2003-05-15 17:33:35 +0000

    add functionality to copy things to clipboard

--- org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/reorg2/CopyToClipboardAction.java
+++ org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/reorg2/CopyToClipboardAction.java
@@ -10,20 +10,36 @@
  *******************************************************************************/
 package org.eclipse.jdt.internal.corext.refactoring.reorg2;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.IPath;
 
+import org.eclipse.swt.SWTError;
+import org.eclipse.swt.dnd.Clipboard;
+import org.eclipse.swt.dnd.DND;
+import org.eclipse.swt.dnd.FileTransfer;
+import org.eclipse.swt.dnd.TextTransfer;
+import org.eclipse.swt.dnd.Transfer;
+import org.eclipse.swt.widgets.Shell;
+
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ILabelProvider;
 import org.eclipse.jface.viewers.IStructuredSelection;
 
 import org.eclipse.ui.IWorkbenchSite;
+import org.eclipse.ui.part.ResourceTransfer;
 
 import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IJavaModel;
 import org.eclipse.jdt.core.IMember;
 import org.eclipse.jdt.core.JavaModelException;
 
+import org.eclipse.jdt.ui.JavaElementLabelProvider;
 import org.eclipse.jdt.ui.actions.SelectionDispatchAction;
+
+import org.eclipse.jdt.internal.ui.util.ExceptionHandler;
 
 import org.eclipse.jdt.internal.corext.Assert;
 import org.eclipse.jdt.internal.corext.refactoring.util.JavaElementUtil;
@@ -31,8 +47,14 @@ import org.eclipse.jdt.internal.corext.refactoring.util.JavaElementUtil;
 
 public class CopyToClipboardAction extends SelectionDispatchAction{
 
-	public CopyToClipboardAction(IWorkbenchSite site) {
+	private final Clipboard fClipboard;
+	private SelectionDispatchAction fPasteAction;//may be null
+
+	public CopyToClipboardAction(IWorkbenchSite site, Clipboard clipboard, SelectionDispatchAction pasteAction) {
 		super(site);
+		Assert.isNotNull(clipboard);
+		fClipboard= clipboard;
+		fPasteAction= pasteAction;
 	}
 
 	/* (non-Javadoc)
@@ -57,18 +79,143 @@ public class CopyToClipboardAction extends SelectionDispatchAction{
 	 * @see org.eclipse.jdt.ui.actions.SelectionDispatchAction#run(org.eclipse.jface.viewers.IStructuredSelection)
 	 */
 	protected void run(IStructuredSelection selection) {
-		// TODO implement me
-		super.run(selection);
+		try {
+			List elements= selection.toList();
+			IResource[] resources= ReorgUtils2.getResources(elements);
+			IJavaElement[] javaElements= ReorgUtils2.getJavaElements(elements);
+			if (elements.size() == resources.length + javaElements.length && canEnable(resources, javaElements)) 
+				doRun(resources, javaElements);
+		} catch (JavaModelException e) {
+			ExceptionHandler.handle(e, getShell(), "Copy To Clipboard", "Internal error. See log for details.");
+		}
+	}
+
+	private void doRun(IResource[] resources, IJavaElement[] javaElements) {
+		new ClipboardCopier(resources, javaElements, fClipboard, getShell()).copyToClipboard();
+
+		// update the enablement of the paste action
+		// workaround since the clipboard does not support callbacks				
+		if (fPasteAction != null && fPasteAction.getSelection() != null)
+			fPasteAction.update(fPasteAction.getSelection());
 	}
 
 	private boolean canEnable(IResource[] resources, IJavaElement[] javaElements) throws JavaModelException {
 		return new CopyToClipboardEnablementPolicy(resources, javaElements).canEnable();
 	}
 	
+	//----------------------------------------------------------------------------------------//
+	
+	private static class ClipboardCopier{
+		private final IResource[] fResources;
+		private final IJavaElement[] fJavaElements;
+		private final Clipboard fClipboard;
+		private final Shell fShell;
+		private final ILabelProvider fLabelProvider;
+		
+		private ClipboardCopier(IResource[] resources, IJavaElement[] javaElements, Clipboard clipboard, Shell shell){
+			Assert.isNotNull(resources);
+			Assert.isNotNull(javaElements);
+			Assert.isNotNull(clipboard);
+			Assert.isNotNull(shell);
+			fResources= resources;
+			fJavaElements= javaElements;
+			fClipboard= clipboard;
+			fShell= shell;
+			fLabelProvider= createLabelProvider();
+		}
+
+		public void copyToClipboard(){
+			IJavaElement[] javaElements= null;
+
+			//List<String>
+			List fileNameList= new ArrayList(fResources.length + fJavaElements.length);
+			StringBuffer namesBuf = new StringBuffer();
+			processResources(fileNameList, namesBuf);
+			processJavaElements(fileNameList, namesBuf);
+			String[] fileNames= (String[]) fileNameList.toArray(new String[fileNameList.size()]);
+			copyToClipboard(fResources, fileNames, namesBuf.toString(), javaElements);
+		}
+		
+		private void processJavaElements(List fileNameList, StringBuffer namesBuf) {
+			for (int i= 0; i < fJavaElements.length; i++) {
+				IJavaElement element= fJavaElements[i];
+				if (! ReorgUtils2.isInsideCompilationUnit(element))
+					addFileNameToList(fileNameList, ReorgUtils2.getResource(element));
+
+				if (fResources.length > 0 || i > 0)
+					namesBuf.append('\n');
+				namesBuf.append(getName(element));
+			}
+		}
+
+		private void processResources(List fileNameList, StringBuffer namesBuf) {
+			for (int i= 0; i < fResources.length; i++) {
+				IResource resource= fResources[i];
+				addFileNameToList(fileNameList, resource);
+
+				if (i > 0)
+					namesBuf.append('\n');
+				namesBuf.append(getName(resource));
+			}
+		}
+
+		private static void addFileNameToList(List fileNameList, IResource resource){
+			if (resource == null)
+				return;
+			IPath location = resource.getLocation();
+			// location may be null. See bug 29491.
+			if (location != null)
+				fileNameList.add(location.toOSString());			
+		}
+		
+		private void copyToClipboard(IResource[] resources, String[] fileNames, String names, IJavaElement[] javaElements){
+			try{
+				//TODO add java element transfer
+				if (fileNames.length > 0) {
+					fClipboard.setContents(
+						new Object[]{
+							resources, 
+							fileNames, 
+							names}, 
+						new Transfer[]{
+							ResourceTransfer.getInstance(), 
+							FileTransfer.getInstance(), 
+							TextTransfer.getInstance()});
+				} else {
+					fClipboard.setContents(
+						new Object[]{
+							resources, 
+							names}, 
+						new Transfer[]{
+							ResourceTransfer.getInstance(), 
+							TextTransfer.getInstance()});
+				}
+			} catch (SWTError e) {
+				if (e.code != DND.ERROR_CANNOT_SET_CLIPBOARD)
+					throw e;
+				if (MessageDialog.openQuestion(fShell, "Problem Copying to Clipboard", "There was a problem when accessing the system clipboard. Retry?"))
+					copyToClipboard(resources, fileNames, names, javaElements);
+			}
+		}
+		private static ILabelProvider createLabelProvider(){
+			return new JavaElementLabelProvider(
+				JavaElementLabelProvider.SHOW_VARIABLE
+				+ JavaElementLabelProvider.SHOW_PARAMETERS
+				+ JavaElementLabelProvider.SHOW_TYPE
+			);		
+		}
+		private String getName(IResource resource){
+			return fLabelProvider.getText(resource);
+		}
+		private String getName(IJavaElement javaElement){
+			return fLabelProvider.getText(javaElement);
+		}
+	}
+	
 	private static class CopyToClipboardEnablementPolicy implements IReorgEnablementPolicy{
 		private final IResource[] fResources;
 		private final IJavaElement[] fJavaElements;
-		CopyToClipboardEnablementPolicy(IResource[] resources, IJavaElement[] javaElements){
+		public CopyToClipboardEnablementPolicy(IResource[] resources, IJavaElement[] javaElements){
 			Assert.isNotNull(resources);
 			Assert.isNotNull(javaElements);
 			fResources= resources;
