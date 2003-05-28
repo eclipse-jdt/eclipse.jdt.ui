6b477ab43db047dc8dcae7eaeab75486365450a4
Adam Kiezun <akiezun>
2003-05-28 17:20:41 +0000

    using the generic message page

--- org.eclipse.jdt.ui/ui refactoring/org/eclipse/jdt/internal/ui/refactoring/InlineTempWizard.java
+++ org.eclipse.jdt.ui/ui refactoring/org/eclipse/jdt/internal/ui/refactoring/InlineTempWizard.java
@@ -10,14 +10,6 @@
  *******************************************************************************/
 package org.eclipse.jdt.internal.ui.refactoring;
 
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Label;
-
-import org.eclipse.jface.dialogs.Dialog;
-
 import org.eclipse.jdt.internal.ui.IJavaHelpContextIds;
 
 import org.eclipse.jdt.internal.corext.refactoring.code.InlineTempRefactoring;
@@ -36,39 +28,22 @@ public class InlineTempWizard extends RefactoringWizard {
 		return 0;
 	}
 	
-	private static class InlineTempInputPage extends UserInputWizardPage {
+	private static class InlineTempInputPage extends MessageInputPage {
 
 		public static final String PAGE_NAME= "InlineTempInputPage"; //$NON-NLS-1$
 	
 		public InlineTempInputPage() {
-			super(PAGE_NAME, true);
+			super(PAGE_NAME, true, MessageInputPage.STYLE_QUESTION);
 		}
 
-		public void createControl(Composite parent) {
-			initializeDialogUnits(parent);
-			Composite result= new Composite(parent, SWT.NONE);
-			setControl(result);
-			GridLayout layout= new GridLayout();
-			layout.marginWidth= 0; layout.marginHeight= 0;
-			result.setLayout(layout);
-			Label spacer= new Label(result, SWT.NONE);
-			GridData gd= new GridData();
-			gd.heightHint= convertHeightInCharsToPixels(1) / 2;
-			spacer.setLayoutData(gd);
-			Label label= new Label(result, SWT.CENTER | SWT.WRAP);
-			label.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		protected String getMessageString() {
 			InlineTempRefactoring refactoring= (InlineTempRefactoring)getRefactoring();
 			int occurences= refactoring.getOccurences();
-			String message;
-			if (occurences == 1) {
-				message= RefactoringMessages.getFormattedString("InlineTempInputPage.message.one",  refactoring.getTempName()); //$NON-NLS-1$
-			} else {
-				message= RefactoringMessages.getFormattedString("InlineTempInputPage.message.multi",  //$NON-NLS-1$
+			if (occurences == 1) 
+				return RefactoringMessages.getFormattedString("InlineTempInputPage.message.one",  refactoring.getTempName()); //$NON-NLS-1$
+			else
+				return RefactoringMessages.getFormattedString("InlineTempInputPage.message.multi",  //$NON-NLS-1$
 					new Object[] { new Integer(occurences),  refactoring.getTempName() });
-			}
-		
-			label.setText(message);
-			Dialog.applyDialogFont(result);
 		}
 	}
 }
