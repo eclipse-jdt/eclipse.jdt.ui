9906f48ee2e15521c159680d39a33da3c512b950
Dani Megert <dmegert>
2003-06-17 09:19:45 +0000

    Workaround for 3888

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/infoviews/AbstractInfoView.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/infoviews/AbstractInfoView.java
@@ -17,6 +17,7 @@ import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 
+import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 
@@ -46,15 +47,19 @@ import org.eclipse.jdt.internal.ui.viewsupport.JavaElementLabels;
 abstract class AbstractInfoView extends ViewPart implements ISelectionListener {
 
 
-	private static final int TITLE_LABEL_FLAGS= 0; // JavaElementLabels.DEFAULT_QUALIFIED | JavaElementLabels.ROOT_POST_QUALIFIED;
-
+	/** JavaElementLabels flags used for the title */
+	private static final int TITLE_LABEL_FLAGS= 0;
+	/** JavaElementLabels flags used for the tool tip text */
 	private static final int TOOLTIP_LABEL_FLAGS= JavaElementLabels.DEFAULT_QUALIFIED | JavaElementLabels.ROOT_POST_QUALIFIED | JavaElementLabels.APPEND_ROOT_PATH |
 			JavaElementLabels.M_PARAMETER_TYPES | JavaElementLabels.M_PARAMETER_NAMES | JavaElementLabels.M_APP_RETURNTYPE | JavaElementLabels.M_EXCEPTIONS | 
 			JavaElementLabels.F_APP_TYPE_SIGNATURE;
 
+	/** The current input */
 	protected IJavaElement fCurrentInput;
-	private IWorkbenchPart fLastActivatedPart;
 
+	/*
+	 * @see IPartListener2
+	 */
 	private IPartListener2 fPartListener= new IPartListener2() {
 		public void partVisible(IWorkbenchPartReference ref) {
 			if (ref.getId().equals(getSite().getId()))
@@ -69,7 +74,6 @@ abstract class AbstractInfoView extends ViewPart implements ISelectionListener {
 				setInputFrom(ref.getPart(false));
 		}
 		public void partActivated(IWorkbenchPartReference ref) {
-			fLastActivatedPart= ref.getPart(false);
 		}
 		public void partBroughtToTop(IWorkbenchPartReference ref) {
 		}
@@ -82,9 +86,33 @@ abstract class AbstractInfoView extends ViewPart implements ISelectionListener {
 	};
 
 
+	/**
+	 * 
+	 * @param input
+	 * @return
+	 */
 	abstract protected boolean setInput(Object input);
+
+	/**
+	 * Create the part control.
+	 * 
+ 	 * @param parent the parent control
+	 * @see IWorkbenchPart#createPartControl(org.eclipse.swt.widgets.Composite)
+	 */
 	abstract protected void internalCreatePartControl(Composite parent);
+
+	/**
+	 * Set the view's foreground color.
+	 * 
+	 * @param color the SWT color
+	 */
 	abstract protected void setForeground(Color color);
+
+	/**
+	 * Set the view's background color.
+	 * 
+	 * @param color the SWT color
+	 */
 	abstract protected void setBackground(Color color);
 
 	public final void createPartControl(Composite parent) {
@@ -117,12 +145,10 @@ abstract class AbstractInfoView extends ViewPart implements ISelectionListener {
 		if (part.equals(this))
 			return;
 			
-		if (fLastActivatedPart != null && fLastActivatedPart.equals(part)) {
-			fLastActivatedPart= null;
+		// XXX: Workaround for bug 38888: javadoc / declaration view: selection is unacceptably slow
+		if (selection instanceof ITextSelection && ((ITextSelection)selection).getLength() > 0)
 			return;
-		}
-		fLastActivatedPart= null;
-		
+
 		setInputFrom(part);
 	}
 
@@ -152,6 +178,12 @@ abstract class AbstractInfoView extends ViewPart implements ISelectionListener {
 		return je;
 	}
 
+	/**
+	 * Finds and returns the type for the given CU.
+	 * 
+	 * @param cu	the compilation unit
+	 * @return	the type with same name as the given CU or the first type in the CU 
+	 */
 	protected IType getTypeForCU(ICompilationUnit cu) {
 		
 		if (cu == null || !cu.exists())
@@ -191,6 +223,9 @@ abstract class AbstractInfoView extends ViewPart implements ISelectionListener {
 		setTitleToolTip(JavaElementLabels.getElementLabel(je, TOOLTIP_LABEL_FLAGS));  //$NON-NLS-1$//$NON-NLS-2$
 	}
 
+	/*
+	 * @see IWorkbenchPart#dispose()
+	 */
 	final public void dispose() {
 		getSite().getWorkbenchWindow().getPartService().removePartListener(fPartListener);
 		internalDispose();
