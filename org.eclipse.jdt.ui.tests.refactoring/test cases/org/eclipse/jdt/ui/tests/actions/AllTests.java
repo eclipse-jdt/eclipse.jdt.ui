3e90eaf477411765938da8a973aa36d2bed6f156
Adam Kiezun <akiezun>
2003-07-07 14:57:41 +0000

    moved stuff to reorg tests

--- org.eclipse.jdt.ui.tests.refactoring/test cases/org/eclipse/jdt/ui/tests/actions/AllTests.java
+++ org.eclipse.jdt.ui.tests.refactoring/test cases/org/eclipse/jdt/ui/tests/actions/AllTests.java
@@ -17,15 +17,8 @@ public class AllTests {
 	
 	public static Test suite ( ) {
 		TestSuite suite= new TestSuite("All Action Tests");
-		suite.addTest(DeleteSourceReferenceEditTests.suite());
 
-//      FIX ME - randomly fails on linux
-//		suite.addTest(PasteSourceReferenceActionTests.suite());
-
-		suite.addTest(DeleteSourceReferenceActionTests.suite());
 		suite.addTest(StructureSelectionActionTests.suite());
-		suite.addTest(CopyResourcesToClipboardActionTest.suite());
-		suite.addTest(PasteResourcesFromClipboardActionTest.suite());
 		suite.addTest(GoToNextPreviousMemberActionTests.suite());
 		return suite;
 	}
