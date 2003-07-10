830300df7da8a2d9253f549148652da06c37cc23
Adam Kiezun <akiezun>
2003-07-10 09:16:40 +0000

    resolve bindings for ?: and better equals

--- org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/typeconstraints/Expressions.java
+++ org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/typeconstraints/Expressions.java
@@ -11,6 +11,7 @@
 package org.eclipse.jdt.internal.corext.refactoring.typeconstraints;
 
 import org.eclipse.jdt.core.dom.Assignment;
+import org.eclipse.jdt.core.dom.ConditionalExpression;
 import org.eclipse.jdt.core.dom.Expression;
 import org.eclipse.jdt.core.dom.FieldAccess;
 import org.eclipse.jdt.core.dom.IBinding;
@@ -32,7 +33,7 @@ public final class Expressions {
 	public static boolean equalBindings(Expression expression1, Expression expression2){
 		if (expression1 == null || expression2 == null)
 			return false;
-		if (expression1 == expression2)
+		if (expression1 == expression2 || expression1.equals(expression2))
 			return true;
 		IBinding binding1= resolveBinding(expression1);
 		IBinding binding2= resolveBinding(expression2);
@@ -50,7 +51,7 @@ public final class Expressions {
 			return binding.getKey().hashCode();
 	}
 	
-	private static IBinding resolveBinding(Expression expression){
+	public static IBinding resolveBinding(Expression expression){
 		if (expression instanceof Name)
 			return ((Name)expression).resolveBinding();
 		if (expression instanceof ParenthesizedExpression)
@@ -62,9 +63,11 @@ public final class Expressions {
 		else if (expression instanceof SuperMethodInvocation)
 			return ((SuperMethodInvocation)expression).resolveMethodBinding();
 		else if (expression instanceof FieldAccess)
-			return ((FieldAccess)expression).getName().resolveBinding();
+			return ((FieldAccess)expression).resolveFieldBinding();
 		else if (expression instanceof SuperFieldAccess)
-			return ((SuperFieldAccess)expression).getName().resolveBinding();
+			return ((SuperFieldAccess)expression).resolveFieldBinding();
+		else if (expression instanceof ConditionalExpression)
+			return resolveBinding(((ConditionalExpression)expression).getThenExpression());
 		return null;
 	}
 }
