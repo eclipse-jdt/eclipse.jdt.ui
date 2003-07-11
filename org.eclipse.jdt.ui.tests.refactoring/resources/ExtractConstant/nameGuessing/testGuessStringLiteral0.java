8175d3533e098eb4499150d0123e08393cc47f44
Adam Kiezun <akiezun>
2003-07-11 17:16:57 +0000

    *** empty log message ***

---
+++ org.eclipse.jdt.ui.tests.refactoring/resources/ExtractConstant/nameGuessing/testGuessStringLiteral0.java
@@ -0,0 +1,6 @@
+package p;
+class A {
+	void foo() {
+		String s= "Foo.HashMap";//expected FOO_HASHMAP
+	}
+}
---
+++ org.eclipse.jdt.ui.tests.refactoring/resources/ExtractConstant/nameGuessing/testGuessStringLiteral1.java
@@ -0,0 +1,6 @@
+package p;
+class A {
+	void foo() {
+		String s= "Foo/Hash+Map";//expected FOO_HASH_MAP
+	}
+}
