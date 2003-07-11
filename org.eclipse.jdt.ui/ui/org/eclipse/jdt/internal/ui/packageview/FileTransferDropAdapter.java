95afe832fbe5cfb9bd507774596e8607384c065c
Adam Kiezun <akiezun>
2003-07-11 09:50:43 +0000

    35796  Drag and drop freezes Windows explorer(modal dialog problem) [dnd]

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/packageview/FileTransferDropAdapter.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/packageview/FileTransferDropAdapter.java
@@ -16,15 +16,12 @@ import org.eclipse.swt.dnd.DND;
 import org.eclipse.swt.dnd.DropTargetEvent;
 import org.eclipse.swt.dnd.FileTransfer;
 import org.eclipse.swt.dnd.Transfer;
-import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
 
-import org.eclipse.jface.dialogs.Dialog;
-import org.eclipse.jface.dialogs.IDialogConstants;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.AbstractTreeViewer;
 
 import org.eclipse.ui.actions.CopyFilesAndFoldersOperation;
-import org.eclipse.ui.dialogs.IOverwriteQuery;
 
 import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IJavaProject;
@@ -32,42 +29,19 @@ import org.eclipse.jdt.core.IPackageFragment;
 import org.eclipse.jdt.core.IPackageFragmentRoot;
 import org.eclipse.jdt.core.JavaModelException;
 
-import org.eclipse.jdt.internal.ui.JavaPlugin;
 import org.eclipse.jdt.internal.ui.dnd.JdtViewerDropAdapter;
 import org.eclipse.jdt.internal.ui.dnd.TransferDropTargetListener;
+import org.eclipse.jdt.internal.ui.util.ExceptionHandler;
 
 /**
  * Adapter to handle file drop from other applications.
  */ 
-class FileTransferDropAdapter extends JdtViewerDropAdapter implements IOverwriteQuery, TransferDropTargetListener {
+class FileTransferDropAdapter extends JdtViewerDropAdapter implements TransferDropTargetListener {
 	
 	FileTransferDropAdapter(AbstractTreeViewer viewer) {
 		super(viewer, DND.FEEDBACK_SCROLL | DND.FEEDBACK_EXPAND);
 	}
 
-	//---- IOverwriteQuery ------------------------------------------------------------
-
-	public String queryOverwrite(String file) {
-		// Fix for http://dev.eclipse.org/bugs/show_bug.cgi?id=19367
-		String[] returnCodes= {YES, ALL, NO, CANCEL};
-		int returnVal= openDialog(getViewer().getControl(), file);
-		return returnVal < 0 ? CANCEL : returnCodes[returnVal];
-	}	
-	
-	private int openDialog(final Control control, final String file) {
-		final int[] result= { Dialog.CANCEL };
-		control.getDisplay().syncExec(new Runnable() {
-			public void run() {
-				String title= PackagesMessages.getString("DropAdapter.question"); //$NON-NLS-1$
-				String msg= PackagesMessages.getFormattedString("DropAdapter.alreadyExists", file); //$NON-NLS-1$
-				String[] options= {IDialogConstants.YES_LABEL, IDialogConstants.YES_TO_ALL_LABEL, IDialogConstants.NO_LABEL, IDialogConstants.CANCEL_LABEL};
-				MessageDialog dialog= new MessageDialog(control.getShell(), title, null, msg, MessageDialog.QUESTION, options, 0);
-				result[0]= dialog.open();
-			}
-		});
-		return result[0];
-	}
-		
 	//---- TransferDropTargetListener interface ---------------------------------------
 	
 	public Transfer getTransfer() {
@@ -101,27 +75,43 @@ class FileTransferDropAdapter extends JdtViewerDropAdapter implements IOverwrite
 	}
 
 	public void drop(Object dropTarget, DropTargetEvent event) {
-		int operation= event.detail;
-		
-		event.detail= DND.DROP_NONE;
-		Object data= event.data;
-		if (data == null || operation != DND.DROP_COPY)
-			return;
-			
-		IContainer target= null;
-		if (dropTarget instanceof IContainer) {
-			target= (IContainer)dropTarget;
-		} else {
-			try {
-				target= (IContainer)((IJavaElement)dropTarget).getCorrespondingResource();	
-			} catch (JavaModelException e) {
-			}
+		try {
+			int operation= event.detail;
+
+			event.detail= DND.DROP_NONE;
+			final Object data= event.data;
+			if (data == null || !(data instanceof String[]) || operation != DND.DROP_COPY)
+				return;
+
+			final IContainer target= getActualTarget(dropTarget);
+			if (target == null)
+				return;
+
+			// Run the import operation asynchronously. 
+			// Otherwise the drag source (e.g., Windows Explorer) will be blocked 
+			// while the operation executes. Fixes bug 35796.
+			Display.getCurrent().asyncExec(new Runnable() {
+				public void run() {
+					getShell().forceActive();
+					new CopyFilesAndFoldersOperation(getShell()).copyFiles((String[]) data, target);
+					// Import always performs a copy.
+					event.detail= DND.DROP_COPY;
+				}
+			});
+		} catch (JavaModelException e) {
+			ExceptionHandler.handle(e, getShell(), "Error", "Unexpected error occurred. See log for details.");
 		}
-		if (target == null)
-			return;
-		
-		new CopyFilesAndFoldersOperation(JavaPlugin.getActiveWorkbenchShell()).copyFiles((String[])data, target);
-		// Import always performs a copy.
-		event.detail= DND.DROP_COPY;
+	}
+	
+	private IContainer getActualTarget(Object dropTarget) throws JavaModelException{
+		if (dropTarget instanceof IContainer)
+			return (IContainer)dropTarget;
+		else if (dropTarget instanceof IJavaElement)
+			return getActualTarget(((IJavaElement)dropTarget).getCorrespondingResource());	
+		return null;
+	}
+	
+	private Shell getShell() {
+		return getViewer().getControl().getShell();
 	}
 }
