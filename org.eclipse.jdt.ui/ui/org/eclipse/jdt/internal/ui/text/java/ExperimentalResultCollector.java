40c71fbc7c761a891c1c6270283b210ba3ae3dbd
Dani Megert <dmegert>
2003-07-15 11:43:19 +0000

    Fixed bug

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/java/ExperimentalResultCollector.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/java/ExperimentalResultCollector.java
@@ -82,7 +82,6 @@ public class ExperimentalResultCollector extends ResultCollector {
 		if (preferenceStore.getBoolean(PreferenceConstants.CODEASSIST_GUESS_METHOD_ARGUMENTS)) {
 			return new ParameterGuessingProposal(
 				start, end - start, original.getImage(), original.getDisplayString(), fViewer, relevance,
-				completionName,
 				name, parameterTypePackageNames, parameterTypeNames, parameterNames, 
 				fCodeAssistOffset, fCompilationUnit);
 				
