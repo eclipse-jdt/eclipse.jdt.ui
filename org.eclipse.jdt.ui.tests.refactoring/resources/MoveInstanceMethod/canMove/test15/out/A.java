9a8066eaf6250414fd2a01cce9037a8a886d67ae
Adam Kiezun <akiezun>
2003-08-13 10:23:37 +0000

    *** empty log message ***

---
+++ org.eclipse.jdt.ui.tests.refactoring/resources/MoveInstanceMethod/canMove/test15/out/A.java
@@ -0,0 +1,29 @@
+package p;
+
+public class A {
+	int fMagic;
+
+	public static void main(String[] args) {
+		Second s= new Second("Bingo");
+		A a= new A();
+		a.fMagic= 17;
+		a.print(s);
+	}
+
+	public void print(Second s) {
+		s.print(this);
+	}
+}
+
+class Second {
+	String fName;
+	public Second(String name) {
+		fName= name;
+	}
+	public String toString() {
+		return fName;
+	}
+	public void print(A a) {
+		System.out.println(this + ": " + a.fMagic);
+	}
+}
