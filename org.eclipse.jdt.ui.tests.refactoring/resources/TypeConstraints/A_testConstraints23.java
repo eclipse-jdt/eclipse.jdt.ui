f694673813ea4ac1734e092e56678e1a93dca647
Adam Kiezun <akiezun>
2003-08-15 11:43:06 +0000

    test for 41271

--- org.eclipse.jdt.ui.tests.refactoring/resources/TypeConstraints/A_testConstraints23.java
+++ org.eclipse.jdt.ui.tests.refactoring/resources/TypeConstraints/A_testConstraints23.java
@@ -1,13 +1,6 @@
 package p;
 class Test1 {
 		public static void main(String[] args) {
-				StringBuffer    buf = new StringBuffer(16);
-
-				buf.append("Args:");
-				for(int i=0; i < args.length; i++)
-						buf.append(" '")
-						   .append(args[i])
-						   .append("'");
-				System.out.println(buf.toString());
+			for(int i=0; i < args.length; );
 		}
 }
