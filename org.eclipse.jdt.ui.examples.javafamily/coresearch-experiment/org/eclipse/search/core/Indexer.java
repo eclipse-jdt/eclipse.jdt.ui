750f16e6e18797dc7f7f590729bbeec386b0dc5b
Philipe Mulet <pmulet>
2003-09-09 11:30:39 +0000

    *** empty log message ***

--- org.eclipse.jdt.ui.examples.javafamily/coresearch-experiment/org/eclipse/search/core/Indexer.java
+++ org.eclipse.jdt.ui.examples.javafamily/coresearch-experiment/org/eclipse/search/core/Indexer.java
@@ -35,7 +35,7 @@ public abstract class Indexer extends InternalIndexer {
 	 * Indexes the given document, adding the document name and the word references 
 	 * to this document to the given <code>Index</code>.
 	 */
-	public abstract void index(Document document) throws IOException;
+	public abstract void index(Document document, IPath indexPath) throws IOException;
 	
 	/**
 	 * Trigger an indexing action at earliest convenience (in background). When the corresponding action is performed, the
