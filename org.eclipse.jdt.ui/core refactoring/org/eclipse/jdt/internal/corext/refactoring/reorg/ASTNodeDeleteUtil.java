a2b22e4b200863b306e787b139add83a3b446fbb
Dirk Baeumer <dbaeumer>
2003-10-08 15:28:33 +0000

    [44421]

--- org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/reorg/ASTNodeDeleteUtil.java
+++ org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/reorg/ASTNodeDeleteUtil.java
@@ -16,6 +16,7 @@ import java.util.Set;
 
 import org.eclipse.jdt.core.IField;
 import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.CompilationUnit;
@@ -38,7 +39,7 @@ public class ASTNodeDeleteUtil {
 	}
 	
 	private static void markAsDeleted(IJavaElement element, CompilationUnit cuNode, ASTRewrite rewrite) throws JavaModelException {
-		ASTNode[] declarationNodes= getDeclarationNodesToDelete(element, cuNode);
+		ASTNode[] declarationNodes= getNodesToDelete(element, cuNode);
 		for (int i= 0; i < declarationNodes.length; i++) {
 			ASTNode node= declarationNodes[i];
 			if (node != null)
@@ -46,10 +47,18 @@ public class ASTNodeDeleteUtil {
 		}
 	}
 
-	private static ASTNode[] getDeclarationNodesToDelete(IJavaElement element, CompilationUnit cuNode) throws JavaModelException {
+	private static ASTNode[] getNodesToDelete(IJavaElement element, CompilationUnit cuNode) throws JavaModelException {
 		//fields are different because you don't delete the whole declaration but only a fragment of it
 		if (element.getElementType() == IJavaElement.FIELD)
 			return new ASTNode[] {ASTNodeSearchUtil.getFieldDeclarationFragmentNode((IField) element, cuNode)};
+		if (element.getElementType() == IJavaElement.TYPE && ((IType)element).isLocal()) {
+			ASTNode[] nodes= ASTNodeSearchUtil.getDeclarationNodes(element, cuNode);
+			if (!((IType)element).isAnonymous()) {
+				// we have to delete the TypeDeclarationStatement
+				nodes[0]= nodes[0].getParent();
+				return nodes;
+			}
+		}
 		return ASTNodeSearchUtil.getDeclarationNodes(element, cuNode);
 	}
 
