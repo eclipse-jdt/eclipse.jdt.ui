a342af46479a6237449110fbf111c47e41bdf327
teicher <teicher>
2003-12-10 17:15:12 +0000

    added isDisposed check (2)

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/link/LinkedUIControl.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/link/LinkedUIControl.java
@@ -328,7 +328,7 @@ public class LinkedUIControl {
 			Display display;
 
 			if (fAssistant == null || fCurrentTarget == null || (text= fCurrentTarget.fWidget) == null 
-					|| (display= text.getDisplay()) == null || display.isDisposed()) {
+					|| text.isDisposed() || (display= text.getDisplay()) == null || display.isDisposed()) {
 				leave(ILinkedListener.EXIT_ALL);
 			} else {
 				// Post in UI thread since the assistant popup will only get the focus after we lose it.
