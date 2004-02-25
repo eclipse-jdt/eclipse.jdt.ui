b78d5e0b83d294359c4b3ebbb9797ca088f73fb3
Martin Aeschlimann <maeschli>
2004-02-25 14:02:02 +0000

    52971 Generated delegate methods not qualified with 'this.' in field accesses.

--- org.eclipse.jdt.ui/core extension/org/eclipse/jdt/internal/corext/codemanipulation/AddDelegateMethodsOperation.java
+++ org.eclipse.jdt.ui/core extension/org/eclipse/jdt/internal/corext/codemanipulation/AddDelegateMethodsOperation.java
@@ -30,6 +30,7 @@ import org.eclipse.jdt.core.Signature;
 import org.eclipse.jdt.core.formatter.CodeFormatter;
 
 import org.eclipse.jdt.ui.CodeGeneration;
+import org.eclipse.jdt.ui.PreferenceConstants;
 
 import org.eclipse.jdt.internal.corext.util.CodeFormatterUtil;
 import org.eclipse.jdt.internal.corext.util.JavaModelUtil;
@@ -169,6 +170,9 @@ public class AddDelegateMethodsOperation implements IWorkspaceRunnable {
 			if (JdtFlags.isStatic(curr)) {
 				body.append(resolveTypeOfField(field).getElementName());
 			} else {
+				if (PreferenceConstants.getPreferenceStore().getBoolean(PreferenceConstants.CODEGEN_KEYWORD_THIS)) {
+					body.append("this."); //$NON-NLS-1$
+				}
 				body.append(field.getElementName());
 			}
 			body.append('.').append(methodName).append('(');
