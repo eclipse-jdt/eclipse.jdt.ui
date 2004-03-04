c73007ab303d198155396f0a65ec2526d764ff34
Thomas Maeder <tmaeder>
2004-03-04 16:29:39 +0000

    fix for 50649

--- org.eclipse.jdt.ui.tests/ui/org/eclipse/jdt/ui/tests/search/SearchTest.java
+++ org.eclipse.jdt.ui.tests/ui/org/eclipse/jdt/ui/tests/search/SearchTest.java
@@ -13,11 +13,39 @@ package org.eclipse.jdt.ui.tests.search;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+import org.eclipse.search.ui.NewSearchUI;
+
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IMethod;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.search.IJavaSearchConstants;
+
+import org.eclipse.jdt.internal.ui.search.JavaSearchQuery;
+import org.eclipse.jdt.internal.ui.search.JavaSearchResult;
+import org.eclipse.jdt.internal.ui.search.ReferenceScopeFactory;
+
 public class SearchTest {
 	public static Test suite() {
 		TestSuite suite= new TestSuite("Java Search Tests"); //$NON-NLS-1$
 		//suite.addTestSuite(WorkspaceScopeTest.class);
 		suite.addTest(WorkspaceReferenceTest.allTests());
 		return suite;
+	}
+
+	static int countMethodRefs(String TypeName, String methodName, String[] parameterTypes) throws JavaModelException {
+		JavaSearchQuery query= runMethodRefQuery(TypeName, methodName, parameterTypes);
+		JavaSearchResult result= (JavaSearchResult) query.getSearchResult();
+		return result.getMatchCount();
+	}
+
+	static JavaSearchQuery runMethodRefQuery(String TypeName, String methodName, String[] parameterTypes) throws JavaModelException {
+		IJavaProject p= JUnitSourceSetup.getProject();
+		IType type= p.findType(TypeName);
+		IMethod method= type.getMethod(methodName, parameterTypes);
+		NewSearchUI.activateSearchResultView();
+		JavaSearchQuery query= new JavaSearchQuery(method, IJavaSearchConstants.REFERENCES, ReferenceScopeFactory.create(method), "workspace scope");
+		NewSearchUI.runQueryInForeground(null, query);
+		return query;
 	}
 }
--- org.eclipse.jdt.ui.tests/ui/org/eclipse/jdt/ui/tests/search/WorkspaceReferenceTest.java
+++ org.eclipse.jdt.ui.tests/ui/org/eclipse/jdt/ui/tests/search/WorkspaceReferenceTest.java
@@ -14,48 +14,27 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-import org.eclipse.search.ui.NewSearchUI;
-
-import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.core.IMethod;
-import org.eclipse.jdt.core.IType;
-import org.eclipse.jdt.core.search.IJavaSearchConstants;
-
-import org.eclipse.jdt.internal.ui.search.JavaSearchQuery;
-import org.eclipse.jdt.internal.ui.search.JavaSearchResult;
-import org.eclipse.jdt.internal.ui.search.ReferenceScopeFactory;
-
 /**
  */
 public class WorkspaceReferenceTest extends TestCase {
 
-		public static Test allTests() {
-			return new JUnitSourceSetup(new TestSuite(WorkspaceReferenceTest.class));
-		};
+	public static Test allTests() {
+		return new JUnitSourceSetup(new TestSuite(WorkspaceReferenceTest.class));
+	};
+	
+	public static Test suite() {
+		return allTests();
+	}
 	
 	public WorkspaceReferenceTest(String name) {
 		super(name);
 	}
 	
 	public void testSimpleMethodRef() throws Exception {
-		IJavaProject p= JUnitSourceSetup.getProject();
-		IType type= p.findType("junit.framework.Test");
-		IMethod method= type.getMethod("countTestCases", new String[0]);
-		NewSearchUI.activateSearchResultView();
-		JavaSearchQuery query= new JavaSearchQuery(method, IJavaSearchConstants.REFERENCES, ReferenceScopeFactory.create(method), "workspace scope");
-		NewSearchUI.runQueryInForeground(null, query);
-		JavaSearchResult result= (JavaSearchResult) query.getSearchResult();
-		assertEquals(9, result.getMatchCount());
+		assertEquals(9, SearchTest.countMethodRefs("junit.framework.Test", "countTestCases", new String[0]));
 	}
 
 	public void testFindOverridden() throws Exception {
-		IJavaProject p= JUnitSourceSetup.getProject();
-		IType type= p.findType("junit.framework.TestCase");
-		IMethod method= type.getMethod("countTestCases", new String[0]);
-		NewSearchUI.activateSearchResultView();
-		JavaSearchQuery query= new JavaSearchQuery(method, IJavaSearchConstants.REFERENCES, ReferenceScopeFactory.create(method), "workspace scope");
-		NewSearchUI.runQueryInForeground(null, query);
-		JavaSearchResult result= (JavaSearchResult) query.getSearchResult();
-		assertEquals(6, result.getMatchCount());
+		assertEquals(6, SearchTest.countMethodRefs("junit.framework.TestCase", "countTestCases", new String[0]));
 	}
 }
