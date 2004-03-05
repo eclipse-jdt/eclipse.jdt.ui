9867b520f416b11364b87a70bfccec65d4bd3048
Thomas Maeder <tmaeder>
2004-03-05 16:33:36 +0000

    First cut of leak test support

---
+++ org.eclipse.jdt.ui.tests/ui/org/eclipse/jdt/ui/tests/LeakTestSuite.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2004 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.jdt.ui.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.eclipse.jdt.ui.tests.leaks.JavaLeakTest;
+
+
+/**
+ * Test for leaks
+ */
+public class LeakTestSuite extends TestSuite {
+
+	/**
+	 * Returns the suite.  This is required to
+	 * use the JUnit Launcher.
+	 */
+	public static Test suite() {
+		return new LeakTestSuite();
+	}
+
+	/**
+	 * Construct the test suite.
+	 */
+	public LeakTestSuite() {
+		addTest(JavaLeakTest.suite());
+	}
+	
+}
+
