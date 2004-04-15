1b4f32b90d642a4690588236fc499558a50603bd
teicher <teicher>
2004-04-15 14:08:53 +0000

    moved quick diff and smart typing preferences to proper page - new quick diff page (needs more tuning) - smart typing on typing page - mnemonic adjustments on all editor pages

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/preferences/JavaEditorPreferencePage.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/preferences/JavaEditorPreferencePage.java
@@ -1,4 +1,4 @@
- /*******************************************************************************
+/*******************************************************************************
  * Copyright (c) 2000, 2003 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Common Public License v1.0
@@ -73,6 +73,8 @@ import org.eclipse.ui.texteditor.ChainedPreferenceStore;
 import org.eclipse.ui.texteditor.ExtendedTextEditorPreferenceConstants;
 import org.eclipse.ui.texteditor.MarkerAnnotationPreferences;
 import org.eclipse.ui.texteditor.PreferencesAdapter;
+import org.eclipse.ui.texteditor.quickdiff.QuickDiff;
+import org.eclipse.ui.texteditor.quickdiff.ReferenceProviderDescriptor;
 
 import org.eclipse.jdt.core.JavaCore;
 
@@ -224,7 +226,22 @@ public class JavaEditorPreferencePage extends PreferencePage implements IWorkben
 	 */
 	private ArrayList fMasterSlaveListeners= new ArrayList();
 
-	
+	/**
+	 * List for the reference provider default. 
+	 * @since 3.0
+	 */
+	private org.eclipse.swt.widgets.List fQuickDiffProviderList;
+	/**
+	 * The reference provider default's list model.
+	 * @since 3.0 
+	 */
+	private String[][] fQuickDiffProviderListModel;
+	/**
+	 * Button controlling default setting of the selected reference provider.
+	 * @since 3.0
+	 */
+	private Button fSetDefaultButton;
+
 	/**
 	 * Creates a new preference page.
 	 */
@@ -236,6 +253,7 @@ public class JavaEditorPreferencePage extends PreferencePage implements IWorkben
 		fKeys= createOverlayStoreKeys(markerAnnotationPreferences);
 		fOverlayStore= new OverlayPreferenceStore(getPreferenceStore(), fKeys);
 		fAnnotationColorListModel= createAnnotationTypeListModel(markerAnnotationPreferences);
+		fQuickDiffProviderListModel= createQuickDiffReferenceListModel();
 	}
 	
 	private OverlayPreferenceStore.OverlayKey[] createOverlayStoreKeys(MarkerAnnotationPreferences preferences) {
@@ -368,6 +386,17 @@ public class JavaEditorPreferencePage extends PreferencePage implements IWorkben
 		overlayKeys.add(new OverlayPreferenceStore.OverlayKey(OverlayPreferenceStore.BOOLEAN, PreferenceConstants.EDITOR_BROWSER_LIKE_LINKS));
 		overlayKeys.add(new OverlayPreferenceStore.OverlayKey(OverlayPreferenceStore.STRING, PreferenceConstants.EDITOR_BROWSER_LIKE_LINKS_KEY_MODIFIER));
 		overlayKeys.add(new OverlayPreferenceStore.OverlayKey(OverlayPreferenceStore.STRING, PreferenceConstants.EDITOR_BROWSER_LIKE_LINKS_KEY_MODIFIER_MASK));
+		
+		overlayKeys.add(new OverlayPreferenceStore.OverlayKey(OverlayPreferenceStore.BOOLEAN, PreferenceConstants.EDITOR_SMART_SEMICOLON));
+		overlayKeys.add(new OverlayPreferenceStore.OverlayKey(OverlayPreferenceStore.BOOLEAN, PreferenceConstants.EDITOR_SMART_TAB));
+		overlayKeys.add(new OverlayPreferenceStore.OverlayKey(OverlayPreferenceStore.BOOLEAN, PreferenceConstants.EDITOR_SMART_OPENING_BRACE));
+		
+		overlayKeys.add(new OverlayPreferenceStore.OverlayKey(OverlayPreferenceStore.BOOLEAN, PreferenceConstants.EDITOR_ANNOTATION_ROLL_OVER));
+		
+		overlayKeys.add(new OverlayPreferenceStore.OverlayKey(OverlayPreferenceStore.BOOLEAN, ExtendedTextEditorPreferenceConstants.QUICK_DIFF_ALWAYS_ON));
+		overlayKeys.add(new OverlayPreferenceStore.OverlayKey(OverlayPreferenceStore.BOOLEAN, ExtendedTextEditorPreferenceConstants.QUICK_DIFF_CHARACTER_MODE));
+		overlayKeys.add(new OverlayPreferenceStore.OverlayKey(OverlayPreferenceStore.STRING, ExtendedTextEditorPreferenceConstants.QUICK_DIFF_DEFAULT_PROVIDER));
+		
 
 		while (e.hasNext()) {
 			AnnotationPreference info= (AnnotationPreference) e.next();
@@ -684,6 +713,9 @@ public class JavaEditorPreferencePage extends PreferencePage implements IWorkben
 		slave= addCheckBox(appearanceComposite, label, ExtendedTextEditorPreferenceConstants.EDITOR_WIDE_CARET, 0);
 		createDependency(master, ExtendedTextEditorPreferenceConstants.EDITOR_USE_CUSTOM_CARETS, slave);
 
+		label= PreferencesMessages.getString("JavaEditorPreferencePage.annotationRollover"); //$NON-NLS-1$
+		addCheckBox(appearanceComposite, label, PreferenceConstants.EDITOR_ANNOTATION_ROLL_OVER, 0); //$NON-NLS-1$
+
 		Label l= new Label(appearanceComposite, SWT.LEFT );
 		GridData gd= new GridData(GridData.HORIZONTAL_ALIGN_FILL);
 		gd.horizontalSpan= 2;
@@ -996,6 +1028,27 @@ public class JavaEditorPreferencePage extends PreferencePage implements IWorkben
 		listModelItems.toArray(items);
 		return items;
 	}	
+
+	private String[][] createQuickDiffReferenceListModel() {
+		java.util.List descriptors= new QuickDiff().getReferenceProviderDescriptors();
+		ArrayList listModelItems= new ArrayList();
+		for (Iterator it= descriptors.iterator(); it.hasNext();) {
+			ReferenceProviderDescriptor descriptor= (ReferenceProviderDescriptor) it.next();
+			String label= descriptor.getLabel();
+			int i= label.indexOf('&');
+			while (i >= 0) {
+				if (i < label.length())
+					label= label.substring(0, i) + label.substring(i+1);
+				else
+					label.substring(0, i);
+				i= label.indexOf('&');
+			}
+			listModelItems.add(new String[] { descriptor.getId(), label });
+		}
+		String[][] items= new String[listModelItems.size()][];
+		listModelItems.toArray(items);
+		return items;
+	}
 	
 	private Control createTypingPage(Composite parent) {
 		Composite composite= new Composite(parent, SWT.NONE);
@@ -1051,6 +1104,15 @@ public class JavaEditorPreferencePage extends PreferencePage implements IWorkben
 		label= PreferencesMessages.getString("JavaEditorPreferencePage.addJavaDocTags"); //$NON-NLS-1$
 		slave= addCheckBox(group, label, PreferenceConstants.EDITOR_ADD_JAVADOC_TAGS, 1);
 		createDependency(master, PreferenceConstants.EDITOR_CLOSE_JAVADOCS, slave);
+
+		label= PreferencesMessages.getString("JavaEditorPreferencePage.typing.smartSemicolon"); //$NON-NLS-1$
+		addCheckBox(group, label, PreferenceConstants.EDITOR_SMART_SEMICOLON, 1);
+		
+		label= PreferencesMessages.getString("JavaEditorPreferencePage.typing.smartOpeningBrace"); //$NON-NLS-1$
+		addCheckBox(group, label, PreferenceConstants.EDITOR_SMART_OPENING_BRACE, 1);
+		
+		label= PreferencesMessages.getString("JavaEditorPreferencePage.typing.smartTab"); //$NON-NLS-1$
+		addCheckBox(group, label, PreferenceConstants.EDITOR_SMART_TAB, 1);
 
 //		label= PreferencesMessages.getString("JavaEditorPreferencePage.formatJavaDocs"); //$NON-NLS-1$
 //		addCheckBox(group, label, PreferenceConstants.EDITOR_FORMAT_JAVADOCS, 1);
@@ -1402,6 +1464,10 @@ public class JavaEditorPreferencePage extends PreferencePage implements IWorkben
 		item.setText(PreferencesMessages.getString("JavaEditorPreferencePage.navigationTab.title")); //$NON-NLS-1$
 		item.setControl(createNavigationPage(folder));
 		
+		item= new TabItem(folder, SWT.NONE);
+		item.setText(PreferencesMessages.getString("JavaEditorPreferencePage.quickdiff.title")); //$NON-NLS-1$
+		item.setControl(createQuickdiffPage(folder));
+		
 		initialize();
 		
 		Dialog.applyDialogFont(folder);
@@ -1452,6 +1518,21 @@ public class JavaEditorPreferencePage extends PreferencePage implements IWorkben
 				if (fContentAssistColorList != null && !fContentAssistColorList.isDisposed()) {
 					fContentAssistColorList.select(0);
 					handleContentAssistColorListSelection();
+				}
+			}
+		});
+		
+		for (int i= 0; i < fQuickDiffProviderListModel.length; i++) {
+			String label= fQuickDiffProviderListModel[i][1];
+			if (fOverlayStore.getString(ExtendedTextEditorPreferenceConstants.QUICK_DIFF_DEFAULT_PROVIDER).equals(fQuickDiffProviderListModel[i][0]))
+				label += " " + PreferenceMessages.getString("JavaEditorPreferencePage.quickdiff.defaultlabel"); //$NON-NLS-1$ //$NON-NLS-2$
+			fQuickDiffProviderList.add(label);
+		}
+		fQuickDiffProviderList.getDisplay().asyncExec(new Runnable() {
+			public void run() {
+				if (fQuickDiffProviderList != null && !fQuickDiffProviderList.isDisposed()) {
+					fQuickDiffProviderList.select(0);
+					handleProviderListSelection();
 				}
 			}
 		});
@@ -1567,6 +1648,7 @@ public class JavaEditorPreferencePage extends PreferencePage implements IWorkben
 		handleAppearanceColorListSelection();
 		handleAnnotationListSelection();
 		handleContentAssistColorListSelection();
+		handleProviderListSelection();
 
 		fJavaEditorHoverConfigurationBlock.performDefaults();		
 
@@ -1719,5 +1801,104 @@ public class JavaEditorPreferencePage extends PreferencePage implements IWorkben
 		status= StatusUtil.getMoreSevere(getBrowserLikeLinksKeyModifierStatus(), status);
 		setValid(!status.matches(IStatus.ERROR));
 		StatusUtil.applyToStatusLine(this, status);
+	}
+	
+	private Control createQuickdiffPage(Composite parent) {
+		Composite composite= new Composite(parent, SWT.NONE);
+		GridLayout layout= new GridLayout(); layout.numColumns= 2;
+		composite.setLayout(layout);
+
+		String label= PreferencesMessages.getString("JavaEditorPreferencePage.quickdiff.showForNewEditors"); //$NON-NLS-1$
+		addCheckBox(composite, label, ExtendedTextEditorPreferenceConstants.QUICK_DIFF_ALWAYS_ON, 0);
+
+		label= PreferencesMessages.getString("JavaEditorPreferencePage.quickdiff.characterMode"); //$NON-NLS-1$
+		addCheckBox(composite, label, ExtendedTextEditorPreferenceConstants.QUICK_DIFF_CHARACTER_MODE, 0);
+
+		Label l= new Label(composite, SWT.LEFT );
+		GridData gd= new GridData(GridData.HORIZONTAL_ALIGN_FILL);
+		gd.horizontalSpan= 2;
+		gd.heightHint= convertHeightInCharsToPixels(1) / 2;
+		l.setLayoutData(gd);
+		
+		l= new Label(composite, SWT.LEFT);
+		l.setText(PreferencesMessages.getString("JavaEditorPreferencePage.quickdiff.referenceProviderTitle")); //$NON-NLS-1$
+		gd= new GridData(GridData.HORIZONTAL_ALIGN_FILL);
+		gd.horizontalSpan= 2;
+		l.setLayoutData(gd);
+
+		Composite editorComposite= new Composite(composite, SWT.NONE);
+		layout= new GridLayout();
+		layout.numColumns= 2;
+		layout.marginHeight= 0;
+		layout.marginWidth= 0;
+		editorComposite.setLayout(layout);
+		gd= new GridData(GridData.HORIZONTAL_ALIGN_FILL | GridData.FILL_VERTICAL);
+		gd.horizontalSpan= 2;
+		editorComposite.setLayoutData(gd);		
+
+		fQuickDiffProviderList= new List(editorComposite, SWT.SINGLE | SWT.V_SCROLL | SWT.BORDER);
+		gd= new GridData(GridData.VERTICAL_ALIGN_BEGINNING | GridData.FILL_HORIZONTAL);
+		gd.heightHint= convertHeightInCharsToPixels(4);
+		fQuickDiffProviderList.setLayoutData(gd);
+						
+		Composite stylesComposite= new Composite(editorComposite, SWT.NONE);
+		layout= new GridLayout();
+		layout.marginHeight= 0;
+		layout.marginWidth= 0;
+		layout.numColumns= 2;
+		stylesComposite.setLayout(layout);
+		stylesComposite.setLayoutData(new GridData(GridData.FILL_BOTH));
+		
+		fSetDefaultButton= new Button(stylesComposite, SWT.PUSH);
+		fSetDefaultButton.setText(PreferencesMessages.getString("JavaEditorPreferencePage.quickdiff.setDefault")); //$NON-NLS-1$
+		gd= new GridData(GridData.FILL_HORIZONTAL);
+		gd.horizontalAlignment= GridData.BEGINNING;
+		gd.horizontalSpan= 2;
+		fSetDefaultButton.setLayoutData(gd);
+		
+		fQuickDiffProviderList.addSelectionListener(new SelectionListener() {
+			public void widgetDefaultSelected(SelectionEvent e) {
+				// do nothing
+			}
+			
+			public void widgetSelected(SelectionEvent e) {
+				handleProviderListSelection();
+			}
+
+		});
+		
+		fSetDefaultButton.addSelectionListener(new SelectionListener() {
+			public void widgetDefaultSelected(SelectionEvent e) {
+				// do nothing
+			}
+			
+			public void widgetSelected(SelectionEvent e) {
+				int i= fQuickDiffProviderList.getSelectionIndex();
+				for (int j= 0; j < fQuickDiffProviderListModel.length; j++) {
+					if (fOverlayStore.getString(ExtendedTextEditorPreferenceConstants.QUICK_DIFF_DEFAULT_PROVIDER).equals(fQuickDiffProviderListModel[j][0])) {
+						fQuickDiffProviderList.remove(j);
+						fQuickDiffProviderList.add(fQuickDiffProviderListModel[j][1], j);
+					}
+					if (i == j) {
+						fQuickDiffProviderList.remove(j);
+						fQuickDiffProviderList.add(fQuickDiffProviderListModel[j][1] + " " + PreferencesMessages.getString("JavaEditorPreferencePage.quickdiff.defaultlabel"), j);  //$NON-NLS-1$//$NON-NLS-2$
+					}
+				}
+				fSetDefaultButton.setEnabled(false);
+				fQuickDiffProviderList.setSelection(i);
+				fQuickDiffProviderList.redraw();
+				
+				fOverlayStore.setValue(ExtendedTextEditorPreferenceConstants.QUICK_DIFF_DEFAULT_PROVIDER, fQuickDiffProviderListModel[i][0]);
+			}
+		});
+		
+		return composite;
+	}
+	
+	private void handleProviderListSelection() {
+		int i= fQuickDiffProviderList.getSelectionIndex();
+		
+		boolean b= getPreferenceStore().getString(ExtendedTextEditorPreferenceConstants.QUICK_DIFF_DEFAULT_PROVIDER).equals(fQuickDiffProviderListModel[i][0]);
+		fSetDefaultButton.setEnabled(!b);
 	}
 }
--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/preferences/WorkInProgressPreferencePage.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/preferences/WorkInProgressPreferencePage.java
@@ -11,11 +11,9 @@
 package org.eclipse.jdt.internal.ui.preferences;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
+
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
@@ -24,16 +22,17 @@ import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
+
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.preference.PreferencePage;
+
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 import org.eclipse.ui.help.WorkbenchHelp;
-import org.eclipse.ui.texteditor.ExtendedTextEditorPreferenceConstants;
-import org.eclipse.ui.texteditor.quickdiff.QuickDiff;
-import org.eclipse.ui.texteditor.quickdiff.ReferenceProviderDescriptor;
+
 import org.eclipse.jdt.ui.PreferenceConstants;
+
 import org.eclipse.jdt.internal.ui.JavaPlugin;
 
 /**
@@ -56,13 +55,6 @@ public class WorkInProgressPreferencePage extends PreferencePage implements IWor
 	private List fRadioButtons;
 	private List fTextControls;
 	
-	/** List for the reference provider default. */
-	private org.eclipse.swt.widgets.List fQuickDiffProviderList;
-	/** The reference provider default's list model. */
-	private String[][] fQuickDiffProviderListModel;
-	/** Button controlling default setting of the selected reference provider. */
-	private Button fSetDefaultButton;
-
 	/**
 	 * creates a new preference page.
 	 */
@@ -71,38 +63,8 @@ public class WorkInProgressPreferencePage extends PreferencePage implements IWor
 		fRadioButtons= new ArrayList();
 		fCheckBoxes= new ArrayList();
 		fTextControls= new ArrayList();
-
-		List providers= new QuickDiff().getReferenceProviderDescriptors();
-		fQuickDiffProviderListModel= createQuickDiffReferenceListModel(providers);
 	}
 
-	private String[][] createQuickDiffReferenceListModel(List providers) {
-		ArrayList listModelItems= new ArrayList();
-		for (Iterator it= providers.iterator(); it.hasNext();) {
-			ReferenceProviderDescriptor provider= (ReferenceProviderDescriptor) it.next();
-			String label= provider.getLabel();
-			int i= label.indexOf('&');
-			while (i >= 0) {
-				if (i < label.length())
-					label= label.substring(0, i) + label.substring(i+1);
-				else
-					label.substring(0, i);
-				i= label.indexOf('&');
-			}
-			listModelItems.add(new String[] { provider.getId(), label });
-		}
-		String[][] items= new String[listModelItems.size()][];
-		listModelItems.toArray(items);
-		return items;
-	}
-	
-	private void handleProviderListSelection() {
-		int i= fQuickDiffProviderList.getSelectionIndex();
-		
-		boolean b= getPreferenceStore().getString(ExtendedTextEditorPreferenceConstants.QUICK_DIFF_DEFAULT_PROVIDER).equals(fQuickDiffProviderListModel[i][0]);
-		fSetDefaultButton.setEnabled(!b);
-	}
-	
 	private Button addCheckBox(Composite parent, String label, String key) { 
 		GridData gd= new GridData(GridData.HORIZONTAL_ALIGN_FILL);
 		
@@ -137,133 +99,7 @@ public class WorkInProgressPreferencePage extends PreferencePage implements IWor
 		result.setLayout(layout);
 
 		addCheckBox(result, PreferencesMessages.getString(PREFIX + "browserWidgetInJavadocView"), PREF_BROWSER_WIDGET_IN_JAVADOC_VIEW); //$NON-NLS-1$
-		
 		Group group= new Group(result, SWT.NONE);
-		group.setLayout(new GridLayout());
-		group.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-		group.setText(PreferencesMessages.getString(PREFIX + "editor")); //$NON-NLS-1$
-	
-		addCheckBox(group, PreferencesMessages.getString(PREFIX + "rollover"), PreferenceConstants.EDITOR_ANNOTATION_ROLL_OVER); //$NON-NLS-1$
-		createSpacer(group, 1);
-	
-		Label label= new Label(group, SWT.NONE);
-		label.setText(PreferencesMessages.getString(PREFIX + "smartTyping.label")); //$NON-NLS-1$
-	
-		addCheckBox(group, PreferencesMessages.getString(PREFIX + "smartTyping.smartSemicolon"), PreferenceConstants.EDITOR_SMART_SEMICOLON); //$NON-NLS-1$
-		
-		addCheckBox(group, PreferencesMessages.getString(PREFIX + "smartTyping.smartOpeningBrace"), PreferenceConstants.EDITOR_SMART_OPENING_BRACE); //$NON-NLS-1$
-		
-		addCheckBox(group, PreferencesMessages.getString(PREFIX + "smartTyping.smartTab"), PreferenceConstants.EDITOR_SMART_TAB); //$NON-NLS-1$
-		
-		/* line change bar */
-		group= new Group(result, SWT.NONE);
-		group.setLayout(new GridLayout());
-		group.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-		group.setText(PreferencesMessages.getString(PREFIX + "quickdiff")); //$NON-NLS-1$
-		
-		Label l= new Label(group, SWT.LEFT );
-		GridData gd= new GridData(GridData.HORIZONTAL_ALIGN_FILL);
-		gd.horizontalSpan= 2;
-		gd.heightHint= convertHeightInCharsToPixels(1) / 2;
-		l.setLayoutData(gd);
-	
-		addCheckBox(group, PreferencesMessages.getString(PREFIX + "showQuickDiffPerDefault"), ExtendedTextEditorPreferenceConstants.QUICK_DIFF_ALWAYS_ON); //$NON-NLS-1$
-		addCheckBox(group, PreferencesMessages.getString(PREFIX + "quickdiff.characterMode"), ExtendedTextEditorPreferenceConstants.QUICK_DIFF_CHARACTER_MODE); //$NON-NLS-1$
-		
-		l= new Label(group, SWT.LEFT );
-		gd= new GridData(GridData.HORIZONTAL_ALIGN_FILL);
-		gd.horizontalSpan= 2;
-		gd.heightHint= convertHeightInCharsToPixels(1) / 2;
-		l.setLayoutData(gd);
-	
-		l= new Label(group, SWT.LEFT);
-		l.setText(PreferencesMessages.getString(PREFIX + "quickdiff.referenceprovidertitle")); //$NON-NLS-1$
-		gd= new GridData(GridData.HORIZONTAL_ALIGN_FILL);
-		gd.horizontalSpan= 2;
-		l.setLayoutData(gd);
-	
-		Composite editorComposite= new Composite(group, SWT.NONE);
-		layout= new GridLayout();
-		layout.numColumns= 2;
-		layout.marginHeight= 0;
-		layout.marginWidth= 0;
-		editorComposite.setLayout(layout);
-		gd= new GridData(GridData.HORIZONTAL_ALIGN_FILL | GridData.FILL_VERTICAL);
-		gd.horizontalSpan= 2;
-		editorComposite.setLayoutData(gd);		
-	
-		fQuickDiffProviderList= new org.eclipse.swt.widgets.List(editorComposite, SWT.SINGLE | SWT.V_SCROLL | SWT.BORDER);
-		gd= new GridData(GridData.VERTICAL_ALIGN_BEGINNING | GridData.FILL_HORIZONTAL);
-		gd.heightHint= convertHeightInCharsToPixels(4);
-		fQuickDiffProviderList.setLayoutData(gd);
-						
-		Composite stylesComposite= new Composite(editorComposite, SWT.NONE);
-		layout= new GridLayout();
-		layout.marginHeight= 0;
-		layout.marginWidth= 0;
-		layout.numColumns= 2;
-		stylesComposite.setLayout(layout);
-		stylesComposite.setLayoutData(new GridData(GridData.FILL_BOTH));
-		
-		fSetDefaultButton= new Button(stylesComposite, SWT.PUSH);
-		fSetDefaultButton.setText(PreferencesMessages.getString(PREFIX + "quickdiff.setDefault")); //$NON-NLS-1$
-		gd= new GridData(GridData.FILL_HORIZONTAL);
-		gd.horizontalAlignment= GridData.BEGINNING;
-		gd.horizontalSpan= 2;
-		fSetDefaultButton.setLayoutData(gd);
-		
-		fQuickDiffProviderList.addSelectionListener(new SelectionListener() {
-			public void widgetDefaultSelected(SelectionEvent e) {
-				// do nothing
-			}
-			
-			public void widgetSelected(SelectionEvent e) {
-				handleProviderListSelection();
-			}
-	
-		});
-		
-		fSetDefaultButton.addSelectionListener(new SelectionListener() {
-			public void widgetDefaultSelected(SelectionEvent e) {
-				// do nothing
-			}
-			
-			public void widgetSelected(SelectionEvent e) {
-				int i= fQuickDiffProviderList.getSelectionIndex();
-				for (int j= 0; j < fQuickDiffProviderListModel.length; j++) {
-					if (getPreferenceStore().getString(ExtendedTextEditorPreferenceConstants.QUICK_DIFF_DEFAULT_PROVIDER).equals(fQuickDiffProviderListModel[j][0])) {
-						fQuickDiffProviderList.remove(j);
-						fQuickDiffProviderList.add(fQuickDiffProviderListModel[j][1], j);
-					}
-					if (i == j) {
-						fQuickDiffProviderList.remove(j);
-						fQuickDiffProviderList.add(fQuickDiffProviderListModel[j][1] + " " + PreferencesMessages.getString(PREFIX + "quickdiff.defaultlabel"), j);  //$NON-NLS-1$//$NON-NLS-2$
-					}
-				}
-				fSetDefaultButton.setEnabled(false);
-				fQuickDiffProviderList.setSelection(i);
-				fQuickDiffProviderList.redraw();
-				
-				getPreferenceStore().setValue(ExtendedTextEditorPreferenceConstants.QUICK_DIFF_DEFAULT_PROVIDER, fQuickDiffProviderListModel[i][0]);
-			}
-		});
-		
-		for (int i= 0; i < fQuickDiffProviderListModel.length; i++) {
-			String sLabel= fQuickDiffProviderListModel[i][1];
-			if (getPreferenceStore().getString(ExtendedTextEditorPreferenceConstants.QUICK_DIFF_DEFAULT_PROVIDER).equals(fQuickDiffProviderListModel[i][0]))
-				sLabel += " " + PreferencesMessages.getString(PREFIX + "quickdiff.defaultlabel"); //$NON-NLS-1$ //$NON-NLS-2$
-			fQuickDiffProviderList.add(sLabel);
-		}
-		fQuickDiffProviderList.getDisplay().asyncExec(new Runnable() {
-			public void run() {
-				if (fQuickDiffProviderList != null && !fQuickDiffProviderList.isDisposed()) {
-					fQuickDiffProviderList.select(0);
-					handleProviderListSelection();
-				}
-			}
-		});
-	
-		group= new Group(result, SWT.NONE);
 		group.setLayout(new GridLayout());
 		group.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		group.setText(PreferencesMessages.getString(PREFIX + "search")); //$NON-NLS-1$
@@ -317,7 +153,6 @@ public class WorkInProgressPreferencePage extends PreferencePage implements IWor
 			String key= (String) text.getData();
 			text.setText(store.getDefaultString(key));
 		}
-		handleProviderListSelection();
 		
 		super.performDefaults();
 	}
