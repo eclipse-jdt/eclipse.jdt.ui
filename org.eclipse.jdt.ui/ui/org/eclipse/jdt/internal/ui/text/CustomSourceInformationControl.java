4e5810a81bc78a1f000bc3d8d2f5233ac10dfdf0
Kai Maetzel <kmaetzel>
2004-04-28 19:05:05 +0000

    *** empty log message ***

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/CustomSourceInformationControl.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/CustomSourceInformationControl.java
@@ -14,7 +14,6 @@ import java.util.Arrays;
 
 import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.graphics.Font;
-import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
