52bccef2015b2725da735ced7a2b20c97b56cdb5
Martin Aeschlimann <maeschli>
2004-04-29 14:59:11 +0000

    enabled nls tests

--- org.eclipse.jdt.ui.tests.refactoring/test cases/org/eclipse/jdt/ui/tests/all/AllAllTests.java
+++ org.eclipse.jdt.ui.tests.refactoring/test cases/org/eclipse/jdt/ui/tests/all/AllAllTests.java
@@ -17,7 +17,7 @@ public class AllAllTests {
 	public static Test suite ( ) {
 		TestSuite suite= new TestSuite("All Tests");
 		suite.addTest(org.eclipse.jdt.ui.tests.actions.AllTests.suite());
-		//suite.addTest(org.eclipse.jdt.ui.tests.nls.AllTests.suite());
+		suite.addTest(org.eclipse.jdt.ui.tests.nls.AllTests.suite());
 		suite.addTest(org.eclipse.jdt.ui.tests.refactoring.AllTests.suite());
 		suite.addTest(org.eclipse.jdt.ui.tests.refactoring.extensions.AllTests.suite());
 		suite.addTest(org.eclipse.jdt.ui.tests.changes.AllTests.suite());
