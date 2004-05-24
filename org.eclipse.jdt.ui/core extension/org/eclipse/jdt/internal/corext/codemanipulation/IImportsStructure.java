e865e2d783b2cdb88fcba2f94ffa710ef016549f
Martin Aeschlimann <maeschli>
2004-05-24 22:14:27 +0000

    63522 Unnecessary import for protected inner class [code manipulation]

--- org.eclipse.jdt.ui/core extension/org/eclipse/jdt/internal/corext/codemanipulation/IImportsStructure.java
+++ org.eclipse.jdt.ui/core extension/org/eclipse/jdt/internal/corext/codemanipulation/IImportsStructure.java
@@ -24,17 +24,5 @@ public interface IImportsStructure {
 	 */		
 	String addImport(String qualifiedTypeName);
 	
-
-	/**
-	 * Adds a new import declaration that is sorted in the structure using
-	 * a best match algorithm. If an import already exists, the import is
-	 * not added. An import is also not added if its container is 'java.lang' or the
-	 * same package as the compilation unit.
-	 * @param typeContainerName The name of the type container (package name or outer type name) to import
-	 * @param typeName The type name of the type to import (can be '*' for imports-on-demand)
-	 * @return Retuns the simple type name that can be used in the code or the
-	 * fully qualified type name if an import conflict prevented the import.
-	 */
-	String addImport(String typeContainerName, String typeName);
 	
 }
