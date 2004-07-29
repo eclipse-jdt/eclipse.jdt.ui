6e92197c7ac9d0ae6b7eef141daa34be7e769f8b
Christof Marti <cmarti>
2004-07-29 16:15:41 +0000

    initial release

---
+++ org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/PerformanceTestCase.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2004 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.jdt.text.tests.performance;
+
+import junit.framework.TestCase;
+
+public class PerformanceTestCase extends TestCase {
+	
+	protected void runTest() throws Throwable {
+		PerformanceMeter meter= createPerformanceMeterFactory().createPerformanceMeter(this);
+		meter.start();
+		super.runTest();
+		meter.stop();
+		meter.commit();
+	}
+
+	protected PerformanceMeterFactory createPerformanceMeterFactory() {
+		return new OSPerformanceMeterFactory();
+	}
+}
