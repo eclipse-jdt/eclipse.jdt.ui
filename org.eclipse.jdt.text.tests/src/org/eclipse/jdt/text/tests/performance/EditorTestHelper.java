ae96ab7108b44e6389cc2cf354ad7ab9e1ffbfa0
teicher <teicher>
2004-08-02 09:41:40 +0000

    added EditTestHelper

---
+++ org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/EditorTestHelper.java
@@ -0,0 +1,93 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2004 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.jdt.text.tests.performance;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.Path;
+
+import org.eclipse.swt.widgets.Shell;
+
+import org.eclipse.jface.text.IDocument;
+
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.ide.IDE;
+import org.eclipse.ui.texteditor.IDocumentProvider;
+import org.eclipse.ui.texteditor.ITextEditor;
+
+
+/**
+ * @since 3.1
+ */
+public class EditorTestHelper {
+
+	public static IFile findFile(String path) {
+		IWorkspaceRoot root= ResourcesPlugin.getWorkspace().getRoot();
+		return root.getFile(new Path(path));
+	}
+	
+	public static IFile[] findFiles(String prefix, String suffix, int i, int n) {
+		IWorkspaceRoot root= ResourcesPlugin.getWorkspace().getRoot();
+		List files= new ArrayList(n - i);
+		for (int j= i; j < i + n; j++) {
+			String path= root.getLocation().toString() + "/" + prefix + j + suffix;
+			files.add(findFile(path));
+		}
+		return (IFile[]) files.toArray(new IFile[files.size()]);
+	}
+	
+	public static IEditorPart openInEditor(IFile file, boolean runEventLoop) throws PartInitException {
+		IEditorPart part= IDE.openEditor(getActivePage(), file);
+		if (runEventLoop)
+			runEventQueue(part);
+		return part;
+	}
+
+	public static IDocument getDocument(ITextEditor editor) {
+		IDocumentProvider provider= editor.getDocumentProvider();
+		IEditorInput input= editor.getEditorInput();
+		return provider.getDocument(input);
+	}
+
+	public static void revertEditor(ITextEditor editor) {
+		editor.doRevertToSaved();
+	}
+	
+	public static void closeAllEditors() {
+		getActivePage().closeAllEditors(false);
+	}
+	
+	public static void runEventQueue() {
+		runEventQueue(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell());
+	}
+	
+	public static void runEventQueue(IWorkbenchPart part) {
+		runEventQueue(part.getSite().getShell());
+	}
+	
+	public static void runEventQueue(Shell shell) {
+		while (shell.getDisplay().readAndDispatch());
+	}
+	
+	private static IWorkbenchPage getActivePage() {
+		return PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+	}
+
+}
--- org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/RevertEditorTest.java
+++ org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/RevertEditorTest.java
@@ -11,28 +11,14 @@
 
 package org.eclipse.jdt.text.tests.performance;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import junit.framework.TestCase;
 
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IWorkspaceRoot;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.Path;
-
-import org.eclipse.swt.widgets.Shell;
 
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
 
-import org.eclipse.ui.IEditorInput;
-import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.ide.IDE;
-import org.eclipse.ui.texteditor.IDocumentProvider;
 import org.eclipse.ui.texteditor.ITextEditor;
 
 public class RevertEditorTest extends TestCase {
@@ -50,7 +36,7 @@ public class RevertEditorTest extends TestCase {
 	private static final String REPLACE_TEXT= "XXX";
 
 	protected void setUp() {
-		runEventQueue(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell());
+		EditorTestHelper.runEventQueue();
 	}
 	
 	public void testRevertJavaEditor() throws PartInitException, BadLocationException {
@@ -61,66 +47,19 @@ public class RevertEditorTest extends TestCase {
 	private void measure() throws PartInitException, BadLocationException {
 		PerformanceMeter performanceMeter= fPerformanceMeterFactory.createPerformanceMeter(this);
 		try {
-			IFile[] files= findFiles(OpenEditorTestSetup.PROJECT + PATH + FILE_PREFIX, FILE_SUFFIX, 0, N_OF_COPIES);
+			IFile[] files= EditorTestHelper.findFiles(OpenEditorTestSetup.PROJECT + PATH + FILE_PREFIX, FILE_SUFFIX, 0, N_OF_COPIES);
 			for (int i= 0, n= files.length; i < n; i++) {
-				IEditorPart part= openInEditor(files[i]);
+				ITextEditor part= (ITextEditor) EditorTestHelper.openInEditor(files[i], true);
 				dirtyEditor(part);
 				performanceMeter.start();
-				revertEditor(part);
+				EditorTestHelper.revertEditor(part);
 				performanceMeter.stop();
 				sleep(2000); // NOTE: runnables posted from other threads, while the main thread waits here, are executed and measured only in the next iteration
 			}
 		} finally {
-			getActivePage().closeAllEditors(false);
+			EditorTestHelper.closeAllEditors();
 			performanceMeter.commit();
 		}
-	}
-
-	private IEditorPart openInEditor(IFile file) throws PartInitException {
-		IEditorPart part= IDE.openEditor(getActivePage(), file);
-		runEventQueue(part.getSite().getShell());
-		return part;
-	}
-
-	private IFile[] findFiles(String prefix, String suffix, int i, int n) {
-		IWorkspaceRoot root= ResourcesPlugin.getWorkspace().getRoot();
-		List types= new ArrayList(n);
-		for (int j= i; j < i + n; j++)
-			types.add(root.getFile(new Path(root.getLocation().toString() + "/" + prefix + j + suffix)));
-		return (IFile[]) types.toArray(new IFile[types.size()]);
-	}
-	
-	private void revertEditor(IEditorPart part) {
-		if (part instanceof ITextEditor) {
-			ITextEditor editor= (ITextEditor) part;
-			editor.doRevertToSaved();
-		}
-	}
-
-	private void dirtyEditor(IEditorPart part) throws BadLocationException {
-		IDocument document= getDocument(part);
-		int line= document.getNumberOfLines() / 2; // dirty middle line
-		int offset= document.getLineOffset(line);
-		document.replace(offset, 0, REPLACE_TEXT);
-		runEventQueue(part.getSite().getShell());
-	}
-
-	private IDocument getDocument(IEditorPart part) throws BadLocationException {
-		if (part instanceof ITextEditor) {
-			ITextEditor editor= (ITextEditor) part;
-			IDocumentProvider provider= editor.getDocumentProvider();
-			IEditorInput input= editor.getEditorInput();
-			return provider.getDocument(input);
-		} else
-			throw new BadLocationException();
-	}
-
-	private void runEventQueue(Shell shell) {
-		while (shell.getDisplay().readAndDispatch());
-	}
-
-	private IWorkbenchPage getActivePage() {
-		return PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
 	}
 
 	private synchronized void sleep(int time) {
@@ -128,5 +67,13 @@ public class RevertEditorTest extends TestCase {
 			wait(time);
 		} catch (InterruptedException e) {
 		}
+	}
+
+	private void dirtyEditor(ITextEditor part) throws BadLocationException {
+		IDocument document= EditorTestHelper.getDocument(part);
+		int line= document.getNumberOfLines() / 2; // dirty middle line
+		int offset= document.getLineOffset(line);
+		document.replace(offset, 0, REPLACE_TEXT);
+		EditorTestHelper.runEventQueue(part);
 	}
 }
