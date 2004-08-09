c01edb8640c5fbe8f8688f66125abfc3f77a9108
Markus Keller <mkeller>
2004-08-09 15:52:22 +0000

    Tests for 70525: Surface new compiler option for missing serialVersionUID from Tobias Widmer

--- org.eclipse.jdt.ui.tests/ui/org/eclipse/jdt/ui/tests/quickfix/QuickFixTest.java
+++ org.eclipse.jdt.ui.tests/ui/org/eclipse/jdt/ui/tests/quickfix/QuickFixTest.java
@@ -54,6 +54,7 @@ public class QuickFixTest extends TestCase {
 
 	public static Test suite() {
 		TestSuite suite= new TestSuite();
+		suite.addTest(SerialVersionQuickFixTest.allTests());
 		suite.addTest(UnresolvedTypesQuickFixTest.allTests());
 		suite.addTest(UnresolvedVariablesQuickFixTest.allTests());
 		suite.addTest(UnresolvedMethodsQuickFixTest.allTests());
