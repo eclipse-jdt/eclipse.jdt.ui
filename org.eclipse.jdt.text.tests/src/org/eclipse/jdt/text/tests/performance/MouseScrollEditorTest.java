5ec319a7a69a80cfe61db1c93ca6fb52d8206377
Christof Marti <cmarti>
2004-08-17 09:46:20 +0000

    make abstract superclass use same computation as source viewer for computing height of scroll button make fTopIndex/Pixel volatile

--- org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/MouseScrollEditorTest.java
+++ org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/MouseScrollEditorTest.java
@@ -11,26 +11,21 @@
 
 package org.eclipse.jdt.text.tests.performance;
 
-import junit.framework.Test;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
 import org.eclipse.core.resources.IFile;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.graphics.Point;
-import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
 
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.PartInitException;
 
-public class MouseScrollEditorTest extends TestCase {
+public abstract class MouseScrollEditorTest extends TestCase {
 
-	private static final Class THIS= MouseScrollEditorTest.class;
-	
 	private static final int[] CTRL_HOME= new int[] { SWT.CTRL, SWT.HOME };
 
 	private static final int[] CTRL_END= new int[] { SWT.CTRL, SWT.END };
@@ -41,14 +36,6 @@ public class MouseScrollEditorTest extends TestCase {
 
 	private volatile boolean fDone;
 
-	public static Test allTests() {
-		return new TestSuite(THIS);
-	}
-
-	public static Test suite() {
-		return allTests();
-	}
-	
 	protected void setUp() throws Exception {
 		fPerformanceMeter= Performance.createPerformanceMeterFactory().createPerformanceMeter(this);
 		EditorTestHelper.bringToTop();
@@ -56,36 +43,26 @@ public class MouseScrollEditorTest extends TestCase {
 		EditorTestHelper.calmDown(3000, 10000, 100);
 	}
 
-	protected IFile getFile() {
-		return ResourceTestHelper.findFile("org.eclipse.swt/Eclipse SWT Custom Widgets/common/org/eclipse/swt/custom/StyledText.java");
-	}
-	public void testMouseScrollEditor() throws PartInitException {
-		measureScrolling(3);
-	}
-	
 	protected void tearDown() throws Exception {
 		EditorTestHelper.closeAllEditors();
 	}
 
+	protected abstract IFile getFile();
+
 	protected void measureScrolling(int nOfRuns) throws PartInitException {
 		final Display display= SWTEventHelper.getActiveDisplay();
-		
 		final StyledText text= (StyledText) fEditor.getAdapter(Control.class);
-		Rectangle textBounds= text.getBounds();
-		Point scrollBarSize= text.getVerticalBar().getSize();
-		int guessX= textBounds.width - ((int) (0.5*scrollBarSize.x));
-		int guessY= scrollBarSize.x + 5;
-		Point point= display.map(text, null, guessX, guessY);
-		final int mappedGuessX= point.x;
-		final int mappedGuessY= point.y;
-		point= display.map(text, null, guessX, textBounds.height);
-
+		
+		int thumbX= text.getBounds().width - (text.getVerticalBar().getSize().x >> 1);
+		int thumbY= text.computeTrim(0, 0, 0, 0).height + 2;
+		final Point thumb= display.map(text, null, thumbX, thumbY);
+		
 		final int numberOfLines= text.getLineCount();
 		final int visibleLinesInViewport= text.getClientArea().height / text.getLineHeight();
 		
 		Runnable runnable= new Runnable() {
-			private int fTopIndex;
-			private int fTopPixel;
+			private volatile int fTopIndex;
+			private volatile int fTopPixel;
 			private Runnable fRunnable= new Runnable() {
 				public void run() {
 					fTopIndex= text.getTopIndex();
@@ -95,12 +72,12 @@ public class MouseScrollEditorTest extends TestCase {
 			public void run() {
 				fTopIndex= 0;
 				fTopPixel= 0;
-				int i= mappedGuessY + 1;
+				int i= thumb.y + 1;
 				int posts= 0;
-				SWTEventHelper.mouseMoveEvent(display, mappedGuessX, mappedGuessY, false);
+				SWTEventHelper.mouseMoveEvent(display, thumb.x, thumb.y, false);
 				SWTEventHelper.mouseDownEvent(display, 1, false);
 				while (fTopIndex + visibleLinesInViewport < numberOfLines - 1) {
-					SWTEventHelper.mouseMoveEvent(display, mappedGuessX, i++, false);
+					SWTEventHelper.mouseMoveEvent(display, thumb.x, i++, false);
 					int oldTopPixel= fTopPixel;
 					while (oldTopPixel == fTopPixel) {
 						posts++;
