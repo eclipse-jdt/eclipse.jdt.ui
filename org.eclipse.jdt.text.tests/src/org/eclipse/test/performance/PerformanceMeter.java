bdb3e70a34d3b6690b5c088cbeb0e8c2a72b5667
Christof Marti <cmarti>
2004-08-24 13:58:39 +0000

    add dispose() move getSample() to internal subclass extend Javadoc

--- org.eclipse.jdt.text.tests/src/org/eclipse/test/performance/PerformanceMeter.java
+++ org.eclipse.jdt.text.tests/src/org/eclipse/test/performance/PerformanceMeter.java
@@ -11,25 +11,45 @@
 
 package org.eclipse.test.performance;
 
-import org.eclipse.test.internal.performance.data.Sample;
-
 /**
- * A <code>PerformanceMeter</code> is used to measure an arbitrary operation
- * multiple times. Typical measurements include time, CPU cycle and/or memory
+ * A <code>PerformanceMeter</code> is used for doing repeated measurements of
+ * an arbitrary operation.
+ * 
+ * The kind of measurement and the retrieval of the results remain internal to
+ * the implementation. Measurements can include time, CPU cycle and memory
  * consumption.
+ * 
+ * A <code>PerformanceMeter</code> is created using the method
+ * {@link Performance#createPerformanceMeter(String)}. An operation is measured
+ * by calling {@link PerformanceMeter#start()} before and
+ * {@link PerformanceMeter#stop()} after that operation. The measurement can be
+ * repeated, for example, to let the VM warm up and to allow for statistical
+ * analysis afterwards.
+ * 
+ * After measurements are done and before an analysis of the results can be made
+ * {@link PerformanceMeter#commit()} has to be called. This allows for example
+ * to prepare the measurements for analysis or persist them.
+ * {@link Performance#assertPerformance(PerformanceMeter)} provides a default
+ * analysis of the measurements. After the <code>PerformanceMeter</code> is no
+ * longer used {@link PerformanceMeter#dispose()} must be called.
  * 
  * Example usage in a test case:
  * <pre>
  * public void testOpenEditor() {
- * 	PerformanceMeter performanceMeter= Performance.getDefault().createPerformanceMeter(this);
- * 	for (int i= 0; i < 10; i++) {
- * 		performanceMeter.start();
- * 		openEditor();
- * 		performanceMeter.stop();
- * 		closeEditor();
+ * 	Performance performance= Performance.getDefault();
+ * 	PerformanceMeter performanceMeter= performance.createPerformanceMeter(performance.getDefaultScenarioId(this));
+ * 	try {
+ * 		for (int i= 0; i &lt; 10; i++) {
+ * 			performanceMeter.start();
+ * 			openEditor();
+ * 			performanceMeter.stop();
+ * 			closeEditor();
+ * 		}
+ * 		performanceMeter.commit();
+ * 		Performance.getDefault().assertPerformance(performanceMeter);
+ * 	} finally {
+ * 		performanceMeter.dispose();
  * 	}
- * 	performanceMeter.commit();
- * 	Performance.getDefault().assertPerformance(performanceMeter);
  * }
  * </pre>
  * 
@@ -38,19 +58,29 @@ import org.eclipse.test.internal.performance.data.Sample;
 public abstract class PerformanceMeter {
 
 	/**
-	 * Called immediately before the operation to measure.
+	 * Called immediately before the operation to measure. Must be followed
+	 * by a call to {@link PerformanceMeter#stop()} before subsequent calls
+	 * to this method or {@link PerformanceMeter#commit()}.
 	 */
 	public abstract void start();
 
 	/**
-	 * Called immediately after the operation to measure.
+	 * Called immediately after the operation to measure. Must be preceded
+	 * by a call to {@link PerformanceMeter#start()}, that follows any
+	 * previous call to this method.
 	 */
 	public abstract void stop();
 
 	/**
-	 * Called after repeated measurement is done.
+	 * Called exactly once after repeated measurements are done and before
+	 * their analysis. Afterwards {@link PerformanceMeter#start()} and
+	 * {@link PerformanceMeter#stop()} must not be called.
 	 */
 	public abstract void commit();
 
-	protected abstract Sample getSample();
+	/**
+	 * Dispose associated resources. Clients must call this method exactly
+	 * once. Afterwards no methods must be called on the performance meter.
+	 */
+	public abstract void dispose();
 }
