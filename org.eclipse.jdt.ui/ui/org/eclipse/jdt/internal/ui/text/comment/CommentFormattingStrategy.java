c9fc12aeba1f709fbe01ee6b785b99a90a2147ff
Christof Marti <cmarti>
2004-10-25 16:01:08 +0000

    infer indentation level in formatting strategy

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/comment/CommentFormattingStrategy.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/comment/CommentFormattingStrategy.java
@@ -12,6 +12,7 @@
 package org.eclipse.jdt.internal.ui.text.comment;
 
 import java.util.LinkedList;
+import java.util.Map;
 
 import org.eclipse.text.edits.MalformedTreeException;
 import org.eclipse.text.edits.TextEdit;
@@ -24,8 +25,10 @@ import org.eclipse.jface.text.formatter.ContextBasedFormattingStrategy;
 import org.eclipse.jface.text.formatter.FormattingContextProperties;
 import org.eclipse.jface.text.formatter.IFormattingContext;
 
+import org.eclipse.jdt.core.formatter.CodeFormatter;
+import org.eclipse.jdt.core.formatter.DefaultCodeFormatterConstants;
+
 import org.eclipse.jdt.internal.corext.text.comment.CommentFormatter;
-import org.eclipse.jdt.internal.corext.text.comment.CommentRegion;
 import org.eclipse.jdt.internal.corext.text.comment.ITextMeasurement;
 import org.eclipse.jdt.internal.corext.text.comment.JavaPartitions;
 
@@ -69,15 +72,20 @@ public class CommentFormattingStrategy extends ContextBasedFormattingStrategy {
 		final TypedPosition position= (TypedPosition)fPartitions.removeFirst();
 		
 		if (document != null && position != null) {
-			CommentFormattingContext.mapOptions(getPreferences());
-			CommentFormatter commentFormatter= new CommentFormatter(fTextMeasurement, getPreferences());
-			// TODO: pass minimal string, pass inferred indentation
-			TextEdit edit= commentFormatter.format(getPartitionKind(position.getType()), document.get(), position.getOffset(), position.getLength(), CommentRegion.INFER_INDENTATION, TextUtilities.getDefaultLineDelimiter(document));
+			TextEdit edit= null;
 			try {
-				
+				Map preferences= CommentFormattingContext.mapOptions(getPreferences());
+				CodeFormatter commentFormatter= new CommentFormatter(fTextMeasurement, preferences);
+				int sourceOffset= document.getLineOffset(document.getLineOfOffset(position.getOffset()));
+				int indentationLevel= inferIndentationLevel(document.get(sourceOffset, position.getOffset() - sourceOffset), getTabSize(preferences));
+				edit= commentFormatter.format(getPartitionKind(position.getType()), document.get(), position.getOffset(), position.getLength(), indentationLevel, TextUtilities.getDefaultLineDelimiter(document));
+			} catch (BadLocationException exception) {
+				JavaPlugin.log(exception);
+			}
+			
+			try {
 				if (edit != null)
 					edit.apply(document);
-				
 			} catch (MalformedTreeException exception) {
 				JavaPlugin.log(exception);
 			} catch (BadLocationException exception) {
@@ -106,6 +114,14 @@ public class CommentFormattingStrategy extends ContextBasedFormattingStrategy {
 		super.formatterStops();
 	}
 
+	/**
+	 * Map from {@link IJavaPartitions}comment partition types to
+	 * {@link CodeFormatter}code snippet kinds.
+	 * 
+	 * @param type the partition type
+	 * @return the code snippet kind
+	 * @since 3.1
+	 */
 	private static int getPartitionKind(String type) {
 		if (IJavaPartitions.JAVA_SINGLE_LINE_COMMENT.equals(type))
 				return JavaPartitions.K_SINGLE_LINE_COMMENT;
@@ -114,5 +130,52 @@ public class CommentFormattingStrategy extends ContextBasedFormattingStrategy {
 		if (IJavaPartitions.JAVA_DOC.equals(type))
 				return JavaPartitions.K_JAVA_DOC;
 		return -1;
+	}
+	
+	/**
+	 * Infer the indentation level based on the given reference indentation
+	 * and tab size.
+	 * 
+	 * @param reference the reference indentation
+	 * @param tabSize the tab size
+	 * @return the inferred indentation level
+	 * @since 3.1
+	 */
+	private static int inferIndentationLevel(String reference, int tabSize) {
+		int level= 0;
+		for (int i= 0, n= reference.length(), spaces= 0; i < n; i++) {
+			char ch= reference.charAt(i);
+			if (ch == ' ') {
+				spaces++;
+				if (spaces >= tabSize) {
+					spaces= 0;
+					level++;
+				}
+			} else if (ch == '\t') {
+				spaces= 0;
+				level++;
+			} else
+				throw new IllegalArgumentException();
+		}
+		return level;
+	}
+	
+	/**
+	 * Returns the value of {@link DefaultCodeFormatterConstants#FORMATTER_TAB_SIZE}
+	 * from the given preferences.
+	 * 
+	 * @param preferences the preferences
+	 * @return the value of {@link DefaultCodeFormatterConstants#FORMATTER_TAB_SIZE}
+	 *         from the given preferences
+	 * @since 3.1
+	 */
+	private static int getTabSize(Map preferences) {
+		if (preferences.containsKey(DefaultCodeFormatterConstants.FORMATTER_TAB_SIZE))
+			try {
+				return Integer.parseInt((String) preferences.get(DefaultCodeFormatterConstants.FORMATTER_TAB_SIZE));
+			} catch (NumberFormatException e) {
+				// use default
+			}
+		return 4;
 	}
 }
