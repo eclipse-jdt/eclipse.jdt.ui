cf5c552756a9796d66ecd0c4cf5cd2c70ccc19fa
Christof Marti <cmarti>
2004-10-27 10:35:34 +0000

    do not modify the original options map

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/comment/CommentFormattingContext.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/comment/CommentFormattingContext.java
@@ -11,7 +11,10 @@
 
 package org.eclipse.jdt.internal.ui.text.comment;
 
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 import org.eclipse.jface.text.formatter.FormattingContext;
 
@@ -25,6 +28,79 @@ import org.eclipse.jdt.internal.corext.text.comment.CommentFormatterPreferenceCo
  * @since 3.0
  */
 public class CommentFormattingContext extends FormattingContext {
+	
+	/**
+	 * A Map that translates keys.
+	 * 
+	 * @since 3.1
+	 */
+	private static class KeyMappingMap implements Map {
+		
+		/** Mapping visible keys to actual keys */
+		private Map fKeyMap;
+		
+		/** Backing Map */
+		private Map fBackingMap;
+		
+		public KeyMappingMap(Map keyMap, Map backingMap) {
+			fKeyMap= keyMap;
+			fBackingMap= backingMap;
+		}
+
+		private Object map(Object key) {
+			if (fKeyMap.containsKey(key))
+				return fKeyMap.get(key);
+			return key;
+		}
+
+		public int size() {
+			return fBackingMap.size();
+		}
+
+		public void clear() {
+			fBackingMap.clear();
+		}
+
+		public boolean isEmpty() {
+			return fBackingMap.isEmpty();
+		}
+
+		public boolean containsKey(Object key) {
+			return fBackingMap.containsKey(map(key));
+		}
+
+		public boolean containsValue(Object value) {
+			return fBackingMap.containsValue(value);
+		}
+
+		public Collection values() {
+			return fBackingMap.values();
+		}
+
+		public void putAll(Map t) {
+			throw new UnsupportedOperationException();
+		}
+
+		public Set entrySet() {
+			throw new UnsupportedOperationException();
+		}
+
+		public Set keySet() {
+			throw new UnsupportedOperationException();
+		}
+
+		public Object get(Object key) {
+			return fBackingMap.get(map(key));
+		}
+
+		public Object remove(Object key) {
+			return fBackingMap.remove(map(key));
+		}
+
+		public Object put(Object key, Object value) {
+			return fBackingMap.put(map(key), value);
+		}
+	}
 	
 	/**
 	 * Preference keys of this context's preferences.
@@ -58,6 +134,12 @@ public class CommentFormattingContext extends FormattingContext {
 		    CommentFormatterPreferenceConstants.FORMATTER_COMMENT_CLEARBLANKLINES, 
 		    CommentFormatterPreferenceConstants.FORMATTER_COMMENT_FORMATHTML };
 
+	/**
+	 * Map from JDT/UI preference keys to JDT/Core preference keys.
+	 * @since 3.1
+	 */
+	private static Map fPreferenceConstantsMap;
+
 	/*
 	 * @see org.eclipse.jface.text.formatter.IFormattingContext#getPreferenceKeys()
 	 */
@@ -84,7 +166,9 @@ public class CommentFormattingContext extends FormattingContext {
 	 * Map from JDT/Text preference keys to JDT/Core preference keys.
 	 * <p>
 	 * TODO: remove after migrating comment formatter preferences to
-	 * JDT/Core
+	 * JDT/Core preference store
+	 * </p><p>
+	 * NOTE: the returned Map may not fully implement the Map interface
 	 * </p>
 	 * 
 	 * @param preferences the JDT/Text preferences
@@ -92,15 +176,30 @@ public class CommentFormattingContext extends FormattingContext {
 	 * @since 3.1
 	 */
 	public static Map mapOptions(Map preferences) {
-		// TODO: stop modifying the original map
-		String[] keys= PREFERENCE_KEYS;
-		int n= keys.length;
-		Object[] values= new Object[n];
-		for (int i= 0; i < n; i++)
-			values[i]= preferences.get(keys[i]);
-		String[] remapedKeys= MAPPED_PREFERENCE_KEYS;
-		for (int i= 0; i < n; i++)
-			preferences.put(remapedKeys[i], values[i]);
-		return preferences;
+		Map preferenceConstantsMap= getPreferenceConstantsMap();
+		if (preferenceConstantsMap.size() == 0)
+			return preferences;
+		return new KeyMappingMap(preferenceConstantsMap, preferences);
+	}
+
+
+	/**
+	 * Returns a Map between JDT/UI and JDT/Core preference keys.
+	 * 
+	 * @return the Map between JDT/UI and JDT/Core preference keys
+	 * @since 3.1
+	 */
+	private static Map getPreferenceConstantsMap() {
+		if (fPreferenceConstantsMap == null) {
+			String[] keys= PREFERENCE_KEYS;
+			String[] mapedKeys= MAPPED_PREFERENCE_KEYS;
+			fPreferenceConstantsMap= new HashMap(keys.length);
+			for (int i= 0, n= keys.length; i < n; i++)
+				if (!mapedKeys[i].equals(keys[i])) {
+					fPreferenceConstantsMap.put(mapedKeys[i], keys[i]);
+					fPreferenceConstantsMap.put(keys[i], mapedKeys[i]);
+				}
+		}
+		return fPreferenceConstantsMap;
 	}
 }
