43538ca53861a621f2987c1629e55ba61587cafc
Christof Marti <cmarti>
2004-11-11 18:42:03 +0000

    consolidation

--- org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/OpenJavaContentAssistTest.java
+++ org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/OpenJavaContentAssistTest.java
@@ -17,83 +17,32 @@ import org.eclipse.test.performance.PerformanceMeter;
 
 import org.eclipse.jface.action.IAction;
 
-import org.eclipse.jface.text.BadLocationException;
-
-import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.texteditor.AbstractTextEditor;
 
 
-public class OpenJavaContentAssistTest extends TextPerformanceTestCase {
+public class OpenJavaContentAssistTest extends OpenQuickControlTest {
 	
 	private static final Class THIS= OpenJavaContentAssistTest.class;
 	
-	private static final String PATH= "/org.eclipse.swt/Eclipse SWT Custom Widgets/common/org/eclipse/swt/custom/";
-	
-	private static final String ORIG_NAME= "StyledText";
-	
-	private static final String ORIG_FILE= PATH + ORIG_NAME + ".java";
-
-	private static final int WARM_UP_RUNS= 10;
-
-	private static final int MEASURED_RUNS= 10;
-
 	private static final int LINE= 3897;
-
+	
 	public static Test suite() {
 		return new PerformanceTestSetup(new TestSuite(THIS));
 	}
-	
-	protected void setUp() throws Exception {
-		super.setUp();
-		setWarmUpRuns(WARM_UP_RUNS);
-		setMeasuredRuns(MEASURED_RUNS);
-		ResourceTestHelper.replicate(ORIG_FILE, PATH + ORIG_NAME, ".java", getWarmUpRuns() + getMeasuredRuns(), ORIG_NAME, ORIG_NAME, ResourceTestHelper.OVERWRITE_IF_EXISTS);
-		ResourceTestHelper.incrementalBuild();
-		EditorTestHelper.bringToTop();
-		EditorTestHelper.joinJobs(1000, 10000, 100);
-	}
-	
-	protected void tearDown() throws Exception {
-		super.tearDown();
-		ResourceTestHelper.delete(PATH + ORIG_NAME, ".java", getWarmUpRuns() + getMeasuredRuns());
-	}
 
-	public void testOpenContentAssist() throws Exception {
-		measureOpenContentAssist(getNullPerformanceMeter(), getNullPerformanceMeter(), 0, getWarmUpRuns());
-		PerformanceMeter coldMeter= createPerformanceMeter(getDefaultScenarioId() + "-cold");
-		PerformanceMeter warmMeter= createPerformanceMeter(getDefaultScenarioId() + "-warm");
-		measureOpenContentAssist(coldMeter, warmMeter, getWarmUpRuns(), getMeasuredRuns());
-		commitAllMeasurements();
-		assertAllPerformance();
-	}
-
-	private void measureOpenContentAssist(PerformanceMeter coldMeter, PerformanceMeter warmMeter, int index, int runs) throws PartInitException, BadLocationException {
-		for (int i= 0; i < runs; i++) {
-			String name= ORIG_NAME + (index + i);
-			String file= PATH + name + ".java";
-			AbstractTextEditor editor= (AbstractTextEditor) EditorTestHelper.openInEditor(ResourceTestHelper.findFile(file), true);
-			EditorTestHelper.joinReconciler(EditorTestHelper.getSourceViewer(editor), 100, 10000, 100);
-			
-			measureOpenContentAssist(editor, coldMeter);
-			measureOpenContentAssist(editor, warmMeter);
-			
-			EditorTestHelper.closeAllEditors();
-		}
-	}
-
-	private void measureOpenContentAssist(AbstractTextEditor editor, PerformanceMeter performanceMeter) throws BadLocationException {
-		IAction showContentAssist= editor.getAction("ContentAssistProposal");
+	protected IAction setUpMeasurement(AbstractTextEditor editor) throws Exception {
 		editor.selectAndReveal(EditorTestHelper.getDocument(editor).getLineOffset(LINE), 0);
-		EditorTestHelper.runEventQueue(10);
-		performanceMeter.start();
-		runAction(showContentAssist);
-		EditorTestHelper.runEventQueue();
-		performanceMeter.stop();
+		EditorTestHelper.runEventQueue(100);
+		return editor.getAction("ContentAssistProposal");
+	}
+
+	protected void tearDownMeasurement(AbstractTextEditor editor) throws Exception {
 		EditorTestHelper.closeAllPopUps(EditorTestHelper.getSourceViewer(editor));
 	}
-
-	private void runAction(IAction action) {
-		action.run();
-		EditorTestHelper.runEventQueue();
+	
+	public void test1() throws Exception {
+		PerformanceMeter coldMeter= createPerformanceMeter(getDefaultScenarioId() + "-cold");
+		PerformanceMeter warmMeter= createPerformanceMeter(getDefaultScenarioId() + "-warm");
+		measureOpenQuickControl(coldMeter, warmMeter);
 	}
 }
--- org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/OpenQuickOutlineTest.java
+++ org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/OpenQuickOutlineTest.java
@@ -21,28 +21,16 @@ import org.eclipse.test.performance.PerformanceMeter;
 
 import org.eclipse.jface.action.IAction;
 
-import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.texteditor.AbstractTextEditor;
-import org.eclipse.ui.texteditor.ITextEditor;
 
 import org.eclipse.jdt.ui.actions.IJavaEditorActionDefinitionIds;
 
-public class OpenQuickOutlineTest extends TextPerformanceTestCase {
+public class OpenQuickOutlineTest extends OpenQuickControlTest {
 	
 	private static final Class THIS= OpenQuickOutlineTest.class;
 	
 	private static final String SHORT_NAME_COLD= "Open Quick Outline (first time)";
 	
-	private static final String PATH= "/org.eclipse.swt/Eclipse SWT Custom Widgets/common/org/eclipse/swt/custom/";
-	
-	private static final String ORIG_NAME= "StyledText";
-	
-	private static final String ORIG_FILE= PATH + ORIG_NAME + ".java";
-
-	private static final int WARM_UP_RUNS= 10;
-
-	private static final int MEASURED_RUNS= 10;
-
 	private static final String OUTLINE_VIEW= "org.eclipse.ui.views.ContentOutline";
 
 	private boolean fWasOutlineViewShown;
@@ -53,51 +41,21 @@ public class OpenQuickOutlineTest extends TextPerformanceTestCase {
 	
 	protected void setUp() throws Exception {
 		super.setUp();
-		setWarmUpRuns(WARM_UP_RUNS);
-		setMeasuredRuns(MEASURED_RUNS);
 		fWasOutlineViewShown= EditorTestHelper.hideView(OUTLINE_VIEW);
-		ResourceTestHelper.replicate(ORIG_FILE, PATH + ORIG_NAME, ".java", getWarmUpRuns() + getMeasuredRuns(), ORIG_NAME, ORIG_NAME, ResourceTestHelper.OVERWRITE_IF_EXISTS);
-		ResourceTestHelper.incrementalBuild();
-		EditorTestHelper.bringToTop();
-		EditorTestHelper.joinJobs(1000, 10000, 100);
 	}
 	
 	protected void tearDown() throws Exception {
 		super.tearDown();
-		ResourceTestHelper.delete(PATH + ORIG_NAME, ".java", getWarmUpRuns() + getMeasuredRuns());
 		if (fWasOutlineViewShown)
 			EditorTestHelper.showView(OUTLINE_VIEW);
 	}
 
-	public void testOpenQuickOutline1() throws Exception {
-		measureOpenQuickOutline(getNullPerformanceMeter(), getNullPerformanceMeter(), 0, getWarmUpRuns());
-		PerformanceMeter coldMeter= createPerformanceMeterForSummary(getDefaultScenarioId() + "-cold", SHORT_NAME_COLD, Dimension.ELAPSED_PROCESS);
-		PerformanceMeter warmMeter= createPerformanceMeter(getDefaultScenarioId() + "-warm");
-		measureOpenQuickOutline(coldMeter, warmMeter, getWarmUpRuns(), getMeasuredRuns());
-		commitAllMeasurements();
-		assertAllPerformance();
+	protected IAction setUpMeasurement(AbstractTextEditor editor) throws Exception {
+		EditorTestHelper.runEventQueue(100);
+		return editor.getAction(IJavaEditorActionDefinitionIds.SHOW_OUTLINE);
 	}
 
-	private void measureOpenQuickOutline(PerformanceMeter coldMeter, PerformanceMeter warmMeter, int index, int runs) throws PartInitException {
-		for (int i= 0; i < runs; i++) {
-			String name= ORIG_NAME + (index + i);
-			String file= PATH + name + ".java";
-			AbstractTextEditor editor= (AbstractTextEditor) EditorTestHelper.openInEditor(ResourceTestHelper.findFile(file), true);
-			EditorTestHelper.joinReconciler(EditorTestHelper.getSourceViewer(editor), 100, 10000, 100);
-			
-			measureOpenQuickOutline(editor, coldMeter);
-			measureOpenQuickOutline(editor, warmMeter);
-			
-			EditorTestHelper.closeAllEditors();
-		}
-	}
-
-	private void measureOpenQuickOutline(ITextEditor editor, PerformanceMeter performanceMeter) {
-		IAction showOutline= editor.getAction(IJavaEditorActionDefinitionIds.SHOW_OUTLINE);
-		EditorTestHelper.joinJobs(500, 1000, 100);
-		performanceMeter.start();
-		runAction(showOutline);
-		performanceMeter.stop();
+	protected void tearDownMeasurement(AbstractTextEditor editor) throws Exception {
 		Shell shell= EditorTestHelper.getActiveDisplay().getActiveShell();
 		assertEquals("", shell.getText());
 		shell.dispose();
@@ -105,8 +63,9 @@ public class OpenQuickOutlineTest extends TextPerformanceTestCase {
 		assertFalse("".equals(shell.getText()));
 	}
 
-	private void runAction(IAction action) {
-		action.run();
-		EditorTestHelper.runEventQueue();
+	public void testOpenQuickOutline1() throws Exception {
+		PerformanceMeter coldMeter= createPerformanceMeterForSummary(getDefaultScenarioId() + "-cold", SHORT_NAME_COLD, Dimension.ELAPSED_PROCESS);
+		PerformanceMeter warmMeter= createPerformanceMeter(getDefaultScenarioId() + "-warm");
+		measureOpenQuickControl(coldMeter, warmMeter);
 	}
 }
