3884407b6798c99454593d0b64c1eb92c9256e4d
Christof Marti <cmarti>
2004-11-22 13:36:53 +0000

    cleanup

--- org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/JavaFormatterTest.java
+++ org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/JavaFormatterTest.java
@@ -62,13 +62,13 @@ public class JavaFormatterTest extends TextPerformanceTestCase {
 	 * @throws Exception
 	 */
 	public void test() throws Exception {
-		measureToggleComment(getNullPerformanceMeter(), getWarmUpRuns());
-		measureToggleComment(createPerformanceMeter(), getMeasuredRuns());
+		measureFormat(getNullPerformanceMeter(), getWarmUpRuns());
+		measureFormat(createPerformanceMeter(), getMeasuredRuns());
 		commitAllMeasurements();
 		assertAllPerformance();
 	}
 
-	private void measureToggleComment(PerformanceMeter performanceMeter, int runs) throws CoreException {
+	private void measureFormat(PerformanceMeter performanceMeter, int runs) throws CoreException {
 		IAction format= fEditor.getAction("Format");
 		for (int i= 0; i < runs; i++) {
 			fEditor.selectAndReveal(0, 0);
