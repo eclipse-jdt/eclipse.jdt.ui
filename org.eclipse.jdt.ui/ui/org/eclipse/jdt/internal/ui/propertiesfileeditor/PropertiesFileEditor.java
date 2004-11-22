9235eea59b8c83287b97cb37cb29b0d6a45326c1
Dani Megert <dmegert>
2004-11-22 15:23:30 +0000

    Fixed bug 78051: [nls tooling] Properties Editor has no 'Show-In'

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/propertiesfileeditor/PropertiesFileEditor.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/propertiesfileeditor/PropertiesFileEditor.java
@@ -18,8 +18,11 @@ import org.eclipse.jface.text.source.ISourceViewer;
 
 import org.eclipse.ui.editors.text.TextEditor;
 
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.part.IShowInTargetList;
 import org.eclipse.ui.texteditor.DefaultMarkerAnnotationAccess;
 
+import org.eclipse.jdt.ui.JavaUI;
 import org.eclipse.jdt.ui.PreferenceConstants;
 import org.eclipse.jdt.ui.text.JavaTextTools;
 
@@ -83,5 +86,22 @@ public class PropertiesFileEditor extends TextEditor {
 	 */
 	protected boolean affectsTextPresentation(PropertyChangeEvent event) {
 		return ((PropertiesFileSourceViewerConfiguration)getSourceViewerConfiguration()).affectsTextPresentation(event) || super.affectsTextPresentation(event);
+	}
+	
+	
+	/*
+	 * @see org.eclipse.ui.editors.text.TextEditor#getAdapter(java.lang.Class)
+	 * @since 3.1
+	 */
+	public Object getAdapter(Class adapter) {
+		if (adapter == IShowInTargetList.class) {
+			return new IShowInTargetList() {
+				public String[] getShowInTargetIds() {
+					return new String[] { JavaUI.ID_PACKAGES, IPageLayout.ID_RES_NAV };
+				}
+
+			};
+		}
+		return super.getAdapter(adapter);
 	}
 }
