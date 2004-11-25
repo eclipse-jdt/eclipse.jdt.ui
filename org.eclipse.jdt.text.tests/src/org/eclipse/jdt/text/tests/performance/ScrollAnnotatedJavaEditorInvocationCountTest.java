ea89724ed107791997f1edc315afe41c47254e28
cvs2svn <platform-releng-dev@eclipse.org>
2004-11-25 16:35:32 +0000

    This commit was manufactured by cvs2svn to create branch 'perf_301'.
    Cherrypick from master 2004-11-25 16:35:31 UTC Christof Marti <cmarti> 'cleanup':
        org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/AbstractScrollAnnotatedJavaEditorTest.java
        org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/ActivateEditorTest.java
        org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/ActivateJavaEditorTest.java
        org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/ActivateTextEditorTest.java
        org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/DebuggingPerformanceTestSuite.java
        org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/DisableAutoBuildTestSetup.java
        org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/ITextEditorTestCase.java
        org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/InvocationCountExampleTest.java
        org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/InvocationCountPerformanceMeter.java
        org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/JavaDocIndentStrategyTest.java
        org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/JavaExpandSelectionTest.java
        org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/JavaFormatterProjectTest.java
        org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/JavaFormatterTest.java
        org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/JavaMoveLineTest.java
        org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/JavaReplaceAllTest.java
        org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/JavaTypingInvocationCountTest.java
        org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/MoveLineTest.java
        org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/OpenJavaContentAssistTest.java
        org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/OpenJavaEditorInvocationCountTest.java
        org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/OpenQuickControlTest.java
        org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/OpenTextEditorStartupTest.java
        org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/QuickDiffInvocationCountTest.java
        org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/SaveEditorTest.java
        org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/SaveJavaEditorTest.java
        org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/SaveTextEditorTest.java
        org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/ScrollAnnotatedJavaEditorInvocationCountTest.java
        org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/TextEditorTestSetup.java
        org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/TextMoveLineTest.java
        org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/TextPluginTestSetup.java
        org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/TextTypingInvocationCountTest.java
        org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/TypingInvocationCountTest.java
        org.eclipse.jdt.text.tests/testResources/org.eclipse.text-R3_0.zip
        org.eclipse.jdt.text.tests/testResources/typingInvocationCountTest1/Test.java
        org.eclipse.jdt.ui.tests/testresources/junit381-noUI-src.zip

---
+++ org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/ScrollAnnotatedJavaEditorInvocationCountTest.java
@@ -0,0 +1,84 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2004 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.jdt.text.tests.performance;
+
+import java.lang.reflect.Method;
+
+import org.eclipse.swt.events.PaintEvent;
+
+import org.eclipse.jface.text.source.AnnotationPainter;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * Measure the number of invocations of {@link org.eclipse.jface.text.source.AnnotationPainter#paintControl(PaintEvent)}
+ * while scrolling with error annotations in the Java editor.
+ * 
+ * @since 3.1
+ */
+public class ScrollAnnotatedJavaEditorInvocationCountTest extends AbstractScrollAnnotatedJavaEditorTest {
+	
+	private static final Class THIS= ScrollAnnotatedJavaEditorInvocationCountTest.class;
+
+	public static Test suite() {
+		return new PerformanceTestSetup(new TestSuite(THIS));
+	}
+
+	/**
+	 * Measure the number of invocations of {@link org.eclipse.jface.text.source.AnnotationPainter#paintControl(PaintEvent)}
+	 * while scrolling page wise with error annotations in the Java editor.
+	 * 
+	 * @throws Exception
+	 */
+	public void testPageWise() throws Exception {
+		measure(PAGE_WISE, createInvocationCountPerformanceMeter(), 0, 1);
+	}
+
+	/**
+	 * Measure the number of invocations of {@link org.eclipse.jface.text.source.AnnotationPainter#paintControl(PaintEvent)}
+	 * while scrolling line wise with error annotations in the Java editor.
+	 * 
+	 * @throws Exception
+	 */
+	public void testLineWise() throws Exception {
+		measure(LINE_WISE, createInvocationCountPerformanceMeter(), 0, 1);
+	}
+	
+	/**
+	 * Measure the number of invocations of {@link org.eclipse.jface.text.source.AnnotationPainter#paintControl(PaintEvent)}
+	 * while scrolling and selecting line wise with error annotations in the
+	 * Java editor.
+	 * 
+	 * @throws Exception
+	 */
+	public void testLineWiseSelect() throws Exception {
+		measure(LINE_WISE_SELECT, createInvocationCountPerformanceMeter(), 0, 1);
+	}
+
+	/**
+	 * Measure the number of invocations of {@link org.eclipse.jface.text.source.AnnotationPainter#paintControl(PaintEvent)}
+	 * while scrolling line wise without moving the caret with error
+	 * annotations in the Java editor.
+	 * 
+	 * @throws Exception
+	 */
+	public void testLineWiseNoCaretMove() throws Exception {
+		measure(LINE_WISE_NO_CARET_MOVE, createInvocationCountPerformanceMeter(), 0, 1);
+	}
+
+	private InvocationCountPerformanceMeter createInvocationCountPerformanceMeter() throws NoSuchMethodException {
+		return createInvocationCountPerformanceMeter(new Method[] {
+			AnnotationPainter.class.getMethod("paintControl", new Class[] { PaintEvent.class }),
+		});
+	}
+}
---
+++ org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/TextTypingInvocationCountTest.java
@@ -0,0 +1,65 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2004 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.jdt.text.tests.performance;
+
+import org.eclipse.swt.events.PaintEvent;
+
+import org.eclipse.jface.text.source.AnnotationPainter;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * Counts number of repaints ({@link AnnotationPainter#paintControl(PaintEvent)})
+ * when typing on a line with annotations shown as squiggles in the text editor.
+ * 
+ * @since 3.1
+ */
+public class TextTypingInvocationCountTest extends TypingInvocationCountTest {
+
+	private static class Setup extends TypingInvocationCountTest.Setup {
+		
+		public Setup(Test test) {
+			super(test);
+		}
+		
+		protected String getEditorId() {
+			return EditorTestHelper.TEXT_EDITOR_ID;
+		}
+
+		protected String getPerspectiveId() {
+			return EditorTestHelper.RESOURCE_PERSPECTIVE_ID;
+		}
+	}
+
+	private static final Class THIS= TypingInvocationCountTest.class;
+	
+	public TextTypingInvocationCountTest() {
+		super();
+	}
+	public TextTypingInvocationCountTest(String name) {
+		super(name);
+	}
+	
+	public static Test suite() {
+		TestSuite suite= new TestSuite(THIS.getName());
+		suite.addTest(new TextTypingInvocationCountTest("test00"));
+		suite.addTest(new TextTypingInvocationCountTest("test01"));
+		suite.addTest(new TextTypingInvocationCountTest("test02"));
+		suite.addTest(new TextTypingInvocationCountTest("test03"));
+		suite.addTest(new TextTypingInvocationCountTest("test10"));
+		suite.addTest(new TextTypingInvocationCountTest("test11"));
+		suite.addTest(new TextTypingInvocationCountTest("test12"));
+		suite.addTest(new TextTypingInvocationCountTest("test13"));
+		return new Setup(suite);
+	}
+}
