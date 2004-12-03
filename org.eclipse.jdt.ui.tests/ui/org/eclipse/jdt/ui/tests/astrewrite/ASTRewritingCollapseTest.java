71a3b117cf0284340afb4d2ddddc579b83ad79b5
Martin Aeschlimann <maeschli>
2004-12-03 15:26:49 +0000

    new API 'createGroupNode'

--- org.eclipse.jdt.ui.tests/ui/org/eclipse/jdt/ui/tests/astrewrite/ASTRewritingCollapseTest.java
+++ org.eclipse.jdt.ui.tests/ui/org/eclipse/jdt/ui/tests/astrewrite/ASTRewritingCollapseTest.java
@@ -16,30 +16,26 @@ import java.util.List;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+import org.eclipse.jdt.testplugin.JavaProjectHelper;
+import org.eclipse.jdt.testplugin.TestOptions;
+
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.IPackageFragment;
 import org.eclipse.jdt.core.IPackageFragmentRoot;
 import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.formatter.DefaultCodeFormatterConstants;
+
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.Block;
 import org.eclipse.jdt.core.dom.CompilationUnit;
-import org.eclipse.jdt.core.dom.ExpressionStatement;
 import org.eclipse.jdt.core.dom.IfStatement;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
-import org.eclipse.jdt.core.dom.MethodInvocation;
-import org.eclipse.jdt.core.dom.ReturnStatement;
-import org.eclipse.jdt.core.dom.Statement;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
-import org.eclipse.jdt.core.formatter.DefaultCodeFormatterConstants;
-
-import org.eclipse.jdt.testplugin.JavaProjectHelper;
-import org.eclipse.jdt.testplugin.TestOptions;
 
 import org.eclipse.jdt.ui.tests.core.ProjectTestSetup;
 
-import org.eclipse.jdt.internal.corext.dom.NodeFinder;
 import org.eclipse.jdt.internal.corext.dom.OldASTRewrite;
 
 /**
@@ -86,102 +82,6 @@ public class ASTRewritingCollapseTest extends ASTRewritingTest {
 	}
 	
 // tests for collapse	
-	
-	
-	public void testCollapsedTargetNodes() throws Exception {
-		IPackageFragment pack1= fSourceFolder.createPackageFragment("test1", false, null);
-		
-		StringBuffer buf= new StringBuffer();
-		buf.append("package test1;\n");
-		buf.append("public class E {\n");
-		buf.append("    public void foo(Object o) {\n");
-		buf.append("        return;\n");
-		buf.append("    }\n");
-		buf.append("}\n");
-		ICompilationUnit cu= pack1.createCompilationUnit("E.java", buf.toString(), false, null);
-
-		CompilationUnit astRoot= createAST(cu);
-		AST ast= astRoot.getAST();
-		OldASTRewrite rewrite= new OldASTRewrite(astRoot);
-		
-		String str= "return;";
-		ReturnStatement returnStatement= (ReturnStatement) NodeFinder.perform(astRoot, buf.indexOf(str), str.length());
-		
-		MethodInvocation newMethodInv1= ast.newMethodInvocation();
-		newMethodInv1.setName(ast.newSimpleName("foo1"));
-		ExpressionStatement st1= ast.newExpressionStatement(newMethodInv1);
-		
-		MethodInvocation newMethodInv2= ast.newMethodInvocation();
-		newMethodInv2.setName(ast.newSimpleName("foo2"));
-		ExpressionStatement st2= ast.newExpressionStatement(newMethodInv2);
-		
-		Block placeholder= rewrite.getCollapseTargetPlaceholder(new Statement[] { st1, st2 });
-		rewrite.replace(returnStatement, placeholder, null);
-			
-		String preview= evaluateRewrite(cu, rewrite); 
-		
-		buf= new StringBuffer();
-		buf.append("package test1;\n");
-		buf.append("public class E {\n");
-		buf.append("    public void foo(Object o) {\n");
-		buf.append("        foo1();\n");
-		buf.append("        foo2();\n");
-		buf.append("    }\n");
-		buf.append("}\n");
-		String expected= buf.toString();		
-
-		assertEqualString(preview, expected);
-	}
-	
-	public void testCollapsedTargetNodes2() throws Exception {
-		IPackageFragment pack1= fSourceFolder.createPackageFragment("test1", false, null);
-		
-		StringBuffer buf= new StringBuffer();
-		buf.append("package test1;\n");
-		buf.append("public class E {\n");
-		buf.append("    public void foo(Object o) {\n");
-		buf.append("        {\n");
-		buf.append("            return;\n");
-		buf.append("        }\n");
-		buf.append("    }\n");
-		buf.append("}\n");
-		ICompilationUnit cu= pack1.createCompilationUnit("E.java", buf.toString(), false, null);
-
-		CompilationUnit astRoot= createAST(cu);
-		AST ast= astRoot.getAST();
-		OldASTRewrite rewrite= new OldASTRewrite(astRoot);
-		
-		String str= "return;";
-		ReturnStatement returnStatement= (ReturnStatement) NodeFinder.perform(astRoot, buf.indexOf(str), str.length());
-		
-		MethodInvocation newMethodInv1= ast.newMethodInvocation();
-		newMethodInv1.setName(ast.newSimpleName("foo1"));
-		ExpressionStatement st1= ast.newExpressionStatement(newMethodInv1);
-		
-		MethodInvocation newMethodInv2= ast.newMethodInvocation();
-		newMethodInv2.setName(ast.newSimpleName("foo2"));
-		ExpressionStatement st2= ast.newExpressionStatement(newMethodInv2);
-		
-		ReturnStatement st3= (ReturnStatement) rewrite.createCopyTarget(returnStatement);
-		
-		Block placeholder= rewrite.getCollapseTargetPlaceholder(new Statement[] { st1, st2, st3 });
-		rewrite.replace(returnStatement.getParent(), placeholder, null);
-		
-		String preview= evaluateRewrite(cu, rewrite);
-		
-		buf= new StringBuffer();
-		buf.append("package test1;\n");
-		buf.append("public class E {\n");
-		buf.append("    public void foo(Object o) {\n");
-		buf.append("        foo1();\n");
-		buf.append("        foo2();\n");
-		buf.append("        return;\n");
-		buf.append("    }\n");
-		buf.append("}\n");
-		String expected= buf.toString();		
-
-		assertEqualString(preview, expected);
-	}
 	
 	
 	public void testMoveCollapsedCD() throws Exception {
