d4280ddc53bcfaf926b8327aaa3c73cb28e213a4
Martin Aeschlimann <maeschli>
2004-12-08 11:45:12 +0000

    using new API for cast and assign checks

--- org.eclipse.jdt.ui/core extension/org/eclipse/jdt/internal/corext/dom/TypeRules.java
+++ org.eclipse.jdt.ui/core extension/org/eclipse/jdt/internal/corext/dom/TypeRules.java
@@ -41,57 +41,7 @@ public class TypeRules {
 		}
 		return getTypeOrder(definedTypeCode) > getTypeOrder(toAssignCode);
 	}
-	
-	/**
-	 * Tests if a two types are assign compatible
-	 * @param typeToAssign The binding of the type to assign
-	 * @param definedType The type of the object that is assigned
-	 * @return boolean Returns true if definedType = typeToAssign is true
-	 */
-	public static boolean canAssign(ITypeBinding typeToAssign, String definedType) {
-		// definedType = typeToAssign;
-
-		int arrStart= definedType.indexOf('[');
-		if (arrStart != -1) {
-			if (!typeToAssign.isArray()) {
-				return false; // can not assign a non-array type 
-			}
-			int definedDim= definedType.length() - arrStart;
-			int toAssignDim= typeToAssign.getDimensions() * 2;
-			if (definedDim == toAssignDim) {
-				definedType= definedType.substring(0, arrStart);
-				typeToAssign= typeToAssign.getElementType();
-				if (typeToAssign.isPrimitive() && !definedType.equals(typeToAssign.getName())) {
-					return false; // can't assign arrays of primitive types to each other
-				}
-				return canAssign(typeToAssign, definedType);
-			} else if (definedDim < toAssignDim) {
-				return isArrayCompatible(definedType.substring(0, arrStart));
-			}
-			return false;
-		}
-
-		PrimitiveType.Code definedTypeCode= PrimitiveType.toCode(definedType);
-		if (typeToAssign.isPrimitive()) {
-			if (definedTypeCode == null) {
-				return false;
-			}
-			PrimitiveType.Code toAssignCode= PrimitiveType.toCode(typeToAssign.getName());
-			return canAssignPrimitive(toAssignCode, definedTypeCode);
-		} else {
-			if (definedTypeCode != null) {
-				return false;
-			}
-			if (typeToAssign.isArray()) {
-				return isArrayCompatible(definedType);
-			}
-			if ("java.lang.Object".equals(definedType)) { //$NON-NLS-1$
-				return true;
-			}
-			return (Bindings.findTypeInHierarchy(typeToAssign, definedType) != null);
-		}
-	}
-	
+		
 	/**
 	 * Tests if two types are assign compatible. Void types are never compatible.
 	 * @param typeToAssign The binding of the type to assign
@@ -99,56 +49,7 @@ public class TypeRules {
 	 * @return boolean Returns true if definedType = typeToAssign is true
 	 */
 	public static boolean canAssign(ITypeBinding typeToAssign, ITypeBinding definedType) {
-		// definedType = typeToAssign;
-		
-		String voidName= PrimitiveType.VOID.toString();
-		if (voidName.equals(typeToAssign.getName()) || voidName.equals(definedType.getName())) {
-			return false;
-		}
-
-		if (typeToAssign.isNullType()) {
-			return !definedType.isPrimitive();
-		}
-		if (definedType.isArray()) {
-			if (!typeToAssign.isArray()) {
-				return false; // can not assign a non-array type to an array
-			}
-			int definedDim= definedType.getDimensions();
-			int toAssignDim= typeToAssign.getDimensions();
-			if (definedDim == toAssignDim) {
-				definedType= definedType.getElementType();
-				typeToAssign= typeToAssign.getElementType();
-				if (typeToAssign.isPrimitive() && typeToAssign != definedType) {
-					return false; // can't assign arrays of different primitive types to each other
-				}
-				// fall through
-			} else if (definedDim < toAssignDim) {
-				return isArrayCompatible(definedType.getElementType());
-			} else {
-				return false;
-			}
-		}
-
-		if (typeToAssign.isPrimitive()) {
-			if (!definedType.isPrimitive()) {
-				return false;
-			}
-			PrimitiveType.Code toAssignCode= PrimitiveType.toCode(typeToAssign.getName());
-			PrimitiveType.Code definedTypeCode= PrimitiveType.toCode(definedType.getName());
-			return canAssignPrimitive(toAssignCode, definedTypeCode);
-		} else {
-			if (definedType.isPrimitive()) {
-				return false;
-			}
-
-			if (typeToAssign.isArray()) {
-				return isArrayCompatible(definedType);
-			}
-			if (isJavaLangObject(definedType)) { //$NON-NLS-1$
-				return true;
-			}
-			return Bindings.isSuperType(definedType, typeToAssign);
-		}
+		return typeToAssign.isAssignmentCompatible(definedType);
 	}
 
 	private static int getTypeOrder(Code type) {
@@ -167,10 +68,6 @@ public class TypeRules {
 		if (type == PrimitiveType.DOUBLE)
 			return 7;
 		return 0;
-	}
-
-	private static boolean isArrayCompatible(String definedType) {
-		return "java.lang.Object".equals(definedType) || "java.io.Serializable".equals(definedType) || "java.lang.Cloneable".equals(definedType); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 	}
 	
 	public static boolean isArrayCompatible(ITypeBinding definedType) {
@@ -196,60 +93,7 @@ public class TypeRules {
 	 * @return boolean Returns true if (castType) bindingToCast is a valid cast expression (can be unnecessary, but not invalid).
 	 */
 	public static boolean canCast(ITypeBinding castType, ITypeBinding bindingToCast) {
-		String voidName= PrimitiveType.VOID.toString();
-		
-		if (castType.isAnonymous() || castType.isNullType() || voidName.equals(castType.getName())) {
-			throw new IllegalArgumentException();
-		}
-		
-		if (castType == bindingToCast) {
-			return true;
-		}
-		
-		if (voidName.equals(bindingToCast.getName())) {
-			return false;
-		}
-		
-		if (bindingToCast.isArray()) {
-			if (!castType.isArray()) {
-				return isArrayCompatible(castType); // can not cast an arraytype to a non array type (except to Object, Serializable...)
-			}
-
-			int toCastDim= bindingToCast.getDimensions();
-			int castTypeDim= castType.getDimensions();
-			if (toCastDim == castTypeDim) {
-				bindingToCast= bindingToCast.getElementType();
-				castType= castType.getElementType();
-				if (castType.isPrimitive() && castType != bindingToCast) {
-					return false; // can't assign arrays of different primitive types to each other
-				}
-				// fall through
-			} else if (toCastDim < castTypeDim) {
-				return isArrayCompatible(bindingToCast.getElementType());
-			} else {
-				return isArrayCompatible(castType.getElementType());
-			}
-		}
-		if (castType.isPrimitive()) {
-			if (!bindingToCast.isPrimitive()) {
-				return false;
-			}
-			String boolName= PrimitiveType.BOOLEAN.toString();
-			return (!boolName.equals(castType.getName()) && !boolName.equals(bindingToCast.getName()));
-		} else {
-			if (bindingToCast.isPrimitive()) {
-				return false;
-			}
-			if (castType.isArray()) {
-				return isArrayCompatible(bindingToCast);
-			}
-			
-			if (castType.isInterface() || bindingToCast.isInterface() || isJavaLangObject(castType)) {
-				return true;
-			}
-			
-			return Bindings.isSuperType(bindingToCast, castType) || Bindings.isSuperType(castType, bindingToCast);
-		}
+		return bindingToCast.isCastCompatible(castType);
 	}
 	
 }
