8b9e5323e2eba00b6a434d3baf2c88404a424115
Christof Marti <cmarti>
2004-12-09 16:36:27 +0000

    traverse content type hierarchy to find engine

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/spelling/newapi/DefaultSpellingEngine.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/spelling/newapi/DefaultSpellingEngine.java
@@ -15,6 +15,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.content.IContentType;
 
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IRegion;
@@ -30,28 +32,51 @@ import org.eclipse.ui.texteditor.spelling.SpellingContext;
  */
 public class DefaultSpellingEngine implements ISpellingEngine {
 	
-	private static final String JAVA_CONTENT_TYPE_ID= "org.eclipse.jdt.core.javaSource"; //$NON-NLS-1$
+	/** Text content type */
+	private static final IContentType TEXT_CONTENT_TYPE= Platform.getContentTypeManager().getContentType("org.eclipse.core.runtime.text"); //$NON-NLS-1$
+
+	/** Java source content type */
+	private static final IContentType JAVA_CONTENT_TYPE= Platform.getContentTypeManager().getContentType("org.eclipse.jdt.core.javaSource"); //$NON-NLS-1$
 	
-//	private static final String PROPERTIES_CONTENT_TYPE_ID= "org.eclipse.jdt.core.javaProperties"; //$NON-NLS-1$
+	/** Java properties content type */
+//	private static final IContentType PROPERTIES_CONTENT_TYPE= Platform.getContentTypeManager().getContentType("org.eclipse.jdt.core.javaProperties"); //$NON-NLS-1$
 	
+	/** Available spelling engines by content type */
 	private Map fEngines= new HashMap();
 	
-	private ISpellingEngine fDefaultEngine;
-
 	public DefaultSpellingEngine() {
-		fEngines.put(JAVA_CONTENT_TYPE_ID, new JavaSpellingEngine());
-//		fEngines.put(PROPERTIES_CONTENT_TYPE_ID, new PropertiesFileSpellingEngine());
-		fDefaultEngine= new TextSpellingEngine();
+		if (JAVA_CONTENT_TYPE != null)
+			fEngines.put(JAVA_CONTENT_TYPE, new JavaSpellingEngine());
+//		if (PROPERTIES_CONTENT_TYPE != null)
+//			fEngines.put(PROPERTIES_CONTENT_TYPE, new PropertiesFileSpellingEngine());
+		if (TEXT_CONTENT_TYPE != null)
+			fEngines.put(TEXT_CONTENT_TYPE, new TextSpellingEngine());
 	}
 	
 	/*
 	 * @see org.eclipse.ui.texteditor.spelling.ISpellingEngine#check(org.eclipse.jface.text.IDocument, org.eclipse.jface.text.IRegion[], org.eclipse.ui.texteditor.spelling.SpellingContext, org.eclipse.ui.texteditor.spelling.ISpellingProblemCollector, org.eclipse.core.runtime.IProgressMonitor)
 	 */
 	public void check(IDocument document, IRegion[] regions, SpellingContext context, ISpellingProblemCollector collector, IProgressMonitor monitor) {
-		String contentTypeId= context.getContentType().getId();
-		if (fEngines.containsKey(contentTypeId))
-			((ISpellingEngine) fEngines.get(contentTypeId)).check(document, regions, context, collector, monitor);
-		else
-			fDefaultEngine.check(document, regions, context, collector, monitor);
+		ISpellingEngine engine= getEngine(context.getContentType());
+		if (engine != null)
+			engine.check(document, regions, context, collector, monitor);
+	}
+
+	/**
+	 * Returns a spelling engine for the given content type or
+	 * <code>null</code> if none could be found.
+	 * 
+	 * @param contentType the content type
+	 * @return a spelling engine for the given content type or
+	 *         <code>null</code> if none could be found
+	 */
+	private ISpellingEngine getEngine(IContentType contentType) {
+		if (contentType == null)
+			return null;
+		
+		if (fEngines.containsKey(contentType))
+			return (ISpellingEngine) fEngines.get(contentType);
+		
+		return getEngine(contentType.getBaseType());
 	}
 }
