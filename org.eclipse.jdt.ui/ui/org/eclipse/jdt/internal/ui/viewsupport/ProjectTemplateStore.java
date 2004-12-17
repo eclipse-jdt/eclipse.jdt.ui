2209a5237ba5de910ba63d2b59202cb42d0e0fea
teicher <teicher>
2004-12-17 17:51:24 +0000

    support instance/project settings

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/viewsupport/ProjectTemplateStore.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/viewsupport/ProjectTemplateStore.java
@@ -11,156 +11,127 @@
 package org.eclipse.jdt.internal.ui.viewsupport;
 
 import java.io.IOException;
-import java.io.Reader;
-import java.io.StringReader;
-import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ProjectScope;
-import org.eclipse.core.runtime.preferences.IEclipsePreferences;
+
+import org.eclipse.jface.preference.IPreferenceStore;
 
 import org.eclipse.jface.text.templates.Template;
 import org.eclipse.jface.text.templates.persistence.TemplatePersistenceData;
-import org.eclipse.jface.text.templates.persistence.TemplateReaderWriter;
 import org.eclipse.jface.text.templates.persistence.TemplateStore;
+
+import org.eclipse.ui.preferences.ScopedPreferenceStore;
 
 import org.eclipse.jdt.ui.JavaUI;
 
 import org.eclipse.jdt.internal.ui.JavaPlugin;
 
-
 /**
  * @since 3.1
  */
 public final class ProjectTemplateStore {
-
-	private final TemplateStore fParent;
-	private final IEclipsePreferences fPreferences;
-	private static final String fKey= "org.eclipse.jdt.ui.text.custom_code_templates"; //$NON-NLS-1$
 	
-	private static final class InheritedTemplatePersistenceData extends TemplatePersistenceData {
+	private static final String KEY= "org.eclipse.jdt.ui.text.custom_code_templates"; //$NON-NLS-1$
 
-		public InheritedTemplatePersistenceData(Template template, boolean enabled, String id) {
-			super(template, enabled, id);
+	private final TemplateStore fInstanceStore;
+	private final TemplateStore fProjectStore;
+
+	private final Map fDatas= new HashMap();
+	
+	public ProjectTemplateStore(IProject project) {
+		fInstanceStore= JavaPlugin.getDefault().getCodeTemplateStore();
+		if (project == null) {
+			fProjectStore= null;
+		} else {
+			IPreferenceStore projectSettings= new ScopedPreferenceStore(new ProjectScope(project), JavaUI.ID_PLUGIN);
+			fProjectStore= new TemplateStore(projectSettings, KEY);
 		}
 	}
-
-	public ProjectTemplateStore(IProject project) {
-		fParent= JavaPlugin.getDefault().getCodeTemplateStore();
-		fPreferences= new ProjectScope(project).getNode(JavaUI.ID_PLUGIN);
+	
+	public TemplatePersistenceData[] getTemplateData(boolean includeDeleted) {
+		if (fProjectStore != null)
+			return (TemplatePersistenceData[]) fDatas.values().toArray(new TemplatePersistenceData[fDatas.size()]);
+		else
+			return fInstanceStore.getTemplateData(includeDeleted);
 	}
 	
-	/**
-	 * Returns the set of project specific or inherited templates. Inherited
-	 * ones are copied if <code>inherit</code> is <code>true</code>.
-	 * 
-	 * @param includeDeleted
-	 * @param inherit
-	 * @return the set of project specific or inherited templates
-	 */
-	public TemplatePersistenceData[] getTemplateData(boolean includeDeleted, boolean inherit) {
-		Map datas= null;
-		if (inherit) {
-			TemplatePersistenceData[] templateData= fParent.getTemplateData(false);
-			if (fPreferences == null)
-				return templateData;
-			
-			datas= new HashMap();
+	public Template findTemplateById(String id) {
+		Template template= null;
+		if (fProjectStore != null)
+			template= fProjectStore.findTemplateById(id);
+		if (template == null)
+			template= fInstanceStore.findTemplateById(id);
+		
+		return template;
+	}
+	
+	public void load() throws IOException {
+		if (fProjectStore == null) {
+			fInstanceStore.load();
+		} else {
+			fDatas.clear();
+			TemplatePersistenceData[] templateData= fInstanceStore.getTemplateData(false);
 			for (int i= 0; i < templateData.length; i++) {
 				final TemplatePersistenceData original= templateData[i];
 				final String id= original.getId();
 				if (id != null) {
-					TemplatePersistenceData copy= new InheritedTemplatePersistenceData(original.getTemplate(), original.isEnabled(), id);
-					datas.put(id, copy);
+					TemplatePersistenceData copy= new TemplatePersistenceData(original.getTemplate(), original.isEnabled(), id);
+					fDatas.put(id, copy);
 				}
 			}
-		}
-		
-		if (datas == null)
-			datas= new HashMap();
-		
-		String pref= fPreferences.get(fKey, null);
-		if (pref != null && pref.trim().length() > 0) {
-			Reader input= new StringReader(pref);
-			TemplateReaderWriter reader= new TemplateReaderWriter();
-			TemplatePersistenceData[] ds;
-			try {
-				ds= reader.read(input);
-				for (int i= 0; i < ds.length; i++) {
-					String id= ds[i].getId();
-					if (id != null)
-						datas.put(id, ds[i].getTemplate());
-				}
-			} catch (IOException e) {
-				JavaPlugin.log(e);
+
+			TemplatePersistenceData[] ds= fProjectStore.getTemplateData(false);
+			for (int i= 0; i < ds.length; i++) {
+				final TemplatePersistenceData original= ds[i];
+				String id= original.getId();
+				if (id != null && !original.isDeleted())
+					fDatas.put(id, original);
 			}
 		}
+	}
+	
+	public boolean isInheriting(TemplatePersistenceData data) {
+		if (fProjectStore == null)
+			return false;
 		
-		return (TemplatePersistenceData[]) datas.values().toArray(new TemplatePersistenceData[datas.values().size()]);
+		return data.isDeleted() || fProjectStore.findTemplateById(data.getId()) == null;
 	}
 	
-	public boolean isInherited(TemplatePersistenceData data) {
-		return data instanceof InheritedTemplatePersistenceData;
-	}
-	
-	public Template findTemplateById(String id) {
-		String projectSetting= fPreferences.get(fKey, null);
-		if (projectSetting != null) {
-			Template template= loadCustomTemplate(id);
-			if (template != null)
-				return template;
-		}
-		return fParent.findTemplateById(id);
-	}
-	
-	private Template loadCustomTemplate(String id) {
-		if (fPreferences == null)
-			return null;
-		String pref= fPreferences.get(fKey, null);
-		if (pref != null && pref.trim().length() > 0) {
-			Reader input= new StringReader(pref);
-			try {
-				TemplateReaderWriter reader= new TemplateReaderWriter();
-				final TemplatePersistenceData data= reader.readSingle(input, id);
-				if (data != null)
-					return data.getTemplate();
-			} catch (IOException e) {
-				JavaPlugin.log(e);
-			}
-		}
-		return null;
-	}
-
-	public void store(TemplatePersistenceData[] datas) {
-		StringWriter output= new StringWriter();
-		TemplateReaderWriter writer= new TemplateReaderWriter();
-		try {
-			writer.save(datas, output);
-		} catch (IOException e) {
-			JavaPlugin.log(e);
-		}
+	public void makeProjectSpecific(TemplatePersistenceData data) {
+		if (fProjectStore == null)
+			return;
 		
-		fPreferences.put(fKey, output.toString());
+		if (fProjectStore.findTemplateById(data.getId()) == null)
+			fProjectStore.add(data);
+		data.setDeleted(false);
 	}
-
-	/**
-	 * @throws IOException
-	 */
-	public void load() throws IOException {
-		fParent.load();
+	
+	public void makeInheriting(TemplatePersistenceData data) {
+		if (fProjectStore == null)
+			return;
+		
+		data.setDeleted(true);
 	}
-	/**
-	 * 
-	 */
+	
 	public void restoreDefaults() {
-		fParent.restoreDefaults();
+		if (fProjectStore == null) {
+			fInstanceStore.restoreDefaults();
+		} else {
+			try {
+				load();
+			} catch (IOException e) {
+				JavaPlugin.log(e);
+			}
+		}
 	}
-	/**
-	 * @throws IOException
-	 */
 	public void save() throws IOException {
-		fParent.save();
+		if (fProjectStore == null) {
+			fInstanceStore.save();
+		} else {
+			fProjectStore.save();
+		}
 	}
 }
