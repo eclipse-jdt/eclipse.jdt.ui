6d111377f944215c462664489c7886c07188c036
Dani Megert <dmegert>
2005-01-14 10:53:10 +0000

    Fixed bug 82204: [key binding] Shortcut for "Open Declaration" doesn't show in context menu of Java Editor

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/ui/actions/OpenEditorActionGroup.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/ui/actions/OpenEditorActionGroup.java
@@ -58,6 +58,7 @@ public class OpenEditorActionGroup extends ActionGroup {
 	public OpenEditorActionGroup(IViewPart part) {
 		fSite= part.getSite();
 		fOpen= new OpenAction(fSite);
+		fOpen.setActionDefinitionId(IJavaEditorActionDefinitionIds.OPEN_EDITOR);
 		initialize(fSite.getSelectionProvider());
 	}
 	
@@ -67,6 +68,7 @@ public class OpenEditorActionGroup extends ActionGroup {
 	public OpenEditorActionGroup(JavaEditor part) {
 		fIsEditorOwner= true;
 		fOpen= new OpenAction(part);
+		fOpen.setActionDefinitionId(IJavaEditorActionDefinitionIds.OPEN_EDITOR);
 		part.setAction("OpenEditor", fOpen); //$NON-NLS-1$
 		fSite= part.getEditorSite();
 		initialize(fSite.getSelectionProvider());
