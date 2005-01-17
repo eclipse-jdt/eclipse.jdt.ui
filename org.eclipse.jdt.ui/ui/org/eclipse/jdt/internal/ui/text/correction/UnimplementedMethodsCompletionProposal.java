e684414a3f2fa54a8f98a6f52e135ceb5031c63a
Tobias Widmer <twidmer>
2005-01-17 11:20:54 +0000

    Extracted utility methods to new StubUtility

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/correction/UnimplementedMethodsCompletionProposal.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/correction/UnimplementedMethodsCompletionProposal.java
@@ -11,29 +11,22 @@
 
 package org.eclipse.jdt.internal.ui.text.correction;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-
 import org.eclipse.core.runtime.CoreException;
 
 import org.eclipse.jdt.core.ICompilationUnit;
-import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.core.IMethod;
-import org.eclipse.jdt.core.JavaModelException;
-
-import org.eclipse.jdt.core.dom.*;
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.AbstractTypeDeclaration;
+import org.eclipse.jdt.core.dom.AnonymousClassDeclaration;
+import org.eclipse.jdt.core.dom.IMethodBinding;
+import org.eclipse.jdt.core.dom.ITypeBinding;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
 import org.eclipse.jdt.core.dom.rewrite.ListRewrite;
 
-import org.eclipse.jdt.ui.CodeGeneration;
-
 import org.eclipse.jdt.internal.corext.codemanipulation.CodeGenerationSettings;
-import org.eclipse.jdt.internal.corext.codemanipulation.ImportRewrite;
-import org.eclipse.jdt.internal.corext.codemanipulation.StubUtility;
-import org.eclipse.jdt.internal.corext.dom.ASTNodeFactory;
-import org.eclipse.jdt.internal.corext.dom.ASTNodes;
-import org.eclipse.jdt.internal.corext.dom.Bindings;
+import org.eclipse.jdt.internal.corext.codemanipulation.StubUtility2;
+
 import org.eclipse.jdt.internal.ui.JavaPlugin;
 import org.eclipse.jdt.internal.ui.JavaPluginImages;
 import org.eclipse.jdt.internal.ui.preferences.JavaPreferencesSettings;
@@ -71,7 +64,7 @@ public class UnimplementedMethodsCompletionProposal extends ASTRewriteCorrection
 			binding= decl.resolveBinding();
 			listRewrite= rewrite.getListRewrite(decl, decl.getBodyDeclarationsProperty());
 		}
-		IMethodBinding[] methods= evalUnimplementedMethods(binding);
+		IMethodBinding[] methods= StubUtility2.evalUnimplementedMethods(binding);
 		fMethodsToOverride= methods;
 		
 		
@@ -81,185 +74,10 @@ public class UnimplementedMethodsCompletionProposal extends ASTRewriteCorrection
 		}
 		
 		for (int i= 0; i < methods.length; i++) {
-			MethodDeclaration newMethodDecl= createNewMethodDeclaration(ast, methods[i], rewrite, binding.getName(), settings);
+			MethodDeclaration newMethodDecl= StubUtility2.createNewMethodDeclaration(getCompilationUnit(), ast, methods[i], rewrite, getImportRewrite(), binding.getName(), settings);
 			listRewrite.insertLast(newMethodDecl, null);
 		}
 		return rewrite;
-	}
-	
-	private MethodDeclaration createNewMethodDeclaration(AST ast, IMethodBinding binding, ASTRewrite rewrite, String typeName, CodeGenerationSettings commentSettings) throws CoreException {
-		ImportRewrite imports= getImportRewrite();
-		
-		MethodDeclaration decl= ast.newMethodDeclaration();
-		decl.modifiers().addAll(ASTNodeFactory.newModifiers(ast, binding.getModifiers() & ~Modifier.ABSTRACT));
-
-		decl.setName(ast.newSimpleName(binding.getName()));
-		decl.setConstructor(false);
-		
-		ITypeBinding[] typeParams= binding.getTypeParameters();
-		List typeParameters= decl.typeParameters();
-		for (int i= 0; i < typeParams.length; i++) {
-			ITypeBinding curr= typeParams[i];
-			TypeParameter newTypeParam= ast.newTypeParameter();
-			newTypeParam.setName(ast.newSimpleName(curr.getName()));
-			ITypeBinding[] typeBounds= curr.getTypeBounds();
-			if (typeBounds.length != 1 || !"java.lang.Object".equals(typeBounds[0].getQualifiedName())) {//$NON-NLS-1$
-				List newTypeBounds= newTypeParam.typeBounds();
-				for (int k= 0; k < typeBounds.length; k++) {
-					newTypeBounds.add(imports.addImport(typeBounds[k], ast));
-				}
-			}
-			typeParameters.add(newTypeParam);
-		}		
-		
-		decl.setReturnType2(imports.addImport(binding.getReturnType(), ast));
-		
-		List parameters= decl.parameters();
-		ITypeBinding[] params= binding.getParameterTypes();
-		String[] paramNames= getArgumentNames(binding);
-		for (int i= 0; i < params.length; i++) {
-			SingleVariableDeclaration var= ast.newSingleVariableDeclaration();
-			var.setType(imports.addImport(params[i], ast));
-			var.setName(ast.newSimpleName(paramNames[i]));
-			parameters.add(var);
-		}		
-		
-		List thrownExceptions= decl.thrownExceptions();
-		ITypeBinding[] excTypes= binding.getExceptionTypes();
-		for (int i= 0; i < excTypes.length; i++) {
-			String excTypeName= imports.addImport(excTypes[i]);
-			thrownExceptions.add(ASTNodeFactory.newName(ast, excTypeName));
-		}
-		
-		Block body= ast.newBlock();
-		decl.setBody(body);
-	
-		String bodyStatement= ""; //$NON-NLS-1$
-		Expression expression= ASTNodeFactory.newDefaultExpression(ast, decl.getReturnType2(), decl.getExtraDimensions());
-		if (expression != null) {
-			ReturnStatement returnStatement= ast.newReturnStatement();
-			returnStatement.setExpression(expression);
-			bodyStatement= ASTNodes.asFormattedString(returnStatement, 0, String.valueOf('\n'));
-		}
-
-		String placeHolder= CodeGeneration.getMethodBodyContent(getCompilationUnit(), typeName, binding.getName(), false, bodyStatement, String.valueOf('\n')); 	
-		if (placeHolder != null) {
-			ASTNode todoNode= rewrite.createStringPlaceholder(placeHolder, ASTNode.RETURN_STATEMENT);
-			body.statements().add(todoNode);
-		}
-		
-		if (commentSettings != null) {
-			String string= CodeGeneration.getMethodComment(getCompilationUnit(), typeName, decl, binding, String.valueOf('\n'));
-			if (string != null) {
-				Javadoc javadoc= (Javadoc) rewrite.createStringPlaceholder(string, ASTNode.JAVADOC);
-				decl.setJavadoc(javadoc);
-			}
-		}
-		return decl;
-	}
-	
-	private String[] getArgumentNames(IMethodBinding binding) {
-		int nParams= binding.getParameterTypes().length;
-		if (nParams > 0) {
-			try {
-				IJavaProject project= getCompilationUnit().getJavaProject();
-				IMethod method= Bindings.findMethod(binding, project);
-				if (method != null) {
-					return StubUtility.suggestArgumentNames(project, method.getParameterNames());
-				}
-			} catch (JavaModelException e) {
-				JavaPlugin.log(e);
-			}
-		}			
-		String[] names= new String[nParams];
-		for (int i= 0; i < names.length; i++) {
-			names[i]= "arg" + i; //$NON-NLS-1$
-		}
-		return names;
-	}
-		
-	private void findUnimplementedInterfaceMethods(ITypeBinding typeBinding, HashSet visited, ArrayList allMethods, IPackageBinding currPack, ArrayList toImplement) {
-		if (visited.add(typeBinding)) {
-			IMethodBinding[] typeMethods= typeBinding.getDeclaredMethods();
-			for (int i= 0; i < typeMethods.length; i++) {
-				IMethodBinding curr= typeMethods[i];
-				IMethodBinding impl= findMethod(curr, allMethods);
-				if (impl == null || !Bindings.isVisibleInHierarchy(impl, currPack) || 
-						((curr.getExceptionTypes().length < impl.getExceptionTypes().length) && !Modifier.isFinal(impl.getModifiers()))) {
-					if (impl != null) {
-						allMethods.remove(impl);
-					}
-					// implement an interface method when it does not exist in the hierarchy
-					// or when it throws less exceptions that the implemented
-					toImplement.add(curr);
-					allMethods.add(curr);
-				}
-			}
-			ITypeBinding[] superInterfaces= typeBinding.getInterfaces();
-			for (int i= 0; i < superInterfaces.length; i++) {
-				findUnimplementedInterfaceMethods(superInterfaces[i], visited, allMethods, currPack, toImplement);
-			}
-		}
-	}
-
-	private IMethodBinding[] evalUnimplementedMethods(ITypeBinding typeBinding) {
-		ArrayList allMethods= new ArrayList();
-		ArrayList toImplement= new ArrayList();
-		
-		IMethodBinding[] typeMethods= typeBinding.getDeclaredMethods();
-		for (int i= 0; i < typeMethods.length; i++) {
-			IMethodBinding curr= typeMethods[i];
-			int modifiers= curr.getModifiers();
-			if (!curr.isConstructor() && !Modifier.isStatic(modifiers) && !Modifier.isPrivate(modifiers)) {
-				allMethods.add(curr);
-			}
-		}
-
-		ITypeBinding superClass= typeBinding.getSuperclass();
-		while (superClass != null) {
-			typeMethods= superClass.getDeclaredMethods();
-			for (int i= 0; i < typeMethods.length; i++) {
-				IMethodBinding curr= typeMethods[i];
-				int modifiers= curr.getModifiers();
-				if (!curr.isConstructor() && !Modifier.isStatic(modifiers) && !Modifier.isPrivate(modifiers)) {
-					if (findMethod(curr, allMethods) == null) {
-						allMethods.add(curr);
-					}
-				}
-			}
-			superClass= superClass.getSuperclass();
-		}	
-
-		for (int i= 0; i < allMethods.size(); i++) {
-			IMethodBinding curr= (IMethodBinding) allMethods.get(i);
-			int modifiers= curr.getModifiers();
-			if ((Modifier.isAbstract(modifiers) || curr.getDeclaringClass().isInterface()) && (typeBinding != curr.getDeclaringClass())) {
-				// implement all abstract methods
-				toImplement.add(curr);
-			}
-		}
-
-		HashSet visited= new HashSet();
-		ITypeBinding curr= typeBinding;
-		while (curr != null) {
-			ITypeBinding[] superInterfaces= curr.getInterfaces();
-			for (int i= 0; i < superInterfaces.length; i++) {
-				findUnimplementedInterfaceMethods(superInterfaces[i], visited, allMethods, typeBinding.getPackage(), toImplement);
-			}
-			curr= curr.getSuperclass();
-		}
-		
-		return (IMethodBinding[]) toImplement.toArray(new IMethodBinding[toImplement.size()]);
-	}
-		
-	private IMethodBinding findMethod(IMethodBinding method, ArrayList allMethods) {
-		for (int i= 0; i < allMethods.size(); i++) {
-			IMethodBinding curr= (IMethodBinding) allMethods.get(i);
-			if (Bindings.isEqualMethod(method, curr.getName(), curr.getParameterTypes())) {
-				return curr;
-			}
-		}
-		return null;
 	}
 	
 	/* (non-Javadoc)
@@ -284,7 +102,4 @@ public class UnimplementedMethodsCompletionProposal extends ASTRewriteCorrection
 		}
 		return null;
 	}
-	
-	
-
 }
