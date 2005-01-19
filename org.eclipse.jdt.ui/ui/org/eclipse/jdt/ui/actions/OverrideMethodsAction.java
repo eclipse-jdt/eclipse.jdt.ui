498ec82fcb5d94e7b461e58b046c957ef177d206
Tobias Widmer <twidmer>
2005-01-19 18:02:22 +0000

    Added support for generics, varargs and annotations to Override Method

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/ui/actions/OverrideMethodsAction.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/ui/actions/OverrideMethodsAction.java
@@ -12,28 +12,30 @@ package org.eclipse.jdt.ui.actions;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.core.runtime.CoreException;
-
-import org.eclipse.jdt.core.ICompilationUnit;
-import org.eclipse.jdt.core.IJavaElement;
-import org.eclipse.jdt.core.IMethod;
-import org.eclipse.jdt.core.IType;
-import org.eclipse.jdt.core.JavaModelException;
 
 import org.eclipse.swt.widgets.Shell;
 
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.operation.IRunnableContext;
-import org.eclipse.jface.text.IRewriteTarget;
-import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.window.Window;
+
+import org.eclipse.jface.text.IRewriteTarget;
+import org.eclipse.jface.text.ITextSelection;
 
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchSite;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.help.WorkbenchHelp;
+
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.dom.IBinding;
+import org.eclipse.jdt.core.dom.IMethodBinding;
 
 import org.eclipse.jdt.internal.corext.codemanipulation.AddUnimplementedMethodsOperation;
 import org.eclipse.jdt.internal.corext.codemanipulation.CodeGenerationSettings;
@@ -53,16 +55,15 @@ import org.eclipse.jdt.internal.ui.util.BusyIndicatorRunnableContext;
 import org.eclipse.jdt.internal.ui.util.ElementValidator;
 import org.eclipse.jdt.internal.ui.util.ExceptionHandler;
 
-
 /**
- * Adds unimplemented methods of a type. Action opens a dialog from
- * which the user can chosse the methods to be added.
+ * Adds unimplemented methods of a type. The action opens a dialog from which the user can
+ * choose the methods to be added.
  * <p>
- * Will open the parent compilation unit in a Java editor. The result is 
- * unsaved, so the user can decide if the changes are acceptable.
+ * Will open the parent compilation unit in a Java editor. The result is unsaved, so the
+ * user can decide if the changes are acceptable.
  * <p>
- * The action is applicable to structured selections containing elements
- * of type <code>IType</code>.
+ * The action is applicable to structured selections containing elements of type
+ * {@link org.eclipse.jdt.core.IType}.
  * 
  * <p>
  * This class may be instantiated; it is not intended to be subclassed.
@@ -72,120 +73,111 @@ import org.eclipse.jdt.internal.ui.util.ExceptionHandler;
  */
 public class OverrideMethodsAction extends SelectionDispatchAction {
 
-	private CompilationUnitEditor fEditor;	
+	/** The dialog title */
 	private static final String DIALOG_TITLE= ActionMessages.getString("OverrideMethodsAction.error.title"); //$NON-NLS-1$
 
-	/**
-	 * Creates a new <code>OverrideMethodsAction</code>. The action requires
-	 * that the selection provided by the site's selection provider is of type <code>
-	 * org.eclipse.jface.viewers.IStructuredSelection</code>.
-	 * 
-	 * @param site the site providing context information for this action
-	 */
-	public OverrideMethodsAction(IWorkbenchSite site) {
-		super(site);
-		setText(ActionMessages.getString("OverrideMethodsAction.label")); //$NON-NLS-1$
-		setDescription(ActionMessages.getString("OverrideMethodsAction.description")); //$NON-NLS-1$
-		setToolTipText(ActionMessages.getString("OverrideMethodsAction.tooltip")); //$NON-NLS-1$		
-		
-		WorkbenchHelp.setHelp(this, IJavaHelpContextIds.ADD_UNIMPLEMENTED_METHODS_ACTION);
-	}
+	/** The compilation unit editor */
+	private CompilationUnitEditor fEditor;
 
 	/**
-	 * Note: This constructor is for internal use only. Clients should not call this constructor.
+	 * Note: This constructor is for internal use only. Clients should not call this
+	 * constructor.
 	 */
-	public OverrideMethodsAction(CompilationUnitEditor editor) {
+	public OverrideMethodsAction(final CompilationUnitEditor editor) {
 		this(editor.getEditorSite());
 		fEditor= editor;
 		setEnabled(checkEnabledEditor());
 	}
-		
-	//---- Structured Viewer -----------------------------------------------------------
-	
-	/* (non-Javadoc)
-	 * Method declared on SelectionDispatchAction
+
+	/**
+	 * Creates a new override method action.
+	 * <p>
+	 * The action requires that the selection provided by the site's selection provider is
+	 * of type {@link org.eclipse.jface.viewers.IStructuredSelection}.
+	 * 
+	 * @param site the workbench site providing context information for this action
 	 */
-	public void selectionChanged(IStructuredSelection selection) {
-		try {
-			setEnabled(canEnable(selection));
-		} catch (JavaModelException e) {
-			// http://bugs.eclipse.org/bugs/show_bug.cgi?id=19253
-			if (JavaModelUtil.filterNotPresentException(e))
-				JavaPlugin.log(e);
-			setEnabled(false);
-		}
+	public OverrideMethodsAction(final IWorkbenchSite site) {
+		super(site);
+		setText(ActionMessages.getString("OverrideMethodsAction.label")); //$NON-NLS-1$
+		setDescription(ActionMessages.getString("OverrideMethodsAction.description")); //$NON-NLS-1$
+		setToolTipText(ActionMessages.getString("OverrideMethodsAction.tooltip")); //$NON-NLS-1$		
+		WorkbenchHelp.setHelp(this, IJavaHelpContextIds.ADD_UNIMPLEMENTED_METHODS_ACTION);
 	}
-	
+
 	private boolean canEnable(IStructuredSelection selection) throws JavaModelException {
 		if ((selection.size() == 1) && (selection.getFirstElement() instanceof IType)) {
-			IType type= (IType) selection.getFirstElement();
-			//	look if class: not cheap but done by all source generation actions
-			// disable anonymous until create method is supported by jdt.core (bug 44395)
-			return type.getCompilationUnit() != null && type.isClass() && !type.isAnonymous();
+			final IType type= (IType) selection.getFirstElement();
+			return type.getCompilationUnit() != null && !type.isAnnotation() && !type.isInterface();
 		}
-
 		if ((selection.size() == 1) && (selection.getFirstElement() instanceof ICompilationUnit))
 			return true;
-
 		return false;
 	}
-	
-	/* (non-Javadoc)
-	 * Method declared on SelectionDispatchAction
-	 */
-	public void run(IStructuredSelection selection) {
-		Shell shell= getShell();
-		try {
-			IType type= getSelectedType(selection);
-			if (type == null) {
-				MessageDialog.openInformation(shell, getDialogTitle(), ActionMessages.getString("OverrideMethodsAction.not_applicable")); //$NON-NLS-1$
-				return;
-			}	
-			if (!ElementValidator.check(type, getShell(), getDialogTitle(), false) || !ActionUtil.isProcessable(getShell(), type)) {
-				return;
-			}
-						
-			if (type == null) {
-				MessageDialog.openError(shell, getDialogTitle(), ActionMessages.getString("OverrideMethodsAction.error.type_removed_in_editor")); //$NON-NLS-1$
-				return;
-			}
-			
-			run(shell, type);
-		} catch (CoreException e) {
-			ExceptionHandler.handle(e, shell, getDialogTitle(), ActionMessages.getString("OverrideMethodsAction.error.actionfailed"));  //$NON-NLS-1$
-		}			
-	}
 
-	//---- Java Editior --------------------------------------------------------------
-	
-	/* (non-Javadoc)
-	 * Method declared on SelectionDispatchAction
-	 */
-	public void selectionChanged(ITextSelection selection) {
-	}
-	
 	private boolean checkEnabledEditor() {
 		return fEditor != null && SelectionConverter.canOperateOn(fEditor);
 	}
 
-	/* (non-Javadoc)
-	 * Method declared on SelectionDispatchAction
+	private String getDialogTitle() {
+		return DIALOG_TITLE;
+	}
+
+	private IType getSelectedType(IStructuredSelection selection) throws JavaModelException {
+		final Object[] elements= selection.toArray();
+		if (elements.length == 1 && (elements[0] instanceof IType)) {
+			final IType type= (IType) elements[0];
+			if (type.getCompilationUnit() != null && !type.isAnnotation() && !type.isInterface()) {
+				return type;
+			}
+		} else if (elements[0] instanceof ICompilationUnit) {
+			final IType type= ((ICompilationUnit) elements[0]).findPrimaryType();
+			if (!type.isAnnotation() && !type.isInterface())
+				return type;
+		}
+		return null;
+	}
+
+	/*
+	 * @see org.eclipse.jdt.ui.actions.SelectionDispatchAction#run(org.eclipse.jface.viewers.IStructuredSelection)
+	 */
+	public void run(IStructuredSelection selection) {
+		try {
+			final IType type= getSelectedType(selection);
+			if (type == null) {
+				MessageDialog.openInformation(getShell(), getDialogTitle(), ActionMessages.getString("OverrideMethodsAction.not_applicable")); //$NON-NLS-1$
+				return;
+			}
+			if (!ElementValidator.check(type, getShell(), getDialogTitle(), false) || !ActionUtil.isProcessable(getShell(), type)) {
+				return;
+			}
+			if (type == null) {
+				MessageDialog.openError(getShell(), getDialogTitle(), ActionMessages.getString("OverrideMethodsAction.error.type_removed_in_editor")); //$NON-NLS-1$
+				return;
+			}
+			run(getShell(), type);
+		} catch (CoreException exception) {
+			ExceptionHandler.handle(exception, getShell(), getDialogTitle(), ActionMessages.getString("OverrideMethodsAction.error.actionfailed")); //$NON-NLS-1$
+		}
+	}
+
+	/*
+	 * @see org.eclipse.jdt.ui.actions.SelectionDispatchAction#run(org.eclipse.jface.text.ITextSelection)
 	 */
 	public void run(ITextSelection selection) {
-		Shell shell= getShell();
 		try {
-			IType type= SelectionConverter.getTypeAtOffset(fEditor);
+			final IType type= SelectionConverter.getTypeAtOffset(fEditor);
 			if (type != null) {
-				if (!ElementValidator.check(type, shell, getDialogTitle(), false) || !ActionUtil.isProcessable(shell, type)) {
+				if (!ElementValidator.check(type, getShell(), getDialogTitle(), false) || !ActionUtil.isProcessable(getShell(), type)) {
 					return;
 				}
-				if (type.isInterface() || type.isAnonymous()) {
-					MessageDialog.openInformation(shell, getDialogTitle(), ActionMessages.getString("OverrideMethodsAction.not_applicable")); //$NON-NLS-1$
+				if (type.isInterface() || type.isAnnotation()) {
+					MessageDialog.openInformation(getShell(), getDialogTitle(), ActionMessages.getString("OverrideMethodsAction.not_applicable")); //$NON-NLS-1$
 					return;
 				}
-				run(shell, type);
+				run(getShell(), type);
 			} else {
-				MessageDialog.openInformation(shell, getDialogTitle(), ActionMessages.getString("OverrideMethodsAction.not_applicable")); //$NON-NLS-1$
+				MessageDialog.openInformation(getShell(), getDialogTitle(), ActionMessages.getString("OverrideMethodsAction.not_applicable")); //$NON-NLS-1$
 			}
 		} catch (JavaModelException e) {
 			ExceptionHandler.handle(e, getShell(), getDialogTitle(), null);
@@ -194,90 +186,66 @@ public class OverrideMethodsAction extends SelectionDispatchAction {
 		}
 	}
 
-	//---- Helpers -------------------------------------------------------------------
-	
 	private void run(Shell shell, IType type) throws JavaModelException, CoreException {
-		OverrideMethodDialog dialog= new OverrideMethodDialog(shell, fEditor, type, false);
-
-		IMethod[] selected= null;
+		final OverrideMethodDialog dialog= new OverrideMethodDialog(shell, fEditor, type, false);
+		String[] keys= null;
 		if (!dialog.hasMethodsToOverride()) {
 			MessageDialog.openInformation(shell, getDialogTitle(), ActionMessages.getString("OverrideMethodsAction.error.nothing_found")); //$NON-NLS-1$
 			return;
 		}
-		int dialogResult= dialog.open();
-		if (dialogResult == Window.OK) {		
-			Object[] checkedElements= dialog.getResult();
-			if (checkedElements == null)
+		if (dialog.open() == Window.OK) {
+			final Object[] selected= dialog.getResult();
+			if (selected == null)
 				return;
-			ArrayList result= new ArrayList(checkedElements.length);
-			for (int i= 0; i < checkedElements.length; i++) {
-				Object curr= checkedElements[i];
-				if (curr instanceof IMethod) {
-					result.add(curr);
-				}
+			final List list= new ArrayList(selected.length);
+			for (int index= 0; index < selected.length; index++) {
+				if (selected[index] instanceof IMethodBinding)
+					list.add(((IBinding) selected[index]).getKey());
 			}
-			selected= (IMethod[]) result.toArray(new IMethod[result.size()]);
-			
-			CodeGenerationSettings settings= JavaPreferencesSettings.getCodeGenerationSettings(type.getJavaProject());
+			keys= (String[]) list.toArray(new String[list.size()]);
+			final CodeGenerationSettings settings= JavaPreferencesSettings.getCodeGenerationSettings(type.getJavaProject());
 			settings.createComments= dialog.getGenerateComment();
-	
-			IEditorPart editor= EditorUtility.openInEditor(type.getCompilationUnit());
-
-			IRewriteTarget target= editor != null ? (IRewriteTarget) editor.getAdapter(IRewriteTarget.class) : null;
-			if (target != null) {
-				target.beginCompoundChange();		
-			}
+			final IEditorPart editor= EditorUtility.openInEditor(type.getCompilationUnit());
+			final IRewriteTarget target= editor != null ? (IRewriteTarget) editor.getAdapter(IRewriteTarget.class) : null;
+			if (target != null)
+				target.beginCompoundChange();
 			try {
-				IJavaElement elementPosition= dialog.getElementPosition();
-				
-				AddUnimplementedMethodsOperation op= new AddUnimplementedMethodsOperation(type, settings, selected, false, elementPosition);
-							
+				final AddUnimplementedMethodsOperation operation= new AddUnimplementedMethodsOperation(type, dialog.getElementPosition(), keys, settings, dialog.getGenerateAnnotation(), false);
 				IRunnableContext context= JavaPlugin.getActiveWorkbenchWindow();
-				if (context == null) {
+				if (context == null)
 					context= new BusyIndicatorRunnableContext();
-				}
-				PlatformUI.getWorkbench().getProgressService().runInUI(context,
-					new WorkbenchRunnableAdapter(op, op.getScheduleRule()), 
-					op.getScheduleRule());
-				
-				IMethod[] res= op.getCreatedMethods();
-				if (res == null || res.length == 0) {
+				PlatformUI.getWorkbench().getProgressService().runInUI(context, new WorkbenchRunnableAdapter(operation, operation.getSchedulingRule()), operation.getSchedulingRule());
+				final String[] created= operation.getCreatedMethods();
+				if (created == null || created.length == 0)
 					MessageDialog.openInformation(shell, getDialogTitle(), ActionMessages.getString("OverrideMethodsAction.error.nothing_found")); //$NON-NLS-1$
-				} else if (editor != null) {
-					EditorUtility.revealInEditor(editor, res[0]);
-				}
-			} catch (InvocationTargetException e) {
-				ExceptionHandler.handle(e, shell, getDialogTitle(), null); 
-			} catch (InterruptedException e) {
+			} catch (InvocationTargetException exception) {
+				ExceptionHandler.handle(exception, shell, getDialogTitle(), null);
+			} catch (InterruptedException exception) {
 				// Do nothing. Operation has been canceled by user.
 			} finally {
-				if (target != null) {
-					target.endCompoundChange();		
-				}
+				if (target != null)
+					target.endCompoundChange();
 			}
 		}
 	}
-		
-	private IType getSelectedType(IStructuredSelection selection) throws JavaModelException {
-		Object[] elements= selection.toArray();
-		if (elements.length == 1 && (elements[0] instanceof IType)) {
-			IType type= (IType) elements[0];
-			if (type.getCompilationUnit() != null && type.isClass()) {
-				return type;
-			}
+
+	/*
+	 * @see org.eclipse.jdt.ui.actions.SelectionDispatchAction#selectionChanged(org.eclipse.jface.viewers.IStructuredSelection)
+	 */
+	public void selectionChanged(IStructuredSelection selection) {
+		try {
+			setEnabled(canEnable(selection));
+		} catch (JavaModelException exception) {
+			if (JavaModelUtil.filterNotPresentException(exception))
+				JavaPlugin.log(exception);
+			setEnabled(false);
 		}
-		else if (elements[0] instanceof ICompilationUnit) {
-			ICompilationUnit cu= (ICompilationUnit) elements[0];
-			IType type= cu.findPrimaryType();
-			if (!type.isInterface())
-				return type;
-		}
-		return null;
-	}
-	
-	private String getDialogTitle() {
-		return DIALOG_TITLE;
 	}
 
-
+	/*
+	 * @see org.eclipse.jdt.ui.actions.SelectionDispatchAction#run(org.eclipse.jface.text.ITextSelection)
+	 */
+	public void selectionChanged(ITextSelection selection) {
+		// Do nothing
+	}
 }
