7d9f27416bc722ef7d61b8e330bc8a2e3d5b69d7
Tobias Widmer <twidmer>
2005-01-25 09:53:41 +0000

    Extracted label provider to separate class

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/dialogs/OverrideMethodDialog.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/dialogs/OverrideMethodDialog.java
@@ -17,7 +17,6 @@ import java.util.List;
 import org.eclipse.core.runtime.IStatus;
 
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
@@ -27,10 +26,8 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.ToolBarManager;
 import org.eclipse.jface.dialogs.IDialogSettings;
-import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.viewers.CheckboxTreeViewer;
 import org.eclipse.jface.viewers.ITreeContentProvider;
-import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerSorter;
 
@@ -47,7 +44,6 @@ import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.IBinding;
 import org.eclipse.jdt.core.dom.IMethodBinding;
 import org.eclipse.jdt.core.dom.ITypeBinding;
-import org.eclipse.jdt.core.dom.IVariableBinding;
 import org.eclipse.jdt.core.dom.Modifier;
 
 import org.eclipse.jdt.internal.corext.codemanipulation.StubUtility2;
@@ -55,9 +51,6 @@ import org.eclipse.jdt.internal.corext.dom.Bindings;
 import org.eclipse.jdt.internal.corext.refactoring.structure.ASTNodeSearchUtil;
 import org.eclipse.jdt.internal.corext.refactoring.util.RefactoringASTParser;
 
-import org.eclipse.jdt.ui.JavaElementImageDescriptor;
-import org.eclipse.jdt.ui.JavaElementLabelProvider;
-import org.eclipse.jdt.ui.JavaElementLabels;
 
 import org.eclipse.jdt.internal.ui.IJavaHelpContextIds;
 import org.eclipse.jdt.internal.ui.JavaPlugin;
@@ -65,8 +58,6 @@ import org.eclipse.jdt.internal.ui.JavaPluginImages;
 import org.eclipse.jdt.internal.ui.JavaUIMessages;
 import org.eclipse.jdt.internal.ui.javaeditor.CompilationUnitEditor;
 import org.eclipse.jdt.internal.ui.util.ViewerPane;
-import org.eclipse.jdt.internal.ui.viewsupport.ImageDescriptorRegistry;
-import org.eclipse.jdt.internal.ui.viewsupport.JavaElementImageProvider;
 
 public class OverrideMethodDialog extends SourceActionDialog {
 
@@ -204,319 +195,6 @@ public class OverrideMethodDialog extends SourceActionDialog {
 		}
 	}
 
-	private static class OverrideMethodLabelProvider extends LabelProvider {
-
-		private static int computeJavaAdornmentFlags(IBinding binding, int flags) {
-			int adornments= 0;
-			if (binding instanceof ITypeBinding || binding instanceof IMethodBinding) {
-
-				if (binding instanceof IMethodBinding && ((IMethodBinding) binding).isConstructor())
-					adornments|= JavaElementImageDescriptor.CONSTRUCTOR;
-
-				int modifiers= binding.getModifiers();
-				if (Modifier.isAbstract(modifiers) && isAbstract(binding))
-					adornments|= JavaElementImageDescriptor.ABSTRACT;
-				if (Modifier.isFinal(modifiers) || isInterfaceField(binding))
-					adornments|= JavaElementImageDescriptor.FINAL;
-				if (Modifier.isSynchronized(modifiers) && isSynchronized(binding))
-					adornments|= JavaElementImageDescriptor.SYNCHRONIZED;
-				if (Modifier.isStatic(modifiers) || isInterfaceField(binding))
-					adornments|= JavaElementImageDescriptor.STATIC;
-
-				if (binding.isDeprecated())
-					adornments|= JavaElementImageDescriptor.DEPRECATED;
-			}
-			return adornments;
-		}
-
-		private static ImageDescriptor getBaseImageDescriptor(IBinding binding, int flags) {
-			if (binding instanceof ITypeBinding)
-				return getTypeImageDescriptor((ITypeBinding) binding, flags);
-			else if (binding instanceof IMethodBinding) {
-				ITypeBinding type= ((IMethodBinding) binding).getDeclaringClass();
-				return getMethodImageDescriptor(type.isAnnotation() || type.isInterface(), binding.getModifiers());
-			}
-			return JavaPluginImages.DESC_OBJS_UNKNOWN;
-		}
-
-		private static ImageDescriptor getClassImageDescriptor(int modifiers) {
-			if (Modifier.isPublic(modifiers) || Modifier.isProtected(modifiers) || Modifier.isPrivate(modifiers))
-				return JavaPluginImages.DESC_OBJS_CLASS;
-			else
-				return JavaPluginImages.DESC_OBJS_CLASS_DEFAULT;
-		}
-
-		private static boolean getFlag(long flags, long flag) {
-			return (flags & flag) != 0;
-		}
-
-		private static ImageDescriptor getInnerClassImageDescriptor(boolean isInInterface, int modifiers) {
-			if (Modifier.isPublic(modifiers) || isInInterface)
-				return JavaPluginImages.DESC_OBJS_INNER_CLASS_PUBLIC;
-			else if (Modifier.isPrivate(modifiers))
-				return JavaPluginImages.DESC_OBJS_INNER_CLASS_PRIVATE;
-			else if (Modifier.isProtected(modifiers))
-				return JavaPluginImages.DESC_OBJS_INNER_CLASS_PROTECTED;
-			else
-				return JavaPluginImages.DESC_OBJS_INNER_CLASS_DEFAULT;
-		}
-
-		private static ImageDescriptor getInnerInterfaceImageDescriptor(boolean isInInterface, int modifiers) {
-			if (Modifier.isPublic(modifiers) || isInInterface)
-				return JavaPluginImages.DESC_OBJS_INNER_INTERFACE_PUBLIC;
-			else if (Modifier.isPrivate(modifiers))
-				return JavaPluginImages.DESC_OBJS_INNER_INTERFACE_PRIVATE;
-			else if (Modifier.isProtected(modifiers))
-				return JavaPluginImages.DESC_OBJS_INNER_INTERFACE_PROTECTED;
-			else
-				return JavaPluginImages.DESC_OBJS_INTERFACE_DEFAULT;
-		}
-
-		private static ImageDescriptor getInterfaceImageDescriptor(int modifiers) {
-			if (Modifier.isPublic(modifiers) || Modifier.isProtected(modifiers) || Modifier.isPrivate(modifiers))
-				return JavaPluginImages.DESC_OBJS_INTERFACE;
-			else
-				return JavaPluginImages.DESC_OBJS_INTERFACE_DEFAULT;
-		}
-
-		public static ImageDescriptor getJavaImageDescriptor(IBinding binding, int flags) {
-			return new JavaElementImageDescriptor(getBaseImageDescriptor(binding, flags), computeJavaAdornmentFlags(binding, flags), useSmallSize(flags) ? JavaElementImageProvider.SMALL_SIZE : JavaElementImageProvider.BIG_SIZE);
-		}
-
-		private static ImageDescriptor getMethodImageDescriptor(boolean deferred, int modifiers) {
-			if (Modifier.isPublic(modifiers) || deferred)
-				return JavaPluginImages.DESC_MISC_PUBLIC;
-			if (Modifier.isProtected(modifiers))
-				return JavaPluginImages.DESC_MISC_PROTECTED;
-			if (Modifier.isPrivate(modifiers))
-				return JavaPluginImages.DESC_MISC_PRIVATE;
-
-			return JavaPluginImages.DESC_MISC_DEFAULT;
-		}
-
-		private static void getMethodLabel(IMethodBinding binding, long flags, StringBuffer buffer) {
-			// return type
-			if (getFlag(flags, JavaElementLabels.M_PRE_TYPE_PARAMETERS)) {
-				ITypeBinding[] parameters= binding.getTypeParameters();
-				if (parameters.length > 0) {
-					buffer.append('<');
-					for (int i= 0; i < parameters.length; i++) {
-						if (i > 0) {
-							buffer.append(JavaElementLabels.COMMA_STRING);
-						}
-						buffer.append(parameters[i].getName());
-					}
-					buffer.append('>');
-					buffer.append(' ');
-				}
-			}
-			// return type
-			if (getFlag(flags, JavaElementLabels.M_PRE_RETURNTYPE) && !binding.isConstructor()) {
-				buffer.append(binding.getReturnType().getName());
-				buffer.append(' ');
-			}
-			// qualification
-			if (getFlag(flags, JavaElementLabels.M_FULLY_QUALIFIED)) {
-				getTypeLabel(binding.getDeclaringClass(), JavaElementLabels.T_FULLY_QUALIFIED | (flags & JavaElementLabels.P_COMPRESSED), buffer);
-				buffer.append('.');
-			}
-			buffer.append(binding.getName());
-			// parameters
-			buffer.append('(');
-			if (getFlag(flags, JavaElementLabels.M_PARAMETER_TYPES | JavaElementLabels.M_PARAMETER_NAMES)) {
-				ITypeBinding[] arguments= getFlag(flags, JavaElementLabels.M_PARAMETER_TYPES) ? binding.getParameterTypes() : null;
-				if (arguments != null) {
-					for (int index= 0; index < arguments.length; index++) {
-						if (index > 0) {
-							buffer.append(JavaElementLabels.COMMA_STRING); //$NON-NLS-1$
-						}
-						if (arguments != null) {
-							if (binding.isVarargs() && (index == arguments.length - 1)) {
-								int dimension= arguments[index].getDimensions() - 1;
-								if (dimension >= 0)
-									buffer.append(arguments[index].getElementType().getName());
-								else
-									buffer.append(arguments[index].getName());
-								for (int offset= 0; offset < dimension; offset++) {
-									buffer.append("[]"); //$NON-NLS-1$
-								}
-								buffer.append("..."); //$NON-NLS-1$
-							} else {
-								buffer.append(arguments[index].getName());
-							}
-						}
-					}
-				}
-			} else {
-				if (binding.getParameterTypes().length > 0) {
-					buffer.append("..."); //$NON-NLS-1$
-				}
-			}
-			buffer.append(')');
-			if (getFlag(flags, JavaElementLabels.M_EXCEPTIONS)) {
-				ITypeBinding[] exceptions= binding.getExceptionTypes();
-				if (exceptions.length > 0) {
-					buffer.append(" throws "); //$NON-NLS-1$
-					for (int index= 0; index < exceptions.length; index++) {
-						if (index > 0) {
-							buffer.append(JavaElementLabels.COMMA_STRING);
-						}
-						buffer.append(exceptions[index].getName());
-					}
-				}
-			}
-			if (getFlag(flags, JavaElementLabels.M_APP_TYPE_PARAMETERS)) {
-				ITypeBinding[] parameters= binding.getTypeParameters();
-				if (parameters.length > 0) {
-					buffer.append(' ');
-					buffer.append('<');
-					for (int index= 0; index < parameters.length; index++) {
-						if (index > 0) {
-							buffer.append(JavaElementLabels.COMMA_STRING);
-						}
-						buffer.append(parameters[index].getName());
-					}
-					buffer.append('>');
-				}
-			}
-			if (getFlag(flags, JavaElementLabels.M_APP_RETURNTYPE) && !binding.isConstructor()) {
-				buffer.append(JavaElementLabels.DECL_STRING);
-				buffer.append(binding.getReturnType().getName());
-			}
-			// post qualification
-			if (getFlag(flags, JavaElementLabels.M_POST_QUALIFIED)) {
-				buffer.append(JavaElementLabels.CONCAT_STRING);
-				getTypeLabel(binding.getDeclaringClass(), JavaElementLabels.T_FULLY_QUALIFIED | (flags & JavaElementLabels.P_COMPRESSED), buffer);
-			}
-		}
-
-		private static ImageDescriptor getTypeImageDescriptor(boolean isInner, boolean isInInterface, ITypeBinding binding, int flags) {
-			if (binding.isEnum()) {
-				return JavaPluginImages.DESC_OBJS_ENUM;
-			} else if (binding.isAnnotation()) {
-				return JavaPluginImages.DESC_OBJS_ANNOTATION;
-			} else if (binding.isInterface()) {
-				if (useLightIcons(flags)) {
-					return JavaPluginImages.DESC_OBJS_INTERFACEALT;
-				}
-				if (isInner) {
-					return getInnerInterfaceImageDescriptor(isInInterface, binding.getModifiers());
-				}
-				return getInterfaceImageDescriptor(binding.getModifiers());
-			} else {
-				if (useLightIcons(flags)) {
-					return JavaPluginImages.DESC_OBJS_CLASSALT;
-				}
-				if (isInner) {
-					return getInnerClassImageDescriptor(isInInterface, binding.getModifiers());
-				}
-				return getClassImageDescriptor(binding.getModifiers());
-			}
-		}
-
-		private static ImageDescriptor getTypeImageDescriptor(ITypeBinding binding, int flags) {
-			ITypeBinding declaring= binding.getDeclaringClass();
-			boolean isInner= declaring != null;
-			boolean isInInterface= isInner && (declaring.isInterface() || declaring.isAnnotation());
-			return getTypeImageDescriptor(isInner, isInInterface, binding, flags);
-		}
-
-		private static void getTypeLabel(ITypeBinding binding, long flags, StringBuffer buffer) {
-			String name= binding.getName();
-			if (name.length() == 0) { // anonymous
-				if (binding.isEnum()) {
-					name= "{...}"; //$NON-NLS-1$
-				} else {
-					ITypeBinding ancestor= binding.getSuperclass();
-					if (ancestor != null) {
-						name= JavaUIMessages.getFormattedString("JavaElementLabels.anonym_type", ancestor.getName()); //$NON-NLS-1$
-					}
-				}
-				if (name == null || name.length() == 0)
-					name= JavaUIMessages.getString("JavaElementLabels.anonym"); //$NON-NLS-1$
-			}
-			buffer.append(name);
-		}
-
-		private static boolean isAbstract(IBinding binding) {
-			if (binding instanceof ITypeBinding) {
-				ITypeBinding type= (ITypeBinding) binding;
-				return !type.isInterface() && !type.isAnnotation();
-			}
-			if (binding instanceof IMethodBinding) {
-				ITypeBinding type= ((IMethodBinding) binding).getDeclaringClass();
-				return !type.isInterface() && !type.isAnnotation();
-			} else if (binding instanceof IVariableBinding) {
-				ITypeBinding type= ((IVariableBinding) binding).getDeclaringClass();
-				return !type.isInterface() && !type.isAnnotation();
-			}
-			return false;
-		}
-
-		private static boolean isInterfaceField(IBinding binding) {
-			if (binding instanceof IVariableBinding) {
-				ITypeBinding type= (ITypeBinding) binding;
-				return type.isInterface() || type.isAnnotation();
-			}
-			return false;
-		}
-
-		private static boolean isSynchronized(IBinding binding) {
-			return binding instanceof ITypeBinding;
-		}
-
-		private static boolean useLightIcons(int flags) {
-			return (flags & JavaElementImageProvider.LIGHT_TYPE_ICONS) != 0;
-		}
-
-		private static boolean useSmallSize(int flags) {
-			return (flags & JavaElementImageProvider.SMALL_ICONS) != 0;
-		}
-
-		private ImageDescriptorRegistry fRegistry= null;
-
-		/*
-		 * @see org.eclipse.jface.viewers.ILabelProvider#getImage(java.lang.Object)
-		 */
-		public Image getImage(Object element) {
-			if (element instanceof ITypeBinding) {
-				return getImageLabel(getJavaImageDescriptor(((ITypeBinding) element), JavaElementLabelProvider.SHOW_DEFAULT));
-			} else if (element instanceof IMethodBinding) {
-				return getImageLabel(getJavaImageDescriptor(((IMethodBinding) element), JavaElementLabelProvider.SHOW_DEFAULT));
-			}
-			return null;
-		}
-
-		private Image getImageLabel(ImageDescriptor descriptor) {
-			if (descriptor == null)
-				return null;
-			return getRegistry().get(descriptor);
-		}
-
-		private ImageDescriptorRegistry getRegistry() {
-			if (fRegistry == null) {
-				fRegistry= JavaPlugin.getImageDescriptorRegistry();
-			}
-			return fRegistry;
-		}
-
-		/*
-		 * @see org.eclipse.jface.viewers.ILabelProvider#getText(java.lang.Object)
-		 */
-		public String getText(Object element) {
-			if (element instanceof ITypeBinding) {
-				StringBuffer buffer= new StringBuffer();
-				getTypeLabel(((ITypeBinding) element), 0, buffer);
-				return buffer.toString();
-			} else if (element instanceof IMethodBinding) {
-				StringBuffer buffer= new StringBuffer();
-				getMethodLabel(((IMethodBinding) element), JavaElementLabels.M_PARAMETER_TYPES, buffer);
-				return buffer.toString();
-			}
-			return null;
-		}
-	}
-
 	private static class OverrideMethodSorter extends ViewerSorter {
 
 		private ITypeBinding[] fAllTypes;
@@ -571,7 +249,7 @@ public class OverrideMethodDialog extends SourceActionDialog {
 	}
 
 	public OverrideMethodDialog(Shell parent, CompilationUnitEditor editor, IType type, boolean isSubType) throws JavaModelException {
-		super(parent, new OverrideMethodLabelProvider(), new OverrideMethodContentProvider(), editor, type, false);
+		super(parent, new SourceActionLabelProvider(), new OverrideMethodContentProvider(), editor, type, false);
 		RefactoringASTParser parser= new RefactoringASTParser(AST.JLS3);
 		CompilationUnit unit= parser.parse(type.getCompilationUnit(), true);
 		ITypeBinding binding= null;
