d73e561a92ec28eeaef2d7280bc8a1d9e2661dfa
Markus Keller <mkeller>
2005-01-25 09:57:49 +0000

    enabled a rename enum test (bug 79691)

--- org.eclipse.jdt.ui.tests.refactoring/test cases/org/eclipse/jdt/ui/tests/refactoring/RenameTypeTests.java
+++ org.eclipse.jdt.ui.tests.refactoring/test cases/org/eclipse/jdt/ui/tests/refactoring/RenameTypeTests.java
@@ -39,7 +39,7 @@ public class RenameTypeTests extends RefactoringTest {
 	private static final Class clazz= RenameTypeTests.class;
 	private static final String REFACTORING_PATH= "RenameType/";
 	
-	private static final boolean BUG_79691_core_rename_enum_cu= true;
+	private static final boolean BUG_83593_core_rename_enum_cu= true;
 	private static final boolean BUG_83012_core_annotation_search= true;
 	
 	public RenameTypeTests(String name) {
@@ -1080,7 +1080,7 @@ public class RenameTypeTests extends RefactoringTest {
 	}
 	
 	public void testEnum1() throws Exception {		
-		if (BUG_79691_core_rename_enum_cu)
+		if (BUG_83593_core_rename_enum_cu)
 			return;
 		
 		IPackageFragment p2= getRoot().createPackageFragment("p2", true, null);
@@ -1094,16 +1094,13 @@ public class RenameTypeTests extends RefactoringTest {
 	}
 
 	public void testEnum2() throws Exception {
-		if (BUG_79691_core_rename_enum_cu)
+		if (BUG_83593_core_rename_enum_cu)
 			return;
 		
 		helper2("A", "B");
 	}
 	
 	public void testEnum3() throws Exception {
-		if (BUG_79691_core_rename_enum_cu)
-			return;
-		
 		ICompilationUnit enumbered= createCUfromTestFile(getPackageP(), "Enumbered");
 		helper2("A", "B");
 		assertEqualLines("invalid renaming in Enumbered", getFileContents(getOutputTestFileName("Enumbered")), enumbered.getSource());
@@ -1114,7 +1111,7 @@ public class RenameTypeTests extends RefactoringTest {
 	}
 	
 	public void testEnum5() throws Exception {
-		if (BUG_79691_core_rename_enum_cu)
+		if (BUG_83593_core_rename_enum_cu)
 			return;
 		
 		helper2("A", "B");
