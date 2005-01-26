c37ca3df6cabc3a04f7b764dc4259a85c287ea9c
Tobias Widmer <twidmer>
2005-01-26 15:33:19 +0000

    Reimplementation based on bindings Support for annotations, generics and local types

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/java/OverrideCompletionProposal.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/java/OverrideCompletionProposal.java
@@ -10,116 +10,134 @@
  *******************************************************************************/
 package org.eclipse.jdt.internal.ui.text.java;
 
+import org.eclipse.text.edits.MalformedTreeException;
+import org.eclipse.text.edits.TextEdit;
+
 import org.eclipse.core.runtime.CoreException;
 
 import org.eclipse.jface.text.Assert;
 import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.Document;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IRegion;
 
-import org.eclipse.jdt.core.Flags;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.core.IMethod;
 import org.eclipse.jdt.core.IType;
-import org.eclipse.jdt.core.ITypeHierarchy;
-import org.eclipse.jdt.core.formatter.CodeFormatter;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.AbstractTypeDeclaration;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.IMethodBinding;
+import org.eclipse.jdt.core.dom.ITypeBinding;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
+import org.eclipse.jdt.core.dom.rewrite.ITrackedNodePosition;
+import org.eclipse.jdt.core.dom.rewrite.ListRewrite;
 
+import org.eclipse.jdt.internal.corext.codemanipulation.CodeGenerationSettings;
 import org.eclipse.jdt.internal.corext.codemanipulation.ImportsStructure;
 import org.eclipse.jdt.internal.corext.codemanipulation.StubUtility;
-import org.eclipse.jdt.internal.corext.codemanipulation.StubUtility.GenStubSettings;
-import org.eclipse.jdt.internal.corext.util.CodeFormatterUtil;
+import org.eclipse.jdt.internal.corext.codemanipulation.StubUtility2;
+import org.eclipse.jdt.internal.corext.dom.Bindings;
+import org.eclipse.jdt.internal.corext.refactoring.structure.ASTNodeSearchUtil;
+import org.eclipse.jdt.internal.corext.refactoring.util.RefactoringASTParser;
 import org.eclipse.jdt.internal.corext.util.JavaModelUtil;
-import org.eclipse.jdt.internal.corext.util.JdtFlags;
 import org.eclipse.jdt.internal.corext.util.Strings;
-import org.eclipse.jdt.internal.corext.util.SuperTypeHierarchyCache;
+
+import org.eclipse.jdt.internal.ui.JavaPlugin;
 import org.eclipse.jdt.internal.ui.preferences.JavaPreferencesSettings;
 
 public class OverrideCompletionProposal extends JavaTypeCompletionProposal {
-	
-	private String fTypeName;
-	private String fMethodName;
-	private String[] fParamTypes;
+
+	private boolean fAnnotations= true;
+
 	private IJavaProject fJavaProject;
 
-	public OverrideCompletionProposal(IJavaProject jproject, ICompilationUnit cu, String declaringTypeName, String methodName, String[] paramTypes, int start, int length, String displayName, String completionProposal) {
+	private String fMethodName;
+
+	private String[] fParamTypes;
+
+	public OverrideCompletionProposal(IJavaProject jproject, ICompilationUnit cu, String methodName, String[] paramTypes, int start, int length, String displayName, String completionProposal) {
 		super(completionProposal, cu, start, length, null, displayName, 0);
 		Assert.isNotNull(jproject);
 		Assert.isNotNull(methodName);
-		Assert.isNotNull(declaringTypeName);
 		Assert.isNotNull(paramTypes);
 
-		fTypeName= declaringTypeName;
 		fParamTypes= paramTypes;
 		fMethodName= methodName;
 
 		fJavaProject= jproject;
 	}
 
-	/* (non-Javadoc)
-	 * @see JavaTypeCompletionProposal#updateReplacementString(IDocument, char, int, ImportsStructure)
-	 */
-	protected boolean updateReplacementString(IDocument document, char trigger, int offset, ImportsStructure impStructure) throws CoreException, BadLocationException {
-		IType definingType= null;
-		if (impStructure != null) {
-			ICompilationUnit cu= impStructure.getCompilationUnit();
-			JavaModelUtil.reconcile(cu);
-			IJavaElement currElem= cu.getElementAt(offset);
-			if (currElem != null) {
-				definingType= (IType) currElem.getAncestor(IJavaElement.TYPE);
-			}
-		}
-		
-		IMethod method= null;
-		if (definingType != null) {
-			ITypeHierarchy hierarchy= SuperTypeHierarchyCache.getTypeHierarchy(definingType);
-			method= JavaModelUtil.findMethodDefininition(hierarchy, definingType, fMethodName, fParamTypes, false, true);
-		} else {
-			IType declaringType= JavaModelUtil.findType(fJavaProject, fTypeName);
-			if (declaringType != null) {
-				method= JavaModelUtil.findMethod(fMethodName, fParamTypes, false, declaringType);
-			}
-		}
-		
-		if (method != null) {
-			GenStubSettings settings= new GenStubSettings(JavaPreferencesSettings.getCodeGenerationSettings(fJavaProject));
-			int flags= method.getFlags();
-
-			settings.noBody= (definingType != null) && definingType.isInterface();
-			settings.callSuper= !JdtFlags.isAbstract(method) && !Flags.isStatic(flags);
-			settings.methodOverwrites= !Flags.isStatic(flags);
-			settings.methodModifiers= flags;
-			
-			String definingTypeName= (definingType != null) ? definingType.getElementName() : ""; //$NON-NLS-1$
-			
-			String stub= StubUtility.genStub(fCompilationUnit, definingTypeName, method, method.getDeclaringType(), settings, impStructure);
-
-			// use the code formatter
-			String lineDelim= StubUtility.getLineDelimiterFor(document);
-			IRegion region= document.getLineInformationOfOffset(getReplacementOffset());
-			int lineStart= region.getOffset();
-			int indent= Strings.computeIndent(document.get(lineStart, getReplacementOffset() - lineStart), settings.tabWidth);
-
-			String replacement= CodeFormatterUtil.format(CodeFormatter.K_CLASS_BODY_DECLARATIONS, stub, indent, null, lineDelim, fJavaProject);
-			
-			if (replacement.endsWith(lineDelim)) {
-				replacement= replacement.substring(0, replacement.length() - lineDelim.length());
-			}
-			
-			setReplacementString(Strings.trimLeadingTabsAndSpaces(replacement));
-			return true;
-		}
-
-		return false;
-	}
-	
 	/*
-	 * @see org.eclipse.jface.text.contentassist.ICompletionProposalExtension3#getPrefixCompletionText(org.eclipse.jface.text.IDocument, int)
+	 * @see org.eclipse.jface.text.contentassist.ICompletionProposalExtension3#getPrefixCompletionText(org.eclipse.jface.text.IDocument,int)
 	 */
 	public CharSequence getPrefixCompletionText(IDocument document, int completionOffset) {
 		return fMethodName;
 	}
-	
-}
 
+	public void setAnnotations(boolean generate) {
+		fAnnotations= generate;
+	}
+
+	/*
+	 * @see JavaTypeCompletionProposal#updateReplacementString(IDocument,char,int,ImportsStructure)
+	 */
+	protected boolean updateReplacementString(IDocument document, char trigger, int offset, ImportsStructure structure) throws CoreException, BadLocationException {
+		IType type= null;
+		if (structure != null) {
+			ICompilationUnit unit= structure.getCompilationUnit();
+			JavaModelUtil.reconcile(unit);
+			IJavaElement element= unit.getElementAt(offset);
+			if (element != null)
+				type= (IType) element.getAncestor(IJavaElement.TYPE);
+		}
+		RefactoringASTParser parser= new RefactoringASTParser(AST.JLS3);
+		CompilationUnit unit= parser.parse(type.getCompilationUnit(), true);
+		ITypeBinding binding= null;
+		final AbstractTypeDeclaration declaration= ASTNodeSearchUtil.getAbstractTypeDeclarationNode(type, unit);
+		if (declaration != null) {
+			binding= declaration.resolveBinding();
+			if (binding != null) {
+				ASTRewrite rewrite= ASTRewrite.create(unit.getAST());
+				IMethodBinding[] bindings= StubUtility2.getOverridableMethods(binding, true);
+				if (bindings != null && bindings.length > 0) {
+					IMethodBinding methodBinding= Bindings.findMethodInHierarchy(binding, fMethodName, fParamTypes);
+					if (methodBinding != null) {
+						CodeGenerationSettings settings= JavaPreferencesSettings.getCodeGenerationSettings(fJavaProject);
+						ListRewrite rewriter= rewrite.getListRewrite(declaration, declaration.getBodyDeclarationsProperty());
+						String key= methodBinding.getKey();
+						MethodDeclaration stub= null;
+						for (int index= 0; index < bindings.length; index++) {
+							if (key.equals(bindings[index].getKey())) {
+								stub= StubUtility2.createImplementationStub(fCompilationUnit, rewrite, structure, unit.getAST(), bindings[index], binding.getName(), settings, fAnnotations && fJavaProject.getOption(JavaCore.COMPILER_COMPLIANCE, true).equals(JavaCore.VERSION_1_5) && fJavaProject.getOption(JavaCore.COMPILER_CODEGEN_TARGET_PLATFORM, true).equals(JavaCore.VERSION_1_5));
+								if (stub != null)
+									rewriter.insertFirst(stub, null);
+								break;
+							}
+						}
+						if (stub != null) {
+							IDocument contents= new Document(fCompilationUnit.getBuffer().getContents());
+							IRegion region= contents.getLineInformationOfOffset(getReplacementOffset());
+							ITrackedNodePosition position= rewrite.track(stub);
+							String indent= Strings.getIndentString(contents.get(region.getOffset(), region.getLength()), settings.tabWidth);
+							try {
+								rewrite.rewriteAST(contents, fJavaProject.getOptions(true)).apply(contents, TextEdit.UPDATE_REGIONS);
+							} catch (MalformedTreeException exception) {
+								JavaPlugin.log(exception);
+							} catch (IllegalArgumentException exception) {
+								JavaPlugin.log(exception);
+							} catch (BadLocationException exception) {
+								JavaPlugin.log(exception);
+							}
+							setReplacementString(Strings.changeIndent(Strings.trimIndentation(contents.get(position.getStartPosition(), position.getLength()), settings.tabWidth, false), 0, settings.tabWidth, indent, StubUtility.getLineDelimiterFor(contents)));
+						}
+					}
+				}
+			}
+		}
+		return true;
+	}
+}
