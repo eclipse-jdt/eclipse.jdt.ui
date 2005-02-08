0dd97e5d1fe1d0d8bb35fd15342dd13e70a8f207
Dani Megert <dmegert>
2005-02-08 15:55:19 +0000

    Disabled quick diff

--- org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/JavaReplaceAllTest.java
+++ org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/JavaReplaceAllTest.java
@@ -49,10 +49,7 @@ public class JavaReplaceAllTest extends TextPerformanceTestCase {
 	protected void setUp() throws Exception {
 		super.setUp();
 		fEditor= (AbstractTextEditor) EditorTestHelper.openInEditor(ResourceTestHelper.findFile(FILE), true);
-		
-		// FIXME: temporarily added for debugging purposes
-		System.err.println("JavaReplaceAllTest - quick diff enabled: " + fEditor.isChangeInformationShowing());
-		
+		fEditor.showChangeInformation(false);
 		EditorTestHelper.joinBackgroundActivities(fEditor);
 		setWarmUpRuns(WARM_UP_RUNS);
 		setMeasuredRuns(MEASURED_RUNS);
