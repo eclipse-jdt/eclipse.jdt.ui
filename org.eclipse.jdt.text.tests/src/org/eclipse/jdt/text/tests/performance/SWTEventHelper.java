f104f7776d5c3d351353094b475029f890ad9b22
Dani Megert <dmegert>
2005-02-11 12:05:55 +0000

    Refactored posting of event

--- org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/SWTEventHelper.java
+++ org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/SWTEventHelper.java
@@ -18,7 +18,6 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
 
 
-
 /**
  * @since 3.1
  */
@@ -64,10 +63,7 @@ public class SWTEventHelper {
 	public static void keyCodeEvent(Display display, int type, int keyCode, boolean runEventQueue) {
 		fgKeyCodeEvent.type= type;
 		fgKeyCodeEvent.keyCode= keyCode;
-		
-		Assert.assertTrue(display.post(fgKeyCodeEvent));
-		if (runEventQueue)
-			EditorTestHelper.runEventQueue();
+		postEvent(display, fgKeyCodeEvent, runEventQueue);
 	}
 	
 	public static void pressKeyChar(Display display, char keyChar) {
@@ -102,10 +98,20 @@ public class SWTEventHelper {
 	public static void keyCharEvent(Display display, int type, char keyChar, boolean runEventQueue) {
 		fgKeyCharEvent.type= type;
 		fgKeyCharEvent.character= keyChar;
-		
-		Assert.assertTrue(display.post(fgKeyCharEvent));
+		postEvent(display, fgKeyCharEvent, runEventQueue);
+	}
+
+	private static void postEvent(final Display display, final Event event, boolean runEventQueue) {
+		DisplayHelper helper= new DisplayHelper() {
+			public boolean condition() {
+				return display.post(event);
+			}
+		};
+		Assert.assertTrue(helper.waitForCondition(display, 1000));
+
 		if (runEventQueue)
 			EditorTestHelper.runEventQueue();
+			
 	}
 	
 	private static Event fgMouseMoveEvent= new Event();
@@ -113,10 +119,7 @@ public class SWTEventHelper {
 		fgMouseMoveEvent.type= SWT.MouseMove;
 		fgMouseMoveEvent.x= x;
 		fgMouseMoveEvent.y= y;
-		
-		Assert.assertTrue(display.post(fgMouseMoveEvent));
-		if (runEventQueue)
-			EditorTestHelper.runEventQueue();
+		postEvent(display, fgMouseMoveEvent, runEventQueue);
 	}
 	
 	public static void mouseDownEvent(Display display, int button, boolean runEventQueue) {
@@ -131,9 +134,6 @@ public class SWTEventHelper {
 	public static void mouseButtonEvent(Display display, int type, int button, boolean runEventQueue) {
 		fgMouseButtonEvent.type= type;
 		fgMouseButtonEvent.button= button;
-		
-		Assert.assertTrue(display.post(fgMouseButtonEvent));
-		if (runEventQueue)
-			EditorTestHelper.runEventQueue();
+		postEvent(display, fgMouseButtonEvent, runEventQueue);
 	}
 }
