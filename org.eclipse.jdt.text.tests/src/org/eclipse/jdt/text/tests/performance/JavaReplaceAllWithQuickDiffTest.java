5bee9bdf6d5b47f7c647d48d688a85bea3c741a5
Dani Megert <dmegert>
2005-02-11 12:05:45 +0000

    Share common code

--- org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/JavaReplaceAllWithQuickDiffTest.java
+++ org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/JavaReplaceAllWithQuickDiffTest.java
@@ -14,101 +14,20 @@ package org.eclipse.jdt.text.tests.performance;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
-import org.eclipse.jface.action.IAction;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.test.performance.PerformanceMeter;
-import org.eclipse.ui.texteditor.AbstractTextEditor;
-import org.eclipse.ui.texteditor.ITextEditorActionConstants;
 
 /**
- * Measures the time to replaceAll in a large file.
+ * Measures the time to replaceAll in a large file with Quick Diff enabled.
  * 
  * @since 3.1
  */
-public class JavaReplaceAllWithQuickDiffTest extends TextPerformanceTestCase {
-	
-	private static final Class THIS= JavaReplaceAllWithQuickDiffTest.class;
-	
-	private static final String FILE= PerformanceTestSetup.TEXT_LAYOUT;
-	
-	private static final int WARM_UP_RUNS= 3;
-
-	private static final int MEASURED_RUNS= 3;
-	
-	private static final char FIND= 'e';
-	
-	private static final char REPLACE= 'x';
-	
-	private AbstractTextEditor fEditor;
+public class JavaReplaceAllWithQuickDiffTest extends AbstractJavaReplaceAllTest {
 
 	public static Test suite() {
-		return new PerformanceTestSetup(new TestSuite(THIS));
+		return new PerformanceTestSetup(new TestSuite(JavaReplaceAllWithQuickDiffTest.class));
+	}
+
+	protected boolean isQuickDiffEnabled() {
+		return true;
 	}
 	
-	protected void setUp() throws Exception {
-		super.setUp();
-		fEditor= (AbstractTextEditor) EditorTestHelper.openInEditor(ResourceTestHelper.findFile(FILE), true);
-		fEditor.showChangeInformation(true);
-		EditorTestHelper.joinBackgroundActivities(fEditor);
-		setWarmUpRuns(WARM_UP_RUNS);
-		setMeasuredRuns(MEASURED_RUNS);
-	}
-	
-	protected void tearDown() throws Exception {
-		super.tearDown();
-		EditorTestHelper.closeAllEditors();
-	}
-
-	/**
-	 * Measures the time to replaceAll in a large file.
-	 * 
-	 * @throws Exception
-	 */
-	public void test() throws Exception {
-		measure(getNullPerformanceMeter(), getWarmUpRuns());
-		measure(createPerformanceMeter(), getMeasuredRuns());
-		commitAllMeasurements();
-		assertAllPerformance();
-	}
-
-	private void measure(PerformanceMeter performanceMeter, int runs) throws Exception {
-		IAction action= fEditor.getAction(ITextEditorActionConstants.FIND);
-		Display display= EditorTestHelper.getActiveDisplay();
-		for (int i= 0; i < runs; i++) {
-			fEditor.selectAndReveal(0, 0);
-			runAction(action);
-			
-			// Fill Find field
-			SWTEventHelper.pressKeyChar(display, FIND);
-			
-			// Switch to Replace field			
-			SWTEventHelper.keyCodeDown(display, SWT.MOD3);
-			SWTEventHelper.pressKeyChar(display, 'e');
-			SWTEventHelper.keyCodeUp(display, SWT.MOD3);
-
-			// Fill Replace field			
-			SWTEventHelper.pressKeyChar(display, REPLACE);
-			
-			performanceMeter.start();
-			SWTEventHelper.keyCodeDown(display, SWT.MOD3);
-			SWTEventHelper.pressKeyChar(display, 'a');
-			SWTEventHelper.keyCodeUp(display, SWT.MOD3);
-			performanceMeter.stop();
-			SWTEventHelper.pressKeyCodeCombination(display, new int[] {SWT.ALT, SWT.F4});
-			DisplayHelper helper= new DisplayHelper() {
-				public boolean condition() {
-					return fEditor.isDirty();
-				}
-			};
-			assertTrue(helper.waitForCondition(display, 1000));
-			EditorTestHelper.revertEditor(fEditor, true);
-			EditorTestHelper.joinBackgroundActivities(fEditor);
-		}
-	}
-
-	private void runAction(IAction action) {
-		action.run();
-		EditorTestHelper.runEventQueue();
-	}
 }
