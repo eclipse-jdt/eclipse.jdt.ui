17ecffe05c40c2b7ed5c8326898504b2c723014e
Tobias Widmer <twidmer>
2005-02-21 10:02:51 +0000

    Renamed TypeConstraint to SubTypeConstraint

--- org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/typeconstraints2/SubTypeConstraint2.java
+++ org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/typeconstraints2/SubTypeConstraint2.java
@@ -12,19 +12,16 @@ package org.eclipse.jdt.internal.corext.refactoring.typeconstraints2;
 
 import org.eclipse.jdt.internal.corext.Assert;
 
-public final class TypeConstraint2 {
+public final class SubTypeConstraint2 implements ITypeConstraint2 {
 	
 	private final ConstraintVariable2 fLeft;
 	private final ConstraintVariable2 fRight;
-	private final ConstraintOperator2 fOperator;
-	
-	public TypeConstraint2(ConstraintVariable2 left, ConstraintVariable2 right, ConstraintOperator2 operator) {
+
+	public SubTypeConstraint2(ConstraintVariable2 left, ConstraintVariable2 right) {
 		Assert.isNotNull(left);
 		Assert.isNotNull(right);
-		Assert.isNotNull(operator);
 		fLeft= left;
 		fRight= right;
-		fOperator= operator;
 	}
 
 	public ConstraintVariable2 getLeft() {
@@ -35,12 +32,8 @@ public final class TypeConstraint2 {
 		return fRight;
 	}
 
-	public ConstraintOperator2 getOperator() {
-		return fOperator;
-	}
-
-	public  String toString(){
-		return getLeft().toString() + " " + fOperator.toString() + " " + getRight().toString(); //$NON-NLS-1$ //$NON-NLS-2$
+	public String toString(){
+		return getLeft().toString() + " <= " + getRight().toString(); //$NON-NLS-1$ //$NON-NLS-2$
 	}
 
 	/*
@@ -48,19 +41,18 @@ public final class TypeConstraint2 {
 	 */
 	public boolean equals(Object other) {
 		// can use object identity on ConstraintVariables, since we have the stored (or to be stored) objects
-		if (other.getClass() != TypeConstraint2.class)
+		if (other.getClass() != SubTypeConstraint2.class)
 			return false;
 		
-		TypeConstraint2 otherTC= (TypeConstraint2) other;
+		ITypeConstraint2 otherTC= (ITypeConstraint2) other;
 		return getLeft() == otherTC.getLeft()
-				&& getRight() == otherTC.getRight()
-				&& getOperator() == otherTC.getOperator();
+				&& getRight() == otherTC.getRight();
 	}
 	
 	/*
 	 * @see java.lang.Object#hashCode()
 	 */
 	public int hashCode() {
-		return getLeft().hashCode() ^ 37 * getRight().hashCode() ^ 37 * getOperator().hashCode();
+		return getLeft().hashCode() ^ 37 * getRight().hashCode();
 	}
 }
