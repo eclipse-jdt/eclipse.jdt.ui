b82091f6f9b354909733c29270a46791657976e6
Dani Megert <dmegert>
2005-02-25 16:41:58 +0000

    Reverted: Changed CPL to EPL

--- org.eclipse.jdt.text.tests/testResources/javaDocIndentStrategyTest1/Test.java
+++ org.eclipse.jdt.text.tests/testResources/javaDocIndentStrategyTest1/Test.java
@@ -1,13 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2005 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *     IBM Corporation - initial API and implementation
- *******************************************************************************/
 public class Test {
 	/**
 	public int foo(int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8, int a9, int a10) throws Throwable, Error, Exception, ClassCircularityError, RuntimeException, IllegalArgumentException, NullPointerException, ClassCastException, OutOfMemoryError, AssertionError {
