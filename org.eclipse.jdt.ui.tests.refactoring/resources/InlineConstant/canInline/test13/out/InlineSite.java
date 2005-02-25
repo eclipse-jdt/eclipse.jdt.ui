7c2a689b2e04e48fbb54ecce1221cf13a50f110f
Markus Keller <mkeller>
2005-02-25 17:43:18 +0000

    revert addition of copyright headers to test resources

--- org.eclipse.jdt.ui.tests.refactoring/resources/InlineConstant/canInline/test13/out/InlineSite.java
+++ org.eclipse.jdt.ui.tests.refactoring/resources/InlineConstant/canInline/test13/out/InlineSite.java
@@ -1,13 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2005 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *     IBM Corporation - initial API and implementation
- *******************************************************************************/
 // Here, an import is added for a type needed only after a qualification is added.
 // 8, 19 -> 8, 29  removeAll == false
 package p2;
