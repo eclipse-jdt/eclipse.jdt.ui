b8a88859ccc6b785687f38607a8e9a805dccbe79
cvs2svn <platform-releng-dev@eclipse.org>
2005-03-11 08:50:17 +0000

    This commit was manufactured by cvs2svn to create tag 'R3_0_2'.
    Sprout from R3_0_maintenance 2005-03-11 08:50:16 UTC Dani Megert <dmegert> 'Increased verion to 3.0.2'
    Cherrypick from R3_0_maintenance 2004-06-25 10:48:47 UTC cvs2svn <platform-releng-dev@eclipse.org> 'This commit was manufactured by cvs2svn to create branch 'R3_0_maintenance'.':
        org.eclipse.jdt.ui.tests/ui/org/eclipse/jdt/ui/tests/quickfix/AssistQuickFixTest.java

--- org.eclipse.jdt.ui.tests/ui/org/eclipse/jdt/ui/tests/quickfix/AssistQuickFixTest.java
+++ org.eclipse.jdt.ui.tests/ui/org/eclipse/jdt/ui/tests/quickfix/AssistQuickFixTest.java
@@ -2007,7 +2007,7 @@ public class AssistQuickFixTest extends QuickFixTest {
         buf.append("}\n");
         ICompilationUnit cu= pack1.createCompilationUnit("E.java", buf.toString(), false, null);
         
-        String str= "E().equals";
+        String str= "E().sequals";
         AssistContext context= getCorrectionContext(cu, buf.toString().indexOf(str) + str.length(), 0);
         List proposals= collectAssists(context, FILTER_EQ);
         
