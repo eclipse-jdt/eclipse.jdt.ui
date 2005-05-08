828ebb73a671c4f39ad2e062859132cb06c9a458
Dani Megert <dmegert>
2005-05-08 19:32:18 +0000

    Do not hide test failures.

--- org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/NewForLoopJavaContextTest.java
+++ org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/NewForLoopJavaContextTest.java
@@ -45,9 +45,7 @@ import org.eclipse.jdt.internal.ui.JavaPlugin;
  * @since 3.1
  */
 public class NewForLoopJavaContextTest extends TestCase {
-	
-	public static final boolean BUG_93964= true;
-	
+
 	private static final String PROJECT= "NewForLoopJavaContextTest";
 	private static final String SRC= "src";
 	private static final String CU_NAME= "A.java";
@@ -146,9 +144,6 @@ public class NewForLoopJavaContextTest extends TestCase {
 	}
 	
 	public void testInnerArray() throws Exception {
-		if (BUG_93964)
-			return;
-		
 		String template= evaluateTemplateInMethod("void array(Inner[] array)");
 		assertEquals(
 				"	for (Inner inner : array) {\n" + 
