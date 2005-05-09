8d038a845543179b58eec5cbe60e05f892a67d56
Dirk Baeumer <dbaeumer>
2005-05-09 12:31:37 +0000

    Added a loop to the open package explorer warm. One run takes 100ms which is hard to measure. Made the assertion band absolute.

--- org.eclipse.jdt.ui.tests/performance/org/eclipse/jdt/ui/tests/performance/views/PackageExplorerWarmPerfTest.java
+++ org.eclipse.jdt.ui.tests/performance/org/eclipse/jdt/ui/tests/performance/views/PackageExplorerWarmPerfTest.java
@@ -23,6 +23,7 @@ import org.eclipse.ui.PlatformUI;
 import org.eclipse.jdt.ui.JavaUI;
 
 import org.eclipse.test.performance.Dimension;
+import org.eclipse.test.performance.Performance;
 
 import org.eclipse.jdt.ui.tests.performance.JdtPerformanceTestCase;
 
@@ -43,17 +44,26 @@ public class PackageExplorerWarmPerfTest extends JdtPerformanceTestCase {
 	}
 
 	public void testOpen() throws Exception {
-		tagAsSummary("Open Package Explorer - Warm", Dimension.ELAPSED_PROCESS);
 		IWorkbenchWindow activeWorkbenchWindow= PlatformUI.getWorkbench().getActiveWorkbenchWindow();
 		IWorkbenchPage page= activeWorkbenchWindow.getActivePage();
 		page.close();
 		page= activeWorkbenchWindow.openPage("org.eclipse.ui.resourcePerspective", ResourcesPlugin.getWorkspace().getRoot());
 		joinBackgroudActivities();
 		page.showView(JavaUI.ID_PACKAGES);
-		page.close();
-		page= activeWorkbenchWindow.openPage("org.eclipse.ui.resourcePerspective", ResourcesPlugin.getWorkspace().getRoot());
-		startMeasuring();
-		page.showView(JavaUI.ID_PACKAGES);
-		finishMeasurements();
+		for (int i = 0; i < 10; i++) {
+			page.close();
+			page= activeWorkbenchWindow.openPage("org.eclipse.ui.resourcePerspective", ResourcesPlugin.getWorkspace().getRoot());
+			joinBackgroudActivities();
+			startMeasuring();
+			page.showView(JavaUI.ID_PACKAGES);
+			finishMeasurements();
+		}
+	}
+	
+	protected void finishMeasurements() {
+		stopMeasuring();
+		commitMeasurements();
+		// don't spend more than 500 ms.
+		Performance.getDefault().assertPerformanceInAbsoluteBand(fPerformanceMeter, Dimension.ELAPSED_PROCESS, 0, 500);		
 	}
 }
