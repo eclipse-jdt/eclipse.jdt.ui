838aeb183a16f03a072d457966a34f74e672fb9a
Dirk Baeumer <dbaeumer>
2005-05-20 16:42:21 +0000

    Adapted code to new job handling in JDT/Core.

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/InitializeAfterLoadJob.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/InitializeAfterLoadJob.java
@@ -14,16 +14,10 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.SubProgressMonitor;
-import org.eclipse.core.runtime.jobs.Job;
-
-import org.eclipse.core.resources.ResourcesPlugin;
 
 import org.eclipse.ui.progress.UIJob;
 
 import org.eclipse.jdt.core.JavaCore;
-
-import org.eclipse.jdt.internal.corext.util.TypeInfoHistory;
 
 public class InitializeAfterLoadJob extends UIJob {
 	public InitializeAfterLoadJob() {
@@ -31,24 +25,12 @@ public class InitializeAfterLoadJob extends UIJob {
 		setSystem(true);
 	}
 	public IStatus runInUIThread(IProgressMonitor monitor) {
-		Job realJob= new Job(JavaUIMessages.InitializeAfterLoadJob_real_job_name) {
-			protected IStatus run(IProgressMonitor pm) {
-				pm.beginTask(JavaUIMessages.TypeSelectionDialog_progress_consistency, 100);
-				try {
-					JavaCore.initializeAfterLoad(new SubProgressMonitor(pm, 90));
-					TypeInfoHistory.getInstance().checkConsistency(new SubProgressMonitor(pm, 10));
-				} catch (CoreException e) {
-					JavaPlugin.log(e);
-					return e.getStatus();
-				} finally {
-					pm.done();
-				}
-				return new Status(IStatus.OK, JavaPlugin.getPluginId(), IStatus.OK, "", null); //$NON-NLS-1$
-			}
-		};
-		realJob.setRule(ResourcesPlugin.getWorkspace().getRoot());
-		// wait another two seconds
-		realJob.schedule(2 * 1000);
+		try {
+			JavaCore.initializeAfterLoad(monitor);
+		} catch (CoreException e) {
+			JavaPlugin.log(e);
+			return e.getStatus();
+		}
 		return new Status(IStatus.OK, JavaPlugin.getPluginId(), IStatus.OK, "", null); //$NON-NLS-1$
 	}
 }
