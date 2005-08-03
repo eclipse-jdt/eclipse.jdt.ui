2b41a15ab44ee4bbaef1b74758897c794df6231c
Martin Aeschlimann <maeschli>
2005-08-03 15:43:07 +0000

    added wrap and copy to clipboard

--- org.eclipse.jdt.ui.tests/examples/org/eclipse/jdt/ui/examples/MyQuickAssistProcessor.java
+++ org.eclipse.jdt.ui.tests/examples/org/eclipse/jdt/ui/examples/MyQuickAssistProcessor.java
@@ -13,16 +13,26 @@ package org.eclipse.jdt.ui.examples;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+
+import org.eclipse.core.filebuffers.FileBuffers;
+import org.eclipse.core.filebuffers.ITextFileBufferManager;
+
+import org.eclipse.swt.dnd.Clipboard;
+import org.eclipse.swt.dnd.TextTransfer;
+import org.eclipse.swt.dnd.Transfer;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Display;
+
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.IRegion;
+
+import org.eclipse.ui.IEditorPart;
 
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTNode;
-import org.eclipse.jdt.core.dom.ASTVisitor;
-import org.eclipse.jdt.core.dom.Expression;
-import org.eclipse.jdt.core.dom.IMethodBinding;
-import org.eclipse.jdt.core.dom.ITypeBinding;
-import org.eclipse.jdt.core.dom.MethodInvocation;
-import org.eclipse.jdt.core.dom.Name;
 import org.eclipse.jdt.core.dom.StringLiteral;
 import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
 
@@ -31,9 +41,9 @@ import org.eclipse.jdt.ui.text.java.IJavaCompletionProposal;
 import org.eclipse.jdt.ui.text.java.IProblemLocation;
 import org.eclipse.jdt.ui.text.java.IQuickAssistProcessor;
 
-import org.eclipse.jdt.internal.corext.dom.Bindings;
 import org.eclipse.jdt.internal.ui.JavaPluginImages;
 import org.eclipse.jdt.internal.ui.text.correction.ASTRewriteCorrectionProposal;
+import org.eclipse.jdt.internal.ui.text.correction.ChangeCorrectionProposal;
 
 /**
  *
@@ -62,107 +72,99 @@ public class MyQuickAssistProcessor implements IQuickAssistProcessor {
 		return true;
 	}
 	
-	private boolean getASTProposal(IInvocationContext context, List result) {
-		ASTNode node= context.getASTRoot();
+	private boolean getStringWrappedProposal(final IInvocationContext context, List result) throws CoreException {
+		int selectionOffset= context.getSelectionOffset();
+		int selectionLength= context.getSelectionLength();
+
+		ITextFileBufferManager textFileBufferManager= FileBuffers.getTextFileBufferManager();
+		IPath path= context.getCompilationUnit().getPath();
 		
-		AST ast= node.getAST();
-		final ASTRewrite rewrite= ASTRewrite.create(ast);
-		
-		ASTVisitor visitor= new ASTVisitor() {
-			public boolean visit(MethodInvocation inv) {
-				if (!inv.arguments().isEmpty()) {
-					return true;
-				}
-				Expression expression= inv.getExpression();
-				if (expression == null || expression.resolveTypeBinding() == null) {
-					return true;
-				}
-				ITypeBinding expressionBinding= expression.resolveTypeBinding();
-				if (Bindings.findTypeInHierarchy(expressionBinding, ASTNode.class.getName()) == null) {
-					return true;
-				}
-				IMethodBinding methodBinding= inv.resolveMethodBinding();
-				if (methodBinding == null) {
-					return true;
-				}
-				ITypeBinding returnType= methodBinding.getReturnType();
-				String name= returnType.getName();
-				if ("void".equals(name)) { //$NON-NLS-1$
-					return true;
-				}
-				if ("getAST".equals(methodBinding.getName())) { //$NON-NLS-1$
-					return true;
-				}
-				
-				String invName;
-				if ("int".equals(name)) { //$NON-NLS-1$
-					invName= "getIntAttribute"; //$NON-NLS-1$
-				} else if ("boolean".equals(name)) { //$NON-NLS-1$
-					invName= "getBooleanAttribute"; //$NON-NLS-1$
-				} else if ("List".equals(name)) { //$NON-NLS-1$
-					invName= "getChildList"; //$NON-NLS-1$
-				} else if (Bindings.findTypeInHierarchy(returnType, ASTNode.class.getName()) != null) {
-					invName= "getChildNode"; //$NON-NLS-1$
-				} else {
-					invName= "getAttribute"; //$NON-NLS-1$
-				}
-				AST ast2= inv.getAST();
-				MethodInvocation newInv= ast2.newMethodInvocation();
-				newInv.arguments().add(rewrite.createCopyTarget(expression));
-				
-				Name arg= ast2.newName(new String[] { expressionBinding.getName(), toConstantName(methodBinding.getName()) });
-				newInv.arguments().add(arg);
-				newInv.setName(ast2.newSimpleName(invName));
-				
-				rewrite.replace(inv, newInv, null);
+		try {
+			textFileBufferManager.connect(path, null);
+			IDocument document= textFileBufferManager.getTextFileBuffer(path).getDocument();
+			int startLine= document.getLineOfOffset(selectionOffset);
+			int endLine= document.getLineOfOffset(selectionOffset + selectionLength);
+			
+			
+			if (startLine == endLine) {
+				return false;
+			}
+			if (result == null) {
 				return true;
 			}
-		};
-		node.accept(visitor);
-		
-		Image image= JavaPluginImages.get(JavaPluginImages.IMG_CORRECTION_CHANGE);
-		result.add(new ASTRewriteCorrectionProposal("Use AST Properties", context.getCompilationUnit(), rewrite, 10, image));
+			Image image= JavaPluginImages.get(JavaPluginImages.IMG_CORRECTION_CHANGE);
+			result.add(new ChangeCorrectionProposal("To clipboard in buf.append()", null, 3, image) {
+				/* (non-Javadoc)
+				 * @see org.eclipse.jdt.internal.ui.text.correction.ChangeCorrectionProposal#performChange(org.eclipse.ui.IEditorPart, org.eclipse.jface.text.IDocument)
+				 */
+				protected void performChange(IEditorPart activeEditor, IDocument doc) throws CoreException {
+					wrapAndCopyToClipboard(context, doc);
+					super.performChange(activeEditor, doc);
+				}
+			});
+
+		} catch (BadLocationException e) {
+			return false;
+		} finally {
+			textFileBufferManager.disconnect(path, null);
+		}
 		return true;
 	}
 	
-	private static String getPropertyName(String name) {
-		if (name.startsWith("get")) {
-			return name.substring(3);
-		} else if (name.startsWith("is")) {
-			return name;
-		}
-		return name;
-	}
 	
-	private static String toConstantName(String name) {
-		String string= getPropertyName(name);
-		
+	/**
+	 * @param context
+	 * @param document
+	 */
+	protected void wrapAndCopyToClipboard(IInvocationContext context, IDocument document) {
 		StringBuffer buf= new StringBuffer();
-		for (int i= 0; i < string.length(); i++) {
-			char ch= string.charAt(i);
-			if (i != 0 && Character.isUpperCase(ch)) {
-				buf.append('_');
+		try {
+			int selectionOffset= context.getSelectionOffset();
+			int selectionLength= context.getSelectionLength();
+			int startLine= document.getLineOfOffset(selectionOffset);
+			int endLine= document.getLineOfOffset(selectionOffset + selectionLength);
+			
+			for (int i= startLine; i <= endLine; i++) {
+				IRegion lineInfo= document.getLineInformation(i);
+				String lineContent= document.get(lineInfo.getOffset(), lineInfo.getLength());
+				buf.append("buf.append(\"");
+				for (int k= 0; k < lineContent.length(); k++) {
+					char ch= lineContent.charAt(k);
+					if (ch == '\t') {
+						buf.append("    "); // 4 spaces
+					} else if (ch == '"' || ch == '\\') {
+						buf.append('\\').append(ch);
+					} else {
+						buf.append(ch);
+					}
+				}
+				buf.append("\\n\");");
+				if (i != endLine) {
+					buf.append('\n');
+				}
 			}
-			buf.append(Character.toUpperCase(ch));
+		} catch (BadLocationException e) {
+			// ignore
 		}
-		buf.append("_PROPERTY"); //$NON-NLS-1$
-		return buf.toString();
+		Clipboard clipboard= new Clipboard(Display.getCurrent());
+		clipboard.setContents(new Object[] { buf.toString() }, new Transfer[] { TextTransfer.getInstance() } );
+
 	}
 
 	/* (non-Javadoc)
 	 * @see org.eclipse.jdt.ui.text.java.IQuickAssistProcessor#hasAssists(org.eclipse.jdt.ui.text.java.IInvocationContext)
 	 */
-	public boolean hasAssists(IInvocationContext context) {
-		return getConvertProposal(context, null);
+	public boolean hasAssists(IInvocationContext context) throws CoreException {
+		return getConvertProposal(context, null) || getStringWrappedProposal(context, null);
 	}
 
 	/* (non-Javadoc)
 	 * @see org.eclipse.jdt.ui.text.java.IQuickAssistProcessor#getAssists(org.eclipse.jdt.ui.text.java.IInvocationContext, org.eclipse.jdt.ui.text.java.IProblemLocation[])
 	 */
-	public IJavaCompletionProposal[] getAssists(IInvocationContext context, IProblemLocation[] locations) {
+	public IJavaCompletionProposal[] getAssists(IInvocationContext context, IProblemLocation[] locations) throws CoreException {
 		ArrayList resultingCollections= new ArrayList();
 		getConvertProposal(context, resultingCollections);
-		//getASTProposal(context, resultingCollections);
+		getStringWrappedProposal(context, resultingCollections);
 		return (IJavaCompletionProposal[]) resultingCollections.toArray(new IJavaCompletionProposal[resultingCollections.size()]);
 	}
 
