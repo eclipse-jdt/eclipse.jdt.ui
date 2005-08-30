9c40a8663313383f42276239d79fe73986b6055d
Markus Keller <mkeller>
2005-08-30 14:01:10 +0000

    108115: [refactoring] Extract Local Variable and Ex. Constant don't find matching invocations of generic methods

---
+++ org.eclipse.jdt.ui.tests.refactoring/resources/ExtractTemp/canExtract/A_test92_out.java
@@ -0,0 +1,16 @@
+package p;
+
+import static java.util.Arrays.*;
+import java.util.List;
+
+class Bug {
+	final static String[] side = new String[0];
+	{
+		List<String> asList= asList(side);
+		if (true) {
+			System.out.println(asList);
+		} else {
+			System.out.println(asList);
+		}
+	}
+}
