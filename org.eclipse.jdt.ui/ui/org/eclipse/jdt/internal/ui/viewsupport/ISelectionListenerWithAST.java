8acc30ae265eb55752233e30a02f9f47773eea25
Dani Megert <dmegert>
2005-09-19 09:07:03 +0000

    Formatting

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/viewsupport/ISelectionListenerWithAST.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/viewsupport/ISelectionListenerWithAST.java
@@ -26,7 +26,8 @@ public interface ISelectionListenerWithAST {
 	/**
 	 * Called when a selection has changed. The method is called in a post selection event in an background
 	 * thread.
-	 * @param part The editor part in which the selection change has occured.
+	 * 
+	 * @param part The editor part in which the selection change has occurred.
 	 * @param selection The new text selection
 	 * @param astRoot The AST tree corresponding to the editor's input. This AST is shared and must
 	 * not be modified.
