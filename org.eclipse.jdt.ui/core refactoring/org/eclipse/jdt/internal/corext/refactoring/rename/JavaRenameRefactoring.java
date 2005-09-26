3aa06d159b665f7780e27e8973d3da08192190c8
Tobias Widmer <twidmer>
2005-09-26 17:24:12 +0000

    Added new self-initializing rename refactoring

---
+++ org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/rename/JavaRenameRefactoring.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * Copyright (c) 2005 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.jdt.internal.corext.refactoring.rename;
+
+import org.eclipse.ltk.core.refactoring.participants.RefactoringArguments;
+import org.eclipse.ltk.core.refactoring.participants.RefactoringProcessor;
+import org.eclipse.ltk.core.refactoring.participants.RenameProcessor;
+import org.eclipse.ltk.core.refactoring.participants.RenameRefactoring;
+import org.eclipse.ltk.internal.core.refactoring.history.IInitializableRefactoringObject;
+
+import org.eclipse.jdt.internal.corext.Assert;
+
+/**
+ * A rename refactoring which can be initialized with refactoring arguments.
+ * 
+ * @since 3.2
+ */
+public class JavaRenameRefactoring extends RenameRefactoring implements IInitializableRefactoringObject {
+
+	/**
+	 * Creates a new java rename refactoring.
+	 * 
+	 * @param processor
+	 *            the rename processor to use
+	 */
+	public JavaRenameRefactoring(final RenameProcessor processor) {
+		super(processor);
+	}
+
+	/*
+	 * @see org.eclipse.ltk.internal.core.refactoring.history.IInitializableRefactoringObject#initialize(org.eclipse.ltk.core.refactoring.participants.RefactoringArguments)
+	 */
+	public final boolean initialize(final RefactoringArguments arguments) {
+		Assert.isNotNull(arguments);
+		final RefactoringProcessor processor= getProcessor();
+		if (processor instanceof IInitializableRefactoringObject) {
+			final IInitializableRefactoringObject object= (IInitializableRefactoringObject) processor;
+			return object.initialize(arguments);
+		}
+		return false;
+	}
+}
