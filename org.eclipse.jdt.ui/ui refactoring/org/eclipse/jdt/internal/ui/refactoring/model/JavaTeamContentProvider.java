afbaa0414f33abb6138bf583a7ebb7355a763ef8
Tobias Widmer <twidmer>
2005-12-07 11:12:36 +0000

    Added support to display Java content

---
+++ org.eclipse.jdt.ui/ui refactoring/org/eclipse/jdt/internal/ui/refactoring/model/JavaTeamContentProvider.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2005 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.jdt.internal.ui.refactoring.model;
+
+import org.eclipse.team.internal.ui.mapping.ResourceTeamAwareContentProvider;
+
+/**
+ * Content provider for model-based team operations.
+ * 
+ * @since 3.2
+ */
+public final class JavaTeamContentProvider extends ResourceTeamAwareContentProvider {
+
+	/**
+	 * {@inheritDoc}
+	 */
+	protected String getModelProviderId() {
+		return JavaRefactoringModelProvider.JAVA_REFACTORING_MODEL_PROVIDER_ID;
+	}
+}
