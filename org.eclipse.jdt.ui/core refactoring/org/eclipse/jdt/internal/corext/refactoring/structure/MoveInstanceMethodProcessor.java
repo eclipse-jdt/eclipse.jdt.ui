c7c2e998115d49e437dcea09ff4557e20478f6e3
Tobias Widmer <twidmer>
2006-01-11 16:04:03 +0000

    114509 [refactoring] Move instance method with generating delegates unnecessarily raises field visibility

--- org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/structure/MoveInstanceMethodProcessor.java
+++ org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/structure/MoveInstanceMethodProcessor.java
@@ -1638,7 +1638,7 @@ public final class MoveInstanceMethodProcessor extends MoveProcessor implements 
 			// Do not adjust visibility of a target field; references to the
 			// field will be removed anyway.
 			final IJavaElement targetElement= fTarget.getJavaElement();
-			if (targetElement != null && targetElement instanceof IField && Flags.isPrivate(fMethod.getFlags())) {
+			if (targetElement != null && targetElement instanceof IField && (Flags.isPrivate(fMethod.getFlags()) || !fInline)) {
 				final IVisibilityAdjustment adjustmentForTarget= (IVisibilityAdjustment) adjustor.getAdjustments().get(targetElement);
 				if (adjustmentForTarget != null)
 					adjustor.getAdjustments().remove(targetElement);
