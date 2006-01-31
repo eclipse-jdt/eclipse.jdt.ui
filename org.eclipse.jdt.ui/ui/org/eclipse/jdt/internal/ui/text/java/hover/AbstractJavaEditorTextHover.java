dd210cc7ed9b1aa8c4d5177afa2cbd0f1fd689b7
Dani Megert <dmegert>
2006-01-31 14:50:36 +0000

    - replaced deprecated command manager by using IBindingService. - more support for non-workspace CUs.

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/java/hover/AbstractJavaEditorTextHover.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/java/hover/AbstractJavaEditorTextHover.java
@@ -10,8 +10,6 @@
  *******************************************************************************/
 package org.eclipse.jdt.internal.ui.text.java.hover;
 
-import java.util.List;
-
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Shell;
 
@@ -25,10 +23,7 @@ import org.eclipse.jface.text.ITextViewer;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.commands.ICommand;
-import org.eclipse.ui.commands.ICommandManager;
-import org.eclipse.ui.commands.IKeySequenceBinding;
-import org.eclipse.ui.keys.KeySequence;
+import org.eclipse.ui.keys.IBindingService;
 
 import org.eclipse.jdt.core.ICodeAssist;
 import org.eclipse.jdt.core.IJavaElement;
@@ -36,13 +31,13 @@ import org.eclipse.jdt.core.JavaModelException;
 
 import org.eclipse.jdt.internal.corext.util.Messages;
 
-import org.eclipse.jdt.ui.IWorkingCopyManager;
 import org.eclipse.jdt.ui.PreferenceConstants;
 import org.eclipse.jdt.ui.actions.IJavaEditorActionDefinitionIds;
 import org.eclipse.jdt.ui.text.java.hover.IJavaEditorTextHover;
 
 import org.eclipse.jdt.internal.ui.JavaPlugin;
 import org.eclipse.jdt.internal.ui.javaeditor.IClassFileEditorInput;
+import org.eclipse.jdt.internal.ui.javaeditor.WorkingCopyManager;
 import org.eclipse.jdt.internal.ui.text.HTMLTextPresenter;
 import org.eclipse.jdt.internal.ui.text.JavaWordFinder;
 
@@ -55,12 +50,9 @@ public abstract class AbstractJavaEditorTextHover implements IJavaEditorTextHove
 
 
 	private IEditorPart fEditor;
-	private ICommand fCommand;
+	private IBindingService fBindingService;
 	{
-		ICommandManager commandManager= PlatformUI.getWorkbench().getCommandSupport().getCommandManager();
-		fCommand= commandManager.getCommand(IJavaEditorActionDefinitionIds.SHOW_JAVADOC);
-		if (!fCommand.isDefined())
-			fCommand= null;
+		fBindingService= (IBindingService)PlatformUI.getWorkbench().getAdapter(IBindingService.class);
 	}
 
 	/*
@@ -82,8 +74,8 @@ public abstract class AbstractJavaEditorTextHover implements IJavaEditorTextHove
 				return cfeInput.getClassFile();
 			}
 
-			IWorkingCopyManager manager= JavaPlugin.getDefault().getWorkingCopyManager();
-			return manager.getWorkingCopy(input);
+			WorkingCopyManager manager= JavaPlugin.getDefault().getWorkingCopyManager();
+			return manager.getWorkingCopy(input, false);
 		}
 
 		return null;
@@ -156,36 +148,11 @@ public abstract class AbstractJavaEditorTextHover implements IJavaEditorTextHove
 	 * @since 3.0
 	 */
 	protected String getTooltipAffordanceString() {
-		if (!JavaPlugin.getDefault().getPreferenceStore().getBoolean(PreferenceConstants.EDITOR_SHOW_TEXT_HOVER_AFFORDANCE))
+		if (fBindingService == null || !JavaPlugin.getDefault().getPreferenceStore().getBoolean(PreferenceConstants.EDITOR_SHOW_TEXT_HOVER_AFFORDANCE))
 			return null;
 
-		KeySequence[] sequences= getKeySequences();
-		if (sequences == null)
-			return null;
-
-		String keySequence= sequences[0].format();
+		String keySequence= fBindingService.getBestActiveBindingFormattedFor(IJavaEditorActionDefinitionIds.SHOW_JAVADOC);
 		return Messages.format(JavaHoverMessages.JavaTextHover_makeStickyHint, keySequence == null ? "" : keySequence); //$NON-NLS-1$
 	}
 
-	/**
-	 * Returns the array of valid key sequence bindings for the
-	 * show tool tip description command.
-	 *
-	 * @return the array with the {@link KeySequence}s
-	 *
-	 * @since 3.0
-	 */
-	private KeySequence[] getKeySequences() {
-		if (fCommand != null) {
-			List list= fCommand.getKeySequenceBindings();
-			if (!list.isEmpty()) {
-				KeySequence[] keySequences= new KeySequence[list.size()];
-				for (int i= 0; i < keySequences.length; i++) {
-					keySequences[i]= ((IKeySequenceBinding) list.get(i)).getKeySequence();
-				}
-				return keySequences;
-			}
-		}
-		return null;
-	}
 }
