d9d1bc98e9498f410e5c7528186298d8db749748
Martin Aeschlimann <maeschli>
2006-02-09 18:28:09 +0000

    125450 Enable conditional loading of APT

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/correction/ContributedProcessorDescriptor.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/correction/ContributedProcessorDescriptor.java
@@ -12,16 +12,19 @@ package org.eclipse.jdt.internal.ui.text.correction;
 
 import java.util.Arrays;
 
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IStatus;
+
 import org.eclipse.core.expressions.EvaluationContext;
 import org.eclipse.core.expressions.EvaluationResult;
 import org.eclipse.core.expressions.Expression;
 import org.eclipse.core.expressions.ExpressionConverter;
 import org.eclipse.core.expressions.ExpressionTagNames;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IConfigurationElement;
-import org.eclipse.core.runtime.IStatus;
 
 import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.JavaCore;
 
 import org.eclipse.jdt.internal.ui.JavaPlugin;
 import org.eclipse.jdt.internal.ui.dialogs.StatusInfo;
@@ -30,7 +33,6 @@ public class ContributedProcessorDescriptor {
 
 	private IConfigurationElement fConfigurationElement;
 	private Object fProcessorInstance;
-	private ICompilationUnit fLastCUnit;
 	private Boolean fStatus;
 	private boolean fLastResult;
 
@@ -40,7 +42,6 @@ public class ContributedProcessorDescriptor {
 	public ContributedProcessorDescriptor(IConfigurationElement element) {
 		fConfigurationElement= element;
 		fProcessorInstance= null;
-		fLastCUnit= null;
 		fStatus= null; // undefined
 		if (fConfigurationElement.getChildren(ExpressionTagNames.ENABLEMENT).length == 0) {
 			fStatus= Boolean.TRUE;
@@ -51,7 +52,7 @@ public class ContributedProcessorDescriptor {
 		IConfigurationElement[] children= fConfigurationElement.getChildren(ExpressionTagNames.ENABLEMENT);
 		if (children.length > 1) {
 			String id= fConfigurationElement.getAttribute(ID);
-			return new StatusInfo(IStatus.ERROR, "Only one <enablement> element allowed. Disabling " + id); //$NON-NLS-1$
+			return new StatusInfo(IStatus.ERROR, "Only one < enablement > element allowed. Disabling " + id); //$NON-NLS-1$
 		}
 		return new StatusInfo(IStatus.OK, "Syntactically correct quick assist/fix processor"); //$NON-NLS-1$
 	}
@@ -63,19 +64,16 @@ public class ContributedProcessorDescriptor {
 
 		IConfigurationElement[] children= fConfigurationElement.getChildren(ExpressionTagNames.ENABLEMENT);
 		if (children.length == 1) {
-			if (cunit.equals(fLastCUnit)) {
-				return fLastResult;
-			}
 			try {
 				ExpressionConverter parser= ExpressionConverter.getDefault();
 				Expression expression= parser.perform(children[0]);
 				EvaluationContext evalContext= new EvaluationContext(null, cunit);
 				evalContext.addVariable("compilationUnit", cunit); //$NON-NLS-1$
-				String[] natures= cunit.getJavaProject().getProject().getDescription().getNatureIds();
+				IJavaProject javaProject= cunit.getJavaProject();
+				String[] natures= javaProject.getProject().getDescription().getNatureIds();
 				evalContext.addVariable("projectNatures", Arrays.asList(natures)); //$NON-NLS-1$
-
+				evalContext.addVariable("sourceCompliance", javaProject.getOption(JavaCore.COMPILER_SOURCE, true)); //$NON-NLS-1$
 				fLastResult= !(expression.evaluate(evalContext) != EvaluationResult.TRUE);
-				fLastCUnit= cunit;
 				return fLastResult;
 			} catch (CoreException e) {
 				JavaPlugin.log(e);
@@ -84,7 +82,7 @@ public class ContributedProcessorDescriptor {
 		fStatus= Boolean.FALSE;
 		return false;
 	}
-
+	
 	public Object getProcessor(ICompilationUnit cunit) throws CoreException {
 		if (matches(cunit)) {
 			if (fProcessorInstance == null) {
