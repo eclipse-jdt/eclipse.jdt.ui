f86424b7138dc6065de933a4ea2cb223afc3ec61
Martin Aeschlimann <maeschli>
2006-02-10 11:16:42 +0000

    updated spec

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/ui/text/java/IQuickFixProcessor.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/ui/text/java/IQuickFixProcessor.java
@@ -19,7 +19,13 @@ import org.eclipse.jdt.core.ICompilationUnit;
 /**
  * Interface to be implemented by contributors to the extension point
  * <code>org.eclipse.jdt.ui.quickFixProcessors</code>.
- *
+ * <p>Since 3.2, each extension specifies the marker types it can handle, and {@link #hasCorrections(ICompilationUnit, int)} and
+ * {@link #getCorrections(IInvocationContext, IProblemLocation[])} are called if (and only if) quick fix is
+ * required for a problem of these types.</p>
+ * <p>Note, if a extension does not specify marker types it will be only called for problem of type
+ * <code>org.eclipse.jdt.core.problem</code>, <code>org.eclipse.jdt.core.buildpath_problem</code>
+ * and <code>org.eclipse.jdt.core.task</code>; compatible with the behavior prior to 3.2</p>
+ * 
  * @since 3.0
  */
 public interface IQuickFixProcessor {
@@ -29,7 +35,8 @@ public interface IQuickFixProcessor {
 	 * optimistic guess and be very cheap.
 	 *
 	 * @param unit the compilation unit
-	 * @param problemId the problem Id
+	 * @param problemId the problem Id. The id is of a problem of the problem type(s) this processor specified in
+	 * the extension point.
 	 * @return <code>true</code> if the processor has proposals for the given problem
 	 */
 	boolean hasCorrections(ICompilationUnit unit, int problemId);
