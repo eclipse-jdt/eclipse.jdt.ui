20060afec463af1a86c6785bc5492d892b685a31
Benno Baumgartner <bbaumgart>
2006-03-01 12:15:29 +0000

    129799 type decorations issues

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/InterfaceIndicatorLabelDecorator.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/InterfaceIndicatorLabelDecorator.java
@@ -10,6 +10,8 @@
  *******************************************************************************/
 package org.eclipse.jdt.internal.ui;
 
+import org.eclipse.core.runtime.ListenerList;
+
 import org.eclipse.swt.graphics.Image;
 
 import org.eclipse.jface.resource.ImageDescriptor;
@@ -17,14 +19,35 @@ import org.eclipse.jface.viewers.IDecoration;
 import org.eclipse.jface.viewers.ILabelDecorator;
 import org.eclipse.jface.viewers.ILabelProviderListener;
 import org.eclipse.jface.viewers.ILightweightLabelDecorator;
+import org.eclipse.jface.viewers.LabelProviderChangedEvent;
 
+import org.eclipse.jdt.core.ElementChangedEvent;
+import org.eclipse.jdt.core.IClassFile;
 import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IElementChangedListener;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IJavaElementDelta;
 import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.JavaModelException;
 
 import org.eclipse.jdt.internal.corext.refactoring.util.JavaElementUtil;
 
 public class InterfaceIndicatorLabelDecorator implements ILabelDecorator, ILightweightLabelDecorator {
+	
+	private class IntefaceIndicatorChangeListener implements IElementChangedListener {
+
+		/**
+		 * {@inheritDoc}
+		 */
+		public void elementChanged(ElementChangedEvent event) {
+			handleDelta(event.getDelta());
+		}
+		
+	}
+	
+	private ListenerList fListeners;
+	private IElementChangedListener fChangeListener;
 
 	public InterfaceIndicatorLabelDecorator() {
 	}
@@ -47,12 +70,33 @@ public class InterfaceIndicatorLabelDecorator implements ILabelDecorator, ILight
 	 * {@inheritDoc}
 	 */
 	public void addListener(ILabelProviderListener listener) {
+		if (fChangeListener == null) {
+			fChangeListener= new IntefaceIndicatorChangeListener();
+			JavaCore.addElementChangedListener(fChangeListener);
+		}
+		
+		if (fListeners == null) {
+			fListeners= new ListenerList();
+		}
+		
+		fListeners.add(listener);
 	}
 
 	/**
 	 * {@inheritDoc}
 	 */
 	public void dispose() {
+		if (fChangeListener != null) {
+			JavaCore.removeElementChangedListener(fChangeListener);
+			fChangeListener= null;
+		}
+		if (fListeners != null) {
+			Object[] listeners= fListeners.getListeners();
+			for (int i= 0; i < listeners.length; i++) {
+				fListeners.remove(listeners[i]);
+			}
+			fListeners= null;
+		}
 	}
 
 	/**
@@ -66,28 +110,47 @@ public class InterfaceIndicatorLabelDecorator implements ILabelDecorator, ILight
 	 * {@inheritDoc}
 	 */
 	public void removeListener(ILabelProviderListener listener) {
+		if (fListeners == null)
+			return;
+		
+		fListeners.remove(listener);
+		
+		if (fListeners.isEmpty() && fChangeListener != null) {
+			JavaCore.removeElementChangedListener(fChangeListener);
+			fChangeListener= null;
+		}
 	}
 
 	/**
 	 * {@inheritDoc}
 	 */
 	public void decorate(Object element, IDecoration decoration) {
-		if (element instanceof ICompilationUnit) {
-			ICompilationUnit unit= (ICompilationUnit)element;
-			try {
-				IType type= JavaElementUtil.getMainType(unit);
-				if (type != null) {
-					ImageDescriptor overlay= getOverlay(type);
-					if (overlay != null) {
-						decoration.addOverlay(overlay, IDecoration.TOP_LEFT);
-					}
-				}
-			} catch (JavaModelException e) {
+		try {
+			IType type= getMainType(element);
+	
+			if (type == null)
 				return;
-			}
+			
+			ImageDescriptor overlay= getOverlay(type);
+			if (overlay == null)
+				return;
+			
+			decoration.addOverlay(overlay, IDecoration.TOP_LEFT);
+		} catch (JavaModelException e) {
+			return;
 		}
 	}
 	
+	private IType getMainType(Object element) throws JavaModelException {
+		if (element instanceof ICompilationUnit)
+			return JavaElementUtil.getMainType((ICompilationUnit)element);
+		
+		if (element instanceof IClassFile)
+			return ((IClassFile)element).getType();
+		
+		return null;
+	}
+
 	private ImageDescriptor getOverlay(IType type) throws JavaModelException {
 		if (type.isAnnotation()) {
 			return JavaPluginImages.DESC_OVR_ANNOTATION;
@@ -97,6 +160,24 @@ public class InterfaceIndicatorLabelDecorator implements ILabelDecorator, ILight
 			return JavaPluginImages.DESC_OVR_ENUM;
 		}
 		return null;
+	}
+
+	private void handleDelta(IJavaElementDelta delta) {
+		IJavaElement element= delta.getElement();
+		if (!(element instanceof ICompilationUnit))
+			return;
+		
+		if (delta.getKind() != IJavaElementDelta.CHANGED)
+			return;
+		
+		if (fListeners != null && !fListeners.isEmpty()) {
+			LabelProviderChangedEvent event1= new LabelProviderChangedEvent(this, element);
+			Object[] listeners= fListeners.getListeners();
+			for (int i= 0; i < listeners.length; i++) {
+				((ILabelProviderListener) listeners[i]).labelProviderChanged(event1);
+			}
+		}
+		
 	}
 
 }
