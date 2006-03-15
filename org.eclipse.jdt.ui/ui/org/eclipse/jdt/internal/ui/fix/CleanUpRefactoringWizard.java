aa6763f3a3807bdfb42e8adffdf50a8a0f1d52bc
Benno Baumgartner <bbaumgart>
2006-03-15 16:56:10 +0000

    127668 Refactorings with wizard based user interface have bad dialog sizes

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/fix/CleanUpRefactoringWizard.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/fix/CleanUpRefactoringWizard.java
@@ -245,26 +245,7 @@ public class CleanUpRefactoringWizard extends RefactoringWizard {
 		private class TabFolderLayout extends Layout {
 
 			protected Point computeSize (Composite composite, int wHint, int hHint, boolean flushCache) {
-				if (wHint != SWT.DEFAULT && hHint != SWT.DEFAULT)
-					return new Point(wHint, hHint);
-					
-				Control [] children = composite.getChildren ();
-				int count = children.length;
-				int maxWidth = 0, maxHeight = 0;
-				for (int i=0; i<count; i++) {
-					Control child = children [i];
-					Point pt = child.computeSize (SWT.DEFAULT, SWT.DEFAULT, flushCache);
-					maxWidth = Math.max (maxWidth, pt.x);
-					maxHeight = Math.max (maxHeight, pt.y);
-				}
-				
-				if (wHint != SWT.DEFAULT)
-					maxWidth= wHint;
-				if (hHint != SWT.DEFAULT)
-					maxHeight= hHint;
-				
-				return new Point(maxWidth, maxHeight);	
-				
+				return new Point(680, 340);
 			}
 			
 			protected void layout (Composite composite, boolean flushCache) {
