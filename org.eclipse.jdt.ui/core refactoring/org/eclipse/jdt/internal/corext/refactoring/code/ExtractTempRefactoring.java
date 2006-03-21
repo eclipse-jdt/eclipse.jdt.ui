33b286d6576363102f0c5f8411a0558c4a0ccdbe
Markus Keller <mkeller>
2006-03-21 09:59:11 +0000

    use ASTProvider

--- org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/code/ExtractTempRefactoring.java
+++ org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/code/ExtractTempRefactoring.java
@@ -121,6 +121,7 @@ import org.eclipse.jdt.internal.corext.util.CodeFormatterUtil;
 import org.eclipse.jdt.internal.corext.util.Messages;
 
 import org.eclipse.jdt.internal.ui.JavaPlugin;
+import org.eclipse.jdt.internal.ui.javaeditor.ASTProvider;
 
 /**
  * Extract Local Variable (from selected expression inside method or initializer).
@@ -503,7 +504,7 @@ public class ExtractTempRefactoring extends CommentRefactoring implements IIniti
 			if (result.hasFatalError())
 				return result;
 
-			CompilationUnit rootNode= new RefactoringASTParser(AST.JLS3).parse(fCu, null, true, true, new SubProgressMonitor(pm, 3));
+			CompilationUnit rootNode= ASTProvider.getASTProvider().getAST(fCu, ASTProvider.WAIT_YES, new SubProgressMonitor(pm, 3));
 			
 			result.merge(checkActivationBasics(rootNode, new SubProgressMonitor(pm, 3)));
 			if ((!result.hasFatalError()) && isLiteralNodeSelected())
