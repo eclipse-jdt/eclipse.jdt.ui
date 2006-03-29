892e942169b7e9064a92a570982e9913513a1323
Tobias Widmer <twidmer>
2006-03-29 08:25:23 +0000

    133236 [Model Sync] Java with incoming src directory has no children

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/model/JavaSynchronizationContentProvider.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/model/JavaSynchronizationContentProvider.java
@@ -10,14 +10,17 @@
  *******************************************************************************/
 package org.eclipse.jdt.internal.ui.model;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
 
 import org.eclipse.core.resources.IFile;
@@ -29,17 +32,27 @@ import org.eclipse.core.resources.mapping.ResourceMapping;
 import org.eclipse.core.resources.mapping.ResourceMappingContext;
 import org.eclipse.core.resources.mapping.ResourceTraversal;
 
+import org.eclipse.swt.custom.BusyIndicator;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.TreeItem;
+
 import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.jface.viewers.TreeViewer;
 
 import org.eclipse.ui.navigator.IPipelinedTreeContentProvider;
 import org.eclipse.ui.navigator.PipelinedShapeModification;
 import org.eclipse.ui.navigator.PipelinedViewerUpdate;
 
+import org.eclipse.team.core.diff.FastDiffFilter;
 import org.eclipse.team.core.diff.IDiff;
+import org.eclipse.team.core.diff.IDiffChangeEvent;
+import org.eclipse.team.core.diff.IDiffTree;
 import org.eclipse.team.core.diff.IDiffVisitor;
 import org.eclipse.team.core.mapping.IResourceDiffTree;
 import org.eclipse.team.core.mapping.ISynchronizationContext;
 import org.eclipse.team.core.mapping.ISynchronizationScope;
+import org.eclipse.team.core.mapping.provider.ResourceDiffTree;
 
 import org.eclipse.ltk.core.refactoring.history.RefactoringHistory;
 import org.eclipse.ltk.ui.refactoring.model.AbstractSynchronizationContentProvider;
@@ -63,7 +76,7 @@ import org.eclipse.jdt.internal.ui.JavaPlugin;
 public final class JavaSynchronizationContentProvider extends AbstractSynchronizationContentProvider implements IPipelinedTreeContentProvider {
 
 	/** The refactorings folder */
-//	private static final String NAME_REFACTORING_FOLDER= ".refactorings"; //$NON-NLS-1$
+	private static final String NAME_REFACTORING_FOLDER= ".refactorings"; //$NON-NLS-1$
 
 	/**
 	 * Returns the diffs associated with the element.
@@ -106,13 +119,12 @@ public final class JavaSynchronizationContentProvider extends AbstractSynchroniz
 	 */
 	static ResourceTraversal[] getResourceTraversals(final Object element) {
 		final ResourceMapping mapping= getResourceMapping(element);
-		if (mapping != null) {
+		if (mapping != null)
 			try {
 				return mapping.getTraversals(ResourceMappingContext.LOCAL_CONTEXT, new NullProgressMonitor());
-			} catch (CoreException exception) {
+			} catch (final CoreException exception) {
 				JavaPlugin.log(exception);
 			}
-		}
 		return new ResourceTraversal[0];
 	}
 
@@ -130,11 +142,11 @@ public final class JavaSynchronizationContentProvider extends AbstractSynchroniz
 	 * @return the associated java element, or <code>null</code> if the
 	 *         project is not a java project
 	 */
-	private IJavaElement asJavaProject(final IProject project) {
+	private IJavaProject asJavaProject(final IProject project) {
 		try {
 			if (project.getDescription().hasNature(JavaCore.NATURE_ID))
 				return JavaCore.create(project);
-		} catch (CoreException exception) {
+		} catch (final CoreException exception) {
 			JavaPlugin.log(exception);
 		}
 		return null;
@@ -155,7 +167,7 @@ public final class JavaSynchronizationContentProvider extends AbstractSynchroniz
 				return;
 			}
 		}
-		if (parent instanceof ISynchronizationContext || parent instanceof ISynchronizationScope) {
+		if (parent instanceof ISynchronizationContext) {
 			final Set result= new HashSet();
 			for (final Iterator iterator= modification.getChildren().iterator(); iterator.hasNext();) {
 				final Object element= iterator.next();
@@ -198,6 +210,48 @@ public final class JavaSynchronizationContentProvider extends AbstractSynchroniz
 	/**
 	 * {@inheritDoc}
 	 */
+	public void diffsChanged(final IDiffChangeEvent event, final IProgressMonitor monitor) {
+		syncExec(new Runnable() {
+
+			public void run() {
+				handleChange(event);
+			}
+		}, getViewer().getControl());
+	}
+
+	/**
+	 * Returns the changed projects from the event.
+	 * 
+	 * @param event
+	 *            the event
+	 * @return the changed projects
+	 */
+	private IJavaProject[] getChangedProjects(final IDiffChangeEvent event) {
+		final Set result= new HashSet();
+		final IDiff[] changes= event.getChanges();
+		for (int index= 0; index < changes.length; index++) {
+			final IResource resource= ResourceDiffTree.getResourceFor(changes[index]);
+			if (resource != null) {
+				final IJavaProject project= asJavaProject(resource.getProject());
+				if (project != null)
+					result.add(project);
+			}
+		}
+		final IDiff[] additions= event.getAdditions();
+		for (int index= 0; index < additions.length; index++) {
+			final IResource resource= ResourceDiffTree.getResourceFor(additions[index]);
+			if (resource != null) {
+				final IJavaProject project= asJavaProject(resource.getProject());
+				if (project != null)
+					result.add(project);
+			}
+		}
+		return (IJavaProject[]) result.toArray(new IJavaProject[result.size()]);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
 	protected Object[] getChildrenInContext(final ISynchronizationContext context, final Object parent, final Object[] children) {
 		final Object[] elements= super.getChildrenInContext(context, parent, children);
 		if (parent instanceof IPackageFragment)
@@ -221,6 +275,21 @@ public final class JavaSynchronizationContentProvider extends AbstractSynchroniz
 	}
 
 	/**
+	 * Since the this content provider overrides the resource content provider,
+	 * this method is only invoked when the resource content provider is
+	 * disabled. In this case, we still want the Java projects to appear at the
+	 * root of the view.
+	 * 
+	 * {@inheritDoc}
+	 */
+	public Object[] getElements(Object parent) {
+		if (parent instanceof ISynchronizationContext)
+			// Put the resource projects directly under the context
+			parent= getModelRoot();
+		return super.getElements(parent);
+	}
+
+	/**
 	 * Returns the java project children in the current scope.
 	 * 
 	 * @param context
@@ -239,31 +308,35 @@ public final class JavaSynchronizationContentProvider extends AbstractSynchroniz
 				try {
 					if (!fragment.hasChildren())
 						continue;
-				} catch (JavaModelException exception) {
+				} catch (final JavaModelException exception) {
 					JavaPlugin.log(exception);
 				}
 			}
 			list.add(children[index]);
 		}
-//		final IResource resource= JavaModelProvider.getResource(parent);
-//		if (resource != null) {
-//			final IResourceDiffTree tree= context.getDiffTree();
-//			final IResource[] members= tree.members(resource);
-//			for (int index= 0; index < members.length; index++) {
-//				final int type= members[index].getType();
-//				if (type == IResource.FOLDER) {
-//					if (isInScope(context.getScope(), parent, members[index])) {
-//						if (members[index].getName().equals(NAME_REFACTORING_FOLDER)) {
-//							final RefactoringHistory history= getRefactorings(context, (IProject) resource, null);
-//							if (!history.isEmpty()) {
-//								list.remove(members[index]);
-//								list.addFirst(history);
-//							}
-//						}
-//					}
-//				}
-//			}
-//		}
+
+		final IResource resource= JavaModelProvider.getResource(parent);
+		if (resource != null) {
+			final IResourceDiffTree tree= context.getDiffTree();
+			final IResource[] members= tree.members(resource);
+			for (int index= 0; index < members.length; index++)
+				if (isVisible(context, members[index])) {
+					if (!members[index].exists()) {
+						// Add any phantom resources that are visible
+						list.add(members[index]);
+					}
+					if (members[index] instanceof IFolder) {
+						final IFolder folder= (IFolder) members[index];
+						if (folder.getName().equals(NAME_REFACTORING_FOLDER)) {
+							final RefactoringHistory history= getRefactorings(context, (IProject) resource, null);
+							if (!history.isEmpty()) {
+								list.remove(folder);
+								list.addFirst(history);
+							}
+						}
+					}
+				}
+		}
 		return list.toArray(new Object[list.size()]);
 	}
 
@@ -306,13 +379,12 @@ public final class JavaSynchronizationContentProvider extends AbstractSynchroniz
 				final int type= members[index].getType();
 				if (type == IResource.FILE) {
 					final IDiff diff= tree.getDiff(members[index]);
-					if (diff != null && isVisible(diff)) {
+					if (diff != null && isVisible(diff))
 						if (isInScope(context.getScope(), parent, members[index])) {
 							final IJavaElement element= JavaCore.create(members[index]);
 							if (element != null)
 								set.add(element);
 						}
-					}
 				}
 			}
 		}
@@ -385,7 +457,10 @@ public final class JavaSynchronizationContentProvider extends AbstractSynchroniz
 	 * {@inheritDoc}
 	 */
 	public void getPipelinedChildren(final Object parent, final Set children) {
-		if (parent instanceof ISynchronizationContext || parent instanceof ISynchronizationScope) {
+		if (parent instanceof ISynchronizationContext) {
+			// When a context is the root, the resource content provider returns
+			// projects as direct children. We should replace any projects that
+			// are Java projects with an IJavaProject
 			final Set result= new HashSet(children.size());
 			for (final Iterator iterator= children.iterator(); iterator.hasNext();) {
 				final Object element= iterator.next();
@@ -398,7 +473,11 @@ public final class JavaSynchronizationContentProvider extends AbstractSynchroniz
 				}
 			}
 			children.addAll(result);
-		}
+		} else if (parent instanceof ISynchronizationScope)
+			// When the root is a scope, we should return the
+			// Java model provider so all model providers appear
+			// at the root of the viewer.
+			children.add(getModelProvider());
 	}
 
 	/**
@@ -418,10 +497,92 @@ public final class JavaSynchronizationContentProvider extends AbstractSynchroniz
 	}
 
 	/**
+	 * Returns the removed projects from the event.
+	 * 
+	 * @param event
+	 *            the event
+	 * @return the removed projects
+	 */
+	private Set getRemovedProjects(final IDiffChangeEvent event) {
+		final Set result= new HashSet();
+		final IPath[] removals= event.getRemovals();
+		for (int index= 0; index < removals.length; index++) {
+			final IPath path= removals[index];
+			if (path.segmentCount() > 0)
+				result.add(ResourcesPlugin.getWorkspace().getRoot().getProject(path.segment(0)));
+		}
+		return result;
+	}
+
+	/**
 	 * {@inheritDoc}
 	 */
 	protected ResourceTraversal[] getTraversals(final ISynchronizationContext context, final Object object) {
 		return getResourceTraversals(object);
+	}
+
+	/**
+	 * Returns the visible projects.
+	 * 
+	 * @return the visible projects
+	 */
+	private Set getVisibleProjects() {
+		final TreeItem[] children= ((TreeViewer) getViewer()).getTree().getItems();
+		final Set result= new HashSet();
+		for (int index= 0; index < children.length; index++) {
+			final Object data= children[index].getData();
+			if (data instanceof IJavaProject)
+				result.add(data);
+		}
+		return result;
+	}
+
+	/**
+	 * Handles a diff change event.
+	 * 
+	 * @param event
+	 *            the event
+	 */
+	private void handleChange(final IDiffChangeEvent event) {
+		final Set existing= getVisibleProjects();
+		// First, get the set of projects that contain changes or additions
+		final IJavaProject[] changed= getChangedProjects(event);
+		final List refreshes= new ArrayList(changed.length);
+		final List additions= new ArrayList(changed.length);
+		final List removals= new ArrayList(changed.length);
+		for (int index= 0; index < changed.length; index++) {
+			final IJavaProject project= changed[index];
+			if (event.getTree().getChildren(project.getResource().getFullPath()).length > 0)
+				if (existing.contains(project))
+					refreshes.add(project);
+				else if (hasVisibleChanges(event.getTree(), project))
+					additions.add(project);
+		}
+		// Remove any java projects that correspond to removed projects
+		final Set removed= getRemovedProjects(event);
+		for (final Iterator iterator= existing.iterator(); iterator.hasNext();) {
+			final IJavaProject element= (IJavaProject) iterator.next();
+			if (removed.contains(element.getResource()))
+				removals.add(element);
+		}
+
+		if (!removals.isEmpty() || !additions.isEmpty() || !refreshes.isEmpty()) {
+			final TreeViewer viewer= (TreeViewer) getViewer();
+			final Tree tree= viewer.getTree();
+			try {
+				tree.setRedraw(false);
+				if (!additions.isEmpty())
+					viewer.add(viewer.getInput(), additions.toArray());
+				if (!removals.isEmpty())
+					viewer.remove(viewer.getInput(), removals.toArray());
+				if (!refreshes.isEmpty()) {
+					for (final Iterator iter= refreshes.iterator(); iter.hasNext();)
+						viewer.refresh(iter.next());
+				}
+			} finally {
+				tree.setRedraw(true);
+			}
+		}
 	}
 
 	/**
@@ -445,11 +606,29 @@ public final class JavaSynchronizationContentProvider extends AbstractSynchroniz
 					return true;
 			return false;
 		}
-		for (int index= 0; index < roots.length; index++) {
+		for (int index= 0; index < roots.length; index++)
 			if (path.isPrefixOf(roots[index].getFullPath()))
 				return true;
-		}
 		return false;
+	}
+
+	/**
+	 * Has the java project visible changes?
+	 * 
+	 * @param tree
+	 *            the diff tree
+	 * @param project
+	 *            the java project
+	 * @return <code>true</code> if it has visible changes, <code>false</code>
+	 *         otherwise
+	 */
+	private boolean hasVisibleChanges(final IDiffTree tree, final IJavaProject project) {
+		return tree.hasMatchingDiffs(project.getResource().getFullPath(), new FastDiffFilter() {
+
+			public boolean select(final IDiff diff) {
+				return isVisible(diff);
+			}
+		});
 	}
 
 	/**
@@ -478,7 +657,7 @@ public final class JavaSynchronizationContentProvider extends AbstractSynchroniz
 	/**
 	 * {@inheritDoc}
 	 */
-	public boolean interceptUpdate(PipelinedViewerUpdate anUpdateSynchronization) {
+	public boolean interceptUpdate(final PipelinedViewerUpdate anUpdateSynchronization) {
 		return convertToJavaElements(anUpdateSynchronization);
 	}
 
@@ -494,5 +673,24 @@ public final class JavaSynchronizationContentProvider extends AbstractSynchroniz
 		if (hasChildrenInScope(scope, element, resource))
 			return true;
 		return false;
+	}
+
+	/**
+	 * Executes the given runnable.
+	 * 
+	 * @param runnable
+	 *            the runnable
+	 * @param control
+	 *            the control
+	 */
+	private void syncExec(final Runnable runnable, final Control control) {
+		if (control != null && !control.isDisposed())
+			control.getDisplay().syncExec(new Runnable() {
+
+				public void run() {
+					if (!control.isDisposed())
+						BusyIndicator.showWhile(control.getDisplay(), runnable);
+				}
+			});
 	}
 }
