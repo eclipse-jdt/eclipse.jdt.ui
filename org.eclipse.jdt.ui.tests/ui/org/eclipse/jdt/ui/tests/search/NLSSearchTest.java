62dbcffa21230017cfda297fba28c6e9a406a6d6
Benno Baumgartner <bbaumgart>
2006-04-05 09:06:01 +0000

    Don't load SearchPlugin->Test failer in PluginsNotLoadedTests

--- org.eclipse.jdt.ui.tests/ui/org/eclipse/jdt/ui/tests/search/NLSSearchTest.java
+++ org.eclipse.jdt.ui.tests/ui/org/eclipse/jdt/ui/tests/search/NLSSearchTest.java
@@ -24,21 +24,10 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 
-import org.eclipse.search.ui.NewSearchUI;
-import org.eclipse.search.ui.text.Match;
-
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.IPackageFragment;
 import org.eclipse.jdt.core.IPackageFragmentRoot;
-import org.eclipse.jdt.core.IType;
-import org.eclipse.jdt.core.search.SearchEngine;
-
-import org.eclipse.jdt.internal.ui.refactoring.nls.search.CompilationUnitEntry;
-import org.eclipse.jdt.internal.ui.refactoring.nls.search.FileEntry;
-import org.eclipse.jdt.internal.ui.refactoring.nls.search.NLSSearchQuery;
-import org.eclipse.jdt.internal.ui.refactoring.nls.search.NLSSearchResult;
-import org.eclipse.jdt.internal.ui.util.BusyIndicatorRunnableContext;
 
 import org.eclipse.jdt.testplugin.JavaProjectHelper;
 
@@ -53,8 +42,12 @@ public class NLSSearchTest extends TestCase {
 		super(name);
 	}
 
+	public static Test allTests() {
+		return new ProjectTestSetup(new TestSuite(NLSSearchTest.class));
+	}
+	
 	public static Test suite() {
-		return new ProjectTestSetup(new TestSuite((NLSSearchTest.class)));
+		return allTests();
 	}
 
 	public void setUp() throws CoreException {
@@ -85,90 +78,6 @@ public class NLSSearchTest extends TestCase {
 		return file;
 	}
 
-	private NLSSearchResult searchProblems(ICompilationUnit accessor, IFile propertiesFile) {
-		NLSSearchQuery query= new NLSSearchQuery((new IType[] {accessor.getType("Accessor")}), (new IFile[] {propertiesFile}), SearchEngine.createWorkspaceScope(), "");
-		NewSearchUI.runQueryInForeground(new BusyIndicatorRunnableContext(), query);
-		NLSSearchResult result= (NLSSearchResult)query.getSearchResult();
-		return result;
-	}
-
-	private static void assertNumberResults(NLSSearchResult result, int expected) {
-		int is= result.getElements().length;
-		assertTrue("Expected number of results is " + expected + " but was " + is, is == expected);
-	}
-
-	private void assertResultHasUndefinedKey(String key, IFile file, NLSSearchResult result) throws IOException, CoreException {
-		Match[] matches= result.getFileMatchAdapter().computeContainedMatches(result, file);
-
-		for (int i= 0; i < matches.length; i++) {
-			Match match= matches[i];
-			if (match.getElement() instanceof ICompilationUnit) {
-				ICompilationUnit unit= (ICompilationUnit)match.getElement();
-				String field= unit.getSource().substring(match.getOffset(), match.getOffset() + match.getLength());
-				if (field.indexOf(key) != -1)
-					return;
-			}
-		}
-
-		assertTrue("No undefined key problem found for " + key + " in " + file.getName(), false);		
-	}
-
-	private void assertResultHasUnusedKey(String key, IFile file, NLSSearchResult result) throws IOException, CoreException {
-		Match[] matches= result.getFileMatchAdapter().computeContainedMatches(result, file);
-
-		for (int i= 0; i < matches.length; i++) {
-			Match match= matches[i];
-			if (match.getElement() instanceof CompilationUnitEntry) {
-				ICompilationUnit unit= ((CompilationUnitEntry)match.getElement()).getCompilationUnit();
-				String field= unit.getSource().substring(match.getOffset(), match.getOffset() + match.getLength());
-				if (field.indexOf(key) != -1)
-					return;
-			} else if (match.getElement() instanceof FileEntry) {
-				FileEntry entry= (FileEntry)match.getElement();
-				String content= getContent(entry.getPropertiesFile());
-				String propkey= content.substring(match.getOffset(), match.getOffset() + match.getLength());
-				if (propkey.indexOf(key) != -1)
-					return;
-			}
-		}
-
-		assertTrue("No unused key problem found for " + key + " in " + file.getName(), false);		
-	}
-
-	private void assertResultHasDuplicateKey(String key, IFile file, NLSSearchResult result) throws CoreException, IOException {
-		Match[] matches= result.getFileMatchAdapter().computeContainedMatches(result, file);
-
-		for (int i= 0; i < matches.length; i++) {
-			Match match= matches[i];
-			if (match.getElement() instanceof FileEntry) {
-				FileEntry entry= (FileEntry)match.getElement();
-				String content= getContent(entry.getPropertiesFile());
-				int firstIndex= content.indexOf(key);
-				if (firstIndex != -1 && content.indexOf(key, firstIndex + 1) != -1)
-					return;
-			}
-		}
-
-		assertTrue("No duplicate key problem found for " + key + " in " + file.getName(), false);
-	}
-
-	private static String getContent(IFile entry) throws CoreException, IOException {
-		StringBuffer buf= new StringBuffer();
-		InputStream contents= entry.getContents();
-		try {
-			char ch= (char)contents.read();
-			int avilable= contents.available();
-			while (avilable > 0 && ch != -1) {
-				buf.append(ch);
-				ch= (char)contents.read();
-				avilable--;
-			}
-			return buf.toString();
-		} finally {
-			contents.close();
-		}
-	}
-
 	public void test01() throws Exception {
 		IPackageFragment pack1= fSourceFolder.createPackageFragment("test", false, null);
 		StringBuffer buf= new StringBuffer();
@@ -183,8 +92,7 @@ public class NLSSearchTest extends TestCase {
 
 		IFile propertiesFile= write((IFolder)pack1.getCorrespondingResource(), "", "Accessor.properties");
 
-		NLSSearchResult result= searchProblems(accessor, propertiesFile);
-		assertNumberResults(result, 0);
+		NLSSearchTestHelper.assertNumberOfProblems(accessor, propertiesFile, 0);
 	}
 
 	public void test02() throws Exception {
@@ -213,8 +121,7 @@ public class NLSSearchTest extends TestCase {
 		buf.append("Client_s1=s1\n");
 		IFile propertiesFile= write((IFolder)pack1.getCorrespondingResource(), buf.toString(), "Accessor.properties");
 
-		NLSSearchResult result= searchProblems(accessor, propertiesFile);
-		assertNumberResults(result, 0);
+		NLSSearchTestHelper.assertNumberOfProblems(accessor, propertiesFile, 0);
 	}
 
 	public void test03() throws Exception {
@@ -242,11 +149,10 @@ public class NLSSearchTest extends TestCase {
 		buf= new StringBuffer();
 		IFile propertiesFile= write((IFolder)pack1.getCorrespondingResource(), buf.toString(), "Accessor.properties");
 
-		NLSSearchResult result= searchProblems(accessor, propertiesFile);
-		assertNumberResults(result, 2);
+		NLSSearchTestHelper.assertNumberOfProblems(accessor, propertiesFile, 2);
 
-		assertResultHasUndefinedKey("Client_s1", (IFile)accessor.getCorrespondingResource(), result);
-		assertResultHasUndefinedKey("Client_s1", (IFile)client.getCorrespondingResource(), result);
+		NLSSearchTestHelper.assertHasUndefinedKey(accessor, propertiesFile, "Client_s1", (IFile)accessor.getCorrespondingResource());
+		NLSSearchTestHelper.assertHasUndefinedKey(accessor, propertiesFile, "Client_s1", (IFile)client.getCorrespondingResource());
 	}
 
 	public void test04() throws Exception {
@@ -274,11 +180,10 @@ public class NLSSearchTest extends TestCase {
 		buf.append("Client_s1=foo\n");
 		IFile propertiesFile= write((IFolder)pack1.getCorrespondingResource(), buf.toString(), "Accessor.properties");
 
-		NLSSearchResult result= searchProblems(accessor, propertiesFile);
-		assertNumberResults(result, 2);
+		NLSSearchTestHelper.assertNumberOfProblems(accessor, propertiesFile, 2);
 
-		assertResultHasUnusedKey("Client_s1", (IFile)accessor.getCorrespondingResource(), result);
-		assertResultHasUnusedKey("Client_s1", propertiesFile, result);
+		NLSSearchTestHelper.assertHasUnusedKey(accessor, propertiesFile, "Client_s1", (IFile)accessor.getCorrespondingResource());
+		NLSSearchTestHelper.assertHasUnusedKey(accessor, propertiesFile, "Client_s1", propertiesFile);
 	}
 
 	public void test05() throws Exception {
@@ -305,11 +210,10 @@ public class NLSSearchTest extends TestCase {
 		buf= new StringBuffer();
 		IFile propertiesFile= write((IFolder)pack1.getCorrespondingResource(), buf.toString(), "Accessor.properties");
 
-		NLSSearchResult result= searchProblems(accessor, propertiesFile);
-		assertNumberResults(result, 2);
-
-		assertResultHasUnusedKey("Client_s1", (IFile)accessor.getCorrespondingResource(), result);
-		assertResultHasUndefinedKey("Client_s1", (IFile)accessor.getCorrespondingResource(), result);
+		NLSSearchTestHelper.assertNumberOfProblems(accessor, propertiesFile, 2);
+		
+		NLSSearchTestHelper.assertHasUnusedKey(accessor, propertiesFile, "Client_s1", (IFile)accessor.getCorrespondingResource());
+		NLSSearchTestHelper.assertHasUndefinedKey(accessor, propertiesFile, "Client_s1", (IFile)accessor.getCorrespondingResource());
 	}
 
 	public void test06() throws Exception {
@@ -339,9 +243,8 @@ public class NLSSearchTest extends TestCase {
 		buf.append("Client_s1=s1\n");
 		IFile propertiesFile= write((IFolder)pack1.getCorrespondingResource(), buf.toString(), "Accessor.properties");
 
-		NLSSearchResult result= searchProblems(accessor, propertiesFile);
-		assertNumberResults(result, 1);
-
-		assertResultHasDuplicateKey("Client_s1", propertiesFile, result);
+		NLSSearchTestHelper.assertNumberOfProblems(accessor, propertiesFile, 1);
+		
+		NLSSearchTestHelper.assertHasDuplicateKey(accessor, propertiesFile, "Client_s1", propertiesFile);
 	}
 }
