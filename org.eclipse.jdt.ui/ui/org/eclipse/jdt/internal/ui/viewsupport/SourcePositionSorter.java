a50ba4f1c4b919b6765f75daacc47e0ba5ef5c97
Dani Megert <dmegert>
2006-04-10 11:33:16 +0000

    Fixed bug 135824: [misc] Binary nested type not at source position in unsorted Outline

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/viewsupport/SourcePositionSorter.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/viewsupport/SourcePositionSorter.java
@@ -13,9 +13,14 @@ package org.eclipse.jdt.internal.ui.viewsupport;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerSorter;
 
+import org.eclipse.jdt.core.Flags;
+import org.eclipse.jdt.core.IClassFile;
+import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IMember;
 import org.eclipse.jdt.core.ISourceRange;
 import org.eclipse.jdt.core.ISourceReference;
+import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.JavaModelException;
 
 
@@ -36,8 +41,42 @@ public class SourcePositionSorter extends ViewerSorter {
 		if (!(e2 instanceof ISourceReference))
 			return 0;
 		
-		if (((IJavaElement)e1).getParent() != ((IJavaElement)e2).getParent())
-			return 0;
+		IJavaElement parent1= ((IJavaElement)e1).getParent();
+		if (parent1 == null || !parent1.equals(((IJavaElement)e2).getParent())) {
+				IType t1= getOutermostDeclaringType(e1);
+				if (t1 == null)
+					return 0;
+				
+				IType t2= getOutermostDeclaringType(e2);
+				try {
+					if (!t1.equals(t2)) {
+						if (t2 == null)
+							return 0;
+
+						if (Flags.isPublic(t1.getFlags()) && Flags.isPublic(t2.getFlags()))
+							return 0;
+
+						if (!t1.getPackageFragment().equals(t2.getPackageFragment()))
+							return 0;
+
+						ICompilationUnit cu1= (ICompilationUnit)((IJavaElement)e1).getAncestor(IJavaElement.COMPILATION_UNIT);
+						if (cu1 != null) {
+							if (!cu1.equals(((IJavaElement)e2).getAncestor(IJavaElement.COMPILATION_UNIT)))
+								return 0;
+						} else {
+							IClassFile cf1= (IClassFile)((IJavaElement)e1).getAncestor(IJavaElement.CLASS_FILE);
+							if (cf1 == null)
+								return 0;
+							IClassFile cf2= (IClassFile)((IJavaElement)e2).getAncestor(IJavaElement.CLASS_FILE);
+							String source1= cf1.getSource();
+							if (source1 != null && !source1.equals(cf2.getSource()))
+								return 0;
+						}
+					}
+				} catch (JavaModelException e3) {
+					return 0;
+				}
+		}
 		
 		try {
 			ISourceRange sr1= ((ISourceReference)e1).getSourceRange();
@@ -50,5 +89,26 @@ public class SourcePositionSorter extends ViewerSorter {
 		} catch (JavaModelException e) {
 			return 0;
 		}
+	}
+
+	private IType getOutermostDeclaringType(Object element) {
+		if (!(element instanceof IMember))
+			return null;
+		
+		IType declaringType;
+		if (element instanceof IType)
+			declaringType= (IType)element;
+		else {
+			declaringType= ((IMember)element).getDeclaringType();
+			if (declaringType == null)
+				return null;
+		}
+		
+		IType declaringTypeDeclaringType= declaringType.getDeclaringType();
+		while (declaringTypeDeclaringType != null) {
+			declaringType= declaringTypeDeclaringType;
+			declaringTypeDeclaringType= declaringType.getDeclaringType();
+		}
+		return declaringType;
 	}
 }
