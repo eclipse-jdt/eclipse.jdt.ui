56e3d73738bbcc51417ed6dd4cb6d3e2b3aca767
Dani Megert <dmegert>
2006-04-25 15:50:29 +0000

    Fixed off by one error.

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/JavaWordFinder.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/JavaWordFinder.java
@@ -17,15 +17,13 @@ import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.Region;
 
 public class JavaWordFinder {
-
+	
 	public static IRegion findWord(IDocument document, int offset) {
 
-		int start= -1;
+		int start= -2;
 		int end= -1;
-
-
+		
 		try {
-
 			int pos= offset;
 			char c;
 
@@ -35,7 +33,6 @@ public class JavaWordFinder {
 					break;
 				--pos;
 			}
-
 			start= pos;
 
 			pos= offset;
@@ -47,13 +44,12 @@ public class JavaWordFinder {
 					break;
 				++pos;
 			}
-
 			end= pos;
 
 		} catch (BadLocationException x) {
 		}
 
-		if (start > -1 && end > -1) {
+		if (start >= -1 && end > -1) {
 			if (start == offset && end == offset)
 				return new Region(offset, 0);
 			else if (start == offset)
