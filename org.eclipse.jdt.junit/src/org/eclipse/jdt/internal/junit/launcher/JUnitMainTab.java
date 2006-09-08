4514c977b1693f430e359368a1a40e6160a72919
Markus Keller <mkeller>
2006-09-08 16:38:40 +0000

    avoid internal API break (for PDE)

---
+++ org.eclipse.jdt.junit/src/org/eclipse/jdt/internal/junit/launcher/JUnitMainTab.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *     Sebastian Davids: sdavids@gmx.de bug: 26293, 27889 
+ *     David Saff (saff@mit.edu) - bug 102632: [JUnit] Support for JUnit 4.
+ *******************************************************************************/
+package org.eclipse.jdt.internal.junit.launcher;
+
+import org.eclipse.jdt.junit.launcher.JUnitLaunchConfigurationTab;
+
+/**
+ * TODO: remove after I20060912
+ */
+public class JUnitMainTab extends JUnitLaunchConfigurationTab {
+	
+}
