ba62d0ba049c6d23d27d251c867d2ee5363fd812
Markus Keller <mkeller>
2006-09-15 10:37:23 +0000

    157203: [ltk] [patch] TextEditBasedChange/TextChange provides incorrect diff when one side is empty

--- org.eclipse.jdt.ui.tests.refactoring/test cases/org/eclipse/jdt/ui/tests/changes/AllTests.java
+++ org.eclipse.jdt.ui.tests.refactoring/test cases/org/eclipse/jdt/ui/tests/changes/AllTests.java
@@ -20,6 +20,7 @@ public class AllTests {
 	public static Test suite ( ) {
 		TestSuite suite= new TestSuite("All Change Tests");
 		suite.addTest(TrackPositionTest.suite());
+		suite.addTest(TextDiffContentTest.suite());
 	    return suite;
 	}
 }
