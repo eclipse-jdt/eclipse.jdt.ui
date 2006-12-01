63a607ed3a1bdc432c499031139af0e49fc12938
Dani Megert <dmegert>
2006-12-01 14:16:41 +0000

    Fixed bug 165689: [spell checking] enable quick fix menu

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/propertiesfileeditor/PropertiesFileEditorActionContributor.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/propertiesfileeditor/PropertiesFileEditorActionContributor.java
@@ -10,16 +10,11 @@
  *******************************************************************************/
 package org.eclipse.jdt.internal.ui.propertiesfileeditor;
 
-import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.IStatusLineManager;
 
 import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.IWorkbenchActionConstants;
 import org.eclipse.ui.texteditor.ITextEditor;
-import org.eclipse.ui.texteditor.ITextEditorActionConstants;
-import org.eclipse.ui.texteditor.ITextEditorActionDefinitionIds;
-import org.eclipse.ui.texteditor.RetargetTextEditorAction;
 
 import org.eclipse.ui.editors.text.TextEditorActionContributor;
 
@@ -33,34 +28,11 @@ import org.eclipse.jdt.ui.actions.JdtActionConstants;
  */
 public class PropertiesFileEditorActionContributor extends TextEditorActionContributor {
 
-	protected RetargetTextEditorAction fCorrectionAssist;
-
-
-
-	public PropertiesFileEditorActionContributor() {
-		fCorrectionAssist= new RetargetTextEditorAction(PropertiesFileEditorMessages.getBundleForConstructedKeys(), "CorrectionAssistProposal."); //$NON-NLS-1$
-		fCorrectionAssist.setActionDefinitionId(ITextEditorActionDefinitionIds.QUICK_ASSIST);
-	}
-
-	/*
-	 * @see org.eclipse.ui.part.EditorActionBarContributor#contributeToMenu(org.eclipse.jface.action.IMenuManager)
-	 */
-	public void contributeToMenu(IMenuManager menu) {
-
-		super.contributeToMenu(menu);
-
-		IMenuManager editMenu= menu.findMenuUsingPath(IWorkbenchActionConstants.M_EDIT);
-		if (editMenu != null) {
-			editMenu.appendToGroup(ITextEditorActionConstants.GROUP_ASSIST, fCorrectionAssist);
-		}
-
-	}
-
+	
 	/*
 	 * @see EditorActionBarContributor#setActiveEditor(IEditorPart)
 	 */
 	public void setActiveEditor(IEditorPart part) {
-
 		super.setActiveEditor(part);
 
 		IActionBars actionBars= getActionBars();
@@ -73,7 +45,6 @@ public class PropertiesFileEditorActionContributor extends TextEditorActionContr
 			textEditor= (ITextEditor)part;
 
 		actionBars.setGlobalActionHandler(JdtActionConstants.OPEN, getAction(textEditor, JdtActionConstants.OPEN));
-		fCorrectionAssist.setAction(getAction(textEditor, "CorrectionAssistProposal")); //$NON-NLS-1$
 	}
 
 	/*
