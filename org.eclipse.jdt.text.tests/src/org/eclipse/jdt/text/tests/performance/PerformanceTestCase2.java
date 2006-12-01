6cff626c4f283115277a8d898ffbf609050a9a8e
Dani Megert <dmegert>
2006-12-01 13:04:16 +0000

    Fixed bug 158292: add document performance tests

---
+++ org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/PerformanceTestCase2.java
@@ -0,0 +1,172 @@
+/*******************************************************************************
+ * Copyright (c) 2006 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.jdt.text.tests.performance;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.eclipse.test.performance.Performance;
+import org.eclipse.test.performance.PerformanceMeter;
+
+import junit.framework.TestCase;
+
+
+/**
+ * @since 3.3
+ */
+public class PerformanceTestCase2 extends TestCase {
+
+	private String fBaseScenarioId;
+	private List fPerformanceMeters;
+	
+
+	public PerformanceTestCase2() {
+		super();
+	}
+
+	public PerformanceTestCase2(String name) {
+		super(name);
+	}
+	
+	protected int getWarmUpRuns() {
+		return 20;
+	}
+	
+	protected int getMeasuredRuns() {
+		return 20;
+	}
+	
+	protected void runTest() throws Throwable {
+		assertNotNull(getName());
+		Method runMethod= null;
+		try {
+			// use getMethod to get all public inherited
+			// methods. getDeclaredMethods returns all
+			// methods of this class but excludes the
+			// inherited ones.
+			runMethod= getClass().getMethod(getName(), new Class[] {PerformanceMeter.class});
+		} catch (NoSuchMethodException e) {
+			fail("Method \""+getName()+"\" not found");  //$NON-NLS-1$//$NON-NLS-2$
+		}
+		if (!Modifier.isPublic(runMethod.getModifiers())) {
+			fail("Method \""+getName()+"\" should be public");  //$NON-NLS-1$//$NON-NLS-2$
+		}
+
+		try {
+			int warmUpRuns= getWarmUpRuns();
+			PerformanceMeter nullMeter= Performance.getDefault().getNullPerformanceMeter();
+			Object[] args= { nullMeter };
+			for (int i= 0; i < warmUpRuns; i++)
+				runMethod.invoke(this, args);
+			
+			int measuredRuns= getMeasuredRuns();
+			PerformanceMeter meter= createPerformanceMeter();
+			args[0]= meter;
+			for (int i= 0; i < measuredRuns; i++)
+				runMethod.invoke(this, args);
+			
+			commitAllMeasurements();
+			assertAllPerformance();
+		}
+		catch (InvocationTargetException e) {
+			e.fillInStackTrace();
+			throw e.getTargetException();
+		}
+		catch (IllegalAccessException e) {
+			e.fillInStackTrace();
+			throw e;
+		}
+	}
+	
+	/**
+	 * Returns the base scenario id for this test which has the default
+	 * scenario id as its default.
+	 * 
+	 * @return the base scenario id
+	 */
+	protected final String getBaseScenarioId() {
+		if (fBaseScenarioId == null)
+			fBaseScenarioId= Performance.getDefault().getDefaultScenarioId(this);
+		return fBaseScenarioId;
+	}
+
+	/**
+	 * Create a performance meter with the given scenario id. The
+	 * performance meter will be disposed on {@link #tearDown()}.
+	 * 
+	 * @param scenarioId the scenario id
+	 * @return the created performance meter
+	 */
+	private PerformanceMeter internalCreatePerformanceMeter(String scenarioId) {
+		PerformanceMeter performanceMeter= Performance.getDefault().createPerformanceMeter(scenarioId);
+		addPerformanceMeter(performanceMeter);
+		return performanceMeter;
+	}
+	
+	/**
+	 * Add the given performance meter to the managed performance meters.
+	 * 
+	 * @param performanceMeter the performance meter
+	 */
+	private void addPerformanceMeter(PerformanceMeter performanceMeter) {
+		if (fPerformanceMeters == null)
+			fPerformanceMeters= new ArrayList();
+		fPerformanceMeters.add(performanceMeter);
+	}
+	
+	/**
+	 * Commits the measurements captured by all performance meters created
+	 * through one of this class' factory methods.
+	 */
+	protected final void commitAllMeasurements() {
+		if (fPerformanceMeters != null)
+			for (Iterator iter= fPerformanceMeters.iterator(); iter.hasNext();)
+				((PerformanceMeter) iter.next()).commit();
+	}
+	
+	/**
+	 * Asserts default properties of the measurements captured by the given
+	 * performance meter.
+	 * 
+	 * @param performanceMeter the performance meter
+	 * @throws RuntimeException if the properties do not hold
+	 */
+	protected final void assertPerformance(PerformanceMeter performanceMeter) {
+		Performance.getDefault().assertPerformance(performanceMeter);
+	}
+
+	/**
+	 * Asserts default properties of the measurements captured by all
+	 * performance meters created through one of this class' factory
+	 * methods.
+	 * 
+	 * @throws RuntimeException if the properties do not hold
+	 */
+	protected final void assertAllPerformance() {
+		if (fPerformanceMeters != null)
+			for (Iterator iter= fPerformanceMeters.iterator(); iter.hasNext();)
+				assertPerformance((PerformanceMeter) iter.next());
+	}
+	
+	/**
+	 * Create a performance meter with the base scenario id. The
+	 * performance meter will be disposed on {@link #tearDown()}.
+	 * 
+	 * @return the created performance meter
+	 */
+	protected final PerformanceMeter createPerformanceMeter() {
+		return internalCreatePerformanceMeter(getBaseScenarioId());
+	}
+}
---
+++ org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/TextPerformanceTestCase2.java
@@ -0,0 +1,115 @@
+/*******************************************************************************
+ * Copyright (c) 2006 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.jdt.text.tests.performance;
+
+
+import org.eclipse.core.runtime.Platform;
+
+import org.eclipse.jdt.text.tests.JdtTextTestPlugin;
+
+
+/**
+ * @since 3.3
+ */
+public class TextPerformanceTestCase2 extends PerformanceTestCase2 {
+	/** containing plug-in id */
+	private static final String PLUGIN_ID= JdtTextTestPlugin.PLUGIN_ID;
+	
+	/** boolean option, a value of <code>"true"</code> enables overriding of the number of runs */
+	private static final String OVERRIDE_RUNS_OPTION= "/debug/performance/OverrideRuns";
+
+	/** integer option, its value overrides the number of warm-up runs */
+	private static final String OVERRIDE_WARM_UP_RUNS_OPTION= "/debug/performance/OverrideWarmUpRuns";
+
+	/** integer option, its value overrides the number of measured runs */
+	private static final String OVERRIDE_MEASURED_RUNS_OPTION= "/debug/performance/OverrideMeasuredRuns";
+
+	/** <code>true</code> iff the number of runs should be overridden */
+	private static final boolean OVERRIDE_RUNS= Boolean.toString(true).equals(Platform.getDebugOption(PLUGIN_ID + OVERRIDE_RUNS_OPTION));
+	
+	/** overridden number of warm-up runs */
+	private static final int OVERRIDE_WARM_UP_RUNS= intValueOf(Platform.getDebugOption(PLUGIN_ID + OVERRIDE_WARM_UP_RUNS_OPTION), 2);
+	
+	/** overridden number of measured runs */
+	private static final int OVERRIDE_MEASURED_RUNS= intValueOf(Platform.getDebugOption(PLUGIN_ID + OVERRIDE_MEASURED_RUNS_OPTION), 2);
+	
+	/**
+	 * Returns the integer value of the given string unless the string
+	 * cannot be interpreted as such, in this case the given default is
+	 * returned.
+	 * 
+	 * @param stringValue the string to be interpreted as integer
+	 * @param defaultValue the default integer value
+	 * @return the integer value
+	 */
+	private static int intValueOf(String stringValue, int defaultValue) {
+		try {
+			if (stringValue != null)
+				return Integer.valueOf(stringValue).intValue();
+		} catch (NumberFormatException e) {
+			// use default
+		}
+		return defaultValue;
+	}
+	
+	/** custom number of warm-up runs */
+	private int fCustomWarmUpRuns= 20;
+	
+	/** custom number of measured runs */
+	private int fCustomMeasuredRuns= 20;
+
+	public TextPerformanceTestCase2() {
+		super();
+	}
+
+	public TextPerformanceTestCase2(String name) {
+		super(name);
+	}
+	
+	/**
+	 * @return number of warm-up runs, must have been set before
+	 */
+	protected final int getWarmUpRuns() {
+		if (OVERRIDE_RUNS)
+			return OVERRIDE_WARM_UP_RUNS;
+		return fCustomWarmUpRuns;
+	}
+	
+	/**
+	 * Sets the number of warm-up runs. Can be overridden.
+	 * 
+	 * @param runs number of warm-up runs
+	 */
+	protected final void setWarmUpRuns(int runs) {
+		assertTrue(runs >= 0);
+		fCustomWarmUpRuns= runs;
+	}
+	
+	/**
+	 * @return number of measured runs, must have been set before
+	 */
+	protected final int getMeasuredRuns() {
+		if (OVERRIDE_RUNS)
+			return OVERRIDE_MEASURED_RUNS;
+		return fCustomMeasuredRuns;
+	}
+	
+	/**
+	 * Sets the number of measured runs. Can be overridden.
+	 * 
+	 * @param runs number of measured runs
+	 */
+	protected final void setMeasuredRuns(int runs) {
+		assertTrue(runs >= 0);
+		fCustomMeasuredRuns= runs;
+	}
+
+}
