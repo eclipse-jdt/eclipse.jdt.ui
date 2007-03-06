007a7bc422ea3782052447c0407cb3e66787cc06
Markus Keller <mkeller>
2007-03-06 14:49:56 +0000

    174405: [rename] Refactoring dialog blanks out editor

--- org.eclipse.jdt.ui/ui refactoring/org/eclipse/jdt/internal/ui/refactoring/reorg/RenameLinkedMode.java
+++ org.eclipse.jdt.ui/ui refactoring/org/eclipse/jdt/internal/ui/refactoring/reorg/RenameLinkedMode.java
@@ -11,7 +11,13 @@ import org.eclipse.core.runtime.Status;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.VerifyEvent;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 
 import org.eclipse.jface.dialogs.IDialogSettings;
@@ -31,6 +37,7 @@ import org.eclipse.jface.text.link.LinkedPositionGroup;
 import org.eclipse.jface.text.link.LinkedModeUI.ExitFlags;
 import org.eclipse.jface.text.link.LinkedModeUI.IExitPolicy;
 import org.eclipse.jface.text.source.ISourceViewer;
+import org.eclipse.jface.text.source.SourceViewer;
 
 import org.eclipse.ui.texteditor.link.EditorLinkedModeUI;
 
@@ -246,8 +253,42 @@ public class RenameLinkedMode {
 	void doRename(boolean showPreview) {
 		cancel();
 		
+		Image image= null;
+		Label label= null;
+		
 		try {
-			fEditor.getViewer().getTextWidget().setRedraw(false);
+			ISourceViewer viewer= fEditor.getViewer();
+			if (viewer instanceof SourceViewer) {
+				SourceViewer sourceViewer= (SourceViewer) viewer;
+				Control viewerControl= sourceViewer.getControl();
+				if (viewerControl instanceof Composite) {
+					Composite composite= (Composite) viewerControl;
+					Display display= composite.getDisplay();
+					
+					// Flush pending redraw requests:
+					while (! display.isDisposed() && display.readAndDispatch()) {
+					}
+					
+					// Copy editor area:
+					GC gc= new GC(composite);
+					Point size;
+					try {
+						size= composite.getSize();
+						image= new Image(gc.getDevice(), size.x, size.y);
+						gc.copyArea(image, 0, 0);
+					} finally {
+						gc.dispose();
+						gc= null;
+					}
+					
+					// Persist editor area while executing refactoring:
+					label= new Label(composite, SWT.NONE);
+					label.setImage(image);
+					label.setBounds(0, 0, size.x, size.y);
+					label.moveAbove(null);
+				}
+			}
+			
 			String newName= fNamePosition.getContent();
 			if (fOriginalName.equals(newName))
 				return;
@@ -276,7 +317,10 @@ public class RenameLinkedMode {
 		} catch (BadLocationException e) {
 			JavaPlugin.log(e);
 		} finally {
-			fEditor.getViewer().getTextWidget().setRedraw(true);
+			if (label != null)
+				label.dispose();
+			if (image != null)
+				image.dispose();
 		}
 	}
 
