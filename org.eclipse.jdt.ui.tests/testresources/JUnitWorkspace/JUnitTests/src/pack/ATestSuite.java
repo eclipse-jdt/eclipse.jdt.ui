5a93a0739431c7d60796268c648fc8283f081941
Markus Keller <mkeller>
2007-03-13 15:45:33 +0000

    don't extend TestSuite

--- org.eclipse.jdt.ui.tests/testresources/JUnitWorkspace/JUnitTests/src/pack/ATestSuite.java
+++ org.eclipse.jdt.ui.tests/testresources/JUnitWorkspace/JUnitTests/src/pack/ATestSuite.java
@@ -1,6 +1,6 @@
 package pack;
 import junit.framework.*;
-public class ATestSuite extends TestSuite {
+public class ATestSuite {
 	public static Test suite() {
 		TestSuite suite= new TestSuite("Test Suite");
 		suite.addTestSuite(ATestCase.class);
