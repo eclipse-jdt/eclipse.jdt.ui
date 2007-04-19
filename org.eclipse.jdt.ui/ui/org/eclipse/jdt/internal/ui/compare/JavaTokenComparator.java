191219982a545a98c212424146cb40f9a92c0c8c
Dani Megert <dmegert>
2007-04-19 16:09:28 +0000

    Modified patch from Michael to fix bug 78063: [Compare] Full Javadoc line selected even though only a character has been added to a word

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/compare/JavaTokenComparator.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/compare/JavaTokenComparator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,11 +12,15 @@ package org.eclipse.jdt.internal.ui.compare;
 
 import org.eclipse.core.runtime.Assert;
 
-import org.eclipse.jdt.core.ToolFactory;
-import org.eclipse.jdt.core.compiler.*;
-
 import org.eclipse.compare.contentmergeviewer.ITokenComparator;
 import org.eclipse.compare.rangedifferencer.IRangeComparator;
+
+import org.eclipse.jdt.core.ToolFactory;
+import org.eclipse.jdt.core.compiler.IScanner;
+import org.eclipse.jdt.core.compiler.ITerminalSymbols;
+import org.eclipse.jdt.core.compiler.InvalidInputException;
+
+import org.eclipse.jdt.internal.corext.dom.TokenScanner;
 
 
 /**
@@ -24,21 +28,25 @@ import org.eclipse.compare.rangedifferencer.IRangeComparator;
  */
 public class JavaTokenComparator implements ITokenComparator {
 		
+	private static final boolean DEBUG= false;
 	private String fText;
-	private boolean fShouldEscape= true;
+	private boolean fAllowToSkip= true;
 	private int fCount;
 	private int[] fStarts;
 	private int[] fLengths;
 
 	/**
-	 * Creates a TokenComparator for the given string.
+	 * Creates a token comparator for the given string.
+	 * 
+	 * @param text the text to be tokenized 
+	 * @param allowToSkip <code>true</code> it is allowed to skip range comparisons 
 	 */
-	public JavaTokenComparator(String text, boolean shouldEscape) {
+	public JavaTokenComparator(String text, boolean allowToSkip) {
 		
-		Assert.isNotNull(text);
+		Assert.isLegal(text != null);
 		
 		fText= text;
-		fShouldEscape= shouldEscape;
+		fAllowToSkip= allowToSkip;
 		
 		int length= fText.length();
 		fStarts= new int[length];
@@ -47,29 +55,72 @@ public class JavaTokenComparator implements ITokenComparator {
 		
 		IScanner scanner= ToolFactory.createScanner(true, true, false, false); // returns comments & whitespace
 		scanner.setSource(fText.toCharArray());
+		int endPos= 0;
 		try {
-			int endPos= 0;
-			while (scanner.getNextToken() != ITerminalSymbols.TokenNameEOF) {
+			int tokenType;
+			while ((tokenType= scanner.getNextToken()) != ITerminalSymbols.TokenNameEOF) {
 				int start= scanner.getCurrentTokenStartPosition();
 				int end= scanner.getCurrentTokenEndPosition()+1;
-				fStarts[fCount]= start;
-				fLengths[fCount]= end - start;
+				// Comments are treated as a single token (see bug https://bugs.eclipse.org/bugs/show_bug.cgi?id=78063)
+				if (TokenScanner.isComment(tokenType)) {
+					int dl= getCommentStartTokenLength(tokenType);
+					recordTokenRange(start, dl);
+					parseComment(start + dl, text.substring(start + dl, end), allowToSkip);
+				} else {
+					recordTokenRange(start, end - start);
+				}
 				endPos= end;
-				fCount++;
-			}
-			// workaround for #13907
-			if (endPos < length) {
-				fStarts[fCount]= endPos;
-				fLengths[fCount]= length-endPos;
-				fCount++;
 			}
 		} catch (InvalidInputException ex) {
-			// NeedWork
+			// We couldn't parse part of the input. Fall through and make the rest a single token
 		}
-	}	
+		// Workaround for https://bugs.eclipse.org/bugs/show_bug.cgi?id=13907
+		if (endPos < length) {
+			recordTokenRange(endPos, length - endPos);
+		}
+	}
 
 	/**
-	 * Returns the number of token in the string.
+	 * Records the given token range.
+	 * 
+	 * @param start of the token
+	 * @param length length of the token
+	 * @since 3.3
+	 */
+	private void recordTokenRange(int start, int length) {
+		fStarts[fCount]= start;
+		fLengths[fCount]= length;
+		fCount++;
+		if (DEBUG)
+			System.out.println(fText.substring(start, start + length));
+	}
+
+	private void parseComment(int start, String commentText, boolean shouldEscape) {
+		JavaTokenComparator subTokenizer= new JavaTokenComparator(commentText, shouldEscape);
+		int count= subTokenizer.getRangeCount();
+		for (int i= 0; i < count; i++) {
+			int subStart= subTokenizer.getTokenStart(i);
+			int subLength= subTokenizer.getTokenLength(i);
+			recordTokenRange(start + subStart, subLength);
+		}
+	}
+
+	/**
+	 * Returns the length of the token that
+	 * initiates the given comment type. 
+	 * 
+	 * @param tokenType
+	 * @return the length of the token that start a comment
+	 * @since 3.3
+	 */
+	private static int getCommentStartTokenLength(int tokenType) {
+		if (tokenType == ITerminalSymbols.TokenNameCOMMENT_JAVADOC)
+			return 3;
+		return 2;
+	}
+
+	/**
+	 * Returns the number of tokens in the string.
 	 *
 	 * @return number of token in the string
 	 */
@@ -120,26 +171,31 @@ public class JavaTokenComparator implements ITokenComparator {
 	/**
 	 * Aborts the comparison if the number of tokens is too large.
 	 *
+	 * @param length a number on which to base the decision whether to return
+	 * 	<code>true</code> or <code>false</code>
+	 * @param maxLength another number on which to base the decision whether to return
+	 *	<code>true</code> or <code>false</code>
+	 * @param other the other <code>IRangeComparator</code> to compare with
 	 * @return <code>true</code> to abort a token comparison
 	 */
-	public boolean skipRangeComparison(int length, int max, IRangeComparator other) {
+	public boolean skipRangeComparison(int length, int maxLength, IRangeComparator other) {
 
-		if (!fShouldEscape)
+		if (!fAllowToSkip)
 			return false;
 
 		if (getRangeCount() < 50 || other.getRangeCount() < 50)
 			return false;
 
-		if (max < 100)
+		if (maxLength < 100)
 			return false;
 
 		if (length < 100)
 			return false;
 
-		if (max > 800)
+		if (maxLength > 800)
 			return true;
 
-		if (length < max / 4)
+		if (length < maxLength / 4)
 			return false;
 
 		return true;
