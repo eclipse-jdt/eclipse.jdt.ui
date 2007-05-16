08f9dec76fa73406f78921c80eb3251c440f018c
Benno Baumgartner <bbaumgart>
2007-05-16 08:58:34 +0000

    Bug 171022 [build path] Editing of user library resets export state of this library

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/wizards/buildpaths/UserLibraryWizardPage.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/wizards/buildpaths/UserLibraryWizardPage.java
@@ -70,6 +70,7 @@ public class UserLibraryWizardPage extends NewElementWizardPage implements IClas
 	private Set fUsedPaths;
 	private boolean fIsEditMode;
 	private IJavaProject fProject;
+	private boolean fIsExported;
 	
 	public UserLibraryWizardPage() {
 		super("UserLibraryWizardPage"); //$NON-NLS-1$
@@ -247,7 +248,7 @@ public class UserLibraryWizardPage extends NewElementWizardPage implements IClas
 	 */
 	public IClasspathEntry getSelection() {
 		if (fEditResult != null) {
-			return JavaCore.newContainerEntry(fEditResult.getPath());
+			return JavaCore.newContainerEntry(fEditResult.getPath(), fIsExported);
 		}
 		return null;
 	}
@@ -260,7 +261,7 @@ public class UserLibraryWizardPage extends NewElementWizardPage implements IClas
 		IClasspathEntry[] res= new IClasspathEntry[selected.size()];
 		for (int i= 0; i < res.length; i++) {
 			CPUserLibraryElement curr= (CPUserLibraryElement) selected.get(i);
-			res[i]= JavaCore.newContainerEntry(curr.getPath());
+			res[i]= JavaCore.newContainerEntry(curr.getPath(), fIsExported);
 		}
 		return res;
 	}
@@ -269,6 +270,8 @@ public class UserLibraryWizardPage extends NewElementWizardPage implements IClas
 	 * @see org.eclipse.jdt.ui.wizards.IClasspathContainerPage#setSelection(org.eclipse.jdt.core.IClasspathEntry)
 	 */
 	public void setSelection(IClasspathEntry containerEntry) {
+		fIsExported= containerEntry != null && containerEntry.isExported();
+		
 		updateDescription(containerEntry);
 		fIsEditMode= (containerEntry != null);
 		if (containerEntry != null) {
