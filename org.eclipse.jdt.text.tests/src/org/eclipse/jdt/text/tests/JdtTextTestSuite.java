5600883e7ce11937af1311cfb9385bb6c2df79b0
Dani Megert <dmegert>
2007-08-06 14:42:16 +0000

    Backported fix for bug 198890: [templates] some typos in templates

--- org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/JdtTextTestSuite.java
+++ org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/JdtTextTestSuite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -15,6 +15,7 @@ import junit.framework.TestSuite;
 import org.eclipse.jdt.text.tests.comments.CommentsTestSuite;
 import org.eclipse.jdt.text.tests.contentassist.ContentAssistTestSuite;
 import org.eclipse.jdt.text.tests.spelling.SpellingTestSuite;
+import org.eclipse.jdt.text.tests.templates.TemplatesTestSuite;
 
 
 /**
@@ -51,6 +52,7 @@ public class JdtTextTestSuite {
 		suite.addTest(BreakContinueTargetFinderTest.suite());
 		suite.addTest(ContentAssistTestSuite.suite());
 		suite.addTest(IndentActionTest.suite());
+		suite.addTest(TemplatesTestSuite.suite());
 		//$JUnit-END$
 		
 		return suite;
---
+++ org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/templates/TemplatesTestSuite.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2007 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.jdt.text.tests.templates;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+
+/**
+ * Template test suite.
+ * 
+ * @since 3.4
+ */
+public class TemplatesTestSuite {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite("Test Suite for org.eclipse.jdt.internal.corext.template"); //$NON-NLS-1$
+		//$JUnit-BEGIN$
+		suite.addTest(TemplateContributionTest.suite());
+		//$JUnit-END$
+		return suite;
+	}
+}
