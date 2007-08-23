81f36a736b1c21f2f53cca3a2db9d60a2a6177ff
Markus Keller <mkeller>
2007-08-23 09:11:06 +0000

    190700: [convert local] NPE in PromoteTempToFieldRefactoring.checkTempTypeForLocalTypeUsage

---
+++ org.eclipse.jdt.ui.tests.refactoring/resources/PromoteTempToField/canPromote/A_testDeclaringMethodBindingUnavailable01_in.java
@@ -0,0 +1,11 @@
+// 9, 14, 9, 18
+package p;
+import java.util.Date;
+class A {
+	void a() {
+	}
+
+	void a() {
+		Date date= new Date(2006, 5, 3);
+	}
+}
---
+++ org.eclipse.jdt.ui.tests.refactoring/resources/PromoteTempToField/canPromote/A_testDeclaringMethodBindingUnavailable01_out.java
@@ -0,0 +1,13 @@
+// 9, 14, 9, 18
+package p;
+import java.util.Date;
+class A {
+	private Date fDate;
+
+	void a() {
+	}
+
+	void a() {
+		fDate= new Date(2006, 5, 3);
+	}
+}
