f52b76dab8b6cba7c87bbad7fd9419c4d94554c6
Martin Aeschlimann <maeschli>
2007-09-26 13:22:33 +0000

    204642 [change method signature] Logical error in restoreSelection of ChangeParametersControl

--- org.eclipse.jdt.ui/ui refactoring/org/eclipse/jdt/internal/ui/refactoring/ChangeParametersControl.java
+++ org.eclipse.jdt.ui/ui refactoring/org/eclipse/jdt/internal/ui/refactoring/ChangeParametersControl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -529,9 +529,10 @@ public class ChangeParametersControl extends Composite {
 				fTableViewer.refresh();
 				fTableViewer.getControl().setFocus();
 				int itemCount= getTableItemCount();
-				if (itemCount != 0 && index >= itemCount) {
-					index= itemCount - 1;
-					getTable().setSelection(index);
+				if (itemCount != 0) {
+				    if (index >= itemCount)
+				        index= itemCount - 1;
+				    getTable().setSelection(index);
 				}
 				fListener.parameterListChanged();
 				updateButtonsEnabledState();
