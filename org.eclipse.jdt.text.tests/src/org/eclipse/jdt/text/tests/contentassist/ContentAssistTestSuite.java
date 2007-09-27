4aa145898092ee220715953b6cfa28e6b2791484
Dani Megert <dmegert>
2007-09-27 08:58:36 +0000

    Moved CodeCompleteionTest from jdt.ui.tests to jdt.text.tests.

--- org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/contentassist/ContentAssistTestSuite.java
+++ org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/contentassist/ContentAssistTestSuite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2006 IBM Corporation and others.
+ * Copyright (c) 2005, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,6 +12,7 @@ package org.eclipse.jdt.text.tests.contentassist;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
+
 
 /**
  * Content assist test suite.
@@ -33,6 +34,7 @@ public class ContentAssistTestSuite {
 		suite.addTest(MethodParameterGuessingCompletionTest.suite());
 		suite.addTest(TypeCompletionTest.suite());
 		suite.addTest(SpecialMethodsCompletionTest.suite());
+		suite.addTest(CodeCompletionTest.suite());
 		//$JUnit-END$
 		
 		return suite;
