766d084002f7410945473e61713d4d0ec72f14ec
Martin Aeschlimann <maeschli>
2007-10-02 14:33:24 +0000

    added SearchLeakTest

--- org.eclipse.jdt.ui.tests/ui/org/eclipse/jdt/ui/tests/LeakTestSuite.java
+++ org.eclipse.jdt.ui.tests/ui/org/eclipse/jdt/ui/tests/LeakTestSuite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -16,6 +16,7 @@ import junit.framework.TestSuite;
 import org.eclipse.jdt.ui.tests.leaks.JavaLeakTest;
 import org.eclipse.jdt.ui.tests.leaks.TextViewerUndoManagerLeakTest;
 import org.eclipse.jdt.ui.tests.leaks.UndoManagerLeakTest;
+import org.eclipse.jdt.ui.tests.search.SearchLeakTest;
 
 
 /**
@@ -38,6 +39,7 @@ public class LeakTestSuite extends TestSuite {
 	 */
 	public LeakTestSuite() {
 		addTest(JavaLeakTest.suite());
+		addTest(SearchLeakTest.suite());
 		addTest(UndoManagerLeakTest.suite());
 		addTest(TextViewerUndoManagerLeakTest.suite());
 	}
