9f1628f732bfa9fba03113df8f1bfe23c59b2307
Benno Baumgartner <bbaumgart>
2007-10-11 15:03:22 +0000

    185333 [package explorer] Better defaults when switching to working set mode

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/workingsets/ConfigureWorkingSetAction.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/workingsets/ConfigureWorkingSetAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -39,7 +39,12 @@ public class ConfigureWorkingSetAction extends Action {
 	 */
 	public void run() {
 		List workingSets= new ArrayList(Arrays.asList(fWorkingSetModel.getAllWorkingSets()));
-		IWorkingSet[] activeWorkingSets= fWorkingSetModel.getActiveWorkingSets();
+		IWorkingSet[] activeWorkingSets;
+		if (fWorkingSetModel.needsConfiguration()) {
+			activeWorkingSets= fWorkingSetModel.getAllWorkingSets();
+		} else {
+			activeWorkingSets= fWorkingSetModel.getActiveWorkingSets();
+		}
 		WorkingSetConfigurationDialog dialog= new WorkingSetConfigurationDialog(
 			fSite.getShell(), 
 			(IWorkingSet[])workingSets.toArray(new IWorkingSet[workingSets.size()]),
