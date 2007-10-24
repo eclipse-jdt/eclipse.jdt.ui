811643af63de8f343eabb4b27c2712a91cf8c6b7
Martin Aeschlimann <maeschli>
2007-10-24 14:12:25 +0000

    add unimplemented methods

--- org.eclipse.jdt.ui.tests/ui/org/eclipse/jdt/ui/tests/browsing/TestDelta.java
+++ org.eclipse.jdt.ui.tests/ui/org/eclipse/jdt/ui/tests/browsing/TestDelta.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -168,6 +168,9 @@ public class TestDelta implements IJavaElementDelta {
 	 */
 	public CompilationUnit getCompilationUnitAST() {
 		// TODO Auto-generated method stub
+		return null;
+	}
+	public IJavaElementDelta[] getAnnotationDeltas() {
 		return null;
 	}
 
