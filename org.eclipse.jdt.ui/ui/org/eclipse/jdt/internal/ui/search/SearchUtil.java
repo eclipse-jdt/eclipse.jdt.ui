4f84450da8e37d1597a3bde14bb2537560ceb53a
Dani Megert <dmegert>
2007-10-30 15:52:28 +0000

    Fixed Javadoc warnings.

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/search/SearchUtil.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/search/SearchUtil.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -70,6 +70,8 @@ public class SearchUtil {
 	 * of the Search plug-in: the VM verifies the method call and hence loads the
 	 * types used in the method signature, eventually triggering the loading of
 	 * a plug-in (in this case ISearchQuery results in Search plug-in being loaded).
+	 * 
+	 * @param query the search query 
 	 */
 	public static void runQueryInBackground(Object query) {
 		NewSearchUI.runQueryInBackground((ISearchQuery)query);
@@ -80,6 +82,10 @@ public class SearchUtil {
 	 * of the Search plug-in: the VM verifies the method call and hence loads the
 	 * types used in the method signature, eventually triggering the loading of
 	 * a plug-in (in this case ISearchQuery results in Search plug-in being loaded).
+	 * 
+	 * @param context the runnable context 
+	 * @param query the search query
+	 * @return status
 	 */
 	public static IStatus runQueryInForeground(IRunnableContext context, Object query) {
 		return NewSearchUI.runQueryInForeground(context, (ISearchQuery)query);
