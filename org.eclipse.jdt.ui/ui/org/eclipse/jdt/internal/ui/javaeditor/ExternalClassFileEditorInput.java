3cbd5ef7d1c8f40b7a01186a24aba75cd10c9201
Dani Megert <dmegert>
2007-11-02 10:08:34 +0000

    Fixed adapter.

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/javaeditor/ExternalClassFileEditorInput.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/javaeditor/ExternalClassFileEditorInput.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -9,8 +9,6 @@
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
 package org.eclipse.jdt.internal.ui.javaeditor;
-
-
 
 import org.eclipse.core.resources.IFile;
 
@@ -54,7 +52,7 @@ public class ExternalClassFileEditorInput extends FileEditorInput implements ICl
 	public Object getAdapter(Class adapter) {
 		if (adapter == IClassFile.class)
 			return fClassFile;
-		return fClassFile.getAdapter(adapter);
+		return super.getAdapter(adapter);
 	}
 
 }
