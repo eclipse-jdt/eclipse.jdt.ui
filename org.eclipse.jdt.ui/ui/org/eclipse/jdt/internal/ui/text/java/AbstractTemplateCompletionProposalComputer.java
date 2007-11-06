e82d6340849675236455dd2b728b6826c2b0c537
Dani Megert <dmegert>
2007-11-06 14:26:30 +0000

    Never remove a suggested template.

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/java/AbstractTemplateCompletionProposalComputer.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/java/AbstractTemplateCompletionProposalComputer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2007 IBM Corporation and others.
+ * Copyright (c) 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -13,9 +13,7 @@ package org.eclipse.jdt.internal.ui.text.java;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 
@@ -29,9 +27,10 @@ import org.eclipse.jdt.ui.text.java.JavaContentAssistInvocationContext;
 import org.eclipse.jdt.internal.ui.text.template.contentassist.TemplateEngine;
 import org.eclipse.jdt.internal.ui.text.template.contentassist.TemplateProposal;
 
+
 /**
  * An template completion proposal computer can generate template completion proposals
- * from a given TemplateEngine. 
+ * from a given TemplateEngine.
  * 
  * Subclasses must implement {@link #computeCompletionEngine(JavaContentAssistInvocationContext)}
  * 
@@ -57,45 +56,38 @@ public abstract class AbstractTemplateCompletionProposalComputer implements IJav
 			return Collections.EMPTY_LIST;
 
 		fEngine= computeCompletionEngine(javaContext);
-		if (fEngine != null) {
-			
-			fEngine.reset();
-			fEngine.complete(javaContext.getViewer(), javaContext.getInvocationOffset(), unit);
+		if (fEngine == null)
+			return Collections.EMPTY_LIST;
 
-			TemplateProposal[] templateProposals= fEngine.getResults();
-			List result= new ArrayList(Arrays.asList(templateProposals));
+		fEngine.reset();
+		fEngine.complete(javaContext.getViewer(), javaContext.getInvocationOffset(), unit);
 
-			IJavaCompletionProposal[] keyWordResults= javaContext.getKeywordProposals();
-			if (keyWordResults.length > 0) {
-				List removals= new ArrayList();
-				
-				// update relevance of template proposals that match with a keyword
-				// give those templates slightly more relevance than the keyword to
-				// sort them first
-				// remove keyword templates that don't have an equivalent
-				// keyword proposal
-				if (keyWordResults.length > 0) {
-					outer: for (int k= 0; k < templateProposals.length; k++) {
-						TemplateProposal curr= templateProposals[k];
-						String name= curr.getTemplate().getName();
-						for (int i= 0; i < keyWordResults.length; i++) {
-							String keyword= keyWordResults[i].getDisplayString();
-							if (name.startsWith(keyword)) {
-								curr.setRelevance(keyWordResults[i].getRelevance() + 1);
-								continue outer;
-							}
-						}
-						if (isKeyword(name))
-							removals.add(curr);
+		TemplateProposal[] templateProposals= fEngine.getResults();
+		List result= new ArrayList(Arrays.asList(templateProposals));
+
+		IJavaCompletionProposal[] keyWordResults= javaContext.getKeywordProposals();
+		if (keyWordResults.length == 0)
+			return result;
+
+		/* Update relevance of template proposals that match with a keyword
+		 * give those templates slightly more relevance than the keyword to
+		 * sort them first.
+		 */
+		for (int k= 0; k < templateProposals.length; k++) {
+			TemplateProposal curr= templateProposals[k];
+			String name= curr.getTemplate().getPattern();
+			for (int i= 0; i < keyWordResults.length; i++) {
+				String keyword= keyWordResults[i].getDisplayString();
+				if (name.startsWith(keyword)) {
+					String content= curr.getTemplate().getPattern();
+					if (content.startsWith(keyword)) {
+						curr.setRelevance(keyWordResults[i].getRelevance() + 1);
+						break;
 					}
 				}
-				
-				result.removeAll(removals);
 			}
-			return result;
 		}
-		
-		return Collections.EMPTY_LIST;
+		return result;
 	}
 
 	/**
@@ -111,58 +103,6 @@ public abstract class AbstractTemplateCompletionProposalComputer implements IJav
 	 */
 	public List computeContextInformation(ContentAssistInvocationContext context, IProgressMonitor monitor) {
 		return Collections.EMPTY_LIST;
-	}
-
-	private static final Set KEYWORDS;
-	
-	static {
-		Set keywords= new HashSet(42);
-		keywords.add("abstract"); //$NON-NLS-1$
-		keywords.add("assert"); //$NON-NLS-1$
-		keywords.add("break"); //$NON-NLS-1$
-		keywords.add("case"); //$NON-NLS-1$
-		keywords.add("catch"); //$NON-NLS-1$
-		keywords.add("class"); //$NON-NLS-1$
-		keywords.add("continue"); //$NON-NLS-1$
-		keywords.add("default"); //$NON-NLS-1$
-		keywords.add("do"); //$NON-NLS-1$
-		keywords.add("else"); //$NON-NLS-1$
-		keywords.add("elseif"); //$NON-NLS-1$
-		keywords.add("extends"); //$NON-NLS-1$
-		keywords.add("final"); //$NON-NLS-1$
-		keywords.add("finally"); //$NON-NLS-1$
-		keywords.add("for"); //$NON-NLS-1$
-		keywords.add("if"); //$NON-NLS-1$
-		keywords.add("implements"); //$NON-NLS-1$
-		keywords.add("import"); //$NON-NLS-1$
-		keywords.add("interface"); //$NON-NLS-1$
-		keywords.add("native"); //$NON-NLS-1$
-		keywords.add("new"); //$NON-NLS-1$
-		keywords.add("package"); //$NON-NLS-1$
-		keywords.add("private"); //$NON-NLS-1$
-		keywords.add("protected"); //$NON-NLS-1$
-		keywords.add("public"); //$NON-NLS-1$
-		keywords.add("return"); //$NON-NLS-1$
-		keywords.add("static"); //$NON-NLS-1$
-		keywords.add("strictfp"); //$NON-NLS-1$
-		keywords.add("super"); //$NON-NLS-1$
-		keywords.add("switch"); //$NON-NLS-1$
-		keywords.add("synchronized"); //$NON-NLS-1$
-		keywords.add("this"); //$NON-NLS-1$
-		keywords.add("throw"); //$NON-NLS-1$
-		keywords.add("throws"); //$NON-NLS-1$
-		keywords.add("transient"); //$NON-NLS-1$
-		keywords.add("try"); //$NON-NLS-1$
-		keywords.add("volatile"); //$NON-NLS-1$
-		keywords.add("while"); //$NON-NLS-1$
-		keywords.add("true"); //$NON-NLS-1$
-		keywords.add("false"); //$NON-NLS-1$
-		keywords.add("null"); //$NON-NLS-1$
-		KEYWORDS= Collections.unmodifiableSet(keywords);
-	}
-
-	private boolean isKeyword(String name) {
-		return KEYWORDS.contains(name);
 	}
 
 	/*
