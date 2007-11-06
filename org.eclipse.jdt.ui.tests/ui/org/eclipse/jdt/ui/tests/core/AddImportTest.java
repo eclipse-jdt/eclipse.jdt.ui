ab40a79d49405b4f4114be581bfe7a121c748514
Martin Aeschlimann <maeschli>
2007-11-06 17:38:07 +0000

    removed unnecessary thrown Exception

--- org.eclipse.jdt.ui.tests/ui/org/eclipse/jdt/ui/tests/core/AddImportTest.java
+++ org.eclipse.jdt.ui.tests/ui/org/eclipse/jdt/ui/tests/core/AddImportTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -47,8 +47,6 @@ import org.eclipse.jdt.internal.ui.text.correction.AssistContext;
 
 import org.eclipse.jdt.testplugin.JavaProjectHelper;
 import org.eclipse.jdt.testplugin.TestOptions;
-
-import org.osgi.service.prefs.BackingStoreException;
 
 public class AddImportTest extends CoreTests {
 	
@@ -1194,7 +1192,7 @@ public class AddImportTest extends CoreTests {
 		return importRewrite.getAddedStaticImports();
 	}
 
-	private ImportRewrite newImportsRewrite(ICompilationUnit cu, String[] order, int threshold, boolean restoreExistingImports) throws CoreException, BackingStoreException {
+	private ImportRewrite newImportsRewrite(ICompilationUnit cu, String[] order, int threshold, boolean restoreExistingImports) throws CoreException {
 		return newImportsRewrite(cu, order, threshold, threshold, restoreExistingImports);
 	}
 		
