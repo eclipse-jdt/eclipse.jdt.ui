7968ce90e8c394dde0a4d6b319851523d4b7fb04
Martin Aeschlimann <maeschli>
2007-11-07 13:29:21 +0000

    Use JavaModelUtil.getFullyQualifiedName

--- org.eclipse.jdt.ui.tests/ui/org/eclipse/jdt/ui/tests/core/TypeInfoTest.java
+++ org.eclipse.jdt.ui.tests/ui/org/eclipse/jdt/ui/tests/core/TypeInfoTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -147,7 +147,7 @@ public class TypeInfoTest extends TestCase {
 		if (!resolvedType.exists()) {
 			assertTrue("Resolved type does not exist: " + ref.toString(), false);
 		}
-		StringAsserts.assertEqualString(resolvedType.getFullyQualifiedName('.'), JavaModelUtil.getFullyQualifiedName(ref));
+		StringAsserts.assertEqualString(JavaModelUtil.getFullyQualifiedName(resolvedType), JavaModelUtil.getFullyQualifiedName(ref));
 	}
 	
 	private void findTypeRef(List refs, String fullname) {
