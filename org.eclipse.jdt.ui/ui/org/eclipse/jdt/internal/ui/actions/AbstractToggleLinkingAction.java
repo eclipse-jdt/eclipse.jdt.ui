f044768478e508230a735366bb5129a0f9aa0e98
Martin Aeschlimann <maeschli>
2007-11-13 14:55:27 +0000

    unchecked by default

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/actions/AbstractToggleLinkingAction.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/actions/AbstractToggleLinkingAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -35,6 +35,7 @@ public abstract class AbstractToggleLinkingAction extends Action {
 		setToolTipText(ActionMessages.ToggleLinkingAction_tooltip); 
 		JavaPluginImages.setLocalImageDescriptors(this, "synced.gif"); //$NON-NLS-1$		
 		PlatformUI.getWorkbench().getHelpSystem().setHelp(this, IJavaHelpContextIds.LINK_EDITOR_ACTION);
+		setChecked(false);
 	}
 
 	/**
