4c54191cc74e3d885251d96d1b3122dceed398d3
Martin Aeschlimann <maeschli>
2007-12-03 14:47:08 +0000

    fixed deprecation

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/workingsets/WorkingSetMenuContributionItem.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/workingsets/WorkingSetMenuContributionItem.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -64,7 +64,7 @@ public class WorkingSetMenuContributionItem extends ContributionItem {
 		
 		mi.setText("&" + fId + " " + name);  //$NON-NLS-1$  //$NON-NLS-2$
 		if (fImage == null) {
-			ImageDescriptor imageDescriptor= fWorkingSet.getImage();
+			ImageDescriptor imageDescriptor= fWorkingSet.getImageDescriptor();
 			if (imageDescriptor != null)
 				fImage= imageDescriptor.createImage();
 		}
