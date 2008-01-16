c5be3a85489ba21c6d7f30a4616cd4295d33e02b
Markus Keller <mkeller>
2008-01-16 15:38:16 +0000

    202781: [jar exporter] JarFileExportOperation does not export class-files anymore

--- org.eclipse.jdt.ui.tests/ui/org/eclipse/jdt/ui/tests/jarexport/JarExportTests.java
+++ org.eclipse.jdt.ui.tests/ui/org/eclipse/jdt/ui/tests/jarexport/JarExportTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007 IBM Corporation and others.
+ * Copyright (c) 2007, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -19,7 +19,8 @@ public class JarExportTests {
 	public static Test suite() {
 		TestSuite suite= new TestSuite("Test for org.eclipse.jdt.ui.tests.jarexport");
 		//$JUnit-BEGIN$
-		suite.addTest(FatJarExportTests.allTests());
+		suite.addTest(PlainJarExportTests.suite());
+		suite.addTest(FatJarExportTests.suite());
 		//$JUnit-END$
 		return suite;
 	}
