b051dabd427b66614f53854b1924c739619d771b
Dani Megert <dmegert>
2008-01-16 11:36:48 +0000

    Fixed bug 202781: [jar exporter] JarFileExportOperation does not export class-files anymore

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/jarpackager/JarFileExportOperation.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/jarpackager/JarFileExportOperation.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2007 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -302,6 +302,13 @@ public class JarFileExportOperation extends WorkspaceModifyOperation implements 
 					return;
 				}
 				if (isInJavaProject) {
+
+					IJavaElement je= JavaCore.create(resource);
+					if (je instanceof ITypeRoot && je.exists()) {
+						exportElement(je, progressMonitor);
+						return;
+					}
+
 					jProject= JavaCore.create(resource.getProject());
 					try {
 						IPackageFragment pkgFragment= jProject.findPackageFragment(resource.getFullPath().removeLastSegments(1));
