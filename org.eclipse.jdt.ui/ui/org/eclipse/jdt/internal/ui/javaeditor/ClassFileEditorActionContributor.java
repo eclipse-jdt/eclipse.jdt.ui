2b144a7cc8f67510760ad1daeee93ff41e1f5014
Dani Megert <dmegert>
2008-01-22 10:49:01 +0000

    Don't activate if breadcrumb is active.

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/javaeditor/ClassFileEditorActionContributor.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/javaeditor/ClassFileEditorActionContributor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,13 +8,26 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
-
 package org.eclipse.jdt.internal.ui.javaeditor;
+
+import org.eclipse.ui.IEditorPart;
 
 
 public class ClassFileEditorActionContributor extends BasicJavaEditorActionContributor {
 
 	public ClassFileEditorActionContributor() {
 		super();
+	}
+	
+	/*
+	 * @see org.eclipse.jdt.internal.ui.javaeditor.BasicJavaEditorActionContributor#setActiveEditor(org.eclipse.ui.IEditorPart)
+	 * @since 3.4
+	 */
+	public void setActiveEditor(IEditorPart part) {
+		if (part instanceof JavaEditor) {
+			if (((JavaEditor) part).isBreadcrumbActive())
+				return;
+		}
+		super.setActiveEditor(part);
 	}
 }
