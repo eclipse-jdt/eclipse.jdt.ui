1131ad8121dd7b80af9e9a10bdf6ed871b5661f8
Dani Megert <dmegert>
2008-01-23 14:53:10 +0000

    Fixed Javadoc warnings.

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/filters/OutputFolderFilter.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/filters/OutputFolderFilter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,10 +11,11 @@
 package org.eclipse.jdt.internal.ui.filters;
 
 
-import org.eclipse.core.resources.IFolder;
-import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
+
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
 
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerFilter;
@@ -24,7 +25,7 @@ import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.JavaCore;
 
 
-/** 
+/**
  * Filters out all output folders.
  * <p>
  * Note: Folder which are direct children of a Java element
@@ -38,8 +39,10 @@ public class OutputFolderFilter extends ViewerFilter {
 	/**
 	 * Returns the result of this filter, when applied to the
 	 * given element.
-	 *
-	 * @param element the element to test 
+	 * 
+	 * @param viewer the viewer
+	 * @param parent the parent
+	 * @param element the element to test
 	 * @return <code>true</code> if element should be included
 	 * @since 3.0
 	 */
