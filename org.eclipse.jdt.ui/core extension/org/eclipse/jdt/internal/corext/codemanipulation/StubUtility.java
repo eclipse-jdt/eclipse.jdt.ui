67430966e440fcb4d901d63e11063c03282cd5d6
Markus Keller <mkeller>
2008-02-15 14:37:33 +0000

    really use project setting for line delimiter

--- org.eclipse.jdt.ui/core extension/org/eclipse/jdt/internal/corext/codemanipulation/StubUtility.java
+++ org.eclipse.jdt.ui/core extension/org/eclipse/jdt/internal/corext/codemanipulation/StubUtility.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2007 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -816,7 +816,8 @@ public class StubUtility {
 				// Use project setting
 			}
 		}
-		return getProjectLineDelimiter(null);
+		IJavaProject project= elem.getJavaProject();
+		return getProjectLineDelimiter(project.exists() ? project : null);
 	}
 
 	/**
