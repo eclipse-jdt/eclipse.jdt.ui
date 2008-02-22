cf8e956ec4427408da59e3a943831c2d1bc675d7
Martin Aeschlimann <maeschli>
2008-02-22 14:08:58 +0000

    210636 [content assist] adding type sometimes adds qualification instead of importing

--- org.eclipse.jdt.ui/core extension/org/eclipse/jdt/internal/corext/codemanipulation/ContextSensitiveImportRewriteContext.java
+++ org.eclipse.jdt.ui/core extension/org/eclipse/jdt/internal/corext/codemanipulation/ContextSensitiveImportRewriteContext.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2007 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -78,7 +78,7 @@ public class ContextSensitiveImportRewriteContext extends ImportRewriteContext {
 		Name[] names= getImportedNames();
 		for (int i= 0; i < names.length; i++) {
 			IBinding binding= names[i].resolveBinding();
-			if (binding instanceof ITypeBinding) {
+			if (binding instanceof ITypeBinding && !binding.isRecovered()) {
 				ITypeBinding typeBinding= (ITypeBinding)binding;
 				if (isConflictingType(typeBinding, qualifier, name)) {
 					return RES_NAME_CONFLICT;
