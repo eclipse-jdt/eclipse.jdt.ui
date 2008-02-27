3697ba60ab18f1b6858b23682b58ef551fdb9a81
Martin Aeschlimann <maeschli>
2008-02-27 16:59:16 +0000

    workaround for NPE

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/viewsupport/ColoringLabelProvider.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/viewsupport/ColoringLabelProvider.java
@@ -10,6 +10,11 @@
  *******************************************************************************/
 package org.eclipse.jdt.internal.ui.viewsupport;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.StyleRange;
+import org.eclipse.swt.graphics.Image;
+
+import org.eclipse.jface.viewers.BaseLabelProvider;
 import org.eclipse.jface.viewers.ColumnViewer;
 import org.eclipse.jface.viewers.DecoratingStyledCellLabelProvider;
 import org.eclipse.jface.viewers.IDecorationContext;
@@ -19,8 +24,19 @@ import org.eclipse.jface.viewers.ViewerColumn;
 
 public class ColoringLabelProvider extends DecoratingStyledCellLabelProvider implements ILabelProvider {
 	
+	//TODO remove after IBuild > 20080227
+	static class VoidLD extends BaseLabelProvider  implements ILabelDecorator {
+		public Image decorateImage(Image image, Object element) {
+			return null;
+		}
+
+		public String decorateText(String text, Object element) {
+			return null;
+		}
+	}
+	
 	public ColoringLabelProvider(IStyledLabelProvider labelProvider) {
-		this(labelProvider, null, null);
+		this(labelProvider, new VoidLD(), null);
 	}
 	
 	public ColoringLabelProvider(IStyledLabelProvider labelProvider, ILabelDecorator decorator, IDecorationContext decorationContext) {
@@ -52,6 +68,15 @@ public class ColoringLabelProvider extends DecoratingStyledCellLabelProvider imp
 		} else if (showColoredLabels) {
 			viewer.refresh();
 		}
+	}
+	
+	protected StyleRange prepareStyleRange(StyleRange styleRange, boolean applyColors) {
+		if (!applyColors && styleRange.background != null) {
+			styleRange= super.prepareStyleRange(styleRange, applyColors);
+			styleRange.borderStyle= SWT.BORDER_DASH;
+			return styleRange;
+		}
+		return super.prepareStyleRange(styleRange, applyColors);
 	}
 	
 	public String getText(Object element) {
