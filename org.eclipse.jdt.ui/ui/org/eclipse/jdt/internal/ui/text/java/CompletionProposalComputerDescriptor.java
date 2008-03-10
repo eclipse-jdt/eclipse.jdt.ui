08b47b4aeab5e41c42d32a59d1f7a16e8ff8660d
Dani Megert <dmegert>
2008-03-10 14:17:09 +0000

    Protect against failing to load computer.

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/java/CompletionProposalComputerDescriptor.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/java/CompletionProposalComputerDescriptor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2007 IBM Corporation and others.
+ * Copyright (c) 2005, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -124,14 +124,20 @@ final class CompletionProposalComputerDescriptor {
 	private boolean fIsReportingDelay= false;
 	/** The start of the last operation. */
 	private long fStart;
+	/**
+	 * Tells whether we tried to load the computer.
+	 * @since 3.4
+	 */
+	boolean fTriedLoadingComputer= false;
 
+	
 	/**
 	 * Creates a new descriptor.
 	 * 
 	 * @param element the configuration element to read
 	 * @param registry the computer registry creating this descriptor
 	 * @param categories the categories
-	 * @throws InvalidRegistryObjectException if this extension is no longer valid 
+	 * @throws InvalidRegistryObjectException if this extension is no longer valid
 	 */
 	CompletionProposalComputerDescriptor(IConfigurationElement element, CompletionProposalComputerRegistry registry, List categories) throws InvalidRegistryObjectException {
 		Assert.isLegal(registry != null);
@@ -194,7 +200,7 @@ final class CompletionProposalComputerDescriptor {
 	 * 
 	 * @param obj the element to be checked
 	 * @param attribute the attribute
-	 * @throws InvalidRegistryObjectException if <code>obj</code> is <code>null</code> 
+	 * @throws InvalidRegistryObjectException if <code>obj</code> is <code>null</code>
 	 */
 	private void checkNotNull(Object obj, String attribute) throws InvalidRegistryObjectException {
 		if (obj == null) {
@@ -232,7 +238,7 @@ final class CompletionProposalComputerDescriptor {
 	public Set getPartitions() {
 		return fPartitions;
 	}
-	
+
 	/**
 	 * Returns a cached instance of the computer as described in the
 	 * extension's xml. The computer is
@@ -246,8 +252,10 @@ final class CompletionProposalComputerDescriptor {
 	 *         valid any longer (e.g. due to plug-in unloading)
 	 */
 	private synchronized IJavaCompletionProposalComputer getComputer() throws CoreException, InvalidRegistryObjectException {
-		if (fComputer == null && (fActivate || isPluginLoaded()))
+		if (fComputer == null && !fTriedLoadingComputer && (fActivate || isPluginLoaded())) {
+			fTriedLoadingComputer= true;
 			fComputer= createComputer();
+		}
 		return fComputer;
 	}
 
@@ -485,14 +493,14 @@ final class CompletionProposalComputerDescriptor {
 	}
 
 	private IStatus createExceptionStatus(CoreException x) {
-		// unable to instantiate the extension - log & disable
+		// unable to instantiate the extension - log
 		String blame= createBlameMessage();
 		String reason= JavaTextMessages.CompletionProposalComputerDescriptor_reason_instantiation;
 		return new Status(IStatus.ERROR, JavaPlugin.getPluginId(), IStatus.OK, blame + " " + reason, x); //$NON-NLS-1$
 	}
 	
 	private IStatus createExceptionStatus(RuntimeException x) {
-		// misbehaving extension - log & disable
+		// misbehaving extension - log
 		String blame= createBlameMessage();
 		String reason= JavaTextMessages.CompletionProposalComputerDescriptor_reason_runtime_ex;
 		return new Status(IStatus.WARNING, JavaPlugin.getPluginId(), IStatus.OK, blame + " " + reason, x); //$NON-NLS-1$
@@ -550,7 +558,7 @@ final class CompletionProposalComputerDescriptor {
 	        return fElement.getContributor();
         } catch (InvalidRegistryObjectException e) {
         	return null;
-        }	    
+        }
     }
 	
 }
