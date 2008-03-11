e1a80b09badd333ec38d88124e7ca31c11707482
Benno Baumgartner <bbaumgart>
2008-03-11 15:35:01 +0000

    221743 [preferences] Save Actions preference page layout is smushed

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/fix/CleanUpSaveParticipantPreferenceConfiguration.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/fix/CleanUpSaveParticipantPreferenceConfiguration.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2007 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -191,7 +191,7 @@ public class CleanUpSaveParticipantPreferenceConfiguration extends AbstractSaveP
 	
 	private Composite createAdvancedComposite(final Composite parent) {
 		Composite composite= new Composite(parent, SWT.NONE);
-		GridData gridData= new GridData(SWT.FILL, SWT.FILL, true, true);
+		GridData gridData= new GridData(SWT.FILL, SWT.TOP, true, false);
 		gridData.horizontalIndent= INDENT;
 		composite.setLayoutData(gridData);
 		GridLayout layout= new GridLayout(2, false);
