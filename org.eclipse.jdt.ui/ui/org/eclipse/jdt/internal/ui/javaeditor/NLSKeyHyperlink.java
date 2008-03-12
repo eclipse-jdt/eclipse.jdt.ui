327a84f9ac551f4814059fbc97fe474f63bb7db9
Benno Baumgartner <bbaumgart>
2008-03-12 15:43:58 +0000

    222369 [navigation] better (shorter) name for NLS hyperlink

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/javaeditor/NLSKeyHyperlink.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/javaeditor/NLSKeyHyperlink.java
@@ -197,6 +197,6 @@ public class NLSKeyHyperlink implements IHyperlink {
 	public String getHyperlinkText() {
 		String bundleName= fAccessorClassReference.getResourceBundleName();
 		String propertyFileName= bundleName.substring(bundleName.lastIndexOf('.') + 1, bundleName.length()) + NLSRefactoring.PROPERTY_FILE_EXT;
-		return Messages.format(JavaEditorMessages.Editor_OpenPropertiesFile_hyperlinkText, new Object[] { fKeyName, propertyFileName });
+		return Messages.format(JavaEditorMessages.Editor_OpenPropertiesFile_hyperlinkText, new Object[] { propertyFileName });
 	}
 }
