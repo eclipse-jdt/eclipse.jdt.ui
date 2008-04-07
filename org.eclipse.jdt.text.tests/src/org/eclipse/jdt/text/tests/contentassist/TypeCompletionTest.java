28e4da34e649717a30cba41e18bb7cb02edd2262
Dani Megert <dmegert>
2008-04-07 09:47:21 +0000

    Adjusted test to adopt fix for bug 215331.

--- org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/contentassist/TypeCompletionTest.java
+++ org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/contentassist/TypeCompletionTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2007 IBM Corporation and others.
+ * Copyright (c) 2005, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -117,7 +117,7 @@ public class TypeCompletionTest extends AbstractCompletionTest {
 	public void testGenericInnerTypeOuterImported() throws Exception {
 		addImport("java.util.Map");
 		expectImport("java.util.Map");
-		assertMethodBodyProposal("Map.E|", "Map<K,V>.Entry", "Map.Entry<|K|, V>");
+		assertMethodBodyProposal("Map.E|", "Entry", "Map.Entry<|K|, V>");
 	}
 	
 	public void testInnerTypeOfGenericOuter() throws Exception {
