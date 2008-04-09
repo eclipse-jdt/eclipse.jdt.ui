6355b131c5683d1dd2fd44bc4edfe7c8a5c68d4e
Dani Megert <dmegert>
2008-04-09 17:22:46 +0000

    ma: 70005 Missing Getter/Setter code assist proposals

--- org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/contentassist/CodeCompletionTest.java
+++ org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/contentassist/CodeCompletionTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2007 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -532,6 +532,7 @@ public class CodeCompletionTest extends AbstractCompletionTest {
 	
 	private CompletionProposalCollector createCollector(ICompilationUnit cu, int offset) throws PartInitException, JavaModelException {
 		CompletionProposalCollector collector= new CompletionProposalCollector(cu);
+		collector.setRequireExtendedContext(true);
 		collector.setInvocationContext(createContext(offset, cu));
 		return collector;
 	}
