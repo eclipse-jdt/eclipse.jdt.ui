3e0e4ba94df617b2ffd67aeb6addba8db45db1ad
Markus Keller <mkeller>
2008-04-17 10:02:00 +0000

    Javadoc

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/ui/wizards/ClasspathAttributeConfiguration.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/ui/wizards/ClasspathAttributeConfiguration.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -22,7 +22,7 @@ import org.eclipse.jdt.core.IJavaProject;
  * A {@link ClasspathAttributeConfiguration} specifies how a {@link IClasspathAttribute class path attribute} is presented and configured
  * in the Java build path dialog. 
  * <p>
- * Clients should implement this interface and include the name of their 
+ * Clients should extend this class and include the name of their 
  * class in an extension contributed to the jdt.ui's classpath attribute configuration 
  * extension point (named <code>org.eclipse.jdt.ui.classpathAttributeConfiguration
  * </code>).
