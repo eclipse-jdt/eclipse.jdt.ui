e2458108aa026890e79a38f058dcf6226b420a13
Dani Megert <dmegert>
2008-04-21 10:05:04 +0000

    Removed BIDI segment listener test.

--- org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/JdtTextTestSuite.java
+++ org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/JdtTextTestSuite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2007 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -36,7 +36,6 @@ public class JdtTextTestSuite {
 		suite.addTest(JavaPartitionerExtensionTest.suite());
 		suite.addTest(JavaColoringTest.suite());
 		suite.addTest(SmartSemicolonAutoEditStrategyTest.suite());
-		suite.addTest(JavaLineSegmentationTest.suite());
 		suite.addTest(JavaPartitionerTest.suite());
 		suite.addTest(PropertiesFilePartitionerTest.suite());
 //		suite.addTest(PartitionTokenScannerTest.suite());
