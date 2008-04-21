2c899c80efe310bd693fe5689ee4e2241cd47e57
Dani Megert <dmegert>
2008-04-21 13:35:19 +0000

    Removed useless call.

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/ui/text/JavaSourceViewerConfiguration.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/ui/text/JavaSourceViewerConfiguration.java
@@ -438,7 +438,6 @@ public class JavaSourceViewerConfiguration extends TextSourceViewerConfiguration
 
 			JavaCompositeReconcilingStrategy strategy= new JavaCompositeReconcilingStrategy(sourceViewer, editor, getConfiguredDocumentPartitioning(sourceViewer));
 			JavaReconciler reconciler= new JavaReconciler(editor, strategy, false);
-			reconciler.setIsIncrementalReconciler(false);
 			reconciler.setIsAllowedToModifyDocument(false);
 			reconciler.setDelay(500);
 
