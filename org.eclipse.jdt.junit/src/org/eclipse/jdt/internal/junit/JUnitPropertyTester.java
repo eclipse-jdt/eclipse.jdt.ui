299d30d87729957c1bc10baf5ba971e86e1fc057
Martin Aeschlimann <maeschli>
2008-04-22 08:00:49 +0000

    228146 [JUnit] flaw in JUnitPropertyTester.test(...) logic?

--- org.eclipse.jdt.junit/src/org/eclipse/jdt/internal/junit/JUnitPropertyTester.java
+++ org.eclipse.jdt.junit/src/org/eclipse/jdt/internal/junit/JUnitPropertyTester.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2007 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -62,7 +62,6 @@ public class JUnitPropertyTester extends PropertyTester {
 				if (element == null) {
 					return false;
 				}
-				return false;
 			}
 		}
 		if (PROPERTY_IS_TEST.equals(property)) { 
