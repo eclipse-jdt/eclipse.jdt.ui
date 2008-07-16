c1a33a8bd12ebc0175b1626214573fd8c8b092d4
Dani Megert <dmegert>
2008-07-16 12:18:44 +0000

    Fixed compiler warnings.

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/java/hover/AnnotationExpandHover.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/java/hover/AnnotationExpandHover.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2007 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -54,7 +54,7 @@ import org.eclipse.jdt.internal.ui.text.java.hover.AnnotationExpansionControl.An
  * This class got moved here form Platform Text since it was not used there
  * and caused discouraged access warnings. It will be moved down again once
  * annotation roll-over support is provided by Platform Text.
- *   
+ * 
  * @since 3.2
  */
 public class AnnotationExpandHover implements IAnnotationHover, IAnnotationHoverExtension {
@@ -219,7 +219,7 @@ public class AnnotationExpandHover implements IAnnotationHover, IAnnotationHover
 		
 		if (messagesAtPosition.containsKey(position)) {
 			Object value= messagesAtPosition.get(position);
-			if (message == null || message.equals(value))
+			if (message.equals(value))
 				return true;
 
 			if (value instanceof List) {
