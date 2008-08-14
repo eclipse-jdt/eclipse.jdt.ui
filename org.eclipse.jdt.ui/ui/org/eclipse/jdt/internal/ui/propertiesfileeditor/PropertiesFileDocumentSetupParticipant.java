01b3c0d55dc8b27040efa7d51f525866bb9bcb38
Dani Megert <dmegert>
2008-08-14 07:33:33 +0000

    Fixed Javadoc warning.

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/propertiesfileeditor/PropertiesFileDocumentSetupParticipant.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/propertiesfileeditor/PropertiesFileDocumentSetupParticipant.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -32,6 +32,7 @@ public class PropertiesFileDocumentSetupParticipant  implements IDocumentSetupPa
 	}
 
 	/**
+	 * @param document the document
 	 * @see org.eclipse.core.filebuffers.IDocumentSetupParticipant#setup(org.eclipse.jface.text.IDocument)
 	 */
 	public static void setupDocument(IDocument document) {
