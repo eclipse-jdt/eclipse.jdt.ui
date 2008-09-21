d246389083498f24c07ba8887db53a41886c4fcb
Dani Megert <dmegert>
2008-09-21 16:50:23 +0000

    Fixed compile warning.

--- org.eclipse.jdt.ui/ui refactoring/org/eclipse/jdt/internal/ui/refactoring/nls/search/Properties.java
+++ org.eclipse.jdt.ui/ui refactoring/org/eclipse/jdt/internal/ui/refactoring/nls/search/Properties.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -39,7 +39,7 @@ class Properties extends java.util.Properties {
 	/*
 	 * @see java.util.Map#put(Object, Object)
 	 */
-	public Object put(Object arg0, Object arg1) {
+	public synchronized Object put(Object arg0, Object arg1) {
 		if (arg0 != null && containsKey(arg0))
 			fDuplicateKeys.add(arg0);
 		return super.put(arg0, arg1);
