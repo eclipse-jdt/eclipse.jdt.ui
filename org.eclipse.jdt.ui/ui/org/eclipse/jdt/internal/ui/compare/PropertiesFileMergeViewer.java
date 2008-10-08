52226ad2fd84a14ba98b25ac66d41b49a925c8e3
Dani Megert <dmegert>
2008-10-08 16:19:59 +0000

    Fixed bug 80213: [Viewers] Compare editor background color doesn't obey settings

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/compare/PropertiesFileMergeViewer.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/compare/PropertiesFileMergeViewer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,10 +10,17 @@
  *******************************************************************************/
 package org.eclipse.jdt.internal.ui.compare;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.DisposeEvent;
 import org.eclipse.swt.widgets.Composite;
 
 import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.util.IPropertyChangeListener;
+import org.eclipse.jface.util.PropertyChangeEvent;
 
 import org.eclipse.jface.text.IDocumentPartitioner;
 import org.eclipse.jface.text.TextViewer;
@@ -23,8 +30,6 @@ import org.eclipse.jface.text.source.SourceViewerConfiguration;
 
 import org.eclipse.compare.CompareConfiguration;
 import org.eclipse.compare.contentmergeviewer.TextMergeViewer;
-
-import org.eclipse.jdt.ui.text.JavaTextTools;
 
 import org.eclipse.jdt.internal.ui.JavaPlugin;
 import org.eclipse.jdt.internal.ui.propertiesfileeditor.IPropertiesFilePartitions;
@@ -37,6 +42,13 @@ import org.eclipse.jdt.internal.ui.propertiesfileeditor.PropertiesFileSourceView
  * @since 3.1
  */
 public class PropertiesFileMergeViewer extends TextMergeViewer {
+
+	private List fSourceViewerConfigurations;
+
+	private IPropertyChangeListener fPreferenceChangeListener;
+
+	private IPreferenceStore fPreferenceStore;
+
 
 	/**
 	 * Creates a properties file merge viewer under the given parent control.
@@ -52,16 +64,28 @@ public class PropertiesFileMergeViewer extends TextMergeViewer {
 	 * @see org.eclipse.compare.contentmergeviewer.TextMergeViewer#configureTextViewer(org.eclipse.jface.text.TextViewer)
 	 */
 	protected void configureTextViewer(TextViewer textViewer) {
-		if (textViewer instanceof SourceViewer) {
-			JavaTextTools tools= JavaCompareUtilities.getJavaTextTools();
-			if (tools != null)
-				((SourceViewer)textViewer).configure(getSourceViewerConfiguration(tools));
-		}
-	}
+		if (!(textViewer instanceof SourceViewer))
+			return;
 
-	private SourceViewerConfiguration getSourceViewerConfiguration(JavaTextTools textTools) {
-		IPreferenceStore store= JavaPlugin.getDefault().getCombinedPreferenceStore();
-		return new PropertiesFileSourceViewerConfiguration(textTools.getColorManager(), store, null, getDocumentPartitioning());
+		if (fPreferenceStore == null) {
+			fSourceViewerConfigurations= new ArrayList(3);
+			fPreferenceStore= JavaPlugin.getDefault().getCombinedPreferenceStore();
+			fPreferenceChangeListener= new IPropertyChangeListener() {
+				public void propertyChange(PropertyChangeEvent event) {
+					Iterator iter= fSourceViewerConfigurations.iterator();
+					while (iter.hasNext())
+						((PropertiesFileSourceViewerConfiguration)iter.next()).handlePropertyChangeEvent(event);
+					invalidateTextPresentation();
+				}
+			};
+			fPreferenceStore.addPropertyChangeListener(fPreferenceChangeListener);
+		}
+
+		SourceViewerConfiguration sourceViewerConfiguration= new PropertiesFileSourceViewerConfiguration(JavaPlugin.getDefault().getJavaTextTools().getColorManager(), fPreferenceStore, null,
+				getDocumentPartitioning());
+
+		fSourceViewerConfigurations.add(sourceViewerConfiguration);
+		((SourceViewer)textViewer).configure(sourceViewerConfiguration);
 	}
 
 	/*
@@ -84,5 +108,19 @@ public class PropertiesFileMergeViewer extends TextMergeViewer {
 	 */
 	public String getTitle() {
 		return CompareMessages.PropertiesFileMergeViewer_title;
+	}
+
+	/*
+	 * @see org.eclipse.compare.contentmergeviewer.TextMergeViewer#handleDispose(org.eclipse.swt.events.DisposeEvent)
+	 * @since 3.5
+	 */
+	protected void handleDispose(DisposeEvent event) {
+		if (fPreferenceStore != null) {
+			fPreferenceStore.removePropertyChangeListener(fPreferenceChangeListener);
+			fPreferenceStore= null;
+			fPreferenceChangeListener= null;
+			fSourceViewerConfigurations= null;
+		}
+		super.handleDispose(event);
 	}
 }
