2e2328b86218efb0350519d4446dbc82dd44f299
Dani Megert <dmegert>
2008-10-10 13:36:47 +0000

    Fixed Javadoc warning.

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/java/MemberProposalInfo.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/java/MemberProposalInfo.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2006 IBM Corporation and others.
+ * Copyright (c) 2005, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -46,9 +46,9 @@ public abstract class MemberProposalInfo extends ProposalInfo {
 
 	/**
 	 * Returns the java element that this computer corresponds to, possibly <code>null</code>.
-	 *
+	 * 
 	 * @return the java element that this computer corresponds to, possibly <code>null</code>
-	 * @throws JavaModelException
+	 * @throws JavaModelException if accessing the java model fails
 	 */
 	public IJavaElement getJavaElement() throws JavaModelException {
 		if (!fJavaElementResolved) {
