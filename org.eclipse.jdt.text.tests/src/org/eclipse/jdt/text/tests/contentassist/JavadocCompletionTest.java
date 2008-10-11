74fb551ff6da37d08d6820baa339a03f644270f2
Dani Megert <dmegert>
2008-10-11 09:57:06 +0000

    Removed no longer needed restriction.

--- org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/contentassist/JavadocCompletionTest.java
+++ org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/contentassist/JavadocCompletionTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -392,8 +392,6 @@ public class JavadocCompletionTest extends AbstractCompletionTest {
 		tearDown();
 		for (int i= 0; i < TYPE_BLOCK_TAGS.length; i++) {
 			String tag= TYPE_BLOCK_TAGS[i];
-			if ("@author".equals(tag)) // author template is messing up here
-				continue;
 			setUp();
 			assertNoMethodBodyProposals(" * {@|", tag);
 			tearDown();
