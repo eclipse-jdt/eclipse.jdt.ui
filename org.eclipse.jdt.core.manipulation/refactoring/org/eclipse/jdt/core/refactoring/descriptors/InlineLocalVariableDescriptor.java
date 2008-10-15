4c8bf20c77aac4e006549f7e46d0858347a6356f
Dani Megert <dmegert>
2008-10-15 11:47:32 +0000

    Fixed compile warnings.

--- org.eclipse.jdt.core.manipulation/refactoring/org/eclipse/jdt/core/refactoring/descriptors/InlineLocalVariableDescriptor.java
+++ org.eclipse.jdt.core.manipulation/refactoring/org/eclipse/jdt/core/refactoring/descriptors/InlineLocalVariableDescriptor.java
@@ -33,7 +33,6 @@ import org.eclipse.jdt.core.refactoring.IJavaRefactorings;
  * @since 1.1
  *
  * @noinstantiate This class is not intended to be instantiated by clients.
- * @noextend This class is not intended to be subclassed by clients.
  */
 public final class InlineLocalVariableDescriptor extends JavaRefactoringDescriptor {
 
