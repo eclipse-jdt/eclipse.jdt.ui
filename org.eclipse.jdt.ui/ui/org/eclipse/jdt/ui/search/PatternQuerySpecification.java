2bf10fbe8fd8f6dc4d719f2e187a3c488a1d2170
Dani Megert <dmegert>
2008-10-16 12:00:47 +0000

    Formatting.

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/ui/search/PatternQuerySpecification.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/ui/search/PatternQuerySpecification.java
@@ -20,11 +20,9 @@ import org.eclipse.jdt.core.search.IJavaSearchScope;
  * <p>
  * This class is not intended to be instantiated or subclassed by clients.
  * </p>
- *
+ * 
  * @see org.eclipse.jdt.ui.search.QuerySpecification
- *
  * @since 3.0
- *
  * @noinstantiate This class is not intended to be instantiated by clients.
  * @noextend This class is not intended to be subclassed by clients.
  */
