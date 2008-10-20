8884bdbc09e2a2b57c24fac26f801bd52e4a1aa0
Dani Megert <dmegert>
2008-10-20 08:56:49 +0000

    Fixed compile warnings.

--- org.eclipse.jdt.ui.tests.refactoring/test cases/org/eclipse/jdt/ui/tests/refactoring/AbstractSelectionTestCase.java
+++ org.eclipse.jdt.ui.tests.refactoring/test cases/org/eclipse/jdt/ui/tests/refactoring/AbstractSelectionTestCase.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -145,7 +145,7 @@ public abstract class AbstractSelectionTestCase extends AbstractCUTestCase {
 
 	protected RefactoringStatus checkPreconditions(Refactoring refactoring, IProgressMonitor pm) throws CoreException {
 		CheckConditionsOperation op= new CheckConditionsOperation(refactoring, getCheckingStyle());
-		op.run(new NullProgressMonitor());
+		op.run(pm);
 		return op.getStatus();
 	}
 
