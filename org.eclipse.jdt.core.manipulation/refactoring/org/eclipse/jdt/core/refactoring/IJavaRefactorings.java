475470629aa42715bd08662e41498dfaf14da09e
Dani Megert <dmegert>
2008-11-19 09:39:34 +0000

    removed duplicate @noextend.

--- org.eclipse.jdt.core.manipulation/refactoring/org/eclipse/jdt/core/refactoring/IJavaRefactorings.java
+++ org.eclipse.jdt.core.manipulation/refactoring/org/eclipse/jdt/core/refactoring/IJavaRefactorings.java
@@ -69,7 +69,6 @@ import org.eclipse.jdt.core.refactoring.descriptors.UseSupertypeDescriptor;
  * @since 1.1
  * @noimplement This interface is not intended to be implemented by clients.
  * @noextend This interface is not intended to be extended by clients.
- * @noextend This interface is not intended to be extended by clients.
  */
 public interface IJavaRefactorings {
 
