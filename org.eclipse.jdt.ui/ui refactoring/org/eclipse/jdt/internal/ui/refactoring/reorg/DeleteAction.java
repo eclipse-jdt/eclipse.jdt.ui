27d1191a3637ff15bc038b195b98359d3901423c
Dani Megert <dmegert>
2008-11-25 10:17:30 +0000

    Fixed deprecation warning.

--- org.eclipse.jdt.ui/ui refactoring/org/eclipse/jdt/internal/ui/refactoring/reorg/DeleteAction.java
+++ org.eclipse.jdt.ui/ui refactoring/org/eclipse/jdt/internal/ui/refactoring/reorg/DeleteAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2007 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -58,7 +58,7 @@ public class DeleteAction extends SelectionDispatchAction {
 	}
 
 	private IAction createWorkbenchAction(IStructuredSelection selection) {
-		DeleteResourceAction action= new DeleteResourceAction(getShell());
+		DeleteResourceAction action= new DeleteResourceAction(getSite());
 		action.selectionChanged(selection);
 		return action;
 	}
