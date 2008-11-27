b47423313c496158de97b53e5876926b16204b2b
Markus Keller <mkeller>
2008-11-27 11:16:12 +0000

    added hashCode()

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/wizards/buildpaths/CPListElementAttribute.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/wizards/buildpaths/CPListElementAttribute.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2007 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -118,6 +118,10 @@ public class CPListElementAttribute {
             return false;
         CPListElementAttribute attrib= (CPListElementAttribute)obj;
         return attrib.fKey== this.fKey && attrib.getParent().getPath().equals(fParent.getPath());
+    }
+    
+    public int hashCode() {
+    	return fKey.hashCode() * 89 + fParent.getPath().hashCode();
     }
 
     public CPListElementAttribute copy() {
