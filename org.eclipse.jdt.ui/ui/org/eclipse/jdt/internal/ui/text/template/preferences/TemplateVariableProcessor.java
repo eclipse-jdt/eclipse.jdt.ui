7a5f6d4b916ceb835330d43228732c4b678578a1
Markus Keller <mkeller>
2008-11-27 11:49:32 +0000

    removed useless equals method

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/template/preferences/TemplateVariableProcessor.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/template/preferences/TemplateVariableProcessor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2007 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -34,10 +34,6 @@ public class TemplateVariableProcessor implements IContentAssistProcessor {
 			TemplateVariableProposal proposal1= (TemplateVariableProposal) arg1;
 
 			return proposal0.getDisplayString().compareTo(proposal1.getDisplayString());
-		}
-
-		public boolean equals(Object arg0) {
-			return false;
 		}
 	};
 
