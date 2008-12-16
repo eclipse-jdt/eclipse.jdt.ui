baf32529ba1f4b69ac4a6cefab75d085fde8014c
Dani Megert <dmegert>
2008-12-16 09:39:07 +0000

    Fixed compile warning.

--- org.eclipse.jdt.ui.tests.refactoring/test cases/org/eclipse/jdt/ui/tests/refactoring/nls/NLSHintTest.java
+++ org.eclipse.jdt.ui.tests.refactoring/test cases/org/eclipse/jdt/ui/tests/refactoring/nls/NLSHintTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -55,12 +55,15 @@ public class NLSHintTest extends TestCase {
 		"	}\n" +
 		"}\n";
 
+	private static final boolean ALL_TESTS= true;
+
+
     public NLSHintTest(String arg) {
         super(arg);
     }
 
     public static Test allTests() {
-    	if (true) {
+		if (ALL_TESTS) {
     		return new ProjectTestSetup(new TestSuite(NLSHintTest.class));
     	} else {
 			TestSuite suite= new TestSuite();
