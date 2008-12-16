6670cb84478b44b9b9c62c398227c7ab55833b46
Dani Megert <dmegert>
2008-12-16 10:19:59 +0000

    Fixed bad code.

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/wizards/buildpaths/FolderSelectionDialog.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/wizards/buildpaths/FolderSelectionDialog.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -87,12 +87,7 @@ public class FolderSelectionDialog extends ElementTreeSelectionDialog implements
 	}
 
 	protected void newFolderButtonPressed() {
-		NewFolderDialog dialog= new NewFolderDialog(getShell(), fSelectedContainer) {
-			protected Control createContents(Composite parent) {
-				PlatformUI.getWorkbench().getHelpSystem().setHelp(parent, IJavaHelpContextIds.BP_CREATE_NEW_FOLDER);
-				return super.createContents(parent);
-			}
-		};
+		NewFolderDialog dialog= new NewFolderDialog(getShell(), fSelectedContainer);
 		if (dialog.open() == Window.OK) {
 			TreeViewer treeViewer= getTreeViewer();
 			treeViewer.refresh(fSelectedContainer);
