1af8163c4d16db8a193b811c5c1cc52767290d69
Dani Megert <dmegert>
2008-12-19 17:03:16 +0000

    Removed unused getter.

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/actions/CompositeActionGroup.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/actions/CompositeActionGroup.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -33,12 +33,6 @@ public class CompositeActionGroup extends ActionGroup {
 		Assert.isTrue(fGroups == null);
 		Assert.isNotNull(groups);
 		fGroups= groups;
-	}
-
-	public ActionGroup get(int index) {
-		if (fGroups == null)
-			return null;
-		return fGroups[index];
 	}
 
 	public void addGroup(ActionGroup group) {
