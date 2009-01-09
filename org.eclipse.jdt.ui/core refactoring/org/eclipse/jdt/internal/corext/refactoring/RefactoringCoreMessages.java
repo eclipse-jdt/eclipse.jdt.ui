2d26e2055d20a44a8a629d1a11889d34e6a57f6e
Dani Megert <dmegert>
2009-01-09 08:50:07 +0000

    Fixed chkpii errors.

--- org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/RefactoringCoreMessages.java
+++ org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/RefactoringCoreMessages.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -2247,8 +2247,6 @@ public final class RefactoringCoreMessages extends NLS {
 	public static String ReorgUtils_17;
 
 	public static String ReorgUtils_18;
-
-	public static String ReorgUtils_19;
 
 	public static String ReorgUtils_2;
 
