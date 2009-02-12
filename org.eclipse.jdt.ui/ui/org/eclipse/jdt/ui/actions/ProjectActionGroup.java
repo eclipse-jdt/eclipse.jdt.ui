e80ec3566e422ff1eb274dcbc96bcb583980fe66
Markus Keller <mkeller>
2009-02-12 13:43:13 +0000

    revert last change

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/ui/actions/ProjectActionGroup.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/ui/actions/ProjectActionGroup.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -128,8 +128,7 @@ public class ProjectActionGroup extends ActionGroup {
 		int selectionStatus= evaluateSelection(array, openProjects);
 		StructuredSelection sel= new StructuredSelection(openProjects);
 
-		fOpenAction.setEnabled((selectionStatus & CLOSED_PROJECTS_SELECTED) != 0
-				|| (selectionStatus == 0 && array.length == 0 && hasClosedProjectsInWorkspace()));
+		fOpenAction.setEnabled((selectionStatus & CLOSED_PROJECTS_SELECTED) != 0 || (selectionStatus == 0 && hasClosedProjectsInWorkspace()));
 		fCloseAction.selectionChanged(sel);
 		fCloseUnrelatedAction.selectionChanged(sel);
 	}
