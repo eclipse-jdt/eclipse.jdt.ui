db07243770be47c24bc796cf2a8b27e6033da32e
Dani Megert <dmegert>
2009-02-18 08:48:37 +0000

    Fixed bug 265197: [misc] static final String member with specific content truncated in hover tooltip

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/java/hover/JavadocHover.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/java/hover/JavadocHover.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -503,6 +503,7 @@ public class JavadocHover extends AbstractJavaEditorTextHover {
 		String constantValue;
 		if (elements.length == 1 && elements[0].getElementType() == IJavaElement.FIELD) {
 			constantValue= getConstantValue((IField) elements[0], hoverRegion);
+			constantValue= HTMLPrinter.convertToHTMLContent(constantValue);
 		} else {
 			constantValue= null;
 		}
