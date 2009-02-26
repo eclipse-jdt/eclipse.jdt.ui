3620b0d07ce75287888039cf925f8544c7356af2
Markus Keller <mkeller>
2009-02-26 18:33:10 +0000

    Bug 79053: Compare: Title not informative enough, when comparing methods from different compilation units

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/compare/JavaCompareAction.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/compare/JavaCompareAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -117,6 +117,7 @@ public class JavaCompareAction implements IActionDelegate {
 		fJavaElementLabelProvider= new JavaElementLabelProvider(
 					JavaElementLabelProvider.SHOW_PARAMETERS |
 					JavaElementLabelProvider.SHOW_OVERLAY_ICONS |
+					JavaElementLabelProvider.SHOW_POST_QUALIFIED |
 					JavaElementLabelProvider.SHOW_ROOT);
 
 		if (left == null || right == null) {
