f9af5389655c2cad3756432f89d2b2137c67878a
Dani Megert <dmegert>
2009-02-27 09:08:15 +0000

    Committed Raksha's patch to fix bug 44277: [navigation] Enable CTRL-mouse navigation for implementing classes

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/javaeditor/JavaElementHyperlinkDetector.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/javaeditor/JavaElementHyperlinkDetector.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -68,16 +68,46 @@ public class JavaElementHyperlinkDetector extends AbstractHyperlinkDetector {
 			elements= selectOpenableElements(elements);
 			if (elements.length == 0)
 				return null;
-
-			IHyperlink[] result= new IHyperlink[elements.length];
+			
+			IHyperlink[] links= new IHyperlink[elements.length];
+			int j= 0;
 			for (int i= 0; i < elements.length; i++) {
-				result[i]= new JavaElementHyperlink(wordRegion, (SelectionDispatchAction) openAction, elements[i], elements.length > 1);
+				IHyperlink link= createHyperlink(wordRegion, (SelectionDispatchAction)openAction, elements[i], elements.length > 1, textEditor);
+				if (link != null) {
+					links[j++]= link;
+				}
 			}
-			return result;
+			if (j == 0) {
+				return null;
+			} else if (j < elements.length) {
+				IHyperlink[] result= new IHyperlink[j];
+				System.arraycopy(links, 0, result, 0, j);
+				return result;
+			}
+			return links;
+
 		} catch (JavaModelException e) {
 			return null;
 		}
 	}
+
+	/**
+	 * Creates a java element hyperlink.
+	 * 
+	 * @param wordRegion the region of the link
+	 * @param openAction the action to use to open the java elements
+	 * @param element the java element to open
+	 * @param qualify <code>true</code> if the hyperlink text should show a qualified name for
+	 *            element
+	 * @param editor the active java editor
+	 * @return a Java element hyperlink or <code>null</code> if no hyperlink can be created for the
+	 *         given arguments
+	 * @since 3.5
+	 */
+	protected IHyperlink createHyperlink(IRegion wordRegion, SelectionDispatchAction openAction, IJavaElement element, boolean qualify, ITextEditor editor) {
+		return new JavaElementHyperlink(wordRegion, openAction, element, qualify);
+	}
+
 
 	/**
 	 * Selects the openable elements out of the given ones.
---
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/javaeditor/JavaElementImplementationHyperlink.java
@@ -0,0 +1,237 @@
+/*******************************************************************************
+ * Copyright (c) 2009 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.jdt.internal.ui.javaeditor;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+
+import org.eclipse.core.runtime.Assert;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.OperationCanceledException;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.SubProgressMonitor;
+
+import org.eclipse.jface.operation.IRunnableContext;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.viewers.StructuredSelection;
+
+import org.eclipse.jface.text.IRegion;
+import org.eclipse.jface.text.ITextOperationTarget;
+import org.eclipse.jface.text.hyperlink.IHyperlink;
+
+import org.eclipse.ui.texteditor.ITextEditor;
+
+import org.eclipse.jdt.core.Flags;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IMember;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.ITypeRoot;
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.Expression;
+import org.eclipse.jdt.core.dom.ITypeBinding;
+import org.eclipse.jdt.core.dom.MethodInvocation;
+import org.eclipse.jdt.core.dom.NodeFinder;
+import org.eclipse.jdt.core.dom.SimpleName;
+import org.eclipse.jdt.core.search.IJavaSearchConstants;
+import org.eclipse.jdt.core.search.SearchEngine;
+import org.eclipse.jdt.core.search.SearchMatch;
+import org.eclipse.jdt.core.search.SearchParticipant;
+import org.eclipse.jdt.core.search.SearchPattern;
+import org.eclipse.jdt.core.search.SearchRequestor;
+
+import org.eclipse.jdt.internal.corext.dom.Bindings;
+import org.eclipse.jdt.internal.corext.util.Messages;
+
+import org.eclipse.jdt.ui.JavaElementLabels;
+import org.eclipse.jdt.ui.SharedASTProvider;
+import org.eclipse.jdt.ui.actions.SelectionDispatchAction;
+
+import org.eclipse.jdt.internal.ui.JavaPlugin;
+
+
+/**
+ * Java element implementation hyperlink.
+ * 
+ * @since 3.5
+ */
+public class JavaElementImplementationHyperlink implements IHyperlink {
+	
+	private final IRegion fRegion;
+	private final SelectionDispatchAction fOpenAction;
+	private final IJavaElement fElement;
+	private final boolean fQualify;
+
+	/**
+	 * The current text editor.
+	 */
+	private ITextEditor fEditor;
+
+	/**
+	 * Creates a new Java element implementation hyperlink for methods.
+	 * 
+	 * @param region the region of the link
+	 * @param openAction the action to use to open the java elements
+	 * @param element the java element to open
+	 * @param qualify <code>true</code> if the hyperlink text should show a qualified name for
+	 *            element.
+	 * @param editor the active java editor
+	 */
+	public JavaElementImplementationHyperlink(IRegion region, SelectionDispatchAction openAction, IJavaElement element, boolean qualify, ITextEditor editor) {
+		Assert.isNotNull(openAction);
+		Assert.isNotNull(region);
+		Assert.isNotNull(element);
+
+		fRegion= region;
+		fOpenAction= openAction;
+		fElement= element;
+		fQualify= qualify;
+		fEditor= editor;
+	}
+
+	/*
+	 * @see org.eclipse.jdt.internal.ui.javaeditor.IHyperlink#getHyperlinkRegion()
+	 */
+	public IRegion getHyperlinkRegion() {
+		return fRegion;
+	}
+
+	/*
+	 * @see org.eclipse.jdt.internal.ui.javaeditor.IHyperlink#getHyperlinkText()
+	 */
+	public String getHyperlinkText() {
+		if (fQualify) {
+			String elementLabel= JavaElementLabels.getElementLabel(fElement, JavaElementLabels.ALL_FULLY_QUALIFIED);
+			return Messages.format(JavaEditorMessages.JavaElementImplementationHyperlink_hyperlinkText_qualified, new Object[] { elementLabel });
+		} else {
+			return JavaEditorMessages.JavaElementImplementationHyperlink_hyperlinkText;
+		}
+	}
+
+	/*
+	 * @see org.eclipse.jdt.internal.ui.javaeditor.IHyperlink#getTypeLabel()
+	 */
+	public String getTypeLabel() {
+		return null;
+	}
+
+	/**
+	 * Opens the given implementation hyperlink for methods.
+	 * <p>
+	 * If there's only one implementor that hyperlink is opened in the editor, otherwise the
+	 * Quick Hierarchy is opened.
+	 * </p>
+	 */
+	public void open() {
+		ITypeRoot editorInput= EditorUtility.getEditorInputJavaElement(fEditor, false);
+
+		CompilationUnit ast= SharedASTProvider.getAST(editorInput, SharedASTProvider.WAIT_ACTIVE_ONLY, null);
+		if (ast == null) {
+			openQuickHierarchy();
+			return;
+		}
+
+		ASTNode node= NodeFinder.perform(ast, fRegion.getOffset(), fRegion.getLength());
+		IJavaElement elem= null;
+		if (node instanceof SimpleName) {
+			ASTNode parent= node.getParent();
+			if (parent instanceof MethodInvocation) {
+				Expression expression= ((MethodInvocation)parent).getExpression();
+				if (expression == null) {
+					ITypeBinding parentTypeBinding= Bindings.getBindingOfParentType(node);
+					elem= parentTypeBinding.getJavaElement();
+				} else {
+					ITypeBinding binding= expression.resolveTypeBinding();
+					elem= binding.getJavaElement();
+				}
+			}
+		}
+		final IType type= (IType)elem;
+		if (type == null) {
+			openQuickHierarchy();
+			return;
+		}
+
+		final String dummyString= new String();
+		final ArrayList links= new ArrayList();
+		IRunnableWithProgress runnable= new IRunnableWithProgress() {
+
+			public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+				if (monitor == null) {
+					monitor= new NullProgressMonitor();
+				}
+				try {
+					String methodLabel= JavaElementLabels.getElementLabel(fElement, JavaElementLabels.DEFAULT_QUALIFIED);
+					monitor.beginTask(Messages.format(JavaEditorMessages.JavaElementImplementationHyperlink_search_method_implementors, methodLabel), 100);
+					SearchRequestor requestor= new SearchRequestor() {
+						public void acceptSearchMatch(SearchMatch match) throws CoreException {
+							if (match.getAccuracy() == SearchMatch.A_ACCURATE) {
+								IJavaElement element= (IJavaElement)match.getElement();
+								if (element instanceof IMember) {
+									IMember declaredClass= (IMember)element.getParent();
+									int flags= declaredClass.getFlags();
+									int memberFlags= ((IMember)element).getFlags();
+									if (!Flags.isInterface(flags) && !((Flags.isAbstract(flags)) && Flags.isAbstract(memberFlags))) {
+										links.add(element);
+										if (links.size() > 1) {
+											throw new OperationCanceledException(dummyString);
+										}
+									}
+								}
+							}
+						}
+					};
+					SearchPattern pattern= SearchPattern.createPattern(fElement, IJavaSearchConstants.DECLARATIONS);
+					SearchParticipant[] participants= new SearchParticipant[] { SearchEngine.getDefaultSearchParticipant() };
+					SearchEngine engine= new SearchEngine();
+					engine.search(pattern, participants, SearchEngine.createHierarchyScope(type), requestor, new SubProgressMonitor(monitor, 100));
+
+					if (monitor.isCanceled()) {
+						throw new OperationCanceledException();
+					}
+				} catch (CoreException e) {
+					throw new InvocationTargetException(e);
+				} finally {
+					monitor.done();
+				}
+			}
+		};
+
+		try {
+			IRunnableContext context= fEditor.getSite().getWorkbenchWindow();
+			context.run(true, true, runnable);
+		} catch (InvocationTargetException e) {
+			JavaPlugin.log(new Status(IStatus.ERROR, JavaPlugin.getPluginId(), IStatus.OK, "", e)); //$NON-NLS-1$
+			return;
+		} catch (InterruptedException e) {
+			if (e.getMessage() != dummyString) {
+				return;
+			}
+		}
+
+		if (links.size() == 1) {
+			fOpenAction.run(new StructuredSelection(links.get(0)));
+		} else {
+			openQuickHierarchy();
+		}
+	}
+
+	/**
+	 * Opens a quick type hierarchy for the editor's current input.
+	 */
+	private void openQuickHierarchy() {
+		ITextOperationTarget textOperationTarget= (ITextOperationTarget)fEditor.getAdapter(ITextOperationTarget.class);
+		textOperationTarget.doOperation(JavaSourceViewer.SHOW_HIERARCHY);
+	}
+}
