e3ce787c3096b6cf5654bb82673f86dcba4f89af
Dani Megert <dmegert>
2009-03-10 16:38:10 +0000

    Fixed Javadoc warning.

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/java/SWTTemplateCompletionProposalComputer.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/java/SWTTemplateCompletionProposalComputer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2008 IBM Corporation and others.
+ * Copyright (c) 2007, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -179,8 +179,8 @@ public class SWTTemplateCompletionProposalComputer extends AbstractTemplateCompl
 
 	/**
 	 * Tells whether SWT is on the given project's class path.
-	 *
-	 * @param javaProject
+	 * 
+	 * @param javaProject the Java project
 	 * @return <code>true</code> if the given project's class path
 	 */
 	private synchronized boolean isSWTOnClasspath(IJavaProject javaProject) {
