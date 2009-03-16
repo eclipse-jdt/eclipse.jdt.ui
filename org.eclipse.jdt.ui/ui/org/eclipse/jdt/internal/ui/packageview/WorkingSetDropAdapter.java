6eaa029b6543930a4acbb9bb720744cbee4c3473
Dani Megert <dmegert>
2009-03-16 14:45:32 +0000

    Committed Raksha's patch to fix bug 267358: [package explorer] [working sets] [dnd] When sorted, should not show drop target for WS

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/packageview/WorkingSetDropAdapter.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/packageview/WorkingSetDropAdapter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -133,7 +133,8 @@ public class WorkingSetDropAdapter extends JdtViewerDropAdapter implements Trans
 
 		if (isWorkingSetSelection()) {
 			setExpandEnabled(false);
-			if (getCurrentLocation() == LOCATION_BEFORE || getCurrentLocation() == LOCATION_AFTER) {
+			if ((getCurrentLocation() == LOCATION_BEFORE || getCurrentLocation() == LOCATION_AFTER) &&
+					!fPackageExplorer.getWorkingSetModel().isSortingEnabled()) {
 				setFeedbackEnabled(true);
 				return DND.DROP_MOVE;
 			}
