2a877dd3cf4da4a8949af64fb1d871ebbafc3d2b
Markus Keller <mkeller>
2009-03-30 16:32:36 +0000

    Bug 125296: [JUnit] Eclipse shows only one failure or error from the JUnit test result

--- org.eclipse.jdt.junit/src/org/eclipse/jdt/internal/junit/model/TestElement.java
+++ org.eclipse.jdt.junit/src/org/eclipse/jdt/internal/junit/model/TestElement.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -261,9 +261,6 @@ public abstract class TestElement implements ITestElement {
 	}
 
 	public void setStatus(Status status) {
-		//TODO: notify about change?
-		//TODO: multiple errors/failures per test https://bugs.eclipse.org/bugs/show_bug.cgi?id=125296
-
 		if (status == Status.RUNNING) {
 			fTime= - System.currentTimeMillis() / 1000d ;
 		} else if (status.convertToProgressState() == ProgressState.COMPLETED) {
@@ -280,11 +277,14 @@ public abstract class TestElement implements ITestElement {
 	}
 
 	public void setStatus(Status status, String trace, String expected, String actual) {
-		//TODO: notify about change?
-		//TODO: multiple errors/failures per test https://bugs.eclipse.org/bugs/show_bug.cgi?id=125296
-		fTrace= trace;
-		fExpected= expected;
-		fActual= actual;
+		if (trace != null && fTrace != null) {
+			//don't overwrite first trace if same test run logs multiple errors
+			fTrace= fTrace + trace;
+		} else {
+			fTrace= trace;
+			fExpected= expected;
+			fActual= actual;
+		}
 		setStatus(status);
 	}
 
