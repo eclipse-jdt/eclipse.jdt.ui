15d806dc4f47a493208531a404c268186466c354
Dani Megert <dmegert>
2009-04-29 12:43:08 +0000

    Fixed bug 271436: Reminder: mark activate editor tests gray if Platform UI does not fix the regression

--- org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/ActivateTextEditorTest.java
+++ org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/ActivateTextEditorTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -34,5 +34,13 @@ public class ActivateTextEditorTest extends ActivateEditorTest {
 	public void testActivateEditor() {
 		setShortName(SHORT_NAME);
 		super.testActivateEditor();
+	}
+
+	/*
+	 * @see org.eclipse.jdt.text.tests.performance.ActivateEditorTest#getDegradationComment()
+	 * @since 3.5
+	 */
+	protected String getDegradationComment() {
+		return "This test is slower than in 3.4 due the changes in Platform UI, see bug 258253 for details.";
 	}
 }
