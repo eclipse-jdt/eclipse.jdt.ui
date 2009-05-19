ca1c1e576ba9935d8bcdb95d0226d2e3e85ea001
Dani Megert <dmegert>
2009-05-19 14:08:28 +0000

    Fixed bug 276708: [content assist] code assist in file of simple project logs exception

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/ui/text/java/JavaContentAssistInvocationContext.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/ui/text/java/JavaContentAssistInvocationContext.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2008 IBM Corporation and others.
+ * Copyright (c) 2005, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -284,7 +284,8 @@ public class JavaContentAssistInvocationContext extends ContentAssistInvocationC
 			if (fLabelProvider == null)
 				fLabelProvider= collector.getLabelProvider();
 		} catch (JavaModelException x) {
-			JavaPlugin.log(x);
+			if (!x.isDoesNotExist() || cu.getJavaProject() == null || cu.getJavaProject().isOnClasspath(cu))
+				JavaPlugin.log(x);
 			if (fKeywordProposals == null)
 				fKeywordProposals= new IJavaCompletionProposal[0];
 		}
