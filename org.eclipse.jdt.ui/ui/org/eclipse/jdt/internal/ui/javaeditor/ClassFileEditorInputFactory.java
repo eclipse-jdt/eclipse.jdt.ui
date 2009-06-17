c13a128c192dbc6cfd908e74459fb8c652021670
Dani Megert <dmegert>
2009-06-17 10:05:12 +0000

    Removed link to fixed bug.

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/javaeditor/ClassFileEditorInputFactory.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/javaeditor/ClassFileEditorInputFactory.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -51,7 +51,7 @@ public class ClassFileEditorInputFactory implements IElementFactory {
 				 * see https://bugs.eclipse.org/bugs/show_bug.cgi?id=83221
 				 */
 				IClassFile cf= (IClassFile)element;
-				IType type= cf.getType(); // this will work, see https://bugs.eclipse.org/bugs/show_bug.cgi?id=154667
+				IType type= cf.getType();
 				IJavaProject project= element.getJavaProject();
 				if (project != null) {
 					type= project.findType(type.getFullyQualifiedName());
