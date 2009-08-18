f384f51527f0a96d9333534e793f9a43389c5b29
Markus Keller <mkeller>
2009-08-18 09:30:36 +0000

    Bug 286845: Added new IProblem id UnresolvedVariable

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/correction/QuickFixProcessor.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/correction/QuickFixProcessor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -51,6 +51,7 @@ public class QuickFixProcessor implements IQuickFixProcessor {
 			case IProblem.MethodButWithConstructorName:
 			case IProblem.UndefinedField:
 			case IProblem.UndefinedName:
+			case IProblem.UnresolvedVariable:
 			case IProblem.PublicClassMustMatchFileName:
 			case IProblem.PackageIsNotExpectedPackage:
 			case IProblem.UndefinedType:
@@ -291,6 +292,7 @@ public class QuickFixProcessor implements IQuickFixProcessor {
 				break;
 			case IProblem.UndefinedField:
 			case IProblem.UndefinedName:
+			case IProblem.UnresolvedVariable:
 				UnresolvedElementsSubProcessor.getVariableProposals(context, problem, null, proposals);
 				break;
 			case IProblem.AmbiguousType:
