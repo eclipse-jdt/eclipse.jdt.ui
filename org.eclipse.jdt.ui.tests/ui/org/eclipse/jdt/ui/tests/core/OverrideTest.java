8f49de8cf9c467f1ab2800d84f12e9c659d471ca
Markus Keller <mkeller>
2009-09-14 10:01:04 +0000

    Bug 289247: [1.5][compiler]Detecting duplicate methods should not consider return type

--- org.eclipse.jdt.ui.tests/ui/org/eclipse/jdt/ui/tests/core/OverrideTest.java
+++ org.eclipse.jdt.ui.tests/ui/org/eclipse/jdt/ui/tests/core/OverrideTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -211,7 +211,9 @@ public class OverrideTest extends TestCase {
 
 		CompilationUnit astRoot= createAST(cu);
 		IProblem[] problems= astRoot.getProblems();
-		assertTrue(problems.length == 0);
+		assertTrue(problems.length == 1);
+		assertTrue(problems[0].isError());
+		assertTrue(problems[0].getID() == IProblem.MethodNameClash);
 
 		TypeDeclaration bb= (TypeDeclaration) astRoot.types().get(1);
 		IMethodBinding bbtest= bb.getMethods()[0].resolveBinding();
