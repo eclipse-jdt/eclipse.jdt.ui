669e4a2207007c1af369085311faa19d1a5b19f0
Dani Megert <dmegert>
2009-10-21 14:22:35 +0000

    Fixed compile warning.

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/JavaElementAdapterFactory.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/JavaElementAdapterFactory.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -43,11 +43,12 @@ import org.eclipse.jdt.internal.ui.javaeditor.IClassFileEditorInput;
 import org.eclipse.jdt.internal.ui.search.JavaSearchPageScoreComputer;
 import org.eclipse.jdt.internal.ui.search.SearchUtil;
 
+
 /**
  * Implements basic UI support for Java elements.
  * Implements handle to persistent support for Java elements.
  */
-public class JavaElementAdapterFactory implements IAdapterFactory, IContributorResourceAdapter, IContributorResourceAdapter2 {
+public class JavaElementAdapterFactory implements IAdapterFactory, IContributorResourceAdapter2 {
 
 	private static Class[] PROPERTIES= new Class[] {
 		IPropertySource.class,
