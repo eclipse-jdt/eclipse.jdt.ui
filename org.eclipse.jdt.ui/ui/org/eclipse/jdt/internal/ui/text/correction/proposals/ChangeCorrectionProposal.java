9375e9ffaaff547234222d5aeb1d5d1d5150272d
Markus Keller <mkeller>
2010-01-05 15:23:36 +0000

    Bug 290805: [quick fix] BadLocationException (race condition with preview calculation)

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/correction/proposals/ChangeCorrectionProposal.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/correction/proposals/ChangeCorrectionProposal.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -259,7 +259,7 @@ public class ChangeCorrectionProposal implements IJavaCompletionProposal, IComma
 	 * @return returns the change for this proposal.
 	 * @throws CoreException thrown when the change could not be created
 	 */
-	public final Change getChange() throws CoreException {
+	public synchronized final Change getChange() throws CoreException {
 		if (fChange == null) {
 			fChange= createChange();
 		}
