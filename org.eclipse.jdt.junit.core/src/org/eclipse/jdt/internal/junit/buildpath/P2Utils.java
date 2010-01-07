87cf68d8604fed6c00b0e7f63b39b57adb7b703e
Dani Megert <dmegert>
2010-01-07 17:47:59 +0000

    Committed Patrick's patch to fix bug 297663: [JUnit] JUnit not found when JDT installed as dropin

--- org.eclipse.jdt.junit.core/src/org/eclipse/jdt/internal/junit/buildpath/P2Utils.java
+++ org.eclipse.jdt.junit.core/src/org/eclipse/jdt/internal/junit/buildpath/P2Utils.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 IBM Corporation and others.
+ * Copyright (c) 2009, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Patrick Higgins <patrick133t@yahoo.com> - https://bugs.eclipse.org/bugs/show_bug.cgi?id=297663
  *******************************************************************************/
 package org.eclipse.jdt.internal.junit.buildpath;
 
@@ -16,6 +17,9 @@ import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
 import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import org.osgi.framework.Version;
 
@@ -45,81 +49,12 @@ import org.eclipse.jdt.internal.junit.JUnitCorePlugin;
  */
 class P2Utils {
 
-	private static final String CONFIG_FOLDER = "configuration"; //$NON-NLS-1$
-	
 	private static final String SRC_INFO_FOLDER = "org.eclipse.equinox.source"; //$NON-NLS-1$
 	private static final String SRC_INFO_PATH= SRC_INFO_FOLDER + File.separator + "source.info"; //$NON-NLS-1$
 
 	private static final String BUNDLE_INFO_FOLDER= "org.eclipse.equinox.simpleconfigurator"; //$NON-NLS-1$
 	private static final String BUNDLE_INFO_PATH = BUNDLE_INFO_FOLDER + File.separator + "bundles.info"; //$NON-NLS-1$
 
-
-	/**
-	 * Returns bundles defined by the 'bundles.info'.
-	 * 
-	 * @param isSourceBundle <code>true</code> if source bundles should be read <code>false</code>
-	 *            otherwise
-	 * @param useConfigArea <code>true</code> if the bundles are read from the config area, or
-	 *            <code>false</code> if the install location should be used
-	 * @return all bundles in the installation or <code>null</code> if not able to locate a
-	 *         bundles.info
-	 */
-	private static BundleInfo[] readBundles(boolean isSourceBundle, boolean useConfigArea) {
-		final Location bundlesLocation;
-		final String bundleInfoPath;
-		if (useConfigArea) {
-			bundlesLocation= Platform.getConfigurationLocation();
-			if (isSourceBundle)
-				bundleInfoPath= SRC_INFO_PATH;
-			else
-				bundleInfoPath= BUNDLE_INFO_PATH;
-		} else {
-			bundlesLocation= Platform.getInstallLocation();
-			if (isSourceBundle)
-				bundleInfoPath= CONFIG_FOLDER + File.separator + SRC_INFO_PATH;
-			else
-				bundleInfoPath= CONFIG_FOLDER + File.separator + BUNDLE_INFO_PATH;
-		}
-		if (bundlesLocation == null)
-			return null;
-
-		URL bundlesLocationURL= bundlesLocation.getURL();
-		if (bundleInfoPath == null)
-			return null;
-
-		try {
-			URL bundlesTxt= new URL(bundlesLocationURL.getProtocol(), bundlesLocationURL.getHost(), new File(bundlesLocationURL.getFile(), bundleInfoPath).getAbsolutePath());
-			BundleInfo bundles[]= getBundlesFromFile(bundlesLocationURL, bundlesTxt);
-			if (bundles == null || bundles.length == 0) {
-				return null;
-			}
-			return bundles;
-		} catch (MalformedURLException e) {
-			JUnitCorePlugin.log(e);
-			return null;
-		} catch (IOException e) {
-			JUnitCorePlugin.log(e);
-			return null;
-		}
-	}
-
-	/**
-	 * Returns an array of {@link BundleInfo} for each bundle entry or <code>null</code> if there is a
-	 * problem reading the file.
-	 * 
-	 * @param bundlesLocation the URL of the bundle location
-	 * @param fileURL the URL of the info file
-	 * @return array of bundle infos or <code>null</code>
-	 * @throws IOException if loading the configuration fails
-	 */
-	private static BundleInfo[] getBundlesFromFile(URL bundlesLocation, URL fileURL) throws IOException {
-		SimpleConfiguratorManipulator manipulator= (SimpleConfiguratorManipulator)JUnitCorePlugin.getDefault().getService(SimpleConfiguratorManipulator.class.getName());
-		if (manipulator == null)
-			return null;
-
-		File home= new File(bundlesLocation.getFile());
-		return manipulator.loadConfiguration(fileURL, home);
-	}
 
 	/**
 	 * Finds the bundle info for the given arguments.
@@ -140,6 +75,31 @@ class P2Utils {
 	}
 
 	/**
+	 * Adds the directories (as <code>java.io.File</code>) for the given location and all of its parents to the given list.
+	 * 
+	 * @param locations the list to add the URLs to
+	 * @param location the location
+	 * @param useParent <code>true</code> if location's parent directory should be used <code>false</code> otherwise
+	 */
+	private static void addLocationDirs(List locations, Location location, boolean useParent) {
+		while (location != null) {
+			URL url= location.getURL();
+			if (url != null) {
+				try {
+					File dir = new File(FileLocator.toFileURL(url).getPath());
+					if (useParent)
+						dir = dir.getParentFile();
+					if (!locations.contains(dir))
+						locations.add(dir);
+				} catch (IOException e) {
+					JUnitCorePlugin.log(e);
+				}
+			}
+			location= location.getParentLocation();
+		}
+	}
+
+	/**
 	 * Finds the bundle info for the given arguments.
 	 * <p>
 	 * The first match will be returned if more than one bundle matches the arguments.
@@ -154,51 +114,50 @@ class P2Utils {
 		Assert.isLegal(symbolicName != null);
 		Assert.isLegal(versionRange != null);
 
-		// First try to find the bundle in the config area
-		BundleInfo bundleInfo= findBundle(symbolicName, versionRange, isSourceBundle, true);
-		if (bundleInfo != null)
-			return bundleInfo;
-
-		// Use install location if not found in config area
-		return findBundle(symbolicName, versionRange, isSourceBundle, false);
-	}
-
-	/**
-	 * Finds the bundle info for the given arguments.
-	 * 
-	 * @param symbolicName the symbolic name
-	 * @param versionRange the version range for the bundle version
-	 * @param isSourceBundle <code>true</code> if source bundles should be read <code>false</code>
-	 *            otherwise
-	 * @param useConfigArea <code>true</code> if the bundles are read from the config area, or
-	 *            <code>false</code> if the install location should be used
-	 * @return the bundle info or <code>null</code> if not found
-	 * @since 3.5
-	 */
-	private static BundleInfo findBundle(String symbolicName, VersionRange versionRange, boolean isSourceBundle, boolean useConfigArea) {
-		BundleInfo[] bundles= readBundles(isSourceBundle, useConfigArea);
-		if (bundles == null)
+		SimpleConfiguratorManipulator manipulator= (SimpleConfiguratorManipulator)JUnitCorePlugin.getDefault().getService(SimpleConfiguratorManipulator.class.getName());
+		if (manipulator == null)
 			return null;
-		
-		BundleInfo bestMatch= null;
-		Version bestVersion= null;
-		for (int i= 0; i < bundles.length; i++) {
-			BundleInfo bundleInfo= bundles[i];
-			if (symbolicName.equals(bundleInfo.getSymbolicName())) {
-				Version version= new Version(bundleInfo.getVersion());
-				if (versionRange.isIncluded(version)) {
-					IPath path= getBundleLocationPath(bundleInfo);
-					if (path.toFile().exists()) {
-						if (bestMatch == null || bestVersion.compareTo(version) < 0) {
-							bestMatch= bundleInfo;
-							bestVersion= version;
+
+		List bundleLocations = new ArrayList();
+		addLocationDirs(bundleLocations, Platform.getConfigurationLocation(), true);
+		addLocationDirs(bundleLocations, Platform.getInstallLocation(), false);
+
+		for (Location configLocation= Platform.getConfigurationLocation(); configLocation != null; configLocation= configLocation.getParentLocation()) {
+			URL configUrl= configLocation.getURL();
+			if (configUrl == null)
+				continue;
+
+			try {
+				String bundleInfoPath= null;
+				if (isSourceBundle)
+					bundleInfoPath= SRC_INFO_PATH;
+				else
+					bundleInfoPath= BUNDLE_INFO_PATH;
+
+				URL bundlesTxt = new URL(configUrl.getProtocol(), configUrl.getHost(), new File(configUrl.getPath(), bundleInfoPath).getAbsolutePath());
+
+				for (Iterator i= bundleLocations.iterator(); i.hasNext(); ) {
+					File home= (File) i.next();
+					BundleInfo bundles[]= manipulator.loadConfiguration(bundlesTxt, home);
+					if (bundles != null) {
+						for (int j= 0; j < bundles.length; j++) {
+							BundleInfo bundle= bundles[j];
+							if (symbolicName.equals(bundle.getSymbolicName()) && versionRange.isIncluded(new Version(bundle.getVersion()))) {
+								IPath path= getBundleLocationPath(bundle);
+								if (path.toFile().exists())
+									return bundle;
+							}
 						}
 					}
 				}
+			} catch (MalformedURLException e) {
+				JUnitCorePlugin.log(e);
+			} catch (IOException e) {
+				JUnitCorePlugin.log(e);
 			}
 		}
 		
-		return bestMatch;
+		return null;
 	}
 
 	/**
@@ -216,7 +175,7 @@ class P2Utils {
 			return null;
 		
 		try {
-			String fileStr= FileLocator.toFileURL(URIUtil.toURL(bundleLocation)).getFile();
+			String fileStr= FileLocator.toFileURL(URIUtil.toURL(bundleLocation)).getPath();
 			fileStr= URLDecoder.decode(fileStr, "UTF-8"); //$NON-NLS-1$
 			return new Path(fileStr);
 		} catch (IOException e) {
