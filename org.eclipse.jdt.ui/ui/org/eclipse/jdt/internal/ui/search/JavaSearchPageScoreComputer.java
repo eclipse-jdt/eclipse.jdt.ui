7c9bbfbaa183292816a03ba7701899f612f56b06
Dani Megert <dmegert>
2010-01-11 12:55:50 +0000

    Fixed bug 299151: Search opens on wrong page when started from Java editor

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/search/JavaSearchPageScoreComputer.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/search/JavaSearchPageScoreComputer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,9 +10,13 @@
  *******************************************************************************/
 package org.eclipse.jdt.internal.ui.search;
 
+import org.eclipse.ui.IEditorInput;
+
 import org.eclipse.search.ui.ISearchPageScoreComputer;
 
 import org.eclipse.jdt.core.IJavaElement;
+
+import org.eclipse.jdt.ui.JavaUI;
 
 import org.eclipse.jdt.internal.ui.browsing.LogicalPackage;
 import org.eclipse.jdt.internal.ui.javaeditor.IClassFileEditorInput;
@@ -24,7 +28,8 @@ public class JavaSearchPageScoreComputer implements ISearchPageScoreComputer {
 			// Can't decide
 			return ISearchPageScoreComputer.UNKNOWN;
 
-		if (element instanceof IJavaElement || element instanceof IClassFileEditorInput || element instanceof LogicalPackage)
+		if (element instanceof IJavaElement || element instanceof IClassFileEditorInput || element instanceof LogicalPackage
+				|| (element instanceof IEditorInput && JavaUI.getEditorInputJavaElement((IEditorInput)element) != null))
 			return 90;
 
 		return ISearchPageScoreComputer.LOWEST;
