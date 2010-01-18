e324fd889693977203365c2c23adcd1a713699f6
Markus Keller <mkeller>
2010-01-18 14:18:40 +0000

    Bug 299931: [hovering] NPEs when trying to configure workspace preferences after 'Configure Problem Severity'

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/preferences/JavadocProblemsPreferencePage.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/preferences/JavadocProblemsPreferencePage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -152,9 +152,11 @@ public class JavadocProblemsPreferencePage extends PropertyAndPreferencePage {
 		super.applyData(data);
 		if (data instanceof Map && fConfigurationBlock != null) {
 			Map map= (Map) data;
-			Boolean useProjectSpecific= (Boolean) map.get(DATA_USE_PROJECT_SPECIFIC_OPTIONS);
-			if (useProjectSpecific != null) {
-				enableProjectSpecificSettings(useProjectSpecific.booleanValue());
+			if (isProjectPreferencePage()) {
+				Boolean useProjectSpecific= (Boolean) map.get(DATA_USE_PROJECT_SPECIFIC_OPTIONS);
+				if (useProjectSpecific != null) {
+					enableProjectSpecificSettings(useProjectSpecific.booleanValue());
+				}
 			}
 
 			Object key= map.get(DATA_SELECT_OPTION_KEY);
--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/preferences/ProblemSeveritiesPreferencePage.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/preferences/ProblemSeveritiesPreferencePage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -147,9 +147,11 @@ public class ProblemSeveritiesPreferencePage extends PropertyAndPreferencePage {
 		super.applyData(data);
 		if (data instanceof Map && fConfigurationBlock != null) {
 			Map map= (Map) data;
-			Boolean useProjectOptions= (Boolean) map.get(USE_PROJECT_SPECIFIC_OPTIONS);
-			if (useProjectOptions != null) {
-				enableProjectSpecificSettings(useProjectOptions.booleanValue());
+			if (isProjectPreferencePage()) {
+				Boolean useProjectOptions= (Boolean) map.get(USE_PROJECT_SPECIFIC_OPTIONS);
+				if (useProjectOptions != null) {
+					enableProjectSpecificSettings(useProjectOptions.booleanValue());
+				}
 			}
 
 			Object key= map.get(DATA_SELECT_OPTION_KEY);
