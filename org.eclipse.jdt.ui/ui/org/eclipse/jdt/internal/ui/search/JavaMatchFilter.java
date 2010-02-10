5773ae60f2ffddec8c5dd01b8bd3150cfd69b034
Markus Keller <mkeller>
2010-02-10 11:55:37 +0000

    Bug 194175: [search] Search result filters not persisted any more

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/search/JavaMatchFilter.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/search/JavaMatchFilter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -63,7 +63,7 @@ abstract class JavaMatchFilter extends MatchFilter {
 
 	public static MatchFilter[] getLastUsedFilters() {
 		String string= JavaPlugin.getDefault().getDialogSettings().get(SETTINGS_LAST_USED_FILTERS);
-		if (string != null && string.length() > 0) {
+		if (string != null) {
 			return decodeFiltersString(string);
 		}
 		return getDefaultFilters();
