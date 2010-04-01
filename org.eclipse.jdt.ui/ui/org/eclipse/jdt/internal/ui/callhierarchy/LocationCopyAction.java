95e87329c54de8125128349b26f0e48689d86d4f
Raksha Vasisht <rvasisht>
2010-04-01 04:07:19 +0000

    Fixed bug 303762: [call hierarchy] NPE thrown when trying to copy a non-existent selection in 'Call Hierarchy'

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/callhierarchy/LocationCopyAction.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/callhierarchy/LocationCopyAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2009 IBM Corporation and others.
+ * Copyright (c) 2006, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -45,6 +45,7 @@ class LocationCopyAction extends Action {
 
 		setText(CallHierarchyMessages.LocationCopyAction_copy);
 		setActionDefinitionId(IWorkbenchCommandConstants.EDIT_COPY);
+		setEnabled(!fLocationViewer.getSelection().isEmpty());
 
 		locationViewer.addSelectionChangedListener(new ISelectionChangedListener() {
 			public void selectionChanged(SelectionChangedEvent event) {
