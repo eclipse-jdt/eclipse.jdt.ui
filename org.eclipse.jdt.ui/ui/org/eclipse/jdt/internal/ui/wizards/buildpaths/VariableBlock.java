c646bb61acb92f3a069ac94b2c5c41236ece1e48
Markus Keller <mkeller>
2010-04-22 12:13:32 +0000

    Bug 309977: Classpath variable initialization doesn't fail silently

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/wizards/buildpaths/VariableBlock.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/wizards/buildpaths/VariableBlock.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -412,8 +412,6 @@ public class VariableBlock {
 				if (name.equals(initSelection)) {
 					initSelectedElement= elem;
 				}
-			} else {
-				JavaPlugin.logErrorMessage("VariableBlock: Classpath variable with null value: " + name); //$NON-NLS-1$
 			}
 		}
 
