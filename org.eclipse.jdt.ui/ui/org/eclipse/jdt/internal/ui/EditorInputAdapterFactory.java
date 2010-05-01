6d1c973e0c5680ce4b172fd277aaf914f0c5f2ff
Dani Megert <dmegert>
2010-05-01 07:42:38 +0000

    Fixed  bug 310687: [search] JavaSearchPageScoreComputer returned to adapt C++ Editor Input

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/EditorInputAdapterFactory.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/EditorInputAdapterFactory.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -41,7 +41,7 @@ public class EditorInputAdapterFactory implements IAdapterFactory {
 
 	public Object getAdapter(Object element, Class key) {
 		updateLazyLoadedAdapters();
-		if (fSearchPageScoreComputer != null && ISearchPageScoreComputer.class.equals(key))
+		if (fSearchPageScoreComputer != null && ISearchPageScoreComputer.class.equals(key) && element instanceof IEditorInput && JavaUI.getEditorInputJavaElement((IEditorInput)element) != null)
 			return fSearchPageScoreComputer;
 
 		if (IJavaElement.class.equals(key) && element instanceof IEditorInput) {
