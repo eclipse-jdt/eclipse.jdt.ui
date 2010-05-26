b525ed9cfda510d37acdd30a862f4ceb7b3cf54f
Dani Megert <dmegert>
2010-05-26 11:17:51 +0000

    Added missing loaded bundle.

--- org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/PluginsNotLoadedTest.java
+++ org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/PluginsNotLoadedTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -73,7 +73,6 @@ public class PluginsNotLoadedTest extends TestCase {
 			"org.eclipse.core.resources.compatibility",
 			"org.eclipse.core.resources.win32",
 			"org.eclipse.core.runtime.compatibility.registry",
-			"org.eclipse.core.variables",
 			"org.eclipse.debug.ui",
 			"org.eclipse.equinox.http.jetty",
 			"org.eclipse.equinox.http.registry",
