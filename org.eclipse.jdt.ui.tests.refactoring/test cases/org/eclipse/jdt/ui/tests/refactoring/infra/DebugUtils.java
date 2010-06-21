1734ac1c692d2bd01090058472078ed770c602f2
Dani Megert <dmegert>
2010-06-21 07:43:57 +0000

    Removed unused parameter.

--- org.eclipse.jdt.ui.tests.refactoring/test cases/org/eclipse/jdt/ui/tests/refactoring/infra/DebugUtils.java
+++ org.eclipse.jdt.ui.tests.refactoring/test cases/org/eclipse/jdt/ui/tests/refactoring/infra/DebugUtils.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -46,7 +46,7 @@ public final class DebugUtils {
 			System.out.println(refs[i].toString());
 	}
 
-	public static void dumpCollectionCollection(String msg, Collection c){
+	public static void dumpCollectionCollection(Collection c) {
 		for (Iterator iter= c.iterator(); iter.hasNext(); ){
 			dumpCollection("", (List)iter.next()); //$NON-NLS-1$
 		}
