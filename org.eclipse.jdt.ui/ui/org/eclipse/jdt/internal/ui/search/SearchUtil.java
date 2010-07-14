995dea0a4965473ddacd5a8f9b76a1f9f7ec5e76
Markus Keller <mkeller>
2010-07-14 13:38:50 +0000

    Bug 319854: [search] wrong condition for warning about references to inlined constant

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/search/SearchUtil.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/search/SearchUtil.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -213,7 +213,7 @@ public class SearchUtil {
 	}
 
 	public static void warnIfBinaryConstant(IJavaElement element, Shell shell) {
-		if (isBinaryPrimitiveConstantOrString(element))
+		if (isPrimitiveConstantOrString(element))
 			OptionalMessageDialog.open(
 				BIN_PRIM_CONST_WARN_DIALOG_ID,
 				shell,
@@ -225,7 +225,7 @@ public class SearchUtil {
 				0);
 	}
 
-	private static boolean isBinaryPrimitiveConstantOrString(IJavaElement element) {
+	private static boolean isPrimitiveConstantOrString(IJavaElement element) {
 		if (element != null && element.getElementType() == IJavaElement.FIELD) {
 			IField field= (IField)element;
 			int flags;
@@ -234,7 +234,7 @@ public class SearchUtil {
 			} catch (JavaModelException ex) {
 				return false;
 			}
-			return field.isBinary() && Flags.isStatic(flags) && Flags.isFinal(flags) && isPrimitiveOrString(field);
+			return Flags.isStatic(flags) && Flags.isFinal(flags) && isPrimitiveOrString(field);
 		}
 		return false;
 	}
@@ -248,6 +248,7 @@ public class SearchUtil {
 		}
 		char first= fieldType.charAt(0);
 		return (first != Signature.C_RESOLVED && first != Signature.C_UNRESOLVED && first != Signature.C_ARRAY)
-			|| (first == Signature.C_RESOLVED && fieldType.substring(1, fieldType.length() - 1).equals(String.class.getName()));
+			|| ((first == Signature.C_RESOLVED || first == Signature.C_UNRESOLVED) && fieldType.substring(1, fieldType.length() - 1).equals(String.class.getName())
+			|| (first == Signature.C_UNRESOLVED && fieldType.substring(1, fieldType.length() - 1).equals("String"))); //$NON-NLS-1$
 	}
 }
