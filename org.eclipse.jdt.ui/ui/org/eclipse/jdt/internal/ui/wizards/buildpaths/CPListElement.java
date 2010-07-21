14fd31e3f13be69351a10a97650184baa7b5c644
Raksha Vasisht <rvasisht>
2010-07-21 06:24:17 +0000

    Fixed bug 312054: [preferences] UserLibraryPreferencePage: After renaming a user library the Up, Down, Remove buttons do not work until dialog is reopened

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/wizards/buildpaths/CPListElement.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/wizards/buildpaths/CPListElement.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -391,6 +391,16 @@ public class CPListElement {
 
 	public Object getParentContainer() {
 		return fParentContainer;
+	}
+
+	/**
+	 * Sets the parent container.
+	 * 
+	 * @param parent the parent container
+	 * @since 3.7
+	 */
+	void setParentContainer(CPUserLibraryElement parent) {
+		fParentContainer= parent;
 	}
 
 	/**
--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/wizards/buildpaths/CPUserLibraryElement.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/wizards/buildpaths/CPUserLibraryElement.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -84,11 +84,21 @@ public class CPUserLibraryElement {
 		}
 	}
 
+	/**
+	 * Creates a new user library element with the given name and children and sets itself as the
+	 * parent container to each given child element.
+	 * 
+	 * @param name the name of the library element
+	 * @param isSystemLibrary <code>true</code> if the library is a system library,
+	 *            <code>false</code> otherwise
+	 * @param children the children elements of the library element or <code>null</code>
+	 */
 	public CPUserLibraryElement(String name, boolean isSystemLibrary, CPListElement[] children) {
 		fName= name;
 		fChildren= new ArrayList();
 		if (children != null) {
 			for (int i= 0; i < children.length; i++) {
+				children[i].setParentContainer(this);
 				fChildren.add(children[i]);
 			}
 		}
