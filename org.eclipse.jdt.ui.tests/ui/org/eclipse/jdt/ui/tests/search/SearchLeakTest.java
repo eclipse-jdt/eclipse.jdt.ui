f62e07562f48dde5e6d2fa3d57af9951f6b6fb35
Dani Megert <dmegert>
2010-08-02 07:16:52 +0000

    Fixed bug 321385: SearchLeakTestWrapper#testRemoveSearchQueries() fails on build machine

--- org.eclipse.jdt.ui.tests/ui/org/eclipse/jdt/ui/tests/search/SearchLeakTest.java
+++ org.eclipse.jdt.ui.tests/ui/org/eclipse/jdt/ui/tests/search/SearchLeakTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -45,6 +45,7 @@ public class SearchLeakTest extends LeakTestCase {
 	}
 
 	public void testRemoveSearchQueries() throws Exception {
+		InternalSearchUI.getInstance().removeAllQueries();
 		JavaSearchQuery query1= SearchTestHelper.runMethodRefQuery("junit.framework.Test", "countTestCases", new String[0]);
 		JavaSearchQuery query2= SearchTestHelper.runMethodRefQuery("junit.framework.TestCase", "countTestCases", new String[0]);
 		InternalSearchUI.getInstance().removeQuery(query1);
