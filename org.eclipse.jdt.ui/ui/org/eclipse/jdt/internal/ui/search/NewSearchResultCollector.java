7d2e5c7b1904b77ee02b42cbafefd2a022b46255
Dani Megert <dmegert>
2010-08-25 12:58:11 +0000

    Declaration is a write access.

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/search/NewSearchResultCollector.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/search/NewSearchResultCollector.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -15,7 +15,9 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.search.ui.text.AbstractTextSearchResult;
 
 import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.search.FieldDeclarationMatch;
 import org.eclipse.jdt.core.search.FieldReferenceMatch;
+import org.eclipse.jdt.core.search.LocalVariableDeclarationMatch;
 import org.eclipse.jdt.core.search.LocalVariableReferenceMatch;
 import org.eclipse.jdt.core.search.MethodReferenceMatch;
 import org.eclipse.jdt.core.search.SearchMatch;
@@ -43,10 +45,14 @@ public class NewSearchResultCollector extends SearchRequestor {
 				FieldReferenceMatch fieldRef= ((FieldReferenceMatch) match);
 				isWriteAccess= fieldRef.isWriteAccess();
 				isReadAccess= fieldRef.isReadAccess();
+			} else if (match instanceof FieldDeclarationMatch) {
+				isWriteAccess= true;
 			} else if (match instanceof LocalVariableReferenceMatch) {
 				LocalVariableReferenceMatch localVarRef= ((LocalVariableReferenceMatch) match);
 				isWriteAccess= localVarRef.isWriteAccess();
 				isReadAccess= localVarRef.isReadAccess();
+			} else if (match instanceof LocalVariableDeclarationMatch) {
+				isWriteAccess= true;
 			}
 			boolean isSuperInvocation= false;
 			if (match instanceof MethodReferenceMatch) {
