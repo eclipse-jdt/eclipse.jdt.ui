53a3d5aed34303ed56fea69c1f010b2c87aca7a3
Dani Megert <dmegert>
2010-09-22 11:09:24 +0000

    Use "see " instead of "see: "

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/browsing/PackagesViewTableViewer.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/browsing/PackagesViewTableViewer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -83,7 +83,7 @@ class PackagesViewTableViewer extends ProblemTableViewer implements IPackagesVie
 		return filter(logicalPackage.getFragments()).length > 0;
 	}
 
-	// --------- see: IPackagesViewViewer ----------
+	// --------- see IPackagesViewViewer ----------
 
 	public Widget doFindItem(Object element){
 		return super.doFindItem(element);
--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/browsing/PackagesViewTreeViewer.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/browsing/PackagesViewTreeViewer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -94,7 +94,7 @@ public class PackagesViewTreeViewer extends ProblemTreeViewer implements IPackag
 		return false;
 	}
 
-	// --------- see: IPackagesViewViewer ----------
+	// --------- see IPackagesViewViewer ----------
 
 	public Widget doFindItem(Object element) {
 		return super.doFindItem(element);
