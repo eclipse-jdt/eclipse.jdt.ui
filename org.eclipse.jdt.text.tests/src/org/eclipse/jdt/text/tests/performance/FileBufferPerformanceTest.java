5c394ee4943ab60a83323917f3c7f5e23963633e
Dani Megert <dmegert>
2010-10-07 16:10:44 +0000

    Fixed bug 327137: Some tests are setup differently when run as a suite and when run individually

--- org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/FileBufferPerformanceTest.java
+++ org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/performance/FileBufferPerformanceTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2008 IBM Corporation and others.
+ * Copyright (c) 2007, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -53,11 +53,6 @@ public class FileBufferPerformanceTest extends TextPerformanceTestCase2 {
 	public static Test suite() {
 		return new PerfTestSuite(FileBufferPerformanceTest.class);
 	}
-
-	public static Test setUpTest(Test test) {
-		return new PerformanceTestSetup(test);
-	}
-
 
 	protected void setUp() throws Exception {
 		fProject= ResourceHelper.createProject("project");
