0f8a2a2075caadac1a1e4ddc297facd74758f028
Dani Megert <dmegert>
2010-10-13 06:43:46 +0000

    Fixed Javadoc warning.

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/wizards/buildpaths/ClasspathContainerSelectionPage.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/wizards/buildpaths/ClasspathContainerSelectionPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -56,7 +56,8 @@ public class ClasspathContainerSelectionPage extends WizardPage {
 
 	/**
 	 * Constructor for ClasspathContainerWizardPage.
-	 * @param containerPages
+	 * 
+	 * @param containerPages the array of container pages
 	 */
 	protected ClasspathContainerSelectionPage(ClasspathContainerDescriptor[] containerPages) {
 		super("ClasspathContainerWizardPage"); //$NON-NLS-1$
