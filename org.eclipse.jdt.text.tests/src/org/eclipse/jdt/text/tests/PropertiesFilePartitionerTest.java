447233bd71560fb5f4ca19890b000164185f8cf2
Markus Keller <mkeller>
2010-11-18 20:23:51 +0000

    Bug 329614 comment 6: [linked mode] Incorrect selection and caret position after rename in file

--- org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/PropertiesFilePartitionerTest.java
+++ org.eclipse.jdt.text.tests/src/org/eclipse/jdt/text/tests/PropertiesFilePartitionerTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -50,6 +50,7 @@ public class PropertiesFilePartitionerTest extends TestCase {
 		fDocument= new Document();
 		PropertiesFileDocumentSetupParticipant.setupDocument(fDocument);
 		fDocument.set("###Comment\nkey=value\nkey value\nkey:value");
+		//             01234567890 1234567890 1234567890 123456789
 
 		fDocumentPartitioningChanged= false;
 		fDocument.addDocumentPartitioningListener(new IDocumentPartitioningListener() {
