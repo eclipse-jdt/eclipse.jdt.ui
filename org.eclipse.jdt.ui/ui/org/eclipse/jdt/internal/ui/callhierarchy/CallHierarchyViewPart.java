ef7e18a75b169ed2de465ad02992452248cb8214
Raksha Vasisht <rvasisht>
2011-01-11 08:46:45 +0000

    Fixed bug 332053: [call hierarchy] Clearing history should clear the status bar

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/callhierarchy/CallHierarchyViewPart.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/callhierarchy/CallHierarchyViewPart.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2010 IBM Corporation and others.
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -614,6 +614,7 @@ public class CallHierarchyViewPart extends ViewPart implements ICallHierarchyVie
 		if (isEmpty) {
 			setContentDescription(""); //$NON-NLS-1$
 			setTitleToolTip(getPartName());
+			getViewSite().getActionBars().getStatusLineManager().setMessage(""); //$NON-NLS-1$			
 		}
 		fPagebook.showPage(control);
 		if (fRefreshViewAction != null)
