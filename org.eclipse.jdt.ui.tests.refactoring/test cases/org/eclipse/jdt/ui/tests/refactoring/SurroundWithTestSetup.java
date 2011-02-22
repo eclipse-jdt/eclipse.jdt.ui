cf810ebc79da695bc7f6079aaace6445ef0a130f
Dani Megert <dmegert>
2011-02-22 07:12:14 +0000

    Dummy search is already done by delete.

--- org.eclipse.jdt.ui.tests.refactoring/test cases/org/eclipse/jdt/ui/tests/refactoring/SurroundWithTestSetup.java
+++ org.eclipse.jdt.ui.tests.refactoring/test cases/org/eclipse/jdt/ui/tests/refactoring/SurroundWithTestSetup.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -73,7 +73,6 @@ public class SurroundWithTestSetup extends TestSetup {
 
 	protected void tearDown() throws Exception {
 		super.tearDown();
-		RefactoringTest.performDummySearch(fJavaProject);
 		JavaProjectHelper.delete(fJavaProject);
 	}
 
