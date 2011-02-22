94f42049bd0cdec7bb4b1e271e22071c795c4683
Markus Keller <mkeller>
2011-02-22 20:22:06 +0000

    fixed typo in ArrayType#doCanAssignTo(CaptureType) and updated TypeEnvironmentsTest

--- org.eclipse.jdt.ui.tests.refactoring/test cases/org/eclipse/jdt/ui/tests/refactoring/typeconstraints/TypeEnvironmentTests.java
+++ org.eclipse.jdt.ui.tests.refactoring/test cases/org/eclipse/jdt/ui/tests/refactoring/typeconstraints/TypeEnvironmentTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -265,16 +265,6 @@ public class TypeEnvironmentTests extends AbstractCUTestCase {
 	private void checkCanAssignTo(ITypeBinding rhsBinding, ITypeBinding lhsBinding, TType rhs, TType lhs) {
 		boolean coreResult= rhsBinding.isAssignmentCompatible(lhsBinding);
 		boolean uiResult= rhs.canAssignTo(lhs);
-		if (coreResult != uiResult) {
-			if (lhs.isCaptureType() || rhs.isCaptureType()) { // see bug 93082
-				System.out.println("Different assignment rule(" +
-					PrettySignatures.get(lhsBinding) + "= " + PrettySignatures.get(rhsBinding) +
-					"): Bindings<" + coreResult +
-					"> TType<" + uiResult + ">");
-				return;
-			}
-		}
-
 		assertEquals("Different assignment rule(" +
 			PrettySignatures.get(lhsBinding) + "= " + PrettySignatures.get(rhsBinding) +
 			"): ", coreResult, uiResult);
--- org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/typeconstraints/types/ArrayType.java
+++ org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/typeconstraints/types/ArrayType.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -120,7 +120,7 @@ public final class ArrayType extends TType {
 
 			case TYPE_VARIABLE: return false;
 			case CAPTURE_TYPE:
-				return ((CaptureType)lhs).checkLowerBound(lhs);
+				return ((CaptureType)lhs).checkLowerBound(this);
 		}
 		return false;
 	}
