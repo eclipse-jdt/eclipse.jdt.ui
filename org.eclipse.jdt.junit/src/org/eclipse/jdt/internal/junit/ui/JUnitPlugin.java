5233c6a40678cc96d21413446b8755f675dcae53
Markus Keller <mkeller>
2011-02-28 11:12:41 +0000

    removed pre-1.5 type argument comments

--- org.eclipse.jdt.junit/src/org/eclipse/jdt/internal/junit/ui/JUnitPlugin.java
+++ org.eclipse.jdt.junit/src/org/eclipse/jdt/internal/junit/ui/JUnitPlugin.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2010 IBM Corporation and others.
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -240,7 +240,7 @@ public class JUnitPlugin extends AbstractUIPlugin {
 	/**
 	 * @return a list of all JUnit launch configuration types
 	 */
-	public List/*<String>*/<String> getJUnitLaunchConfigTypeIDs() {
+	public List<String> getJUnitLaunchConfigTypeIDs() {
 		if (fJUnitLaunchConfigTypeIDs == null) {
 			loadLaunchConfigTypeIDs();
 		}
