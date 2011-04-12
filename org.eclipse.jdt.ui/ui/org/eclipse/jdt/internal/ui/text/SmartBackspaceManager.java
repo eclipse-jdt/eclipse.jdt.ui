c9a36f98b90f9c28bf8199fc128ae2653400b7a3
Dani Megert <dmegert>
2011-04-12 12:11:38 +0000

    Fixed several Javadoc warnings.

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/SmartBackspaceManager.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/SmartBackspaceManager.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -32,7 +32,6 @@ import org.eclipse.jface.text.ITextViewerExtension;
 import org.eclipse.jface.text.TextViewer;
 
 import org.eclipse.jdt.internal.ui.text.TypingRun.ChangeType;
-
 
 
 /**
@@ -224,8 +223,8 @@ public class SmartBackspaceManager {
 
 	/**
 	 * Installs the receiver on a text viewer.
-	 *
-	 * @param viewer
+	 * 
+	 * @param viewer the text viewer
 	 */
 	public void install(ITextViewer viewer) {
 		Assert.isLegal(viewer != null);
