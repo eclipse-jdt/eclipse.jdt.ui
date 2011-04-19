4252ef90a5a42970ced8d72990d039562e83014b
Markus Keller <mkeller>
2011-04-19 13:04:05 +0000

    fixed bad equals(Object)

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/javadocexport/JavadocLinkRef.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/javadocexport/JavadocLinkRef.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2010 IBM Corporation and others.
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -78,7 +78,9 @@ public class JavadocLinkRef {
 				return false;
 			}
 			if (!isProjectRef()) {
-				return !fClasspathEntry.equals(other.fClasspathEntry);
+				return fClasspathEntry.equals(other.fClasspathEntry);
+			} else {
+				return true;
 			}
 		}
 		return false;
