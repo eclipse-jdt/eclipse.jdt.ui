73d0fed960fdc56870325d9a35b7e9f05fea1a70
Markus Keller <mkeller>
2011-04-21 14:18:49 +0000

    show if resource doesn't exist

--- org.eclipse.jdt.jeview/src/org/eclipse/jdt/jeview/views/JEResource.java
+++ org.eclipse.jdt.jeview/src/org/eclipse/jdt/jeview/views/JEResource.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2007 IBM Corporation and others.
+ * Copyright (c) 2005, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -132,6 +132,8 @@ public class JEResource extends JEAttribute {
 		String label= fResource.getName();
 		if (fName != null)
 			label= fName +  ": " + label;
+		if (! fResource.exists())
+			label= label + " (does not exist)";
 		return label;
 	}
 
