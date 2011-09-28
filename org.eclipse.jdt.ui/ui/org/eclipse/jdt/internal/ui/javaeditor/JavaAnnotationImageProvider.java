8a98dd1ef1bed6e32f6423260d4cc31d8ee9a196
Dani Megert <dmegert>
2011-09-28 17:53:09 +0200

    Fixed bug 312159: [rulers] Color blindness issue with red and gray Xs for markers in vertical ruler

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/javaeditor/JavaAnnotationImageProvider.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/javaeditor/JavaAnnotationImageProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -156,7 +156,7 @@ public class JavaAnnotationImageProvider implements IAnnotationImageProvider {
 					String key= Integer.toString(image.hashCode());
 					Image grayImage= registry.get(key);
 					if (grayImage == null) {
-						grayImage= new Image(display, image, SWT.IMAGE_GRAY);
+						grayImage= new Image(display, image, SWT.IMAGE_DISABLE);
 						registry.put(key, grayImage);
 					}
 					image= grayImage;
