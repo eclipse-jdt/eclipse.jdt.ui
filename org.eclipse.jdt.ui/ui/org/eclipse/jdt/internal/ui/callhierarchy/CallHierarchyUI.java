6bbf682e6522f612bf600a0d65b63cc8658b3bbe
Markus Keller <mkeller>
2012-03-01 20:24:08 +0100

    Bug 372973: [call hierarchy] Selecting element in callee hierarchy reveals wrong editor location

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/callhierarchy/CallHierarchyUI.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/callhierarchy/CallHierarchyUI.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2011 IBM Corporation and others.
+ * Copyright (c) 2000, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -201,7 +201,7 @@ public class CallHierarchyUI {
         if (elem instanceof MethodWrapper) {
             javaElement= ((MethodWrapper) elem).getMember();
         } else if (elem instanceof CallLocation) {
-            javaElement= ((CallLocation) elem).getCalledMember();
+            javaElement= ((CallLocation) elem).getMember();
         }
         if (javaElement != null) {
             return EditorUtility.isOpenInEditor(javaElement);
