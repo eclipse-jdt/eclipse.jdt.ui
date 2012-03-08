7e3d7066d9916b05de7e57fdd20a355cd1a0b82e
Markus Keller <mkeller>
2012-03-08 11:44:42 +0100

    Bug 322556: [hovering] Quick Fix hover cuts message

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/java/hover/AbstractAnnotationHover.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/java/hover/AbstractAnnotationHover.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2011 IBM Corporation and others.
+ * Copyright (c) 2000, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -251,7 +251,8 @@ public abstract class AbstractAnnotationHover extends AbstractJavaEditorTextHove
 			if (constrains == null)
 				return preferedSize;
 
-			Point constrainedSize= getShell().computeSize(constrains.x, SWT.DEFAULT, true);
+			int trimWidth= getShell().computeTrim(0, 0, 0, 0).width;
+			Point constrainedSize= getShell().computeSize(constrains.x - trimWidth, SWT.DEFAULT, true);
 
 			int width= Math.min(preferedSize.x, constrainedSize.x);
 			int height= Math.max(preferedSize.y, constrainedSize.y);
