7399e1f49a5263e5c4e2e98683f9afa6948c6fa9
Dani Megert <dmegert>
2012-10-03 13:12:47 +0200

    Fixed warning.

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/javaeditor/JavaSelectMarkerRulerAction2.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/javaeditor/JavaSelectMarkerRulerAction2.java
@@ -93,8 +93,8 @@ public class JavaSelectMarkerRulerAction2 extends SelectAnnotationRulerAction {
 
 	/**
 	 * Checks whether the given annotation is a breakpoint annotation.
-	 *
-	 * @param annotation
+	 * 
+	 * @param annotation the annotation
 	 * @return <code>true</code> if the annotation is a breakpoint annotation
 	 */
 	private boolean isBreakpoint(Annotation annotation) {
