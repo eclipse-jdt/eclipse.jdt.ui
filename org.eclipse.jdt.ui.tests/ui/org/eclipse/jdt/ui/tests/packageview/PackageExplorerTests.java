3cc7374aad25b1fafc38139f43e8b84e4fa1b4f2
Dani Megert <dmegert>
2013-01-21 13:13:07 +0100

    Fixed bug 357450: Class folder in Java project have refresh problem

--- org.eclipse.jdt.ui.tests/ui/org/eclipse/jdt/ui/tests/packageview/PackageExplorerTests.java
+++ org.eclipse.jdt.ui.tests/ui/org/eclipse/jdt/ui/tests/packageview/PackageExplorerTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2011 IBM Corporation and others.
+ * Copyright (c) 2000, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -24,6 +24,7 @@ public class PackageExplorerTests {
 		suite.addTest(ContentProviderTests3.suite());
 		suite.addTest(ContentProviderTests4.suite());
 		suite.addTest(ContentProviderTests5.suite());
+		suite.addTest(ContentProviderTests6.suite());
 		suite.addTest(PackageExplorerShowInTests.suite());
 		suite.addTestSuite(WorkingSetDropAdapterTest.class);
 		suite.addTest(HierarchicalContentProviderTests.suite());
