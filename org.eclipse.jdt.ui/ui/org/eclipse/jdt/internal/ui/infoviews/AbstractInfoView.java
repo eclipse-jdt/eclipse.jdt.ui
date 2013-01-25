4849b9f8486e20eaf22834caa5a77f1e1dccbf72
Markus Keller <markus_keller@ch.ibm.com>
2013-01-25 17:19:28 +0100

    don't deal with inexistent elements

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/infoviews/AbstractInfoView.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/infoviews/AbstractInfoView.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2012 IBM Corporation and others.
+ * Copyright (c) 2000, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -566,7 +566,10 @@ public abstract class AbstractInfoView extends ViewPart implements ISelectionLis
 		if (element instanceof IAdaptable)
 			je= (IJavaElement)((IAdaptable)element).getAdapter(IJavaElement.class);
 
-		return je;
+		if (je != null && je.exists())
+			return je;
+		
+		return null;
 	}
 
 	/**
