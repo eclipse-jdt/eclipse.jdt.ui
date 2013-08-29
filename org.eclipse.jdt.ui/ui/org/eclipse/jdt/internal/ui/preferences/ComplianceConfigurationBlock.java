8d44c89b323bc2537b01957eaddf25d53426a154
Manju Mathew <manju_mathew@in.ibm.com>
2013-08-29 09:03:55 +0530

    Fixed bug 416016: [1.8][preferences] Add support for target 1.8 in the compiler preference page

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/preferences/ComplianceConfigurationBlock.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/preferences/ComplianceConfigurationBlock.java
@@ -331,16 +331,17 @@ public class ComplianceConfigurationBlock extends OptionsConfigurationBlock {
 		
 		int indent= LayoutUtil.getIndent();
 
-		String[] versions= new String[] { VERSION_CLDC_1_1, VERSION_1_1, VERSION_1_2, VERSION_1_3, VERSION_1_4, VERSION_1_5, VERSION_1_6, VERSION_1_7 };
+		String[] versions= new String[] { VERSION_CLDC_1_1, VERSION_1_1, VERSION_1_2, VERSION_1_3, VERSION_1_4, VERSION_1_5, VERSION_1_6, VERSION_1_7, VERSION_1_8 };
 		String[] versionsLabels= new String[] {
-			PreferencesMessages.ComplianceConfigurationBlock_versionCLDC11,
-			PreferencesMessages.ComplianceConfigurationBlock_version11,
-			PreferencesMessages.ComplianceConfigurationBlock_version12,
-			PreferencesMessages.ComplianceConfigurationBlock_version13,
-			PreferencesMessages.ComplianceConfigurationBlock_version14,
-			PreferencesMessages.ComplianceConfigurationBlock_version15,
-			PreferencesMessages.ComplianceConfigurationBlock_version16,
-			PreferencesMessages.ComplianceConfigurationBlock_version17
+				PreferencesMessages.ComplianceConfigurationBlock_versionCLDC11,
+				PreferencesMessages.ComplianceConfigurationBlock_version11,
+				PreferencesMessages.ComplianceConfigurationBlock_version12,
+				PreferencesMessages.ComplianceConfigurationBlock_version13,
+				PreferencesMessages.ComplianceConfigurationBlock_version14,
+				PreferencesMessages.ComplianceConfigurationBlock_version15,
+				PreferencesMessages.ComplianceConfigurationBlock_version16,
+				PreferencesMessages.ComplianceConfigurationBlock_version17,
+				PreferencesMessages.ComplianceConfigurationBlock_version18
 		};
 
 		boolean showJsr14= ComplianceConfigurationBlock.VERSION_JSR14.equals(getValue(PREF_CODEGEN_TARGET_PLATFORM));
