a9f59f5a249a7f22519f90b482e2619a1a2459d1
Markus Keller <markus_keller@ch.ibm.com>
2014-01-03 18:03:09 +0100

    Bug 424852: [create on paste] Respect line delimiter preference when creating new CU

--- org.eclipse.jdt.ui/core extension/org/eclipse/jdt/internal/corext/codemanipulation/StubUtility.java
+++ org.eclipse.jdt.ui/core extension/org/eclipse/jdt/internal/corext/codemanipulation/StubUtility.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2013 IBM Corporation and others.
+ * Copyright (c) 2000, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -820,10 +820,8 @@ public class StubUtility {
 	}
 
 	/**
-	 * Examines a string and returns the first line delimiter found.
-	 * 
-	 * @param elem the element
-	 * @return the line delimiter used for the element
+	 * @param elem a Java element (doesn't have to exist)
+	 * @return the existing or default line delimiter for the element
 	 */
 	public static String getLineDelimiterUsed(IJavaElement elem) {
 		IOpenable openable= elem.getOpenable();
