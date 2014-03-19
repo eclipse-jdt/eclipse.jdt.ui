f048c67c0f135852854f69dab7fc4fa6435978a6
Markus Keller <markus_keller@ch.ibm.com>
2014-03-19 18:31:02 +0100

    Bug 430574: [change method signature] SWTException: Widget is disposed with Change method signature

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/dialogs/TableTextCellEditor.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/dialogs/TableTextCellEditor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2011 IBM Corporation and others.
+ * Copyright (c) 2005, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -250,6 +250,8 @@ public class TableTextCellEditor extends CellEditor {
 			public void focusLost(FocusEvent e) {
             	e.display.asyncExec(new Runnable() {
 					public void run() {
+						if (text.isDisposed())
+							return;
 						// without the asyncExec, focus has not had a chance to go to the content assist proposals
 						TableTextCellEditor.this.focusLost();
 					}
