8aeda7ae41f4794a64998b383745d98a6e4a00ea
Noopur Gupta <noopur_gupta@in.ibm.com>
2014-05-13 16:14:25 +0200

    Bug 424331: [quick assist] Quick assists available at location with error

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/correction/JavaCorrectionAssistant.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/correction/JavaCorrectionAssistant.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2012 IBM Corporation and others.
+ * Copyright (c) 2000, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -238,7 +238,7 @@ public class JavaCorrectionAssistant extends QuickAssistAssistant {
 				}
 			}
 			if (bestOffset == Integer.MAX_VALUE) {
-				return invocationLocation;
+				bestOffset= invocationLocation;
 			}
 			for (int i= 0; i < allPositions.size(); i++) {
 				Position pos= allPositions.get(i);
