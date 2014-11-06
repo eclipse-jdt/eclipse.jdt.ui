509b0c54f865aa33e5ffae3999c5eb94fb1aee3a
Markus Keller <markus_keller@ch.ibm.com>
2014-11-06 17:10:37 +0100

    added some more resource properties

--- org.eclipse.jdt.jeview/src/org/eclipse/jdt/jeview/properties/ResourceProperties.java
+++ org.eclipse.jdt.jeview/src/org/eclipse/jdt/jeview/properties/ResourceProperties.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005 IBM Corporation and others.
+ * Copyright (c) 2005, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -30,6 +30,14 @@ public class ResourceProperties implements IPropertySource {
 	private static final String P_RAW_LOCATION= "org.eclipse.jdt.jeview.IResource.rawLocation";
 	private static final String P_LOCAL_TIME_STAMP= "org.eclipse.jdt.jeview.IResource.localTimeStamp";
 	private static final String P_MODIFICATION_STAMP= "org.eclipse.jdt.jeview.IResource.modificationStamp";
+	private static final String P_EXISTS= "org.eclipse.jdt.jeview.IResource.exists";
+	private static final String P_IS_ACCESSIBLE= "org.eclipse.jdt.jeview.IResource.isAccessible";
+	private static final String P_IS_DERIVED= "org.eclipse.jdt.jeview.IResource.isDerived";
+	private static final String P_IS_HIDDEN= "org.eclipse.jdt.jeview.IResource.isHidden";
+	private static final String P_IS_LINKED= "org.eclipse.jdt.jeview.IResource.isLinked";
+	private static final String P_IS_PHANTOM= "org.eclipse.jdt.jeview.IResource.isPhantom";
+	private static final String P_IS_TEAM_PRIVATE_MEMBER= "org.eclipse.jdt.jeview.IResource.isTeamPrivateMember";
+	private static final String P_IS_VIRTUAL= "org.eclipse.jdt.jeview.IResource.isVirtual";
 	
 	protected IResource fResource;
 	
@@ -42,6 +50,14 @@ public class ResourceProperties implements IPropertySource {
 		addResourceDescriptor(new PropertyDescriptor(P_RAW_LOCATION, "rawLocation"));
 		addResourceDescriptor(new PropertyDescriptor(P_LOCAL_TIME_STAMP, "localTimeStamp"));
 		addResourceDescriptor(new PropertyDescriptor(P_MODIFICATION_STAMP, "modificationStamp"));
+		addResourceDescriptor(new PropertyDescriptor(P_EXISTS, "exists"));
+		addResourceDescriptor(new PropertyDescriptor(P_IS_ACCESSIBLE, "isAccessible"));
+		addResourceDescriptor(new PropertyDescriptor(P_IS_DERIVED, "isDerived"));
+		addResourceDescriptor(new PropertyDescriptor(P_IS_HIDDEN, "isHidden"));
+		addResourceDescriptor(new PropertyDescriptor(P_IS_LINKED, "isLinked"));
+		addResourceDescriptor(new PropertyDescriptor(P_IS_PHANTOM, "isPhantom"));
+		addResourceDescriptor(new PropertyDescriptor(P_IS_TEAM_PRIVATE_MEMBER, "isTeamPrivateMember"));
+		addResourceDescriptor(new PropertyDescriptor(P_IS_VIRTUAL, "isVirtual"));
 	}
 
 	private static void addResourceDescriptor(PropertyDescriptor descriptor) {
@@ -64,6 +80,7 @@ public class ResourceProperties implements IPropertySource {
 		
 		return result.toArray(new IPropertyDescriptor[result.size()]);
 	}
+
 	
 	public Object getPropertyValue(Object name) {
 		if (name.equals(P_NAME)) {
@@ -80,6 +97,22 @@ public class ResourceProperties implements IPropertySource {
 			return fResource.getLocalTimeStamp();
 		} else 	if (name.equals(P_MODIFICATION_STAMP)) {
 			return fResource.getModificationStamp();
+		} else 	if (name.equals(P_EXISTS)) {
+			return fResource.exists();
+		} else 	if (name.equals(P_IS_ACCESSIBLE)) {
+			return fResource.isAccessible();
+		} else 	if (name.equals(P_IS_DERIVED)) {
+			return fResource.isDerived();
+		} else 	if (name.equals(P_IS_HIDDEN)) {
+			return fResource.isHidden();
+		} else 	if (name.equals(P_IS_LINKED)) {
+			return fResource.isLinked();
+		} else 	if (name.equals(P_IS_PHANTOM)) {
+			return fResource.isPhantom();
+		} else 	if (name.equals(P_IS_TEAM_PRIVATE_MEMBER)) {
+			return fResource.isTeamPrivateMember();
+		} else 	if (name.equals(P_IS_VIRTUAL)) {
+			return fResource.isVirtual();
 		}
 		
 		return null;
