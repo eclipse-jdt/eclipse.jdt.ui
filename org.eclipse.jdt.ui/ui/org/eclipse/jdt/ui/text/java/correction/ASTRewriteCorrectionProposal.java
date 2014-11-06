06121af783cbbe98cd88ce38a4a8f7c435c19402
Markus Keller <markus_keller@ch.ibm.com>
2014-11-06 16:51:21 +0100

    Javadoc typo

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/ui/text/java/correction/ASTRewriteCorrectionProposal.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/ui/text/java/correction/ASTRewriteCorrectionProposal.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2012 IBM Corporation and others.
+ * Copyright (c) 2000, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -72,7 +72,7 @@ public class ASTRewriteCorrectionProposal extends CUCorrectionProposal {
 	}
 
 	/**
-	 * Returns the import rewrite used for this compilation unit. <code>
+	 * Returns the import rewrite used for this compilation unit.
 	 * 
 	 * @return the import rewrite or <code>null</code> if no import rewrite has been set
 	 * @nooverride This method is not intended to be re-implemented or extended by clients.
