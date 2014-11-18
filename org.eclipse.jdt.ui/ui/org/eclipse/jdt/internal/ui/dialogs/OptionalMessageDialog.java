9a0fa80f59bb56bcd64d35ace658655ea2d8c8f4
Dani Megert <dmegert>
2014-11-18 14:05:25 +0100

    Fixed bug 446604: [platform] IOOBE in OptionalMessageDialog.buttonPressed

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/dialogs/OptionalMessageDialog.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/dialogs/OptionalMessageDialog.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2012 IBM Corporation and others.
+ * Copyright (c) 2000, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -113,7 +113,9 @@ public class OptionalMessageDialog extends MessageDialog {
 	@Override
 	protected void buttonPressed(int buttonId) {
 		super.buttonPressed(buttonId);
-		if (IDialogConstants.CANCEL_LABEL.equals(getButtonLabels()[getReturnCode()]))
+		String[] buttonLabels= getButtonLabels();
+		int returnCode= getReturnCode();
+		if (returnCode < buttonLabels.length && IDialogConstants.CANCEL_LABEL.equals(buttonLabels[returnCode]))
 			setDialogEnabled(fId, true); // don't store if cancelled
 	}
 
