01224fe2087d11a93beb82045ee005242beb5c78
Dani Megert <dmegert>
2015-02-19 22:34:57 +0100

    Reuse local variable

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/dialogs/MultiElementListSelectionDialog.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/dialogs/MultiElementListSelectionDialog.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2011 IBM Corporation and others.
+ * Copyright (c) 2000, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -262,7 +262,7 @@ public class MultiElementListSelectionDialog extends AbstractElementListSelectio
 		list.set(fCurrentPage, selectedElements);
 
 		// store result
-		setResult(fCurrentPage, getSelectedElements());
+		setResult(fCurrentPage, selectedElements);
 
 		if (toNextPage) {
 			if (fCurrentPage + 1 >= fNumberOfPages)
