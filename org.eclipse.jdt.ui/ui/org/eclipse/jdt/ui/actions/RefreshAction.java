e1f746ec4450945aca6e6655480c5e7cbe57ac45
Andrey Loskutov <loskutov@gmx.de>
2015-03-21 20:49:43 +0100

    Bug 462760 - SelectionListenerAction.getSelectedResources() return type too strict
    Fix for compile error if the super class will relax return type bounds
    as planned via bug 462760.
    
    Change-Id: I9c9412c396f1489c6a924182fab441291046f506
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/ui/actions/RefreshAction.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/ui/actions/RefreshAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2012 IBM Corporation and others.
+ * Copyright (c) 2000, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -80,8 +80,8 @@ public class RefreshAction extends SelectionDispatchAction {
 		}
 
 		@Override
-		protected List<IResource> getSelectedResources() {
-			List<IResource> selectedResources= super.getSelectedResources();
+		protected List<? extends IResource> getSelectedResources() {
+			List<? extends IResource> selectedResources= super.getSelectedResources();
 			if (!getStructuredSelection().isEmpty() && selectedResources.size() == 1 && selectedResources.get(0) instanceof IWorkspaceRoot) {
 				selectedResources= Collections.emptyList(); // Refresh action refreshes root when it can't find any resources in selection
 			}
