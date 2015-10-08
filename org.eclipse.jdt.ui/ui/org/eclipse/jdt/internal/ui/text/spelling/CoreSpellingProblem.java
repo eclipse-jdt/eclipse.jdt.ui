dfc14c2716b2c92b756365596886952f060219a5
Noopur Gupta <noopur_gupta@in.ibm.com>
2015-10-08 16:31:51 +0530

    Bug 478780 - add API IProblem#isInfo() - update JDT UI code
    Change-Id: I9a2238fdc184e09aa315f85ac33723c80725f969

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/spelling/CoreSpellingProblem.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/spelling/CoreSpellingProblem.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2011 IBM Corporation and others.
+ * Copyright (c) 2005, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -163,6 +163,11 @@ public class CoreSpellingProblem extends CategorizedProblem {
 	@Override
 	public boolean isWarning() {
 		return true;
+	}
+
+	@Override
+	public boolean isInfo() {
+		return false;
 	}
 
 	/*
