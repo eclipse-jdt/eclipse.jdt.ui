a5c7dfaf05dd6a538b0798d59bb1b05d0c65c5a6
Markus Keller <markus_keller@ch.ibm.com>
2015-11-11 17:38:29 +0100

    Bug 481932: Java compare editor should block Toggle Word Wrap (Alt+Shift+Y) shortcut

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/compare/JavaMergeViewer.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/compare/JavaMergeViewer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2014 IBM Corporation and others.
+ * Copyright (c) 2000, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -607,6 +607,10 @@ public class JavaMergeViewer extends TextMergeViewer {
 		@Override
 		public boolean isEditorInputReadOnly() {
 			return !fEditable;
+		}
+		@Override
+		protected boolean isWordWrapSupported() {
+			return false;
 		}
 		@Override
 		protected void setActionsActivated(boolean state) {
