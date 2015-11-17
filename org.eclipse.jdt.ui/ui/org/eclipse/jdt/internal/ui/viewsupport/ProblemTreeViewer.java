2ecd846b70c1cab712d85e3132666e3ebcff1bb2
Noopur Gupta <noopur_gupta@in.ibm.com>
2015-11-17 18:12:34 +0530

    Bug 481474: Change Viewer.setFilters to varargs - updated jdt.ui to get rid of the warnings
    Change-Id: Id5dda239f1ec94829e6744cccd8f5850749e5cbf

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/viewsupport/ProblemTreeViewer.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/viewsupport/ProblemTreeViewer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2011 IBM Corporation and others.
+ * Copyright (c) 2000, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -127,7 +127,7 @@ public class ProblemTreeViewer extends TreeViewer implements ResourceToItemsMapp
 	}
 
 	@Override
-	public void setFilters(ViewerFilter[] filters) {
+	public void setFilters(ViewerFilter... filters) {
 		ViewerFilter[] oldFilters= getFilters();
 		for (int i= 0; i < filters.length; i++) {
 			ViewerFilter curr= filters[i];
