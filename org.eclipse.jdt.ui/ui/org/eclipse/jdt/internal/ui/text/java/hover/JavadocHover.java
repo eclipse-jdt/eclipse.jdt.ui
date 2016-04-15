fd3cf64994236fa858ef3c96dd3e08f2cfaf245d
Noopur Gupta <noopur_gupta@in.ibm.com>
2016-04-15 18:34:12 +0530

    Fixed bug 457523: [hovering] Javadoc following @link to a constant does not show its value
    Change-Id: I263abd8c83b510b295d6af1c20b97999767ff08b

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/java/hover/JavadocHover.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/java/hover/JavadocHover.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2015 IBM Corporation and others.
+ * Copyright (c) 2000, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -871,11 +871,15 @@ public class JavadocHover extends AbstractJavaEditorTextHover {
 		if (!isStaticFinal(field))
 			return null;
 
-		ASTNode node= getHoveredASTNode(editorInputElement, hoverRegion);
-		if (node == null)
-			return null;
-
-		Object constantValue= getVariableBindingConstValue(node, field);
+		Object constantValue= null;
+		if (editorInputElement == null || hoverRegion == null) {
+			constantValue= JavadocView.computeFieldConstantFromTypeAST(field, null);
+		} else {
+			ASTNode node= getHoveredASTNode(editorInputElement, hoverRegion);
+			if (node == null)
+				return null;
+			constantValue= getVariableBindingConstValue(node, field);
+		}
 		if (constantValue == null)
 			return null;
 
