51234ed1a519f265dcc569c030d7d871aecda738
Markus Keller <markus_keller@ch.ibm.com>
2016-06-06 00:28:21 +0200

    Bug 482578: [1.9] Syntax coloring for new keywords in module-info.java
    fixed potential NPE

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/ui/text/JavaSourceViewerConfiguration.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/ui/text/JavaSourceViewerConfiguration.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2015 IBM Corporation and others.
+ * Copyright (c) 2000, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -348,7 +348,8 @@ public class JavaSourceViewerConfiguration extends TextSourceViewerConfiguration
 	private void initializeScanners() {
 		Assert.isTrue(isNewSetup());
 		boolean isModuleInfoEditor= false;
-		if (fTextEditor != null && JavaUI.ID_MODULE_INFO_EDITOR.equals(fTextEditor.getEditorSite().getId())) {
+		if (fTextEditor != null && fTextEditor.getEditorSite() != null
+				&& JavaUI.ID_MODULE_INFO_EDITOR.equals(fTextEditor.getEditorSite().getId())) {
 			isModuleInfoEditor= true;
 		}
 		fCodeScanner= new JavaCodeScanner(getColorManager(), fPreferenceStore, isModuleInfoEditor);
