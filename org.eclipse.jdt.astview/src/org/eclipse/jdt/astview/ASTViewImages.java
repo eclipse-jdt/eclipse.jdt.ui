0eb9683b1b733472825648687cc8d3ceef81268d
Matthias Becker <ma.becker@sap.com>
2016-09-16 13:13:20 +0200

    Bug 501592 jdt.jeview and jdt.astview still use GIFs
    Change-Id: I1325072f0e296e7a6d25558b10b5196899932823
    Signed-off-by: Matthias Becker <ma.becker@sap.com>

--- org.eclipse.jdt.astview/src/org/eclipse/jdt/astview/ASTViewImages.java
+++ org.eclipse.jdt.astview/src/org/eclipse/jdt/astview/ASTViewImages.java
@@ -1,10 +1,10 @@
 /*******************************************************************************
  * Copyright (c) 2000, 2006 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials 
+ * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
@@ -24,25 +24,25 @@ import org.osgi.framework.Bundle;
 public class ASTViewImages {
 
 	private static final IPath ICONS_PATH= new Path("$nl$/icons"); //$NON-NLS-1$
-	
-	public static final String COLLAPSE= "collapseall.gif"; //$NON-NLS-1$
-	public static final String EXPAND= "expandall.gif"; //$NON-NLS-1$
-	public static final String LINK_WITH_EDITOR= "synced.gif"; //$NON-NLS-1$
 
-	public static final String SETFOCUS= "setfocus.gif"; //$NON-NLS-1$
-	public static final String REFRESH= "refresh.gif"; //$NON-NLS-1$
-	public static final String CLEAR= "clear.gif"; //$NON-NLS-1$
+	public static final String COLLAPSE= "collapseall.png"; //$NON-NLS-1$
+	public static final String EXPAND= "expandall.png"; //$NON-NLS-1$
+	public static final String LINK_WITH_EDITOR= "synced.png"; //$NON-NLS-1$
 
-	public static final String ADD_TO_TRAY= "add.gif"; //$NON-NLS-1$
-	
+	public static final String SETFOCUS= "setfocus.png"; //$NON-NLS-1$
+	public static final String REFRESH= "refresh.png"; //$NON-NLS-1$
+	public static final String CLEAR= "clear.png"; //$NON-NLS-1$
+
+	public static final String ADD_TO_TRAY= "add.png"; //$NON-NLS-1$
+
 	//---- Helper methods to access icons on the file system --------------------------------------
 
 	public static void setImageDescriptors(IAction action, String type) {
 		ImageDescriptor id= create("d", type); //$NON-NLS-1$
 		if (id != null)
 			action.setDisabledImageDescriptor(id);
-	
-		id= create("c", type); //$NON-NLS-1$
+
+		id= create("e", type); //$NON-NLS-1$
 		if (id != null) {
 			action.setHoverImageDescriptor(id);
 			action.setImageDescriptor(id);
@@ -50,12 +50,12 @@ public class ASTViewImages {
 			action.setImageDescriptor(ImageDescriptor.getMissingImageDescriptor());
 		}
 	}
-	
+
 	private static ImageDescriptor create(String prefix, String name) {
 		IPath path= ICONS_PATH.append(prefix).append(name);
 		return createImageDescriptor(ASTViewPlugin.getDefault().getBundle(), path);
 	}
-	
+
 	/*
 	 * Since 3.1.1. Load from icon paths with $NL$
 	 */
