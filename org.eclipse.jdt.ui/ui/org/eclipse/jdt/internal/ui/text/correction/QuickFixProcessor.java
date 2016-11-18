82a3072b22d4d434b33e7166630e0ce2e8ac7d4d
Till Brychcy <register.eclipse@brychcy.de>
2016-11-18 21:02:36 +0100

    Bug 508219 - [quick fix] "remove redundant NonNullByDefault" caused by NonNullByDefault on fields or local variables
    Change-Id: Idc0539991e412193de8f29435aa5664f6a813530
    Signed-off-by: Till Brychcy <register.eclipse@brychcy.de>

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/correction/QuickFixProcessor.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/correction/QuickFixProcessor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2016 IBM Corporation and others.
+ * Copyright (c) 2000, 2017 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -761,6 +761,8 @@ public class QuickFixProcessor implements IQuickFixProcessor {
 			case IProblem.RedundantNullDefaultAnnotationPackage:
 			case IProblem.RedundantNullDefaultAnnotationType:
 			case IProblem.RedundantNullDefaultAnnotationMethod:
+			case IProblem.RedundantNullDefaultAnnotationLocal:
+			case IProblem.RedundantNullDefaultAnnotationField:
 				NullAnnotationsCorrectionProcessor.addRemoveRedundantAnnotationProposal(context, problem, proposals);
 				break;
 			case IProblem.UnusedTypeParameter:
