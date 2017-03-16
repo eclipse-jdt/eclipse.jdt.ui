84ac6be47f33fd76d325d6909fe69a47906f3bbb
Markus Keller <markus_keller@ch.ibm.com>
2017-03-16 20:42:33 +0100

    Bug 514733: Use Image#getImageData(int zoom) in JDT UI
    Change-Id: I5bb68d96d0b7bd8cdaca9b6f904c62dc354fefe8

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/viewsupport/ImageImageDescriptor.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/viewsupport/ImageImageDescriptor.java
@@ -12,7 +12,6 @@ package org.eclipse.jdt.internal.ui.viewsupport;
 
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.ImageData;
-import org.eclipse.swt.graphics.Rectangle;
 
 import org.eclipse.jface.resource.ImageDescriptor;
 
@@ -33,24 +32,7 @@ public class ImageImageDescriptor extends ImageDescriptor {
 
 	@Override
 	public ImageData getImageData(int zoom) {
-		// workaround for the missing API Image#getImageData(int zoom) (bug 496409)
-		if (zoom == 100) {
-			return fImage.getImageData();
-		}
-		ImageData zoomedImageData = fImage.getImageDataAtCurrentZoom();
-		Rectangle bounds = fImage.getBounds();
-		//TODO: Probably has off-by-one problems at fractional zoom levels:
-		if (bounds.width == scaleDown(zoomedImageData.width, zoom)
-				&& bounds.height == scaleDown(zoomedImageData.height, zoom)) {
-			return zoomedImageData;
-		}
-		return null;
-	}
-
-	private static int scaleDown(int value, int zoom) {
-		// @see SWT's internal DPIUtil#autoScaleDown(int)
-		float scaleFactor = zoom / 100f;
-		return Math.round(value / scaleFactor);
+		return fImage.getImageData(zoom);
 	}
 
 	@Override
