a777839b0092bedb5bbcee91d6e58cb27789719e
Noopur Gupta <noopur_gupta@in.ibm.com>
2017-07-10 17:20:01 +0530

    Bug 519451: [9][render] private interface methods should be rendered with correct image
    Change-Id: I62870fa20d709e2eeabffe5521f5ecb636f4497b

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/viewsupport/JavaElementImageProvider.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/viewsupport/JavaElementImageProvider.java
@@ -1,9 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2016 IBM Corporation and others.
+ * Copyright (c) 2000, 2017 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * This is an implementation of an early-draft specification developed under the Java
+ * Community Process (JCP) and is made available for testing and evaluation purposes
+ * only. The code is not compatible with any specification of the JCP.
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
@@ -436,12 +440,14 @@ public class JavaElementImageProvider {
 	}
 
 	public static ImageDescriptor getMethodImageDescriptor(boolean isInInterfaceOrAnnotation, int flags) {
-		if (Flags.isPublic(flags) || isInInterfaceOrAnnotation)
+		if (Flags.isPublic(flags))
 			return JavaPluginImages.DESC_MISC_PUBLIC;
 		if (Flags.isProtected(flags))
 			return JavaPluginImages.DESC_MISC_PROTECTED;
 		if (Flags.isPrivate(flags))
 			return JavaPluginImages.DESC_MISC_PRIVATE;
+		if (isInInterfaceOrAnnotation)
+			return JavaPluginImages.DESC_MISC_PUBLIC;
 
 		return JavaPluginImages.DESC_MISC_DEFAULT;
 	}
