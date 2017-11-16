e37c2f79e0c31afe012ca847ada228b05b4d974c
Simeon Andreev <simeon.danailov.andreev@gmail.com>
2017-11-16 14:06:06 +0100

    Bug 506009 -  UI freezes reported in PackageFragment.getElementName
    The Package Explorer View has two options which are very slow for large
    package structures. Those are the hierarchical resp. flat representation
    of packages.
    
    This is the case, since the Java model structure does not match the
    hierarchical structure presented in the view. The view computes the
    package hierarchy on its own. The view does so by going over all
    packages in a package root, for its every operation. This results in
    e.g. refresh operations which are quadratic in the number of packages of
    a project. For package numbers above 1000, refreshes can freeze the UI
    for minutes.
    
    With this change the PackageExplorerContentProvider computes the
    hierarchical structure of packages once and caches it. The cache is
    cleaned on every Java model change. This reduces the complexity of
    operations that run on the view, including refreshing. The  UI freeze
    duration is reduced to a few seconds, down from minutes.
    
    Change-Id: Ib28aa66d5718a879491b9076f2d78c3c7e4e73b6
    Signed-off-by: Simeon Andreev <simeon.danailov.andreev@gmail.com>

---
+++ org.eclipse.jdt.ui.tests/ui/org/eclipse/jdt/ui/tests/packageview/PackageCacheTest.java
@@ -0,0 +1,233 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Simeon Andreev and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Simeon Andreev - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.jdt.ui.tests.packageview;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+
+import org.eclipse.jdt.testplugin.JavaProjectHelper;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
+
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IPackageFragment;
+import org.eclipse.jdt.core.IPackageFragmentRoot;
+
+import org.eclipse.jdt.internal.ui.packageview.PackageCache;
+
+import junit.framework.TestCase;
+
+/**
+ * Tests for {@link PackageCache}.
+ *
+ */
+public class PackageCacheTest extends TestCase {
+
+	private IJavaProject testProject;
+
+	private IPackageFragmentRoot src;
+
+	private IPackageFragment package_a;
+	private IPackageFragment package_a_b;
+	private IPackageFragment package_a_b_c;
+	private IPackageFragment package_a_b_c_d1;
+	private IPackageFragment package_a_b_c_d2;
+	private IPackageFragment package_a_b_e;
+	private IPackageFragment package_f;
+	private IPackageFragment package_f_g;
+
+	private PackageCache packageCache;
+
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+
+		IProgressMonitor monitor= new NullProgressMonitor();
+
+		String projectName= getClass().getSimpleName();
+		testProject= JavaProjectHelper.createJavaProject(projectName, "bin");
+		src= JavaProjectHelper.addSourceContainer(testProject, "src");
+
+		boolean force= true;
+		package_a= src.createPackageFragment("a", force, monitor);
+		package_a_b= src.createPackageFragment("a.b", force, monitor);
+		package_a_b_c= src.createPackageFragment("a.b.c", force, monitor);
+		package_a_b_c_d1= src.createPackageFragment("a.b.c.d1", force, monitor);
+		package_a_b_c_d2= src.createPackageFragment("a.b.c.d2", force, monitor);
+		package_a_b_e= src.createPackageFragment("a.b.e", force, monitor);
+		package_f= src.createPackageFragment("f", force, monitor);
+		package_f_g= src.createPackageFragment("f.g", force, monitor);
+
+		packageCache= new PackageCache(src);
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		testProject.getProject().delete(true, false, new NullProgressMonitor());
+		super.tearDown();
+	}
+
+
+	@Test
+	public void testGetDirectChildren() throws Exception {
+		List<IPackageFragment> allPackages= allPackages();
+
+		Map<IPackageFragment, List<IPackageFragment>> actualChildren = new LinkedHashMap<>();
+		for (IPackageFragment packageFragment : allPackages) {
+			List<IPackageFragment> childrenOfPackage= packageCache.getDirectChildren(packageFragment);
+			actualChildren.put(packageFragment, childrenOfPackage);
+		}
+
+		Map<IPackageFragment, List<IPackageFragment>> expectedChildren = new LinkedHashMap<>();
+		expectedChildren.put(package_a, Arrays.asList(package_a_b));
+		expectedChildren.put(package_a_b, Arrays.asList(package_a_b_c, package_a_b_e));
+		expectedChildren.put(package_a_b_c, Arrays.asList(package_a_b_c_d1, package_a_b_c_d2));
+		expectedChildren.put(package_a_b_c_d1, Collections.emptyList());
+		expectedChildren.put(package_a_b_c_d2, Collections.emptyList());
+		expectedChildren.put(package_a_b_e, Collections.emptyList());
+		expectedChildren.put(package_f, Arrays.asList(package_f_g));
+		expectedChildren.put(package_f_g, Collections.emptyList());
+
+		assertEquals("method returned wrong results",
+				expectedChildren, actualChildren);
+	}
+
+	@Test
+	public void testGetSingleChild() throws Exception {
+		Map<IPackageFragment, IPackageFragment> actualSingleChildren= actualSingleChildren();
+		Map<IPackageFragment, IPackageFragment> expectedSingleChildren= expectedSingleChildren();
+
+		assertEquals("method returned wrong results",
+				expectedSingleChildren, actualSingleChildren);
+	}
+
+	@Test
+	public void testSingleChildAgainstOldImplementation() throws Exception {
+		Map<IPackageFragment, IPackageFragment> actualSingleChildren= actualSingleChildren();
+
+		List<IPackageFragment> allPackages= allPackages();
+		Map<IPackageFragment, IPackageFragment> expectedSingleChildren= new LinkedHashMap<>();
+		for (IPackageFragment packageFragment : allPackages) {
+			IPackageFragment singleChild= findSinglePackageChild(packageFragment);
+			if (singleChild != null) {
+				expectedSingleChildren.put(packageFragment, singleChild);
+			}
+		}
+
+		assertEquals("method returned wrong results",
+				expectedSingleChildren, actualSingleChildren);
+	}
+
+	@Test
+	public void testHasSingleChild() throws Exception {
+		List<IPackageFragment> actualPackagesWithSingleChild= actualPackagesWithSingleChild();
+		List<IPackageFragment> expectedPackagesWithSingleChild= expectedPackagesWithSingleChild();
+
+		assertEquals("method returned wrong results",
+				expectedPackagesWithSingleChild, actualPackagesWithSingleChild);
+	}
+
+	@Test
+	public void testHasSingleChildAgainstOldImplementation() throws Exception {
+		List<IPackageFragment> actualPackagesWithSingleChild= actualPackagesWithSingleChild();
+
+		List<IPackageFragment> allPackages= allPackages();
+		List<IPackageFragment> expectedPackagesWithSingleChild= new ArrayList<>();
+		for (IPackageFragment packageFragment : allPackages) {
+			IPackageFragment singleChild= findSinglePackageChild(packageFragment);
+			if (singleChild != null) {
+				expectedPackagesWithSingleChild.add(packageFragment);
+			}
+		}
+
+		assertEquals("method returned wrong results",
+				expectedPackagesWithSingleChild, actualPackagesWithSingleChild);
+	}
+
+	private Map<IPackageFragment, IPackageFragment> actualSingleChildren() throws Exception {
+		List<IPackageFragment> allPackages= allPackages();
+		Map<IPackageFragment, IPackageFragment> actualSingleChildren= new LinkedHashMap<>();
+		for (IPackageFragment packageFragment : allPackages) {
+			IPackageFragment singleChild= packageCache.getSingleChild(packageFragment);
+			if (singleChild != null) {
+				actualSingleChildren.put(packageFragment, singleChild);
+			}
+		}
+		return actualSingleChildren;
+	}
+
+	private Map<IPackageFragment, IPackageFragment> expectedSingleChildren() {
+		Map<IPackageFragment, IPackageFragment> expectedSingleChildren= new LinkedHashMap<>();
+		expectedSingleChildren.put(package_a, package_a_b);
+		expectedSingleChildren.put(package_f, package_f_g);
+		return expectedSingleChildren;
+	}
+
+	private List<IPackageFragment> actualPackagesWithSingleChild() throws Exception {
+		List<IPackageFragment> allPackages= allPackages();
+		List<IPackageFragment> actualPackagesWithSingleChild= new ArrayList<>();
+		for (IPackageFragment packageFragment : allPackages) {
+			if (packageCache.hasSingleChild(packageFragment)) {
+				actualPackagesWithSingleChild.add(packageFragment);
+			}
+		}
+		return actualPackagesWithSingleChild;
+	}
+
+	private List<IPackageFragment> expectedPackagesWithSingleChild() throws Exception {
+		Map<IPackageFragment, IPackageFragment> expectedSingleChildren= expectedSingleChildren();
+		return new ArrayList<>(expectedSingleChildren.keySet());
+	}
+
+	private List<IPackageFragment> allPackages() {
+		return Arrays.asList(
+				package_a,
+				package_a_b,
+				package_a_b_c,
+				package_a_b_c_d1,
+				package_a_b_c_d2,
+				package_a_b_e,
+				package_f,
+				package_f_g);
+	}
+
+	/*
+	 * Old "reference" implementation from org.eclipse.jdt.internal.ui.packageview.PackageExplorerContentProvider,
+	 * which computes the single child package of a package (if any).
+	 */
+	private IPackageFragment findSinglePackageChild(IPackageFragment fragment) throws Exception {
+		IJavaElement[] children= src.getChildren();
+		String prefix= fragment.getElementName() + '.';
+		int prefixLen= prefix.length();
+		IPackageFragment found= null;
+		for (int i= 0; i < children.length; ++i) {
+			IJavaElement element= children[i];
+			String name= element.getElementName();
+			if (name.startsWith(prefix) && name.length() > prefixLen && name.indexOf('.', prefixLen) == -1) {
+				if (found == null) {
+					found= (IPackageFragment) element;
+				} else {
+					return null;
+				}
+			}
+		}
+		return found;
+	}
+}
