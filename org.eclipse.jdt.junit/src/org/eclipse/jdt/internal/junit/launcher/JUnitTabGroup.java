e2548a4c1a797182dabaa37262e256e85e00d0ad
Till Brychcy <register.eclipse@brychcy.de>
2018-02-02 21:30:51 +0100

    Bug 522405 - [9] JUnit should set classpath and modulepath during launch
    Change-Id: I052c3d0e4e9cdfe185622bb2b5aede39a875726c

--- org.eclipse.jdt.junit/src/org/eclipse/jdt/internal/junit/launcher/JUnitTabGroup.java
+++ org.eclipse.jdt.junit/src/org/eclipse/jdt/internal/junit/launcher/JUnitTabGroup.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2010 IBM Corporation and others.
+ * Copyright (c) 2000, 2018 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -14,28 +14,35 @@ package org.eclipse.jdt.internal.junit.launcher;
 
 import org.eclipse.jdt.debug.ui.launchConfigurations.JavaArgumentsTab;
 import org.eclipse.jdt.debug.ui.launchConfigurations.JavaClasspathTab;
+import org.eclipse.jdt.debug.ui.launchConfigurations.JavaDependenciesTab;
 import org.eclipse.jdt.debug.ui.launchConfigurations.JavaJRETab;
 import org.eclipse.jdt.junit.launcher.JUnitLaunchConfigurationTab;
 
+import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
 
 import org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup;
 import org.eclipse.debug.ui.CommonTab;
+import org.eclipse.debug.ui.DebugUITools;
 import org.eclipse.debug.ui.EnvironmentTab;
 import org.eclipse.debug.ui.ILaunchConfigurationDialog;
 import org.eclipse.debug.ui.ILaunchConfigurationTab;
 import org.eclipse.debug.ui.sourcelookup.SourceLookupTab;
+
+import org.eclipse.jdt.launching.JavaRuntime;
 
 
 public class JUnitTabGroup extends AbstractLaunchConfigurationTabGroup {
 
 	@Override
 	public void createTabs(ILaunchConfigurationDialog dialog, String mode) {
+		ILaunchConfiguration configuration = DebugUITools.getLaunchConfiguration(dialog);
+		boolean isModularConfiguration= configuration != null && JavaRuntime.isModularConfiguration(configuration);
 		ILaunchConfigurationTab[] tabs= new ILaunchConfigurationTab[] {
 			new JUnitLaunchConfigurationTab(),
 			new JavaArgumentsTab(),
-			new JavaClasspathTab(),
-			new JavaJRETab(),
+			new JavaJRETab(true),
+			isModularConfiguration ? new JavaDependenciesTab() : new JavaClasspathTab(),
 			new SourceLookupTab(),
 			new EnvironmentTab(),
 			new CommonTab()
