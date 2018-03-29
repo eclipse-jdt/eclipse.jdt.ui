2dc1a28a14f143347136773486163db2e9eeb369
Noopur Gupta <noopur_gupta@in.ibm.com>
2018-03-29 15:13:21 +0530

    Bug 532996: [10] Refactor -> Rename operation should not be available on 'var' type
    Change-Id: Icf161d46c67379a59925cb1f96dc39664d76f969

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/correction/QuickAssistProcessor.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/correction/QuickAssistProcessor.java
@@ -2570,7 +2570,7 @@ public class QuickAssistProcessor implements IQuickAssistProcessor {
 			return false;
 		}
 		SimpleName name= (SimpleName) node;
-		if (name.isVar()) {
+		if (name.getAST().apiLevel() >= AST.JLS10 && name.isVar()) {
 			return false;
 		}
 		IBinding binding= name.resolveBinding();
@@ -2608,7 +2608,7 @@ public class QuickAssistProcessor implements IQuickAssistProcessor {
 			return false;
 		}
 		SimpleName name= (SimpleName) node;
-		if (name.isVar()) {
+		if (name.getAST().apiLevel() >= AST.JLS10 && name.isVar()) {
 			return false;
 		}
 		IBinding binding= name.resolveBinding();
