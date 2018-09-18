c115e2c7380d3ffe3be81b3d5a0508c20c8dde79
Kalyan Prasad Tatavarthi <kalyan_prasad@in.ibm.com>
2018-09-18 11:02:30 +0530

    Bug 526331 - [package explorer] Copy and Copy Qualified name on module in Package Explorer is wrong
    Change-Id: Ibcc279a482851242b0328d5929316c33339e4c38
    Signed-off-by: Kalyan Prasad Tatavarthi <kalyan_prasad@in.ibm.com>

--- org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/TypedSource.java
+++ org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/TypedSource.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2016 IBM Corporation and others.
+ * Copyright (c) 2000, 2018 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -104,7 +104,8 @@ public class TypedSource {
 				|| 	type == IJavaElement.IMPORT_DECLARATION
 				|| 	type == IJavaElement.INITIALIZER
 				|| 	type == IJavaElement.METHOD
-				|| 	type == IJavaElement.PACKAGE_DECLARATION;
+				|| 	type == IJavaElement.PACKAGE_DECLARATION
+				|| 	type == IJavaElement.JAVA_MODULE;
 	}
 
 
