c86deb49e06927a9e48e062fabe5dbcb53a33b9d
angelozerr <angelo.zerr@gmail.com>
2018-11-30 13:08:28 +0100

    Bug 541727 - [code mining] Ctrl+click to navigate to ref/implementation
    Support Ctrl+Click on one reference to open the referenced or
    implemented java element in an editor.
    
    Change-Id: Idd896084b1d51262f58734bd87520caf74d0572b
    Signed-off-by: angelozerr <angelo.zerr@gmail.com>
    Signed-off-by: Jeff Johnston <jjohnstn@redhat.com>

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/javaeditor/codemining/JavaImplementationCodeMining.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/javaeditor/codemining/JavaImplementationCodeMining.java
@@ -16,11 +16,13 @@ package org.eclipse.jdt.internal.ui.javaeditor.codemining;
 
 import java.text.MessageFormat;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.MouseEvent;
 
 import org.eclipse.core.runtime.CoreException;
@@ -33,14 +35,21 @@ import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.ITextViewer;
 import org.eclipse.jface.text.codemining.ICodeMiningProvider;
 
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.PartInitException;
+
+import org.eclipse.ui.texteditor.ITextEditor;
+
 import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IMethod;
+import org.eclipse.jdt.core.ISourceReference;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.JavaModelException;
 
 import org.eclipse.jdt.ui.actions.FindDeclarationsInHierarchyAction;
 import org.eclipse.jdt.ui.actions.OpenTypeHierarchyAction;
 
+import org.eclipse.jdt.internal.ui.javaeditor.EditorUtility;
 import org.eclipse.jdt.internal.ui.javaeditor.JavaEditor;
 
 /**
@@ -69,24 +78,53 @@ public class JavaImplementationCodeMining extends AbstractJavaElementLineHeaderC
 		return CompletableFuture.runAsync(() -> {
 			try {
 				IJavaElement element= super.getElement();
-				long implCount = 0;
 				if (element instanceof IType) {
 					// for a type, count types implementing this type and show type hierarchy
-					implCount= countTypeImplementations((IType) element, monitor);
-					action= implCount > 0 ? e -> new OpenTypeHierarchyAction(editor).run(new StructuredSelection(element)) : null;
-					if (implCount == 0 && showImplementationsAtLeastOne) {
+					final long typeImplCount= countTypeImplementations((IType) element, monitor);
+					action= typeImplCount > 0 ? e -> {
+						if (typeImplCount == 1 && (e.stateMask & SWT.CTRL) == SWT.CTRL) {
+							// Ctrl + Click is done, open the referenced element in the Java Editor
+							try {
+								IType javaElement= getTypeImplementation((IType)element, monitor);
+								showJavaElementInEditor(javaElement);
+							} catch (JavaModelException e1) {
+								// Should never occur
+							} catch (CoreException e1) {
+								// Should never occur
+							}
+						} else {
+							// Otherwise, launch open type hierarchy
+							new OpenTypeHierarchyAction(editor).run(new StructuredSelection(element));
+						}
+					} : null;
+					if (typeImplCount == 0 && showImplementationsAtLeastOne) {
 						super.setLabel(""); //$NON-NLS-1$
 					} else {
-						super.setLabel(MessageFormat.format(JavaCodeMiningMessages.JavaImplementationCodeMining_label, implCount));
+						super.setLabel(MessageFormat.format(JavaCodeMiningMessages.JavaImplementationCodeMining_label, typeImplCount));
 					}
 				} else if (element instanceof IMethod) {
 					// for a method, count declarations in hierarchy and show search->declarations->hierarchy
-					implCount= countMethodImplementations((IMethod) element, monitor);
-					action= implCount > 0 ? e -> new FindDeclarationsInHierarchyAction(editor, true).run(element) : null;
-					if (implCount == 0 && showImplementationsAtLeastOne) {
+					final long methodImplCount= countMethodImplementations((IMethod) element, monitor);
+					action= methodImplCount > 0 ? e -> {
+						if (methodImplCount == 1 && (e.stateMask & SWT.CTRL) == SWT.CTRL) {
+							// Ctrl + Click is done, open the referenced element in the Java Editor
+							try {
+								IMethod javaElement= getMethodImplementation((IMethod)element, monitor);
+								showJavaElementInEditor(javaElement);
+							} catch (JavaModelException e1) {
+								// Should never occur
+							} catch (CoreException e1) {
+								// Should never occur
+							}
+						} else {
+							// Otherwise, launch find declarations in type hierarchy
+							new FindDeclarationsInHierarchyAction(editor, true).run(element);
+						}
+					} : null;
+					if (methodImplCount == 0 && showImplementationsAtLeastOne) {
 						super.setLabel(""); //$NON-NLS-1$
 					} else {
-						super.setLabel(MessageFormat.format(JavaCodeMiningMessages.JavaImplementationCodeMining_label, implCount));
+						super.setLabel(MessageFormat.format(JavaCodeMiningMessages.JavaImplementationCodeMining_label, methodImplCount));
 					}
 				}
 			} catch (CoreException e1) {
@@ -98,6 +136,25 @@ public class JavaImplementationCodeMining extends AbstractJavaElementLineHeaderC
 	@Override
 	public Consumer<MouseEvent> getAction() {
 		return action;
+	}
+
+	/**
+	 * Show Java Element in editor
+	 *
+	 * @param javaElement element to show
+	 * @throws JavaModelException if failure occurs in reveal
+	 * @throws PartInitException if failure occurs in opening editor
+	 */
+	private static void showJavaElementInEditor(IJavaElement javaElement) throws JavaModelException, PartInitException {
+		IEditorPart part= EditorUtility.openInEditor(javaElement);
+		if (part != null) {
+			EditorUtility.revealInEditor(part, javaElement);
+			if (part instanceof ITextEditor) {
+				ITextEditor textEditor= (ITextEditor) part;
+				textEditor.selectAndReveal(((ISourceReference)javaElement).getNameRange().getOffset(),
+						((ISourceReference)javaElement).getNameRange().getLength());
+			}
+		}
 	}
 
 	/**
@@ -114,11 +171,24 @@ public class JavaImplementationCodeMining extends AbstractJavaElementLineHeaderC
 	}
 
 	/**
+	 * Return the first implementation for the given java element type.
+	 *
+	 * @param type the java element type.
+	 * @param monitor the monitor
+	 * @return the first type implementation.
+	 * @throws JavaModelException throws when Java error
+	 */
+	private static IType getTypeImplementation(IType type, IProgressMonitor monitor) throws JavaModelException {
+		IType[] results= type.newTypeHierarchy(monitor).getAllSubtypes(type);
+		return Stream.of(results).filter(t -> t.getAncestor(IJavaElement.COMPILATION_UNIT) != null).findFirst().get();
+	}
+
+	/**
 	 * Return the count of implementation for the java element method.
 	 *
 	 * @param method the java element method.
 	 * @param monitor the monitor
-	 * @return the count of implementation for the given java element type.
+	 * @return the count of implementation for the given java element method.
 	 * @throws CoreException throws when java error
 	 */
 	private static long countMethodImplementations(IMethod method, IProgressMonitor monitor) throws CoreException {
@@ -130,6 +200,22 @@ public class JavaImplementationCodeMining extends AbstractJavaElementLineHeaderC
 		List<IType> list= Stream.of(results).filter(t -> t.getAncestor(IJavaElement.COMPILATION_UNIT) != null).collect(Collectors.toList());
 		long count= list.stream().filter(t -> t.getMethod(method.getElementName(), method.getParameterTypes()).exists()).count();
 		return count;
+	}
+
+	/**
+	 * Return the implementation of a java element method.
+	 *
+	 * @param method the java element method.
+	 * @param monitor the monitor
+	 * @return the implementation for the given java element method.
+	 * @throws CoreException throws when java error
+	 */
+	private static IMethod getMethodImplementation(IMethod method, IProgressMonitor monitor) throws CoreException {
+		IType type= method.getDeclaringType();
+		IType[] results= type.newTypeHierarchy(monitor).getAllSubtypes(type);
+		List<IType> list= Stream.of(results).filter(t -> t.getAncestor(IJavaElement.COMPILATION_UNIT) != null).collect(Collectors.toList());
+		Optional<IType> foundType= list.stream().filter(t -> t.getMethod(method.getElementName(), method.getParameterTypes()).exists()).findFirst();
+		return foundType.get().getMethod(method.getElementName(), method.getParameterTypes());
 	}
 
 }
--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/javaeditor/codemining/JavaReferenceCodeMining.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/javaeditor/codemining/JavaReferenceCodeMining.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018 Angelo Zerr and others.
+ * Copyright (c) 2018, 2019 Angelo Zerr and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -18,17 +18,26 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Consumer;
 
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.MouseEvent;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+
+import org.eclipse.core.resources.ResourcesPlugin;
 
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.ITextViewer;
 import org.eclipse.jface.text.codemining.ICodeMiningProvider;
 
+import org.eclipse.ui.IEditorPart;
+
+import org.eclipse.ui.texteditor.ITextEditor;
+
 import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.core.search.IJavaSearchConstants;
 import org.eclipse.jdt.core.search.IJavaSearchScope;
@@ -40,6 +49,7 @@ import org.eclipse.jdt.core.search.SearchRequestor;
 
 import org.eclipse.jdt.ui.actions.FindReferencesAction;
 
+import org.eclipse.jdt.internal.ui.javaeditor.EditorUtility;
 import org.eclipse.jdt.internal.ui.javaeditor.JavaEditor;
 import org.eclipse.jdt.internal.ui.search.JavaSearchScopeFactory;
 
@@ -70,10 +80,33 @@ public class JavaReferenceCodeMining extends AbstractJavaElementLineHeaderCodeMi
 		return CompletableFuture.runAsync(() -> {
 			try {
 				monitor.isCanceled();
-				IJavaElement element = super.getElement();
+				IJavaElement element= super.getElement();
 				long refCount= countReferences(element, monitor);
 				monitor.isCanceled();
-				action= refCount > 0 ? e -> new FindReferencesAction(editor).run(element) : null;
+				action= refCount > 0 ? e -> {
+					if (refCount == 1 && (e.stateMask & SWT.CTRL) == SWT.CTRL) {
+						// Ctrl + Click is done, open the referenced element in the Java Editor
+						try {
+							SearchMatch match= getReferenceMatch(element, monitor);
+							IJavaElement javaElement= (IJavaElement) match.getElement();
+							IEditorPart part= EditorUtility.openInEditor(javaElement);
+							if (part != null) {
+								EditorUtility.revealInEditor(part, javaElement);
+								if (part instanceof ITextEditor) {
+									ITextEditor textEditor= (ITextEditor) part;
+									textEditor.selectAndReveal(match.getOffset(), match.getLength());
+								}
+							}
+						} catch (JavaModelException e1) {
+							// Should never occur
+						} catch (CoreException e1) {
+							// Should never occur
+						}
+					} else {
+						// Otherwise, launch references search
+						new FindReferencesAction(editor).run(element);
+					}
+				} : null;
 				if (refCount == 0 && showReferencesAtLeastOne) {
 					super.setLabel(""); //$NON-NLS-1$
 				} else {
@@ -129,6 +162,39 @@ public class JavaReferenceCodeMining extends AbstractJavaElementLineHeaderCodeMi
 	}
 
 	/**
+	 * Return the single search match of references for the given java element.
+	 *
+	 * @param element the java element.
+	 * @param monitor the monitor
+	 * @return he number of references for the given java element.
+	 * @throws JavaModelException throws when java error.
+	 * @throws CoreException throws when java error.
+	 */
+	private SearchMatch getReferenceMatch(IJavaElement element, IProgressMonitor monitor)
+			throws JavaModelException, CoreException {
+		if (element == null) {
+			return null;
+		}
+		final SearchMatch[] matches= new SearchMatch[1];
+		SearchPattern pattern= SearchPattern.createPattern(element, IJavaSearchConstants.REFERENCES);
+		SearchEngine engine= new SearchEngine();
+		engine.search(pattern, new SearchParticipant[] { SearchEngine.getDefaultSearchParticipant() },
+				createSourceSearchScope(), new SearchRequestor() {
+
+					@Override
+					public void acceptSearchMatch(final SearchMatch match) throws CoreException {
+						Object o= match.getElement();
+						if (o instanceof IJavaElement
+								&& ((IJavaElement) o).getAncestor(IJavaElement.COMPILATION_UNIT) != null) {
+							matches[0]= match;
+						}
+					}
+				}, monitor);
+
+		return matches[0];
+	}
+
+	/**
 	 * Create Java workspace scope.
 	 *
 	 * @param element IJavaElement to search references for
@@ -141,5 +207,16 @@ public class JavaReferenceCodeMining extends AbstractJavaElementLineHeaderCodeMi
 		boolean isInsideJRE = factory.isInsideJRE(element);
 		IJavaSearchScope scope= factory.createWorkspaceScope(isInsideJRE);
 		return scope;
+	}
+
+	/**
+	 * Create Java source search scope.
+	 *
+	 * @return the Java workspace scope.
+	 * @throws JavaModelException when java error.
+	 */
+	private static IJavaSearchScope createSourceSearchScope() throws JavaModelException {
+		IJavaProject[] projects= JavaCore.create(ResourcesPlugin.getWorkspace().getRoot()).getJavaProjects();
+		return SearchEngine.createJavaSearchScope(projects, IJavaSearchScope.SOURCES);
 	}
 }
