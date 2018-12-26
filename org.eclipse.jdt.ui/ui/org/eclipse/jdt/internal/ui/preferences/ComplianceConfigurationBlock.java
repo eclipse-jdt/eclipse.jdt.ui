fba46fd31cef5f99aa4cf26fbda80d3d72beef6d
Kalyan Prasad Tatavarthi <kalyan_prasad@in.ibm.com>
2018-12-26 11:48:32 +0530

    Bug 543029 - "Disallow identifiers ..." compliance settings wrongly enabled
    Change-Id: I59d6673280ef11aac2a08044fd6195e85e15c3af
    Signed-off-by: Kalyan Prasad Tatavarthi <kalyan_prasad@in.ibm.com>

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/preferences/ComplianceConfigurationBlock.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/preferences/ComplianceConfigurationBlock.java
@@ -651,6 +651,7 @@ public class ComplianceConfigurationBlock extends OptionsConfigurationBlock {
 			updateComplianceEnableState();
 			validateComplianceStatus();
 			updateInlineJSREnableState();
+			updateAssertEnumAsIdentifierEnableState();
 			updateStoreMethodParamNamesEnableState();
 		}
 	}
