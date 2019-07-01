3fc5d9a7a3c8e66824d6b72011c0901c469aa431
Alexander Kurtakov <akurtako@redhat.com>
2019-07-01 22:50:58 +0300

    Remove needless semicolon.
    Change-Id: Ie42c62cadb52c62567899b6edd950730b646dac6
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- org.eclipse.jdt.ui.tests/chkpii/org/eclipse/jdt/ui/tests/chkpii/HTMLTidyTest.java
+++ org.eclipse.jdt.ui.tests/chkpii/org/eclipse/jdt/ui/tests/chkpii/HTMLTidyTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2018 IBM Corporation and others.
+ * Copyright (c) 2008, 2019 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -127,7 +127,7 @@ public class HTMLTidyTest extends TestCase {
 					matchers.add(new StringMatcher(line, true, false));
 				}
 			}
-		};
+		}
 		return matchers.toArray(new StringMatcher[matchers.size()]);
 	}
 
