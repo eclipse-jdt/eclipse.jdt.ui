36a145402ddf82c315351f2edb69c668aa0a8e96
Noopur Gupta <noopur_gupta@in.ibm.com>
2019-09-16 12:37:57 +0530

    Bug 547718: [13] Remove BETA from Compiler Preference page "Compiler compliance level" dropdown
    Change-Id: I4d2858eb30fc7846616593e55ced818f9078fb16

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/preferences/ComplianceConfigurationBlock.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/preferences/ComplianceConfigurationBlock.java
@@ -784,12 +784,12 @@ public class ComplianceConfigurationBlock extends OptionsConfigurationBlock {
 			}
 
 			//TODO: Comment once Java SE 13 has been shipped:
-			String selectedCompliance= getValue(PREF_COMPLIANCE);
+			/*String selectedCompliance= getValue(PREF_COMPLIANCE);
 			if (VERSION_13.equals(selectedCompliance)) {
 				fJRE50InfoText.setText(
 						"This is an implementation of an early-draft specification developed under the Java Community Process (JCP) and is made available for testing and evaluation purposes only. The code is not compatible with any specification of the JCP."); //$NON-NLS-1$
 				isVisible= true;
-			}
+			}*/
 			
 			fJRE50InfoText.setVisible(isVisible);
 			fJRE50InfoImage.setImage(isVisible ? image : null);
