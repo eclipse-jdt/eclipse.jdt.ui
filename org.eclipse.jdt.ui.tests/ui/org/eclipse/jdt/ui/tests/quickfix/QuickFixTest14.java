73e2a326d7e4f577b718d9e98d0d4bfbe331e3bb
Kalyan Prasad Tatavarthi <kalyan_prasad@in.ibm.com>
2020-03-27 10:19:30 +0530

    Bug 553474 - [14] Move QuickFixTest13 test cases to depend on Java 14 and modify accordingly
    Change-Id: I49b72ce08ae25151846f75b802bb5b5c1044ebbc
    Signed-off-by: Kalyan Prasad Tatavarthi <kalyan_prasad@in.ibm.com>

--- org.eclipse.jdt.ui.tests/ui/org/eclipse/jdt/ui/tests/quickfix/QuickFixTest14.java
+++ org.eclipse.jdt.ui.tests/ui/org/eclipse/jdt/ui/tests/quickfix/QuickFixTest14.java
@@ -31,10 +31,14 @@ import org.eclipse.jdt.core.IPackageFragment;
 import org.eclipse.jdt.core.IPackageFragmentRoot;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 
+import org.eclipse.jdt.internal.corext.util.Messages;
+
 import org.eclipse.jdt.ui.tests.core.rules.Java14ProjectTestSetup;
 import org.eclipse.jdt.ui.tests.core.rules.ProjectTestSetup;
 import org.eclipse.jdt.ui.text.java.IJavaCompletionProposal;
 import org.eclipse.jdt.ui.text.java.correction.CUCorrectionProposal;
+
+import org.eclipse.jdt.internal.ui.text.correction.CorrectionMessages;
 
 @RunWith(JUnit4.class)
 public class QuickFixTest14 extends QuickFixTest {
@@ -55,129 +59,69 @@ public class QuickFixTest14 extends QuickFixTest {
 		}
 	}
 
-//	@Test
-//	public void testEnablePreviewsAndOpenCompilerPropertiesProposals() throws Exception {
-//		fJProject1= JavaProjectHelper.createJavaProject("TestProject1", "bin");
-//		fJProject1.setRawClasspath(Java14ProjectTestSetup.getDefaultClasspath(), null);
-//		JavaProjectHelper.set13CompilerOptions(fJProject1, false);
-//
-//		fSourceFolder= JavaProjectHelper.addSourceContainer(fJProject1, "src");
-//
-//
-//		StringBuffer buf= new StringBuffer();
-//		buf.append("module test {\n");
-//		buf.append("}\n");
-//		IPackageFragment def= fSourceFolder.createPackageFragment("", false, null);
-//		def.createCompilationUnit("module-info.java", buf.toString(), false, null);
-//
-//		IPackageFragment pack= fSourceFolder.createPackageFragment("test", false, null);
-//		buf= new StringBuffer();
-//		buf.append("package test;\n");
-//		buf.append("public class Cls {\n");
-//		buf.append("  String foo(Day day) {\n");
-//		buf.append("	int x = 0;\n");
-//		buf.append("	var today = switch(day){\n");
-//		buf.append("		case SATURDAY, SUNDAY: yield \"Weekend day\";\n");
-//		buf.append("		case MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY: {\n");
-//		buf.append("   	 		var kind = \"Working day\";\n");
-//		buf.append("    		yield kind;\n");
-//		buf.append("		}\n");
-//		buf.append("		default: {\n");
-//		buf.append("    		var kind = day.name();\n");
-//		buf.append("   	 		System.out.println(kind + x);\n");
-//		buf.append("   	 		throw new IllegalArgumentException(\"Invalid day: \" + kind);\n");
-//		buf.append("		}\n");
-//		buf.append("  	};\n");
-//		buf.append("  	return today;\n");
-//		buf.append("  }\n");
-//		buf.append("}\n");
-//		ICompilationUnit cu= pack.createCompilationUnit("Cls.java", buf.toString(), false, null);
-//
-//		buf= new StringBuffer();
-//		buf= new StringBuffer();
-//		buf.append("package test;\n");
-//		buf.append("public enum Day {\n");
-//		buf.append("	SUNDAY,\n");
-//		buf.append("	MONDAY,\n");
-//		buf.append("	TUESDAY,\n");
-//		buf.append("	WEDNESDAY,\n");
-//		buf.append("	THURSDAY,\n");
-//		buf.append("	FRIDAY,\n");
-//		buf.append("	SATURDAY\n");
-//		buf.append("}\n");
-//		pack.createCompilationUnit("Day.java", buf.toString(), false, null);
-//
-//		CompilationUnit astRoot= getASTRoot(cu);
-//		ArrayList<IJavaCompletionProposal> proposals= collectCorrections(cu, astRoot, 11, null);
-//
-//		assertNumberOfProposals(proposals, 2);
-//		String label1= CorrectionMessages.PreviewFeaturesSubProcessor_enable_preview_features;
-//		assertProposalExists(proposals, label1);
-//		String label2= CorrectionMessages.PreviewFeaturesSubProcessor_open_compliance_properties_page_enable_preview_features;
-//		assertProposalExists(proposals, label2);
-//	}
-//
-//	@Test
-//	public void testGetNeedHigherComplianceProposalsAndEnablePreviewsProposal() throws Exception {
-//		fJProject1= JavaProjectHelper.createJavaProject("TestProject1", "bin");
-//		fJProject1.setRawClasspath(Java10ProjectTestSetup.getDefaultClasspath(), null);
-//		JavaProjectHelper.set12CompilerOptions(fJProject1, false);
-//
-//		fSourceFolder= JavaProjectHelper.addSourceContainer(fJProject1, "src");
-//
-//
-//		StringBuffer buf= new StringBuffer();
-//		buf.append("module test {\n");
-//		buf.append("}\n");
-//		IPackageFragment def= fSourceFolder.createPackageFragment("", false, null);
-//		def.createCompilationUnit("module-info.java", buf.toString(), false, null);
-//
-//		IPackageFragment pack= fSourceFolder.createPackageFragment("test", false, null);
-//		buf= new StringBuffer();
-//		buf.append("package test;\n");
-//		buf.append("public class Cls {\n");
-//		buf.append("  String foo(Day day) {\n");
-//		buf.append("	int x = 0;\n");
-//		buf.append("	var today = switch(day){\n");
-//		buf.append("		case SATURDAY, SUNDAY: yield \"Weekend day\";\n");
-//		buf.append("		case MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY: {\n");
-//		buf.append("   	 		var kind = \"Working day\";\n");
-//		buf.append("    		yield kind;\n");
-//		buf.append("		}\n");
-//		buf.append("		default: {\n");
-//		buf.append("    		var kind = day.name();\n");
-//		buf.append("   	 		System.out.println(kind + x);\n");
-//		buf.append("   	 		throw new IllegalArgumentException(\"Invalid day: \" + kind);\n");
-//		buf.append("		}\n");
-//		buf.append("  	};\n");
-//		buf.append("  	return today;\n");
-//		buf.append("  }\n");
-//		buf.append("}\n");
-//		ICompilationUnit cu= pack.createCompilationUnit("Cls.java", buf.toString(), false, null);
-//
-//		buf= new StringBuffer();
-//		buf= new StringBuffer();
-//		buf.append("package test;\n");
-//		buf.append("public enum Day {\n");
-//		buf.append("	SUNDAY,\n");
-//		buf.append("	MONDAY,\n");
-//		buf.append("	TUESDAY,\n");
-//		buf.append("	WEDNESDAY,\n");
-//		buf.append("	THURSDAY,\n");
-//		buf.append("	FRIDAY,\n");
-//		buf.append("	SATURDAY\n");
-//		buf.append("}\n");
-//		pack.createCompilationUnit("Day.java", buf.toString(), false, null);
-//
-//		CompilationUnit astRoot= getASTRoot(cu);
-//		ArrayList<IJavaCompletionProposal> proposals= collectCorrections(cu, astRoot, 10, null);
-//
-//		assertNumberOfProposals(proposals, 1);
-//		String label1= Messages.format(CorrectionMessages.ReorgCorrectionsSubProcessor_change_project_compliance_description, "13");
-//		String label2= CorrectionMessages.PreviewFeaturesSubProcessor_enable_preview_features;
-//		String label= Messages.format(CorrectionMessages.ReorgCorrectionsSubProcessor_combine_two_quickfixes, new String[] {label1, label2});
-//		assertProposalExists(proposals, label);
-//	}
+	@Test
+	public void testEnablePreviewsAndOpenCompilerPropertiesProposals() throws Exception {
+		fJProject1= JavaProjectHelper.createJavaProject("TestProject1", "bin");
+		fJProject1.setRawClasspath(Java14ProjectTestSetup.getDefaultClasspath(), null);
+		JavaProjectHelper.set14CompilerOptions(fJProject1, false);
+
+		fSourceFolder= JavaProjectHelper.addSourceContainer(fJProject1, "src");
+
+
+		StringBuffer buf= new StringBuffer();
+		buf.append("module test {\n");
+		buf.append("}\n");
+		IPackageFragment def= fSourceFolder.createPackageFragment("", false, null);
+		def.createCompilationUnit("module-info.java", buf.toString(), false, null);
+
+		IPackageFragment pack= fSourceFolder.createPackageFragment("test", false, null);
+		buf= new StringBuffer();
+		buf.append("package test;\n");
+		buf.append("public record Rec1() {\n");
+		buf.append("}\n");
+		ICompilationUnit cu= pack.createCompilationUnit("Rec1.java", buf.toString(), false, null);
+
+		CompilationUnit astRoot= getASTRoot(cu);
+		ArrayList<IJavaCompletionProposal> proposals= collectCorrections(cu, astRoot, 1, null);
+
+		assertNumberOfProposals(proposals, 2);
+		String label1= CorrectionMessages.PreviewFeaturesSubProcessor_enable_preview_features;
+		assertProposalExists(proposals, label1);
+		String label2= CorrectionMessages.PreviewFeaturesSubProcessor_open_compliance_properties_page_enable_preview_features;
+		assertProposalExists(proposals, label2);
+	}
+
+	@Test
+	public void testGetNeedHigherComplianceProposalsAndEnablePreviewsProposal() throws Exception {
+		fJProject1= JavaProjectHelper.createJavaProject("TestProject1", "bin");
+		fJProject1.setRawClasspath(Java14ProjectTestSetup.getDefaultClasspath(), null);
+		JavaProjectHelper.set13CompilerOptions(fJProject1, false);
+
+		fSourceFolder= JavaProjectHelper.addSourceContainer(fJProject1, "src");
+
+
+		StringBuffer buf= new StringBuffer();
+		buf.append("module test {\n");
+		buf.append("}\n");
+		IPackageFragment def= fSourceFolder.createPackageFragment("", false, null);
+		def.createCompilationUnit("module-info.java", buf.toString(), false, null);
+
+		IPackageFragment pack= fSourceFolder.createPackageFragment("test", false, null);
+		buf= new StringBuffer();
+		buf.append("package test;\n");
+		buf.append("public record Rec1() {\n");
+		buf.append("}\n");
+		ICompilationUnit cu= pack.createCompilationUnit("Rec1.java", buf.toString(), false, null);
+
+		CompilationUnit astRoot= getASTRoot(cu);
+		ArrayList<IJavaCompletionProposal> proposals= collectCorrections(cu, astRoot, 1, null);
+
+		assertNumberOfProposals(proposals, 1);
+		String label1= Messages.format(CorrectionMessages.ReorgCorrectionsSubProcessor_change_project_compliance_description, "14");
+		String label2= CorrectionMessages.PreviewFeaturesSubProcessor_enable_preview_features;
+		String label= Messages.format(CorrectionMessages.ReorgCorrectionsSubProcessor_combine_two_quickfixes, new String[] {label1, label2});
+		assertProposalExists(proposals, label);
+	}
 
 	@Test
 	public void testNoEnablePreviewProposal() throws Exception {
