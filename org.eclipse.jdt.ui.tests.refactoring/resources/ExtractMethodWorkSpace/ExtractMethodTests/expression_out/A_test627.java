245ce1ca31cac6dd28ce848490ed61cd85c5ef5d
Fabrice Tiercelin <fabrice.tiercelin@yahoo.fr>
2020-05-19 06:39:36 +0200

    Bug 563315 - [extract method] Clean all useless parenthesis
    Change-Id: I2575b055dc5a6ca985520ccfae72f58b10a705a8
    Signed-off-by: Fabrice Tiercelin <fabrice.tiercelin@yahoo.fr>

--- org.eclipse.jdt.ui.tests.refactoring/resources/ExtractMethodWorkSpace/ExtractMethodTests/expression_out/A_test627.java
+++ org.eclipse.jdt.ui.tests.refactoring/resources/ExtractMethodWorkSpace/ExtractMethodTests/expression_out/A_test627.java
@@ -3,8 +3,8 @@ package expression_out;
 public class A_test627 {
 
 	public void foo() {
-		int i= 1 - (extracted());
-		int j= 1 - (extracted());
+		int i= 1 - extracted();
+		int j= 1 - extracted();
 	}
 
 	protected int extracted() {
