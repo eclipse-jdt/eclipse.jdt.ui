85412f19c7c6308ff4db072789f517f65924bfe5
Carsten Hammer <carsten.hammer@t-online.de>
2020-09-02 18:39:42 +0200

    Bug 566614 - don't create var just to throw it away
    Change-Id: I081b5fbcc2d79345911b37ff613e365e7934fcdc
    Signed-off-by: Carsten Hammer <carsten.hammer@t-online.de>

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/javadocexport/RecentSettingsStore.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/javadocexport/RecentSettingsStore.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2017 IBM Corporation and others.
+ * Copyright (c) 2000, 2020 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -141,6 +141,7 @@ public class RecentSettingsStore {
 		ProjectData data= fPerProjectSettings.get(project);
 		if (data == null) {
 			data= new ProjectData();
+			fPerProjectSettings.put(project, data);
 		}
 		data.setDestination(destination);
 		data.setAntpath(antpath);
