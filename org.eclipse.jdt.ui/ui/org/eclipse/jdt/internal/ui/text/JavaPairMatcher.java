eclipse-jdt/eclipse.jdt.ui

commit c1d332e75bb971450ba5e1faa3c0af77e4cc8125

angular brackets for 1.5 source: - highlighting - auto-closing - double clicking

org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/JavaPairMatcher.java
@@ -18,6 +18,8 @@ import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.Region;
 import org.eclipse.jface.text.source.ICharacterPairMatcher;
 
+import org.eclipse.jdt.core.JavaCore;
+
 /**
  * JavaPairMatcher.java
  */
@@ -35,6 +37,11 @@ public class JavaPairMatcher implements ICharacterPairMatcher {
 	protected int fAnchor;
 	
 	protected JavaCodeReader fReader= new JavaCodeReader();
+	/**
+	 * Stores the source version state.
+	 * @since 3.1
+	 */
+	private boolean fHighlightAngularBrackets= false;
 	
 	
 	public JavaPairMatcher(char[] pairs) {
@@ -152,6 +159,8 @@ public class JavaPairMatcher implements ICharacterPairMatcher {
 	}
 	
 	protected int searchForClosingPeer(int offset, int openingPeer, int closingPeer, IDocument document) throws IOException {
+		if (openingPeer == '<' && !(fHighlightAngularBrackets && isTypeParameterBracket(offset, document)))
+			return -1;
 		
 		fReader.configureForwardReader(document, offset + 1, document.getLength(), true, true);
 		
@@ -172,7 +181,10 @@ public class JavaPairMatcher implements ICharacterPairMatcher {
 		return  -1;
 	}
 	
+	
 	protected int searchForOpeningPeer(int offset, int openingPeer, int closingPeer, IDocument document) throws IOException {
+		if (openingPeer == '<' && !fHighlightAngularBrackets)
+			return -1;
 		
 		fReader.configureBackwardReader(document, offset, true, true);
 		
@@ -184,12 +196,93 @@ public class JavaPairMatcher implements ICharacterPairMatcher {
 			else if (c == openingPeer)
 				stack--;
 				
-			if (stack == 0)
+			if (stack == 0) {
+				if (closingPeer == '>' && !isTypeParameterBracket(fReader.getOffset(), document))
+					return -1;
 				return fReader.getOffset();
+			}
 				
 			c= fReader.read();
 		}
 		
 		return -1;
+	}
+	
+	/**
+	 * Checks if the angular bracket at <code>offset</code> is a type
+	 * parameter bracket.
+	 * 
+	 * @param offset the offset of the opening bracket
+	 * @param document the document
+	 * @return <code>true</code> if the bracket is part of a type parameter,
+	 *         <code>false</code> otherwise
+	 * @since 3.1
+	 */
+	private boolean isTypeParameterBracket(int offset, IDocument document) {
+		/* 
+		 * type parameter come after braces (closing or opening), semicolons, or after
+		 * a Type name (heuristic: starts with capital character, or after a modifier
+		 * keyword in a method declaration (visibility, static, synchronized, final) 
+		 */
+		
+		try {
+			IRegion line= document.getLineInformationOfOffset(offset);
+			
+			JavaHeuristicScanner scanner= new JavaHeuristicScanner(document);
+			int prevToken= scanner.previousToken(offset - 1, line.getOffset());
+			int prevTokenOffset= scanner.getPosition() + 1;
+			String previous= prevToken == Symbols.TokenEOF ? null : document.get(prevTokenOffset, offset - prevTokenOffset).trim();
+			
+			if (	   prevToken == Symbols.TokenLBRACE	
+					|| prevToken == Symbols.TokenRBRACE 
+					|| prevToken == Symbols.TokenSEMICOLON
+					|| prevToken == Symbols.TokenSYNCHRONIZED
+					|| prevToken == Symbols.TokenSTATIC
+					|| (prevToken == Symbols.TokenIDENT && isTypeParameterIntroducer(previous))
+					|| prevToken == Symbols.TokenEOF)
+				return true;
+		} catch (BadLocationException e) {
+			return false;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Returns <code>true</code> if <code>identifier</code> is an identifier
+	 * that could come right before a type parameter list. It uses a heuristic:
+	 * if the identifier starts with an upper case, it is assumed a type name.
+	 * Also, if <code>identifier</code> is a method modifier, it is assumed
+	 * that the angular bracket is part of the generic type parameter of a
+	 * method.
+	 * 
+	 * @param identifier the identifier to check
+	 * @return <code>true</code> if the identifier could introduce a type
+	 *         parameter list
+	 * @since 3.1
+	 */
+	private boolean isTypeParameterIntroducer(String identifier) {
+		return identifier.length() > 0
+				&& (Character.isUpperCase(identifier.charAt(0)) 
+						|| identifier.startsWith("final") //$NON-NLS-1$
+						|| identifier.startsWith("public") //$NON-NLS-1$
+						|| identifier.startsWith("public") //$NON-NLS-1$
+						|| identifier.startsWith("protected") //$NON-NLS-1$
+						|| identifier.startsWith("private")); //$NON-NLS-1$
+	}
+	
+	/**
+	 * Sets the source version to one of the <code>JavaCore.VERSION_</code>
+	 * identifiers. Versions greater than {@link JavaCore#VERSION_1_4} have
+	 * angular bracket highlighting.
+	 * 
+	 * @param version the new version
+	 * @since 3.1
+	 */
+	public void setVersion(String version) {
+		if (JavaCore.VERSION_1_5.compareTo(version) <= 0)
+			fHighlightAngularBrackets= true;
+		else
+			fHighlightAngularBrackets= false;
 	}
 }

org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/javaeditor/CompilationUnitEditor.java
@@ -122,7 +122,9 @@ import org.eclipse.jdt.internal.ui.actions.RemoveBlockCommentAction;
 import org.eclipse.jdt.internal.ui.compare.LocalHistoryActionGroup;
 import org.eclipse.jdt.internal.ui.text.ContentAssistPreference;
 import org.eclipse.jdt.internal.ui.text.IJavaPartitions;
+import org.eclipse.jdt.internal.ui.text.JavaHeuristicScanner;
 import org.eclipse.jdt.internal.ui.text.SmartBackspaceManager;
+import org.eclipse.jdt.internal.ui.text.Symbols;
 import org.eclipse.jdt.internal.ui.text.comment.CommentFormattingContext;
 import org.eclipse.jdt.internal.ui.text.correction.JavaCorrectionAssistant;
 import org.eclipse.jdt.internal.ui.text.java.IJavaReconcilingListener;
@@ -548,57 +550,14 @@ public class CompilationUnitEditor extends JavaEditor implements IJavaReconcilin
 			fCloseAngularBrackets= enabled;
 		}
 
-		private boolean hasIdentifierToTheRight(IDocument document, int offset) {
-			try {
-				int end= offset;
-				IRegion endLine= document.getLineInformationOfOffset(end);
-				int maxEnd= endLine.getOffset() + endLine.getLength();
-				while (end != maxEnd && Character.isWhitespace(document.getChar(end)))
-					++end;
-
-				return end != maxEnd && Character.isJavaIdentifierPart(document.getChar(end));
-
-			} catch (BadLocationException e) {
-				// be conservative
-				return true;
-			}
-		}
-
-		private boolean hasIdentifierToTheLeft(IDocument document, int offset, boolean onlyUpperCase) {
-			try {
-				int start= offset;
-				IRegion startLine= document.getLineInformationOfOffset(start);
-				int minStart= startLine.getOffset();
-				while (start != minStart && Character.isWhitespace(document.getChar(start - 1)))
-					--start;
-				
-				while (start != minStart && Character.isJavaIdentifierPart(document.getChar(start - 1)))
-					--start;
-				
-				return start != minStart 
-						&& Character.isJavaIdentifierPart(document.getChar(start))
-						&& (!onlyUpperCase || Character.isUpperCase(document.getChar(start)));
-
-			} catch (BadLocationException e) {
-				return true;
-			}			
-		}
-
-		private boolean hasCharacterToTheRight(IDocument document, int offset, char character) {
-			try {
-				int end= offset;
-				IRegion endLine= document.getLineInformationOfOffset(end);
-				int maxEnd= endLine.getOffset() + endLine.getLength();
-				while (end != maxEnd && Character.isWhitespace(document.getChar(end)))
-					++end;
-				
-				return end != maxEnd && document.getChar(end) == character;
-
-
-			} catch (BadLocationException e) {
-				// be conservative
-				return true;
-			}			
+		private boolean isAngularIntroducer(String identifier) {
+			return identifier.length() > 0
+					&& (Character.isUpperCase(identifier.charAt(0)) 
+							|| identifier.startsWith("final") //$NON-NLS-1$
+							|| identifier.startsWith("public") //$NON-NLS-1$
+							|| identifier.startsWith("public") //$NON-NLS-1$
+							|| identifier.startsWith("protected") //$NON-NLS-1$
+							|| identifier.startsWith("private")); //$NON-NLS-1$
 		}
 		
 		/*
@@ -606,8 +565,19 @@ public class CompilationUnitEditor extends JavaEditor implements IJavaReconcilin
 		 */
 		public void verifyKey(VerifyEvent event) {			
 
+			// early pruning to slow down normal typing as little as possible
 			if (!event.doit || getInsertMode() != SMART_INSERT)
 				return;
+			switch (event.character) {
+				case '(':
+				case '<':
+				case '[':
+				case '\'':
+				case '\"':
+					break;
+				default:
+					return;
+			}
 				
 			final ISourceViewer sourceViewer= getSourceViewer();
 			IDocument document= sourceViewer.getDocument();
@@ -615,52 +585,61 @@ public class CompilationUnitEditor extends JavaEditor implements IJavaReconcilin
 			final Point selection= sourceViewer.getSelectedRange();
 			final int offset= selection.x;
 			final int length= selection.y;
-
-			switch (event.character) {
-			case '(':
-				if (hasCharacterToTheRight(document, offset + length, '('))
-					return;
-				if (hasIdentifierToTheRight(document, offset + length))
-					return;
-				break;
-
-			case '<':
-				if (!fCloseAngularBrackets)
-					return;
-				if (hasCharacterToTheRight(document, offset + length, '<'))
-					return;
-				if (hasIdentifierToTheRight(document, offset + length))
-					return;
-				if (!hasIdentifierToTheLeft(document, offset, true))
-					return;
-				break;
-
-			case '[':
-				if (!fCloseBrackets)
-					return;
-				if (hasIdentifierToTheRight(document, offset + length))
-					return;
-				break;
 			
-			case '\'':
-				if (!fCloseStrings)
-					return;
-				if (hasIdentifierToTheLeft(document, offset, false) || hasIdentifierToTheRight(document, offset + length))
-					return;
-				break;
-
-			case '"':
-				if (!fCloseStrings)
-					return;
-				if (hasIdentifierToTheLeft(document, offset, false) || hasIdentifierToTheRight(document, offset + length))
-					return;
-				break;
-			
-			default:
-				return;
-			}
-			
-			try {		
+			try {
+				IRegion startLine= document.getLineInformationOfOffset(offset);
+				IRegion endLine= document.getLineInformationOfOffset(offset + length);
+				
+				JavaHeuristicScanner scanner= new JavaHeuristicScanner(document);
+				int nextToken= scanner.nextToken(offset + length, endLine.getOffset() + endLine.getLength());
+				String next= nextToken == Symbols.TokenEOF ? null : document.get(offset, scanner.getPosition() - offset).trim();
+				int prevToken= scanner.previousToken(offset - 1, startLine.getOffset());
+				int prevTokenOffset= scanner.getPosition() + 1;
+				String previous= prevToken == Symbols.TokenEOF ? null : document.get(prevTokenOffset, offset - prevTokenOffset).trim();
+				
+				switch (event.character) {
+					case '(':
+						if (nextToken == Symbols.TokenLPAREN
+								|| nextToken == Symbols.TokenIDENT 
+								|| next != null && next.length() > 1)
+							return;
+						break;
+						
+					case '<':
+						if (!fCloseAngularBrackets
+								|| nextToken == Symbols.TokenLESSTHAN 
+								|| nextToken == Symbols.TokenIDENT
+								|| 		   prevToken != Symbols.TokenLBRACE	
+										&& prevToken != Symbols.TokenRBRACE 
+										&& prevToken != Symbols.TokenSEMICOLON
+										&& prevToken != Symbols.TokenSYNCHRONIZED
+										&& prevToken != Symbols.TokenSTATIC
+										&& (prevToken != Symbols.TokenIDENT || !isAngularIntroducer(previous))
+										&& prevToken != Symbols.TokenEOF)
+							return;
+						break;
+						
+					case '[':
+						if (!fCloseBrackets
+								|| nextToken == Symbols.TokenIDENT 
+								|| next != null && next.length() > 1)
+							return;
+						break;
+						
+					case '\'':
+					case '"':
+						if (!fCloseStrings
+								|| nextToken == Symbols.TokenIDENT 
+								|| prevToken == Symbols.TokenIDENT
+								|| next != null && next.length() > 1
+								|| previous != null && previous.length() > 1)
+							return;
+						break;
+						
+					default:
+						return;
+				}
+				
 				ITypedRegion partition= TextUtilities.getPartition(document, IJavaPartitions.JAVA_PARTITIONING, offset, true);
 				if (!IDocument.DEFAULT_CONTENT_TYPE.equals(partition.getType()))
 					return;
@@ -1192,13 +1171,13 @@ public class CompilationUnitEditor extends JavaEditor implements IJavaReconcilin
 		WorkbenchHelp.setHelp(action, IJavaHelpContextIds.QUICK_FIX_ACTION);
 
 		action= new ContentAssistAction(JavaEditorMessages.getResourceBundle(), "ContentAssistProposal.", this); //$NON-NLS-1$
-		action.setActionDefinitionId(IJavaEditorActionDefinitionIds.CONTENT_ASSIST_PROPOSALS);		
+		action.setActionDefinitionId(ITextEditorActionDefinitionIds.CONTENT_ASSIST_PROPOSALS);		
 		setAction("ContentAssistProposal", action); //$NON-NLS-1$
 		markAsStateDependentAction("ContentAssistProposal", true); //$NON-NLS-1$
 		WorkbenchHelp.setHelp(action, IJavaHelpContextIds.CONTENT_ASSIST_ACTION);
 
 		action= new TextOperationAction(JavaEditorMessages.getResourceBundle(), "ContentAssistContextInformation.", this, ISourceViewer.CONTENTASSIST_CONTEXT_INFORMATION);	//$NON-NLS-1$
-		action.setActionDefinitionId(IJavaEditorActionDefinitionIds.CONTENT_ASSIST_CONTEXT_INFORMATION);		
+		action.setActionDefinitionId(ITextEditorActionDefinitionIds.CONTENT_ASSIST_CONTEXT_INFORMATION);		
 		setAction("ContentAssistContextInformation", action); //$NON-NLS-1$
 		markAsStateDependentAction("ContentAssistContextInformation", true); //$NON-NLS-1$
 		WorkbenchHelp.setHelp(action, IJavaHelpContextIds.PARAMETER_HINTS_ACTION);
@@ -1619,7 +1598,7 @@ public class CompilationUnitEditor extends JavaEditor implements IJavaReconcilin
 		IPreferenceStore preferenceStore= getPreferenceStore();
 		boolean closeBrackets= preferenceStore.getBoolean(CLOSE_BRACKETS);
 		boolean closeStrings= preferenceStore.getBoolean(CLOSE_STRINGS);
-		boolean closeAngularBrackets= JavaCore.VERSION_1_5.equals(preferenceStore.getString(JavaCore.COMPILER_SOURCE));
+		boolean closeAngularBrackets= JavaCore.VERSION_1_5.compareTo(preferenceStore.getString(JavaCore.COMPILER_SOURCE)) <= 0;
 		
 		fBracketInserter.setCloseBracketsEnabled(closeBrackets);
 		fBracketInserter.setCloseStringsEnabled(closeStrings);
@@ -1695,7 +1674,7 @@ public class CompilationUnitEditor extends JavaEditor implements IJavaReconcilin
 				}
 				
 				if (JavaCore.COMPILER_SOURCE.equals(p)) {
-					boolean closeAngularBrackets= JavaCore.VERSION_1_5.equals(getPreferenceStore().getString(p));
+					boolean closeAngularBrackets= JavaCore.VERSION_1_5.compareTo(getPreferenceStore().getString(p)) <= 0;
 					fBracketInserter.setCloseAngularBracketsEnabled(closeAngularBrackets);
 				}
 								

org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/javaeditor/JavaEditor.java
@@ -119,6 +119,7 @@ import org.eclipse.jface.text.source.AnnotationRulerColumn;
 import org.eclipse.jface.text.source.CompositeRuler;
 import org.eclipse.jface.text.source.IAnnotationModel;
 import org.eclipse.jface.text.source.IAnnotationModelExtension;
+import org.eclipse.jface.text.source.ICharacterPairMatcher;
 import org.eclipse.jface.text.source.IOverviewRuler;
 import org.eclipse.jface.text.source.ISourceViewer;
 import org.eclipse.jface.text.source.ISourceViewerExtension2;
@@ -2184,7 +2185,7 @@ public abstract class JavaEditor extends AbstractDecoratedTextEditor implements 
 	 */
 	private final static String BROWSER_LIKE_LINKS_KEY_MODIFIER_MASK= PreferenceConstants.EDITOR_BROWSER_LIKE_LINKS_KEY_MODIFIER_MASK;
 	
-	protected final static char[] BRACKETS= { '{', '}', '(', ')', '[', ']' };
+	protected final static char[] BRACKETS= { '{', '}', '(', ')', '[', ']', '<', '>' };
 
 	/** The outline page */
 	protected JavaOutlinePage fOutlinePage;
@@ -3171,6 +3172,12 @@ public abstract class JavaEditor extends AbstractDecoratedTextEditor implements 
 				return;
 			}
 			
+			if (JavaCore.COMPILER_SOURCE.equals(property)) {
+				if (event.getNewValue() instanceof String)
+					fBracketMatcher.setVersion((String) event.getNewValue());
+				// fall through as others are interested in source change as well.
+			}
+			
 			((JavaSourceViewerConfiguration)getSourceViewerConfiguration()).handlePropertyChangeEvent(event);
 			
 			if (affectsOverrideIndicatorAnnotations(event)) {
@@ -3453,6 +3460,7 @@ public abstract class JavaEditor extends AbstractDecoratedTextEditor implements 
 	
 	protected void configureSourceViewerDecorationSupport(SourceViewerDecorationSupport support) {
 		
+		fBracketMatcher.setVersion(getPreferenceStore().getString(JavaCore.COMPILER_SOURCE));
 		support.setCharacterPairMatcher(fBracketMatcher);
 		support.setMatchingCharacterPainterPreferenceKeys(MATCHING_BRACKETS, MATCHING_BRACKETS_COLOR);
 		
@@ -4017,7 +4025,7 @@ public abstract class JavaEditor extends AbstractDecoratedTextEditor implements 
 			
 		int anchor= fBracketMatcher.getAnchor();
 		// http://dev.eclipse.org/bugs/show_bug.cgi?id=34195
-		int targetOffset= (JavaPairMatcher.RIGHT == anchor) ? offset + 1: offset + length;
+		int targetOffset= (ICharacterPairMatcher.RIGHT == anchor) ? offset + 1: offset + length;
 		
 		boolean visible= false;
 		if (sourceViewer instanceof ITextViewerExtension5) {

org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/Symbols.java
@@ -29,6 +29,8 @@ public interface Symbols {
 	int TokenQUESTIONMARK= 10;
 	int TokenCOMMA= 11;
 	int TokenEQUAL= 12;
+	int TokenLESSTHAN= 13;
+	int TokenGREATERTHAN= 14;
 	int TokenIF= 109;
 	int TokenDO= 1010;
 	int TokenFOR= 1011;

org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/java/JavaDoubleClickSelector.java
@@ -11,12 +11,15 @@
 package org.eclipse.jdt.internal.ui.text.java;
 
 
-import org.eclipse.jdt.internal.ui.text.JavaPairMatcher;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.ITextDoubleClickStrategy;
 import org.eclipse.jface.text.ITextViewer;
+
+import org.eclipse.jdt.core.JavaCore;
+
+import org.eclipse.jdt.internal.ui.text.JavaPairMatcher;
 
 /**
  * Double click strategy aware of Java identifier syntax rules.
@@ -24,7 +27,7 @@ import org.eclipse.jface.text.ITextViewer;
 public class JavaDoubleClickSelector implements ITextDoubleClickStrategy {
 	
 	
-	protected static final char[] BRACKETS= {'{', '}', '(', ')', '[', ']' };
+	protected static final char[] BRACKETS= {'{', '}', '(', ')', '[', ']', '<', '>' };
 	
 	protected JavaPairMatcher fPairMatcher= new JavaPairMatcher(BRACKETS);
 		
@@ -87,5 +90,18 @@ public class JavaDoubleClickSelector implements ITextDoubleClickStrategy {
 			
 		} catch (BadLocationException x) {
 		}
+	}
+	
+	/**
+	 * Sets the source version to one of the <code>JavaCore.VERSION_</code>
+	 * identifiers. Versions greater than {@link JavaCore#VERSION_1_4} have
+	 * angular bracket highlighting.
+	 * 
+	 * @param version the new version
+	 * @since 3.1
+	 * @see JavaPairMatcher#setVersion(String)
+	 */
+	public void setVersion(String version) {
+		fPairMatcher.setVersion(version);
 	}
 }

org.eclipse.jdt.ui/ui/org/eclipse/jdt/ui/text/JavaSourceViewerConfiguration.java
@@ -55,6 +55,8 @@ import org.eclipse.ui.texteditor.AbstractDecoratedTextEditorPreferenceConstants;
 import org.eclipse.ui.texteditor.ChainedPreferenceStore;
 import org.eclipse.ui.texteditor.ITextEditor;
 
+import org.eclipse.jdt.core.JavaCore;
+
 import org.eclipse.jdt.ui.PreferenceConstants;
 import org.eclipse.jdt.ui.actions.IJavaEditorActionDefinitionIds;
 
@@ -160,6 +162,11 @@ public class JavaSourceViewerConfiguration extends SourceViewerConfiguration {
 	 * @since 3.0
 	 */
 	private IColorManager fColorManager;
+	/**
+	 * The double click strategy.
+	 * @since 3.1
+	 */
+	private JavaDoubleClickSelector fJavaDoubleClickSelector;
 	
 	/**
 	 * Creates a new Java source viewer configuration for viewers in the given editor 
@@ -446,7 +453,11 @@ public class JavaSourceViewerConfiguration extends SourceViewerConfiguration {
 		else if (IJavaPartitions.JAVA_STRING.equals(contentType) ||
 				IJavaPartitions.JAVA_CHARACTER.equals(contentType))
 			return new JavaStringDoubleClickSelector(getConfiguredDocumentPartitioning(sourceViewer));
-		return new JavaDoubleClickSelector();
+		if (fJavaDoubleClickSelector == null) {
+			fJavaDoubleClickSelector= new JavaDoubleClickSelector();
+			fJavaDoubleClickSelector.setVersion(fPreferenceStore.getString(JavaCore.COMPILER_SOURCE));
+		}
+		return fJavaDoubleClickSelector;
 	}
 
 	/*
@@ -797,5 +808,8 @@ public class JavaSourceViewerConfiguration extends SourceViewerConfiguration {
 			fStringScanner.adaptToPreferenceChange(event);
 		if (fJavaDocScanner.affectsBehavior(event))
 			fJavaDocScanner.adaptToPreferenceChange(event);
+		if (JavaCore.COMPILER_SOURCE.equals(event.getProperty()))
+			if (event.getNewValue() instanceof String)
+				fJavaDoubleClickSelector.setVersion((String) event.getNewValue());
 	}
 }

commit 0a15452b665ed884389344ef331d04ed74ca45df

Bug 566510 - Remove duplicated conditions
Introduced in
commit c1d332e75bb971450ba5e1faa3c0af77e4cc8125
Author: teicher <teicher> 2004-09-20 17:26:36

Change-Id: I6507ca06ca2dc2538f52036b3127423680758ddd
Signed-off-by: Carsten Hammer <carsten.hammer@t-online.de>

org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/JavaPairMatcher.java
@@ -212,7 +212,6 @@ public final class JavaPairMatcher extends DefaultCharacterPairMatcher implement
 				&& (Character.isUpperCase(identifier.charAt(0))
 						|| identifier.startsWith("final") //$NON-NLS-1$
 						|| identifier.startsWith("public") //$NON-NLS-1$
-						|| identifier.startsWith("public") //$NON-NLS-1$
 						|| identifier.startsWith("protected") //$NON-NLS-1$
 						|| identifier.startsWith("private")); //$NON-NLS-1$
 	}