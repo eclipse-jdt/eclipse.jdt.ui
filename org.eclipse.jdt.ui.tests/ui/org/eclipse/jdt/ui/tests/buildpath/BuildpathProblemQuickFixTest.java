9fe1cb257c39faae6bc498dbdad1e399fab5e76d
Carsten Hammer <carsten.hammer@t-online.de>
2020-09-26 15:09:18 +0200

    Bug 567486 - Primitive is boxed to call Integer.compareTo(Integer)
    Bug: Primitive is boxed to call Integer.compareTo(Integer): use
    Integer.compare(int, int) instead
    
    A boxed primitive is created just to call compareTo method. It's more
    efficient to use static compare method(for double and float since Java
    1.4, for other primitive types since Java 1.7) which works on primitives
    directly.
    
    Rank: Of Concern (16), confidence: High
    Pattern: DM_BOXED_PRIMITIVE_FOR_COMPARE
    Type: Bx, Category: PERFORMANCE (Performance)
    
    
    Change-Id: Ieb4bcef1cfce0ccea02360d96cd41d2ae82d4259
    Signed-off-by: Carsten Hammer <carsten.hammer@t-online.de>

--- org.eclipse.jdt.ui.tests/ui/org/eclipse/jdt/ui/tests/buildpath/BuildpathProblemQuickFixTest.java
+++ org.eclipse.jdt.ui.tests/ui/org/eclipse/jdt/ui/tests/buildpath/BuildpathProblemQuickFixTest.java
@@ -94,7 +94,7 @@ public class BuildpathProblemQuickFixTest {
 					? ((IMarkerResolutionRelevance) e2).getRelevanceForResolution()
 					: 0;
 			if (relevanceMarker1 != relevanceMarker2) {
-				return Integer.valueOf(relevanceMarker2).compareTo(relevanceMarker1);
+				return Integer.compare(relevanceMarker2,relevanceMarker1);
 			}
 			return e1.getLabel().compareTo(
 					e2.getLabel());
