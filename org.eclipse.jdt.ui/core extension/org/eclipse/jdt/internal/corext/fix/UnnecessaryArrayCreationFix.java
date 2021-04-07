91f439fd840c3e99b6f9b6a672bcf0794ef32aa0
Jeff Johnston <jjohnstn@redhat.com>
2021-04-07 16:14:12 -0400

    Bug 572656 - Remove unnecessary array creation may change called method
    - fix UnnecessaryArrayCreationFix to calculate where to start looking
      for methods that might match if array creation is unwrapped
    - for super method calls, constructor calls, calls from expression,
      and static method calls where the method is imported, use the
      declaring class of the method called; otherwise use the
      enclosing class where the call is made
    - also check for nested classes and allow matches for private
      methods found in enclosing class
    - add new tests and reformat some of UnnecessaryArrayCreationFix
    
    Given:
    public class A {
    	public class ChildClass extends A {
    		public void overloadedMethod(int number) {
    		}
    
    		public void doNotCallAnotherMethod(int number) {
    			overloadedMethod(new int[]{number});
    		}
    	}
    
    	public void overloadedMethod(int... numbers) {
    	}
    }
    
    Applying "Remove unnecessary array creation" cleanup should not
    change the overloadedMethod() call because an existing method with
    a single int argument exists.
    
    Change-Id: Id45393641d8cd078e2d8a2a769f8a623add7fb6c
    Signed-off-by: Fabrice Tiercelin <fabrice.tiercelin@yahoo.fr>
    Signed-off-by: Jeff Johnston <jjohnstn@redhat.com>
    Reviewed-on: https://git.eclipse.org/r/c/jdt/eclipse.jdt.ui/+/178974
    Tested-by: JDT Bot <jdt-bot@eclipse.org>

--- org.eclipse.jdt.ui/core extension/org/eclipse/jdt/internal/corext/fix/UnnecessaryArrayCreationFix.java
+++ org.eclipse.jdt.ui/core extension/org/eclipse/jdt/internal/corext/fix/UnnecessaryArrayCreationFix.java
@@ -15,8 +15,10 @@ package org.eclipse.jdt.internal.corext.fix;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 import org.eclipse.core.runtime.IStatus;
@@ -30,10 +32,13 @@ import org.eclipse.jdt.core.dom.Expression;
 import org.eclipse.jdt.core.dom.IMethodBinding;
 import org.eclipse.jdt.core.dom.IPackageBinding;
 import org.eclipse.jdt.core.dom.ITypeBinding;
+import org.eclipse.jdt.core.dom.ImportDeclaration;
 import org.eclipse.jdt.core.dom.MethodInvocation;
 import org.eclipse.jdt.core.dom.Modifier;
 import org.eclipse.jdt.core.dom.NullLiteral;
+import org.eclipse.jdt.core.dom.QualifiedName;
 import org.eclipse.jdt.core.dom.SuperMethodInvocation;
+import org.eclipse.jdt.core.dom.ThisExpression;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
 
 import org.eclipse.jdt.internal.corext.dom.ASTNodes;
@@ -66,11 +71,13 @@ public class UnnecessaryArrayCreationFix extends CompilationUnitRewriteOperation
 			if (initializer != null
 					&& initializer.expressions() != null
 					&& initializer.expressions().size() == 1) {
-				Expression singleElement= (Expression) initializer.expressions().get(0);
-				NullLiteral nullLiteral= ASTNodes.as(singleElement, NullLiteral.class);
+				List<Expression> expressions= initializer.expressions();
+				ITypeBinding singleElement= expressions.get(0).resolveTypeBinding();
+				NullLiteral nullLiteral= ASTNodes.as(expressions.get(0), NullLiteral.class);
 
 				if (nullLiteral != null
-						|| singleElement.resolveTypeBinding() == null || singleElement.resolveTypeBinding().isArray()) {
+						|| singleElement == null
+						|| singleElement.isArray()) {
 					return true;
 				}
 			}
@@ -80,22 +87,19 @@ public class UnnecessaryArrayCreationFix extends CompilationUnitRewriteOperation
 			if (parent instanceof ClassInstanceCreation) {
 				ClassInstanceCreation cic= (ClassInstanceCreation) parent;
 
-				if (isUselessArrayCreation(node, cic.arguments(), cic.resolveConstructorBinding()) &&
-						!hasEquivalentMethod(node, cic.arguments(), cic.resolveConstructorBinding())) {
+				if (canArrayBeRemoved(node, cic.arguments(), cic.resolveConstructorBinding())) {
 					fResult.add(new UnwrapNewArrayOperation(node, cic));
 				}
 			} else if (parent instanceof MethodInvocation) {
 				MethodInvocation m= (MethodInvocation) parent;
 
-				if (isUselessArrayCreation(node, m.arguments(), m.resolveMethodBinding()) &&
-						!hasEquivalentMethod(node, m.arguments(), m.resolveMethodBinding())) {
+				if (canArrayBeRemoved(node, m.arguments(), m.resolveMethodBinding())) {
 					fResult.add(new UnwrapNewArrayOperation(node, m));
 				}
 			} else if (parent instanceof SuperMethodInvocation) {
 				SuperMethodInvocation sm= (SuperMethodInvocation) parent;
 
-				if (isUselessArrayCreation(node, sm.arguments(), sm.resolveMethodBinding()) &&
-						!hasEquivalentMethod(node, sm.arguments(), sm.resolveMethodBinding())) {
+				if (canArrayBeRemoved(node, sm.arguments(), sm.resolveMethodBinding())) {
 					fResult.add(new UnwrapNewArrayOperation(node, sm));
 				}
 			}
@@ -103,9 +107,14 @@ public class UnnecessaryArrayCreationFix extends CompilationUnitRewriteOperation
 			return true;
 		}
 
+		private boolean canArrayBeRemoved(ArrayCreation node, List<Expression> arguments, IMethodBinding binding) {
+			return isUselessArrayCreation(node, arguments, binding)
+					&& !hasEquivalentMethod(node, arguments, binding);
+		}
+
 		private boolean isUselessArrayCreation(ArrayCreation node, List<Expression> arguments, IMethodBinding binding) {
-			return (node.getInitializer() != null || (node.dimensions().size() == 1 && Long.valueOf(0).equals(ASTNodes.getIntegerLiteral((Expression) node.dimensions().get(0)))))
-					&& arguments.size() > 0
+			return (node.getInitializer() != null || (node.dimensions().size() == 1 && Long.valueOf(0L).equals(ASTNodes.getIntegerLiteral((Expression) node.dimensions().get(0)))))
+					&& !arguments.isEmpty()
 					&& arguments.get(arguments.size() - 1) == node
 					&& binding != null
 					&& binding.isVarargs()
@@ -115,72 +124,171 @@ public class UnnecessaryArrayCreationFix extends CompilationUnitRewriteOperation
 		}
 
 		private boolean hasEquivalentMethod(ArrayCreation node, List<Expression> arguments, IMethodBinding binding) {
-			ITypeBinding type= binding.getDeclaringClass();
-			TypeDeclaration typeDeclaration= (TypeDeclaration)ASTNodes.getFirstAncestorOrNull(node, TypeDeclaration.class);
-			ITypeBinding callerBinding= null;
-			boolean inSameClass= false;
-			boolean inSamePackage= false;
-			if (typeDeclaration != null) {
-				callerBinding= typeDeclaration.resolveBinding();
-			}
-			if (callerBinding == null || type.isEqualTo(callerBinding)) {
-				inSameClass= true;
+			TypeDeclaration typeDeclaration= ASTNodes.getTypedAncestor(node, TypeDeclaration.class);
+
+			if (typeDeclaration == null) {
+				return true;
 			}
 
-			while (type != null) {
-				IPackageBinding packageBinding= type.getPackage();
-				inSamePackage= callerBinding == null || packageBinding.isEqualTo(callerBinding.getPackage());
-				if (hasEquivalentMethodForType(node, arguments, binding, type, inSameClass, inSamePackage)) {
-					return true;
-				}
-				type= type.getSuperclass();
-				inSameClass= false;
+			ITypeBinding type= typeDeclaration.resolveBinding();
+
+			if (type == null) {
+				return true;
 			}
-			return false;
-		}
 
-		private boolean hasEquivalentMethodForType(ArrayCreation node, List<Expression> arguments, IMethodBinding binding,
-				ITypeBinding type, boolean inSameClass, boolean inSamePackage) {
-			for (IMethodBinding method : type.getDeclaredMethods()) {
-				if (!binding.isEqualTo(method) && binding.getName().equals(method.getName()) &&
-						((inSameClass || (method.getModifiers() & (Modifier.PUBLIC | Modifier.PROTECTED)) != 0)
-						|| (inSamePackage && (method.getModifiers() & Modifier.PRIVATE) == 0))) {
-					ITypeBinding[] typeArguments= method.getParameterTypes();
-					ArrayInitializer initializer= node.getInitializer();
+			boolean inSameClass= true;
+			ASTNode parent= node.getParent();
+			ITypeBinding[] parameterTypesForConflictingMethod= getParameterTypesForConflictingMethod(arguments, node);
 
-					if ((initializer == null ? 0 : initializer.expressions().size()) + arguments.size() - 1 == typeArguments.length) {
-						boolean match= true;
+			// Figure out the type where we need to start looking at methods in the hierarchy.
+			// If we have a new class instance or super method call or this expression or
+			// we have a static call that is qualified, we use the referenced class as the starting point.
+			// If we have a non-qualified method call, we use the class containing the call.
+			// Otherwise, we bail on the clean-up.
+			if (parent instanceof ClassInstanceCreation) {
+				type= ((ClassInstanceCreation) parent).resolveTypeBinding();
+				inSameClass= type.isNested();
+			} else if (parent instanceof MethodInvocation) {
+				MethodInvocation methodInvocation= (MethodInvocation) parent;
+				Expression expression= methodInvocation.getExpression();
 
-						for (int i= 0; i < arguments.size() - 1; ++i) {
-							ITypeBinding argBinding= arguments.get(i).resolveTypeBinding();
+				if (expression != null) {
+					if (!(expression instanceof ThisExpression)) {
+						inSameClass= binding.getDeclaringClass().isEqualTo(type);
+						type= expression.resolveTypeBinding();
+					}
+				} else {
+					ASTNode root= node.getRoot();
 
-							if (argBinding == null) {
-								return true;
-							}
+					if (root instanceof CompilationUnit) {
+						CompilationUnit compilationUnit= (CompilationUnit) root;
+						List<ImportDeclaration> imports= compilationUnit.imports();
+						String localPackage= compilationUnit.getPackage().getName().getFullyQualifiedName();
 
-							if (!argBinding.isAssignmentCompatible(typeArguments[i])) {
-								match= false;
-								break;
-							}
-						}
+						for (ImportDeclaration oneImport : imports) {
+							if (oneImport.isStatic()
+									&& !oneImport.isOnDemand()
+									&& oneImport.getName() instanceof QualifiedName) {
+								QualifiedName methodName= (QualifiedName) oneImport.getName();
+								String methodIdentifier= methodName.getName().getIdentifier();
+								ITypeBinding conflictingType= methodName.getQualifier().resolveTypeBinding();
 
-						if (match) {
-							for (int i= arguments.size() - 1, j= 0; i < typeArguments.length; ++i, ++j) {
-								Expression exp= (Expression) initializer.expressions().get(j);
-								ITypeBinding expBinding= exp.resolveTypeBinding();
+								if (conflictingType == null || conflictingType.getPackage() == null) {
+									return true; // Error on side of caution
+								}
 
-								if (expBinding != null && !expBinding.isAssignmentCompatible(typeArguments[i])) {
-									match= false;
-									break;
+								String importPackage= conflictingType.getPackage().getName();
+								boolean inSamePackage= Objects.equals(localPackage, importPackage);
+
+								for (IMethodBinding declaredMethod : conflictingType.getDeclaredMethods()) {
+									if (methodIdentifier.equals(declaredMethod.getName())
+											&& isMethodMatching(parameterTypesForConflictingMethod, binding, false, inSamePackage, declaredMethod)) {
+										return true;
+									}
 								}
 							}
 						}
+					}
 
-						if (match) {
-							return true;
-						}
+					if (Modifier.isStatic(binding.getModifiers())) {
+						inSameClass= binding.getDeclaringClass().isEqualTo(type);
+						type= binding.getDeclaringClass();
 					}
 				}
+			} else if (parent instanceof SuperMethodInvocation) {
+				inSameClass= type.isNested();
+				type= type.getSuperclass();
+			} else {
+				return true; // Error on side of caution
+			}
+
+			if (type == null) {
+				return true;
+			}
+
+			return hasEquivalentMethodForInheritedTypes(parameterTypesForConflictingMethod, binding, type, type, inSameClass);
+		}
+
+		private ITypeBinding[] getParameterTypesForConflictingMethod(List<Expression> arguments, ArrayCreation node) {
+			ArrayInitializer initializer= node.getInitializer();
+
+			List<Expression> initializerExpressions;
+			if (initializer != null) {
+				initializerExpressions= initializer.expressions();
+			} else {
+				initializerExpressions= Collections.EMPTY_LIST;
+			}
+
+			ITypeBinding[] parameterTypesForConflictingMethod= new ITypeBinding[arguments.size() - 1 + initializerExpressions.size()];
+
+			for (int i= 0; i < arguments.size() - 1; i++) {
+				parameterTypesForConflictingMethod[i]= arguments.get(i).resolveTypeBinding();
+			}
+
+			for (int i= 0; i < initializerExpressions.size(); i++) {
+				parameterTypesForConflictingMethod[arguments.size() - 1 + i]= initializerExpressions.get(i).resolveTypeBinding();
+			}
+
+			return parameterTypesForConflictingMethod;
+		}
+
+		private boolean hasEquivalentMethodForInheritedTypes(ITypeBinding[] parameterTypesForConflictingMethod, IMethodBinding binding, ITypeBinding type,
+				ITypeBinding origType, boolean inSameClass) {
+			while (type != null) {
+				IPackageBinding packageBinding= type.getPackage();
+				boolean inSamePackage= packageBinding.isEqualTo(origType.getPackage());
+
+				if (hasEquivalentMethodForOneType(parameterTypesForConflictingMethod, binding, type, inSameClass, inSamePackage)) {
+					return true;
+				}
+
+				if (type.isNested()) {
+					if (hasEquivalentMethodForInheritedTypes(parameterTypesForConflictingMethod, binding, type.getDeclaringClass(), origType, inSameClass)) {
+						return true;
+					}
+
+					type= type.getSuperclass();
+					inSameClass&= type.isNested();
+				} else {
+					type= type.getSuperclass();
+					inSameClass= false;
+				}
+			}
+
+			return false;
+		}
+
+		private boolean hasEquivalentMethodForOneType(ITypeBinding[] parameterTypesForConflictingMethod, IMethodBinding binding,
+				ITypeBinding type, boolean inSameClass, boolean inSamePackage) {
+			for (IMethodBinding method : type.getDeclaredMethods()) {
+				if (isMethodMatching(parameterTypesForConflictingMethod, binding, inSameClass, inSamePackage, method)) {
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		private boolean isMethodMatching(ITypeBinding[] parameterTypesForConflictingMethod, IMethodBinding binding, boolean inSameClass, boolean inSamePackage, IMethodBinding testedMethod) {
+			int methodModifiers= testedMethod.getModifiers();
+			ITypeBinding[] parameterTypes= testedMethod.getParameterTypes();
+
+			if (!binding.isEqualTo(testedMethod)
+					&& parameterTypesForConflictingMethod.length == parameterTypes.length
+					&& binding.getName().equals(testedMethod.getName())
+					&& (inSameClass || Modifier.isPublic(methodModifiers) || Modifier.isProtected(methodModifiers)
+							|| (inSamePackage && !Modifier.isPrivate(methodModifiers)))) {
+				for (int i= 0; i < parameterTypesForConflictingMethod.length; i++) {
+					if (parameterTypesForConflictingMethod[i] == null || parameterTypes[i] == null) {
+						return true;
+					}
+
+					if (!parameterTypesForConflictingMethod[i].isAssignmentCompatible(parameterTypes[i])) {
+						return false;
+					}
+				}
+
+				return true;
 			}
 
 			return false;
