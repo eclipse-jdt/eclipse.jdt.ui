4273819b34eb70759e603af678eb8f8255461eb3
Kalyan Prasad Tatavarthi <kalyan_prasad@in.ibm.com>
2022-03-23 10:25:21 +0530

    Bug 577723 - [18] Remove JCP disclaimers from code, UI and marketplace
    Change-Id: Id69ca07f290d7c7887d03f597c8d4e10307adb13
    Signed-off-by: Kalyan Prasad Tatavarthi <kalyan_prasad@in.ibm.com>
    Reviewed-on: https://git.eclipse.org/r/c/jdt/eclipse.jdt.ui/+/192174
    Tested-by: JDT Bot <jdt-bot@eclipse.org>

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/javadoc/JavadocContentAccess2.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/javadoc/JavadocContentAccess2.java
@@ -8,10 +8,6 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  *
- * This is an implementation of an early-draft specification developed under the Java
- * Community Process (JCP) and is made available for testing and evaluation purposes
- * only. The code is not compatible with any specification of the JCP.
- *
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *     Tom Hofmann, Google <eclipse@tom.eicher.name> - [hovering] NPE when hovering over @value reference within a type's javadoc - https://bugs.eclipse.org/bugs/show_bug.cgi?id=320084
