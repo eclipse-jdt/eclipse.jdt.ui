ba52e3fb7ac0e4e66b05cd77bcdf7dd252bc2292
Jeff Johnston <jjohnstn@redhat.com>
2022-06-23 18:29:18 -0400

    Fix type logic for while iterator to enhanced for cleanup
    - Fix EntrySet issue 109 with complex iterator types
    
      https://github.com/eclipse-jdt/eclipse.jdt.ui/issues/109
    
    - use same logic from for-with-iterator to enhanced for cleanup
      to properly set the type of the enhanced for loop variable

--- org.eclipse.jdt.core.manipulation/core extension/org/eclipse/jdt/internal/corext/fix/helper/WhileToForEach.java
+++ org.eclipse.jdt.core.manipulation/core extension/org/eclipse/jdt/internal/corext/fix/helper/WhileToForEach.java
@@ -36,10 +36,8 @@ import org.eclipse.jdt.core.dom.IMethodBinding;
 import org.eclipse.jdt.core.dom.ITypeBinding;
 import org.eclipse.jdt.core.dom.IVariableBinding;
 import org.eclipse.jdt.core.dom.MethodInvocation;
-import org.eclipse.jdt.core.dom.ParameterizedType;
 import org.eclipse.jdt.core.dom.ParenthesizedExpression;
 import org.eclipse.jdt.core.dom.SimpleName;
-import org.eclipse.jdt.core.dom.SimpleType;
 import org.eclipse.jdt.core.dom.SingleVariableDeclaration;
 import org.eclipse.jdt.core.dom.Statement;
 import org.eclipse.jdt.core.dom.StructuralPropertyDescriptor;
@@ -49,9 +47,14 @@ import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
 import org.eclipse.jdt.core.dom.VariableDeclarationStatement;
 import org.eclipse.jdt.core.dom.WhileStatement;
 import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
+import org.eclipse.jdt.core.dom.rewrite.ImportRewrite;
+import org.eclipse.jdt.core.dom.rewrite.ImportRewrite.ImportRewriteContext;
+import org.eclipse.jdt.core.dom.rewrite.ImportRewrite.TypeLocation;
 
 import org.eclipse.jdt.internal.common.HelperVisitor;
 import org.eclipse.jdt.internal.common.ReferenceHolder;
+import org.eclipse.jdt.internal.core.manipulation.dom.ASTResolving;
+import org.eclipse.jdt.internal.corext.codemanipulation.ContextSensitiveImportRewriteContext;
 import org.eclipse.jdt.internal.corext.dom.ASTNodes;
 import org.eclipse.jdt.internal.corext.dom.AbortSearchException;
 import org.eclipse.jdt.internal.corext.dom.Bindings;
@@ -59,6 +62,7 @@ import org.eclipse.jdt.internal.corext.fix.CompilationUnitRewriteOperationsFixCo
 import org.eclipse.jdt.internal.corext.fix.ConvertLoopOperation;
 import org.eclipse.jdt.internal.corext.fix.UseIteratorToForLoopFixCore;
 import org.eclipse.jdt.internal.corext.refactoring.structure.CompilationUnitRewrite;
+import org.eclipse.jdt.internal.corext.refactoring.structure.ImportRemover;
 
 /**
  * Find: while (it.hasNext()){ System.out.println(it.next()); }
@@ -389,6 +393,9 @@ public class WhileToForEach extends AbstractTool<WhileLoopToChangeHit> {
 		ASTRewrite rewrite= cuRewrite.getASTRewrite();
 		AST ast= cuRewrite.getRoot().getAST();
 
+		ImportRewrite importRewrite= cuRewrite.getImportRewrite();
+		ImportRemover remover= cuRewrite.getImportRemover();
+
 		EnhancedForStatement newEnhancedForStatement= ast.newEnhancedForStatement();
 
 		SingleVariableDeclaration result= ast.newSingleVariableDeclaration();
@@ -407,8 +414,6 @@ public class WhileToForEach extends AbstractTool<WhileLoopToChangeHit> {
 			VariableDeclarationStatement typedAncestor= ASTNodes.getTypedAncestor(hit.loopVarDeclaration, VariableDeclarationStatement.class);
 			type= typedAncestor.getType();
 		}
-		ParameterizedType mytype= null;
-		Type type2= null;
 		if (type == null) {
 			looptargettype= "java.lang.Object"; //$NON-NLS-1$
 			ITypeBinding binding= computeTypeArgument(hit.iteratorDeclaration);
@@ -417,23 +422,14 @@ public class WhileToForEach extends AbstractTool<WhileLoopToChangeHit> {
 			}
 			Type collectionType= ast.newSimpleType(addImport(looptargettype, cuRewrite, ast));
 			result.setType(collectionType);
-		} else if (type instanceof ParameterizedType) {
-			mytype= (ParameterizedType) type;
-			type2= mytype.getType();
-			List<Type> typeArguments= mytype.typeArguments();
-			looptargettype= type2.resolveBinding().getErasure().getQualifiedName();
-			Type collectionType= ast.newSimpleType(addImport(looptargettype, cuRewrite, ast));
-			ParameterizedType genericType= ast.newParameterizedType(collectionType);
-			for (Type typeArgument : typeArguments) {
-				addGenericTypes(ast, typeArgument, genericType, cuRewrite);
-			}
-			result.setType(genericType);
 		} else {
-			looptargettype= type.resolveBinding().getQualifiedName();
-			Type collectionType= ast.newSimpleType(addImport(looptargettype, cuRewrite, ast));
-			result.setType(collectionType);
-		}
+			ITypeBinding varBinding= hit.iteratorDeclaration.getType().resolveBinding();
+			ITypeBinding elementType= getElementType(varBinding, ast);
+			Type importType= importType(elementType, hit.iteratorDeclaration, importRewrite, (CompilationUnit)hit.iteratorDeclaration.getRoot(), TypeLocation.LOCAL_VARIABLE);
+			remover.registerAddedImports(importType);
 
+			result.setType(importType);
+		}
 		newEnhancedForStatement.setParameter(result);
 		if (hit.self) {
 			ThisExpression newThisExpression= ast.newThisExpression();
@@ -443,8 +439,10 @@ public class WhileToForEach extends AbstractTool<WhileLoopToChangeHit> {
 			newEnhancedForStatement.setExpression(loopExpression);
 		}
 		ASTNodes.removeButKeepComment(rewrite, hit.iteratorDeclaration, group);
+		remover.registerRemovedNode(hit.iteratorDeclaration.getType());
 		if (hit.iteratorCall != hit.iteratorDeclaration) {
 			ASTNodes.removeButKeepComment(rewrite, hit.iteratorCall, group);
+			remover.registerRemovedNode(hit.iteratorCall);
 		}
 		if (hit.nextFound) {
 			if (hit.nextWithoutVariableDeclaration) {
@@ -455,33 +453,49 @@ public class WhileToForEach extends AbstractTool<WhileLoopToChangeHit> {
 				}
 				if (loopVarDeclaration.getLocationInParent() == ExpressionStatement.EXPRESSION_PROPERTY) {
 					rewrite.remove(loopVarDeclaration.getParent(), group);
+					remover.registerRemovedNode(loopVarDeclaration);
 				} else {
 					ASTNodes.replaceButKeepComment(rewrite, hit.loopVarDeclaration, name, group);
+					remover.registerRemovedNode(hit.loopVarDeclaration);
 				}
 			} else {
-				ASTNodes.removeButKeepComment(rewrite, ASTNodes.getTypedAncestor(hit.loopVarDeclaration, VariableDeclarationStatement.class), group);
+				ASTNode node= ASTNodes.getTypedAncestor(hit.loopVarDeclaration, VariableDeclarationStatement.class);
+				ASTNodes.removeButKeepComment(rewrite, node, group);
+				remover.registerRemovedNode(node);
 			}
 		}
 		newEnhancedForStatement.setBody(ASTNodes.createMoveTarget(rewrite, hit.whileStatement.getBody()));
 		ASTNodes.replaceButKeepComment(rewrite, hit.whileStatement, newEnhancedForStatement, group);
+		remover.registerRemovedNode(hit.whileStatement.getExpression());
+		remover.applyRemoves(importRewrite);
 	}
 
-	private void addGenericTypes(AST ast, Type typeArgument, ParameterizedType genericType, CompilationUnitRewrite cuRewrite) {
-		if (typeArgument instanceof SimpleType) {
-			String fullyQualifiedName= ((SimpleType)typeArgument).getName().getFullyQualifiedName();
-			genericType.typeArguments().add(ast.newSimpleType(ast.newName(fullyQualifiedName)));
-		} else if (typeArgument instanceof ParameterizedType) {
-			ParameterizedType pType= (ParameterizedType)typeArgument;
-			Type pType2= pType.getType();
-			String pTypeName= pType2.resolveBinding().getErasure().getQualifiedName();
-			Type newPTypeType= ast.newSimpleType(addImport(pTypeName, cuRewrite, ast));
-			ParameterizedType newGenericType= ast.newParameterizedType(newPTypeType);
-			List<Type> pTypeArguments= pType.typeArguments();
-			for (Type pTypeArgument : pTypeArguments) {
-				addGenericTypes(ast, pTypeArgument, newGenericType, cuRewrite);
+	private Type importType(final ITypeBinding toImport, final ASTNode accessor, ImportRewrite imports, final CompilationUnit compilationUnit, TypeLocation location) {
+		ImportRewriteContext importContext= new ContextSensitiveImportRewriteContext(compilationUnit, accessor.getStartPosition(), imports);
+		return imports.addImport(toImport, compilationUnit.getAST(), importContext, location);
+	}
+
+	/**
+	 * Returns the type of elements returned by the iterator.
+	 *
+	 * @param iterator
+	 *            the iterator type binding, or <code>null</code>
+	 * @param ast
+	 *            the AST
+	 * @return the element type
+	 */
+	private ITypeBinding getElementType(final ITypeBinding iterator, AST ast) {
+		if (iterator != null) {
+			ITypeBinding[] bindings= iterator.getTypeArguments();
+			if (bindings.length > 0) {
+				ITypeBinding arg= bindings[0];
+				if (arg.isWildcardType()) {
+					arg= ASTResolving.normalizeWildcardType(arg, true, ast);
+				}
+				return arg;
 			}
-			genericType.typeArguments().add(newGenericType);
 		}
+		return ast.resolveWellKnownType(Object.class.getCanonicalName());
 	}
 
 	@Override
