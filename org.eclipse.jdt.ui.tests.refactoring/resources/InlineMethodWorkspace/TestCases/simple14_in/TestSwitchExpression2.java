63b5daf0ca69aef09cd1d854aaa7e27e4e617704
Kalyan Prasad Tatavarthi <kalyan_prasad@in.ibm.com>
2022-08-09 12:30:25 +0530

    Adding tests

---
+++ org.eclipse.jdt.ui.tests.refactoring/resources/InlineMethodWorkspace/TestCases/simple14_in/TestSwitchExpression2.java
@@ -0,0 +1,16 @@
+package simple18_in;
+
+import java.lang.String;
+
+public class TestSwitchExpression1 {
+	public static String format (String... input) {
+		return "";
+	}
+	public static void main(String[] args) {
+		int value = 0;
+		String message = switch (value) {
+			case 0 : yield /*]*/format("")/*[*/;
+			default : yield "";
+		};
+	}
+}
---
+++ org.eclipse.jdt.ui.tests.refactoring/resources/InlineMethodWorkspace/TestCases/simple14_out/TestSwitchExpression4.java
@@ -0,0 +1,31 @@
+package simple18_in;
+
+import java.lang.String;
+
+public class TestSwitchExpression1 {
+	public static String format (String... input) {
+		if (input == null) {
+			return "null";
+		} else if (input.length > 1) {
+			return "multiple";
+		} else {
+			return "single";
+		}
+	}
+	public static void main(String[] args) {
+		int value = 0;
+		String message = switch (value) {
+			case 0 : {
+					String[] input = {""};
+					if (input == null) {
+						yield "null";
+					} else if (input.length > 1) {
+						yield "multiple";
+					} else {
+						yield "single";
+					}
+				}
+			default : yield "";
+		};
+	}
+}
