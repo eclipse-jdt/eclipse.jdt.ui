74490955192cf93c498ac4e2822352a17dd5e5f7
chixiaye <xychichina@gmail.com>
2023-03-05 07:21:24 -0800

    reformat

--- org.eclipse.jdt.ui.tests.refactoring/resources/ExtractTemp/canExtract/A_test46_out.java
+++ org.eclipse.jdt.ui.tests.refactoring/resources/ExtractTemp/canExtract/A_test46_out.java
@@ -1,13 +1,13 @@
 package p;
+
 class A {
 	public static long fibonacci(long number) {
-        if ((number == 0) || (number == 1)){
-            System.out.println("stop recursion");
-            return number;
-        } 
-        else {
-            long fibonacci= fibonacci(number - 1);
-            return fibonacci(number - 1) + fibonacci(number - 2);
-        }
-    }
+		if ((number == 0) || (number == 1)) {
+			System.out.println("stop recursion");
+			return number;
+		} else {
+			long fibonacci= fibonacci(number - 1);
+			return fibonacci(number - 1) + fibonacci(number - 2);
+		}
+	}
 }
