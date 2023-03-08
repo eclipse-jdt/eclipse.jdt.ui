e5cac26d4c0f14cc061efdebadbf4b6a6be4eef1
Noopur Gupta <noopur_gupta@in.ibm.com>
2023-03-08 18:19:14 +0530

    [20] Add AST level 20 to AST view (#465)
    Fixes https://github.com/eclipse-jdt/eclipse.jdt.ui/issues/464

--- org.eclipse.jdt.core.manipulation/common/org/eclipse/jdt/internal/ui/util/ASTHelper.java
+++ org.eclipse.jdt.core.manipulation/common/org/eclipse/jdt/internal/ui/util/ASTHelper.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019, 2022 IBM Corporation and others.
+ * Copyright (c) 2019, 2023 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -7,6 +7,10 @@
  * https://www.eclipse.org/legal/epl-2.0/
  *
  * SPDX-License-Identifier: EPL-2.0
+ *
+ * This is an implementation of an early-draft specification developed under the Java
+ * Community Process (JCP) and is made available for testing and evaluation purposes
+ * only. The code is not compatible with any specification of the JCP.
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
@@ -35,6 +39,7 @@ public class ASTHelper {
 	public static final int JLS17 = AST.JLS17;
 	public static final int JLS18 = AST.JLS18;
 	public static final int JLS19 = AST.JLS19;
+	public static final int JLS20 = AST.JLS20;
 
 	private static boolean isNodeTypeSupportedInAST(AST ast, int nodeType) {
 		switch (nodeType) {
