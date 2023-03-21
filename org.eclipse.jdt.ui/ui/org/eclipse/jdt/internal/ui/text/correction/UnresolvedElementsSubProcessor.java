eclipse-jdt/eclipse.jdt.ui

commit 546a6978432fa5f2b01083919364848dd89805dc

Make quickfix that suggests importing a class smarter (#352)
* Make quickfix that suggests importing a class smarter

- change UnresolvedElementsSubProcessor.addSimilarTypeProposals() to
  check if an interface is expected in which case make sure that
  each suggested class to import has the interface in its hierarchy
- fixes #349

org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/correction/UnresolvedElementsSubProcessor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2021 IBM Corporation and others.
+ * Copyright (c) 2000, 202 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -65,6 +65,7 @@ import org.eclipse.jdt.core.compiler.IProblem;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTMatcher;
 import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.ASTParser;
 import org.eclipse.jdt.core.dom.Annotation;
 import org.eclipse.jdt.core.dom.ArrayType;
 import org.eclipse.jdt.core.dom.Assignment;
@@ -126,6 +127,7 @@ import org.eclipse.jdt.internal.corext.codemanipulation.ContextSensitiveImportRe
 import org.eclipse.jdt.internal.corext.dom.ASTNodeFactory;
 import org.eclipse.jdt.internal.corext.dom.ASTNodes;
 import org.eclipse.jdt.internal.corext.dom.Bindings;
+import org.eclipse.jdt.internal.corext.dom.IASTSharedValues;
 import org.eclipse.jdt.internal.corext.dom.ScopeAnalyzer;
 import org.eclipse.jdt.internal.corext.refactoring.changes.ClasspathChange;
 import org.eclipse.jdt.internal.corext.util.JavaModelUtil;
@@ -840,8 +842,9 @@ public class UnresolvedElementsSubProcessor {
 		// try to resolve type in context -> highest severity
 		String resolvedTypeName= null;
 		ITypeBinding binding= ASTResolving.guessBindingForTypeReference(node);
+		ITypeBinding simpleBinding= null;
 		if (binding != null) {
-			ITypeBinding simpleBinding= binding;
+			simpleBinding= binding;
 			if (simpleBinding.isArray()) {
 				simpleBinding= simpleBinding.getElementType();
 			}
@@ -885,6 +888,27 @@ public class UnresolvedElementsSubProcessor {
 			if ((elem.getKind() & TypeKinds.ALL_TYPES) != 0) {
 				String fullName= elem.getName();
 				if (!fullName.equals(resolvedTypeName)) {
+					if (simpleBinding != null && !simpleBinding.isPrimitive()) {
+						// If we have an expected type, we should verify that any classes we suggest to import
+						// inherit directly or indirectly from the type
+						ITypeBinding qualifiedTypeBinding= null;
+						try {
+							IJavaProject focus= simpleBinding.getJavaElement().getJavaProject();
+							IType javaElementType= focus.findType(fullName);
+							ASTParser parser= ASTParser.newParser(IASTSharedValues.SHARED_AST_LEVEL);
+							parser.setProject(focus);
+							IBinding[] bindings= parser.createBindings(new IJavaElement[] {javaElementType} , null);
+							qualifiedTypeBinding=(ITypeBinding)bindings[0];
+						} catch (JavaModelException e) {
+							// fall through
+						}
+
+						if (qualifiedTypeBinding != null) {
+							if (!isInherited(qualifiedTypeBinding, simpleBinding)) {
+								continue;
+							}
+						}
+					}
 					CUCorrectionProposal cuProposal= createTypeRefChangeProposal(cu, fullName, node, relevance, elements.length);
 					ChangeCorrectionProposal compositeProposal= getCompositeChangeProposal(cuProposal);
 					if (compositeProposal != null) {
@@ -898,6 +922,24 @@ public class UnresolvedElementsSubProcessor {
 		if (elements.length == 0) {
 			addRequiresModuleProposals(cu, node, IProposalRelevance.IMPORT_NOT_FOUND_ADD_REQUIRES_MODULE, proposals, true);
 		}
+	}
+
+	private static boolean isInherited(ITypeBinding binding, ITypeBinding ancestorBinding) {
+		if (binding == null) {
+			return false;
+		}
+		if (binding.isEqualTo(ancestorBinding)) {
+			return true;
+		}
+		for (ITypeBinding interfaceBinding : binding.getInterfaces()) {
+			if (isInherited(interfaceBinding, ancestorBinding)) {
+				return true;
+			}
+		}
+		if (isInherited(binding.getSuperclass(), ancestorBinding)) {
+			return true;
+		}
+		return false;
 	}
 
 	private static ChangeCorrectionProposal getCompositeChangeProposal(ChangeCorrectionProposal proposal) throws CoreException {

commit 0e209ac2f0eaf61b38579ff55e8b2a87fa05d652

Fix regression in UnresolvedElementsSubProcessor
- fix problem introduced by #352
- check for inheritance should not be run if the suggested
  class binding has same name as the missing class
- fixes #485

org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/correction/UnresolvedElementsSubProcessor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 202 IBM Corporation and others.
+ * Copyright (c) 2000, 2023 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -895,16 +895,18 @@ public class UnresolvedElementsSubProcessor {
 						try {
 							IJavaProject focus= simpleBinding.getJavaElement().getJavaProject();
 							IType javaElementType= focus.findType(fullName);
-							ASTParser parser= ASTParser.newParser(IASTSharedValues.SHARED_AST_LEVEL);
-							parser.setProject(focus);
-							IBinding[] bindings= parser.createBindings(new IJavaElement[] {javaElementType} , null);
-							qualifiedTypeBinding=(ITypeBinding)bindings[0];
+							if (javaElementType != null) {
+								ASTParser parser= ASTParser.newParser(IASTSharedValues.SHARED_AST_LEVEL);
+								parser.setProject(focus);
+								IBinding[] bindings= parser.createBindings(new IJavaElement[] {javaElementType} , null);
+								qualifiedTypeBinding=(ITypeBinding)bindings[0];
+							}
 						} catch (JavaModelException e) {
 							// fall through
 						}
 
 						if (qualifiedTypeBinding != null) {
-							if (!isInherited(qualifiedTypeBinding, simpleBinding)) {
+							if (!qualifiedTypeBinding.getName().equals(simpleBinding.getName()) && !isInherited(qualifiedTypeBinding, simpleBinding)) {
 								continue;
 							}
 						}