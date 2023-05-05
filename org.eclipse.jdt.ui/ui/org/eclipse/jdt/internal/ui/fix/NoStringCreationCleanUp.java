fa6c7bb33043556ced4b4f67abc3194bc5d6a489
Jeff Johnston <jjohnstn@redhat.com>
2023-05-05 11:08:26 -0400

    Enhance NoStringCreationCleanUp (#552)
    - modify cleanup so that it handles cases where the arguments
      to new String() are: nested new String() calls, method
      calls or variable references that are subsequently used to
      invoke String methods
    - don't change new String() calls that are used to copy a
      String to another
    - update preview content
    - update CleanUpTest tests
    - fixes: https://bugs.eclipse.org/bugs/show_bug.cgi?id=573119

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/fix/NoStringCreationCleanUp.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/fix/NoStringCreationCleanUp.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2020 Fabrice TIERCELIN and others.
+ * Copyright (c) 2020, 2023 Fabrice TIERCELIN and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -19,20 +19,22 @@ import java.util.List;
 import java.util.Map;
 
 import org.eclipse.core.runtime.CoreException;
-
-import org.eclipse.text.edits.TextEditGroup;
-
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.Assignment;
 import org.eclipse.jdt.core.dom.ClassInstanceCreation;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.Expression;
+import org.eclipse.jdt.core.dom.FieldAccess;
 import org.eclipse.jdt.core.dom.InfixExpression;
+import org.eclipse.jdt.core.dom.MethodInvocation;
+import org.eclipse.jdt.core.dom.ParenthesizedExpression;
+import org.eclipse.jdt.core.dom.SimpleName;
 import org.eclipse.jdt.core.dom.StringLiteral;
+import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
 import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
-
 import org.eclipse.jdt.internal.corext.dom.ASTNodeFactory;
 import org.eclipse.jdt.internal.corext.dom.ASTNodes;
 import org.eclipse.jdt.internal.corext.fix.CleanUpConstants;
@@ -40,10 +42,10 @@ import org.eclipse.jdt.internal.corext.fix.CompilationUnitRewriteOperationsFix;
 import org.eclipse.jdt.internal.corext.fix.CompilationUnitRewriteOperationsFix.CompilationUnitRewriteOperation;
 import org.eclipse.jdt.internal.corext.fix.LinkedProposalModel;
 import org.eclipse.jdt.internal.corext.refactoring.structure.CompilationUnitRewrite;
-
 import org.eclipse.jdt.ui.cleanup.CleanUpRequirements;
 import org.eclipse.jdt.ui.cleanup.ICleanUpFix;
 import org.eclipse.jdt.ui.text.java.IProblemLocation;
+import org.eclipse.text.edits.TextEditGroup;
 
 /**
  * A fix that removes a String instance from a String literal.
@@ -75,11 +77,16 @@ public class NoStringCreationCleanUp extends AbstractMultiFix {
 	@Override
 	public String getPreview() {
 		if (isEnabled(CleanUpConstants.NO_STRING_CREATION)) {
-			return "String bar = \"foo\";\n"; //$NON-NLS-1$
+			return "" //$NON-NLS-1$
+					+ "String bar = \"foo\";\n" //$NON-NLS-1$
+					+ "String newBar = bar.concat(\"abc\");\n" //$NON-NLS-1$
+					+ "String cantChange = new String(possibleNullObject)\n"; //$NON-NLS-1$
 		}
 
 		return "" //$NON-NLS-1$
-				+ "String bar = new String(\"foo\");\n"; //$NON-NLS-1$
+				+ "String bar = new String(\"foo\");\n" //$NON-NLS-1$
+				+ "String newBar = (new String(bar)).concat(\"abc\");\n" //$NON-NLS-1$
+				+ "String cantChange = new String(possibleNullObject)\n"; //$NON-NLS-1$
 	}
 
 	@Override
@@ -94,12 +101,28 @@ public class NoStringCreationCleanUp extends AbstractMultiFix {
 			@Override
 			public boolean visit(final ClassInstanceCreation node) {
 				if (ASTNodes.hasType(node, String.class.getCanonicalName()) && node.arguments().size() == 1) {
-					Expression arg0= (Expression) node.arguments().get(0);
-
-					if (ASTNodes.hasType(arg0, String.class.getCanonicalName())
-							&& (arg0 instanceof StringLiteral || arg0 instanceof InfixExpression)) {
-						rewriteOperations.add(new NoStringCreationOperation(node, arg0));
-						return false;
+					Expression arg0= ASTNodes.getUnparenthesedExpression((Expression)node.arguments().get(0));
+					while (arg0 instanceof ClassInstanceCreation c && ASTNodes.hasType(c, String.class.getCanonicalName()) &&
+							c.arguments().size() == 1) {
+						arg0= ASTNodes.getUnparenthesedExpression((Expression)c.arguments().get(0));
+					}
+					if (ASTNodes.hasType(arg0, String.class.getCanonicalName())) {
+						if (arg0 instanceof StringLiteral || arg0 instanceof InfixExpression) {
+							rewriteOperations.add(new NoStringCreationOperation(node, arg0));
+							return false;
+						} else if (arg0 instanceof MethodInvocation || arg0 instanceof SimpleName) {
+							ASTNode parent= node.getParent();
+							while (parent instanceof ParenthesizedExpression) {
+								parent= parent.getParent();
+							}
+							if (parent instanceof Assignment || parent instanceof VariableDeclarationFragment) {
+								return true;
+							}
+							if (parent instanceof MethodInvocation || parent instanceof FieldAccess) {
+								rewriteOperations.add(new NoStringCreationOperation(node, arg0));
+							}
+							return false;
+						}
 					}
 				}
 
@@ -141,7 +164,11 @@ public class NoStringCreationCleanUp extends AbstractMultiFix {
 			TextEditGroup group= createTextEditGroup(MultiFixMessages.NoStringCreationCleanUp_description, cuRewrite);
 			ASTNode replacement= ASTNodeFactory.parenthesizeIfNeeded(ast, ASTNodes.createMoveTarget(rewrite, arg0));
 
-			ASTNodes.replaceButKeepComment(rewrite, node, replacement, group);
+			ASTNode nodeToReplace= node;
+			while (nodeToReplace.getParent() instanceof ParenthesizedExpression) {
+				nodeToReplace= nodeToReplace.getParent();
+			}
+			ASTNodes.replaceButKeepComment(rewrite, nodeToReplace, replacement, group);
 		}
 	}
 }
