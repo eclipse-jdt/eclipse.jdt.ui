b6a9832ca93bb900e47f76c705132ad2300cce15
Zsombor Gegesy <gzsombor@gmail.com>
2023-05-13 23:03:35 +0200

    Add ability to open the Call Hieararchy on Ctrl+Click (#575)
    Add ability to open the Call Hieararchy viewer whenever the user clicks
    on a method call or method or field declaration.

---
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/javaeditor/OpenCallHierarchyHyperlinkDetector.java
@@ -0,0 +1,82 @@
+/*******************************************************************************
+ * Copyright (c) 2023 Zsombor Gegesy
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Zsombor Gegesy - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.jdt.internal.ui.javaeditor;
+
+import java.util.List;
+
+import org.eclipse.jface.text.IRegion;
+import org.eclipse.jface.text.hyperlink.IHyperlink;
+
+import org.eclipse.ui.IWorkbenchWindow;
+
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IMember;
+
+import org.eclipse.jdt.internal.corext.callhierarchy.CallHierarchy;
+
+import org.eclipse.jdt.ui.actions.SelectionDispatchAction;
+
+import org.eclipse.jdt.internal.ui.callhierarchy.CallHierarchyUI;
+
+/**
+ * Provides a hyperlink for method calls, method declarations to open the Call Hierarchy view.
+ */
+public class OpenCallHierarchyHyperlinkDetector extends JavaElementHyperlinkDetector {
+
+	@Override
+	protected void addHyperlinks(List<IHyperlink> hyperlinksCollector, IRegion wordRegion, SelectionDispatchAction openAction, IJavaElement element, boolean qualify, JavaEditor editor) {
+		if ((element.getElementType() == IJavaElement.METHOD || element.getElementType() == IJavaElement.FIELD) && (element instanceof IMember imember)) {
+			hyperlinksCollector.add(new OpenCallHierarchyHyperlink(wordRegion, openAction.getSite().getWorkbenchWindow(), imember));
+		}
+	}
+
+	private static class OpenCallHierarchyHyperlink implements IHyperlink {
+
+		private final IRegion region;
+
+		private final IWorkbenchWindow iWorkbenchWindow;
+
+		private final IMember element;
+
+		public OpenCallHierarchyHyperlink(IRegion region, IWorkbenchWindow iWorkbenchWindow, IMember element) {
+			this.region= region;
+			this.iWorkbenchWindow= iWorkbenchWindow;
+			this.element= element;
+		}
+
+		@Override
+		public IRegion getHyperlinkRegion() {
+			return region;
+		}
+
+		@Override
+		public String getTypeLabel() {
+			return null;
+		}
+
+		@Override
+		public String getHyperlinkText() {
+			return JavaEditorMessages.OpenCallHierarchyHyperlinkDetector_hyperlinkText;
+		}
+
+		@Override
+		public void open() {
+			if (CallHierarchy.isPossibleInputElement(element)) {
+				IMember[] members= new IMember[] { element };
+				CallHierarchyUI.openSelectionDialog(members, iWorkbenchWindow);
+			}
+		}
+
+	}
+}
