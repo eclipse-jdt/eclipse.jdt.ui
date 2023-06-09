49a3cc4b2e91ac50831dc567bec8a238815fbe83
DongChunhao <m15231670380@163.com>
2023-06-09 10:21:18 +0800

    modify

--- org.eclipse.ltk.ui.refactoring/src/org/eclipse/ltk/internal/ui/refactoring/actions/RenameResourceHandler.java
+++ org.eclipse.ltk.ui.refactoring/src/org/eclipse/ltk/internal/ui/refactoring/actions/RenameResourceHandler.java
@@ -14,6 +14,10 @@
  ******************************************************************************/
 package org.eclipse.ltk.internal.ui.refactoring.actions;
 
+import java.util.AbstractList;
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.eclipse.swt.widgets.Shell;
 
 import org.eclipse.core.commands.ExecutionEvent;
@@ -22,12 +26,14 @@ import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.NullProgressMonitor;
 
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
 
 import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 
+import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 import org.eclipse.ltk.core.refactoring.Change;
@@ -42,13 +48,24 @@ import org.eclipse.ltk.internal.core.refactoring.resource.RenameResourceProcesso
 import org.eclipse.ltk.internal.ui.refactoring.InternalAPI;
 import org.eclipse.ltk.internal.ui.refactoring.RefactoringUIMessages;
 import org.eclipse.ltk.internal.ui.refactoring.RefactoringUIPlugin;
-import org.eclipse.ltk.ti.refactoring.rename.suggest.SuggestRenameField;
 import org.eclipse.ltk.ui.refactoring.RefactoringWizardOpenOperation;
 import org.eclipse.ltk.ui.refactoring.resource.RenameResourceWizard;
+
+import org.eclipse.jdt.core.Flags;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IField;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.NamingConventions;
+
 
 public class RenameResourceHandler extends AbstractResourcesHandler {
 	private static final String LTK_RENAME_COMMAND_NEWNAME_PARAMETER_KEY= "org.eclipse.ltk.ui.refactoring.commands.renameResource.newName.parameter.key"; //$NON-NLS-1$
 	private static final String LTK_CHECK_COMPOSITE_RENAME_PARAMETER_KEY= "org.eclipse.ltk.ui.refactoring.commands.checkCompositeRename.parameter.key"; //$NON-NLS-1$
+	private IField fSelectedField;
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
@@ -61,6 +78,7 @@ public class RenameResourceHandler extends AbstractResourcesHandler {
 		return null;
 	}
 
+	@SuppressWarnings("boxing")
 	private Object checkForCompositeRename(ExecutionEvent event) {
 		ISelection sel= HandlerUtil.getCurrentSelection(event);
 		if (sel instanceof IStructuredSelection) {
@@ -83,12 +101,46 @@ public class RenameResourceHandler extends AbstractResourcesHandler {
 		if (newNameValue instanceof String) {
 			newName= (String) newNameValue;
 		}
+
 		ISelection sel= HandlerUtil.getCurrentSelection(event);
-		try {
-			newName = SuggestRenameField.getFieldNameSuggestions(sel,event);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		fSelectedField= null;
+		ICompilationUnit compilationUnit = null;
+		if (sel instanceof IStructuredSelection) {
+		    Object firstElement = ((IStructuredSelection) sel).getFirstElement();
+		    if (firstElement instanceof IJavaElement) {
+		        IJavaElement javaElement = (IJavaElement) firstElement;
+		        fSelectedField = javaElement.getAdapter(IField.class);
+		        compilationUnit = javaElement.getAdapter(ICompilationUnit.class);
+		    }
+		}
+
+		IEditorPart editorPart = HandlerUtil.getActiveEditor(event);
+		IFile file = editorPart.getEditorInput().getAdapter(IFile.class);
+		IJavaProject project= JavaCore.create(file.getProject());
+
+		String[] excluded = new String[0];
+		if (compilationUnit != null) {
+		        IType[] types;
+				try {
+					types= compilationUnit.getAllTypes();
+					 for (IType type : types) {
+				            IField[] fields = type.getFields();
+				            for (IField field : fields) {
+				                String fieldName = field.getElementName();
+				                excluded = Arrays.copyOf(excluded, excluded.length + 1);
+				                excluded[excluded.length - 1] = fieldName;
+				            }
+				        }
+					 int fieldModifiers = fSelectedField.getFlags();
+						String[] newNames = getFieldNameSuggestions(project, newName,fieldModifiers, excluded);
+						if(newNames.length>0) {
+							newName = newNames[0];
+						}
+				} catch (JavaModelException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
 		}
 		if (sel instanceof IStructuredSelection) {
 			IResource resource= getCurrentResource((IStructuredSelection) sel);
@@ -145,5 +197,71 @@ public class RenameResourceHandler extends AbstractResourcesHandler {
 			return resources[0];
 		}
 		return null;
+	}
+	public static String[] getFieldNameSuggestions(IJavaProject project,String originalField, int fieldModifiers, String[] excluded) {
+		return getFieldNameSuggestions(project, originalField, 0, fieldModifiers, excluded);
+	}
+	public static String[] getFieldNameSuggestions(IJavaProject project, String baseName, int dimensions, int modifiers, String[] excluded) {
+		if (Flags.isFinal(modifiers) && Flags.isStatic(modifiers)) {
+			return getVariableNameSuggestions(NamingConventions.VK_STATIC_FINAL_FIELD, project, baseName, dimensions, new ExcludedCollection(excluded), true);
+		} else if (Flags.isStatic(modifiers)) {
+			return getVariableNameSuggestions(NamingConventions.VK_STATIC_FIELD, project, baseName, dimensions, new ExcludedCollection(excluded), true);
+		}
+		return getVariableNameSuggestions(NamingConventions.VK_INSTANCE_FIELD, project, baseName, dimensions, new ExcludedCollection(excluded), true);
+	}
+	public static String[] getVariableNameSuggestions(int variableKind, IJavaProject project, String baseName, int dimensions, Collection<String> excluded, boolean evaluateDefault) {
+		return NamingConventions.suggestVariableNames(variableKind, NamingConventions.BK_TYPE_NAME, removeTypeArguments(baseName), project, dimensions, getExcludedArray(excluded), evaluateDefault);
+	}
+	private static class ExcludedCollection extends AbstractList<String> {
+		private String[] fExcluded;
+
+		public ExcludedCollection(String[] excluded) {
+			fExcluded= excluded;
+		}
+
+		public String[] getExcludedArray() {
+			return fExcluded;
+		}
+
+		@Override
+		public int size() {
+			return fExcluded.length;
+		}
+
+		@Override
+		public String get(int index) {
+			return fExcluded[index];
+		}
+
+		@Override
+		public int indexOf(Object o) {
+			if (o instanceof String) {
+				for (int i= 0; i < fExcluded.length; i++) {
+					if (o.equals(fExcluded[i]))
+						return i;
+				}
+			}
+			return -1;
+		}
+
+		@Override
+		public boolean contains(Object o) {
+			return indexOf(o) != -1;
+		}
+	}
+	private static String removeTypeArguments(String baseName) {
+		int idx= baseName.indexOf('<');
+		if (idx != -1) {
+			return baseName.substring(0, idx);
+		}
+		return baseName;
+	}
+	private static String[] getExcludedArray(Collection<String> excluded) {
+		if (excluded == null) {
+			return null;
+		} else if (excluded instanceof ExcludedCollection) {
+			return ((ExcludedCollection)excluded).getExcludedArray();
+		}
+		return excluded.toArray(new String[excluded.size()]);
 	}
 }
