a873aafa2888e5f3b1ba11df84bf5b76e614171e
Federico Jeanne <2205684+fedejeanne@users.noreply.github.com>
2023-08-12 03:18:02 +0200

    Show progress when searching test methods in JUnit run configuration #653 (#675)
    * Show progress when searching test methods in run configuration #653
    
    Move the logic to search for test methods from
    `JUnitLaunchConfigurationTab` to `TestSearchEngine::findTestMethods`.
    Extract the whole logic for caching the results into `TestMethodsCache`
    (new class) and do the whole searching and caching more efficiently i.e.
    only when necessary and showing the progress with a monitor by using
    `ModalContext::run`
    
    Fixes https://github.com/eclipse-jdt/eclipse.jdt.ui/issues/683
    Contributes to https://github.com/eclipse-jdt/eclipse.jdt.ui/issues/653
    
    Co-authored-by: Jeff Johnston <jjohnstn@redhat.com>

--- org.eclipse.jdt.junit/src/org/eclipse/jdt/internal/junit/util/TestSearchEngine.java
+++ org.eclipse.jdt.junit/src/org/eclipse/jdt/internal/junit/util/TestSearchEngine.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2010 IBM Corporation and others.
+ * Copyright (c) 2000, 2023 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -18,14 +18,27 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.SubMonitor;
 
 import org.eclipse.jface.operation.IRunnableContext;
 import org.eclipse.jface.operation.IRunnableWithProgress;
 
+import org.eclipse.jdt.core.IAnnotation;
 import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IMethod;
 import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.Signature;
+import org.eclipse.jdt.core.dom.Modifier;
 
+import org.eclipse.jdt.internal.junit.JUnitCorePlugin;
+import org.eclipse.jdt.internal.junit.Messages;
 import org.eclipse.jdt.internal.junit.launcher.ITestKind;
+import org.eclipse.jdt.internal.junit.launcher.TestKind;
+import org.eclipse.jdt.internal.junit.launcher.TestKindRegistry;
+import org.eclipse.jdt.internal.junit.ui.JUnitMessages;
 
 
 /**
@@ -48,4 +61,124 @@ public class TestSearchEngine extends CoreTestSearchEngine {
 		return result;
 	}
 
+	public static Set<String> findTestMethods(IRunnableContext context, final IJavaProject javaProject, IType type, TestKind testKind) throws InvocationTargetException, InterruptedException {
+		final Set<String> result= new HashSet<>();
+
+		IRunnableWithProgress runnable= progressMonitor -> {
+			try {
+				String message= Messages.format(JUnitMessages.TestSearchEngine_search_message_progress_monitor, type.getElementName());
+				SubMonitor subMonitor= SubMonitor.convert(progressMonitor, message, 1);
+
+				collectMethodNames(type, javaProject, testKind.getId(), result, subMonitor.split(1));
+			} catch (CoreException e) {
+				throw new InvocationTargetException(e);
+			}
+		};
+		context.run(true, true, runnable);
+		return result;
+	}
+
+	private static void collectMethodNames(IType type, IJavaProject javaProject, String testKindId, Set<String> methodNames, IProgressMonitor monitor) throws JavaModelException {
+		if (type == null) {
+			return;
+		}
+
+		SubMonitor subMonitor= SubMonitor.convert(monitor, 3);
+
+		collectDeclaredMethodNames(type, javaProject, testKindId, methodNames);
+		subMonitor.split(1);
+
+		String superclassName= type.getSuperclassName();
+		IType superType= getResolvedType(superclassName, type, javaProject);
+		collectMethodNames(superType, javaProject, testKindId, methodNames, subMonitor.split(1));
+
+		String[] superInterfaceNames= type.getSuperInterfaceNames();
+		subMonitor.setWorkRemaining(superInterfaceNames.length);
+		for (String interfaceName : superInterfaceNames) {
+			superType= getResolvedType(interfaceName, type, javaProject);
+			collectMethodNames(superType, javaProject, testKindId, methodNames, subMonitor.split(1));
+		}
+	}
+
+	private static IType getResolvedType(String typeName, IType type, IJavaProject javaProject) throws JavaModelException {
+		IType resolvedType= null;
+		if (typeName != null) {
+			int pos= typeName.indexOf('<');
+			if (pos != -1) {
+				typeName= typeName.substring(0, pos);
+			}
+			String[][] resolvedTypeNames= type.resolveType(typeName);
+			if (resolvedTypeNames != null && resolvedTypeNames.length > 0) {
+				String[] resolvedTypeName= resolvedTypeNames[0];
+				resolvedType= javaProject.findType(resolvedTypeName[0], resolvedTypeName[1]); // secondary types not found by this API
+			}
+		}
+		return resolvedType;
+	}
+
+	private static void collectDeclaredMethodNames(IType type, IJavaProject javaProject, String testKindId, Set<String> methodNames) throws JavaModelException {
+		IMethod[] methods= type.getMethods();
+		for (IMethod method : methods) {
+			String methodName= method.getElementName();
+			int flags= method.getFlags();
+			// Only include public, non-static, no-arg methods that return void and start with "test":
+			if (Modifier.isPublic(flags) && !Modifier.isStatic(flags) &&
+					method.getNumberOfParameters() == 0 && Signature.SIG_VOID.equals(method.getReturnType()) &&
+					methodName.startsWith("test")) { //$NON-NLS-1$
+				methodNames.add(methodName);
+			}
+			boolean isJUnit3= TestKindRegistry.JUNIT3_TEST_KIND_ID.equals(testKindId);
+			boolean isJUnit5= TestKindRegistry.JUNIT5_TEST_KIND_ID.equals(testKindId);
+			if (!isJUnit3 && !Modifier.isPrivate(flags) && !Modifier.isStatic(flags)) {
+				IAnnotation annotation= method.getAnnotation("Test"); //$NON-NLS-1$
+				if (annotation.exists()) {
+					methodNames.add(methodName + JUnitStubUtility.getParameterTypes(method, false));
+				} else if (isJUnit5) {
+					boolean hasAnyTestAnnotation= method.getAnnotation("TestFactory").exists() //$NON-NLS-1$
+							|| method.getAnnotation("Testable").exists() //$NON-NLS-1$
+							|| method.getAnnotation("TestTemplate").exists() //$NON-NLS-1$
+							|| method.getAnnotation("ParameterizedTest").exists() //$NON-NLS-1$
+							|| method.getAnnotation("RepeatedTest").exists(); //$NON-NLS-1$
+					if (hasAnyTestAnnotation || isAnnotatedWithTestable(method, type, javaProject)) {
+						methodNames.add(methodName + JUnitStubUtility.getParameterTypes(method, false));
+					}
+				}
+			}
+		}
+	}
+
+	// See JUnit5TestFinder.Annotation#annotates also.
+	private static boolean isAnnotatedWithTestable(IMethod method, IType declaringType, IJavaProject javaProject) throws JavaModelException {
+		for (IAnnotation annotation : method.getAnnotations()) {
+			IType annotationType= getResolvedType(annotation.getElementName(), declaringType, javaProject);
+			if (annotationType != null) {
+				if (matchesTestable(annotationType)) {
+					return true;
+				}
+				Set<IType> hierarchy= new HashSet<>();
+				if (matchesTestableInAnnotationHierarchy(annotationType, javaProject, hierarchy)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	private static boolean matchesTestable(IType annotationType) {
+		return annotationType != null && JUnitCorePlugin.JUNIT5_TESTABLE_ANNOTATION_NAME.equals(annotationType.getFullyQualifiedName());
+	}
+
+	private static boolean matchesTestableInAnnotationHierarchy(IType annotationType, IJavaProject javaProject, Set<IType> hierarchy) throws JavaModelException {
+		if (annotationType != null) {
+			for (IAnnotation annotation : annotationType.getAnnotations()) {
+				IType annType= getResolvedType(annotation.getElementName(), annotationType, javaProject);
+				if (annType != null && hierarchy.add(annType)) {
+					if (matchesTestable(annType) || matchesTestableInAnnotationHierarchy(annType, javaProject, hierarchy)) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
 }
---
+++ org.eclipse.jdt.junit/src/org/eclipse/jdt/junit/launcher/TestMethodsCache.java
@@ -0,0 +1,87 @@
+/*******************************************************************************
+ * Copyright (c) 2023 Vector Informatik GmbH and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Vector Informatik GmbH - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.jdt.junit.launcher;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * This class has the necessary logic to calculate the cache of all test methods that belong to a
+ * JUnit configuration.
+ */
+class TestMethodsCache {
+	/**
+	 * An <code>AutoCloseable</code> that can contain nested instances and can run a
+	 * <code>Runnable</code> upon closing the <i>outer</i> instance. <br/>
+	 * <br/>
+	 * Example:
+	 *
+	 * <pre>
+	 *
+	 * try (var outer= new NestedAutoCloseable(() -> System.out.println("Bye bye outer"))) {
+	 * 	try (var inner= new NestedAutoCloseable(() -> System.out.println("Bye bye inner"))) {
+	 * 		// ...
+	 * 	} // doesn't print anything
+	 * } // prints "Bye bye outer"
+	 * </pre>
+	 */
+	private static class NestedAutoCloseable implements AutoCloseable {
+		private static int fgDepth;
+
+		NestedAutoCloseable(Runnable onCreateOuterBlock) {
+			if (fgDepth == 0) {
+				onCreateOuterBlock.run();
+			}
+			fgDepth++;
+		}
+
+		@Override
+		public final void close() {
+			fgDepth--;
+		}
+	}
+
+	private boolean fCanceled;
+
+	private final Map<String, Set<String>> fCacheMap= new HashMap<>();
+
+	void put(String key, Set<String> value) {
+		fCacheMap.put(key, value);
+	}
+
+	Set<String> get(String key) {
+		return fCacheMap.get(key);
+	}
+
+	boolean containsKey(String key) {
+		return fCacheMap.containsKey(key);
+	}
+
+	boolean isCanceled() {
+		return fCanceled;
+	}
+
+	void setCanceled(boolean canceled) {
+		fCanceled= canceled;
+	}
+
+	/**
+	 * @return an <code>AutoCloseable</code> that guarantees that searching for test methods needs
+	 *         to be canceled only once even in nested calls.
+	 */
+	NestedAutoCloseable runNestedCancelable() {
+		return new NestedAutoCloseable(() -> fCanceled= false);
+	}
+}
