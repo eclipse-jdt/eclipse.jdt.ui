53552b3c9a52255e4cf3e2e97e2ec0204cf32d40
Jeff Johnston <jjohnstn@redhat.com>
2023-08-17 14:15:58 -0400

    Add new test

--- org.eclipse.jdt.ui.tests/ui/org/eclipse/jdt/ui/tests/quickfix/AssistQuickFixTest1d8.java
+++ org.eclipse.jdt.ui.tests/ui/org/eclipse/jdt/ui/tests/quickfix/AssistQuickFixTest1d8.java
@@ -6782,5 +6782,41 @@ public class AssistQuickFixTest1d8 extends QuickFixTest {
 		assertExpectedExistInProposals(proposals, new String[] { expected });
 	}
 
+	@Test
+	public void testInlineDeprecated_6() throws Exception {
+		Hashtable<String, String> options= JavaCore.getOptions();
+		options.put(JavaCore.COMPILER_PB_DEPRECATION_WHEN_OVERRIDING_DEPRECATED_METHOD, JavaCore.ENABLED);
+		JavaCore.setOptions(options);
+		IPackageFragment pack1= fSourceFolder.createPackageFragment("test1", false, null);
+		String src=
+				"package test1;\n" +
+				"\n" +
+				"public class E1 {\n" +
+				"    public int v = 5;\n" +
+				"    public int foo(int a, int b) {\n" +
+				"        return a + b;\n" +
+				"    }\n" +
+				"    public int foo(int x, int y, int z) {\n" +
+				"        int k = 2*y + 3*z + v;\n" +
+				"        return foo(x, k);\n" +
+				"    }\n" +
+				"}\n";
+		pack1.createCompilationUnit("E1.java", src, false, null);
+		String src1=
+				"package test1;\n" +
+				"\n" +
+				"class E {\n" +
+				"    public int callfoo(int a, int b, int c) {\n" +
+				"        E1 e1= new E1();\n" +
+				"        return e1.foo(a, b, c);\n" +
+				"    }\n" +
+				"}\n";
+		ICompilationUnit cu1= pack1.createCompilationUnit("E.java", src1, false, null);
+		int offset= src1.indexOf("e1.foo");
+		AssistContext context= getCorrectionContext(cu1, offset + 3, 3);
+		List<IJavaCompletionProposal> proposals= collectAssists(context, false);
+		assertNumberOfProposals(proposals, 0);
+	}
+
 }
 
