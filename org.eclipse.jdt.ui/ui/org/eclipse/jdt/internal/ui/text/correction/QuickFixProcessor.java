40b5a0cec2fc44f85b62003fadc64cc135683464
Noopur Gupta <noopur_gupta@in.ibm.com>
2023-09-15 12:57:27 +0530

    [21][quick fix] Provide quick fix to add default case for exhaustive enhanced switch statement
    https://github.com/eclipse-jdt/eclipse.jdt.ui/issues/783

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/correction/QuickFixProcessor.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/text/correction/QuickFixProcessor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2022 IBM Corporation and others.
+ * Copyright (c) 2000, 2023 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -7,6 +7,10 @@
  * https://www.eclipse.org/legal/epl-2.0/
  *
  * SPDX-License-Identifier: EPL-2.0
+ *
+ * This is an implementation of an early-draft specification developed under the Java
+ * Community Process (JCP) and is made available for testing and evaluation purposes
+ * only. The code is not compatible with any specification of the JCP.
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
@@ -324,6 +328,7 @@ public class QuickFixProcessor implements IQuickFixProcessor {
 			case IProblem.DanglingReference:
 			case IProblem.UnclosedCloseable:
 			case IProblem.PotentiallyUnclosedCloseable:
+			case IProblem.EnhancedSwitchMissingDefault:
 				return true;
 			default:
 				return SuppressWarningsSubProcessor.hasSuppressWarningsProposal(cu.getJavaProject(), problemId)
@@ -782,6 +787,7 @@ public class QuickFixProcessor implements IQuickFixProcessor {
 				break;
 			case IProblem.MissingDefaultCase:
 			case IProblem.SwitchExpressionsYieldMissingDefaultCase:
+			case IProblem.EnhancedSwitchMissingDefault:
 				LocalCorrectionsSubProcessor.addMissingDefaultCaseProposal(context, problem, proposals);
 				break;
 			case IProblem.MissingEnumConstantCaseDespiteDefault:
