eclipse-jdt/eclipse.jdt.ui

commit 816a5d21cf7c29294915c50bd95503e0462eff02

Proposals branch (#815)
- split the proposal logic into core and U moving the vast majority of the logic into the core classes
- maintain existing UI non-core classes as wrappers for stability.
- create IInvocationContextCore class and makes it the default interface for use in the core classes

Signed-off-by: Rob Stryker <stryker@redhat.com>
Co-authored-by: Rob Stryker <stryker@redhat.com>
Co-authored-by: Jeff Johnston <jjohnstn@redhat.com>

org.eclipse.jdt.ui/ui/org/eclipse/jdt/ui/text/java/correction/CUCorrectionProposal.java
@@ -32,6 +32,7 @@ import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchPage;
 
 import org.eclipse.ltk.core.refactoring.Change;
+import org.eclipse.ltk.core.refactoring.NullChange;
 import org.eclipse.ltk.core.refactoring.TextChange;
 
 import org.eclipse.jdt.core.ICompilationUnit;
@@ -64,6 +65,7 @@ public class CUCorrectionProposal extends ChangeCorrectionProposal implements IC
 
 	private boolean fSwitchedEditor;
 	private CUCorrectionProposalCore fProposalCore;
+	private ICompilationUnit cu;
 
 	/**
 	 * Constructs a correction proposal working on a compilation unit with a given text change.
@@ -79,10 +81,22 @@ public class CUCorrectionProposal extends ChangeCorrectionProposal implements IC
 	 */
 	public CUCorrectionProposal(String name, ICompilationUnit cu, TextChange change, int relevance, Image image) {
 		super(name, change, relevance, image);
+		this.cu = cu;
 		if (cu == null) {
 			throw new IllegalArgumentException("Compilation unit must not be null"); //$NON-NLS-1$
 		}
-		fProposalCore = new CUCorrectionProposalCore(this, name, cu, change, relevance);
+	}
+
+	/**
+	 * @since 3.31
+	 * @param name
+	 * @param cu
+	 * @param change
+	 * @param relevance
+	 * @return
+	 */
+	protected CUCorrectionProposalCore createDelegate(String name, ICompilationUnit cu, TextChange change, int relevance) {
+		return new CUCorrectionProposalCore(this, name, cu, change, relevance);
 	}
 
 	/**
@@ -118,6 +132,25 @@ public class CUCorrectionProposal extends ChangeCorrectionProposal implements IC
 	}
 
 	/**
+	 * @since 3.31
+	 * @return the delegate
+	 */
+	protected CUCorrectionProposalCore getDelegate() {
+		if( fProposalCore == null ) {
+			fProposalCore = createDelegate(fName, this.cu, fChange instanceof NullChange ? null : (TextChange)fChange, fRelevance);
+		}
+		return fProposalCore;
+	}
+
+	/**
+	 * @since 3.31
+	 * @param delegate to set
+	 */
+	protected void setDelegate(CUCorrectionProposalCore del) {
+		fProposalCore = del;
+	}
+
+	/**
 	 * Called when the {@link CompilationUnitChange} is initialized. Subclasses can override to add
 	 * text edits to the root edit of the change. Implementors must not access the proposal, e.g.
 	 * not call {@link #getChange()}.
@@ -130,12 +163,12 @@ public class CUCorrectionProposal extends ChangeCorrectionProposal implements IC
 	 * @throws CoreException can be thrown if adding the edits is failing.
 	 */
 	protected void addEdits(IDocument document, TextEdit editRoot) throws CoreException {
-		// empty default implementation
+		getDelegate().addEdits(document, editRoot);
 	}
 
 	@Override
 	public Object getAdditionalProposalInfo(IProgressMonitor monitor) {
-		return fProposalCore.getAdditionalProposalInfo(monitor);
+		return getDelegate().getAdditionalProposalInfo(monitor);
 	}
 
 	@Override
@@ -193,11 +226,14 @@ public class CUCorrectionProposal extends ChangeCorrectionProposal implements IC
 		TextChange change = fProposalCore.getNewChange();
 		// initialize text change
 		IDocument document= change.getCurrentDocument(new NullProgressMonitor());
-		addEdits(document, change.getEdit());
+		getDelegate().addEdits(document, change.getEdit());
 		return change;
 	}
 
 
+	/**
+	 * Clients should not override this method
+	 */
 	@Override
 	protected final Change createChange() throws CoreException {
 		return createTextChange(); // make sure that only text changes are allowed here
@@ -211,7 +247,7 @@ public class CUCorrectionProposal extends ChangeCorrectionProposal implements IC
 	 */
 	@Override
 	public final TextChange getTextChange() throws CoreException {
-		return (TextChange) getChange();
+		return (TextChange) getDelegate().getChange();
 	}
 
 	/**
@@ -220,7 +256,7 @@ public class CUCorrectionProposal extends ChangeCorrectionProposal implements IC
 	 * @return the compilation unit on which the change works
 	 */
 	public final ICompilationUnit getCompilationUnit() {
-		return fProposalCore.getCompilationUnit();
+		return getDelegate().getCompilationUnit();
 	}
 
 	/**
@@ -231,10 +267,25 @@ public class CUCorrectionProposal extends ChangeCorrectionProposal implements IC
 	 *
 	 * @noreference This method is not intended to be referenced by clients.
 	 */
-	public String getPreviewContent() throws CoreException {
-		return getTextChange().getPreviewContent(new NullProgressMonitor());
+	public final String getPreviewContent() throws CoreException {
+		return getDelegate().getTextChange().getPreviewContent(new NullProgressMonitor());
 	}
 
+	@Override
+	public String getName() {
+		if( super.getName() == null || super.getName().isEmpty()) {
+			return getDelegate().getName();
+		}
+		return super.getName();
+	}
+
+	@Override
+	public String getCommandId() {
+		if( super.getCommandId() == null || super.getCommandId().isEmpty()) {
+			return getDelegate().getCommandId();
+		}
+		return super.getCommandId();
+	}
 	@Override
 	public String toString() {
 		try {

commit 4dfce0ad5d0c27872964d392ef118bfbe43a1ddb

Javadoc error fix for CUCorrectionProposal.setDelegate()
See https://download.eclipse.org/eclipse/downloads/drops4/I20230925-1800/compilelogs/jdt.doc.isv.javadoc.txt

../../../eclipse.jdt.ui/org.eclipse.jdt.ui/ui/org/eclipse/jdt/ui/text/java/correction/CUCorrectionProposal.java:147:
error: @param name not found
	 * @param delegate to set
	          ^

Regression from 816a5d21cf7c29294915c50bd95503e0462eff02

org.eclipse.jdt.ui/ui/org/eclipse/jdt/ui/text/java/correction/CUCorrectionProposal.java
@@ -146,8 +146,8 @@ public class CUCorrectionProposal extends ChangeCorrectionProposal implements IC
 	 * @since 3.31
 	 * @param delegate to set
 	 */
-	protected void setDelegate(CUCorrectionProposalCore del) {
-		fProposalCore = del;
+	protected void setDelegate(CUCorrectionProposalCore delegate) {
+		fProposalCore = delegate;
 	}
 
 	/**