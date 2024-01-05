34751395d3fc8b669d1f623bffc1cc3fb90be4d7
Jeff Johnston <jjohnstn@redhat.com>
2024-01-05 18:37:35 -0500

    Fix ExtractConstantRefactoring to recognize no type is available
    - fixes #1023
    - add new test to ExtractConstantTests1d7.java

--- org.eclipse.jdt.core.manipulation/core extension/org/eclipse/jdt/internal/corext/refactoring/RefactoringCoreMessages.java
+++ org.eclipse.jdt.core.manipulation/core extension/org/eclipse/jdt/internal/corext/refactoring/RefactoringCoreMessages.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2022 IBM Corporation and others.
+ * Copyright (c) 2000, 2023 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -454,6 +454,8 @@ public final class RefactoringCoreMessages extends NLS {
 	public static String ExtractConstantRefactoring_descriptor_description_short;
 
 	public static String ExtractConstantRefactoring_name;
+
+	public static String ExtractConstantRefactoring_no_type;
 
 	public static String ExtractConstantRefactoring_no_void;
 
