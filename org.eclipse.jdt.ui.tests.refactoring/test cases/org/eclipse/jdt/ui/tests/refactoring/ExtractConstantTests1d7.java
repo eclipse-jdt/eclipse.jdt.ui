d513389aad3f99ac11e328d6fff4a7cfb66d98d5
Jeff Johnston <jjohnstn@redhat.com>
2024-01-08 15:58:44 -0500

    Fix ExtractConstantRefactoring to recognize no type is available (#1046)
    - fixes #1023
    - add new test to ExtractConstantTests1d7.java

--- org.eclipse.jdt.ui.tests.refactoring/test cases/org/eclipse/jdt/ui/tests/refactoring/ExtractConstantTests1d7.java
+++ org.eclipse.jdt.ui.tests.refactoring/test cases/org/eclipse/jdt/ui/tests/refactoring/ExtractConstantTests1d7.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 IBM Corporation and others.
+ * Copyright (c) 2011, 2023 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -13,11 +13,23 @@
  *******************************************************************************/
 package org.eclipse.jdt.ui.tests.refactoring;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import org.eclipse.ltk.core.refactoring.RefactoringStatus;
+
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.ISourceRange;
+
+import org.eclipse.jdt.internal.corext.refactoring.RefactoringCoreMessages;
+import org.eclipse.jdt.internal.corext.refactoring.code.ExtractConstantRefactoring;
+
 import org.eclipse.jdt.ui.tests.CustomBaseRunner;
 import org.eclipse.jdt.ui.tests.IgnoreInheritedTests;
+import org.eclipse.jdt.ui.tests.refactoring.infra.TextRangeUtil;
 import org.eclipse.jdt.ui.tests.refactoring.rules.Java1d7Setup;
 
 /**
@@ -37,6 +49,21 @@ public class ExtractConstantTests1d7 extends ExtractConstantTests {
 		return fileName.append(getSimpleTestFileName(canExtract, input)).toString();
 	}
 
+	private void failHelper2(int startLine, int startColumn, int endLine, int endColumn, boolean replaceAll, boolean allowLoadtime, String constantName, String errorMsg, boolean checkMsg) throws Exception{
+		ICompilationUnit cu= createCU(getPackageP(), "package-info.java", getFileContents(getTestFileName(false, true)));
+		ISourceRange selection= TextRangeUtil.getSelection(cu, startLine, startColumn, endLine, endColumn);
+		ExtractConstantRefactoring ref= new ExtractConstantRefactoring(cu, selection.getOffset(), selection.getLength());
+		ref.setReplaceAllOccurrences(replaceAll);
+		ref.setConstantName(constantName);
+		RefactoringStatus result= performRefactoring(ref);
+
+		if(!allowLoadtime && !ref.selectionAllStaticFinal())
+			return;
+
+		assertNotNull("precondition was supposed to fail", result);
+		if(checkMsg)
+			assertTrue(errorMsg.equals(result.getEntryMatchingSeverity(RefactoringStatus.FATAL).getMessage()));
+	}
 	//--- TESTS
 
 	// -- testing failing preconditions
@@ -44,5 +71,10 @@ public class ExtractConstantTests1d7 extends ExtractConstantTests {
 	@Test
 	public void testFail0() throws Exception{
 		failHelper1(10, 14, 10, 56, true, true, "CONSTANT");
+	}
+
+	@Test
+	public void testFailNoType() throws Exception{
+		failHelper2(2, 41, 2, 48, true, true, "CONSTANT", RefactoringCoreMessages.ExtractConstantRefactoring_no_type, true);
 	}
 }
