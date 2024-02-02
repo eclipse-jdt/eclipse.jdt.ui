af2e418807f7bc34cdef3a03349ecf1746e1a988
Jeff Johnston <jjohnstn@redhat.com>
2024-02-01 20:12:23 -0500

    Add missing synchronization for ChangeCorrectionProposalCore (#1162)
    - add missing synchronization for getCurrentChange() which accesses
      fChange which is created in a synchronized method
    - potential fix for #1136

--- org.eclipse.jdt.core.manipulation/common/org/eclipse/jdt/core/manipulation/ChangeCorrectionProposalCore.java
+++ org.eclipse.jdt.core.manipulation/common/org/eclipse/jdt/core/manipulation/ChangeCorrectionProposalCore.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018 IBM Corporation and others.
+ * Copyright (c) 2018, 2024 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -161,7 +161,9 @@ public class ChangeCorrectionProposalCore implements IAdaptable {
 	 * @return The current change, or null if not initialized yet
 	 */
 	public Change getCurrentChange() {
-		return fChange;
+		synchronized (this) {
+			return fChange;
+		}
 	}
 
 
