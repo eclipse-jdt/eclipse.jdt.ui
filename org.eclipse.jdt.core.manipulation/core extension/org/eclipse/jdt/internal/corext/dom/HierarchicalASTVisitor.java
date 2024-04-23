03e4d5e73855c1c0fc6e49ae17d5047ece6188c6
Noopur Gupta <noopur_gupta@in.ibm.com>
2024-04-23 15:19:06 +0530

    [23] Remove String Template preview feature from JDT UI
    Fixes https://github.com/eclipse-jdt/eclipse.jdt.ui/issues/1362

--- org.eclipse.jdt.core.manipulation/core extension/org/eclipse/jdt/internal/corext/dom/HierarchicalASTVisitor.java
+++ org.eclipse.jdt.core.manipulation/core extension/org/eclipse/jdt/internal/corext/dom/HierarchicalASTVisitor.java
@@ -7,6 +7,10 @@
  * https://www.eclipse.org/legal/epl-2.0/
  *
  * SPDX-License-Identifier: EPL-2.0
+ * 
+ * This is an implementation of an early-draft specification developed under the Java
+ * Community Process (JCP) and is made available for testing and evaluation purposes
+ * only. The code is not compatible with any specification of the JCP.
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
@@ -738,36 +742,6 @@ public abstract class HierarchicalASTVisitor extends ASTVisitor {
 	@Override
 	public void endVisit(StringLiteral node) {
 		endVisit((Expression)node);
-	}
-
-	@Override
-	public boolean visit(StringTemplateExpression node) {
-		return visit((Expression) node);
-	}
-
-	@Override
-	public boolean visit(StringTemplateComponent node) {
-		return visit((Expression) node);
-	}
-
-	@Override
-	public boolean visit(StringFragment node) {
-		return visit((Expression) node);
-	}
-
-	@Override
-	public void endVisit(StringTemplateExpression node) {
-		endVisit((Expression) node);
-	}
-
-	@Override
-	public void endVisit(StringTemplateComponent node) {
-		endVisit((Expression) node);
-	}
-
-	@Override
-	public void endVisit(StringFragment node) {
-		endVisit((Expression) node);
 	}
 
 	@Override
