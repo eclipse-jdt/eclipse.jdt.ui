5bf7d728946c15e8030b7895bd9b710dd4a41e7d
Jeff Johnston <jjohnstn@redhat.com>
2024-06-07 17:41:09 -0400

    Fix move method to handle field access modifier adjustment (#1427)
    * Fix move method to handle field access modifier adjustment
    
    - fix MoveInstanceMethodProcessor.createMethodBody() to perform any
      needed adjustments to field modifiers if required by the move
    - add new test to MoveInstanceMethodTests
    - fixes #1297

---
+++ org.eclipse.jdt.ui.tests.refactoring/resources/MoveInstanceMethod/canMove/test79/in/A.java
@@ -0,0 +1,18 @@
+class B {
+    public void f() {
+
+    }
+}
+
+class A {
+    B b;
+    private int c;
+    
+    public void m() {
+        n(c);
+    }
+    
+    private void n(int x) {
+        
+    }
+}
