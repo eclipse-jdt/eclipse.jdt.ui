3fdf5d5357a938bb8d0ccf123ec98073b6ba35af
Noopur Gupta <noopur_gupta@in.ibm.com>
2024-09-16 12:06:47 +0530

    [23] Remove beta and disclaimers from copyright notes and UI
    Fixes https://github.com/eclipse-jdt/eclipse.jdt.ui/issues/1647

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/preferences/ComplianceConfigurationBlock.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/preferences/ComplianceConfigurationBlock.java
@@ -8,10 +8,6 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  *
- * This is an implementation of an early-draft specification developed under the Java
- * Community Process (JCP) and is made available for testing and evaluation purposes
- * only. The code is not compatible with any specification of the JCP.
- *
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *     Jesper S Møller - Bug 529432 - Allow JDT UI to target Java 10
@@ -705,8 +701,10 @@ public class ComplianceConfigurationBlock extends OptionsConfigurationBlock {
 		}
 	}
 
+	@SuppressWarnings("unused")
 	protected boolean isBetaVersion(String compliance) {
-		return JavaCore.VERSION_23.equals(compliance);
+		return false;
+//		return JavaCore.VERSION_23.equals(compliance);
 	}
 
 	private String addsExportToSystemModule() {
