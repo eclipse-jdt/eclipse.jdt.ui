64ae4a641278cc444769d1fe0b04b89d9ba8c297
Александър Куртаков <akurtakov@gmail.com>
2024-09-17 21:59:23 +0300

    Move away of deprecated CopyProjectAction(Shell)
    Use CopyProjectAction(IShellProvider) instead.

--- org.eclipse.jdt.ui/ui refactoring/org/eclipse/jdt/internal/ui/refactoring/reorg/ReorgCopyAction.java
+++ org.eclipse.jdt.ui/ui refactoring/org/eclipse/jdt/internal/ui/refactoring/reorg/ReorgCopyAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2023 IBM Corporation and others.
+ * Copyright (c) 2000, 2024 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -76,7 +76,7 @@ public class ReorgCopyAction extends SelectionDispatchAction {
 	}
 
 	private CopyProjectAction createWorkbenchAction(IStructuredSelection selection) {
-		CopyProjectAction action= new CopyProjectAction(getShell());
+		CopyProjectAction action= new CopyProjectAction(getSite());
 		action.selectionChanged(selection);
 		return action;
 	}
