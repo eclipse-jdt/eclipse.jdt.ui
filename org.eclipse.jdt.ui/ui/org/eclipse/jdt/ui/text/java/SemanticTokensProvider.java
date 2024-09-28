480dc85db43d3824c9fd4cfddd5db96a0047b3cb
aboyko <aboyko@vmware.com>
2024-09-27 20:05:32 -0400

    Cleanup

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/ui/text/java/SemanticTokensProvider.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/ui/text/java/SemanticTokensProvider.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2024 Broadcom Inc. and others.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *  - Alex Boyko (Broadcom Inc.) - Initial implementation
+ *******************************************************************************/
 package org.eclipse.jdt.ui.text.java;
 
 import java.util.Collection;
