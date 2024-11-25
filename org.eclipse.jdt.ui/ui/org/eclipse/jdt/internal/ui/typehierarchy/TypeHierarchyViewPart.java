06958fed001b21f4aced01a1b26508725a026cb8
jannisCode <jannis.wunderlich@vector.com>
2024-11-25 09:53:11 +0100

    it is now possible to expand the tree below in AbstractTreeViewer

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/typehierarchy/TypeHierarchyViewPart.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/typehierarchy/TypeHierarchyViewPart.java
@@ -642,10 +642,10 @@ public class TypeHierarchyViewPart extends ViewPart implements ITypeHierarchyVie
 		setMemberFilter(null);
 		internalSelectType(null, false); // clear selection
 		fIsEnableMemberFilter= false;
-		updateHierarchyViewer(true);
 		IType root= getSelectableType(fInputElements);
 		internalSelectType(root, true);
 		updateMethodViewer(root);
+		updateHierarchyViewer(true);
 		updateToolbarButtons();
 		updateToolTipAndDescription();
 		showMembersInHierarchy(false);
