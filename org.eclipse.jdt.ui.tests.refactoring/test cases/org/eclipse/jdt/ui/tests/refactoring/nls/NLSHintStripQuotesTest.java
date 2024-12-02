b85953c29a76fdc53643089db93983de31cd1721
Christian Schima <eclipse@schima24.de>
2024-12-02 22:47:18 +0100

    Bugfix for #1803: [Externalize Strings] IndexOutOfBoundsException.

--- org.eclipse.jdt.ui.tests.refactoring/test cases/org/eclipse/jdt/ui/tests/refactoring/nls/NLSHintStripQuotesTest.java
+++ org.eclipse.jdt.ui.tests.refactoring/test cases/org/eclipse/jdt/ui/tests/refactoring/nls/NLSHintStripQuotesTest.java
@@ -37,84 +37,78 @@ public class NLSHintStripQuotesTest {
 	@Rule
 	public Java15ProjectTestSetup pts15= new Java15ProjectTestSetup();
 
-	private IJavaProject javaProject;
-	private IJavaProject javaProject15;
+    private IJavaProject javaProject;
+    private IJavaProject javaProject15;
 
-	@Before
+
+    @Before
 	public void setUp() throws Exception {
-		javaProject= pts.getProject();
-		javaProject15= pts15.getProject();
-	}
+        javaProject= pts.getProject();
+        javaProject15= pts15.getProject();
+    }
 
-	@After
+    @After
 	public void tearDown() throws Exception {
-		JavaProjectHelper.clear(javaProject, pts.getDefaultClasspath());
-		JavaProjectHelper.clear(javaProject15, pts15.getDefaultClasspath());
-	}
+        JavaProjectHelper.clear(javaProject, pts.getDefaultClasspath());
+        JavaProjectHelper.clear(javaProject15, pts15.getDefaultClasspath());
+    }
 
-	@Test
-	public void test01() throws Exception {
-		String x= "\"abc\n\"";
-		String y= NLSHint.stripQuotes(x, javaProject.getJavaProject());
-		String expected= "abc\n";
-		assertEquals(expected, y);
-	}
-
-	@Test
-	public void test02() throws Exception {
-		String x= "\"\"\"abc\n\"\"\"";
-		String y= NLSHint.stripQuotes(x, javaProject.getJavaProject());
-		String expected= "\"\"abc\n\"\"";
-		assertEquals(expected, y);
-	}
-
-	@Test
-	public void test03() throws Exception {
-		String x="\"\"\"\nabc\ndef\n\"\"\"";
-		String y= NLSHint.stripQuotes(x, javaProject15.getJavaProject());
-		String expected= "abc\ndef\n";
-		assertEquals(expected, y);
-	}
-
-	@Test
-	public void test04() throws Exception {
-		String x="\"\"\" \nabc\\s\ndef\n\"\"\"";
-		String y= NLSHint.stripQuotes(x, javaProject15.getJavaProject());
-		String expected= "abc \ndef\n";
-		assertEquals(expected, y);
-	}
-
-	@Test
-	public void test05() throws Exception {
-		String x="\"\"\" \n    abc\\s\n    def\n    \"\"\"";
-		String y= NLSHint.stripQuotes(x, javaProject15.getJavaProject());
-		String expected= "abc \ndef\n";
-		assertEquals(expected, y);
-	}
-
-	@Test
-	public void test06() throws Exception {
-		String x="\"\"\" \nabc  \\s\ndef\n\"\"\"";
-		String y= NLSHint.stripQuotes(x, javaProject15.getJavaProject());
-		String expected= "abc   \ndef\n";
-		assertEquals(expected, y);
-	}
-
-	@Test
-	public void test07() throws Exception {
-		String x="\"\"\" \nabc  \\\ndef\n\"\"\"";
-		String y= NLSHint.stripQuotes(x, javaProject15.getJavaProject());
-		String expected= "abc  def\n";
-		assertEquals(expected, y);
-	}
-
-	@Test
-	public void test08() throws Exception {
-		String x="\"\"\" \n   abc\ndef\n\"\"\"";
-		String y= NLSHint.stripQuotes(x, javaProject15.getJavaProject());
-		String expected= "   abc\ndef\n";
-		assertEquals(expected, y);
-	}
+    @Test
+    public void test01() throws Exception {
+    	String x= "\"abc\n\"";
+    	String y= NLSHint.stripQuotes(x, javaProject.getJavaProject());
+    	String expected= "abc\n";
+    	assertEquals(expected, y);
+    }
+    @Test
+    public void test02() throws Exception {
+    	String x= "\"\"\"abc\n\"\"\"";
+    	String y= NLSHint.stripQuotes(x, javaProject.getJavaProject());
+    	String expected= "\"\"abc\n\"\"";
+    	assertEquals(expected, y);
+    }
+    @Test
+    public void test03() throws Exception {
+    	String x="\"\"\"\nabc\ndef\n\"\"\"";
+    	String y= NLSHint.stripQuotes(x, javaProject15.getJavaProject());
+    	String expected= "abc\ndef\n";
+    	assertEquals(expected, y);
+    }
+    @Test
+    public void test04() throws Exception {
+    	String x="\"\"\" \nabc\\s\ndef\n\"\"\"";
+    	String y= NLSHint.stripQuotes(x, javaProject15.getJavaProject());
+    	String expected= "abc \ndef\n";
+    	assertEquals(expected, y);
+    }
+    @Test
+    public void test05() throws Exception {
+    	String x="\"\"\" \n    abc\\s\n    def\n    \"\"\"";
+    	String y= NLSHint.stripQuotes(x, javaProject15.getJavaProject());
+    	String expected= "abc \ndef\n";
+    	assertEquals(expected, y);
+    }
+    @Test
+    public void test06() throws Exception {
+    	String x="\"\"\" \nabc  \\s\ndef\n\"\"\"";
+    	String y= NLSHint.stripQuotes(x, javaProject15.getJavaProject());
+    	String expected= "abc   \ndef\n";
+    	assertEquals(expected, y);
+    }
+    @Test
+    public void test07() throws Exception {
+    	String x="\"\"\" \nabc  \\\ndef\n\"\"\"";
+    	String y= NLSHint.stripQuotes(x, javaProject15.getJavaProject());
+    	String expected= "abc  def\n";
+    	assertEquals(expected, y);
+    }
+    @Test
+    public void test08() throws Exception {
+    	String x="\"\"\" \n   abc\ndef\n\"\"\"";
+    	String y= NLSHint.stripQuotes(x, javaProject15.getJavaProject());
+    	String expected= "   abc\ndef\n";
+    	assertEquals(expected, y);
+    }
 
 	@Test
 	public void test09() throws Exception {
