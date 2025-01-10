08d6d6ade0defdd6bba62f5c9f4011586efa5f67
Jörg Kubitz <joerg.kubitz@ssi-schaefer.com>
2025-01-10 08:10:36 +0100

    Progress: add missing license header in Progress.java #1920
    https://github.com/eclipse-jdt/eclipse.jdt.ui/issues/1920

--- org.eclipse.jdt.core.manipulation/common/org/eclipse/jdt/internal/ui/util/Progress.java
+++ org.eclipse.jdt.core.manipulation/common/org/eclipse/jdt/internal/ui/util/Progress.java
@@ -1,3 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2023 SSI and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     SSI - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.jdt.internal.ui.util;
 
 import org.eclipse.core.runtime.IProgressMonitor;
