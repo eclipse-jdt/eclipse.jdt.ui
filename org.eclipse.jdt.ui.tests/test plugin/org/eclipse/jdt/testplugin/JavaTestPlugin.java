8e287dae2d3dc86798ba3be150a4df73c69d8628
Александър Куртаков <akurtakov@gmail.com>
2025-01-16 07:32:47 +0200

    Stop using deprecated URL constructors in o.e.jdt.ui.tests
    * Let Bundle.getEntry do full resolving
    * If it's actually used to openConnection go through URI creation.

--- org.eclipse.jdt.ui.tests/test plugin/org/eclipse/jdt/testplugin/JavaTestPlugin.java
+++ org.eclipse.jdt.ui.tests/test plugin/org/eclipse/jdt/testplugin/JavaTestPlugin.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2017 IBM Corporation and others.
+ * Copyright (c) 2000, 2025 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -48,7 +48,7 @@ public class JavaTestPlugin extends AbstractUIPlugin {
 
 	public File getFileInPlugin(IPath path) throws CoreException {
 		try {
-			URL installURL= new URL(getBundle().getEntry("/"), path.toString());
+			URL installURL= getBundle().getEntry("/"+ path.toString());
 			URL localURL= FileLocator.toFileURL(installURL);
 			return new File(localURL.getFile());
 		} catch (IOException e) {
