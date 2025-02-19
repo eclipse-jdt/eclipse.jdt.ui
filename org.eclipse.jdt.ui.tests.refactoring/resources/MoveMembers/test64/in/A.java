5cd74043cb669a1e8ee84d81274f304e6e400ac6
Jeff Johnston <jjohnstn@redhat.com>
2025-02-18 23:17:46 -0500

    Add checking for import collision when moving methods
    - modify MoveStaticMembersProcessor and MoveInstanceMethodProcessor
      to look for simple types that collide with classes that are already
      imported in the target
    - add new test to MoveMembersTests and MoveInstanceMethodTests
    - fixes #2030

---
+++ org.eclipse.jdt.ui.tests.refactoring/resources/MoveMembers/test64/in/A.java
@@ -0,0 +1,18 @@
+package p1;
+
+import p2.B;
+import p1.a.T;
+
+public class A {
+	public B fB;
+
+	/**
+	 * This is a comment
+	 */
+	public static T m() {
+		System.out.println("abc");
+		return new T();
+	}
+	
+	public void mA2() {}
+}
---
+++ org.eclipse.jdt.ui.tests.refactoring/resources/MoveMembers/test64/in/a/T.java
@@ -0,0 +1,5 @@
+package p1.a;
+
+public class T {
+
+}
