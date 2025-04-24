8030b67a49547dd82da32b1cfdb45afaf2edf570
Jeff Johnston <jjohnstn@redhat.com>
2025-04-24 15:10:47 -0400

    Add caching of cu and time stamp checking
    - also fix for-loop with no expression

--- org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/javaeditor/JavaStickyLinesProvider.java
+++ org.eclipse.jdt.ui/ui/org/eclipse/jdt/internal/ui/javaeditor/JavaStickyLinesProvider.java
@@ -13,13 +13,17 @@
  *******************************************************************************/
 package org.eclipse.jdt.internal.ui.javaeditor;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.eclipse.swt.custom.StyledText;
 
+import org.eclipse.jface.text.AbstractDocument;
+import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.ITextViewerExtension5;
 import org.eclipse.jface.text.source.ISourceViewer;
 
@@ -68,8 +72,12 @@ public class JavaStickyLinesProvider implements IStickyLinesProvider {
 	private final static Pattern ELSE_PATTERN= Pattern.compile("else[\\s,{]"); //$NON-NLS-1$
 	private final static Pattern DO_PATTERN= Pattern.compile("do[\\s,{]"); //$NON-NLS-1$
 	private final static Pattern WHILE_PATTERN= Pattern.compile("while[\\s,{]"); //$NON-NLS-1$
+	private final static Pattern FOR_PATTERN= Pattern.compile("for[\\s,{]"); //$NON-NLS-1$
 	private final static Pattern TRY_PATTERN= Pattern.compile("try[\\s,{]"); //$NON-NLS-1$
 	private final static Pattern NEW_PATTERN= Pattern.compile("new[\\s,{]"); //$NON-NLS-1$
+
+	private static Map<ITypeRoot, CompilationUnit> cuMap= new HashMap<>();
+	private static Map<ITypeRoot, Long> timeMap= new HashMap<>();
 
 	@Override
 	public List<IStickyLine> getStickyLines(ISourceViewer sourceViewer, int lineNumber, StickyLinesProperties properties) {
@@ -96,163 +104,182 @@ public class JavaStickyLinesProvider implements IStickyLinesProvider {
 
 		if (textWidgetLineNumber > 0) {
 			ITypeRoot typeRoot= getJavaInput(javaEditor);
-
 			ASTNode node= null;
 			if (typeRoot != null) {
 				WorkingCopyOwner workingCopyOwner= new WorkingCopyOwner() {
 				};
+				CompilationUnit cu= cuMap.get(typeRoot);
+				IDocument document= sourceViewer.getDocument();
+				long currTime= getDocumentTimestamp(document);
+				if (cu != null && !typeRoot.isReadOnly()) {
+					Long oldTime= timeMap.get(typeRoot);
+					if (oldTime == null || currTime != oldTime.longValue()) {
+						cu= null;
+					}
+				}
 				try {
-					unit= typeRoot.getWorkingCopy(workingCopyOwner, null);
-					if (unit != null) {
-						CompilationUnit cu= convertICompilationUnitToCompilationUnit(unit);
-						node= getASTNode(cu, mapWidgetToLineNumber(sourceViewer, textWidgetLineNumber)+1, line);
-						while (node == null && textWidgetLineNumber > 0) {
+					if (cu == null) {
+						unit= typeRoot.getWorkingCopy(workingCopyOwner, null);
+						if (unit != null) {
+							cu= convertICompilationUnitToCompilationUnit(unit);
+						}
+					}
+					if (cu == null) {
+						return stickyLines;
+					}
+					cuMap.put(typeRoot, cu);
+					timeMap.put(typeRoot, Long.valueOf(currTime));
+					node= getASTNode(cu, mapWidgetToLineNumber(sourceViewer, textWidgetLineNumber)+1, line);
+					while (node == null && textWidgetLineNumber > 0) {
+						line= textWidget.getLine(--textWidgetLineNumber);
+						startIndentation= getIndentation(line);
+						while (startIndentation == IGNORE_LINE_INDENTATION && textWidgetLineNumber > 0) {
 							line= textWidget.getLine(--textWidgetLineNumber);
 							startIndentation= getIndentation(line);
-							while (startIndentation == IGNORE_LINE_INDENTATION && textWidgetLineNumber > 0) {
-								line= textWidget.getLine(--textWidgetLineNumber);
-								startIndentation= getIndentation(line);
-							}
-							if (textWidgetLineNumber > 0) {
-								int position= cu.getPosition(mapWidgetToLineNumber(sourceViewer, textWidgetLineNumber) + 1, startIndentation);
-								if (position >= 0) {
-									node= getASTNode(cu, mapWidgetToLineNumber(sourceViewer, textWidgetLineNumber)+1, line);
-								}
+						}
+						if (textWidgetLineNumber > 0) {
+							int position= cu.getPosition(mapWidgetToLineNumber(sourceViewer, textWidgetLineNumber) + 1, startIndentation);
+							if (position >= 0) {
+								node= getASTNode(cu, mapWidgetToLineNumber(sourceViewer, textWidgetLineNumber)+1, line);
 							}
 						}
-						if (node != null) {
-							boolean addStickyLine= false;
-							int nodeLineNumber= 0;
-							while (node != null) {
-								addStickyLine= false;
-								switch (node.getNodeType()) {
-									case ASTNode.ANNOTATION_TYPE_DECLARATION:
-									case ASTNode.TYPE_DECLARATION:
-									case ASTNode.ENUM_DECLARATION:
-										addStickyLine= true;
-										ASTNode name= ((AbstractTypeDeclaration)node).getName();
-										nodeLineNumber= cu.getLineNumber(name.getStartPosition());
-										break;
-									case ASTNode.TYPE_DECLARATION_STATEMENT:
-										addStickyLine= true;
-										ASTNode typeDeclStmtName= ((TypeDeclarationStatement)node).getDeclaration().getName();
-										nodeLineNumber= cu.getLineNumber(typeDeclStmtName.getStartPosition());
-										break;
-									case ASTNode.METHOD_DECLARATION:
-										addStickyLine= true;
-										ASTNode methodName= ((MethodDeclaration)node).getName();
-										nodeLineNumber= cu.getLineNumber(methodName.getStartPosition());
-										break;
-									case ASTNode.RECORD_DECLARATION:
-										addStickyLine= true;
-										ASTNode recordName= ((RecordDeclaration)node).getName();
-										nodeLineNumber= cu.getLineNumber(recordName.getStartPosition());
-										break;
-									case ASTNode.MODULE_DECLARATION:
-										addStickyLine= true;
-										ASTNode moduleName= ((ModuleDeclaration)node).getName();
-										nodeLineNumber= cu.getLineNumber(moduleName.getStartPosition());
-										break;
-									case ASTNode.LAMBDA_EXPRESSION:
-										addStickyLine= true;
-										ASTNode lambdaBody= ((LambdaExpression)node).getBody();
-										nodeLineNumber= cu.getLineNumber(lambdaBody.getStartPosition());
-										break;
-									case ASTNode.IF_STATEMENT:
-										addStickyLine= true;
-										IfStatement ifStmt= (IfStatement)node;
-										ASTNode ifExpression= ifStmt.getExpression();
-										nodeLineNumber= cu.getLineNumber(ifExpression.getStartPosition());
-										Statement elseStmt= ifStmt.getElseStatement();
-										if (elseStmt != null) {
-											int elseLine= cu.getLineNumber(elseStmt.getStartPosition());
-											if (elseLine <= mapWidgetToLineNumber(sourceViewer, textWidgetLineNumber + 1)) {
-												Pattern p= ELSE_PATTERN;
-												nodeLineNumber= elseLine;
-												String stmtLine= textWidget.getLine(mapLineNumberToWidget(sourceViewer, nodeLineNumber - 1));
-												Matcher m= p.matcher(stmtLine);
-												while (!m.find() && nodeLineNumber > 1) {
-													nodeLineNumber--;
-													stmtLine= textWidget.getLine(mapLineNumberToWidget(sourceViewer, nodeLineNumber - 1));
-													m= p.matcher(stmtLine);
-												}
-												node= node.getParent();
-											}
-										}
-										while (node.getLocationInParent() == IfStatement.ELSE_STATEMENT_PROPERTY) {
-											node= node.getParent();
-										}
-										break;
-									case ASTNode.FOR_STATEMENT:
-										addStickyLine= true;
-										ASTNode forExpression= ((ForStatement)node).getExpression();
-										nodeLineNumber= cu.getLineNumber(forExpression.getStartPosition());
-										break;
-									case ASTNode.ENHANCED_FOR_STATEMENT:
-										addStickyLine= true;
-										ASTNode enhancedForExpression= ((EnhancedForStatement)node).getExpression();
-										nodeLineNumber= cu.getLineNumber(enhancedForExpression.getStartPosition());
-										break;
-									case ASTNode.SWITCH_EXPRESSION:
-										addStickyLine= true;
-										ASTNode switchExpExpression= ((SwitchExpression)node).getExpression();
-										nodeLineNumber= cu.getLineNumber(switchExpExpression.getStartPosition());
-										break;
-									case ASTNode.SWITCH_STATEMENT:
-										addStickyLine= true;
-										ASTNode switchStmtExpression= ((SwitchStatement)node).getExpression();
-										nodeLineNumber= cu.getLineNumber(switchStmtExpression.getStartPosition());
-										break;
-									case ASTNode.WHILE_STATEMENT:
-									case ASTNode.DO_STATEMENT:
-									case ASTNode.TRY_STATEMENT:
-									case ASTNode.ANONYMOUS_CLASS_DECLARATION:
-										addStickyLine= true;
-										ASTNode bodyProperty= null;
-										Pattern pattern= null;
-										if (node.getNodeType() == ASTNode.DO_STATEMENT) {
-											bodyProperty= ((DoStatement)node).getBody();
-											pattern= DO_PATTERN;
-										} else if (node.getNodeType() == ASTNode.WHILE_STATEMENT) {
-											bodyProperty= ((WhileStatement)node).getBody();
-											pattern= WHILE_PATTERN;
-										} else if (node.getNodeType() == ASTNode.TRY_STATEMENT) {
-											bodyProperty= ((TryStatement)node).getBody();
-											pattern= TRY_PATTERN;
-										} else if (node.getNodeType() == ASTNode.ANONYMOUS_CLASS_DECLARATION) {
-											bodyProperty= (ASTNode) ((AnonymousClassDeclaration)node).bodyDeclarations().get(0);
-											pattern= NEW_PATTERN;
-										}
-										if (bodyProperty != null && pattern != null) {
-											nodeLineNumber= cu.getLineNumber(bodyProperty.getStartPosition());
+					}
+					if (node != null) {
+						boolean addStickyLine= false;
+						int nodeLineNumber= 0;
+						while (node != null) {
+							addStickyLine= false;
+							switch (node.getNodeType()) {
+								case ASTNode.ANNOTATION_TYPE_DECLARATION:
+								case ASTNode.TYPE_DECLARATION:
+								case ASTNode.ENUM_DECLARATION:
+									addStickyLine= true;
+									ASTNode name= ((AbstractTypeDeclaration)node).getName();
+									nodeLineNumber= cu.getLineNumber(name.getStartPosition());
+									break;
+								case ASTNode.TYPE_DECLARATION_STATEMENT:
+									addStickyLine= true;
+									ASTNode typeDeclStmtName= ((TypeDeclarationStatement)node).getDeclaration().getName();
+									nodeLineNumber= cu.getLineNumber(typeDeclStmtName.getStartPosition());
+									break;
+								case ASTNode.METHOD_DECLARATION:
+									addStickyLine= true;
+									ASTNode methodName= ((MethodDeclaration)node).getName();
+									nodeLineNumber= cu.getLineNumber(methodName.getStartPosition());
+									break;
+								case ASTNode.RECORD_DECLARATION:
+									addStickyLine= true;
+									ASTNode recordName= ((RecordDeclaration)node).getName();
+									nodeLineNumber= cu.getLineNumber(recordName.getStartPosition());
+									break;
+								case ASTNode.MODULE_DECLARATION:
+									addStickyLine= true;
+									ASTNode moduleName= ((ModuleDeclaration)node).getName();
+									nodeLineNumber= cu.getLineNumber(moduleName.getStartPosition());
+									break;
+								case ASTNode.LAMBDA_EXPRESSION:
+									addStickyLine= true;
+									ASTNode lambdaBody= ((LambdaExpression)node).getBody();
+									nodeLineNumber= cu.getLineNumber(lambdaBody.getStartPosition());
+									break;
+								case ASTNode.IF_STATEMENT:
+									addStickyLine= true;
+									IfStatement ifStmt= (IfStatement)node;
+									ASTNode ifExpression= ifStmt.getExpression();
+									nodeLineNumber= cu.getLineNumber(ifExpression.getStartPosition());
+									Statement elseStmt= ifStmt.getElseStatement();
+									if (elseStmt != null) {
+										int elseLine= cu.getLineNumber(elseStmt.getStartPosition());
+										if (elseLine <= mapWidgetToLineNumber(sourceViewer, textWidgetLineNumber + 1)) {
+											Pattern p= ELSE_PATTERN;
+											nodeLineNumber= elseLine;
 											String stmtLine= textWidget.getLine(mapLineNumberToWidget(sourceViewer, nodeLineNumber - 1));
-											Matcher m= pattern.matcher(stmtLine);
+											Matcher m= p.matcher(stmtLine);
 											while (!m.find() && nodeLineNumber > 1) {
 												nodeLineNumber--;
 												stmtLine= textWidget.getLine(mapLineNumberToWidget(sourceViewer, nodeLineNumber - 1));
-												m= pattern.matcher(stmtLine);
+												m= p.matcher(stmtLine);
 											}
+											node= node.getParent();
 										}
-										break;
-									case ASTNode.SWITCH_CASE:
-									case ASTNode.CASE_DEFAULT_EXPRESSION:
-									case ASTNode.CATCH_CLAUSE:
-										addStickyLine= true;
-										nodeLineNumber= cu.getLineNumber(node.getStartPosition());
-										break;
-									default:
-										break;
-								}
-								if (addStickyLine && nodeLineNumber <= lineNumber) {
-									stickyLines.addFirst(new StickyLine(nodeLineNumber - 1, sourceViewer));
-								}
-								if (node.getNodeType() == ASTNode.MODIFIER) {
-									Modifier modifier= (Modifier)node;
-									startIndentation+= modifier.getLength();
-									node= getASTNode(cu, mapWidgetToLineNumber(sourceViewer, textWidgetLineNumber+1), line);
-								} else {
-									node= node.getParent();
-								}
+									}
+									while (node.getLocationInParent() == IfStatement.ELSE_STATEMENT_PROPERTY) {
+										node= node.getParent();
+									}
+									break;
+								case ASTNode.ENHANCED_FOR_STATEMENT:
+									addStickyLine= true;
+									ASTNode enhancedForExpression= ((EnhancedForStatement)node).getExpression();
+									nodeLineNumber= cu.getLineNumber(enhancedForExpression.getStartPosition());
+									break;
+								case ASTNode.SWITCH_EXPRESSION:
+									addStickyLine= true;
+									ASTNode switchExpExpression= ((SwitchExpression)node).getExpression();
+									nodeLineNumber= cu.getLineNumber(switchExpExpression.getStartPosition());
+									break;
+								case ASTNode.SWITCH_STATEMENT:
+									addStickyLine= true;
+									ASTNode switchStmtExpression= ((SwitchStatement)node).getExpression();
+									nodeLineNumber= cu.getLineNumber(switchStmtExpression.getStartPosition());
+									break;
+								case ASTNode.WHILE_STATEMENT:
+								case ASTNode.DO_STATEMENT:
+								case ASTNode.TRY_STATEMENT:
+								case ASTNode.FOR_STATEMENT:
+								case ASTNode.ANONYMOUS_CLASS_DECLARATION:
+									addStickyLine= true;
+									ASTNode bodyProperty= null;
+									Pattern pattern= null;
+									switch (node.getNodeType()) {
+										case ASTNode.DO_STATEMENT:
+											bodyProperty= ((DoStatement)node).getBody();
+											pattern= DO_PATTERN;
+											break;
+										case ASTNode.FOR_STATEMENT:
+											bodyProperty= ((ForStatement)node).getBody();
+											pattern= FOR_PATTERN;
+											break;
+										case ASTNode.WHILE_STATEMENT:
+											bodyProperty= ((WhileStatement)node).getBody();
+											pattern= WHILE_PATTERN;
+											break;
+										case ASTNode.TRY_STATEMENT:
+											bodyProperty= ((TryStatement)node).getBody();
+											pattern= TRY_PATTERN;
+											break;
+										case ASTNode.ANONYMOUS_CLASS_DECLARATION:
+											bodyProperty= (ASTNode) ((AnonymousClassDeclaration)node).bodyDeclarations().get(0);
+											pattern= NEW_PATTERN;
+											break;
+									}
+									if (bodyProperty != null && pattern != null) {
+										nodeLineNumber= cu.getLineNumber(bodyProperty.getStartPosition());
+										String stmtLine= textWidget.getLine(mapLineNumberToWidget(sourceViewer, nodeLineNumber - 1));
+										Matcher m= pattern.matcher(stmtLine);
+										while (!m.find() && nodeLineNumber > 1) {
+											nodeLineNumber--;
+											stmtLine= textWidget.getLine(mapLineNumberToWidget(sourceViewer, nodeLineNumber - 1));
+											m= pattern.matcher(stmtLine);
+										}
+									}
+									break;
+								case ASTNode.SWITCH_CASE:
+								case ASTNode.CASE_DEFAULT_EXPRESSION:
+								case ASTNode.CATCH_CLAUSE:
+									nodeLineNumber= cu.getLineNumber(node.getStartPosition());
+									break;
+								default:
+									break;
+							}
+							if (addStickyLine && nodeLineNumber <= lineNumber) {
+								stickyLines.addFirst(new StickyLine(nodeLineNumber - 1, sourceViewer));
+							}
+							if (node.getNodeType() == ASTNode.MODIFIER) {
+								Modifier modifier= (Modifier)node;
+								startIndentation+= modifier.getLength();
+								node= getASTNode(cu, mapWidgetToLineNumber(sourceViewer, textWidgetLineNumber+1), line);
+							} else {
+								node= node.getParent();
 							}
 						}
 					}
@@ -265,6 +292,13 @@ public class JavaStickyLinesProvider implements IStickyLinesProvider {
 			}
 		}
 		return stickyLines;
+	}
+
+	private long getDocumentTimestamp(IDocument document) {
+		if (document instanceof AbstractDocument ad) {
+			return ad.getModificationStamp();
+		}
+		return 0;
 	}
 
 	private ASTNode getASTNode(CompilationUnit cu, int lineNum, String line) {
