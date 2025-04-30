546c44bfc5571c379fad2fd4eccc6ffe44dd9760
Jeff Johnston <jjohnstn@redhat.com>
2025-04-30 16:49:12 -0400

    More cleanups prior to M2

--- org.eclipse.jdt.ui/core extension/org/eclipse/jdt/internal/corext/fix/CleanUpConstantsOptions.java
+++ org.eclipse.jdt.ui/core extension/org/eclipse/jdt/internal/corext/fix/CleanUpConstantsOptions.java
@@ -215,7 +215,6 @@ public class CleanUpConstantsOptions extends CleanUpConstants {
 		options.setOption(EXPLICITENCODING_CLEANUP, CleanUpOptions.FALSE);
 		options.setOption(EXPLICITENCODING_KEEP_BEHAVIOR, CleanUpOptions.TRUE);
 		options.setOption(EXPLICITENCODING_INSERT_UTF8, CleanUpOptions.FALSE);
-		options.setOption(EXPLICITENCODING_AGGREGATE_TO_UTF8, CleanUpOptions.FALSE);
 	}
 
 	private static void setSaveParticipantSettings(CleanUpOptions options) {
@@ -411,7 +410,6 @@ public class CleanUpConstantsOptions extends CleanUpConstants {
 		options.setOption(EXPLICITENCODING_CLEANUP, CleanUpOptions.FALSE);
 		options.setOption(EXPLICITENCODING_KEEP_BEHAVIOR, CleanUpOptions.TRUE);
 		options.setOption(EXPLICITENCODING_INSERT_UTF8, CleanUpOptions.FALSE);
-		options.setOption(EXPLICITENCODING_AGGREGATE_TO_UTF8, CleanUpOptions.FALSE);
 	}
 
 	public static void initDefaults(IPreferenceStore store) {
