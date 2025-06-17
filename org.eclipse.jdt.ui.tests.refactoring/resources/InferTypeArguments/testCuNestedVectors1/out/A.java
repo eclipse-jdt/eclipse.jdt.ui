b90dd98066785f70aff5b0a06321e93c42c04439
Jeff Johnston <jjohnstn@redhat.com>
2025-06-17 11:29:54 -0400

    Bug 506243 - Infer Generic Type Arguments should use diamond operator (#2237)
    * Bug 506243 - Infer Generic Type Arguments should use diamond operator
    
    - change InferTypeArgumentsRefactoring to not spell out the type
      for various right-hand expressions
    - fix multiple InferTypeArgumentsTests cases now that behaviour has
      changed
    - replace InferTypeArgumentsTests junit zip files with new behaviour
    - fixes #2236

--- org.eclipse.jdt.ui.tests.refactoring/resources/InferTypeArguments/testCuNestedVectors1/out/A.java
+++ org.eclipse.jdt.ui.tests.refactoring/resources/InferTypeArguments/testCuNestedVectors1/out/A.java
@@ -4,13 +4,13 @@ import java.util.Vector;
 
 class A {
 	void foo() {
-		Vector<Vector<Vector>> v1= new Vector<Vector<Vector>>();
-		Vector<Vector> v2= new Vector<Vector>();
+		Vector<Vector<Vector>> v1= new Vector<>();
+		Vector<Vector> v2= new Vector<>();
 		v2.add(v1);
 		v1.add(v2);
 	}
 	void bar() {
-		Vector<Vector> v3= new Vector<Vector>();
+		Vector<Vector> v3= new Vector<>();
 		v3.add(v3);
 	}
 }
--- org.eclipse.jdt.ui.tests.refactoring/resources/InferTypeArguments/testCuQualifiedName/out/A.java
+++ org.eclipse.jdt.ui.tests.refactoring/resources/InferTypeArguments/testCuQualifiedName/out/A.java
@@ -2,7 +2,7 @@ package p;
 
 public class A {
 	A() {
-		java.util.List<Integer> list= new java.util.ArrayList<Integer>();
+		java.util.List<Integer> list= new java.util.ArrayList<>();
 		list.add(1);
 	}
 }
