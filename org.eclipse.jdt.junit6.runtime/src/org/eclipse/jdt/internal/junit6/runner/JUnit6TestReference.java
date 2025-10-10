24bccdbf612c3ee7e1c09824f27d160e0417a0d8
Ed Merks <ed.merks@gmail.com>
2025-10-10 13:01:52 +0200

    Add org.eclipse.jdt.junit6.runtime
    - It's a copy of org.eclipse.jdt.junit5.runtime but with everything
    renamed for junit 6 includes, packages, classes, the bundle name, and
    references to them.
    - This bundle requires Java 17.
    
    See https://github.com/eclipse-jdt/eclipse.jdt/issues/147

---
+++ org.eclipse.jdt.junit6.runtime/src/org/eclipse/jdt/internal/junit6/runner/JUnit6TestReference.java
@@ -0,0 +1,145 @@
+/*******************************************************************************
+ * Copyright (c) 2016, 2025 IBM Corporation and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.jdt.internal.junit6.runner;
+
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.junit.platform.launcher.Launcher;
+import org.junit.platform.launcher.LauncherDiscoveryRequest;
+import org.junit.platform.launcher.TestExecutionListener;
+import org.junit.platform.launcher.TestIdentifier;
+import org.junit.platform.launcher.TestPlan;
+
+import org.eclipse.jdt.internal.junit.runner.ITestIdentifier;
+import org.eclipse.jdt.internal.junit.runner.ITestReference;
+import org.eclipse.jdt.internal.junit.runner.IVisitsTestTrees;
+import org.eclipse.jdt.internal.junit.runner.RemoteTestRunner;
+import org.eclipse.jdt.internal.junit.runner.TestExecution;
+import org.eclipse.jdt.internal.junit.runner.TestIdMap;
+
+public class JUnit6TestReference implements ITestReference {
+
+	private LauncherDiscoveryRequest fRequest;
+
+	private Launcher fLauncher;
+
+	private TestPlan fTestPlan;
+
+	private RemoteTestRunner fRemoteTestRunner;
+
+	public JUnit6TestReference(LauncherDiscoveryRequest request, Launcher launcher, RemoteTestRunner remoteTestRunner) {
+		fRequest= request;
+		fLauncher= launcher;
+		fRemoteTestRunner= remoteTestRunner;
+		fTestPlan= fLauncher.discover(fRequest);
+	}
+
+	@Override
+	public int countTestCases() {
+		return (int) fTestPlan.countTestIdentifiers(TestIdentifier::isTest);
+	}
+
+	@Override
+	public void sendTree(IVisitsTestTrees notified) {
+		for (TestIdentifier root : fTestPlan.getRoots()) {
+			for (TestIdentifier child : fTestPlan.getChildren(root)) {
+				sendTree(notified, child);
+			}
+		}
+	}
+
+	private void sendTree(IVisitsTestTrees notified, TestIdentifier testIdentifier) {
+		JUnit6Identifier identifier= new JUnit6Identifier(testIdentifier);
+		String parentId= getParentId(testIdentifier, fTestPlan);
+		if (testIdentifier.isTest()) {
+			notified.visitTreeEntry(identifier, false, 1, false, parentId);
+		} else {
+			Set<TestIdentifier> children= fTestPlan.getChildren(testIdentifier);
+			notified.visitTreeEntry(identifier, true, children.size(), false, parentId);
+			for (TestIdentifier child : children) {
+				sendTree(notified, child);
+			}
+		}
+	}
+
+	/**
+	 * @param testIdentifier the test identifier whose parent id is required
+	 * @param testPlan the test plan containing the test
+	 * @return the parent id from {@link TestIdMap} if the parent is present, otherwise
+	 *         <code>"-1"</code>
+	 */
+	private String getParentId(TestIdentifier testIdentifier, TestPlan testPlan) {
+		// Same as JUnit5TestListener.getParentId(TestIdentifier testIdentifier, TestPlan testPlan)
+		return testPlan.getParent(testIdentifier).map(parent -> fRemoteTestRunner.getTestId(new JUnit6Identifier(parent))).orElse("-1"); //$NON-NLS-1$
+	}
+
+	@Override
+	public void run(TestExecution execution) {
+		registerStopListener(fLauncher, execution);
+		boolean foundMethodThatAvoidsRedundantDiscovery;
+		try {
+			fLauncher.getClass().getMethod("execute", TestPlan.class, TestExecutionListener[].class); //$NON-NLS-1$
+			foundMethodThatAvoidsRedundantDiscovery= true;
+		} catch (NoSuchMethodException e) {
+			foundMethodThatAvoidsRedundantDiscovery= false;
+		}
+		if (foundMethodThatAvoidsRedundantDiscovery) {
+			fLauncher.execute(fTestPlan, new JUnit6TestListener(execution.getListener(), fRemoteTestRunner));
+		} else {
+			fLauncher.execute(fRequest, new JUnit6TestListener(execution.getListener(), fRemoteTestRunner));
+		}
+	}
+
+	@Override
+	public ITestIdentifier getIdentifier() { // not used
+		return new JUnit6Identifier(fTestPlan.getRoots().iterator().next());
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (!(obj instanceof JUnit6TestReference))
+			return false;
+
+		JUnit6TestReference ref= (JUnit6TestReference) obj;
+		return (ref.fRequest.equals(fRequest));
+	}
+
+	@Override
+	public int hashCode() {
+		return fRequest.hashCode();
+	}
+
+	@Override
+	public String toString() {
+		return fRequest.toString();
+	}
+
+	// XXX: hack for stopping JUnit 5 tests, use API from https://github.com/junit-team/junit5/issues/1880 once available
+	private static void registerStopListener(Launcher launcher, TestExecution execution) {
+		AtomicBoolean stopped = new AtomicBoolean(false);
+		execution.addStopListener(() -> stopped.set(true));
+		launcher.registerTestExecutionListeners(new TestExecutionListener() {
+			@Override
+			public void executionStarted(TestIdentifier testIdentifier) {
+				if (stopped.get()) {
+					// fake an OOM, since this is the only type of exception that JUnit 5 doesn't swallow
+					// This exception and exact message is handled in RemoteTestRunnerClient
+					throw new OutOfMemoryError("Junit5 test stopped by user"); //$NON-NLS-1$
+				}
+			}
+		});
+	}
+}
