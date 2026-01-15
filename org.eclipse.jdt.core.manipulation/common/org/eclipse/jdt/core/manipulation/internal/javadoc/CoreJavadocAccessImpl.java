69467f063bc9d80e87e51ba9a10de5cb9d8e251c
Suby S Surendran <suby.surendran@ibm.com>
2026-01-15 07:08:10 +0530

    Unexpected behavior of @link or @linkplain with http/https URLs
    This PR fixes an issue where Javadoc @link and @linkplain tags are
    treated as plain text when the referenced URL contains http or https,
    resulting in incorrect rendering in the Javadoc hover.
    
    Fix: https://github.com/eclipse-jdt/eclipse.jdt.ui/issues/2736

--- org.eclipse.jdt.core.manipulation/common/org/eclipse/jdt/core/manipulation/internal/javadoc/CoreJavadocAccessImpl.java
+++ org.eclipse.jdt.core.manipulation/common/org/eclipse/jdt/core/manipulation/internal/javadoc/CoreJavadocAccessImpl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2024 IBM Corporation and others.
+ * Copyright (c) 2008, 2026 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -1517,6 +1517,12 @@ public class CoreJavadocAccessImpl implements IJavadocAccess {
 					SimpleName paramName= param.getName();
 					if (paramName != null)
 						refMethodParamNames[i]= paramName.getIdentifier();
+				}
+			} else if (first instanceof TextElement href) {
+				String tagName = ((TagElement)href.getParent()).getTagName();
+				//should not process anchor tags
+				if (TagElement.TAG_LINK.equals(tagName) || TagElement.TAG_LINKPLAIN.equals(tagName)) {
+					refTypeName = href.getText();
 				}
 			}
 
