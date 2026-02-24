07d1ecae5edc7396578d07a143a93fc5245242ec
Jeff Johnston <jjohnstn@redhat.com>
2026-02-24 16:00:19 -0500

    Fix test

--- org.eclipse.jdt.ui.tests.refactoring/resources/ExtractTemp/canExtract/A_test164_in.java
+++ org.eclipse.jdt.ui.tests.refactoring/resources/ExtractTemp/canExtract/A_test164_in.java
@@ -5,7 +5,7 @@ public class A {
 	public void extractVariableBug() {
 		int a = 1;
 		Integer b = 2;
-		String c = 3;
+		String c = "3";
 		System.out.println(a + b + c);
 	}
 }
--- org.eclipse.jdt.ui.tests.refactoring/resources/ExtractTemp/canExtract/A_test164_out.java
+++ org.eclipse.jdt.ui.tests.refactoring/resources/ExtractTemp/canExtract/A_test164_out.java
@@ -5,7 +5,7 @@ public class A {
 	public void extractVariableBug() {
 		int a = 1;
 		Integer b = 2;
-		String c = 3;
+		String c = "3";
 		String x= b + c;
 		System.out.println(a + x);
 	}
