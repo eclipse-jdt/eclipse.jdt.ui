c30187630d82ba7369a3989f63f5a8e92e427978
Simeon Andreev <simeon.danailov.andreev@gmail.com>
2026-03-25 17:47:42 +0200

    Wait for reconciler background thread in tests
    Tests which open a Java editor should wait on the Java reconciler to finish,
    otherwise the reconciler can run JDT model operations during subsequent tests.
    
    This change adds a wait also for the reconciler background thread,
    on top of waiting on the reconciler family, in SaveParticipantTest and ImportOrganizeTest.
    
    See: #2287

--- org.eclipse.jdt.ui.tests/ui/org/eclipse/jdt/ui/tests/util/TestUtils.java
+++ org.eclipse.jdt.ui.tests/ui/org/eclipse/jdt/ui/tests/util/TestUtils.java
@@ -20,13 +20,56 @@ import org.osgi.framework.ServiceReference;
 
 import org.eclipse.osgi.service.debug.DebugOptions;
 
+import org.eclipse.core.runtime.jobs.Job;
+
+import org.eclipse.jface.internal.text.reconciler.ReconcilerJobFamilies;
+
 import org.eclipse.jdt.internal.core.JavaModelManager;
+
+import org.eclipse.jdt.internal.ui.JavaPlugin;
+import org.eclipse.jdt.internal.ui.text.JavaReconciler;
 
 public class TestUtils {
 
+	private static final String RECONCILER_BACKGROUND_WORKER_THREAD_NAME = JavaReconciler.class.getName();
 
 	public static void waitForIndexer() {
 		JavaModelManager.getIndexManager().waitForIndex(false, null);
+	}
+
+	/**
+	 * Closes all open editors (without saving them), then waits for:
+	 *
+	 * <pre>
+	 * 1. Reconciler family, {@link ReconcilerJobFamilies#FAMILY_RECONCILER}
+	 * 2. Background reconciler threads, {@link JavaReconciler}
+	 * </pre>
+	 *
+	 * @param timeout wait timeout in milliseconds
+	 * @throws RuntimeException if a timeout occurs while waiting
+	 */
+	@SuppressWarnings("restriction")
+	public static void waitForReconciler(long timeout) throws Exception {
+		JavaPlugin.getActivePage().closeAllEditors(false);
+		long s = System.currentTimeMillis();
+		while (System.currentTimeMillis() - s < timeout) {
+			Job[] jobs = Job.getJobManager().find(ReconcilerJobFamilies.FAMILY_RECONCILER);
+			if (jobs.length == 0) {
+				break;
+			}
+			Thread.sleep(50);
+		}
+		// the reconciler starts a background thread, wait for it here
+		while (System.currentTimeMillis() - s < timeout) {
+			boolean reconcilerDone = Thread.getAllStackTraces().keySet().stream().noneMatch(TestUtils::isReconcilerThread);
+			if (reconcilerDone) {
+				break;
+			}
+			Thread.sleep(50);
+		}
+		if (System.currentTimeMillis() - s > timeout) {
+			throw new RuntimeException("Timeout occurred while waiting on reconciler");
+		}
 	}
 
 	/**
@@ -46,5 +89,9 @@ public class TestUtils {
 		} finally {
 			context.ungetService(reference);
 		}
+	}
+
+	private static boolean isReconcilerThread(Thread thread) {
+		return thread != null && RECONCILER_BACKGROUND_WORKER_THREAD_NAME.equals(thread.getName());
 	}
 }
