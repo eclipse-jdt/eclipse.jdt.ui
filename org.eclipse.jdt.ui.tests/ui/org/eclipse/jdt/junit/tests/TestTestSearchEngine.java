f414a0175734288171cb8e2832bbb98272b506f8
Eric Milles <eric.milles@thomsonreuters.com>
2026-03-31 13:55:29 -0500

    Add tests for TestSearchEngine.findTests

--- org.eclipse.jdt.ui.tests/ui/org/eclipse/jdt/junit/tests/TestTestSearchEngine.java
+++ org.eclipse.jdt.ui.tests/ui/org/eclipse/jdt/junit/tests/TestTestSearchEngine.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2020 IBM Corporation and others.
+ * Copyright (c) 2000, 2026 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -13,9 +13,7 @@
  *******************************************************************************/
 package org.eclipse.jdt.junit.tests;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
@@ -45,8 +43,8 @@ import org.eclipse.jdt.internal.junit.util.TestSearchEngine;
 
 import org.eclipse.jdt.internal.ui.util.BusyIndicatorRunnableContext;
 
-
 public class TestTestSearchEngine {
+
 	private IJavaProject fProject;
 	private IPackageFragmentRoot fRoot;
 
@@ -63,6 +61,9 @@ public class TestTestSearchEngine {
 		JavaProjectHelper.delete(fProject);
 	}
 
+	//--------------------------------------------------------------------------
+	// Tests for TestSearchEngine.findTests:
+
 	@Test
 	public void testOnePackage() throws Exception {
 		IPackageFragment p= fRoot.createPackageFragment("p", true, null);
@@ -70,9 +71,9 @@ public class TestTestSearchEngine {
 		ICompilationUnit test2= createCompilationUnit(p, 2);
 
 		var result= findTests(p);
-		assertEqualTypes("Test case not found", List.of(
+		assertThat(result).containsExactlyInAnyOrder(
 				test1.getType("Test1"), test2.getType("Test2")
-			), result);
+		);
 	}
 
 	@Test
@@ -85,9 +86,9 @@ public class TestTestSearchEngine {
 		ICompilationUnit test3= createCompilationUnit(q, 3);
 
 		var result= findTests(new IJavaElement[] {p, q});
-		assertEqualTypes("Test case not found", List.of(
+		assertThat(result).containsExactlyInAnyOrder(
 				test1.getType("Test1"), test2.getType("Test2"), test3.getType("Test3")
-			), result);
+		);
 	}
 
 	@Test
@@ -100,9 +101,9 @@ public class TestTestSearchEngine {
 		createCompilationUnit(q, 3);
 
 		var result= findTests(p);
-		assertEqualTypes("Test case not found", List.of(
+		assertThat(result).containsExactlyInAnyOrder(
 				test1.getType("Test1"), test2.getType("Test2")
-			), result);
+		);
 	}
 
 	@Test
@@ -115,9 +116,9 @@ public class TestTestSearchEngine {
 		ICompilationUnit test3= createCompilationUnit(q, 3);
 
 		var result= findTests(fRoot);
-		assertEqualTypes("Test case not found", List.of(
+		assertThat(result).containsExactlyInAnyOrder(
 				test1.getType("Test1"), test2.getType("Test2"), test3.getType("Test3")
-			), result);
+		);
 	}
 
 	@Test
@@ -136,10 +137,10 @@ public class TestTestSearchEngine {
 		ICompilationUnit test5= createCompilationUnit(r, 5);
 
 		var result= findTests(new IJavaElement[] {fRoot, root2});
-		assertEqualTypes("Test case not found", List.of(
+		assertThat(result).containsExactlyInAnyOrder(
 				test1.getType("Test1"), test2.getType("Test2"), test3.getType("Test3"),
 				test4.getType("Test4"), test5.getType("Test5")
-			), result);
+		);
 	}
 
 	@Test
@@ -158,14 +159,13 @@ public class TestTestSearchEngine {
 		ICompilationUnit test5= createCompilationUnit(r, 5);
 
 		var result= findTests(root2);
-		assertEqualTypes("Test case not found", List.of(
+		assertThat(result).containsExactlyInAnyOrder(
 				test4.getType("Test4"), test5.getType("Test5")
-			), result);
+		);
 	}
 
-	@Test
+	@Test // for https://bugs.eclipse.org/bugs/show_bug.cgi?id=139961
 	public void testTwoPackageFragmentRootsSearchingInOneNoSupertype() throws Exception {
-		// regression test for https://bugs.eclipse.org/bugs/show_bug.cgi?id=139961
 		IPackageFragment p= fRoot.createPackageFragment("p", true, null);
 		createCompilationUnit(p, 1);
 
@@ -178,7 +178,7 @@ public class TestTestSearchEngine {
 				null);
 
 		var result= findTests(root2);
-		assertEqualTypes("Test case not found", List.of(testSub.getType("TestSub")), result);
+		assertThat(result).containsExactlyInAnyOrder(testSub.getType("TestSub"));
 	}
 
 	@Test
@@ -197,10 +197,10 @@ public class TestTestSearchEngine {
 		ICompilationUnit test5= createCompilationUnit(r, 5);
 
 		var result= findTests(fProject);
-		assertEqualTypes("Test case not found", List.of(
+		assertThat(result).containsExactlyInAnyOrder(
 				test1.getType("Test1"), test2.getType("Test2"), test3.getType("Test3"),
 				test4.getType("Test4"), test5.getType("Test5")
-			), result);
+		);
 	}
 
 	@Test
@@ -212,8 +212,10 @@ public class TestTestSearchEngine {
 		createCompilationUnit(q, 2);
 
 		var result= findTests(p);
-		assertEqualTypes("Test case not found", List.of(test1.getType("Test1")), result);
+		assertThat(result).containsExactlyInAnyOrder(test1.getType("Test1"));
 	}
+
+	//
 
 	private List<IType> findTests(IJavaElement element) throws InvocationTargetException, InterruptedException {
 		ITestKind testKind= TestKindRegistry.getContainerTestKind(fProject);
@@ -221,19 +223,12 @@ public class TestTestSearchEngine {
 	}
 
 	private List<IType> findTests(IJavaElement[] elements) throws InvocationTargetException, InterruptedException {
-		HashSet<IType> res= new HashSet<>();
+		var set= new HashSet<IType>();
 		for (IJavaElement element : elements) {
 			var types= findTests(element);
-			res.addAll(types);
+			set.addAll(types);
 		}
-		return new ArrayList<>(res);
-	}
-
-	@Test
-	public void testJUnit4NoSrc() throws Exception {
-		//regression test for https://bugs.eclipse.org/bugs/show_bug.cgi?id=151003
-		IType noTest= fProject.findType("java.lang.Integer");
-		assertFalse(new JUnit4TestFinder().isTest(noTest));
+		return new ArrayList<>(set);
 	}
 
 	private ICompilationUnit createCompilationUnit(IPackageFragment pack, int number) throws JavaModelException {
@@ -243,10 +238,182 @@ public class TestTestSearchEngine {
 			true, null);
 	}
 
-	private void assertEqualTypes(String message, List<IType> expected, List<IType> actual) {
-		assertEquals("Wrong number of found tests", expected.size(), actual.size());
-		for (int i= 0; i < actual.size(); i++) {
-			assertTrue(message + expected.get(i).getFullyQualifiedName(), expected.contains(actual.get(i)));
-		}
+	//--------------------------------------------------------------------------
+	// Tests for TestSearchEngine.findTests:
+
+	@Test
+	public void testNoMethods() throws Exception {
+		IPackageFragment pack= fRoot.createPackageFragment("p", true, null);
+		ICompilationUnit unit= pack.createCompilationUnit("SomeTest.java",
+				"""
+				package p;
+				import org.junit.*;
+				public class SomeTest {
+				}
+				""", true, null);
+
+		var names = findTestMethods(unit.findPrimaryType());
+
+		assertThat(names).isEmpty();
+	}
+
+	@Test
+	public void testOneMethod() throws Exception {
+		IPackageFragment pack= fRoot.createPackageFragment("p", true, null);
+		ICompilationUnit unit= pack.createCompilationUnit("SomeTest.java",
+				"""
+				package p;
+				import org.junit.*;
+				public class SomeTest {
+					@Test public void someTest() {}
+				}
+				""", true, null);
+
+		var names = findTestMethods(unit.findPrimaryType());
+
+		assertThat(names).containsExactlyInAnyOrder("someTest");
+	}
+
+	@Test
+	public void testTwoMethods() throws Exception {
+		IPackageFragment pack= fRoot.createPackageFragment("p", true, null);
+		ICompilationUnit unit= pack.createCompilationUnit("SomeTest.java",
+				"""
+				package p; import org.junit.*;
+				public class SomeTest {
+					static  void testOne() {}
+					private void testTwo() {}
+					public  void testThree() {}
+					@Test public void four() {}
+				}
+				""", true, null);
+
+		var names = findTestMethods(unit.findPrimaryType());
+
+		assertThat(names).containsExactlyInAnyOrder("four", "testThree");
+	}
+
+	@Test
+	public void testSuperMethods() throws Exception {
+		IPackageFragment pack= fRoot.createPackageFragment("p", true, null);
+		pack.createCompilationUnit("A.java",
+				"""
+				package p;
+				import org.junit.*;
+				public interface A {
+					@Test default void a() {}
+				}
+				""", true, null);
+		pack.createCompilationUnit("B.java",
+				"""
+				package p;
+				import org.junit.*;
+					public abstract class B {
+					public abstract void x();
+					@Test void b() {}
+				}"
+				""", true, null);
+		ICompilationUnit unit= pack.createCompilationUnit("C.java",
+				"""
+				package p;
+				public class C extends B implements A {
+					@Override public void x() {}
+				}
+				""", true, null);
+
+		var names = findTestMethods(unit.findPrimaryType());
+
+		assertThat(names).containsExactlyInAnyOrder("a", "b");
+	}
+
+	@Test
+	public void testRejectMethods() throws Exception {
+		IPackageFragment pack= fRoot.createPackageFragment("p", true, null);
+		ICompilationUnit unit= pack.createCompilationUnit("C.java",
+				"""
+				package p;
+				import org.junit.*;"
+				public class C {
+					public void tester(Object o) {}
+					public int tests() {return 42;}
+				}
+				""", true, null);
+
+		var names = findTestMethods(unit.findPrimaryType());
+
+		assertThat(names).isEmpty();
+	}
+
+	@Test
+	public void testLegacyMethods() throws Exception {
+		JavaProjectHelper.removeFromClasspath(fProject, JUnitCore.JUNIT4_CONTAINER_PATH);
+		JavaProjectHelper.addToClasspath(fProject, JavaCore.newContainerEntry(JUnitCore.JUNIT3_CONTAINER_PATH));
+
+		IPackageFragment pack= fRoot.createPackageFragment("p", true, null);
+		ICompilationUnit unit= pack.createCompilationUnit("C.java",
+				"""
+				package p;
+				public class C extends junit.framework.TestCase {
+					public void testFoo() {}
+					public void testBar() {}
+					public void testBaz() {}
+				}
+				""", true, null);
+
+		var names = findTestMethods(unit.findPrimaryType());
+
+		assertThat(names).containsExactlyInAnyOrder("testFoo", "testBar", "testBaz");
+	}
+
+	@Test
+	public void testJupiterMethods() throws Exception {
+		JavaProjectHelper.removeFromClasspath(fProject, JUnitCore.JUNIT4_CONTAINER_PATH);
+		JavaProjectHelper.addToClasspath(fProject, JavaCore.newContainerEntry(JUnitCore.JUNIT5_CONTAINER_PATH));
+
+		IPackageFragment pack= fRoot.createPackageFragment("p", true, null);
+		ICompilationUnit unit= pack.createCompilationUnit("C.java",
+				"""
+				package p;
+				import java.util.*;
+				import org.junit.jupiter.api.*;
+				import org.junit.jupiter.params.*;
+				import org.junit.jupiter.params.provider.*;
+				import org.junit.platform.commons.annotation.*;
+				import static org.junit.jupiter.api.DynamicTest.*;
+				class C {
+					void nope() {}
+					@Test void foo() {}
+					@Testable void bar() {}
+					@TestTemplate void baz(int i) {}
+					@ParameterizedTest @CsvSource("!") void boz(String s) {}
+					@RepeatedTest(2) void foofoo(RepetitionInfo repsInfo) {}
+					@TestFactory Collection<DynamicTest> fizzBuzz() {
+						return List.of(
+							dynamicTest("fizz", () -> {}),
+							dynamicTest("buzz", () -> {}));
+					}
+				}
+				""", true, null);
+
+		var names = findTestMethods(unit.findPrimaryType());
+
+		assertThat(names).containsExactlyInAnyOrder(
+				"foo", "bar", "baz(int)", "boz(java.lang.String)",
+				"foofoo(org.junit.jupiter.api.RepetitionInfo)", "fizzBuzz");
+	}
+
+	//
+
+	private List<String> findTestMethods(IType type) throws InvocationTargetException, InterruptedException {
+		var testKind= (org.eclipse.jdt.internal.junit.launcher.TestKind) TestKindRegistry.getContainerTestKind(fProject);
+		return new ArrayList<>(TestSearchEngine.findTestMethods(new BusyIndicatorRunnableContext(), fProject, type, testKind));
+	}
+
+	//--------------------------------------------------------------------------
+
+	@Test // for https://bugs.eclipse.org/bugs/show_bug.cgi?id=151003
+	public void testJUnit4NoSrc() throws Exception {
+		IType noTest= fProject.findType("java.lang.Integer");
+		assertThat(new JUnit4TestFinder().isTest(noTest)).isFalse();
 	}
 }
