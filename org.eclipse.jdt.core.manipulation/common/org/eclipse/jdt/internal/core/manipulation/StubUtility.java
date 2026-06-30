a46fc4bc8af37eb53202733367f3c4c1a154e0e7
Sougandh S <Sougandh.S@ibm.com>
2026-07-01 02:56:39 +0530

    Honor record params on creating javadoc with "Generate Javadoc Element" (#3048)
    Fixed AddJavaDocStubOperation to properly generate @param tags for
    record components when creating Javadoc comments for record types.

--- org.eclipse.jdt.core.manipulation/common/org/eclipse/jdt/internal/core/manipulation/StubUtility.java
+++ org.eclipse.jdt.core.manipulation/common/org/eclipse/jdt/internal/core/manipulation/StubUtility.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2025 IBM Corporation and others.
+ * Copyright (c) 2000, 2026 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -389,6 +389,14 @@ public class StubUtility {
 			typeParametersNames[i]= typeParameters[i].getElementName();
 		}
 		return typeParametersNames;
+	}
+
+	public static String[] getRecordParameterNames(IField[] recordParameters) {
+		String[] recordParametersNames= new String[recordParameters.length];
+		for (int i= 0; i < recordParameters.length; i++) {
+			recordParametersNames[i]= recordParameters[i].getElementName();
+		}
+		return recordParametersNames;
 	}
 
 	/**
--- org.eclipse.jdt.ui/core extension/org/eclipse/jdt/internal/corext/codemanipulation/AddJavaDocStubOperation.java
+++ org.eclipse.jdt.ui/core extension/org/eclipse/jdt/internal/corext/codemanipulation/AddJavaDocStubOperation.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2021 IBM Corporation and others.
+ * Copyright (c) 2000, 2026 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -72,6 +72,10 @@ public class AddJavaDocStubOperation implements IWorkspaceRunnable {
 
 	private String createTypeComment(IType type, String lineDelimiter) throws CoreException {
 		String[] typeParameterNames= StubUtility.getTypeParameterNames(type.getTypeParameters());
+		if (type.isRecord() && type.getRecordComponents().length > 0) {
+			String[] recordParameterNames= StubUtility.getRecordParameterNames(type.getRecordComponents());
+			return CodeGeneration.getTypeComment(type.getCompilationUnit(), type.getTypeQualifiedName('.'), typeParameterNames,recordParameterNames, lineDelimiter);
+		}
 		return CodeGeneration.getTypeComment(type.getCompilationUnit(), type.getTypeQualifiedName('.'), typeParameterNames, lineDelimiter);
 	}
 
