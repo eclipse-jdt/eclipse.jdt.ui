467bf5531f27040bd1ab958c8570402f74c9b497
Jeff Johnston <jjohnstn@redhat.com>
2026-08-12 19:39:02 -0400

    Add new tests to JavadocHoverTests and MarkdownCommentTests (#3114)

--- org.eclipse.jdt.ui.tests/ui/org/eclipse/jdt/ui/tests/hover/MarkdownCommentTests.java
+++ org.eclipse.jdt.ui.tests/ui/org/eclipse/jdt/ui/tests/hover/MarkdownCommentTests.java
@@ -1116,6 +1116,24 @@ public class MarkdownCommentTests extends CoreTests {
 	}
 
 	@Test
+	public void testReturnWithLink_02() throws CoreException {
+		String source= """
+				/// {@return [Object#hashCode] the method}
+				public class Markdown {}
+				""";
+		ICompilationUnit cu= getWorkingCopy("/TestSetupProject/src/p/Markdown.java", source, null);
+		assertNotNull("Markdown.java", cu);
+
+		String expectedContent = "Returns:</dt><dd><code><a href='eclipse-javadoc:%E2%98%82=TestSetupProject/src%3Cp%7BMarkdown.java%E2%98%83Markdown%E2%98%82Object%E2%98%82hashCode'>Object.hashCode</a></code> the method</dd>";
+		IType type= cu.getType("Markdown");
+		String actualHtmlContent= getHoverHtmlContent(cu, type);
+		int index= actualHtmlContent.lastIndexOf("Returns");
+		assertNotEquals(-1, index);
+		String actualSnippet= actualHtmlContent.substring(index, index + expectedContent.length());
+		assertEquals("sequence doesn't match", expectedContent, actualSnippet);
+	}
+
+	@Test
 	public void testInheritedMethod() throws CoreException {
 		String source= """
 				public class Base {
